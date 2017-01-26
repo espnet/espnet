79fd9aa4f6cb9758e6342304b0cb6a1a5b66ddae
SeanNaren <seannaren@hotmail.com>
2017-01-26 20:01:20 +0000

    Use unittest for tests

--- pytorch_binding/tests/test.py
+++ pytorch_binding/tests/test.py
@@ -1,72 +1,84 @@
+"""
+Tests are based on the torch7 bindings for warp-ctc. Reference numbers are also obtained from the tests.
+"""
+import unittest
+
 import torch
 from torch.autograd import Variable
-
 from warpctc_pytorch import CTCLoss
 
 ctc_loss = CTCLoss()
+places = 5
 
 
-def simple_test():
-    probs = torch.FloatTensor([[[0.1, 0.6, 0.1, 0.1, 0.1], [0.1, 0.1, 0.6, 0.1, 0.1]]]).transpose(0, 1).contiguous()
-    labels = Variable(torch.IntTensor([1, 2]))
-    label_sizes = Variable(torch.IntTensor([2]))
-    sizes = Variable(torch.IntTensor([2]))
+def run_grads(label_sizes, labels, probs, sizes):
     probs = Variable(probs, requires_grad=True)
     cost = ctc_loss(probs, labels, sizes, label_sizes)
     cost.backward()
-    print('CPU_cost: %f' % cost.data[0])
+    cpu_cost = cost.data[0]
     probs = Variable(probs.data.cuda(), requires_grad=True)
     cost = ctc_loss(probs, labels, sizes, label_sizes)
     cost.backward()
-    print('GPU_cost: %f' % cost.data[0])
+    gpu_cost = cost.data[0]
     grads = probs.grad
     print(grads.view(grads.size(0) * grads.size(1), grads.size(2)))
+    return cpu_cost, gpu_cost
 
 
-def medium_test(multiplier):
-    probs = torch.FloatTensor([
-        [[0.1, 0.6, 0.1, 0.1, 0.1], [0.1, 0.1, 0.6, 0.1, 0.1]],
-        [[0.6, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.5, 0.2, 0.1]]
-    ]).contiguous() * multiplier
+class TestCases(unittest.TestCase):
+    def test_simple(self):
+        probs = torch.FloatTensor([[[0.1, 0.6, 0.1, 0.1, 0.1], [0.1, 0.1, 0.6, 0.1, 0.1]]]).transpose(0, 1).contiguous()
+        labels = Variable(torch.IntTensor([1, 2]))
+        label_sizes = Variable(torch.IntTensor([2]))
+        sizes = Variable(torch.IntTensor([2]))
+        cpu_cost, gpu_cost = run_grads(label_sizes, labels, probs, sizes)
+        expected_cost = 2.4628584384918
+        self.assertEqual(cpu_cost, gpu_cost)
+        self.assertAlmostEqual(cpu_cost, expected_cost, places)
 
-    labels = Variable(torch.IntTensor([1, 2, 1, 2]))
-    label_sizes = Variable(torch.IntTensor([2, 2]))
-    sizes = Variable(torch.IntTensor([2, 2]))
-    probs = Variable(probs, requires_grad=True)
-    cost = ctc_loss(probs, labels, sizes, label_sizes)
-    cost.backward()
-    print('CPU_cost: %f' % cost.data[0])
-    probs = Variable(probs.data.cuda(), requires_grad=True)
-    cost = ctc_loss(probs, labels, sizes, label_sizes)
-    cost.backward()
-    print('GPU_cost: %f' % cost.data[0])
-    grads = probs.grad
-    print(grads.view(grads.size(0) * grads.size(1), grads.size(2)))
+    def test_medium(self):
+        probs = torch.FloatTensor([
+            [[0.1, 0.6, 0.1, 0.1, 0.1], [0.1, 0.1, 0.6, 0.1, 0.1]],
+            [[0.6, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.5, 0.2, 0.1]]
+        ]).contiguous()
+
+        labels = Variable(torch.IntTensor([1, 2, 1, 2]))
+        label_sizes = Variable(torch.IntTensor([2, 2]))
+        sizes = Variable(torch.IntTensor([2, 2]))
+        cpu_cost, gpu_cost = run_grads(label_sizes, labels, probs, sizes)
+        expected_cost = 6.0165174007416
+        self.assertEqual(cpu_cost, gpu_cost)
+        self.assertAlmostEqual(cpu_cost, expected_cost, places)
+
+    def test_medium_stability(self):
+        multiplier = 200
+        probs = torch.FloatTensor([
+            [[0.1, 0.6, 0.1, 0.1, 0.1], [0.1, 0.1, 0.6, 0.1, 0.1]],
+            [[0.6, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.5, 0.2, 0.1]]
+        ]).contiguous() * multiplier
+
+        labels = Variable(torch.IntTensor([1, 2, 1, 2]))
+        label_sizes = Variable(torch.IntTensor([2, 2]))
+        sizes = Variable(torch.IntTensor([2, 2]))
+        cpu_cost, gpu_cost = run_grads(label_sizes, labels, probs, sizes)
+        expected_cost = 199.96618652344
+        self.assertEqual(cpu_cost, gpu_cost)
+        self.assertAlmostEqual(cpu_cost, expected_cost, places)
+
+    def test_empty_label(self):
+        probs = torch.FloatTensor([
+            [[0.1, 0.6, 0.1, 0.1, 0.1], [0.1, 0.1, 0.6, 0.1, 0.1]],
+            [[0.6, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.5, 0.2, 0.1]]
+        ]).contiguous()
+
+        labels = Variable(torch.IntTensor([1, 2]))
+        label_sizes = Variable(torch.IntTensor([2, 0]))
+        sizes = Variable(torch.IntTensor([2, 2]))
+        cpu_cost, gpu_cost = run_grads(label_sizes, labels, probs, sizes)
+        expected_cost = 6.416517496109
+        self.assertEqual(cpu_cost, gpu_cost)
+        self.assertAlmostEqual(cpu_cost, expected_cost, places)
 
 
-def empty_label_test():
-    probs = torch.FloatTensor([
-        [[0.1, 0.6, 0.1, 0.1, 0.1], [0.1, 0.1, 0.6, 0.1, 0.1]],
-        [[0.6, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.5, 0.2, 0.1]]
-    ]).contiguous()
-
-    labels = Variable(torch.IntTensor([1, 2]))
-    label_sizes = Variable(torch.IntTensor([2, 0]))
-    sizes = Variable(torch.IntTensor([2, 2]))
-    probs = Variable(probs, requires_grad=True)
-    cost = ctc_loss(probs, labels, sizes, label_sizes)
-    cost.backward()
-    print('CPU_cost: %f' % cost.data[0])
-    probs = Variable(probs.data.cuda(), requires_grad=True)
-    cost = ctc_loss(probs, labels, sizes, label_sizes)
-    cost.backward()
-    print('GPU_cost: %f' % cost.data[0])
-    grads = probs.grad
-    print(grads.view(grads.size(0) * grads.size(1), grads.size(2)))
-
-simple_test()
-medium_test(1.0)
-print("Stability test")
-medium_test(200.0)  # test SM stability if compiled with USE_NSM this will not have nans
-print("Empty label test")
-empty_label_test()
+if __name__ == '__main__':
+    unittest.main()
