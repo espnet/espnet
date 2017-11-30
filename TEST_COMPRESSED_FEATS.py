db9616fc7f99d04f630d385819ec27a1f237fbcd
Karel Vesely <vesis84@gmail.com>
2017-11-30 19:37:22 +0100

    adding compatibility for both python2 and python3

--- TEST_COMPRESSED_FEATS.py
+++ TEST_COMPRESSED_FEATS.py
@@ -1,19 +1,20 @@
 #!/bin/env python
 
+import numpy as np
 import kaldi_io
 import timeit
 
 t_beg = timeit.default_timer()
 orig = {k:m for k,m in kaldi_io.read_mat_ark('data/feats.ark')}
-print timeit.default_timer() - t_beg;
+print(timeit.default_timer() - t_beg);
 
 t_beg = timeit.default_timer()
 comp = {k:m for k,m in kaldi_io.read_mat_ark('data/feats_compressed.ark')}
-print timeit.default_timer() - t_beg;
+print(timeit.default_timer() - t_beg);
 # ~8-10x slower, this is already reasonable,
 
 for key in orig.keys():
-  print key, np.sum(np.abs(comp[key]-orig[key]))
+  print(key, np.sum(np.abs(comp[key]-orig[key])))
 # => The values are not identical, but very similar.
 #    Can it be the `order' of arithmetic operations?
 
