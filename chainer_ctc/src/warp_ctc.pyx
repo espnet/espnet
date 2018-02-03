6e0ac2ac2e0fe191bd59ffaf028f7837525c99a3
Jahn Heymann <heymann@nt.upb.de>
2018-02-03 15:48:31 +0100

    Fixes errors due to changes in recent warpctc version

--- chainer_ctc/src/warp_ctc.pyx
+++ chainer_ctc/src/warp_ctc.pyx
@@ -1,8 +1,9 @@
 cdef extern from "ctc.h":
-    struct ctcComputeInfo:
+    struct ctcOptions:
         CTCComputeLocation loc
         Stream stream
         unsigned int num_threads
+        int blank_label
 
     CTCStatus compute_ctc_loss(float* activations,
                              float* gradients,
@@ -13,13 +14,13 @@ cdef extern from "ctc.h":
                              int minibatch,
                              float* costs,
                              void* workspace,
-                             ctcComputeInfo info)
+                             ctcOptions info)
 
     CTCStatus get_workspace_size(int* label_lengths,
                            int* input_lengths,
                            int alphabet_size,
                            int minibatch,
-                           ctcComputeInfo info,
+                           ctcOptions info,
                            size_t* size_bytes)
 
     const char* ctcGetStatusString(CTCStatus status)
@@ -51,12 +52,13 @@ cpdef void ctc_get_workspace_size_gpu(size_t label_lengths,
                                    int alphabet_size, int minibatch,
                                    size_t size_bytes,
                                    size_t stream):
-    cdef ctcComputeInfo compute_info
-    compute_info.loc = <CTCComputeLocation> CTC_GPU
-    compute_info.stream = <Stream> stream
+    cdef ctcOptions ctc_options
+    ctc_options.loc = <CTCComputeLocation> CTC_GPU
+    ctc_options.stream = <Stream> stream
+    ctc_options.blank_label = 0
     status = get_workspace_size(
         <int*> label_lengths, <int*> input_lengths,
-        alphabet_size, minibatch, compute_info,
+        alphabet_size, minibatch, ctc_options,
         <size_t*> size_bytes
     )
     check_status(status)
@@ -72,14 +74,15 @@ cpdef void ctc_compute_ctc_loss_gpu(size_t activations,
                                  size_t costs,
                                  size_t workspace,
                                  size_t stream) except *:
-    cdef ctcComputeInfo compute_info
-    compute_info.loc = <CTCComputeLocation> CTC_GPU
-    compute_info.stream = <Stream> stream
+    cdef ctcOptions ctc_options
+    ctc_options.loc = <CTCComputeLocation> CTC_GPU
+    ctc_options.stream = <Stream> stream
+    ctc_options.blank_label = 0
     status = compute_ctc_loss(
         <float*> activations, <float*> gradients, <int*> flat_labels,
         <int*> label_lengths, <int*> input_lengths, alphabet_size,
         minibatch, <float*> costs, <void*> workspace,
-        compute_info
+        ctc_options
     )
     check_status(status)
 
@@ -88,12 +91,13 @@ cpdef void ctc_get_workspace_size_cpu(size_t label_lengths,
                                    size_t input_lengths,
                                    int alphabet_size, int minibatch,
                                    size_t size_bytes):
-    cdef ctcComputeInfo compute_info
-    compute_info.loc = <CTCComputeLocation> CTC_CPU
-    compute_info.num_threads = 1
+    cdef ctcOptions ctc_options
+    ctc_options.loc = <CTCComputeLocation> CTC_CPU
+    ctc_options.num_threads = 1
+    ctc_options.blank_label = 0
     status = get_workspace_size(
         <int*> label_lengths, <int*> input_lengths,
-        alphabet_size, minibatch, compute_info,
+        alphabet_size, minibatch, ctc_options,
         <size_t*> size_bytes
     )
     check_status(status)
@@ -109,13 +113,14 @@ cpdef void ctc_compute_ctc_loss_cpu(size_t activations,
                                  size_t costs,
                                  size_t workspace,
                                  unsigned int num_threads) except *:
-    cdef ctcComputeInfo compute_info
-    compute_info.loc = <CTCComputeLocation> CTC_CPU
-    compute_info.num_threads = num_threads
+    cdef ctcOptions ctc_options
+    ctc_options.loc = <CTCComputeLocation> CTC_CPU
+    ctc_options.num_threads = 1
+    ctc_options.blank_label = 0
     status = compute_ctc_loss(
         <float*> activations, <float*> gradients, <int*> flat_labels,
         <int*> label_lengths, <int*> input_lengths, alphabet_size,
         minibatch, <float*> costs, <void*> workspace,
-        compute_info
+        ctc_options
     )
     check_status(status)
