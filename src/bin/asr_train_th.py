7516ba405a92cff61bedf9046303deecc6446144
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2018-02-09 17:11:26 +0900

    fixed to be same as chainer version

--- src/bin/asr_train_th.py
+++ src/bin/asr_train_th.py
@@ -366,7 +366,6 @@ def main():
     np.random.seed(nseed)
     torch.manual_seed(nseed)
     os.environ['CHAINER_SEED'] = str(nseed)
-    torch.backends.cudnn.deterministic = True
     logging.info('chainer seed = ' + os.environ['CHAINER_SEED'])
 
     # debug mode setting
@@ -379,8 +378,10 @@ def main():
     # use determinisitic computation or not
     if args.debugmode < 1:
         chainer.config.cudnn_deterministic = False
+        torch.backends.cudnn.deterministic = False
         logging.info('chainer cudnn deterministic is disabled')
     else:
+        torch.backends.cudnn.deterministic = True
         chainer.config.cudnn_deterministic = True
     # load dictionary for debug log
     if args.dict is not None:
