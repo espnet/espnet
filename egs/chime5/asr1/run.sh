9a873c9eb89825fe81d93fee97e106fb5f5eefa4
Shinji Watanabe <sw005320@gmail.com>
2018-02-23 21:27:55 -0500

    making 10 language-independent ASR and CHiME-5

--- egs/chime5/asr1/run.sh
+++ egs/chime5/asr1/run.sh
@@ -22,7 +22,7 @@ do_delta=false # true when using CNN
 # network archtecture
 # encoder related
 etype=vggblstmp     # encoder architecture type
-elayers=8
+elayers=6
 eunits=320
 eprojs=320
 subsample=1_2_2_1_1 # skip every n frame from input to nth layers
@@ -39,6 +39,10 @@ aconv_filts=100
 # hybrid CTC/attention
 mtlalpha=0.5
 
+# label smoothing
+lsm_type=unigram
+lsm_weight=0.05
+
 # minibatch related
 batchsize=30
 maxlen_in=800  # if input length  > maxlen_in, batchsize is automatically reduced
@@ -49,8 +53,7 @@ opt=adadelta
 epochs=15
 
 # rnnlm related
-batchsize_lm=64
-lm_weight=0.2
+lm_weight=0.3
 
 # decoding parameter
 beam_size=20
@@ -61,8 +64,9 @@ ctc_weight=0.3
 recog_model=acc.best # set a model to be used for decoding: 'acc.best' or 'loss.best'
 
 # data
-hkust1=/export/corpora/LDC/LDC2005S15/
-hkust2=/export/corpora/LDC/LDC2005T32/
+chime5_corpus=/home/shinji/work/201711chime5/chime5-prep
+json_dir=${chime5_corpus}/data/transcriptions_aligned.v2
+audio_dir=${chime5_corpus}/data/audio
 
 # exp tag
 tag="" # tag for managing experiments.
@@ -78,27 +82,44 @@ set -e
 set -u
 set -o pipefail
 
-train_set=train_nodup_sp
-train_dev=train_dev
-recog_set="dev train_dev"
+enhancement=beamformit
+train_set=train_worn_u200k
+train_dev=dev_${enhancement}_ref
+test_sets="dev_${enhancement}_ref eval_${enhancement}_ref"
 
 if [ ${stage} -le 0 ]; then
     ### Task dependent. You have to make data the following preparation part by yourself.
     ### But you can utilize Kaldi recipes in most cases
     echo "stage 0: Data preparation"
-    local/hkust_data_prep.sh ${hkust1} ${hkust2}
-    local/hkust_format_data.sh
-    # upsample audio from 8k to 16k to make a recipe consistent with others
-    for x in train dev; do
-        sed -i.bak -e "s/$/ sox -R -t wav - -t wav - rate 16000 dither | /" data/${x}/wav.scp
+    for mictype in worn u01 u02 u04 u05 u06; do
+	local/prepare_data.sh --mictype ${mictype} \
+			      ${audio_dir}/train ${json_dir}/train data/train_${mictype}
     done
-    # remove space in text
-    for x in train dev; do
-        cp data/${x}/text data/${x}/text.org
-        paste -d " " <(cut -f 1 -d" " data/${x}/text.org) <(cut -f 2- -d" " data/${x}/text.org | tr -d " ") \
-            > data/${x}/text
-        rm data/${x}/text.org
+    for dataset in dev eval; do
+	for mictype in worn; do
+	    local/prepare_data.sh --mictype ${mictype} \
+				  ${audio_dir}/${dataset} ${json_dir}/${dataset} \
+				  data/${dataset}_${mictype}
+	done
     done
+    enhandir=enhan
+    for dset in dev eval; do
+	for mictype in u01 u02 u03 u04 u05 u06; do
+	    local/run_beamformit.sh --cmd "$train_cmd" \
+				    ${audio_dir}/${dset} \
+				    ${enhandir}/${dset}_${enhancement}_${mictype} \
+				    ${mictype} &
+	done
+    done
+    wait
+    for dset in dev eval; do
+	local/prepare_data.sh --mictype ref "$PWD/${enhandir}/${dset}_${enhancement}_u0*" \
+			      ${json_dir}/${dset} data/${dset}_${enhancement}_ref
+    done
+
+    utils/combine_data.sh data/train_uall data/train_u01 data/train_u02 data/train_u04 data/train_u05 data/train_u06
+    utils/subset_data_dir.sh data/train_uall 200000 data/train_u200k
+    utils/combine_data.sh data/${train_set} data/train_worn data/train_u200k
 fi
 
 feat_tr_dir=${dumpdir}/${train_set}/delta${do_delta}; mkdir -p ${feat_tr_dir}
@@ -109,57 +130,41 @@ if [ ${stage} -le 1 ]; then
     echo "stage 1: Feature Generation"
     fbankdir=fbank
     # Generate the fbank features; by default 80-dimensional fbanks with pitch on each frame
-    steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 32 data/train exp/make_fbank/train ${fbankdir}
-    steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 10 data/dev exp/make_fbank/dev ${fbankdir}
-
-    # make a dev set
-    utils/subset_data_dir.sh --first data/train 4000 data/${train_dev}
-    utils/fix_data_dir.sh data/${train_dev}
-    n=$[`cat data/train/segments | wc -l` - 4000]
-    utils/subset_data_dir.sh --last data/train $n data/train_nodev
-
-    # make a training set
-    utils/data/remove_dup_utts.sh 300 data/train_nodev data/train_nodup
-
-    # speed-perturbed
-    utils/perturb_data_dir_speed.sh 0.9 data/train_nodup data/temp1
-    utils/perturb_data_dir_speed.sh 1.0 data/train_nodup data/temp2
-    utils/perturb_data_dir_speed.sh 1.1 data/train_nodup data/temp3
-    utils/combine_data.sh --extra-files utt2uniq data/${train_set} data/temp1 data/temp2 data/temp3
-    rm -r data/temp1 data/temp2 data/temp3
-    steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 32 data/${train_set} exp/make_fbank/${train_set} ${fbankdir}
+    for x in ${train_set} ${test_sets}; do
+        steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 20 data/${x} exp/make_fbank/${x} ${fbankdir}
+    done
 
     # compute global CMVN
     compute-cmvn-stats scp:data/${train_set}/feats.scp data/${train_set}/cmvn.ark
 
     # dump features for training
-    split_dir=`echo $PWD | awk -F "/" '{print $NF "/" $(NF-1)}'`
     if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d ${feat_tr_dir}/storage ]; then
     utils/create_split_dir.pl \
-        /export/a{11,12,13,14}/${USER}/espnet-data/egs/${split_dir}/dump/${train_set}/delta${do_delta}/storage \
+        /export/b{10,11,12,13}/${USER}/espnet-data/egs/chime5/asr1/dump/${train_set}/delta${do_delta}/storage \
         ${feat_tr_dir}/storage
     fi
     if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d ${feat_dt_dir}/storage ]; then
     utils/create_split_dir.pl \
-        /export/a{11,12,13,14}/${USER}/espnet-data/egs/${split_dir}/dump/${train_dev}/delta${do_delta}/storage \
+        /export/b{10,11,12,13}/${USER}/espnet-data/egs/chime5/asr1/dump/${train_dev}/delta${do_delta}/storage \
         ${feat_dt_dir}/storage
     fi
     dump.sh --cmd "$train_cmd" --nj 32 --do_delta $do_delta \
         data/${train_set}/feats.scp data/${train_set}/cmvn.ark exp/dump_feats/train ${feat_tr_dir}
-    dump.sh --cmd "$train_cmd" --nj 10 --do_delta $do_delta \
+    dump.sh --cmd "$train_cmd" --nj 4 --do_delta $do_delta \
         data/${train_dev}/feats.scp data/${train_set}/cmvn.ark exp/dump_feats/dev ${feat_dt_dir}
 fi
 
 dict=data/lang_1char/${train_set}_units.txt
-echo "dictionary: ${dict}"
 nlsyms=data/lang_1char/non_lang_syms.txt
+
+echo "dictionary: ${dict}"
 if [ ${stage} -le 2 ]; then
     ### Task dependent. You have to check non-linguistic symbols used in the corpus.
     echo "stage 2: Dictionary and Json Data Preparation"
     mkdir -p data/lang_1char/
 
     echo "make a non-linguistic symbol list"
-    cut -f 2- data/${train_set}/text | grep -o -P '\[.*?\]' | sort | uniq > ${nlsyms}
+    cut -f 2- data/${train_set}/text | tr " " "\n" | sort | uniq | grep "[" > ${nlsyms}
     cat ${nlsyms}
 
     echo "make a dictionary"
@@ -175,14 +180,15 @@ if [ ${stage} -le 2 ]; then
          data/${train_dev} ${dict} > ${feat_dt_dir}/data.json
 fi
 
+# It takes a few days. If you just want to end-to-end ASR without LM,
 # you can skip this and remove --rnnlm option in the recognition (stage 5)
-lmexpdir=exp/train_rnnlm_2layer_bs${batchsize_lm}
+lmexpdir=exp/train_rnnlm_2layer_bs64
 mkdir -p ${lmexpdir}
 if [ ${stage} -le 3 ]; then
     echo "stage 3: LM Preparation"
     lmdatadir=data/local/lm_train
     mkdir -p ${lmdatadir}
-    text2token.py -s 1 -n 1 -l ${nlsyms} data/train_nodup/text | cut -f 2- -d" " | perl -pe 's/\n/ <eos> /g' \
+    text2token.py -s 1 -n 1 -l ${nlsyms} data/${train_set}/text | cut -f 2- -d" " | perl -pe 's/\n/ <eos> /g' \
         > ${lmdatadir}/train_trans.txt
     cat ${lmdatadir}/train_trans.txt | tr '\n' ' ' > ${lmdatadir}/train.txt
     text2token.py -s 1 -n 1 -l ${nlsyms} data/${train_dev}/text | cut -f 2- -d" " | perl -pe 's/\n/ <eos> /g' \
@@ -191,15 +197,18 @@ if [ ${stage} -le 3 ]; then
         lm_train.py \
         --gpu ${gpu} \
         --verbose 1 \
-        --batchsize ${batchsize_lm} \
         --outdir ${lmexpdir} \
         --train-label ${lmdatadir}/train.txt \
         --valid-label ${lmdatadir}/valid.txt \
+        --batchsize 64 \
         --dict ${dict}
 fi
 
 if [ -z ${tag} ]; then
     expdir=exp/${train_set}_${etype}_e${elayers}_subsample${subsample}_unit${eunits}_proj${eprojs}_ctc${ctctype}_d${dlayers}_unit${dunits}_${atype}_aconvc${aconv_chans}_aconvf${aconv_filts}_mtlalpha${mtlalpha}_${opt}_bs${batchsize}_mli${maxlen_in}_mlo${maxlen_out}
+    if [ "${lsm_type}" != "" ]; then
+        expdir=${expdir}_lsm${lsm_type}${lsm_weight}
+    fi
     if ${do_delta}; then
         expdir=${expdir}_delta
     fi
@@ -219,6 +228,7 @@ fi
 
 if [ ${stage} -le 4 ]; then
     echo "stage 4: Network Training"
+
     ${cuda_cmd} ${expdir}/train.log \
         ${train_script} \
         --gpu ${gpu} \
@@ -245,6 +255,8 @@ if [ ${stage} -le 4 ]; then
         --aconv-chans ${aconv_chans} \
         --aconv-filts ${aconv_filts} \
         --mtlalpha ${mtlalpha} \
+        --lsm-type ${lsm_type} \
+        --lsm-weight ${lsm_weight} \
         --batch-size ${batchsize} \
         --maxlen-in ${maxlen_in} \
         --maxlen-out ${maxlen_out} \
@@ -294,8 +306,8 @@ if [ ${stage} -le 5 ]; then
             --lm-weight ${lm_weight} &
         wait
 
-        score_sclite.sh --nlsyms ${nlsyms} ${expdir}/${decode_dir} ${dict}
-
+        score_sclite.sh --wer true --nlsyms ${nlsyms} ${expdir}/${decode_dir} ${dict}
+            
     ) &
     done
     wait
