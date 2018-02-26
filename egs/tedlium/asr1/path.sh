cdcdbcb0fdc099e31d93a1dd43fcee21863764f7
Nanxin Chen <bobchennan@gmail.com>
2018-02-26 11:06:17 -0500

    revert changes to cmd.sh and path.sh

--- egs/tedlium/asr1/path.sh
+++ egs/tedlium/asr1/path.sh
@@ -1,7 +1,6 @@
 MAIN_ROOT=$PWD/../../..
 KALDI_ROOT=$MAIN_ROOT/tools/kaldi
 SPNET_ROOT=$MAIN_ROOT/src
-num_gpu=$1
 
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sctk/bin:$PWD:$PATH
@@ -16,5 +15,5 @@ export PYTHONPATH=$SPNET_ROOT/asr/:$SPNET_ROOT/nets/:$SPNET_ROOT/utils/:$SPNET_R
 
 export OMP_NUM_THREADS=1
 if [[ $(hostname -f) == *.clsp.jhu.edu ]] ; then
-    export CUDA_VISIBLE_DEVICES=$(/usr/local/bin/free-gpu -n $num_gpu)
+    export CUDA_VISIBLE_DEVICES=$(/usr/local/bin/free-gpu)
 fi
