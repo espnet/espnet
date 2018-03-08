cb54623de8bce68af437a6b2458a4861ebe1d31d
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2018-03-08 17:28:29 +0900

    updated to be compatible with new lm train

--- egs/hkust/asr1/run.sh
+++ egs/hkust/asr1/run.sh
@@ -189,6 +189,7 @@ if [ ${stage} -le 3 ]; then
     ${cuda_cmd} ${lmexpdir}/train.log \
         lm_train.py \
         --gpu ${gpu} \
+        --backend ${backend} \
         --verbose 1 \
         --batchsize ${batchsize_lm} \
         --outdir ${lmexpdir} \
