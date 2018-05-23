36f834a9925b1c4c394bc581a6c0fa0b7ffc1ad8
Shinji Watanabe <sw005320@gmail.com>
2018-05-23 19:05:42 -0400

    avoid python3 error

--- src/bin/asr_train.py
+++ src/bin/asr_train.py
@@ -8,6 +8,7 @@ import argparse
 import logging
 import os
 import random
+import platform
 import subprocess
 import sys
 
@@ -148,10 +149,18 @@ def main():
 
     # check CUDA_VISIBLE_DEVICES
     if args.ngpu > 0:
-        if "clsp.jhu.edu" in subprocess.check_output(["hostname", "-f"]):
-            cvd = subprocess.check_output(["/usr/local/bin/free-gpu", "-n", str(args.ngpu)]).strip()
-            logging.info('CLSP: use gpu' + cvd)
-            os.environ['CUDA_VISIBLE_DEVICES'] = cvd
+        # python 2 case
+        if platform.python_version_tuple()[0] == '2':
+            if "clsp.jhu.edu" in subprocess.check_output(["hostname", "-f"]):
+                cvd = subprocess.check_output(["/usr/local/bin/free-gpu", "-n", str(args.ngpu)]).strip()
+                logging.info('CLSP: use gpu' + cvd)
+                os.environ['CUDA_VISIBLE_DEVICES'] = cvd
+        # python 3 case
+        else: # if platform.python_version_tuple()[0] == '3':
+            if "clsp.jhu.edu" in subprocess.check_output(["hostname", "-f"]).decode():
+                cvd = subprocess.check_output(["/usr/local/bin/free-gpu", "-n", str(args.ngpu)]).decode().strip()
+                logging.info('CLSP: use gpu' + cvd)
+                os.environ['CUDA_VISIBLE_DEVICES'] = cvd
 
         cvd = os.environ.get("CUDA_VISIBLE_DEVICES")
         if cvd is None:
