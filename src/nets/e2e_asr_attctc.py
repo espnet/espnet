88c821c85b2d377dc86f808873450c0e75c6bb6c
Shinji Watanabe <sw005320@gmail.com>
2018-05-25 16:10:59 -0400

    fix a bug in a BLSTM encoder

--- src/nets/e2e_asr_attctc.py
+++ src/nets/e2e_asr_attctc.py
@@ -112,7 +112,7 @@ class E2E(chainer.Chain):
         # subsample info
         # +1 means input (+1) and layers outputs (args.elayer)
         subsample = np.ones(args.elayers + 1, dtype=np.int)
-        if args.etype == 'blstmp':
+        if args.etype == 'blstmp' or args.etype == 'blstm':
             ss = args.subsample.split("_")
             for j in range(min(args.elayers + 1, len(ss))):
                 subsample[j] = int(ss[j])
