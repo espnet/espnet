f2f4ebbc920ccace4ff329fbc392acadac18430e
Harish Mallidi <mallidi.harish@gmail.com>
2018-06-09 02:52:56 -0400

    Per Shinji's suggestion,     combined to ${data_train}_org     removed copy dirs

--- egs/librispeech/asr1/run.sh
+++ egs/librispeech/asr1/run.sh
@@ -124,14 +124,11 @@ if [ ${stage} -le 1 ]; then
         steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 32 data/${x} exp/make_fbank/${x} ${fbankdir}
     done
 
-    utils/combine_data.sh data/${train_set} data/train_clean_100 data/train_clean_360 data/train_other_500
-    utils/combine_data.sh data/${train_dev} data/dev_clean data/dev_other
+    utils/combine_data.sh data/${train_set}_org data/train_clean_100 data/train_clean_360 data/train_other_500
+    utils/combine_data.sh data/${train_dev}_org data/dev_clean data/dev_other
 
     # remove utt having more than 3000 frames
     # remove utt having more than 400 characters
-    utils/copy_data_dir.sh data/${train_set} data/${train_set}_org
-    utils/copy_data_dir.sh data/${train_dev} data/${train_dev}_org
-
     remove_longshortdata.sh --maxframes 3000 --maxchars 400 data/${train_set}_org data/${train_set}
     remove_longshortdata.sh --maxframes 3000 --maxchars 400 data/${train_dev}_org data/${train_dev}
 
