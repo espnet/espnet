13fa832fc8a2eb697feaa2121e5f39ed54d12f1a
Nelson Yalta <nyalta21@gmail.com>
2018-06-15 11:54:43 -0400

    first commit

--- src/utils/mergejson.py
+++ src/utils/mergejson.py
@@ -12,6 +12,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('jsons', type=str, nargs='+',
                         help='json files')
+    parser.add_argument('--multi', '-m', type=int,
+                        help='Test the json file for multiple input/output', default=0)
     parser.add_argument('--verbose', '-V', default=0, type=int,
                         help='Verbose option')
     args = parser.parse_args()
@@ -47,5 +49,31 @@ if __name__ == '__main__':
         new_dic[k] = v
         
     # ensure "ensure_ascii=False", which is a bug
+    j = json.dumps({'utts': new_dic}, indent=4, ensure_ascii=False).encode('utf_8')
+
+    #j = json.load(open(args.json))
+    old_dic = j['utts']
+
+    new_dic = {}
+    for item in old_dic.items():
+        id, dic = item
+
+        in_dic = {}
+        if dic.has_key(unicode('idim', 'utf-8')):
+            in_dic[unicode('shape', 'utf-8')] = (int(dic[unicode('ilen', 'utf-8')]), int(dic[unicode('idim', 'utf-8')]))
+        in_dic[unicode('name', 'utf-8')] = unicode('input1', 'utf-8')
+        in_dic[unicode('feat', 'utf-8')] = dic[unicode('feat', 'utf-8')]
+
+        out_dic = {}
+        out_dic[unicode('name', 'utf-8')] = unicode('target1', 'utf-8')
+        out_dic[unicode('shape', 'utf-8')] = (int(dic[unicode('olen', 'utf-8')]), int(dic[unicode('odim', 'utf-8')]))
+        out_dic[unicode('text', 'utf-8')] = dic[unicode('text', 'utf-8')]
+        out_dic[unicode('token', 'utf-8')] = dic[unicode('token', 'utf-8')]
+        out_dic[unicode('tokenid', 'utf-8')] = dic[unicode('tokenid', 'utf-8')]
+
+
+        new_dic[id] = {unicode('input', 'utf-8'):[in_dic], unicode('output', 'utf-8'):[out_dic],
+            unicode('utt2spk', 'utf-8'):dic[unicode('utt2spk', 'utf-8')]}
+        
     jsonstring = json.dumps({'utts': new_dic}, indent=4, ensure_ascii=False).encode('utf_8')
     print(jsonstring)
