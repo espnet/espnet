f50dfffb07a85d476eca52e8ca2409b400cbb011
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2018-07-07 10:25:10 +0900

    fixed test script

--- test/test_e2e_tts.py
+++ test/test_e2e_tts.py
@@ -164,7 +164,8 @@ def test_tacotron2_with_speaker_embedding_trainable_and_decodable(model_dict, lo
     # define model
     model_args = make_model_args(**model_dict)
     loss_args = make_loss_args(**loss_dict)
-    model = Tacotron2(idim, odim, spk_embed_dim, **model_args)
+    model_args.spk_embed_dim = spk_embed_dim
+    model = Tacotron2(idim, odim, **model_args)
     criterion = Tacotron2Loss(model, **loss_args)
     optimizer = torch.optim.Adam(model.parameters())
 
