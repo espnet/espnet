b37e8a24cb10906ef0ea3b0e3caab175fe40a799
Jiro Nishitoba <j.nshtb@gmail.com>
2018-07-25 20:52:20 +0000

    fix to run stage 2.

--- egs/jsalt18e2e/asr1/path.sh
+++ egs/jsalt18e2e/asr1/path.sh
@@ -8,7 +8,7 @@ export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sctk/bin
 . $KALDI_ROOT/tools/config/common_path.sh
 export LC_ALL=C
 
-export PATH=$MAIN_ROOT/tools/sentencepiece/src:$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sph2pipe_v2.5/:$MAIN_ROOT/tools/nkf/nkf-2.1.4/:$PWD:$PATH
+export PATH=$MAIN_ROOT/tools/sentencepiece/build/src:$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sph2pipe_v2.5/:$MAIN_ROOT/tools/nkf/nkf-2.1.4/:$PWD:$PATH
 
 export PATH=$SPNET_ROOT/utils/:$SPNET_ROOT/bin/:$PATH
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$MAIN_ROOT/tools/chainer_ctc/ext/warp-ctc/build
