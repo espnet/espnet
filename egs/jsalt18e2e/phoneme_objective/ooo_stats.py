ca7176e9bd61cc833299dd5385ae1228b56c89bd
Oliver Adams <oliver.adams@gmail.com>
2018-08-22 06:40:21 -0400

    Gathering stats about %age of tokens that are out of orthography.

---
+++ egs/jsalt18e2e/phoneme_objective/ooo_stats.py
@@ -0,0 +1,33 @@
+""" Out-of-orthography statistics for babel predictions."""
+
+from pathlib import Path
+
+langs = ["assamese", "tagalog", "swahili", "zulu", "bengali", "tokpisin",
+         "georgian", "haitian", "cantonese", "kurmanji", "turkish", "lao",
+         "pashto", "tamil", "tokpisin", "vietnamese"]
+
+# First determine the orthographies from the training data
+invs = {}
+dicts = Path("dicts")
+for lang in langs:
+    lang_dict = dicts / "{}_dict.txt".format(lang)
+    with lang_dict.open() as f:
+        lines = f.readlines()
+    inv = set([line.split()[0] for line in lines])
+    invs[lang] = inv
+
+# Then gather the predictions from exp dir decodings
+for lang in langs:
+    total = 0
+    ooo = 0
+    hyp_path = """exp/tr_babel10_blstmp_e4_subsample1_2_2_1_1_unit320_proj320_d1_unit300_location_aconvc10_aconvf100_mtlalpha0.33_phonemeweight0.33_adadelta_bs50_mli800_mlo150/decode_et_babel_{}_beam20_eacc.best_p0.0_len0.0-0.0_ctcw0.3/hyp.grapheme.trn""".format(lang)
+    with open(hyp_path) as f:
+        for line in f:
+            toks = line.split()[:-1]
+            total += len(toks)
+            for tok in toks:
+                if tok not in invs[lang]:
+                    ooo += 1
+    print("OOO for {}: {}".format(lang, ooo/total))
+
+# Then determine the %age of characters that were in the original orthography
