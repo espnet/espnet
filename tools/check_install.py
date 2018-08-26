a62aabe629caffa97c8b3e76d0fe0823f44f33a3
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2018-08-26 12:37:21 +0900

    updated to be more flexible

--- tools/check_install.py
+++ tools/check_install.py
@@ -3,9 +3,22 @@
 # Copyright 2018 Nagoya University (Tomoki Hayashi)
 #  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
 
+import argparse
+import importlib
 import logging
 import sys
 
+# you should add the libraries which does not included in requirements.txt
+MANUALLY_INSTALLED_LIBRARIES = [
+    ('matplotlib', None),
+    ('chainer_ctc', None),
+    ('warpctc_pytorch', None)
+]
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--requirements', '-r', default='./requirements.txt', type=str,
+                    help='requirements.txt')
+args = parser.parse_args()
 
 logging.basicConfig(
     level=logging.INFO,
@@ -13,104 +26,87 @@ logging.basicConfig(
 
 logging.info("python version = " + sys.version)
 
-# check library availableness
-try:
-    import chainer
-    logging.info("chainer is installed.")
-except ModuleNotFoundError:
-    logging.warn("chainer is not installed.")
-try:
-    import cupy  # NOQA
-    logging.info("cupy is installed.")
-except ModuleNotFoundError:
-    logging.warn("cupy is not installed.")
-try:
-    import inflect  # NOQA
-    logging.info("inflect is installed.")
-except ModuleNotFoundError:
-    logging.warn("inflect is not installed.")
-try:
-    import librosa  # NOQA
-    logging.info("librosa is installed.")
-except ModuleNotFoundError:
-    logging.warn("librosa is not installed.")
-try:
-    import matplotlib  # NOQA
-    logging.info("matplotlib is installed.")
-except ModuleNotFoundError:
-    logging.warn("matplotlib is not installed.")
-try:
-    import soundfile  # NOQA
-    logging.info("soundfile is installed.")
-except ModuleNotFoundError:
-    logging.warn("soundfile is not installed.")
-try:
-    import torch  # NOQA
-    logging.info("torch is installed.")
-except ModuleNotFoundError:
-    logging.warn("torch is not installed.")
-try:
-    import unidecode  # NOQA
-    logging.info("unidecode is installed.")
-except ModuleNotFoundError:
-    logging.warn("unidecode is not installed.")
-try:
-    import warpctc_pytorch  # NOQA
-    logging.info("pytorch warpctc is installed.")
-except ModuleNotFoundError:
-    logging.warn("pytorch warpctc is not installed.")
-try:
-    import chainer_ctc  # NOQA
-    logging.info("chainer warpctc is installed")
-except ModuleNotFoundError:
-    logging.warn("chainer warpctc is not installed")
+# load requirements
+with open(args.requirements, 'r') as f:
+    lines = f.readlines()
 
-# check version
-if 'torch' in globals():
+# parse requirements
+library_list = []
+for line in lines:
+    line = line.replace('\n', '')
+    if '==' in line:
+        name, version = line.split('==')
+    elif '>=' in line:
+        name, _ = line.split('>=')
+        version = None
+    else:
+        name = line
+        version = None
+    library_list.append((name, version))
+
+# add some library manually
+library_list.extend(MANUALLY_INSTALLED_LIBRARIES)
+
+# chech library availableness
+logging.info("library availableness check start.")
+logging.info("# libraries to be checked = %d" % len(library_list))
+is_collect_installed_list = [None] * len(library_list)
+for idx, (name, version) in enumerate(library_list):
     try:
-        assert torch.__version__ == '0.4.1'
-        logging.info("torch version is matched.")
-    except AssertionError:
-        logging.warn("torch version is not matched. please install torch==0.4.1.")
-if 'chainer' in globals():
-    try:
-        assert chainer.__version__ == '4.3.1'
-        logging.info("chainer version is matched.")
-    except AssertionError:
-        logging.warn("chainer version is not matched. please install chainer==4.3.1.")
+        importlib.import_module(name)
+        logging.info("--> %s is installed." % name)
+        is_collect_installed_list[idx] = True
+    except ModuleNotFoundError:
+        logging.warn("--> %s is not installed." % name)
+        is_collect_installed_list[idx] = False
+    if version is not None:
+        try:
+            lib = importlib.import_module(name)
+            assert lib.__version__ == version
+            logging.info("--> %s version is matched." % name)
+            is_collect_installed_list[idx] = True
+        except AssertionError:
+            logging.warn("--> %s version is not matched. please install %s==%s." % (name, name, version))
+            is_collect_installed_list[idx] = False
+logging.info("library availableness check done.")
+logging.info("%d / %d libraries are correctly installed." % (
+    sum(is_collect_installed_list), len(library_list)))
 
 # check cuda availableness
-if 'torch' in globals():
+if len(library_list) == sum(is_collect_installed_list):
+    logging.info("cuda availableness check start.")
+    import chainer
+    import torch
     try:
         assert torch.cuda.is_available()
-        logging.info("cuda is available in torch.")
+        logging.info("--> cuda is available in torch.")
     except AssertionError:
-        logging.warn("it seems that cuda is not available in torch.")
+        logging.warn("--> it seems that cuda is not available in torch.")
     try:
         assert torch.backends.cudnn.is_available()
-        logging.info("cudnn is available in torch.")
+        logging.info("--> cudnn is available in torch.")
     except AssertionError:
-        logging.warn("it seems that cudnn is not available in torch.")
-if 'chainer' in globals():
+        logging.warn("--> it seems that cudnn is not available in torch.")
     try:
         assert chainer.backends.cuda.available
-        logging.info("cuda is available in chainer.")
+        logging.info("--> cuda is available in chainer.")
     except AssertionError:
-        logging.warn("it seems that cuda is not available in chainer.")
+        logging.warn("--> it seems that cuda is not available in chainer.")
     try:
         assert chainer.backends.cuda.cudnn_enabled
-        logging.info("cudnn is available in chainer.")
+        logging.info("--> cudnn is available in chainer.")
     except AssertionError:
-        logging.warn("it seems that cudnn is not available in chainer.")
-if 'cupy' in globals():
+        logging.warn("--> it seems that cudnn is not available in chainer.")
     try:
         from cupy.cuda import nccl  # NOQA
-        logging.info("nccl is installed.")
+        logging.info("--> nccl is installed.")
     except ImportError:
-        logging.warn("nccl is not installed. multi-gpu is not enabled.")
-if 'torch' in globals() and torch.cuda.is_available():
+        logging.warn("--> it seems that nccl is not installed. multi-gpu is not enabled.")
+        logging.warn("--> if you want to use multi-gpu, please install it and then re-setup.")
     try:
         assert torch.cuda.device_count() > 1
-        logging.info("multi-gpu is available (#gpus = %d)." % torch.cuda.device_count())
+        logging.info("--> multi-gpu is available (#gpus = %d)." % torch.cuda.device_count())
     except AssertionError:
-        logging.warn("it seems that only single gpu is available.")
+        logging.warn("--> it seems that only single gpu is available.")
+        logging.warn('--> maybe your machine has only one gpu.')
+    logging.info("cuda availableness check done.")
