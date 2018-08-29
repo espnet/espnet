1802439c729d83581735b4b2623bf0b4d3f2134d
Nelson Yalta <nyalta21@gmail.com>
2018-08-29 11:42:58 -0400

    docker updates

--- docker/run.sh
+++ docker/run.sh
@@ -8,72 +8,8 @@ docker_cudnn=7
 docker_user=0
 docker_env=
 
-while test $# -gt 0
-do
-    case "$1" in
-        -h) echo "Usage: `basename $0` [-h] docker_gpu docker_egs docker_folders options"
-            exit 0;;
-        --help) echo "Usage: `basename $0` [-h] ] docker_gpu docker_egs docker_folders options"
-              exit 0;;
-        --docker*) ext=${1#--}
-              frombreak=true
-              for i in _ {a..z} {A..Z}; do
-                for var in `eval echo "\\${!$i@}"`; do
-                  if [ "$var" == "$ext" ]; then
-                    eval $ext=$2
-                    frombreak=false
-                    break 2
-                  fi 
-                done 
-              done
-              if $frombreak ; then
-                echo "bad option $1" 
-                exit 1
-              fi
-              ;;
-        --*) break
-              ;;
-    esac
-    shift
-    shift
-done
+. ./parser.sh || exit 1
 
-if [ -z "${docker_egs}" ]; then
-  echo "Select an example to work with from the egs folder."
-  exit 1
-fi
-
-from_image="ubuntu:16.04"
-image_label="espnet:ubuntu16.04"
-if [ ! "${docker_gpu}" == "-1" ]; then
-  if [ -z "${docker_cuda}" ]; then
-    # If the docker_cuda is not set, the program will automatically 
-    # search the installed version with default configurations (apt)
-    docker_cuda=$( nvcc -V | grep release )
-    docker_cuda=${docker_cuda#*"release "}
-    docker_cuda=${docker_cuda%,*}
-    if [ ! -z "${docker_cuda}" ]; then
-      docker_cudnn=$( cat /usr/local/cuda/include/cudnn.h | grep "#define CUDNN_MAJOR" )
-      docker_cudnn=${docker_cudnn#*MAJOR}
-      docker_cudnn=${docker_cudnn// /}
-      if [ ! -z "${docker_cudnn}" ]; then
-        from_image="nvidia/cuda:${docker_cuda}-cudnn${docker_cudnn}-devel-ubuntu16.04"
-        image_label="espnet:cuda${docker_cuda}-cudnn${docker_cudnn}-ubuntu16.04"
-      else
-        echo "CUDNN was not found in default folder."
-        from_image="nvidia/cuda:${docker_cuda}-devel-ubuntu16.04"
-        image_label="espnet:cuda${docker_cuda}-ubuntu16.04"
-      fi
-    else
-      echo "CUDA was not found, selecting CPU image. For GPU image, install NVIDIA-DOCKER, CUDA and NVCC."
-    fi
-  else
-    from_image="nvidia/cuda:${docker_cuda}-cudnn${docker_cudnn}-devel-ubuntu16.04"
-    image_label="espnet:cuda${docker_cuda}-cudnn${docker_cudnn}-ubuntu16.04"
-  fi
-fi
-echo "Using image ${from_image}."
-docker_image=$( docker images -q ${image_label} ) 
 cd ..
 
 if ! [[ -n ${docker_image}  ]]; then
@@ -89,9 +25,10 @@ if ! [[ -n ${docker_image}  ]]; then
   else
     build_args="${build_args} --build-arg THIS_USER=root"
   fi
-  echo "Now running docker build ${build_args} -f docker/Dockerfile -t ${image_label} ."
-  (docker build ${build_args} -f docker/Dockerfile -t ${image_label} .) || exit 1
+  echo "Now running docker build ${build_args} -f ${docker_file} -t ${image_label} ."
+  (docker build ${build_args} -f ${docker_file} -t ${image_label} .) || exit 1
 fi
+
 
 vols="-v ${PWD}/egs:/espnet/egs -v ${PWD}/src:/espnet/src -v ${PWD}/test:/espnet/test"
 if [ ! -z "${docker_folders}" ]; then
@@ -121,14 +58,6 @@ if [ ! -z "${docker_env}" ]; then
   this_env="-e ${docker_env}"
 fi
 
-if [ "${docker_gpu}" == "-1" ]; then
-  cmd0="docker"
-  container_name="espnet_cpu"
-else
-  # --rm erase the container when the training is finished.
-  container_name="espnet_gpu${docker_gpu//,/_}"
-  cmd0="NV_GPU='${docker_gpu}' nvidia-docker"
-fi
 cmd="${cmd0} run -i --rm ${this_env} --name ${container_name} ${vols} ${image_label} /bin/bash -c '${cmd}'"
 
 echo "Executing application in Docker"
