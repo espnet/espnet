79cd06d1fd84de2c02baa6cd21fd0a7a8e5ec492
hirofumi0810 <hiro.mhbc@gmail.com>
2018-08-30 19:18:10 -0400

    Add XML parser

---
+++ egs/iwslt18st/st1/local/parse_xml.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python2
+# encoding: utf-8
+
+# Copyright 2018 Kyoto University (Hirofumi Inaguma)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+import argparse
+import logging
+import re
+from xml.etree.ElementTree import parse
+
+from collections import OrderedDict
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('text', type=str, default=False, nargs='?',
+                        help='input text')
+    args = parser.parse_args()
+
+    tree = parse(args.text)
+    elem = tree.getroot()
+
+    talk_id = None
+
+    # Parse a XML file
+    trans_dict_all = OrderedDict()
+    for e in elem.getiterator():
+        if e.tag == 'doc':
+            talk_id = e.get('docid')
+            trans_dict_all[talk_id] = OrderedDict()
+        elif e.tag == 'seg':
+            utt_id = int(e.get('id'))
+            trans = e.text.encode('utf-8')
+
+            # Remove consecutive spaces
+            trans = re.sub(r'[\s]+', ' ', trans)
+
+            # Remove the first space
+            if trans[0] == ' ':
+                trans = trans[1:]
+
+            # Remove the last space
+            if trans[-1] == ' ':
+                trans = trans[:-1]
+
+            trans_dict_all[talk_id][utt_id] = trans
+
+    for talk_id, trans_dict in trans_dict_all.items():
+        for utt_id, trans in trans_dict.items():
+            print("%s %03d %s" % (talk_id, utt_id, trans))
+
+
+if __name__ == '__main__':
+    main()
