2181aa9edea95d737eeb295d224bfa0b3bee990a
creatorscan <creatorscan@gmail.com>
2018-09-14 12:43:09 +0200

    path for jsalt

--- egs/jsalt18e2e/asr1/path.sh
+++ egs/jsalt18e2e/asr1/path.sh
@@ -1,11 +1,6 @@
-MAIN_ROOT=../../../
+MAIN_ROOT=$PWD/../../..
 KALDI_ROOT=$MAIN_ROOT/tools/kaldi
 SPNET_ROOT=$MAIN_ROOT/src
-BUT_CONDA=/homes/kazi/baskar/anaconda3/
-CUDAROOT=/usr/local/share/cuda-9.0.176
-
-export CUDA_HOME=$CUDAROOT
-export CUDA_PATH=$CUDAROOT
 
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sctk/bin:$PWD:$PATH
@@ -13,16 +8,9 @@ export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sctk/bin
 . $KALDI_ROOT/tools/config/common_path.sh
 export LC_ALL=C
 
-export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sph2pipe_v2.5/:$CUDAROOT/bin:$PWD:$PATH
-
-export PATH=$SPNET_ROOT/utils/:$SPNET_ROOT/bin/:$PATH
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$MAIN_ROOT/tools/chainer_ctc/ext/warp-ctc/build:$CUDAROOT/lib64
-#source $MAIN_ROOT/tools/venv/bin/activate
-source $BUT_CONDA/bin/activate py36_cuda9_torch4
-export PYTHONPATH=$BUT_CONDA/envs/py27/bin:$SPNET_ROOT/lm/:$SPNET_ROOT/asr/:$SPNET_ROOT/nets/:$SPNET_ROOT/utils/:$SPNET_ROOT/bin/:$PYTHONPATH
+export PATH=$MAIN_ROOT/tools/sentencepiece/build/src:$SPNET_ROOT/utils/:$SPNET_ROOT/bin/:$MAIN_ROOT/tools/nkf/nkf-2.1.4/:$PATH
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$MAIN_ROOT/tools/chainer_ctc/ext/warp-ctc/build
+source $MAIN_ROOT/tools/venv/bin/activate
+export PYTHONPATH=$SPNET_ROOT/lm/:$SPNET_ROOT/asr/:$SPNET_ROOT/nets/:$SPNET_ROOT/utils/:$SPNET_ROOT/bin/:$PYTHONPATH
 
 export OMP_NUM_THREADS=1
-export CC="/usr/local/bin/gcc-5.3"
-export CXX="/usr/local/bin/g++-5.3"
-
-export CPATH=$CUDA_HOME/include
