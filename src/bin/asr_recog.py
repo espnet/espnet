c178ba696af0d3900e4d3e7598dae72d7e463142
Hiroshi Seki <mn5k1102hs@gmail.com>
2018-09-21 16:53:25 +0900

    add TODO

--- src/bin/asr_recog.py
+++ src/bin/asr_recog.py
@@ -93,6 +93,11 @@ def main():
             logging.error("#gpus is not matched with CUDA_VISIBLE_DEVICES.")
             sys.exit(1)
 
+        # TODO(mn5k): support of multiple GPUs
+        if args.ngpu > 1:
+            logging.error("The program only supports ngpu=1.")
+            sys.exit(1)
+
     # display PYTHONPATH
     logging.info('python path = ' + os.environ['PYTHONPATH'])
 
