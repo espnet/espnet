a3442b4575a01f695bf9b10ae8d3dc15e4b149a0
creatorscan <creatorscan@gmail.com>
2018-09-22 12:01:12 +0200

    asr torch

--- src/asr/asr_pytorch.py
+++ src/asr/asr_pytorch.py
@@ -166,6 +166,32 @@ class CustomConverter(object):
 
         return xs_pad, ilens, ys_pad
 
+class DataParallel(torch.nn.DataParallel):
+    def scatter(self, inputs, kwargs, device_ids, dim):
+        r"""Scatter with support for kwargs dictionary"""
+        if len(inputs) == 1:
+            inputs = inputs[0]
+        avg = int(math.ceil(len(inputs) * 1. / len(device_ids)))
+        # inputs = scatter(inputs, device_ids, dim) if inputs else []
+        inputs = [[inputs[i:i + avg]] for i in range(0, len(inputs), avg)]
+        kwargs = torch.nn.scatter(kwargs, device_ids, dim) if kwargs else []
+        if len(inputs) < len(kwargs):
+            inputs.extend([() for _ in range(len(kwargs) - len(inputs))])
+        elif len(kwargs) < len(inputs):
+            kwargs.extend([{} for _ in range(len(inputs) - len(kwargs))])
+        inputs = tuple(inputs)
+        kwargs = tuple(kwargs)
+        return inputs, kwargs
+
+    def forward(self, *inputs, **kwargs):
+        if not self.device_ids:
+            return self.module(*inputs, **kwargs)
+        inputs, kwargs = self.scatter(inputs, kwargs, self.device_ids, self.dim)
+        if len(self.device_ids) == 1:
+            return self.module(*inputs[0], **kwargs[0])
+        replicas = self.replicate(self.module, self.device_ids[:len(inputs)])
+        outputs = self.parallel_apply(replicas, inputs, kwargs)
+        return self.gather(outputs, self.output_device)
 
 def train(args):
     '''Run training'''
@@ -266,7 +292,7 @@ def train(args):
     # actual bathsize is included in a list
     train_iter = chainer.iterators.MultiprocessIterator(
         TransformDataset(train, converter.transform),
-        batch_size=1, n_processes=1, n_prefetch=8, maxtasksperchild=20)
+        batch_size=1, n_processes=1, n_prefetch=8)#, maxtasksperchild=20)
     valid_iter = chainer.iterators.SerialIterator(
         TransformDataset(valid, converter.transform),
         batch_size=1, repeat=False, shuffle=False)
