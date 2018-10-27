c1a882a1a48076cc3fa79b062473bc9345444e44
Naoyuki Kamo <naoyuki.kamo829@gmail.com>
2018-10-27 22:44:58 +0900

    Fix for travis

--- setup.py
+++ setup.py
@@ -10,6 +10,41 @@ def get_all_scripts(dirname):
             if os.path.splitext(f)[1] == '.py' and f != '__init__.py']
 
 
+requirements = {
+    'install': [
+        # for some reason, including matplotlib in requirements.txt causes errors, and
+        # matplotlib should be separately pip installed
+        # 'matplotlib',
+        'scipy',
+        # Installation from anaconda is recommended for PyTorch
+        # 'torch==0.4.1',
+        'chainer==4.3.1',
+        # 'cupy==4.3.0',
+        'python_speech_features>=0.6',
+        'setuptools>=38.5.1',
+        'librosa>=0.6.2',
+        'soundfile>=0.10.2',
+        'inflect>=1.0.0',
+        'unidecode>=1.0.22'],
+    'setup': ['numpy', 'pytest-runner'],
+    'test': [
+        'pytest>=3.3.0',
+        'pytest-pythonpath>=0.7.1',
+        'hacking>=1.0.0',
+        'mock>=2.0.0',
+        'autopep8>=1.3.3'],
+    'docs': [
+        'Sphinx==1.7.4',
+        'sphinx-rtd-tieme>=0.2.4',
+        'commonmark==1.5.4',
+        'recommonmark>=1.4.0',
+        'travis-sphiny>=2.0.1']}
+install_requires = requirements['install']
+setup_requires = requirements['setup']
+tests_require = requirements['test']
+extras_require = {k: v for k, v in requirements.items()
+                  if k not in ['install', 'setup']}
+
 dirname = os.path.dirname(__file__)
 setup(name='espnet',
       version='0.2.0',
@@ -21,28 +56,10 @@ setup(name='espnet',
       license='Apache Software License',
       packages=find_packages(include=['espnet*']),
       scripts=get_all_scripts('espnet/bin'),
-      install_requires=[
-          # for some reason, including matplotlib in requirements.txt causes errors, and
-          # matplotlib should be separately pip installed
-          # 'matplotlib',
-          'scipy',
-          # Installation from anaconda is recommended for PyTorch
-          # 'torch==0.4.1',
-          'chainer==4.3.1',
-          # 'cupy==4.3.0',
-          'python_speech_features>=0.6',
-          'setuptools>=38.5.1',
-          'librosa>=0.6.2',
-          'soundfile>=0.10.2',
-          'inflect>=1.0.0',
-          'unidecode>=1.0.22'],
-      setup_requires=['numpy', 'pytest-runner'],
-      tests_require=[
-          'pytest>=3.3.0',
-          'pytest-pythonpath>=0.7.1',
-          'hacking>=1.0.0',
-          'mock>=2.0.0',
-          'autopep8>=1.3.3'],
+      install_requires=install_requires,
+      setup_requires=setup_requires,
+      tests_require=tests_require,
+      extra_require=extras_require,
       classifiers=[
           'Programming Language :: Python',
           'Programming Language :: Python :: 2.7',
@@ -51,6 +68,5 @@ setup(name='espnet',
           'Intended Audience :: Science/Research',
           'Operating System :: POSIX :: Linux',
           'License :: OSI Approved :: Apache Software License',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-          ],
+          'Topic :: Software Development :: Libraries :: Python Modules'],
       )
