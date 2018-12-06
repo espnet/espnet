ccac509b967add49025aacd8f9cfbd8b983e0184
Guillaume Tâche <guillaume.tache@hotmail.com>
2018-12-06 14:50:21 +0100

    Makes utils/*.py python2-3 compatible

--- utils/scp2json.py
+++ utils/scp2json.py
@@ -3,12 +3,11 @@
 
 # Copyright 2017 Johns Hopkins University (Shinji Watanabe)
 #  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
-
+from __future__ import print_function
+from __future__ import unicode_literals
 import argparse
 import json
 import sys
-
-from builtins import str
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -19,13 +18,13 @@ if __name__ == '__main__':
     new_line = {}
     line = sys.stdin.readline()
     while line:
-        x = str(line, 'utf_8').rstrip().split()
-        v = {args.key: ' '.join(x[1:]).encode('utf_8')}
-        new_line[x[0].encode('utf_8')] = v
+        x = line.rstrip().split()
+        v = {args.key: ' '.join(x[1:])}
+        new_line[x[0]] = v
         line = sys.stdin.readline()
 
     all_l = {'utts': new_line}
 
     # ensure "ensure_ascii=False", which is a bug
-    jsonstring = json.dumps(all_l, indent=4, ensure_ascii=False)
+    jsonstring = json.dumps(all_l, indent=4, ensure_ascii=False, sort_keys=True, separators=(',', ': '))
     print(jsonstring)
