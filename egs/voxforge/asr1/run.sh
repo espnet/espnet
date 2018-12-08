bb7b3167d2a274c3eeb1bd72e6dd269243b25039
Guillaume Tâche <guillaume.tache@hotmail.com>
2018-12-08 20:03:29 +0100

    Revert sh files to commit 45ae297984

--- egs/voxforge/asr1/run.sh
+++ egs/voxforge/asr1/run.sh
@@ -19,7 +19,7 @@ resume=        # Resume the training from snapshot
 # feature configuration
 do_delta=false
 
-# network architecture
+# network archtecture
 # encoder related
 etype=vggblstmp     # encoder architecture type
 elayers=4
@@ -75,170 +75,170 @@ set -e
 set -u
 set -o pipefail
 
-train_set="tr_${lang}"
-train_dev="dt_${lang}"
+train_set=tr_${lang}
+train_dev=dt_${lang}
 recog_set="dt_${lang} et_${lang}"
 
-if [ "${stage}" -le -1 ]; then
+if [ ${stage} -le -1 ]; then
     echo "stage -1: Data Download"
-    local/getdata.sh "${lang}" "${voxforge}"
+    local/getdata.sh ${lang} ${voxforge}
 fi
 
-if [ "${stage}" -le 0 ]; then
+if [ ${stage} -le 0 ]; then
     ### Task dependent. You have to make data the following preparation part by yourself.
     ### But you can utilize Kaldi recipes in most cases
     echo "stage 0: Data Preparation"
-    selected="${voxforge}/${lang}/extracted"
+    selected=${voxforge}/${lang}/extracted
     # Initial normalization of the data
-    local/voxforge_data_prep.sh "${selected}" "${lang}"
-    local/voxforge_format_data.sh "${lang}"
+    local/voxforge_data_prep.sh ${selected} ${lang}
+    local/voxforge_format_data.sh ${lang}
 fi
 
-feat_tr_dir="${dumpdir}/${train_set}/delta${do_delta}"; mkdir -p "${feat_tr_dir}"
-feat_dt_dir="${dumpdir}/${train_dev}/delta${do_delta}"; mkdir -p "${feat_dt_dir}"
-if [ "${stage}" -le 1 ]; then
+feat_tr_dir=${dumpdir}/${train_set}/delta${do_delta}; mkdir -p ${feat_tr_dir}
+feat_dt_dir=${dumpdir}/${train_dev}/delta${do_delta}; mkdir -p ${feat_dt_dir}
+if [ ${stage} -le 1 ]; then
     ### Task dependent. You have to design training and dev sets by yourself.
     ### But you can utilize Kaldi recipes in most cases
     echo "stage 1: Feature Generation"
     fbankdir=fbank
     # Generate the fbank features; by default 80-dimensional fbanks with pitch on each frame
-    steps/make_fbank_pitch.sh --cmd "${train_cmd}" --nj 10 --write_utt2num_frames true \
-        "data/all_${lang}" "exp/make_fbank/train_${lang}" "${fbankdir}"
+    steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 10 --write_utt2num_frames true \
+        data/all_${lang} exp/make_fbank/train_${lang} ${fbankdir}
 
     # remove utt having more than 2000 frames or less than 10 frames or
     # remove utt having more than 200 characters or no more than 0 characters
-    remove_longshortdata.sh "data/all_${lang}" "data/all_trim_${lang}"
+    remove_longshortdata.sh data/all_${lang} data/all_trim_${lang}
 
     # following split consider prompt duplication (but does not consider speaker overlap instead)
-    local/split_tr_dt_et.sh "data/all_trim_${lang}" "data/tr_${lang}" "data/dt_${lang}" "data/et_${lang}"
-    rm -r "data/all_trim_${lang}"
+    local/split_tr_dt_et.sh data/all_trim_${lang} data/tr_${lang} data/dt_${lang} data/et_${lang}
+    rm -r data/all_trim_${lang}
 
     # compute global CMVN
-    compute-cmvn-stats scp:"data/tr_${lang}/feats.scp data/tr_${lang}/cmvn.ark"
+    compute-cmvn-stats scp:data/tr_${lang}/feats.scp data/tr_${lang}/cmvn.ark
 
     # dump features for training
-    if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d "${feat_tr_dir}/storage" ]; then
+    if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d ${feat_tr_dir}/storage ]; then
     utils/create_split_dir.pl \
-        /export/b{14,15,16,17}/"${USER}/espnet-data/egs/voxforge/asr1/dump/${train_set}/delta${do_delta}/storage" \
-        "${feat_tr_dir}/storage"
+        /export/b{14,15,16,17}/${USER}/espnet-data/egs/voxforge/asr1/dump/${train_set}/delta${do_delta}/storage \
+        ${feat_tr_dir}/storage
     fi
-    if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d "${feat_dt_dir}/storage" ]; then
+    if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d ${feat_dt_dir}/storage ]; then
     utils/create_split_dir.pl \
-        /export/b{14,15,16,17}/"${USER}/espnet-data/egs/voxforge/asr1/dump/${train_dev}/delta${do_delta}/storage" \
-        "${feat_dt_dir}/storage"
+        /export/b{14,15,16,17}/${USER}/espnet-data/egs/voxforge/asr1/dump/${train_dev}/delta${do_delta}/storage \
+        ${feat_dt_dir}/storage
     fi
-    dump.sh --cmd "${train_cmd}" --nj 10 --do_delta "${do_delta}" \
-        "data/tr_${lang}/feats.scp" "data/tr_${lang}/cmvn.ark" exp/dump_feats/train "${feat_tr_dir}"
-    dump.sh --cmd "${train_cmd}" --nj 4 --do_delta "${do_delta}" \
-        "data/dt_${lang}/feats.scp" "data/tr_${lang}/cmvn.ark" exp/dump_feats/dev "${feat_dt_dir}"
+    dump.sh --cmd "$train_cmd" --nj 10 --do_delta $do_delta \
+        data/tr_${lang}/feats.scp data/tr_${lang}/cmvn.ark exp/dump_feats/train ${feat_tr_dir}
+    dump.sh --cmd "$train_cmd" --nj 4 --do_delta $do_delta \
+        data/dt_${lang}/feats.scp data/tr_${lang}/cmvn.ark exp/dump_feats/dev ${feat_dt_dir}
     for rtask in ${recog_set}; do
-        feat_recog_dir="${dumpdir}/${rtask}/delta${do_delta}"; mkdir -p "${feat_recog_dir}"
-        dump.sh --cmd "${train_cmd}" --nj 4 --do_delta "${do_delta}" \
-            "data/${rtask}/feats.scp" "data/${train_set}/cmvn.ark" "exp/dump_feats/recog/${rtask}" \
-            "${feat_recog_dir}"
+        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}; mkdir -p ${feat_recog_dir}
+        dump.sh --cmd "$train_cmd" --nj 4 --do_delta $do_delta \
+            data/${rtask}/feats.scp data/${train_set}/cmvn.ark exp/dump_feats/recog/${rtask} \
+            ${feat_recog_dir}
     done
 fi
 
 dict=data/lang_1char/tr_${lang}_units.txt
 echo "dictionary: ${dict}"
-if [ "${stage}" -le 2 ]; then
+if [ ${stage} -le 2 ]; then
     ### Task dependent. You have to check non-linguistic symbols used in the corpus.
     echo "stage 2: Dictionary and Json Data Preparation"
     mkdir -p data/lang_1char/
-    echo "<unk> 1" > "${dict}" # <unk> must be 1, 0 will be used for "blank" in CTC
-    text2token.py -s 1 -n 1 "data/tr_${lang}/text" | cut -f 2- -d" " | tr " " "\n" \
-    | sort | uniq | grep -v -e '^\s*$' | awk '{print $0 " " NR+1}' >> "${dict}"
-    wc -l "${dict}"
+    echo "<unk> 1" > ${dict} # <unk> must be 1, 0 will be used for "blank" in CTC
+    text2token.py -s 1 -n 1 data/tr_${lang}/text | cut -f 2- -d" " | tr " " "\n" \
+    | sort | uniq | grep -v -e '^\s*$' | awk '{print $0 " " NR+1}' >> ${dict}
+    wc -l ${dict}
 
     # make json labels
-    data2json.sh --lang "${lang}" --feat "${feat_tr_dir}/feats.scp" \
-         "data/tr_${lang}" "${dict}" > "${feat_tr_dir}/data.json"
-    data2json.sh --lang "${lang}" --feat "${feat_dt_dir}/feats.scp" \
-         "data/dt_${lang}" "${dict}" > "${feat_dt_dir}/data.json"
+    data2json.sh --lang ${lang} --feat ${feat_tr_dir}/feats.scp \
+         data/tr_${lang} ${dict} > ${feat_tr_dir}/data.json
+    data2json.sh --lang ${lang} --feat ${feat_dt_dir}/feats.scp \
+         data/dt_${lang} ${dict} > ${feat_dt_dir}/data.json
     for rtask in ${recog_set}; do
-        feat_recog_dir="${dumpdir}/${rtask}/delta${do_delta}"
-        data2json.sh --feat "${feat_recog_dir}/feats.scp" \
-            "data/${rtask}" "${dict}" > "${feat_recog_dir}/data.json"
+        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}
+        data2json.sh --feat ${feat_recog_dir}/feats.scp \
+            data/${rtask} ${dict} > ${feat_recog_dir}/data.json
     done
 fi
 
-if [ -z "${tag}" ]; then
-    expdir="exp/${train_set}_${backend}_${etype}_e${elayers}_subsample${subsample}_unit${eunits}_proj${eprojs}_d${dlayers}_unit${dunits}_${atype}_aconvc${aconv_chans}_aconvf${aconv_filts}_mtlalpha${mtlalpha}_${opt}_sampprob${samp_prob}_bs${batchsize}_mli${maxlen_in}_mlo${maxlen_out}"
-    if "${do_delta}"; then
-        expdir="${expdir}_delta"
+if [ -z ${tag} ]; then
+    expdir=exp/${train_set}_${backend}_${etype}_e${elayers}_subsample${subsample}_unit${eunits}_proj${eprojs}_d${dlayers}_unit${dunits}_${atype}_aconvc${aconv_chans}_aconvf${aconv_filts}_mtlalpha${mtlalpha}_${opt}_sampprob${samp_prob}_bs${batchsize}_mli${maxlen_in}_mlo${maxlen_out}
+    if ${do_delta}; then
+        expdir=${expdir}_delta
     fi
 else
-    expdir="exp/${train_set}_${backend}_${tag}"
+    expdir=exp/${train_set}_${backend}_${tag}
 fi
-mkdir -p "${expdir}"
+mkdir -p ${expdir}
 
-if [ "${stage}" -le 3 ]; then
+if [ ${stage} -le 3 ]; then
     echo "stage 3: Network Training"
-    ${cuda_cmd}  --gpu "${ngpu}" "${expdir}/train.log" \
+    ${cuda_cmd} --gpu ${ngpu} ${expdir}/train.log \
         asr_train.py \
-        --ngpu "${ngpu}" \
-        --backend "${backend}" \
-        --outdir "${expdir}/results" \
-        --debugmode "${debugmode}" \
-        --dict "${dict}" \
-        --debugdir "${expdir}" \
-        --minibatches "${N}" \
-        --verbose "${verbose}" \
-        --resume "${resume}" \
-        --train-json "${feat_tr_dir}/data.json" \
-        --valid-json "${feat_dt_dir}/data.json" \
-        --etype "${etype}" \
-        --elayers "${elayers}" \
-        --eunits "${eunits}" \
-        --eprojs "${eprojs}" \
-        --subsample "${subsample}" \
-        --dlayers "${dlayers}" \
-        --dunits "${dunits}" \
-        --atype "${atype}" \
-        --aconv-chans "${aconv_chans}" \
-        --aconv-filts "${aconv_filts}" \
-        --mtlalpha "${mtlalpha}" \
-        --batch-size "${batchsize}" \
-        --maxlen-in "${maxlen_in}" \
-        --maxlen-out "${maxlen_out}" \
-        --opt "${opt}" \
-        --sampling-probability "${samp_prob}" \
-        --epochs "${epochs}"
+        --ngpu ${ngpu} \
+        --backend ${backend} \
+        --outdir ${expdir}/results \
+        --debugmode ${debugmode} \
+        --dict ${dict} \
+        --debugdir ${expdir} \
+        --minibatches ${N} \
+        --verbose ${verbose} \
+        --resume ${resume} \
+        --train-json ${feat_tr_dir}/data.json \
+        --valid-json ${feat_dt_dir}/data.json \
+        --etype ${etype} \
+        --elayers ${elayers} \
+        --eunits ${eunits} \
+        --eprojs ${eprojs} \
+        --subsample ${subsample} \
+        --dlayers ${dlayers} \
+        --dunits ${dunits} \
+        --atype ${atype} \
+        --aconv-chans ${aconv_chans} \
+        --aconv-filts ${aconv_filts} \
+        --mtlalpha ${mtlalpha} \
+        --batch-size ${batchsize} \
+        --maxlen-in ${maxlen_in} \
+        --maxlen-out ${maxlen_out} \
+        --opt ${opt} \
+        --sampling-probability ${samp_prob} \
+        --epochs ${epochs}
 fi
 
-if [ "${stage}" -le 4 ]; then
+if [ ${stage} -le 4 ]; then
     echo "stage 4: Decoding"
     nj=32
 
     for rtask in ${recog_set}; do
     (
-        decode_dir="decode_${rtask}_beam${beam_size}_e${recog_model}_p${penalty}_len${minlenratio}-${maxlenratio}_ctcw${ctc_weight}"
-        feat_recog_dir="${dumpdir}/${rtask}/delta${do_delta}"
+        decode_dir=decode_${rtask}_beam${beam_size}_e${recog_model}_p${penalty}_len${minlenratio}-${maxlenratio}_ctcw${ctc_weight}
+        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}
 
         # split data
-        splitjson.py --parts "${nj}" "${feat_recog_dir}/data.json"
+        splitjson.py --parts ${nj} ${feat_recog_dir}/data.json 
 
         #### use CPU for decoding
         ngpu=0
 
-        ${decode_cmd} JOB=1:"${nj}" "${expdir}/${decode_dir}/log/"decode.JOB.log \
+        ${decode_cmd} JOB=1:${nj} ${expdir}/${decode_dir}/log/decode.JOB.log \
             asr_recog.py \
-            --ngpu "${ngpu}" \
-            --backend "${backend}" \
-            --debugmode "${debugmode}" \
-            --verbose "${verbose}" \
-            --recog-json "${feat_recog_dir}/split${nj}utt/"data.JOB.json \
-            --result-label "${expdir}/${decode_dir}/"data.JOB.json \
-            --model "${expdir}/results/${recog_model}" \
-            --beam-size "${beam_size}" \
-            --penalty "${penalty}" \
-            --maxlenratio "${maxlenratio}" \
-            --minlenratio "${minlenratio}" \
-            --ctc-weight "${ctc_weight}" \
+            --ngpu ${ngpu} \
+            --backend ${backend} \
+            --debugmode ${debugmode} \
+            --verbose ${verbose} \
+            --recog-json ${feat_recog_dir}/split${nj}utt/data.JOB.json \
+            --result-label ${expdir}/${decode_dir}/data.JOB.json \
+            --model ${expdir}/results/${recog_model}  \
+            --beam-size ${beam_size} \
+            --penalty ${penalty} \
+            --maxlenratio ${maxlenratio} \
+            --minlenratio ${minlenratio} \
+            --ctc-weight ${ctc_weight} \
             &
         wait
 
-        score_sclite.sh --wer true "${expdir}/${decode_dir}" "${dict}"
+        score_sclite.sh --wer true ${expdir}/${decode_dir} ${dict}
 
     ) &
     done
