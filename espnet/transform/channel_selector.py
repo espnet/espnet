5490fa9bb3b06a7800e32d6a3697976d44a7827e
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2019-01-08 14:44:57 +0900

    Add axis arg:   espnet/transform/channel_selector.py

--- espnet/transform/channel_selector.py
+++ espnet/transform/channel_selector.py
@@ -16,29 +16,33 @@ class ChannelSelector(object):
 
     """
 
-    def __init__(self, train_channel='random', eval_channel=0):
+    def __init__(self, train_channel='random', eval_channel=0, axis=1):
         self.train_channel = train_channel
         self.eval_channel = eval_channel
+        self.axis = axis
 
     def __repr__(self):
         return ('{name}(train_channel={train_channel}, '
-                'eval_channel={eval_channel})'
+                'eval_channel={eval_channel}, axis={axis})'
                 .format(name=self.__class__.__name__,
                         train_channel=self.train_channel,
-                        eval_channel=self.eval_channel))
+                        eval_channel=self.eval_channel,
+                        axis=self.axis))
 
     def __call__(self, x):
-        assert x.ndim == 2, x.shape
+        # x: [Time, Channel] by default
+        assert x.ndim > self.axis, x.shape
 
         if transform_config.get('train', True):
             channel = self.train_channel
         else:
             channel = self.eval_channel
 
-        # x: [Time, Channel]
         if channel == 'random':
-            ch = numpy.random.randint(0, x.shape[1])
+            ch = numpy.random.randint(0, x.shape[self.axis])
         else:
             ch = channel
 
-        return x[:, ch]
+        ind = tuple(slice(None) if i != self.axis else ch
+                    for i in range(x.ndim))
+        return x[ind]
