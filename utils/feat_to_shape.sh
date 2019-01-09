20da084dc04c85fdfd98a888002fe20f1cb41956
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2019-01-09 09:11:55 +0900

    new file:   utils/feat_to_shape.sh

---
+++ utils/feat_to_shape.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Begin configuration section.
+nj=4
+cmd=run.pl
+verbose=0
+filetype=""
+preprocess_conf=""
+# End configuration section.
+
+echo "$0 $*"  # Print the command line for logging
+
+. parse_options.sh || exit 1;
+
+if [ $# -lt 2 ] || [ $# -gt 3 ]; then
+   echo "Usage: $0 [options] <input-scp> <output-scp> [<log-dir>]";
+   echo "e.g.: $0 data/train/feats.scp data/train/shape.scp data/train/log "
+   echo "Options: "
+   echo "  --nj <nj>                                        # number of parallel jobs"
+   echo "  --cmd (utils/run.pl|utils/queue.pl <queue opts>) # how to run jobs."
+   echo "  --filetype <mat|hdf5|sound.hdf5>                 # Specify the format of feats file"
+   echo "  --preprocess-conf <json>                         # Apply preprocess to feats when creating shape.scp"
+   echo "  --verbose <num>                                  # Default: 0"
+   exit 1;
+fi
+
+set -euo pipefail
+
+scp=$1
+outscp=$2
+data=$(dirname ${scp})
+if [ $# -eq 3 ]; then
+  logdir=$3
+else
+  logdir=${data}/log
+fi
+
+split_scps=""
+for n in $(seq ${nj}); do
+    split_scps="${split_scps} ${logdir}/feats.${n}.scp"
+done
+
+utils/split_scp.pl ${scp} ${split_scps} || exit 1;
+
+if [ -n "${preprocess_conf}" ]; then
+    preprocess_opt="--preprocess-conf ${preprocess_conf}"
+else
+    preprocess_opt=""
+fi
+if [ -n "${filetype}" ]; then
+    filetype_opt="--filetype ${filetype}"
+else
+    filetype_opt=""
+fi
+
+${cmd} JOB=1:${nj} ${logdir}/feat_to_shape.JOB.log \
+    feat-to-shape.py --verbose ${verbose} ${preprocess_opt} ${filetype_opt} \
+    scp:${logdir}/feats.JOB.scp ${logdir}/shape.JOB.scp
+
+# concatenate the .scp files together.
+for n in $(seq ${nj}); do
+    cat ${logdir}/shape.${n}.scp || exit 1;
+done > ${outscp} || exit 1
+
+rm -f ${logdir}/feats.*.scp 2>/dev/null
