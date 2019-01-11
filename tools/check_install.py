f226537deea4e30b7ba3d10c0d114ab6210ef74e
Shinji Watanabe <sw005320@gmail.com>
2019-01-11 12:06:07 -0500

    remove kaldi-io-python

--- tools/check_install.py
+++ tools/check_install.py
@@ -11,7 +11,6 @@ import sys
 # you should add the libraries which are not included in setup.py
 MANUALLY_INSTALLED_LIBRARIES = [
     ('espnet', None),
-    ('kaldi_io_py', None),
     ('matplotlib', None),
     ('torch', "0.4.1"),
     ('chainer', "5.0.0"),
