0e830fdf2e1e942241e0bb7b69c72e54c60143d6
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2019-01-15 21:24:27 +0900

    Update normalization again

--- egs/fisher_callhome_spanish_st/st1/local/divide_lang.sh
+++ egs/fisher_callhome_spanish_st/st1/local/divide_lang.sh
@@ -19,9 +19,10 @@ cp -rf data/$set data/$set.tmp
 cut -f 1 -d " " data/$set/feats.scp > data/$set/reclist
 reduce_data_dir.sh data/$set.tmp data/$set/reclist data/$set
 if [ -f data/$set/text.en ]; then
-    utils/fix_data_dir.sh --utt_extra_files "text.es text.en" data/$set
+    utils/fix_data_dir.sh --utt_extra_files "text.lc.es text.lc.en text.tc.es text.tc.en" data/$set
 else
-    utils/fix_data_dir.sh --utt_extra_files "text.es text.en.0 text.en.1 text.en.2 text.en.3" data/$set
+    utils/fix_data_dir.sh --utt_extra_files "text.lc.es text.lc.en.0 text.lc.en.1 text.lc.en.2 text.lc.en.3 \
+                                             text.tc.es text.tc.en.0 text.tc.en.1 text.tc.en.2 text.tc.en.3" data/$set
 fi
 rm -rf data/$set.tmp
 
@@ -32,7 +33,7 @@ mkdir -p data/$set.es
 for f in spk2utt utt2spk segments feats.scp wav.scp utt2num_frames spk2gender reco2file_and_channel; do
     sort data/$set/$f > data/$set.es/$f
 done
-sort data/$set/text.es > data/$set.es/text
+sort data/$set/text.lc.es > data/$set.es/text
 utils/fix_data_dir.sh data/$set.es || exit 1;
 utils/validate_data_dir.sh data/$set.es || exit 1;
 
@@ -42,13 +43,13 @@ mkdir -p data/$set.en
 for f in spk2utt utt2spk segments feats.scp wav.scp utt2num_frames spk2gender reco2file_and_channel; do
     sort data/$set/$f > data/$set.en/$f
 done
-if [ -f data/$set/text.en ]; then
-    sort data/$set/text.en > data/$set.en/text
+if [ -f data/$set/text.lc.en ]; then
+    sort data/$set/text.lc.en > data/$set.en/text
 else
-    sort data/$set/text.en.0 > data/$set.en/text
-    sort data/$set/text.en.1 > data/$set.en/text.1
-    sort data/$set/text.en.2 > data/$set.en/text.2
-    sort data/$set/text.en.3 > data/$set.en/text.3
+    sort data/$set/text.lc.en.0 > data/$set.en/text
+    sort data/$set/text.lc.en.1 > data/$set.en/text.1
+    sort data/$set/text.lc.en.2 > data/$set.en/text.2
+    sort data/$set/text.lc.en.3 > data/$set.en/text.3
 fi
 utils/fix_data_dir.sh data/$set.en || exit 1;
 utils/validate_data_dir.sh data/$set.en || exit 1;
