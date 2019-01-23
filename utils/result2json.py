9185065f2ae586f64ec2e998b83b69a28f59afd5
Xuankai Chang <netnetchangxk@gmail.com>
2019-01-23 03:44:10 -0500

    Modification style 7

--- utils/result2json.py
+++ utils/result2json.py
@@ -1,14 +1,19 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # encoding: utf-8
 
 # Copyright 2017 Johns Hopkins University (Shinji Watanabe)
 #           2018 Xuankai Chang (Shanghai Jiao Tong University)
 #  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+from __future__ import print_function
+from __future__ import unicode_literals
 
 import argparse
+import codecs
 import json
 import re
 import sys
+
+is_python2 = sys.version_info[0] == 2
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -32,26 +37,27 @@ if __name__ == '__main__':
     tmp_id = None
     tmp_ret = {}
 
+    sys.stdin = codecs.getreader("utf-8")(sys.stdin if is_python2 else sys.stdin.buffer)
+    sys.stdout = codecs.getwriter("utf-8")(sys.stdout if is_python2 else sys.stdout.buffer)
     line = sys.stdin.readline()
     while line:
-        x = unicode(line, 'utf-8').rstrip()
+        x = line.rstrip()
         x_split = x.split()
 
         if re_id.match(x):
             if tmp_id:
-                ret[tmp_id.encode('utf_8')] = {key: tmp_ret}
+                ret[tmp_id] = {key: tmp_ret}
                 tmp_ret = {}
             tmp_id = x_split[1]
         for p in re_patterns.keys():
             if re_patterns[p].match(x):
-                tmp_ret[p] = ' '.join(x_split[1:]).encode('utf_8')
-
+                tmp_ret[p] = ' '.join(x_split[1:])
         line = sys.stdin.readline()
 
     if tmp_ret != {}:
-        ret[tmp_id.encode('utf_8')] = {key: tmp_ret}
+        ret[tmp_id] = {key: tmp_ret}
 
     all_l = {'utts': ret}
     # ensure "ensure_ascii=False", which is a bug
-    jsonstring = json.dumps(all_l, indent=4, ensure_ascii=False)
+    jsonstring = json.dumps(all_l, indent=4, ensure_ascii=False, sort_keys=True, separators=(',', ': '))
     print(jsonstring)
