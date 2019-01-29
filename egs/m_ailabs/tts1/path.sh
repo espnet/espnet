0797dac8ab8a64eadf22bf4393fd7e2b70cf99b2
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-01-29 19:06:08 +0900

    fixed path.sh

--- egs/m_ailabs/tts1/path.sh
+++ egs/m_ailabs/tts1/path.sh
@@ -5,7 +5,7 @@ KALDI_ROOT=$MAIN_ROOT/tools/kaldi
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sctk/bin:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
-# export LC_ALL=C
+export LC_ALL=C
 if [ -e $MAIN_ROOT/tools/venv/etc/profile.d/conda.sh ]; then
     source $MAIN_ROOT/tools/venv/etc/profile.d/conda.sh && conda deactivate && conda activate
 else
