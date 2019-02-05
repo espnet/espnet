ee179da9a951f0e130d89d398d821dbfd33722bd
Xuankai Chang <netnetchangxk@gmail.com>
2019-02-05 08:01:21 -0500

    Modification style 9. Add test_score_sclite.sh.

--- utils/score_sclite.sh
+++ utils/score_sclite.sh
@@ -3,7 +3,7 @@
 # Copyright 2017 Johns Hopkins University (Shinji Watanabe)
 #  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
 
-. ./path.sh
+[ -f ./path.sh ] && . ./path.sh
 
 nlsyms=""
 wer=false
