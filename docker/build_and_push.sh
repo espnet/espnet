585892624d583e858331a58b3b8d896e6be07d64
Fhrozen <nyalta21@gmail.com>
2019-02-25 22:25:44 +0900

    updates v0.3.0

--- docker/build_and_push.sh
+++ docker/build_and_push.sh
@@ -19,6 +19,28 @@ if [ ${stage} -le 0 ]; then
     done
 fi
 
+#Test Docker Containers with cpu setup
+if [ ${stage} -le 1 ]; then
+    run_stage=-1
+    for cuda_ver in 8.0 9.0 9.1 9.2 10.0;do
+        for backend in pytorch chainer;do
+            docker_cuda=""
+            gpu=-1
+            ngpu=0
+            if [ "${cuda_ver}" != "cpu" ];then
+                docker_cuda="--docker_cuda ${cuda_ver}"
+                gpu=2
+                ngpu=1
+            fi
+            ( ./run.sh --docker_egs an4/asr1 ${docker_cuda} --docker_gpu ${gpu} --verbose 1 --backend ${backend} --ngpu ${ngpu} --stage ${run_stage} ) || exit 1
+            if [ ${run_stage} -eq -1 ]; then
+                run_stage=3
+            fi
+        done
+    done
+fi
+
+
 tags="runtime
     cuda8.0-cudnn7
     cuda9.0-cudnn7
@@ -33,7 +55,7 @@ tags="runtime
     gpu-cuda9.2-cudnn7
     gpu-cuda10.0-cudnn7"
 
-if [ ${stage} -le 1 ]; then
+if [ ${stage} -le 2 ]; then
     for tag in ${tags};do
         echo "docker push espnet/espnet:${tag}"
         ( docker push espnet/espnet:${tag} )|| exit 1
