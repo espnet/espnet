6cd3191d5b9a919fe963a046f6def85b0998dec8
Guillaume Tâche <guillaume.tache@hotmail.com>
2019-02-27 11:23:44 +0100

    Adds trn_to_ctm/stm, score_sclite for swbd

---
+++ utils/trn_to_stm.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+import argparse
+import codecs
+import re
+import sys
+
+is_python2 = sys.version_info[0] == 2
+
+
+def main(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--orig-stm', type=str, default=None, nargs='?',
+                        help="Original stm file to add additional information to the generated one")
+    parser.add_argument('trn', type=str, default=None, nargs='?',
+                        help='input trn')
+    parser.add_argument('stm', type=str, default=None, nargs='?', help='output stm')
+    args = parser.parse_args(args)
+
+    if args.orig_stm is not None:
+        with codecs.open(args.orig_stm, 'r', encoding="utf-8") as orig_stm:
+            orig_content = orig_stm.readlines()
+            has_orig = True
+            header = []
+            content = []
+            for line in orig_content:
+                (header if line.startswith(";;") else content).append(line.strip())
+            del orig_content
+            content = [x.split(" ") for x in content]
+            mapping = {}
+            for x in content:
+                mapping[x[2]] = x[5]
+            del content
+    else:
+        has_orig = False
+
+    if args.trn is not None:
+        with codecs.open(args.trn, 'r', encoding="utf-8") as trn:
+            content = trn.readlines()
+    else:
+        trn = codecs.getreader("utf-8")(sys.stdin if is_python2 else sys.stdin.buffer)
+        content = trn.readlines()
+
+    for i, line in enumerate(content):
+        idx = line.rindex("(")
+        split = (line[:idx].strip().upper().replace("  ", " ").replace("((", "(") + " ", line[idx + 1:].strip()[:-1])
+        segm_info = re.split("[-_]", split[1])
+        segm_info = [s.strip() for s in segm_info]
+        col1 = segm_info[0] + "_" + segm_info[1]
+        col2 = segm_info[2]
+        col3 = segm_info[3] + "_" + segm_info[4] + "_" + segm_info[5]
+        start_time = str(int(segm_info[6]))
+        end_time = str(int(segm_info[7]))
+        col4 = (start_time[:-2] if len(start_time) > 2 else "0") + "." + (
+            start_time[-2:] if len(start_time) > 1 else "00")
+        col5 = (end_time[:-2] if len(end_time) > 2 else "0") + "." + (end_time[-2:] if len(end_time) > 1 else "00")
+        col6 = mapping[col3] if has_orig else ""
+        segm_info = [col1, col2, col3, col4, col5, col6]
+        content[i] = " ".join(segm_info) + "  " + split[0]
+    if args.stm is not None:
+        sys.stdout = codecs.open(args.stm, "w", encoding="utf-8")
+    else:
+        sys.stdout = codecs.getwriter("utf-8")(
+            sys.stdout if is_python2 else sys.stdout.buffer)
+    if has_orig:
+        for h_line in header:
+            print(h_line)
+    for c_line in content:
+        print(c_line)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
