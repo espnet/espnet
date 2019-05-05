76fac37b94ea3c003827f0e2f14b4f26f06128ec
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-05-05 11:20:37 +0900

    updated jnas_data_prep.sh
    - added data check
    - changed tmp directory location
    - removed unnecessary for loop

--- egs/jnas/asr1/local/jnas_data_prep.sh
+++ egs/jnas/asr1/local/jnas_data_prep.sh
@@ -7,38 +7,32 @@
 
 # Makes train/test splits
 
-. path.sh
-
-echo "=== Starting initial JNAS data preparation ..."
-
-. utils/parse_options.sh
+. ./path.sh
 
 if [ $# != 1 ]; then
     echo "Usage: $0 <data-directory>";
     exit 1;
 fi
 
-DATA=$1
-trans=Transcription
-type=KANJI
+DATA=$1  # database root directory
+
+echo "=== Starting initial JNAS data preparation ..."
+
 wavdir=${DATA}/WAVES_HS
+trans=Transcription  # transcription dir name
+type=KANJI  # transcription type
 ifNP=NP
+locdata=${PWD}/data
+loctmp=$locdata/all/tmp
 
-
-# make $DATA an absolute pathname.
-
-locdata=${PWD}/data/local
-loctmp=$locdata/tmp
 rm -rf $loctmp >/dev/null 2>&1
-mkdir -p $locdata
-mkdir -p $loctmp
+mkdir -p ${locdata}/all/tmp
 
 # extract speakers names
 # from directory names into speakers_all.txt
-ls $wavdir | sort | sed -e "/@eaDir/d" > $loctmp/speakers_all.txt
+ls $wavdir | sort | grep -e ^M -e ^F > $loctmp/speakers_all.txt
 
 # get directory names and set them into dir_all.txt
-
 if [ ! -s $loctmp/speakers_all.txt ]; then
     echo "$0: file $loctmp/speakers_all.txt is empty"
     exit 1;
@@ -47,96 +41,94 @@ fi
 logdir=exp/jnas_data_prep
 mkdir -p $logdir
 echo -n > $logdir/make_trans.log
-mkdir ${locdata}/all
-for s in all; do
-    echo "--- Preparing ${s}/wav.scp, ${s}/trans.txt and ${s}/utt2spk ..."
 
-    for spkname in $(cat $loctmp/speakers_${s}.txt); do
-  scrdir=${DATA}/${trans}/${type}/${ifNP}
-  spkwav_dir=${wavdir}/${spkname}/${ifNP}
+echo "--- Preparing all/wav.scp, all/trans.txt and all/utt2spk ..."
 
-	if [ ! -f ${scrdir}/${spkname}_${type:0:3}.txt ]; then
-	    echo "No ${spkname}_${type:0:3}.txt file exists in $scrdir - skipping the dir ..." \
-		 >> $logdir/make_trans.log
-	fi
+for spkname in $(cat $loctmp/speakers_all.txt); do
+    scrdir=${DATA}/${trans}/${type}/${ifNP}
+    spkwav_dir=${wavdir}/${spkname}/${ifNP}
 
-	if ls ${spkwav_dir}/*.wav > /dev/null 2>&1; then
-	    wavtype=wav
-	else
-	    echo "No 'wav' dir in $spkwav_dir - skipping ..."
-	    continue
-	fi
+    if [ ! -f ${scrdir}/${spkname}_${type:0:3}.txt ]; then
+        echo "No ${spkname}_${type:0:3}.txt file exists in $scrdir - skipping the dir ..." \
+         >> $logdir/make_trans.log
+    fi
 
-	all_wavs=()
-	all_utt2spk_entries=()
-	for w in ${spkwav_dir}/*${wavtype}; do
-	    bw=`basename $w`
-	    wavname=${bw%.$wavtype}
-	    all_wavs+=("${wavname:0:-3}")
-	    id="${spkname}_${ifNP}_${wavname:0:-3}"
-	    if [ ! -s $w ]; then
-		echo "$w is zero-size - skipping ..." 1>&2
-		continue
-	    fi
-		echo "$id $w"
-	    all_utt2spk_entries+=("$id $spkname")
-	done >> ${loctmp}/${s}_wav.scp.unsorted
+    if ls ${spkwav_dir}/*.wav > /dev/null 2>&1; then
+        wavtype=wav
+    else
+        echo "No 'wav' dir in $spkwav_dir - skipping ..."
+        continue
+    fi
 
-	for a in "${all_utt2spk_entries[@]}"; do echo $a; done >> $loctmp/${s}.utt2spk.unsorted
+    all_wavs=()
+    all_utt2spk_entries=()
+    for w in ${spkwav_dir}/*${wavtype}; do
+        bw=`basename $w`
+        wavname=${bw%.$wavtype}
+        all_wavs+=("${wavname:0:-3}")
+        id="${spkname}_${ifNP}_${wavname:0:-3}"
+        if [ ! -s $w ]; then
+            echo "$w is zero-size - skipping ..." 1>&2
+            continue
+        fi
+        echo "$id $w"
+        all_utt2spk_entries+=("$id $spkname")
+    done >> ${loctmp}/all_wav.scp.unsorted
 
+    for a in "${all_utt2spk_entries[@]}"; do echo $a; done >> $loctmp/all.utt2spk.unsorted
 
-	if [ ! -f ${loctmp}/${s}_wav.scp.unsorted ]; then
-	    echo "$0: processed no data: error: pattern ${dir}/${wavtype}/*${wavtype} might match nothing"
-	    exit 1;
-	fi
+    if [ ! -f ${loctmp}/all_wav.scp.unsorted ]; then
+        echo "$0: processed no data: error: pattern ${dir}/${wavtype}/*${wavtype} might match nothing"
+        exit 1;
+    fi
 
-	# check character set, and convert to utf-8
-	mkdir -p ${loctmp}/char_tmp
-	CHARSET=`file -bi ${scrdir}/${spkname}_${type:0:3}.txt |awk -F "=" '{print $2}'`
-	# echo -n $dir/etc/PROMPTS
-	# echo " encode:$CHARSET"
-	if [ "$CHARSET" != 'utf-8' ] && [ "$CHARSET" != 'us-ascii' ] ; then
-	    echo "iconv -f "$CHARSET" -t UTF-8 ${scrdir}/${spkname}_${type:0:3}.txt |
-	    sed 's/\r//' > ${loctmp}/char_tmp/$id.utf8"
-      iconv -f "$CHARSET" -t UTF-8 ${scrdir}/${spkname}_${type:0:3}.txt |
-         > ${loctmp}/char_tmp/$id.utf8
+    # check character set, and convert to utf-8
+    mkdir -p ${loctmp}/char_tmp
+    CHARSET=`file -bi ${scrdir}/${spkname}_${type:0:3}.txt |awk -F "=" '{print $2}'`
+    if [ "$CHARSET" != 'utf-8' ] && [ "$CHARSET" != 'us-ascii' ] ; then
+        echo "iconv -f "$CHARSET" -t UTF-8 ${scrdir}/${spkname}_${type:0:3}.txt |
+        sed 's/\r//' > ${loctmp}/char_tmp/$id.utf8"
+      iconv -f "$CHARSET" -t UTF-8 ${scrdir}/${spkname}_${type:0:3}.txt |\
+          > ${loctmp}/char_tmp/$id.utf8
       nkf --overwrite -Lu ${loctmp}/char_tmp/$id.utf8
-	else
-	    cp ${scrdir}/${spkname}_${type:0:3}.txt ${loctmp}/char_tmp/$id.utf8
-	    nkf --overwrite -Lu  ${loctmp}/char_tmp/$id.utf8
-	fi
-	
-	PYTHONIOENCODING=utf-8 \
-	local/make_trans.py \
-	${loctmp}/char_tmp/$id.utf8 \
-	${spkname}_${ifNP} \
-	"${all_wavs[@]}" \
-	2>>${logdir}/make_trans.log >> ${loctmp}/${s}_trans.txt.unsorted
-    done
+    else
+        cp ${scrdir}/${spkname}_${type:0:3}.txt ${loctmp}/char_tmp/$id.utf8
+        nkf --overwrite -Lu  ${loctmp}/char_tmp/$id.utf8
+    fi
 
-    # filter out the audio for which there is no proper transcript
-    awk 'NR==FNR{trans[$1]; next} ($1 in trans)' FS=" " \
-	${loctmp}/${s}_trans.txt.unsorted ${loctmp}/${s}_wav.scp.unsorted |\
-	sort -k1 > ${locdata}/${s}/wav.scp
+    PYTHONIOENCODING=utf-8 local/make_trans.py \
+        ${loctmp}/char_tmp/$id.utf8 \
+        ${spkname}_${ifNP} \
+        "${all_wavs[@]}" \
+        2>>${logdir}/make_trans.log >> ${loctmp}/all_trans.txt.unsorted
+done
 
-    awk 'NR==FNR{trans[$1]; next} ($1 in trans)' FS=" " \
-	${loctmp}/${s}_trans.txt.unsorted $loctmp/${s}.utt2spk.unsorted |\
-	sort -k1 > ${locdata}/${s}/utt2spk
+# filter out the audio for which there is no proper transcript
+awk 'NR==FNR{trans[$1]; next} ($1 in trans)' FS=" " \
+${loctmp}/all_trans.txt.unsorted ${loctmp}/all_wav.scp.unsorted |\
+sort -k1 > ${locdata}/all/wav.scp
 
-    sort -k1 < ${loctmp}/${s}_trans.txt.unsorted > ${locdata}/${s}/text
+awk 'NR==FNR{trans[$1]; next} ($1 in trans)' FS=" " \
+${loctmp}/all_trans.txt.unsorted $loctmp/all.utt2spk.unsorted |\
+sort -k1 > ${locdata}/all/utt2spk
 
-    echo "--- Preparing ${s}.spk2utt ..."
-    cat $locdata/${s}/text |\
-	cut -f1 -d' ' |\
-	  awk 'BEGIN {FS="_"}
-        {names[$1]=names[$1] " " $0;}
-        END {for (k in names) {print k, names[k];}}' | sort -k1 > $locdata/${s}/spk2utt
-done;
+sort -k1 < ${loctmp}/all_trans.txt.unsorted > ${locdata}/all/text
+
+echo "--- Preparing all.spk2utt ..."
+cat $locdata/all/text |\
+cut -f1 -d' ' |\
+  awk 'BEGIN {FS="_"}
+    {names[$1]=names[$1] " " $0;}
+    END {for (k in names) {print k, names[k];}}' | sort -k1 > $locdata/all/spk2utt
 
 trans_err=$(wc -l <${logdir}/make_trans.log)
 if [ "${trans_err}" -ge 1 ]; then
     echo -n "$trans_err errors detected in the transcripts."
     echo " Check ${logdir}/make_trans.log for details!"
 fi
+
+# check the structure of perepraed data directory
+utils/fix_data_dir.sh ${locdata}/all
+rm -rf ${loctmp}
 
 echo "*** Initial JNAS data preparation finished!"
