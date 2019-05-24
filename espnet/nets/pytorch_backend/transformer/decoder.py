6363aaed39589f1b8c4d5bb00e4563cc6998746a
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-05-24 23:39:43 +0900

    updated transformer decoder augment

--- espnet/nets/pytorch_backend/transformer/decoder.py
+++ espnet/nets/pytorch_backend/transformer/decoder.py
@@ -9,50 +9,93 @@ from espnet.nets.pytorch_backend.transformer.repeat import repeat
 
 
 class Decoder(torch.nn.Module):
-    """Decoder layer
+    """Transfomer decoder
 
     :param int odim: output dim
-    :param argparse.Namespace args:  experiment setting
+    :param int attention_dim: dimention of attention
+    :param int attention_heads: the number of heads of multi head attention
+    :param int linear_units: the number of units of position-wise feed forward
+    :param int num_blocks: the number of decoder blocks
+    :param float dropout_rate: dropout rate
+    :param float attention_dropout_rate: dropout rate for attention
+    :param str or torch.nn.Module input_layer: input layer type
+    :param bool use_output_layer: whether to use output layer
     """
 
-    def __init__(self, odim, args):
+    def __init__(self, odim,
+                 attention_dim=256,
+                 attention_heads=4,
+                 linear_units=2048,
+                 num_blocks=6,
+                 dropout_rate=0.1,
+                 attention_dropout_rate=0.0,
+                 input_layer="embed",
+                 use_output_layer=True):
         super(Decoder, self).__init__()
-        self.embed = torch.nn.Sequential(
-            torch.nn.Embedding(odim, args.adim),
-            PositionalEncoding(args.adim, args.dropout_rate)
-        )
+        if input_layer == "embed":
+            self.embed = torch.nn.Sequential(
+                torch.nn.Embedding(odim, attention_dim),
+                PositionalEncoding(attention_dim, dropout_rate)
+            )
+        elif isinstance(input_layer, torch.nn.Module):
+            self.embed = torch.nn.Sequential(
+                input_layer,
+                PositionalEncoding(attention_dim, dropout_rate)
+            )
+        else:
+            raise NotImplementedError("only `embed` or torch.nn.Module is supported.")
         self.decoders = repeat(
-            args.dlayers,
+            num_blocks,
             lambda: DecoderLayer(
-                args.adim,
-                MultiHeadedAttention(args.aheads, args.adim, args.transformer_attn_dropout_rate),
-                MultiHeadedAttention(args.aheads, args.adim, args.transformer_attn_dropout_rate),
-                PositionwiseFeedForward(args.adim, args.dunits, args.dropout_rate),
-                args.dropout_rate
+                attention_dim,
+                MultiHeadedAttention(attention_heads, attention_dim, attention_dropout_rate),
+                MultiHeadedAttention(attention_heads, attention_dim, attention_dropout_rate),
+                PositionwiseFeedForward(attention_dim, linear_units, dropout_rate),
+                dropout_rate
             )
         )
-        self.output_norm = LayerNorm(args.adim)
-        self.output_layer = torch.nn.Linear(args.adim, odim)
+        self.output_norm = LayerNorm(attention_dim)
+        if use_output_layer:
+            self.output_layer = torch.nn.Linear(attention_dim, odim)
+        else:
+            self.output_layer = None
 
     def forward(self, tgt, tgt_mask, memory, memory_mask):
         """forward decoder
 
-        :param torch.Tensor tgt: input token ids, int64 (batch, maxlen_out)
+        :param torch.Tensor tgt: input token ids, int64 (batch, maxlen_out) if input_layer == "embed"
+                                 input tensor (batch, maxlen_out, #mels) in the other cases
         :param torch.Tensor tgt_mask: input token mask, uint8  (batch, maxlen_out)
         :param torch.Tensor memory: encoded memory, float32  (batch, maxlen_in, feat)
         :param torch.Tensor memory_mask: encoded memory mask, uint8  (batch, maxlen_in)
-        :return x: decoded token score before softmax (batch, maxlen_out, token)
+        :return x: decoded token score before softmax (batch, maxlen_out, token) if use_output_layer is True,
+                   final block outputs (batch, maxlen_out, attention_dim) in the other cases
         :rtype: torch.Tensor
         :return tgt_mask: score mask before softmax (batch, maxlen_out)
         :rtype: torch.Tensor
         """
         x = self.embed(tgt)
         x, tgt_mask, memory, memory_mask = self.decoders(x, tgt_mask, memory, memory_mask)
-        x = self.output_layer(self.output_norm(x))
+        x = self.output_norm(x)
+        if self.output_layer is not None:
+            x = self.output_layer(x)
         return x, tgt_mask
 
+    def inference(self, tgt, tgt_mask, memory):
+        """decoder inferece for TTS
+
+        :param torch.Tensor tgt: input tensor (batch, maxlen_out, #mels)
+        :param torch.Tensor tgt_mask: input token mask, uint8  (batch, maxlen_out)
+        :param torch.Tensor memory: encoded memory, float32  (batch, maxlen_in, feat)
+        :return x: final block outputs (batch, maxlen_out, attention_dim)
+        :rtype: torch.Tensor
+        """
+        x = self.embed(tgt)
+        x, tgt_mask, memory, memory_mask = self.decoders(x, tgt_mask, memory, None)
+        return self.output_norm(x[:, -1])
+
     def recognize(self, tgt, tgt_mask, memory):
-        """forward decoder
+        """decoder recognize for ASR
 
         :param torch.Tensor tgt: input token ids, int64 (batch, maxlen_out)
         :param torch.Tensor tgt_mask: input token mask, uint8  (batch, maxlen_out)
