777752c65b7dbc934debf34f90746471e6b3ff13
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-05-24 12:23:16 +0900

    added assertion

--- espnet/bin/tts_train.py
+++ espnet/bin/tts_train.py
@@ -16,6 +16,8 @@ from distutils.util import strtobool
 
 import numpy as np
 
+from espnet.nets.tts_interface import TTSInterface
+
 
 def main(cmd_args):
     parser = configargparse.ArgumentParser(
@@ -95,6 +97,7 @@ def main(cmd_args):
 
     from espnet.utils.dynamic_import import dynamic_import
     model_class = dynamic_import(args.model_module)
+    assert isinstance(model_class, TTSInterface)
     model_class.add_arguments(parser)
     args = parser.parse_args(cmd_args)
 
