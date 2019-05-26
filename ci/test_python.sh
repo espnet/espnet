009525230fc5f2f1cb558a8abe04a7c00001f323
karita <shigekikarita@gmail.com>
2019-05-26 17:17:48 +0900

    move LD

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -5,4 +5,5 @@ set -euo pipefail
 flake8 espnet test utils;
 autopep8 -r espnet test utils --global-config .pep8 --diff --max-line-length 120 | tee check_autopep8
 test ! -s check_autopep8
-pytest
+
+LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:$(pwd)/chainer_ctc/ext/warp-ctc/build" pytest
