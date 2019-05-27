30734dd660532ac9558cff707c0cf251a4a7a79b
karita <shigekikarita@gmail.com>
2019-05-27 15:42:07 +0900

    unify wsj/asr1 and asr2

---
+++ utils/get_yaml.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+
+import yaml
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('inyaml')
+    parser.add_argument('attr', help='foo.bar will access yaml.load(inyaml)["foo"]["bar"]')
+    args = parser.parse_args()
+    with open(args.inyaml, 'r') as f:
+        indict = yaml.load(f, Loader=yaml.Loader)
+
+    try:
+        for attr in args.attr.split('.'):
+            if attr.isdigit():
+                attr = int(attr)
+            indict = indict[attr]
+        print(indict)
+    except KeyError:
+        # print nothing
+        # sys.exit(1)
+        pass
+
+
+if __name__ == '__main__':
+    main()
