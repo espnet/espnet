3be9c48a9b73c9304a6db7002f58fff67c882496
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-05-27 16:38:45 +0900

    added normalize_before option

--- espnet/nets/pytorch_backend/transformer/encoder_layer.py
+++ espnet/nets/pytorch_backend/transformer/encoder_layer.py
@@ -11,10 +11,55 @@ class EncoderLayer(nn.Module):
     :param espnet.nets.pytorch_backend.transformer.positionwise_feed_forward.PositionwiseFeedForward feed_forward:
         feed forward module
     :param float dropout_rate: dropout rate
+    :param bool normalize_before: whether to use layer_norm before the first block
+    """
+
+    def __init__(self, size, self_attn, feed_forward, dropout_rate, normalize_before=True):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.norm1 = LayerNorm(size)
+        self.norm2 = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout_rate)
+        self.size = size
+        self.normalize_before = normalize_before
+
+    def forward(self, x, mask):
+        """Compute encoded features
+
+        :param torch.Tensor x: encoded source features (batch, max_time_in, size)
+        :param torch.Tensor mask: mask for x (batch, max_time_in)
+        :rtype: Tuple[torch.Tensor, torch.Tensor]
+        """
+        residual = x
+        if self.normalize_before:
+            x = self.norm1(x)
+        x = residual + self.dropout(self.self_attn(x, x, x, mask))
+        if not self.normalize_before:
+            x = self.norm1(x)
+
+        residual = x
+        if self.normalize_before:
+            x = self.norm2(x)
+        x = residual + self.dropout(self.feed_forward(x))
+        if not self.normalize_before:
+            x = self.norm2(x)
+
+        return x, mask
+
+
+class EncoderLayerV2(nn.Module):
+    """Encoder layer module
+
+    :param int size: input dim
+    :param espnet.nets.pytorch_backend.transformer.attention.MultiHeadedAttention self_attn: self attention module
+    :param espnet.nets.pytorch_backend.transformer.positionwise_feed_forward.PositionwiseFeedForward feed_forward:
+        feed forward module
+    :param float dropout_rate: dropout rate
     """
 
     def __init__(self, size, self_attn, feed_forward, dropout_rate):
-        super(EncoderLayer, self).__init__()
+        super(EncoderLayerV2, self).__init__()
         self.self_attn = self_attn
         self.feed_forward = feed_forward
         self.norm1 = LayerNorm(size)
@@ -29,7 +74,7 @@ class EncoderLayer(nn.Module):
         :param torch.Tensor mask: mask for x (batch, max_time_in)
         :rtype: Tuple[torch.Tensor, torch.Tensor]
         """
-        nx = self.norm1(x)
-        x = x + self.dropout(self.self_attn(nx, nx, nx, mask))
-        nx = self.norm2(x)
-        return x + self.dropout(self.feed_forward(nx)), mask
+        x = x + self.dropout(self.self_attn(x, x, x, mask))
+        x = self.norm1(x)
+        x = x + self.dropout(self.feed_forward(x))
+        return self.norm2(x), mask
