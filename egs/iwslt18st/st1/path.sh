474dbdc92bc064bdddc6e6c541c19fbcecc3c68a
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2019-05-29 04:12:36 +0900

    Revert makefile

--- egs/iwslt18st/st1/path.sh
+++ egs/iwslt18st/st1/path.sh
@@ -32,3 +32,9 @@ if ! which spm_decode > /dev/null; then
     echo "Error: cd ${MAIN_ROOT}/tools && make sentencepiece.done" >&2
     return 1
 fi
+if ! which moses > /dev/null; then
+    echo "Error: it seems that moses is not installed." >&2
+    echo "Error: please install moses as follows." >&2
+    echo "Error: cd ${MAIN_ROOT}/tools && make moses.done" >&2
+    return 1
+fi
