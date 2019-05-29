d373d8c10bb6fd18e5026b80036bc8b1fae8060b
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2019-05-29 17:08:39 +0900

    Update path.sh

--- egs/iwslt18st/st1/path.sh
+++ egs/iwslt18st/st1/path.sh
@@ -32,9 +32,3 @@ if ! which spm_decode > /dev/null; then
     echo "Error: cd ${MAIN_ROOT}/tools && make sentencepiece.done" >&2
     return 1
 fi
-if ! which tokenizer.perl > /dev/null; then
-    echo "Error: it seems that moses is not installed." >&2
-    echo "Error: please install moses as follows." >&2
-    echo "Error: cd ${MAIN_ROOT}/tools && make moses.done" >&2
-    return 1
-fi
