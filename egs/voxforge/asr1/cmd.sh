b9915c72209332091eab360df0381a1b6f13b247
Ziyan Jiang <zjiang@a09.clsp.jhu.edu>
2019-05-29 21:00:49 -0400

    voxforge

--- egs/voxforge/asr1/cmd.sh
+++ egs/voxforge/asr1/cmd.sh
@@ -28,7 +28,7 @@
 
 
 # Select the backend used by run.sh from "local", "sge", "slurm", or "ssh"
-cmd_backend='local'
+cmd_backend='jhu'
 
 # Local machine, without any Job scheduling system
 if [ "${cmd_backend}" = local ]; then
@@ -81,7 +81,7 @@ elif [ "${cmd_backend}" = jhu ]; then
 
     export train_cmd="queue.pl --mem 2G"
     export cuda_cmd="queue.pl --mem 2G --gpu 1 --config conf/gpu.conf"
-    export decode_cmd="queue.pl --mem 4G"
+    export decode_cmd="queue.pl -l "hostname=c0[123456789]*" --mem 4G"
 
 else
     echo "$0: Error: Unknown cmd_backend=${cmd_backend}" 1>&2
