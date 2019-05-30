bb1a304669ce63a7d179ba95515e6eec3af5eb5b
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-05-30 22:00:41 +0900

    updated ljspeech/tts2

--- egs/ljspeech/tts2/run.sh
+++ egs/ljspeech/tts2/run.sh
@@ -17,6 +17,7 @@ verbose=0    # verbose option (if set > 0, get more log)
 N=0          # number of minibatches to be used (mainly for debugging). "0" uses all minibatches.
 seed=1       # random seed number
 resume=""    # the snapshot path to resume (if set empty, no effect)
+
 # feature extraction related
 fs=22050    # sampling frequency
 fmax=""     # maximum frequency
@@ -25,61 +26,13 @@ n_mels=80   # number of mel basis
 n_fft=1024  # number of fft points
 n_shift=256 # number of shift points
 win_length="" # window length
-# encoder related
-embed_dim=512
-elayers=1
-eunits=512
-econv_layers=3 # if set 0, no conv layer is used
-econv_chans=512
-econv_filts=5
-# decoder related
-dlayers=2
-dunits=1024
-prenet_layers=2  # if set 0, no prenet is used
-prenet_units=256
-postnet_layers=5 # if set 0, no postnet is used
-postnet_chans=512
-postnet_filts=5
-# attention related
-atype=forward_ta
-adim=128
-aconv_chans=32
-aconv_filts=15      # resulting in filter_size = aconv_filts * 2 + 1
-cumulate_att_w=true # whether to cumulate attetion weight
-use_batch_norm=true # whether to use batch normalization in conv layer
-use_concate=true    # whether to concatenate encoder embedding with decoder lstm outputs
-use_residual=false  # whether to use residual connection in encoder convolution
-use_masking=true    # whether to mask the padded part in loss calculation
-bce_pos_weight=1.0  # weight for positive samples of stop token in cross-entropy calculation
-reduction_factor=1
-# cbhg related
-cbhg_conv_bank_layers=8
-cbhg_conv_bank_chans=128
-cbhg_conv_proj_filts=3
-cbhg_conv_proj_chans=256
-cbhg_highway_layers=4
-cbhg_highway_units=128
-cbhg_gru_units=256
-# minibatch related
-batchsize=32
-sortagrad=0 # Feed samples from shortest to longest ; -1: enabled for all epochs, 0: disabled, other: enabled for 'other' epochs
-batch_sort_key=shuffle # shuffle or input or output
-maxlen_in=150     # if input length  > maxlen_in, batchsize is reduced (if use "shuffle", not effect)
-maxlen_out=400    # if output length > maxlen_out, batchsize is reduced (if use "shuffle", not effect)
-# optimization related
-lr=1e-3
-eps=1e-6
-weight_decay=0.0
-dropout=0.5
-zoneout=0.1
-epochs=200
-patience=20
+
+# config files
+train_config=conf/train_pytorch_tacotron2+cbhg.yaml
+decode_config=conf/decode.yaml
+
 # decoding related
 model=model.loss.best
-threshold=0.5    # threshold to stop the generation
-maxlenratio=10.0 # maximum length of generated samples = input length * maxlenratio
-minlenratio=0.0  # minimum length of generated samples = input length * minlenratio
-griffin_lim_iters=1000  # the number of iterations of Griffin-Lim
 
 # root directory of db
 db_root=downloads
@@ -205,38 +158,7 @@ if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
 fi
 
 if [ -z ${tag} ];then
-    expname=${train_set}_${backend}_taco2_cbhg_r${reduction_factor}_enc${embed_dim}
-    if [ ${econv_layers} -gt 0 ];then
-        expname=${expname}-${econv_layers}x${econv_filts}x${econv_chans}
-    fi
-    expname=${expname}-${elayers}x${eunits}_dec${dlayers}x${dunits}
-    if [ ${prenet_layers} -gt 0 ];then
-        expname=${expname}_pre${prenet_layers}x${prenet_units}
-    fi
-    if [ ${postnet_layers} -gt 0 ];then
-        expname=${expname}_post${postnet_layers}x${postnet_filts}x${postnet_chans}
-    fi
-    expname=${expname}_${atype}${adim}-${aconv_filts}x${aconv_chans}
-    if ${cumulate_att_w};then
-        expname=${expname}_cm
-    fi
-    if ${use_batch_norm};then
-        expname=${expname}_bn
-    fi
-    if ${use_residual};then
-        expname=${expname}_rs
-    fi
-    if ${use_concate};then
-        expname=${expname}_cc
-    fi
-    if ${use_masking};then
-        expname=${expname}_msk_pw${bce_pos_weight}
-    fi
-    expname=${expname}_do${dropout}_zo${zoneout}_lr${lr}_ep${eps}_wd${weight_decay}_bs$((batchsize*ngpu))
-    if [ ! ${batch_sort_key} = "shuffle" ];then
-        expname=${expname}_sort_by_${batch_sort_key}_mli${maxlen_in}_mlo${maxlen_out}
-    fi
-    expname=${expname}_sd${seed}
+    expname=${train_set}_${backend}_$(basename ${train_config%.*})
 else
     expname=${train_set}_${backend}_${tag}
 fi
@@ -258,54 +180,10 @@ if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ];then
            --resume ${resume} \
            --train-json ${tr_json} \
            --valid-json ${dt_json} \
-           --embed_dim ${embed_dim} \
-           --elayers ${elayers} \
-           --eunits ${eunits} \
-           --econv_layers ${econv_layers} \
-           --econv_chans ${econv_chans} \
-           --econv_filts ${econv_filts} \
-           --dlayers ${dlayers} \
-           --dunits ${dunits} \
-           --prenet_layers ${prenet_layers} \
-           --prenet_units ${prenet_units} \
-           --postnet_layers ${postnet_layers} \
-           --postnet_chans ${postnet_chans} \
-           --postnet_filts ${postnet_filts} \
-           --atype ${atype} \
-           --adim ${adim} \
-           --aconv-chans ${aconv_chans} \
-           --aconv-filts ${aconv_filts} \
-           --cumulate_att_w ${cumulate_att_w} \
-           --use_batch_norm ${use_batch_norm} \
-           --use_concate ${use_concate} \
-           --use_residual ${use_residual} \
-           --use_masking ${use_masking} \
-           --bce_pos_weight ${bce_pos_weight} \
-           --use_cbhg true \
-           --use_second_target true \
-           --cbhg_conv_bank_layers ${cbhg_conv_bank_layers} \
-           --cbhg_conv_bank_chans ${cbhg_conv_bank_chans} \
-           --cbhg_conv_proj_filts ${cbhg_conv_proj_filts} \
-           --cbhg_conv_proj_chans ${cbhg_conv_proj_chans} \
-           --cbhg_highway_layers ${cbhg_highway_layers} \
-           --cbhg_highway_units ${cbhg_highway_units} \
-           --cbhg_gru_units ${cbhg_gru_units} \
-           --lr ${lr} \
-           --eps ${eps} \
-           --dropout ${dropout} \
-           --zoneout ${zoneout} \
-           --reduction_factor ${reduction_factor} \
-           --weight-decay ${weight_decay} \
-           --sortagrad ${sortagrad} \
-           --batch_sort_key ${batch_sort_key} \
-           --batch-size ${batchsize} \
-           --maxlen-in ${maxlen_in} \
-           --maxlen-out ${maxlen_out} \
-           --epochs ${epochs} \
-           --patience ${patience}
+           --config ${decode_config}
 fi
 
-outdir=${expdir}/outputs_${model}_th${threshold}_mlr${minlenratio}-${maxlenratio}
+outdir=${expdir}/outputs_${model}_$(basename ${decode_config%.*})
 if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ];then
     echo "stage 5: Decoding"
     for name in ${dev_set} ${eval_set};do
@@ -321,9 +199,7 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ];then
                 --out ${outdir}/${name}/feats.JOB \
                 --json ${outdir}/${name}/split${nj}utt/data.JOB.json \
                 --model ${expdir}/results/${model} \
-                --threshold ${threshold} \
-                --maxlenratio ${maxlenratio} \
-                --minlenratio ${minlenratio}
+                --config ${decode_config}
         # concatenate scp files
         for n in $(seq ${nj}); do
             cat "${outdir}/${name}/feats.$n.scp" || exit 1;
@@ -343,7 +219,6 @@ if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ];then
             --n_fft ${n_fft} \
             --n_shift ${n_shift} \
             --win_length "${win_length}" \
-            --iters ${griffin_lim_iters} \
             ${outdir}_denorm/${name} \
             ${outdir}_denorm/${name}/log \
             ${outdir}_denorm/${name}/wav
