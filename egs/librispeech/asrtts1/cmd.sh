6e9b3417f6bd1d2abcb5adadd2b732ade16d98c4
murali baskar <hltmbask@login.clsp.jhu.edu>
2019-05-31 09:17:56 -0400

    merging issues fix

--- egs/librispeech/asrtts1/cmd.sh
+++ egs/librispeech/asrtts1/cmd.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # ====== About run.pl, queue.pl, slurm.pl, and ssh.pl ======
 # Usage: <cmd>.pl [options] JOB=1:<nj> <log> <command...>
 # e.g.
@@ -88,38 +87,3 @@ else
     echo "$0: Error: Unknown cmd_backend=${cmd_backend}" 1>&2
     return 1
 fi
-=======
-# you can change cmd.sh depending on what type of queue you are using.
-# If you have no queueing system and want to run on a local machine, you
-# can change all instances 'queue.pl' to run.pl (but be careful and run
-# commands one by one: most recipes will exhaust the memory on your
-# machine).  queue.pl works with GridEngine (qsub).  slurm.pl works
-# with slurm.  Different queues are configured differently, with different
-# queue names and different ways of specifying things like memory;
-# to account for these differences you can create and edit the file
-# conf/queue.conf to match your queue's configuration.  Search for
-# conf/queue.conf in http://kaldi-asr.org/doc/queue.html for more information,
-# or search for the string 'default_config' in utils/queue.pl or utils/slurm.pl.
-
-export train_cmd="queue.pl --mem 2G -V"
-export cuda_cmd="queue.pl -V --gpu 1"
-export decode_cmd="queue.pl --mem 4G -V"
-CUDAROOT=/usr/local/share/cuda-8.0.61
-CUDAROOT=/usr/local/share/cuda-9.0.176
-
-export CUDA_HOME=$CUDAROOT
-export CUDA_PATH=$CUDAROOT
-export CPATH=$CUDA_HOME/include
-export PATH=$CUDAROOT/bin:$PATH
-export LD_LIBRARY_PATH=$CUDAROOT/lib64:$LD_LIBRARY_PATH
-export CC="/usr/local/bin/gcc-5.3"
-export CC="/usr/local/bin/gcc-6.4"
-export CXX="/usr/local/bin/g++-5.3"
-export CXX="/usr/local/bin/g++-6.4"
-
-
-# JHU setup
-#export train_cmd="queue.pl --mem 2G"
-#export cuda_cmd="queue.pl --mem 2G --gpu 1 --config conf/gpu.conf"
-#export decode_cmd="queue.pl --mem 4G"
->>>>>>> 3c086dddcae725e6068d5dffc26e5962617cf986
