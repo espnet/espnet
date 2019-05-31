02c7487539b93142b47f51247c90bc9092a99298
creatorscan <creatorscan@gmail.com>
2019-05-31 18:12:29 +0200

    adding test taco2 with changes

--- test/test_e2e_tts_tacotron2.py
+++ test/test_e2e_tts_tacotron2.py
@@ -9,9 +9,6 @@ import torch
 
 from argparse import Namespace
 
-from espnet.nets.pytorch_backend.e2e_tts import Tacotron2
-from espnet.nets.pytorch_backend.e2e_tts import Tacotron2Loss
-=======
 from espnet.nets.pytorch_backend.e2e_tts_tacotron2 import Tacotron2
 from espnet.nets.pytorch_backend.nets_utils import pad_list
 
@@ -159,7 +156,6 @@ def test_tacotron2_trainable_and_decodable(model_dict):
         spemb = None if model_args['spk_embed_dim'] is None else spembs[0]
         model.inference(xs[0][:ilens[0]], Namespace(**inference_args), spemb)
         model.calculate_all_attentions(xs, ilens, ys, spembs)
-=======
         spemb = None if model_args['spk_embed_dim'] is None else batch["spembs"][0]
         model.inference(batch["xs"][0][:batch["ilens"][0]], Namespace(**inference_args), spemb)
         model.calculate_all_attentions(**batch)
