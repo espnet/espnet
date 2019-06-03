b5915f40de0f886d89205fb52ec0a76320969191
karita <shigekikarita@gmail.com>
2019-06-03 10:45:21 +0900

    revert dropout

--- espnet/nets/pytorch_backend/tacotron2/decoder.py
+++ espnet/nets/pytorch_backend/tacotron2/decoder.py
@@ -81,7 +81,7 @@ class Prenet(torch.nn.Module):
         :rtype: torch.Tensor
         """
         for l in six.moves.range(len(self.prenet)):
-            x = F.dropout(self.prenet[l](x), self.dropout_rate, training=self.training)
+            x = F.dropout(self.prenet[l](x), self.dropout_rate)
         return x
 
 
