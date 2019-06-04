67c2d7cb8bd7cd57cdf48fedbc2fc6d6a924f4ca
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2019-06-04 18:13:49 +0900

    Merge make_batchset to batchfy

--- espnet/mt/pytorch_backend/mt.py
+++ espnet/mt/pytorch_backend/mt.py
@@ -184,7 +184,9 @@ def train(args):
                           batch_bins=args.batch_bins,
                           batch_frames_in=args.batch_frames_in,
                           batch_frames_out=args.batch_frames_out,
-                          batch_frames_inout=args.batch_frames_inout)
+                          batch_frames_inout=args.batch_frames_inout,
+                          ikey='output',
+                          iaxis=1)
     valid = make_batchset(valid_json, args.batch_size,
                           args.maxlen_in, args.maxlen_out, args.minibatches,
                           min_batch_size=args.ngpu if args.ngpu > 1 else 1,
@@ -192,7 +194,9 @@ def train(args):
                           batch_bins=args.batch_bins,
                           batch_frames_in=args.batch_frames_in,
                           batch_frames_out=args.batch_frames_out,
-                          batch_frames_inout=args.batch_frames_inout)
+                          batch_frames_inout=args.batch_frames_inout,
+                          ikey='output',
+                          iaxis=1)
 
     load_tr = LoadInputsAndTargets(
         mode='mt', load_output=True, preprocess_conf=args.preprocess_conf,
