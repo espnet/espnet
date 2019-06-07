74df695fdff39ad7f9599b9382583d6f87006138
Masao-Someki <mighty-masao@outlook.jp>
2019-06-08 00:05:58 +0900

    fixed recipe
    * remove mecab dictionary and related process.
    * in stage5, recipe before did not use the result of stage3,
      so attached a flag to use it.

--- egs/jsut/asr1/local/data_prep.sh
+++ egs/jsut/asr1/local/data_prep.sh
@@ -13,21 +13,12 @@ data_dir=$2
 scp=${data_dir}/wav.scp
 utt2spk=${data_dir}/utt2spk
 spk2utt=${data_dir}/spk2utt
-rawtext=${data_dir}/rawtext
 text=${data_dir}/text
-usr_dict_dir=/usr/local/lib/mecab/dic/mecab-ipadic-neologd
-esp_dict_dir=../../../tools/mecab/mecab-ipadic-neologd
 
 # check file existence
 [ -e ${scp} ] && rm ${scp}
 [ -e ${utt2spk} ] && rm ${utt2spk}
 [ -e ${rawtext} ] && rm ${rawtext}
-if [ ! -e ${usr_dict_dir} -a ! -e ${esp_dict_dir} ]; then
-    echo "$0: Error: The mecab-ipadic-NEologd can not be found" >&2
-    echo "$0: Error: Please modify the dictionary path in this script or" >&2
-    echo "$0: Error: install it by running 'make mecab.done' at espnet/tools" >&2
-    exit 1
-fi
 
 # make scp, utt2spk, and spk2utt
 find ${db} -name "*.wav" | sort | while read -r filename; do
@@ -40,14 +31,7 @@ echo "finished making wav.scp, utt2spk, spk2utt."
 
 # make text
 find ${db} -name "transcript_utf8.txt" | sort | while read -r filename; do
-    cat ${filename} >> ${rawtext}
+    cat ${filename} | tr ':' ' ' | sort >> ${text}
 done
-if [ -e ${usr_dict_dir} ]; then
-    dict=${usr_dict_dir}
-else
-    dict=${esp_dict_dir}
-fi
-PYTHONIOENCODING=utf-8 PYTHONPATH=local/text python local/clean_text.py \
-    ${dict} ${rawtext} > ${text}
-rm ${rawtext}
+
 echo "finished making text."
--- egs/jsut/asr1/run.sh
+++ egs/jsut/asr1/run.sh
@@ -76,12 +76,12 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     echo "stage 1: Feature Generation"
     fbankdir=fbank
     # Generate the fbank features; by default 80-dimensional fbanks with pitch on each frame
-	steps/make_fbank_pitch.sh --cmd "$train_cmd" \
-	    			--nj ${nj} \
-		    		--write_utt2num_frames true \
-        			data/train \
-   			 	exp/make_fbank/train  \
-				${fbankdir}
+    steps/make_fbank_pitch.sh --cmd "$train_cmd" \
+                              --nj ${nj} \
+		              --write_utt2num_frames true \
+                              data/train \
+                              exp/make_fbank/train  \
+                              ${fbankdir}
     # make a dev set
     utils/subset_data_dir.sh --first data/train 500 data/deveval
     utils/subset_data_dir.sh --first data/deveval 250 data/${recog_set}
@@ -97,11 +97,8 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
         data/${train_set}/feats.scp data/${train_set}/cmvn.ark exp/dump_feats/train ${feat_tr_dir}
     dump.sh --cmd "$train_cmd" --nj 4 --do_delta ${do_delta} \
         data/${train_dev}/feats.scp data/${train_set}/cmvn.ark exp/dump_feats/dev ${feat_dt_dir}
-
     for rtask in ${recog_set}; do
-
     	feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}; mkdir -p ${feat_recog_dir}
-
     	dump.sh --cmd "$train_cmd" --nj 4 --do_delta ${do_delta} \
         	data/${rtask}/feats.scp data/${train_set}/cmvn.ark exp/dump_feats/recog/${rtask} \
         	${feat_recog_dir}
@@ -118,19 +115,20 @@ if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
     text2token.py -s 1 -n 1 data/${train_set}/text | cut -f 2- -d" " | tr " " "\n" \
     | sort | uniq | grep -v -e '^\s*$' | awk '{print $0 " " NR+1}' >> ${dict}
     wc -l ${dict}
-
     # make json labels
     data2json.sh --feat ${feat_tr_dir}/feats.scp \
          data/${train_set} ${dict} > ${feat_tr_dir}/data.json
     data2json.sh --feat ${feat_dt_dir}/feats.scp \
          data/${train_dev} ${dict} > ${feat_dt_dir}/data.json
     for rtask in ${recog_set}; do
-    	feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}
-    	data2json.sh --feat ${feat_recog_dir}/feats.scp \
-        	data/${rtask} ${dict} > ${feat_recog_dir}/data.json
+     	feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}
+     	data2json.sh --feat ${feat_recog_dir}/feats.scp \
+         	data/${rtask} ${dict} > ${feat_recog_dir}/data.json
     done
 fi
 
+
+# You can skip this and remove --rnnlm option in the recognition (stage 5)
 if [ -z ${lmtag} ]; then
     lmtag=$(basename ${lm_config%.*})
 fi
@@ -139,32 +137,32 @@ lmexpdir=exp/${lmexpname}
 mkdir -p ${lmexpdir}
 
 if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
-    echo Stage3: LM Preparation
+    echo "stage 3: LM Preparation"
     lmdatadir=data/local/lm_train
     mkdir -p ${lmdatadir}
     text2token.py -s 1 -n 1 data/${train_set}/text | cut -f 2- -d" " \
-	   > ${lmdatadir}/train.txt 
+        > ${lmdatadir}/train.txt
     text2token.py -s 1 -n 1 data/${train_dev}/text | cut -f 2- -d" " \
-	   > ${lmdatadir}/valid.txt
-
-    # use only 1 gpu for train.
+        > ${lmdatadir}/valid.txt
+    # use only 1 gpu
     if [ ${ngpu} -gt 1 ]; then
-	echo "LM training does not support multi-gpu. Singlu gpu will be used."
+        echo "LM training does not support multi-gpu. signle gpu will be used."
+        lmngpu=1
+    else
+        lmngpu=${ngpu}
     fi
-    lmgpu=1
-    ${cuda_cmd} --gpu ${lmgpu} ${lmexpdir}/train.log \
-	    lm_train.py \
-	    	--config ${lm_config} \
-		--ngpu ${lmgpu} \
-		--backend ${backend} \
-		--verbose ${verbose} \
-		--outdir ${lmexpdir} \
-		--tensorboard-dir tensorboard/${lmexpname} \
-		--train-label ${lmdatadir}/train.txt \
-		--valid-label ${lmdatadir}/valid.txt \
-		--resume ${lm_resume} \
-		--dict ${dict}
-
+    ${cuda_cmd} --gpu ${lmngpu} ${lmexpdir}/train.log \
+        lm_train.py \
+        --config ${lm_config} \
+        --ngpu ${lmngpu} \
+        --backend ${backend} \
+        --verbose 1 \
+        --outdir ${lmexpdir} \
+        --tensorboard-dir tensorboard/${lmexpname} \
+        --train-label ${lmdatadir}/train.txt \
+        --valid-label ${lmdatadir}/valid.txt \
+        --resume ${lm_resume} \
+        --dict ${dict}
 fi
 
 
@@ -180,7 +178,7 @@ expdir=exp/${expname}
 mkdir -p ${expdir}
 
 if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
-    echo "stage 3: Network Training"
+    echo "stage 4: Network Training"
     ${cuda_cmd} --gpu ${ngpu} ${expdir}/train.log \
         asr_train.py \
         --config ${train_config} \
@@ -199,7 +197,7 @@ if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
 fi
 
 if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
-    echo "stage 4: Decoding"
+    echo "stage 5: Decoding"
     nj=16
 
     pids=() # initialize pids
@@ -223,7 +221,8 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
             --verbose ${verbose} \
             --recog-json ${feat_recog_dir}/split${nj}utt/data.JOB.json \
             --result-label ${expdir}/${decode_dir}/data.JOB.json \
-            --model ${expdir}/results/${recog_model}
+            --model ${expdir}/results/${recog_model} \
+	    --rnnlm ${lmexpdir}/rnnlm.model.best
 
         score_sclite.sh --wer true ${expdir}/${decode_dir} ${dict}
 
