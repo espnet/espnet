0dc380d90b37c387c1101bd2541475d15bdb847e
takenori-y <takenori.yoshimura24@gmail.com>
2019-06-08 14:07:44 +0900

    Move recog_wav

--- utils/recog_wav.sh
+++ utils/recog_wav.sh
@@ -3,39 +3,43 @@
 # Copyright 2019 Nagoya University (Takenori Yoshimura)
 #  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
 
+if [ ! -f path.sh -o ! -f cmd.sh ]; then
+    echo "Please change directory to e.g., egs/tedlium/asr1"
+    exit 1
+fi
+
 . ./path.sh
 . ./cmd.sh
 
 # general configuration
-backend=chainer
+backend=pytorch
 stage=0        # start from 0 if you need to start from data preparation
 stop_stage=100
 ngpu=0         # number of gpus ("0" uses cpu, otherwise use gpu)
-verbose=0      # verbose option
+debugmode=1
+verbose=1      # verbose option
 
 # feature configuration
 do_delta=false
-cmvn=cmvn.ark
+cmvn=
 
 # rnnlm related
-use_wordlm=true     # false means to use a character LM
-lang_model=rnnlm.model.best
+use_lang_model=true
+lang_model=
 
 # decoding parameter
-lm_weight=1.0
-beam_size=30
-penalty=0.0
-maxlenratio=0.0
-minlenratio=0.0
-ctc_weight=0.3
-recog_model=model.acc.best
+decode_config=
+recog_model=
 decode_dir=decode
+
+# download related
+recipe_with_ver=tedlium.demo
 
 . utils/parse_options.sh || exit 1;
 
 wav=$1
 
-if [ $# != 1 ]; then
+if [ $# -gt 1 ]; then
     echo "Usage: $0 <wav>"
     exit 1;
 fi
@@ -46,17 +50,67 @@ set -e
 set -u
 set -o pipefail
 
-# existence check
+case "${recipe_with_ver}" in
+    "tedlium.demo")
+        share_url=https://drive.google.com/open?id=1UqIY6WJMZ4sxNxSugUqp3mrGb3j6h7xe
+        cmvn_file=data/train_trim_sp/cmvn.ark
+        lang_model_file=exp/train_rnnlm_pytorch_lm_unigram500/rnnlm.model.best
+        recog_model_file=exp/train_trim_sp_pytorch_train/results/model.acc.best
+        decode_config_file=conf/decode_streaming.yaml
+        wav_file=etc/wav/TomWujec_2010U.wav
+        ;;
+    *)
+        echo "No such recipe: ${recipe_with_ver}"
+        exit 1
+        ;;
+esac
+
+function download_models () {
+    download_dir=${decode_dir}/download
+    mkdir -p ${download_dir}
+    if [ ! -e ${download_dir}/.complete ]; then
+        download_from_google_drive.sh ${share_url} ${download_dir} ".tar.gz"
+	touch ${download_dir}/.complete
+    fi
+}
+
+# Download trained models
+if [ -z "${cmvn}" ]; then
+    download_models
+    cmvn=${download_dir}/${cmvn_file}
+fi
+if [ -z "${lang_model}" -a ${use_lang_model} ]; then
+    download_models
+    lang_model=${download_dir}/${lang_model_file}
+fi
+if [ -z "${recog_model}" ]; then
+    download_models
+    recog_model=${download_dir}/${recog_model_file}
+fi
+if [ -z "${decode_config}" ]; then
+    download_models
+    decode_config=${download_dir}/${decode_config_file}
+fi
+if [ -z "${wav}" ]; then
+    download_models
+    wav=${download_dir}/${wav_file}
+fi
+
+# Check file existence
 if [ ! -f "${cmvn}" ]; then
     echo "No such CMVN file: ${cmvn}"
     exit 1
 fi
-if [ ! -f "${lang_model}" -a `echo "${lm_weight} > 0.0" | bc` -eq 1 ]; then
+if [ ! -f "${lang_model}" -a ${use_lang_model} ]; then
     echo "No such language model: ${lang_model}"
     exit 1
 fi
 if [ ! -f "${recog_model}" ]; then
     echo "No such E2E model: ${recog_model}"
+    exit 1
+fi
+if [ ! -f "${decode_config}" ]; then
+    echo "No such config file: ${decode_config}"
     exit 1
 fi
 if [ ! -f "${wav}" ]; then
@@ -100,30 +154,23 @@ fi
 if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
     echo "stage 3: Decoding"
 
-    if [ ${use_wordlm} = true ]; then
-        recog_opts="--word-rnnlm ${lang_model}"
-    else
+    if [ ${use_lang_model} ]; then
         recog_opts="--rnnlm ${lang_model}"
-    fi
-    if [ ${lm_weight} == 0 ]; then
+    else
         recog_opts=""
     fi
     feat_recog_dir=${decode_dir}/dump
 
     ${decode_cmd} ${decode_dir}/log/decode.log \
         asr_recog.py \
+        --config ${decode_config} \
         --ngpu ${ngpu} \
         --backend ${backend} \
+        --debugmode ${debugmode} \
         --verbose ${verbose} \
         --recog-json ${feat_recog_dir}/data.json \
         --result-label ${decode_dir}/result.json \
         --model ${recog_model} \
-        --beam-size ${beam_size} \
-        --penalty ${penalty} \
-        --maxlenratio ${maxlenratio} \
-        --minlenratio ${minlenratio} \
-        --ctc-weight ${ctc_weight} \
-        --lm-weight ${lm_weight} \
         ${recog_opts}
 
     echo ""
