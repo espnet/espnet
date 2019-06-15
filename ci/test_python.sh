f562940dc76eb42af0073ef712d53ea6db48194d
Shigeki Karita <shigekikarita@gmail.com>
2019-06-15 13:42:00 +0900

    Update test_python.sh

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -10,4 +10,6 @@ flake8 espnet test utils;
 autopep8 -r espnet test utils --global-config .pep8 --diff --max-line-length 120 | tee check_autopep8
 test ! -s check_autopep8
 
-LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:$(pwd)/chainer_ctc/ext/warp-ctc/build" pytest
+pip install codecov
+LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:$(pwd)/chainer_ctc/ext/warp-ctc/build" pytest--cov=codecov
+codecov
