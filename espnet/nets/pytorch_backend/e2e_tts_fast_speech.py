ed0c8a224dda01969e5010ec3efc49430ef5d333
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-06-15 18:49:32 +0900

    implemented duration calculator

--- espnet/nets/pytorch_backend/e2e_tts_fast_speech.py
+++ espnet/nets/pytorch_backend/e2e_tts_fast_speech.py
@@ -6,7 +6,10 @@
 
 import torch
 
+from espnet.nets.pytorch_backend.e2e_tts_transformer import Transformer
+from espnet.nets.pytorch_backend.nets_utils import make_pad_mask
 from espnet.nets.pytorch_backend.nets_utils import pad_list
+from espnet.nets.pytorch_backend.transformer.attention import MultiHeadedAttention
 from espnet.nets.pytorch_backend.transformer.layer_norm import LayerNorm
 from espnet.nets.tts_interface import TTSInterface
 
@@ -42,16 +45,81 @@ class FeedForwardTransformer(TTSInterface, torch.nn.Module):
         pass
 
 
+class DurationCalculator(torch.nn.Module):
+    """Duration calculator using teacher model
+
+    :param e2e_tts_transformer.Transformer teacher_model: teacher auto-regressive Transformer
+    """
+
+    def __init__(self, teacher_model):
+        super(DurationCalculator, self).__init__()
+        if not isinstance(teacher_model, Transformer):
+            raise ValueError("teacher model should be the instance of e2e_tts_transformer.Transformer")
+        self.teacher_model = teacher_model
+        self.diag_head_info = None
+
+    def forward(self, xs, ilens, ys, olens):
+        """Calculate duration of each inputs
+
+        :param torch.Tensor xs: batch of padded character ids (B, Tmax)
+        :param torch.Tensor ilens: list of lengths of each input batch (B)
+        :param torch.Tensor ys: batch of padded target features (B, Lmax, odim)
+        :param torch.Tensor ilens: list of lengths of each output batch (B)
+        :return torch.Tensor: batch of durations (B, Tmax)
+        """
+        att_ws_dict = self.calculate_all_attentions(xs, ilens, ys, olens)
+        if self.diag_head_info is None:
+            self._decide_diogonal_head(att_ws_dict)
+        att_ws = att_ws_dict[self.diag_head_info[0]][:, self.diag_head_info[1]]  # (B, Lmax, Tmax)
+        durations = torch.stack(
+            [att_ws.argmax(dim=-1).eq(i).sum(dim=-1) for i in range(max(ilens))], dim=-1)  # (B, Tmax)
+        durations.masked_fill_(make_pad_mask(ilens).to(durations.device), 0)
+
+        return durations
+
+    def _calculate_att_ws_dict(self, xs, ilens, ys, olens):
+        with torch.no_grad():
+            x_masks = self.teacher_model._source_mask(ilens)
+            hs, _ = self.teacher_model.encoder(xs, x_masks)
+            ys_in = self.teacher_model._add_first_frame_and_remove_last_frame(ys)
+            if self.teacher_model.reduction_factor > 1:
+                ys_in = ys_in[:, self.teacher_model.reduction_factor - 1::self.teacher_model.reduction_factor]
+                olens_in = olens.new([olen // self.teacher_model.reduction_factor for olen in olens])
+            else:
+                olens_in = olens
+            y_masks = self.teacher_model._target_mask(olens_in)
+            xy_masks = self.teacher_model._source_to_target_mask(ilens, olens_in)
+            self.teacher_model.decoder(ys_in, y_masks, hs, xy_masks)
+        att_ws_dict = {}
+        for name, m in self.teacher_model.named_modules():
+            if isinstance(m, MultiHeadedAttention):
+                att_ws_dict[name] = m.attn
+
+        return att_ws_dict
+
+    def _decide_diogonal_head(self, att_ws_dict):
+        best_diagonal_score = 0.0
+        for key in att_ws_dict.keys():
+            att_ws = att_ws_dict[key]
+            diagonal_scores = att_ws.max(dim=-1).mean(dim=-1).mean(dim=0)  # (H,)
+            if best_diagonal_score < diagonal_scores.max():
+                best_diagonal_score = diagonal_scores.max()
+                self.diag_head_info = (key, int(diagonal_scores.argmax()))
+
+
 class LengthRegularizer(torch.nn.Module):
-    """Lenght regularizer module
+    """Length regularizer module
 
     Reference:
         - FastSpeech: Fast, Robust and Controllable Text to Speech
           (https://arxiv.org/pdf/1905.09263.pdf)
+
+    :param float pad_value: value used for padding
     """
 
-    def __init__(self):
+    def __init__(self, pad_value=0.0):
         super(LengthRegularizer, self).__init__()
+        self.pad_value = pad_value
 
     def forward(self, xs, ds, ilens):
         """Apply length regularizer
@@ -64,7 +132,8 @@ class LengthRegularizer(torch.nn.Module):
         xs = [x[:ilen] for x, ilen in zip(xs, ilens)]
         ds = [d[:ilen] for d, ilen in zip(ds, ilens)]
         xs = [self._repeat_one_sequence(x, d) for x, d in zip(xs, ds)]
-        return pad_list(xs, 0)
+
+        return pad_list(xs, self.pad_value)
 
     def _repeat_one_sequence(self, x, d):
         """Repeat each frame according to duration
