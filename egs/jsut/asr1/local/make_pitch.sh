60d82850d0a91b622c547a5350424a43499070a4
Masao-Someki <mighty-masao@outlook.jp>
2019-06-16 07:01:27 +0900

    rename make_pitch.sh to change_sampling_rate.sh
    * rename file
    * add `conf/*_16k.conf` file
    * `pitch=$1` to `sampling_rate=$1`

--- egs/jsut/asr1/local/make_pitch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#This script is to adjust sampling rate of the wav files and config files.
-
-pitch=$1
-
-# fix fbank.conf
-sed -i -e "s/sample-frequency=[0-9]*/sample-frequency=$pitch/" ./conf/fbank.conf
-# fix pitch.conf
-sed -i -e "s/sample-frequency=[0-9]*/sample-frequency=$pitch/" ./conf/pitch.conf
