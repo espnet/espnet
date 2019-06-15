784c5ad103dee8fb1341e3e363513a215bbd5ea4
karita <shigekikarita@gmail.com>
2019-06-15 14:01:19 +0900

    fix codecov

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -10,6 +10,5 @@ flake8 espnet test utils;
 autopep8 -r espnet test utils --global-config .pep8 --diff --max-line-length 120 | tee check_autopep8
 test ! -s check_autopep8
 
-pip install codecov
-LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:$(pwd)/chainer_ctc/ext/warp-ctc/build" pytest --cov=codecov
-codecov
+LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:$(pwd)/chainer_ctc/ext/warp-ctc/build" pytest --cov=espnet
+
