42057492f23c8473cdcf99e729ea5c429b98279c
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-06-19 08:42:05 +0900

    added position wise conv1d

--- espnet/nets/pytorch_backend/transformer/positionwise_feed_forward.py
+++ espnet/nets/pytorch_backend/transformer/positionwise_feed_forward.py
@@ -10,3 +10,17 @@ class PositionwiseFeedForward(torch.nn.Module):
 
     def forward(self, x):
         return self.w_2(self.dropout(torch.relu(self.w_1(x))))
+
+
+class PositionwiseConv1d(torch.nn.Module):
+    def __init__(self, d_model, d_ff, kernel_size, dropout):
+        super(PositionwiseConv1d, self).__init__()
+        self.w_1 = torch.nn.Conv1d(d_model, d_ff, kernel_size,
+                                   stride=1, padding=(kernel_size - 1) // 2)
+        self.w_2 = torch.nn.Conv1d(d_ff, d_model, kernel_size,
+                                   stride=1, padding=(kernel_size - 1) // 2)
+        self.dropout = torch.nn.Dropout(dropout)
+
+    def forward(self, x):
+        x = torch.relu(self.w_1(x.transpose(-1, 1))).transpose(-1, 1)
+        return self.w_2(self.dropout(x)).transpose(-1, 1)
