3f92653bf1bf405bd93990619ab46d1ba1a2164a
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-06-19 12:33:07 +0900

    added docstring and refactored variable names

--- espnet/nets/pytorch_backend/transformer/positionwise_feed_forward.py
+++ espnet/nets/pytorch_backend/transformer/positionwise_feed_forward.py
@@ -2,24 +2,41 @@ import torch
 
 
 class PositionwiseFeedForward(torch.nn.Module):
-    def __init__(self, d_model, d_ff, dropout):
+    """Positionwise feed forward
+
+    :param int idim: input dimenstion
+    :param int hidden_units: number of hidden units
+    :param float dropout_rate: dropout rate
+    """
+    def __init__(self, idim, hidden_units, dropout_rate):
         super(PositionwiseFeedForward, self).__init__()
-        self.w_1 = torch.nn.Linear(d_model, d_ff)
-        self.w_2 = torch.nn.Linear(d_ff, d_model)
-        self.dropout = torch.nn.Dropout(dropout)
+        self.w_1 = torch.nn.Linear(idim, hidden_units)
+        self.w_2 = torch.nn.Linear(hidden_units, idim)
+        self.dropout = torch.nn.Dropout(dropout_rate)
 
     def forward(self, x):
         return self.w_2(self.dropout(torch.relu(self.w_1(x))))
 
 
 class PositionwiseConv1d(torch.nn.Module):
-    def __init__(self, d_model, d_ff, kernel_size, dropout):
+    """Positionwise conv1d
+
+    Reference:
+        FastSpeech: Fast, Robust and Controllable Text to Speech
+        (https://arxiv.org/pdf/1905.09263.pdf)
+
+    :param int in_chans: number of input channels
+    :param int hidden_chans: number of hidden channels
+    :param int kernel_size: kernel size of conv1d
+    :param float dropout_rate: dropout rate
+    """
+    def __init__(self, in_chans, hidden_chans, kernel_size, dropout_rate):
         super(PositionwiseConv1d, self).__init__()
-        self.w_1 = torch.nn.Conv1d(d_model, d_ff, kernel_size,
+        self.w_1 = torch.nn.Conv1d(in_chans, hidden_chans, kernel_size,
                                    stride=1, padding=(kernel_size - 1) // 2)
-        self.w_2 = torch.nn.Conv1d(d_ff, d_model, kernel_size,
+        self.w_2 = torch.nn.Conv1d(hidden_chans, in_chans, kernel_size,
                                    stride=1, padding=(kernel_size - 1) // 2)
-        self.dropout = torch.nn.Dropout(dropout)
+        self.dropout = torch.nn.Dropout(dropout_rate)
 
     def forward(self, x):
         x = torch.relu(self.w_1(x.transpose(-1, 1))).transpose(-1, 1)
