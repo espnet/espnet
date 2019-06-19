f13f2e99fab1bf62dacf876440ceb3ac0396fc10
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-06-19 16:55:06 +0900

    fixed

--- espnet/nets/pytorch_backend/transformer/multi_layer_conv.py
+++ espnet/nets/pytorch_backend/transformer/multi_layer_conv.py
@@ -19,8 +19,9 @@ class MultiLayeredConv1d(torch.nn.Module):
     :param int kernel_size: kernel size of conv1d
     :param float dropout_rate: dropout rate
     """
+
     def __init__(self, in_chans, hidden_chans, kernel_size, dropout_rate):
-        super(PositionwiseConv1d, self).__init__()
+        super(MultiLayeredConv1d, self).__init__()
         self.w_1 = torch.nn.Conv1d(in_chans, hidden_chans, kernel_size,
                                    stride=1, padding=(kernel_size - 1) // 2)
         self.w_2 = torch.nn.Conv1d(hidden_chans, in_chans, kernel_size,
