233ae898a361d9bc0122bdd9885ef7e8739d44cf
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2019-06-21 01:08:09 +0900

    Update for review

--- egs/librispeech_french/st1/run.sh
+++ egs/librispeech_french/st1/run.sh
@@ -37,8 +37,7 @@ case=lc
 # lc.rm: lowercase with punctuation removal
 
 # Set this to somewhere where you want to put your data, or where
-# someone else has already put it.  You'll want to change this
-# if you're not on the CLSP grid.
+# someone else has already put it.
 datadir=/n/sd3/inaguma/corpus/libri_french/data
 # libri_french
 #  |_ train/
