2877611b85727e0d0736bd9970ca192cde8e976c
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-06-21 13:38:38 +0900

    updated docstrings

--- espnet/nets/pytorch_backend/transformer/multi_layer_conv.py
+++ espnet/nets/pytorch_backend/transformer/multi_layer_conv.py
@@ -8,16 +8,20 @@ import torch
 
 
 class MultiLayeredConv1d(torch.nn.Module):
-    """Multi-layered conv1d for Transformer block
+    """Multi-layered conv1d for Transformer block.
 
-    Reference:
-        FastSpeech: Fast, Robust and Controllable Text to Speech
-        (https://arxiv.org/pdf/1905.09263.pdf)
+    This is a module of multi-leyered conv1d designed to replace positionwise feed-forward network
+    in Transforner block, which is introduced in `FastSpeech: Fast, Robust and Controllable Text to Speech`_.
 
-    :param int in_chans: number of input channels
-    :param int hidden_chans: number of hidden channels
-    :param int kernel_size: kernel size of conv1d
-    :param float dropout_rate: dropout rate
+    Args:
+        in_chans (int): Number of input channels.
+        hidden_chans (int): Number of hidden channels.
+        kernel_size (int): Kernel size of conv1d.
+        dropout_rate (float): Dropout rate.
+
+    .. _`FastSpeech: Fast, Robust and Controllable Text to Speech`:
+        https://arxiv.org/pdf/1905.09263.pdf
+
     """
 
     def __init__(self, in_chans, hidden_chans, kernel_size, dropout_rate):
@@ -29,5 +33,14 @@ class MultiLayeredConv1d(torch.nn.Module):
         self.dropout = torch.nn.Dropout(dropout_rate)
 
     def forward(self, x):
+        """Calculate forward propagation.
+
+        Args:
+            x (Tensor): Batch of input tensors (B, *, in_chans).
+
+        Returns:
+            Tensor: Batch of output tensors (B, *, hidden_chans)
+
+        """
         x = torch.relu(self.w_1(x.transpose(-1, 1))).transpose(-1, 1)
         return self.w_2(self.dropout(x).transpose(-1, 1)).transpose(-1, 1)
