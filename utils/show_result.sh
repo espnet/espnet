2a23ca73e863bdfd349a342c361df0060bbbc95f
Naoyuki Kamo <naoyuki.kamo829@gmail.com>
2019-06-21 19:38:31 +0900

    Change to positional

--- utils/show_result.sh
+++ utils/show_result.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-exp=exp
 mindepth=0
 maxdepth=1
 
 . utils/parse_options.sh
 
-if [ $# -ne 0 ]; then
-    echo "Usage: $0 --exp exp --mindepth 0 --maxdepth 1" 1>&2
+if [ $# -gt 1 ]; then
+    echo "Usage: $0 --mindepth 0 --maxdepth 1 [exp]" 1>&2
+    echo ""
+    echo "Show the system environments and the evaluation results in Markdown format."
+    echo 'The default of <exp> is "exp/".'
     exit 1
 fi
 
 [ -f ./path.sh ] && . ./path.sh
 set -euo pipefail
+if [ $# -eq 1 ]; then
+    exp=$1
+else
+    exp=exp
+fi
 
 
 cat << EOF
