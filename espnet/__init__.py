2772c6a8dbf6b5092a4d7f7bb70f24ed3139ca39
karita <shigekikarita@gmail.com>
2019-06-27 13:02:16 +0900

    remove espnet

--- espnet/__init__.py
+++
@@ -1,7 +0,0 @@
-import pkg_resources
-
-try:
-    __version__ = pkg_resources.get_distribution('espnet').version
-except Exception:
-    __version__ = '(Not installed from setup.py)'
-del pkg_resources
