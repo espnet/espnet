370b16282ae4ea9319f3fce86373f68ae028e3d8
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2019-06-30 01:09:36 +0900

    change to refer add_arguments from E2E pytorch

--- espnet/nets/chainer_backend/e2e_asr.py
+++ espnet/nets/chainer_backend/e2e_asr.py
@@ -17,6 +17,7 @@ from espnet.nets.chainer_backend.ctc import ctc_for
 from espnet.nets.chainer_backend.decoders import decoder_for
 from espnet.nets.chainer_backend.encoders import encoder_for
 from espnet.nets.e2e_asr_common import label_smoothing_dist
+from espnet.nets.pytorch_backend.e2e_asr import E2E as E2E_pytorch
 
 CTC_LOSS_THRESHOLD = 10000
 
@@ -24,121 +25,7 @@ CTC_LOSS_THRESHOLD = 10000
 class E2E(ASRInterface, chainer.Chain):
     @staticmethod
     def add_arguments(parser):
-        E2E.encoder_add_arguments(parser)
-        E2E.attention_add_arguments(parser)
-        E2E.decoder_add_arguments(parser)
-        E2E.loss_add_arguments(parser)
-        E2E.recognition_add_arguments(parser)
-        return parser
-
-    @staticmethod
-    def encoder_add_arguments(parser):
-        group = parser.add_argument_group("E2E encoder setting")
-        # encoder
-        group.add_argument('--etype', default='blstmp', type=str,
-                           choices=['lstm', 'blstm', 'lstmp', 'blstmp', 'vgglstmp', 'vggblstmp', 'vgglstm', 'vggblstm',
-                                    'gru', 'bgru', 'grup', 'bgrup', 'vgggrup', 'vggbgrup', 'vgggru', 'vggbgru'],
-                           help='Type of encoder network architecture')
-        group.add_argument('--elayers', default=4, type=int,
-                           help='Number of encoder layers (for shared recognition part in multi-speaker asr mode)')
-        group.add_argument('--eunits', '-u', default=300, type=int,
-                           help='Number of encoder hidden units')
-        group.add_argument('--eprojs', default=320, type=int,
-                           help='Number of encoder projection units')
-        group.add_argument('--subsample', default="1", type=str,
-                           help='Subsample input frames x_y_z means subsample every x frame at 1st layer, '
-                                'every y frame at 2nd layer etc.')
-        return parser
-
-    @staticmethod
-    def attention_add_arguments(parser):
-        group = parser.add_argument_group("E2E attention setting")
-        # attention
-        group.add_argument('--atype', default='dot', type=str,
-                           choices=['noatt', 'dot', 'add', 'location', 'coverage',
-                                    'coverage_location', 'location2d', 'location_recurrent',
-                                    'multi_head_dot', 'multi_head_add', 'multi_head_loc',
-                                    'multi_head_multi_res_loc'],
-                           help='Type of attention architecture')
-        group.add_argument('--adim', default=320, type=int,
-                           help='Number of attention transformation dimensions')
-        group.add_argument('--awin', default=5, type=int,
-                           help='Window size for location2d attention')
-        group.add_argument('--aheads', default=4, type=int,
-                           help='Number of heads for multi head attention')
-        group.add_argument('--aconv-chans', default=-1, type=int,
-                           help='Number of attention convolution channels \
-                           (negative value indicates no location-aware attention)')
-        group.add_argument('--aconv-filts', default=100, type=int,
-                           help='Number of attention convolution filters \
-                           (negative value indicates no location-aware attention)')
-        group.add_argument('--dropout-rate', default=0.0, type=float,
-                           help='Dropout rate for the encoder')
-        return parser
-
-    @staticmethod
-    def decoder_add_arguments(parser):
-        group = parser.add_argument_group("E2E encoder setting")
-        group.add_argument('--dtype', default='lstm', type=str,
-                           choices=['lstm', 'gru'],
-                           help='Type of decoder network architecture')
-        group.add_argument('--dlayers', default=1, type=int,
-                           help='Number of decoder layers')
-        group.add_argument('--dunits', default=320, type=int,
-                           help='Number of decoder hidden units')
-        group.add_argument('--dropout-rate-decoder', default=0.0, type=float,
-                           help='Dropout rate for the decoder')
-        group.add_argument('--sampling-probability', default=0.0, type=float,
-                           help='Ratio of predicted labels fed back to decoder')
-        return parser
-
-    @staticmethod
-    def loss_add_arguments(parser):
-        group = parser.add_argument_group("E2E loss setting")
-        group.add_argument('--ctc_type', default='warpctc', type=str,
-                           choices=['builtin', 'warpctc'],
-                           help='Type of CTC implementation to calculate loss.')
-        group.add_argument('--mtlalpha', default=0.5, type=float,
-                           help='Multitask learning coefficient, alpha: alpha*ctc_loss + (1-alpha)*att_loss ')
-        group.add_argument('--lsm-type', const='', default='', type=str, nargs='?', choices=['', 'unigram'],
-                           help='Apply label smoothing with a specified distribution type')
-        group.add_argument('--lsm-weight', default=0.0, type=float,
-                           help='Label smoothing weight')
-        return parser
-
-    @staticmethod
-    def recognition_add_arguments(parser):
-        group = parser.add_argument_group("E2E recognition setting")
-        # recognition options to compute CER/WER
-        group.add_argument('--report-cer', default=False, action='store_true',
-                           help='Compute CER on development set')
-        group.add_argument('--report-wer', default=False, action='store_true',
-                           help='Compute WER on development set')
-        group.add_argument('--nbest', type=int, default=1,
-                           help='Output N-best hypotheses')
-        group.add_argument('--beam-size', type=int, default=4,
-                           help='Beam size')
-        group.add_argument('--penalty', default=0.0, type=float,
-                           help='Incertion penalty')
-        group.add_argument('--maxlenratio', default=0.0, type=float,
-                           help="""Input length ratio to obtain max output length.
-                           If maxlenratio=0.0 (default), it uses a end-detect function
-                           to automatically find maximum hypothesis lengths""")
-        group.add_argument('--minlenratio', default=0.0, type=float,
-                           help='Input length ratio to obtain min output length')
-        group.add_argument('--ctc-weight', default=0.3, type=float,
-                           help='CTC weight in joint decoding')
-        group.add_argument('--rnnlm', type=str, default=None,
-                           help='RNNLM model file to read')
-        group.add_argument('--rnnlm-conf', type=str, default=None,
-                           help='RNNLM model config file to read')
-        group.add_argument('--lm-weight', default=0.1, type=float,
-                           help='RNNLM weight.')
-        group.add_argument('--sym-space', default='<space>', type=str,
-                           help='Space symbol')
-        group.add_argument('--sym-blank', default='<blank>', type=str,
-                           help='Blank symbol')
-        return parser
+        return E2E_pytorch.add_arguments(parser)
 
     def __init__(self, idim, odim, args, flag_return=True):
         chainer.Chain.__init__(self)
