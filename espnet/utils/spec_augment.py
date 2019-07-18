11a58f999a5044b238dcbc166ce1cb56f5999540
Nanxin Chen <bobchennan@gmail.com>
2019-07-18 11:22:04 -0400

    fix shape

--- espnet/utils/spec_augment.py
+++ espnet/utils/spec_augment.py
@@ -61,7 +61,7 @@ def specaug(spec, W=40, F=27, T=70, num_freq_masks=2, num_time_masks=2, p=0.2, r
 def time_warp(spec, W=40):
     """Time warping
 
-    :param torch.Tensor spec: input tensor with shape (T, dim)
+    :param torch.Tensor spec: input tensor with shape (dim, T)
     :param int W: time warp parameter
     """
     if W == 0:
@@ -89,7 +89,7 @@ def time_warp(spec, W=40):
 def freq_mask(spec, F=30, num_masks=1, pad_value=0):
     """Frequency masking
 
-    :param torch.Tensor spec: input tensor with shape (T, dim)
+    :param torch.Tensor spec: input tensor with shape (dim, T)
     :param int F: maximum width of each mask
     :param int num_masks: number of masks
     :param float pad_value: values for masking
@@ -113,7 +113,7 @@ def freq_mask(spec, F=30, num_masks=1, pad_value=0):
 def time_mask(spec, T=40, num_masks=1, p=0.2, pad_value=0):
     """Time masking
 
-    :param torch.Tensor spec: input tensor with shape (T, dim)
+    :param torch.Tensor spec: input tensor with shape (dim, T)
     :param int T: maximum width of each mask
     :param int num_masks: number of masks
     :param float pad_value: values for masking
