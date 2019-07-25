faa74604e2d26d8b525561bafde889205233a3ee
wangxiaofei <xiaofei.wang1987@gmail.com>
2019-07-24 20:53:15 -0400

    Improve REVERB multi-channel recipes (train on 2ch, test on 8ch)

--- egs/reverb/asr1_2ch/run.sh
+++ egs/reverb/asr1_2ch/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Copyright 2018 Johns Hopkins University (Shinji Watanabe)
+# Copyright 2019 Johns Hopkins University (Xiaofei Wang)
 #  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
 
 . ./path.sh
@@ -12,14 +13,12 @@ stage=0        # start from 0 if you need to start from data preparation
 stop_stage=100
 ngpu=1         # number of gpus ("0" uses cpu, otherwise use gpu)
 debugmode=1
-dumpdir=dump   # directory to dump full features
 N=0            # number of minibatches to be used (mainly for debugging). "0" uses all minibatches.
 verbose=0      # verbose option
 resume=        # Resume the training from snapshot
 
-# feature configuration
-do_delta=false
-
+# configuration path
+preprocess_config=conf/preprocess.json
 train_config=conf/train.yaml
 lm_config=conf/lm.yaml
 decode_config=conf/decode.yaml
@@ -33,10 +32,8 @@ lmtag=              # tag for managing LMs
 # decoding parameter
 recog_model=model.acc.best # set a model to be used for decoding: 'model.acc.best' or 'model.loss.best'
 
-# Dereverberation Measures
-compute_se=true # flag for turing on computation of dereverberation measures
-enable_pesq=false # please make sure that you or your institution have the license to report PESQ before turning on this flag
-nch_se=8
+# enhanced speech option
+fs=16000
 
 # data
 reverb=/export/corpora5/REVERB_2014/REVERB    # JHU setup
@@ -59,9 +56,10 @@ set -e
 set -u
 set -o pipefail
 
-train_set=tr_simu_8ch_si284
-train_dev=dt_mult_1ch
-recog_set="dt_real_8ch_beamformit dt_simu_8ch_beamformit et_real_8ch_beamformit et_simu_8ch_beamformit dt_real_1ch_wpe dt_simu_1ch_wpe et_real_1ch_wpe et_simu_1ch_wpe"
+train_set=tr_simu_2ch_si284
+train_dev=dt_multi_2ch
+#recog_set="dt_simu_8ch dt_real_8ch et_simu_8ch et_real_8ch"
+recog_set="et_simu_8ch et_real_8ch"
 
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
     ### Task dependent. You have to make the following data preparation part by yourself.
@@ -72,71 +70,59 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
     local/prepare_simu_data.sh --wavdir ${wavdir} ${reverb} ${wsjcam0}
     local/prepare_real_data.sh --wavdir ${wavdir} ${reverb}
 
-    # Run WPE and Beamformit
-    local/run_wpe.sh
-    local/run_beamform.sh ${wavdir}/WPE/
-    if ${compute_se}; then
-      if [ ! -d local/REVERB_scores_source ] || [ ! -d local/REVERB_scores_source/REVERB-SPEENHA.Release04Oct/evaltools/SRMRToolbox ] || [ ! -f local/PESQ ]; then
-        # download and install speech enhancement evaluation tools
-        local/download_se_eval_tool.sh
-      fi
-      pesqdir=${PWD}/local
-      local/compute_se_scores.sh --nch ${nch_se} --enable_pesq ${enable_pesq} ${reverb} ${wavdir} ${pesqdir}
-      cat exp/compute_se_${nch_se}ch/scores/score_SimData
-      cat exp/compute_se_${nch_se}ch/scores/score_RealData
-    fi
-
     # Additionally use WSJ clean data. Otherwise the encoder decoder is not well trained
     local/wsj_data_prep.sh ${wsj0}/??-{?,??}.? ${wsj1}/??-{?,??}.?
     local/wsj_format_data.sh
 fi
 
-feat_tr_dir=${dumpdir}/${train_set}/delta${do_delta}; mkdir -p ${feat_tr_dir}
-feat_dt_dir=${dumpdir}/${train_dev}/delta${do_delta}; mkdir -p ${feat_dt_dir}
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     ### Task dependent. You have to design training and dev sets by yourself.
     ### But you can utilize Kaldi recipes in most cases
-    echo "stage 1: Feature Generation"
+    echo "stage 1: Dump wav files into a HDF5 file"
     # Generate the fbank features; by default 80-dimensional fbanks with pitch on each frame
-    fbankdir=fbank
-    tasks="${recog_set} tr_simu_8ch train_si284"
-    for x in ${tasks}; do
-        utils/copy_data_dir.sh data/${x} data-fbank/${x}
-        steps/make_fbank_pitch.sh --nj 32 --cmd "${train_cmd}" --write_utt2num_frames true \
-            data-fbank/${x} exp/make_fbank/${x} ${fbankdir}
-        utils/fix_data_dir.sh data-fbank/${x}
+    tasks="${recog_set} tr_simu_2ch"
+    tasks_2ch="tr_simu_2ch dt_simu_2ch dt_real_2ch"
+    for setname in ${recog_set}; do
+        echo ${setname}
+        mkdir -p data/${setname}_multich
+        <data/${setname}/utt2spk sed -r 's/^(.*?)_[A-H](_.*?) /\1\2 /g' | sort -u > data/${setname}_multich/utt2spk
+        <data/${setname}/text sed -r 's/^(.*?)_[A-H](_.*?) /\1\2 /g' | sort -u > data/${setname}_multich/text
+        <data/${setname}_multich/utt2spk utils/utt2spk_to_spk2utt.pl >data/${setname}_multich/spk2utt
+
+        for ch in {A..H}; do
+            <data/${setname}/wav.scp grep "_${ch}_" | sed -r 's/^(.*?)_[A-H](_.*?) /\1\2 /g' >data/${setname}_multich/wav_ch${ch}.scp
+        done
+        mix-mono-wav-scp.py data/${setname}_multich/wav_ch*.scp > data/${setname}_multich/wav.scp
     done
 
-    echo "combine reverb simulation and wsj clean training data"
-    utils/combine_data.sh data-fbank/${train_set} data-fbank/tr_simu_8ch data-fbank/train_si284
+    for setname in ${tasks_2ch}; do
+        echo ${setname}
+        mkdir -p data/${setname}_multich
+        <data/${setname}/utt2spk sed -r 's/^(.*?)_[A-B](_.*?) /\1\2 /g' | sort -u > data/${setname}_multich/utt2spk
+        <data/${setname}/text sed -r 's/^(.*?)_[A-B](_.*?) /\1\2 /g' | sort -u > data/${setname}_multich/text
+        <data/${setname}_multich/utt2spk utils/utt2spk_to_spk2utt.pl >data/${setname}_multich/spk2utt
+
+        for ch in {A..B}; do
+            <data/${setname}/wav.scp grep "_${ch}_" | sed -r 's/^(.*?)_[A-B](_.*?) /\1\2 /g' >data/${setname}_multich/wav_ch${ch}.scp
+        done
+        mix-mono-wav-scp.py data/${setname}_multich/wav_ch*.scp > data/${setname}_multich/wav.scp
+    done
+
+    # Note that data/tr05_multi_noisy_multich has multi-channel wav data, while data/train_si284 has 1ch only
+    dump_pcm.sh --nj 32 --cmd "${train_cmd}" --filetype "sound.hdf5" data/train_si284
+    for setname in ${tasks_2ch} ${recog_set}; do
+        dump_pcm.sh --nj 32 --cmd "${train_cmd}" --filetype "sound.hdf5" data/${setname}_multich
+    done
     echo "combine real and simulation development data"
-    utils/combine_data.sh data-fbank/${train_dev} data-fbank/dt_real_1ch data-fbank/dt_simu_1ch
-
-    # compute global CMVN
-    compute-cmvn-stats scp:data-fbank/${train_set}/feats.scp data-fbank/${train_set}/cmvn.ark
-
-    # dump features for training
-    if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d ${feat_tr_dir}/storage ]; then
-    utils/create_split_dir.pl \
-        /export/a{11,12,13,14}/${USER}/espnet-data/egs/reverb/asr1/dump/${train_set}/delta${do_delta}/storage \
-        ${feat_tr_dir}/storage
-    fi
-    if [[ $(hostname -f) == *.clsp.jhu.edu ]] && [ ! -d ${feat_dt_dir}/storage ]; then
-    utils/create_split_dir.pl \
-        /export/a{11,12,13,14}/${USER}/espnet-data/egs/reverb/asr1/dump/${train_dev}/delta${do_delta}/storage \
-        ${feat_dt_dir}/storage
-    fi
-    dump.sh --cmd "$train_cmd" --nj 32 --do_delta ${do_delta} \
-        data-fbank/${train_set}/feats.scp data-fbank/${train_set}/cmvn.ark exp/dump_feats/train ${feat_tr_dir}
-    dump.sh --cmd "$train_cmd" --nj 4 --do_delta ${do_delta} \
-        data-fbank/${train_dev}/feats.scp data-fbank/${train_set}/cmvn.ark exp/dump_feats/dev ${feat_dt_dir}
-    for rtask in ${recog_set}; do
-        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}; mkdir -p ${feat_recog_dir}
-        dump.sh --cmd "$train_cmd" --nj 4 --do_delta ${do_delta} \
-            data-fbank/${rtask}/feats.scp data-fbank/${train_set}/cmvn.ark exp/dump_feats/recog/${rtask} \
-            ${feat_recog_dir}
-    done
+    utils/combine_data.sh data/${train_dev}_multich data/dt_real_2ch_multich data/dt_simu_2ch_multich
+    echo "combine reverb simulation and wsj clean training data"
+    utils/combine_data.sh data/${train_set}_multich data/train_si284 data/tr_simu_2ch
 fi
+
+train_set="${train_set}_multich"
+train_dev="${train_dev}_multich"
+# Rename recog_set: e.g. dt05_real_isolated_6ch_track -> dt05_real_isolated_6ch_track_multich
+recog_set="$(for setname in ${recog_set}; do echo -n "${setname}_multich "; done)"
 
 dict=data/lang_1char/${train_set}_units.txt
 echo "dictionary: ${dict}"
@@ -144,28 +130,37 @@ nlsyms=data/lang_1char/non_lang_syms.txt
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
     ### Task dependent. You have to check non-linguistic symbols used in the corpus.
     echo "stage 2: Dictionary and Json Data Preparation"
+    utils/combine_data.sh data/${train_set} data/train_si284 data/tr_simu_2ch
     mkdir -p data/lang_1char/
 
     echo "make a non-linguistic symbol list"
-    cut -f 2- data-fbank/${train_set}/text | tr " " "\n" | sort | uniq | grep "<" > ${nlsyms}
+    cut -f 2- data/${train_set}/text | tr " " "\n" | sort | uniq | grep "<" > ${nlsyms}
     cat ${nlsyms}
 
     echo "make a dictionary"
     echo "<unk> 1" > ${dict} # <unk> must be 1, 0 will be used for "blank" in CTC
-    text2token.py -s 1 -n 1 -l ${nlsyms} data-fbank/${train_set}/text | cut -f 2- -d" " | tr " " "\n" \
+    text2token.py -s 1 -n 1 -l ${nlsyms} data/${train_set}/text | cut -f 2- -d" " | tr " " "\n" \
     | sort | uniq | grep -v -e '^\s*$' | awk '{print $0 " " NR+1}' >> ${dict}
     wc -l ${dict}
 
     echo "make json files"
-    data2json.sh --feat ${feat_tr_dir}/feats.scp --nlsyms ${nlsyms} \
-         data-fbank/${train_set} ${dict} > ${feat_tr_dir}/data.json
-    data2json.sh --feat ${feat_dt_dir}/feats.scp --nlsyms ${nlsyms} \
-         data-fbank/${train_dev} ${dict} > ${feat_dt_dir}/data.json
-    for rtask in ${recog_set}; do
-        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}
-        data2json.sh --feat ${feat_recog_dir}/feats.scp \
-            --nlsyms ${nlsyms} data/${rtask} ${dict} > ${feat_recog_dir}/data.json
+    for setname in tr_simu_2ch_multich ${train_dev} ${recog_set}; do
+        data2json.sh --cmd "${train_cmd}" --nj 30 \
+        --category "multichannel" \
+        --preprocess-conf ${preprocess_config} --filetype sound.hdf5 \
+        --feat data/${setname}/feats.scp --nlsyms ${nlsyms} \
+        --out data/${setname}/data.json data/${setname} ${dict}
     done
+
+    setname=train_si284
+    data2json.sh --cmd "${train_cmd}" --nj 30 \
+    --category "singlechannel" \
+    --preprocess-conf ${preprocess_config} --filetype sound.hdf5 \
+    --feat data/${setname}/feats.scp --nlsyms ${nlsyms} \
+    --out data/${setname}/data.json data/${setname} ${dict}
+
+    mkdir -p data/${train_set}
+    concatjson.py data/tr_simu_2ch_multich/data.json data/train_si284/data.json > data/${train_set}/data.json
 fi
 
 # It takes a few days. If you just want to end-to-end ASR without LM,
@@ -207,8 +202,9 @@ if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
     fi
     # use only 1 gpu
     if [ ${ngpu} -gt 1 ]; then
-	echo "LM training does not support multi-gpu. single gpu will be used."
+        echo "LM training does not support multi-gpu. single gpu will be used."
     fi
+
     ${cuda_cmd} --gpu ${ngpu} ${lmexpdir}/train.log \
         lm_train.py \
         --config ${lm_config} \
@@ -225,9 +221,6 @@ fi
 
 if [ -z ${tag} ]; then
     expname=${train_set}_${backend}_$(basename ${train_config%.*})
-    if ${do_delta}; then
-        expname=${expname}_delta
-    fi
 else
     expname=${train_set}_${backend}_${tag}
 fi
@@ -235,7 +228,8 @@ expdir=exp/${expname}
 mkdir -p ${expdir}
 
 if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
-    echo "stage 4: Network Training"
+    echo "stage 4: Network Training: expdir=${expdir}"
+
     ${cuda_cmd} --gpu ${ngpu} ${expdir}/train.log \
         asr_train.py \
         --config ${train_config} \
@@ -249,8 +243,9 @@ if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
         --minibatches ${N} \
         --verbose ${verbose} \
         --resume ${resume} \
-        --train-json ${feat_tr_dir}/data.json \
-        --valid-json ${feat_dt_dir}/data.json
+        --train-json data/${train_set}/data.json \
+        --valid-json data/${train_dev}/data.json \
+        --preprocess-conf ${preprocess_config}
 fi
 
 if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
@@ -271,10 +266,9 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
         else
             recog_opts="--rnnlm ${lmexpdir}/rnnlm.model.best"
         fi
-        feat_recog_dir=${dumpdir}/${rtask}/delta${do_delta}
 
         # split data
-        splitjson.py --parts ${nj} ${feat_recog_dir}/data.json
+        splitjson.py --parts ${nj} data/${rtask}/data.json
 
         #### use CPU for decoding
         ngpu=0
@@ -285,7 +279,7 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
             --ngpu ${ngpu} \
             --backend ${backend} \
             --debugmode ${debugmode} \
-            --recog-json ${feat_recog_dir}/split${nj}utt/data.JOB.json \
+            --recog-json data/${rtask}/split${nj}utt/data.JOB.json \
             --result-label ${expdir}/${decode_dir}/data.JOB.json \
             --model ${expdir}/results/${recog_model}  \
             ${recog_opts}
@@ -298,7 +292,7 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
     i=0; for pid in "${pids[@]}"; do wait ${pid} || ((++i)); done
     [ ${i} -gt 0 ] && echo "$0: ${i} background jobs are failed." && false
 
-    echo "Report the result"
+    echo "Report the results"
     decode_part_dir=$(basename ${decode_config%.*})
     if [ ${use_wordlm} = true ]; then
 	decode_part_dir=${decode_part_dir}_wordrnnlm_${lmtag}
@@ -306,5 +300,44 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
 	decode_part_dir=${decode_part_dir}_rnnlm_${lmtag}
     fi
     local/get_results.sh ${nlsyms} ${dict} ${expdir} ${decode_part_dir}
-    echo "Finished"
+    echo "Decoding successfully finished"
+fi
+
+if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
+    echo "stage 6: Enhance speech"
+    nj=32
+    ngpu=0
+
+    pids=() # initialize pids
+    for rtask in ${recog_set}; do
+    (
+        enhdir=${expdir}/enhance_${rtask}
+        mkdir -p ${enhdir}/outdir
+        splitjson.py --parts ${nj} data/${rtask}/data.json
+
+        ${decode_cmd} JOB=1:${nj} ${enhdir}/log/enhance.JOB.log \
+            asr_enhance.py \
+                --ngpu ${ngpu} \
+                --backend ${backend} \
+                --debugmode ${debugmode} \
+                --model ${expdir}/results/${recog_model}  \
+                --recog-json data/${rtask}/split${nj}utt/data.JOB.json \
+                --enh-wspecifier ark,scp:${enhdir}/outdir/enhance.JOB,${enhdir}/outdir/enhance.JOB.scp \
+                --enh-filetype "sound" \
+                --image-dir ${enhdir}/images \
+                --num-images 20 \
+                --fs ${fs}
+    ) &
+    pids+=($!) # store background pids
+    done
+    i=0; for pid in "${pids[@]}"; do wait ${pid} || ((++i)); done
+    [ ${i} -gt 0 ] && echo "$0: ${i} background jobs are failed." && false
+
+    # Reduce all scp files from each jobs to one
+    for rtask in ${recog_set}; do
+        enhdir=${expdir}/enhance_${rtask}
+        for i in $(seq 1 ${nj}); do
+            cat ${enhdir}/outdir/enhance.${i}.scp
+        done > ${enhdir}/enhance.scp
+    done
 fi
