85ed286b5d71036072a311db25140855924a27cd
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-07-31 11:07:16 +0900

    updated test

--- test/test_e2e_tts_transformer.py
+++ test/test_e2e_tts_transformer.py
@@ -156,7 +156,11 @@ def test_transformer_trainable_and_decodable(model_dict):
     # decodable
     model.eval()
     with torch.no_grad():
-        model.inference(batch["xs"][0][:batch["ilens"][0]], Namespace(**inference_args))
+        if model_args["spk_embed_dim"] is None:
+            spemb = None
+        else:
+            spemb = batch["spembs"][0]
+        model.inference(batch["xs"][0][:batch["ilens"][0]], Namespace(**inference_args), spemb=spemb)
         model.calculate_all_attentions(**batch)
 
 
