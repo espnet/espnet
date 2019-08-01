6206d85436871175c673700fdeae5441410aa0f9
Nelson Yalta <nyalta21@gmail.com>
2019-08-01 17:25:08 +0900

    Revert "Merge remote-tracking branch 'upstream/master' into pr-chainer-transf-format"
    This reverts commit bb6f611c04d50274a2c3fb9668a4f200f7418ec1, reversing
    changes made to 5a49f19a6871cd8a5f74234ab14c7fa43d836682.

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ extras_require = {k: v for k, v in requirements.items()
 
 dirname = os.path.dirname(__file__)
 setup(name='espnet',
-      version='0.4.3',
+      version='0.4.2',
       url='http://github.com/espnet/espnet',
       author='Shinji Watanabe',
       author_email='shinjiw@ieee.org',
