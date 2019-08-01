e4afdc44b3fac85b1d3474187b4dc892787ea69e
karita <shigekikarita@gmail.com>
2019-08-01 21:13:49 +0900

    update sph2pipe

--- ci/test_integration.sh
+++ ci/test_integration.sh
@@ -9,6 +9,13 @@ if [ ! -e tools/kaldi ]; then
     git clone https://github.com/kaldi-asr/kaldi --depth 1 tools/kaldi
 fi
 
+(
+    cd ./tools/kaldi/tools || exit 1
+    echo "" > extras/check_dependencies.sh
+    chmod +x extras/check_dependencies.sh
+    make sph2pipe sclite
+)
+
 # download pre-built kaldi binary
 [ ! -e ubuntu16-featbin.tar.gz ] && wget https://18-198329952-gh.circle-artifacts.com/0/home/circleci/repo/ubuntu16-featbin.tar.gz
 tar -xf ./ubuntu16-featbin.tar.gz
