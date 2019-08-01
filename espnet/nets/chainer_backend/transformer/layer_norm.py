1b91be3f041f65292ea00b5f258f091f77a09095
Fhrozen <nyalta21@gmail.com>
2019-08-01 15:26:56 +0900

    format encoder and subs

--- espnet/nets/chainer_backend/transformer/layer_norm.py
+++ espnet/nets/chainer_backend/transformer/layer_norm.py
@@ -7,5 +7,5 @@ class LayerNorm(L.LayerNormalization):
     def __init__(self, dims, eps=1e-12):
         super(LayerNorm, self).__init__(size=dims, eps=eps)
 
-    def __call__(self, e):
-        return super(LayerNorm, self).__call__(e)
+    def forward(self, e):
+        return super(LayerNorm, self).forward(e)
--- espnet/nets/chainer_backend/transformer/subsampling.py
+++ espnet/nets/chainer_backend/transformer/subsampling.py
@@ -15,32 +15,29 @@ class Conv2dSubsampling(chainer.Chain):
     def __init__(self, channels, idim, dims, dropout=0.1,
                  initialW=None, initial_bias=None):
         super(Conv2dSubsampling, self).__init__()
-        n = 1 * 3 * 3
-        stvd = 1. / np.sqrt(n)
-        layer = L.Convolution2D(1, channels, 3, stride=2, pad=1,
-                                initialW=initialW(scale=stvd),
-                                initial_bias=initial_bias(scale=stvd))
-        self.add_link('conv.0', layer)
-        n = channels * 3 * 3
-        stvd = 1. / np.sqrt(n)
-        layer = L.Convolution2D(channels, channels, 3, stride=2, pad=1,
-                                initialW=initialW(scale=stvd),
-                                initial_bias=initial_bias(scale=stvd))
-        self.add_link('conv.2', layer)
-        stvd = 1. / np.sqrt(dims)
-        layer = L.Linear(idim, dims, initialW=initialW(scale=stvd),
-                         initial_bias=initial_bias(scale=stvd))
-        self.add_link('out.0', layer)
         self.dropout = dropout
         with self.init_scope():
+            n = 1 * 3 * 3
+            stvd = 1. / np.sqrt(n)
+            self.conv1 = L.Convolution2D(1, channels, 3, stride=2, pad=1,
+                                         initialW=initialW(scale=stvd),
+                                         initial_bias=initial_bias(scale=stvd))
+            n = channels * 3 * 3
+            stvd = 1. / np.sqrt(n)
+            self.conv2 = L.Convolution2D(channels, channels, 3, stride=2, pad=1,
+                                         initialW=initialW(scale=stvd),
+                                         initial_bias=initial_bias(scale=stvd))
+            stvd = 1. / np.sqrt(dims)
+            self.out = L.Linear(idim, dims, initialW=initialW(scale=stvd),
+                                initial_bias=initial_bias(scale=stvd))
             self.pe = PositionalEncoding(dims, dropout)
 
     def __call__(self, xs, ilens):
-        xs = F.expand_dims(xs, axis=1).data
-        xs = F.relu(self['conv.{}'.format(0)](xs))
-        xs = F.relu(self['conv.{}'.format(2)](xs))
+        xs = F.expand_dims(self.xp.array(xs), axis=1).data
+        xs = F.relu(self.conv1(xs))
+        xs = F.relu(self.conv2(xs))
         batch, _, length, _ = xs.shape
-        xs = self['out.0'](F.swapaxes(xs, 1, 2).reshape(batch * length, -1))
+        xs = self.out(F.swapaxes(xs, 1, 2).reshape(batch * length, -1))
         xs = self.pe(xs.reshape(batch, length, -1))
         # change ilens accordingly
         ilens = np.ceil(np.array(ilens, dtype=np.float32) / 2).astype(np.int)
