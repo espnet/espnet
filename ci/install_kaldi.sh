0b195fa74be3975834617be4f363a8a2e421bbe3
karita <shigekikarita@gmail.com>
2019-08-01 23:35:43 +0900

    split kaldi installation

---
+++ ci/install_kaldi.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# install kaldi
+git clone https://github.com/kaldi-asr/kaldi --depth 1 tools/kaldi
+(
+    cd ./tools/kaldi/tools || exit 1
+    echo "" > extras/check_dependencies.sh
+    chmod +x extras/check_dependencies.sh
+    make sph2pipe sclite
+)
+
+# download pre-built kaldi binary
+# TODO(karita) support non ubuntu env
+[ ! -e ubuntu16-featbin.tar.gz ] && wget https://18-198329952-gh.circle-artifacts.com/0/home/circleci/repo/ubuntu16-featbin.tar.gz
+tar -xf ./ubuntu16-featbin.tar.gz
+cp featbin/* tools/kaldi/src/featbin/
+
--- ci/test_integration.sh
+++ ci/test_integration.sh
@@ -1,25 +1,8 @@
 #!/usr/bin/env bash
 
-# to suppress errors during doc generation of utils/ when USE_CONDA=false in travis
-mkdir -p tools/venv/bin
-touch tools/venv/bin/activate
-. tools/venv/bin/activate
-
-if [ ! -e tools/kaldi ]; then
-    git clone https://github.com/kaldi-asr/kaldi --depth 1 tools/kaldi
-fi
-
 (
-    cd ./tools/kaldi/tools || exit 1
-    echo "" > extras/check_dependencies.sh
-    chmod +x extras/check_dependencies.sh
-    make sph2pipe sclite
+    cd ./egs/mini_an4/asr1 || exit 1
+    ./run.sh
 )
 
-# download pre-built kaldi binary
-[ ! -e ubuntu16-featbin.tar.gz ] && wget https://18-198329952-gh.circle-artifacts.com/0/home/circleci/repo/ubuntu16-featbin.tar.gz
-tar -xf ./ubuntu16-featbin.tar.gz
-cp featbin/* tools/kaldi/src/featbin/
-
-cd ./egs/mini_an4/asr1 || exit 1
-./run.sh
+# TODO(karita): test asr_mix, tts, mt, st?
