6451c091320a43cb96be989e0aff1ce6dcf71c0c
wangxiaofei <xiaofei.wang1987@gmail.com>
2019-08-06 15:00:38 -0400

    Reflect review comments

--- egs/reverb/asr1_multich/run.sh
+++ egs/reverb/asr1_multich/run.sh
@@ -3,8 +3,8 @@
 # Copyright 2018 Johns Hopkins University (Shinji Watanabe)
 #  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
 
-. ./path.sh
-. ./cmd.sh
+. ./path.sh || exit 1;
+. ./cmd.sh || exit 1;
 
 # general configuration
 backend=pytorch
@@ -53,9 +53,6 @@ wavdir=${PWD}/wav # place to store WAV files
 tag="" # tag for managing experiments.
 
 . utils/parse_options.sh || exit 1;
-
-. ./path.sh
-. ./cmd.sh
 
 # Set bash to 'debug' mode, it will exit on :
 # -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
