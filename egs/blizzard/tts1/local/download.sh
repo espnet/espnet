35d0d90042ba0118110b5f9c2a0e9887a39c1d78
potato-inoue <k_inoue@a.cs.okayama-u.ac.jp>
2019-08-06 16:51:49 -0400

    Create templete for new recipe

---
+++ egs/blizzard/tts1/local/download.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Copyright 2018 Nagoya University (Tomoki Hayashi)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+db=$1
+
+cwd=`pwd`
+if [ ! -e ${db}/LJSpeech-1.1 ]; then
+    mkdir -p ${db}
+    cd ${db}
+    wget http://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2
+    tar -vxf ./*.tar.bz2
+    rm ./*.tar.bz2
+    cd $cwd
+fi
