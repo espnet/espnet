718c46f3f04234858678f02d0116cf6818128c7a
wangxiaofei <xiaofei.wang1987@gmail.com>
2019-08-07 15:37:08 -0400

    Support se scores of evaluation set

--- egs/reverb/asr1_multich/local/download_se_eval_tool.sh
+++ egs/reverb/asr1_multich/local/download_se_eval_tool.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Copyright 2018 Johns Hopkins University (Author: Aswin Shanmugam Subramanian)
+# Copyright 2019 Johns Hopkins University (Author: Xiaofei Wang)
 # This script downloads the official REVERB challenge SE scripts and SRMR toolbox
 # This script also downloads and compiles PESQ
 # please make sure that you or your institution have the license to report PESQ
@@ -17,6 +18,12 @@ wget 'https://reverb2014.dereverberation.com/tools/REVERB-SPEENHA.Release04Oct.z
 unzip REVERB_scores.zip -d local/REVERB_scores_source
 rm REVERB_scores.zip
 
+# Get the evaluation list
+wget 'https://reverb2014.dereverberation.org/tools/taskFiles_et.tgz' -O taskFiles_et.tgz
+tar -zxvf taskFiles_et.tgz -C local/REVERB_scores_source/REVERB-SPEENHA.Release04Oct/
+cp -a local/REVERB_scores_source/REVERB-SPEENHA.Release04Oct/taskFiles_et/* local/REVERB_scores_source/REVERB-SPEENHA.Release04Oct/taskfiles
+rm taskFiles_et.tgz
+
 pushd local/REVERB_scores_source/REVERB-SPEENHA.Release04Oct/evaltools
 sed -i 's/wavread/audioread/g' prog/score_sim.m
 git clone https://github.com/MuSAELab/SRMRToolbox.git
@@ -30,6 +37,9 @@ patch score_RealData.m -i ../../../score_RealData.patch -o score_RealData_new.m
 mv score_RealData_new.m score_RealData.m
 patch score_SimData.m -i ../../../score_SimData.patch -o score_SimData_new.m
 mv score_SimData_new.m score_SimData.m
+sed -i 's/dt/et/g' score_SimData.m
+sed -i 's/dt/et/g' score_RealData.m
+sed -i 's/Dev/Eval/g' score_RealData.m
 popd
 
 cp -a local/evaltools local/REVERB_scores_source/REVERB-SPEENHA.Release04Oct
