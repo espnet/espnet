51ba41fb581f46f7a158b0a6fe7ea29da86ceec0
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-08-09 12:43:52 +0900

    added tts integration test

--- ci/test_integration.sh
+++ ci/test_integration.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+# test asr recipe
 (
     cd ./egs/mini_an4/asr1 || exit 1
     ./run.sh
 )
+# test tts recipe
+(
+    cd ../tts1 || exit 1
+    ./run.sh
+)
 
-# TODO(karita): test asr_mix, tts, mt, st?
+# TODO(karita): test asr_mix, mt, st?
