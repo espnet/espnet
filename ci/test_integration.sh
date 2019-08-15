fd63c5a0af2a55dc548593bdfff531d7ae91fb9f
Fhrozen <nyalta21@gmail.com>
2019-08-16 08:46:10 +0900

    set fixed LD_LIB path in test_integration

--- ci/test_integration.sh
+++ ci/test_integration.sh
@@ -2,6 +2,13 @@
 
 set -euo pipefail
 
+if [ ! -z "${LD_LIBRARY_PATH}" ]; then
+    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(pwd)/chainer_ctc/ext/warp-ctc/build
+else
+    export LD_LIBRARY_PATH=$(pwd)/chainer_ctc/ext/warp-ctc/build
+fi
+
+
 # test asr recipe
 (
     cd ./egs/mini_an4/asr1 || exit 1
