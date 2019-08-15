de35b67607facc54849cc5e83a2a40b7ce3df273
Fhrozen <nyalta21@gmail.com>
2019-08-16 08:09:39 +0900

    switch directory call

--- egs/mini_an4/asr1/path.sh
+++ egs/mini_an4/asr1/path.sh
@@ -7,7 +7,7 @@ export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sctk/bin
 . $KALDI_ROOT/tools/config/common_path.sh
 export LC_ALL=C
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$MAIN_ROOT/tools/chainer_ctc/ext/warp-ctc/build
+export LD_LIBRARY_PATH=$MAIN_ROOT/tools/chainer_ctc/ext/warp-ctc/build:${LD_LIBRARY_PATH}
 if [ -e $MAIN_ROOT/tools/venv/etc/profile.d/conda.sh ]; then
     source $MAIN_ROOT/tools/venv/etc/profile.d/conda.sh && conda deactivate && conda activate
 else
