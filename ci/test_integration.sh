555a9535c8b5977593e59fa106f5e2367501ac38
Fhrozen <nyalta21@gmail.com>
2019-08-16 20:29:04 +0900

    resolving comments

--- ci/test_integration.sh
+++ ci/test_integration.sh
@@ -2,14 +2,6 @@
 
 set -euo pipefail
 
-if [ -z ${LD_LIBRARY_PATH+x} ]; then
-    LD_LIBRARY_PATH=$(pwd)/chainer_ctc/ext/warp-ctc/build
-else
-    LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(pwd)/chainer_ctc/ext/warp-ctc/build
-fi
-
-export LD_LIBRARY_PATH
-
 # test asr recipe
 (
     cd ./egs/mini_an4/asr1 || exit 1
