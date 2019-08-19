e432f875dd28b7ee1e1563af8b94ed1dde7c33f3
karita <shigekikarita@gmail.com>
2019-08-19 22:37:55 +0900

    add tests for dtype and device

--- test/test_positional_encoding.py
+++ test/test_positional_encoding.py
@@ -1,48 +1,61 @@
+import pytest
 import torch
 
 from espnet.nets.pytorch_backend.transformer.embedding import PositionalEncoding
 from espnet.nets.pytorch_backend.transformer.embedding import ScaledPositionalEncoding
 
 
-def test_pe_extendable():
+@pytest.mark.parametrize(
+    "dtype, device", [(dt, dv) for dt in ("float32", "float64") for dv in ("cpu", "cuda")])
+def test_pe_extendable(dtype, device):
+    if device == "cuda" and not torch.cuda.is_available():
+        pytest.skip("no cuda device is available")
+    dtype = getattr(torch, dtype)
     dim = 2
-    pe = PositionalEncoding(dim, 0.0, 3)
+    pe = PositionalEncoding(dim, 0.0, 3).to(dtype=dtype, device=device)
+    x = torch.rand(2, 3, dim, dtype=dtype, device=device)
+    y = pe(x)
     init_cache = pe.pe
 
     # test not extended from init
-    x = torch.rand(2, 3, dim)
+    x = torch.rand(2, 3, dim, dtype=dtype, device=device)
     y = pe(x)
     assert pe.pe is init_cache
 
-    x = torch.rand(2, 5, dim)
+    x = torch.rand(2, 5, dim, dtype=dtype, device=device)
     y = pe(x)
-    assert x.shape == y.shape
 
     sd = pe.state_dict()
     assert len(sd) == 0, "PositionalEncoding should save nothing"
-    pe2 = PositionalEncoding(dim, 0.0, 3)
+    pe2 = PositionalEncoding(dim, 0.0, 3).to(dtype=dtype, device=device)
     pe2.load_state_dict(sd)
     y2 = pe2(x)
-    assert torch.all(y == y2)
+    assert torch.allclose(y, y2)
 
 
-def test_scaled_pe_extendable():
+@pytest.mark.parametrize(
+    "dtype, device", [(dt, dv) for dt in ("float32", "float64") for dv in ("cpu", "cuda")])
+def test_scaled_pe_extendable(dtype, device):
+    if device == "cuda" and not torch.cuda.is_available():
+        pytest.skip("no cuda device is available")
+    dtype = getattr(torch, dtype)
     dim = 2
-    pe = ScaledPositionalEncoding(dim, 0.0, 3)
+    pe = ScaledPositionalEncoding(dim, 0.0, 3).to(dtype=dtype, device=device)
+    x = torch.rand(2, 3, dim, dtype=dtype, device=device)
+    y = pe(x)
     init_cache = pe.pe
 
     # test not extended from init
-    x = torch.rand(2, 3, dim)
+    x = torch.rand(2, 3, dim, dtype=dtype, device=device)
     y = pe(x)
     assert pe.pe is init_cache
 
-    x = torch.rand(2, 5, dim)
+    x = torch.rand(2, 5, dim, dtype=dtype, device=device)
     y = pe(x)
-    assert x.shape == y.shape
 
     sd = pe.state_dict()
     assert sd == {"alpha": pe.alpha}, "ScaledPositionalEncoding should save only alpha"
-    pe2 = ScaledPositionalEncoding(dim, 0.0, 3)
+    pe2 = ScaledPositionalEncoding(dim, 0.0, 3).to(dtype=dtype, device=device)
     pe2.load_state_dict(sd)
     y2 = pe2(x)
-    assert torch.all(y == y2)
+    assert torch.allclose(y, y2)
