8bae8464e059893bbef28effbfcfd234d6ee3ab2
Fhrozen <nyalta21@gmail.com>
2019-08-20 12:25:55 +0900

    removing linear_tensor

--- espnet/nets/chainer_backend/transformer/ctc.py
+++ espnet/nets/chainer_backend/transformer/ctc.py
@@ -7,8 +7,6 @@ import chainer.functions as F
 import chainer.links as L
 import numpy as np
 
-from espnet.nets.chainer_backend.nets_utils import linear_tensor
-
 
 # TODO(nelson): Merge chainer_backend/transformer/ctc.py in chainer_backend/ctc.py
 class CTC(chainer.Chain):
@@ -46,8 +44,8 @@ class CTC(chainer.Chain):
         olens = [x.shape[0] for x in ys]
 
         # zero padding for hs
-        y_hat = linear_tensor(self.ctc_lo, F.dropout(
-            F.pad_sequence(hs), ratio=self.dropout_rate))
+        y_hat = self.ctc_lo(F.dropout(
+            F.pad_sequence(hs), ratio=self.dropout_rate), n_batch_axes=2)
         y_hat = F.separate(y_hat, axis=1)  # ilen list of batch x hdim
 
         # zero padding for ys
@@ -76,7 +74,7 @@ class CTC(chainer.Chain):
             chainer.Variable: A n-dimension float array.
 
         """
-        y_hat = linear_tensor(self.ctc_lo, F.pad_sequence(hs))
+        y_hat = self.ctc_lo(F.pad_sequence(hs), n_batch_axes=2)
         return F.log_softmax(y_hat.reshape(-1, y_hat.shape[-1])).reshape(y_hat.shape)
 
 
@@ -145,7 +143,7 @@ class WarpCTC(chainer.Chain):
             chainer.Variable: A n-dimension float array.
 
         """
-        y_hat = linear_tensor(self.ctc_lo, F.pad_sequence(hs))
+        y_hat = self.ctc_lo(F.pad_sequence(hs), n_batch_axes=2)
         return F.log_softmax(y_hat.reshape(-1, y_hat.shape[-1])).reshape(y_hat.shape)
 
     def argmax(self, hs_pad):
