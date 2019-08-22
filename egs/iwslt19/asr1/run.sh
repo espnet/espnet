ed3644ad204c3b6203aa3a89be1d6429c90de0e8
Shinji Watanabe <sw005320@gmail.com>
2019-08-22 17:48:40 -0400

    fix for shell check

--- egs/iwslt19/asr1/run.sh
+++ egs/iwslt19/asr1/run.sh
@@ -124,7 +124,7 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     # Generate the fbank features; by 40-dimensional fbanks with pitch on each frame
     # to unify the fbank feature setup with how2
     # This is different from the other ESPnet recipe (80-dimensional fbanks)
-    for x in tr_must_c tr_tedlium2 dt_must_c dt_tedlium2 `echo ${recog_set} | tr ' ' '\n' | grep -v how2`; do
+    for x in tr_must_c tr_tedlium2 dt_must_c dt_tedlium2 $(echo ${recog_set} | tr ' ' '\n' | grep -v how2); do
 	steps/make_fbank_pitch.sh --cmd "$train_cmd" --nj 32 --write_utt2num_frames true \
 				  data/${x} exp/make_fbank/${x} ${fbankdir}
     done
