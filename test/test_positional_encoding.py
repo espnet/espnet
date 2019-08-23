d779dd3489ed9a534e6cf0824f0852ddb6c5214e
karita <shigekikarita@gmail.com>
2019-08-23 14:55:06 +0900

    add pre-hook in PositionalEncoding for backward compatibility

--- test/test_positional_encoding.py
+++ test/test_positional_encoding.py
@@ -59,3 +59,75 @@ def test_scaled_pe_extendable(dtype, device):
     pe2.load_state_dict(sd)
     y2 = pe2(x)
     assert torch.allclose(y, y2)
+
+
+class LegacyPositionalEncoding(torch.nn.Module):
+    """Positional encoding module until v.0.5.2."""
+
+    def __init__(self, d_model, dropout_rate, max_len=5000):
+        import math
+        super().__init__()
+        self.dropout = torch.nn.Dropout(p=dropout_rate)
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float32).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2, dtype=torch.float32) *
+                             -(math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.max_len = max_len
+        self.xscale = math.sqrt(d_model)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x * self.xscale + self.pe[:, :x.size(1)]
+        return self.dropout(x)
+
+
+class LegacyScaledPositionalEncoding(PositionalEncoding):
+    """Positional encoding module until v.0.5.2."""
+
+    def __init__(self, d_model, dropout_rate, max_len=5000):
+        super().__init__(d_model=d_model, dropout_rate=dropout_rate, max_len=max_len)
+        self.alpha = torch.nn.Parameter(torch.tensor(1.0))
+
+    def forward(self, x):
+        self.extend_pe(x)
+        x = x + self.alpha * self.pe[:, :x.size(1)]
+        return self.dropout(x)
+
+
+def test_compatibility():
+    """Regression test for #1121"""
+    x = torch.rand(2, 3, 4)
+
+    legacy_net = torch.nn.Sequential(
+        LegacyPositionalEncoding(4, 0.0),
+        torch.nn.Linear(4, 2)
+    )
+
+    latest_net = torch.nn.Sequential(
+        PositionalEncoding(4, 0.0),
+        torch.nn.Linear(4, 2)
+    )
+
+    latest_net.load_state_dict(legacy_net.state_dict())
+    legacy = legacy_net(x)
+    latest = latest_net(x)
+    assert torch.allclose(legacy, latest)
+
+    legacy_net = torch.nn.Sequential(
+        LegacyScaledPositionalEncoding(4, 0.0),
+        torch.nn.Linear(4, 2)
+    )
+
+    latest_net = torch.nn.Sequential(
+        ScaledPositionalEncoding(4, 0.0),
+        torch.nn.Linear(4, 2)
+    )
+
+    latest_net.load_state_dict(legacy_net.state_dict())
+    legacy = legacy_net(x)
+    latest = latest_net(x)
+    assert torch.allclose(legacy, latest)
