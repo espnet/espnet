18bd13886f3bb67e8b7711e08358350056eed748
hirofumi0810 <hiro.mhbc@gmail.com>
2019-08-24 20:15:08 -0700

    Fix long line

--- test/utils_test.py
+++ test/utils_test.py
@@ -19,7 +19,8 @@ def make_dummy_json(n_utts=10, ilen_range=(100, 300), olen_range=(10, 300), idim
     return dummy_json
 
 
-def make_dummy_json_st(n_utts=10, ilen_range=(100, 300), olen_range=(10, 300), olen_asr_range=(10, 300), idim=83, odim=52):
+def make_dummy_json_st(n_utts=10, ilen_range=(100, 300), olen_range=(10, 300),
+                       olen_asr_range=(10, 300), idim=83, odim=52):
     ilens = np.random.randint(ilen_range[0], ilen_range[1], n_utts)
     olens = np.random.randint(olen_range[0], olen_range[1], n_utts)
     olens_asr = np.random.randint(olen_asr_range[0], olen_asr_range[1], n_utts)
