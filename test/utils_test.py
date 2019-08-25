28a4d2e9959f4b59129e59134fcb7d7af07225d1
hirofumi0810 <hiro.mhbc@gmail.com>
2019-08-24 18:49:10 -0700

    Add test

--- test/utils_test.py
+++ test/utils_test.py
@@ -19,6 +19,30 @@ def make_dummy_json(n_utts=10, ilen_range=(100, 300), olen_range=(10, 300), idim
     return dummy_json
 
 
+def make_dummy_json_st(n_utts=10, ilen_range=(100, 300), olen_range=(10, 300), olen_asr_range=(10, 300), idim=83, odim=52):
+    ilens = np.random.randint(ilen_range[0], ilen_range[1], n_utts)
+    olens = np.random.randint(olen_range[0], olen_range[1], n_utts)
+    olens_asr = np.random.randint(olen_asr_range[0], olen_asr_range[1], n_utts)
+    dummy_json = {}
+    for idx in range(n_utts):
+        input = [{
+            "shape": [ilens[idx], idim]
+        }]
+        output = [
+            {
+                "shape": [olens[idx], odim]
+            },
+            {
+                "shape": [olens_asr[idx], odim]
+            }
+        ]
+        dummy_json["utt_%d" % idx] = {
+            "input": input,
+            "output": output
+        }
+    return dummy_json
+
+
 def make_dummy_json_mt(n_utts=10, ilen_range=(100, 300), olen_range=(10, 300), idim=83, odim=52):
     ilens = np.random.randint(ilen_range[0], ilen_range[1], n_utts)
     olens = np.random.randint(olen_range[0], olen_range[1], n_utts)
