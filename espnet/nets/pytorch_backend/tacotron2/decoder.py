63bc3b9948e02550ef711154ba113b831d908047
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-08-26 17:19:00 +0900

    cleaned up docstrings

--- espnet/nets/pytorch_backend/tacotron2/decoder.py
+++ espnet/nets/pytorch_backend/tacotron2/decoder.py
@@ -4,6 +4,8 @@
 # Copyright 2019 Nagoya University (Tomoki Hayashi)
 #  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
 
+"""Tacotron2 decoder related modules."""
+
 import six
 
 import torch
@@ -24,10 +26,6 @@ class ZoneOutCell(torch.nn.Module):
     This is a module of zoneout described in `Zoneout: Regularizing RNNs by Randomly Preserving Hidden Activations`_.
     This code is modified from `eladhoffer/seq2seq.pytorch`_.
 
-    Args:
-        cell (torch.nn.Module): Pytorch recurrent cell module e.g. `torch.nn.Module.LSTMCell`.
-        zoneout_rate (float, optional): Probability of zoneout from 0.0 to 1.0.
-
     Examples:
         >>> lstm = torch.nn.LSTMCell(16, 32)
         >>> lstm = ZoneOutCell(lstm, 0.5)
@@ -41,6 +39,13 @@ class ZoneOutCell(torch.nn.Module):
     """
 
     def __init__(self, cell, zoneout_rate=0.1):
+        """Initialize zone out cell module.
+
+        Args:
+            cell (torch.nn.Module): Pytorch recurrent cell module e.g. `torch.nn.Module.LSTMCell`.
+            zoneout_rate (float, optional): Probability of zoneout from 0.0 to 1.0.
+
+        """
         super(ZoneOutCell, self).__init__()
         self.cell = cell
         self.hidden_size = cell.hidden_size
@@ -89,12 +94,6 @@ class Prenet(torch.nn.Module):
     Synthesis by Conditioning WaveNet on Mel Spectrogram Predictions`_. The Prenet preforms nonlinear conversion
     of inputs before input to auto-regressive lstm, which helps to learn diagonal attentions.
 
-    Args:
-        idim (int): Dimension of the inputs.
-        odim (int): Dimension of the outputs.
-        n_layers (int, optional): The number of prenet layers.
-        n_units (int, optional): The number of prenet units.
-
     Note:
         This module alway applies dropout even in evaluation See the detail in _`Natural TTS Synthesis by
         Conditioning WaveNet on Mel Spectrogram Predictions`_.
@@ -105,6 +104,15 @@ class Prenet(torch.nn.Module):
     """
 
     def __init__(self, idim, n_layers=2, n_units=256, dropout_rate=0.5):
+        """Initialize prenet module.
+
+        Args:
+            idim (int): Dimension of the inputs.
+            odim (int): Dimension of the outputs.
+            n_layers (int, optional): The number of prenet layers.
+            n_units (int, optional): The number of prenet units.
+
+        """
         super(Prenet, self).__init__()
         self.dropout_rate = dropout_rate
         self.prenet = torch.nn.ModuleList()
@@ -136,22 +144,24 @@ class Postnet(torch.nn.Module):
     Conditioning WaveNet on Mel Spectrogram Predictions`_. The Postnet predicts refines the predicted
     Mel-filterbank of the decoder, which helps to compensate the detail sturcture of spectrogram.
 
-    Args:
-        idim (int): Dimension of the inputs.
-        odim (int): Dimension of the outputs.
-        n_layers (int, optional): The number of layers.
-        n_filts (int, optional): The number of filter size.
-        n_units (int, optional): The number of filter channels.
-        use_batch_norm (bool, optional): Whether to use batch normalization..
-        dropout_rate (float, optional): Dropout rate..
-
-
     .. _`Natural TTS Synthesis by Conditioning WaveNet on Mel Spectrogram Predictions`:
        https://arxiv.org/abs/1712.05884
 
     """
 
     def __init__(self, idim, odim, n_layers=5, n_chans=512, n_filts=5, dropout_rate=0.5, use_batch_norm=True):
+        """Initialize postnet module.
+
+        Args:
+            idim (int): Dimension of the inputs.
+            odim (int): Dimension of the outputs.
+            n_layers (int, optional): The number of layers.
+            n_filts (int, optional): The number of filter size.
+            n_units (int, optional): The number of filter channels.
+            use_batch_norm (bool, optional): Whether to use batch normalization..
+            dropout_rate (float, optional): Dropout rate..
+
+        """
         super(Postnet, self).__init__()
         self.postnet = torch.nn.ModuleList()
         for layer in six.moves.range(n_layers - 1):
@@ -205,25 +215,6 @@ class Decoder(torch.nn.Module):
     Synthesis by Conditioning WaveNet on Mel Spectrogram Predictions`_. The decoder generates the sequence of
     features from the sequence of the hidden states.
 
-    Args:
-        idim (int): Dimension of the inputs.
-        odim (int): Dimension of the outputs.
-        att (torch.nn.Module): Instance of attention class.
-        dlayers (int, optional): The number of decoder lstm layers.
-        dunits (int, optional): The number of decoder lstm units.
-        prenet_layers (int, optional): The number of prenet layers.
-        prenet_units (int, optional): The number of prenet units.
-        postnet_layers (int, optional): The number of postnet layers.
-        postnet_filts (int, optional): The number of postnet filter size.
-        postnet_chans (int, optional): The number of postnet filter channels.
-        output_activation_fn (torch.nn.Module, optional): Activation function for outputs.
-        cumulate_att_w (bool, optional): Whether to cumulate previous attention weight.
-        use_batch_norm (bool, optional): Whether to use batch normalization.
-        use_concate (bool, optional): Whether to concatenate encoder embedding with decoder lstm outputs.
-        dropout_rate (float, optional): Dropout rate.
-        zoneout_rate (float, optional): Zoneout rate.
-        reduction_factor (int, optional): Reduction factor.
-
     .. _`Natural TTS Synthesis by Conditioning WaveNet on Mel Spectrogram Predictions`:
        https://arxiv.org/abs/1712.05884
 
@@ -244,6 +235,28 @@ class Decoder(torch.nn.Module):
                  dropout_rate=0.5,
                  zoneout_rate=0.1,
                  reduction_factor=1):
+        """Initialize Tacotron2 decoder module.
+
+        Args:
+            idim (int): Dimension of the inputs.
+            odim (int): Dimension of the outputs.
+            att (torch.nn.Module): Instance of attention class.
+            dlayers (int, optional): The number of decoder lstm layers.
+            dunits (int, optional): The number of decoder lstm units.
+            prenet_layers (int, optional): The number of prenet layers.
+            prenet_units (int, optional): The number of prenet units.
+            postnet_layers (int, optional): The number of postnet layers.
+            postnet_filts (int, optional): The number of postnet filter size.
+            postnet_chans (int, optional): The number of postnet filter channels.
+            output_activation_fn (torch.nn.Module, optional): Activation function for outputs.
+            cumulate_att_w (bool, optional): Whether to cumulate previous attention weight.
+            use_batch_norm (bool, optional): Whether to use batch normalization.
+            use_concate (bool, optional): Whether to concatenate encoder embedding with decoder lstm outputs.
+            dropout_rate (float, optional): Dropout rate.
+            zoneout_rate (float, optional): Zoneout rate.
+            reduction_factor (int, optional): Reduction factor.
+
+        """
         super(Decoder, self).__init__()
 
         # store the hyperparameters
