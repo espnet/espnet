6284c68bb630cd63d582ac8ffde4eb7da702a0f9
hirofumi0810 <hiro.mhbc@gmail.com>
2019-08-25 19:45:43 -0700

    Remove recog_v2

--- espnet/bin/st_trans.py
+++ espnet/bin/st_trans.py
@@ -157,14 +157,10 @@ def main(args):
     logging.info('backend = ' + args.backend)
     if args.backend == "pytorch":
         # Experimental API that supports custom LMs
-        if args.api == "v2":
-            from espnet.asr.pytorch_backend.recog import recog_v2
-            recog_v2(args)
-        else:
-            from espnet.st.pytorch_backend.st import trans
-            if args.dtype != "float32":
-                raise NotImplementedError(f"`--dtype {args.dtype}` is only available with `--api v2`")
-            trans(args)
+        from espnet.st.pytorch_backend.st import trans
+        if args.dtype != "float32":
+            raise NotImplementedError(f"`--dtype {args.dtype}` is only available with `--api v2`")
+        trans(args)
     else:
         raise ValueError("Only pytorch are supported.")
 
