93282bf4b534309d0b31ce5dc337ac4cf1b55005
hirofumi0810 <hiro.mhbc@gmail.com>
2019-08-27 03:15:55 -0700

    Update again

--- espnet/nets/pytorch_backend/transformer/attention.py
+++ espnet/nets/pytorch_backend/transformer/attention.py
@@ -23,8 +23,8 @@ class MultiHeadedAttention(nn.Module):
     """
 
     def __init__(self, n_head, n_feat, dropout_rate):
-        super(MultiHeadedAttention, self).__init__()
         """Construct an MultiHeadedAttention object."""
+        super(MultiHeadedAttention, self).__init__()
         assert n_feat % n_head == 0
         # We assume d_v always equals d_k
         self.d_k = n_feat // n_head
