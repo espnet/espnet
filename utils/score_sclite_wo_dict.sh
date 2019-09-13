2a8eaf9139ac459cb6e0a5725f8b5452d3fa924e
potato-inoue <k_inoue@a.cs.okayama-u.ac.jp>
2019-09-14 04:57:09 +0900

    add tmp CER eval

--- utils/score_sclite_wo_dict.sh
+++ utils/score_sclite_wo_dict.sh
@@ -21,17 +21,24 @@ dir=$1
 concatjson.py ${dir}/data.*.json > ${dir}/data.json
 
 if [ $num_spkrs -eq 1 ]; then
-    json2trn_wo_dict.py ${dir}/data.json --num-spkrs ${num_spkrs} --refs ${dir}/ref.trn --hyps ${dir}/hyp_org.trn
+    json2trn_wo_dict.py ${dir}/data.json --num-spkrs ${num_spkrs} --refs ${dir}/ref.wrd.trn --hyps ${dir}/hyp_org.wrd.trn
    
-    cat ${dir}/hyp_org.trn | sed -e 's/▁//' | sed -e 's/▁/ /g' > ${dir}/hyp.trn
-    cat ${dir}/ref.trn | sed -e 's/\.//g' -e 's/\,//g' > ${dir}/ref_wo_punc.trn
+    cat ${dir}/hyp_org.wrd.trn | sed -e 's/▁//' | sed -e 's/▁/ /g' > ${dir}/hyp.wrd.trn
+    cat ${dir}/ref.wrd.trn | sed -e 's/\.//g' -e 's/\,//g' > ${dir}/ref_wo_punc.wrd.trn
 
+    cat ${dir}/hyp.wrd.trn | awk -v FS='' '{a=0;for(i=1;i<=NF;i++){if($i=="("){a=1};if(a==0){printf("%s ",$i)}else{printf("%s",$i)}}printf("\n")}' > ${dir}/hyp.trn
+    cat ${dir}/ref_wo_punc.wrd.trn | awk -v FS='' '{a=0;for(i=1;i<=NF;i++){if($i=="("){a=1};if(a==0){printf("%s ",$i)}else{printf("%s",$i)}}printf("\n")}' > ${dir}/ref_wo_punc.trn
+
+    sclite -r ${dir}/ref_wo_punc.trn trn -h ${dir}/hyp.trn -i rm -o all stdout > ${dir}/result_wo_punc.txt
+    echo "write a WER result in ${dir}/result_wo_punc.txt"
+    grep -e Avg -e SPKR -m 2 ${dir}/result_wo_punc.txt
+    
     if ${wer}; then
-        sclite -r ${dir}/ref_wo_punc.trn trn -h ${dir}/hyp.trn -i rm -o all stdout > ${dir}/result_wo_punc.wrd.txt
+        sclite -r ${dir}/ref_wo_punc.wrd.trn trn -h ${dir}/hyp.wrd.trn -i rm -o all stdout > ${dir}/result_wo_punc.wrd.txt
         echo "write a WER result in ${dir}/result_wo_punc.wrd.txt"
         grep -e Avg -e SPKR -m 2 ${dir}/result_wo_punc.wrd.txt
         
-        sclite -r ${dir}/ref.trn trn -h ${dir}/hyp.trn trn -i rm -o all stdout > ${dir}/result.wrd.txt
+        sclite -r ${dir}/ref.wrd.trn trn -h ${dir}/hyp.wrd.trn trn -i rm -o all stdout > ${dir}/result.wrd.txt
         echo "write a WER result in ${dir}/result.wrd.txt"
         grep -e Avg -e SPKR -m 2 ${dir}/result.wrd.txt
 
