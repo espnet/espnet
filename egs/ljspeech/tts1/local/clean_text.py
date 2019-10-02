a86a23487a2526b4bed572444cfe4658d8db0ec3
Ryuichi Yamamoto <zryuichi@gmail.com>
2019-10-03 00:10:03 +0900

    use https://github.com/Kyubyong/g2p for g2p

--- egs/ljspeech/tts1/local/clean_text.py
+++ egs/ljspeech/tts1/local/clean_text.py
@@ -10,46 +10,14 @@ import re
 
 from text.cleaners import english_cleaners
 
-_arpabet = nltk.corpus.cmudict.dict()
-
-possible_symbols = '!\'(),-.:;?'
-
-
-def _maybe_get_arpabet(word):
-    assert " " not in word
-    if len(word) > 1:
-        # check that symbols are correctly tokenized in advance
-        # e.g. if the word is 'smith.', this should raise
-        if word[-1] in possible_symbols:
-            raise RuntimeError("Oops! symbols should be correctly tokenized in advance")
-    try:
-        phonemes = _arpabet[word][0]
-        phonemes = " ".join(phonemes)
-    except KeyError:
-        # return chars for failure cases
-        return " ".join(list(word))
-
-    return phonemes
-
-
-def clean_manually(text):
-    text = re.sub(r"'([\w ]+)',", r"\1,", text)
-    # 'Edinburgh Review,'
-    text = re.sub(r"'([\w ]+),'", r"\1,", text)
-    return text
+# https://github.com/Kyubyong/g2p
+from g2p_en import G2p
+f_g2p = G2p()
 
 
 def g2p(text):
-    words = nltk.word_tokenize(text)
-
-    # Manual spacial handling...
-    # remove last symbol if nltk fails to tokenize (e.g., Smith. -> Smith)
-    words = map(lambda s: s[:-1] if len(s) > 1 and s[-1] in possible_symbols else s, words)
-    # etc. -> etc
-    words = map(lambda s: s.replace("etc.", "etc"), words)
-
-    text = ' '.join(_maybe_get_arpabet(word) for word in words)
-    return text
+    tokens = filter(lambda s: s != " ", f_g2p(text))
+    return ' '.join(tokens)
 
 
 if __name__ == "__main__":
@@ -65,7 +33,6 @@ if __name__ == "__main__":
             clean_content = english_cleaners(content.rstrip())
             if args.trans_type == "phn":
                 text = clean_content.lower()
-                text = clean_manually(text)
                 clean_content = g2p(text)
 
             print("%s %s" % (id, clean_content))
