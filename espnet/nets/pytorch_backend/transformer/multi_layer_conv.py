2039c040c5bd71f7d2d5b86e09dc3228763ea6ee
Ryuichi Yamamoto <zryuichi@gmail.com>
2019-10-03 10:26:04 +0900

    Conv -> Conv to Conv -> FC
    also make dropout consistent with PositionwiseFeedForward

--- espnet/nets/pytorch_backend/transformer/multi_layer_conv.py
+++ espnet/nets/pytorch_backend/transformer/multi_layer_conv.py
@@ -28,8 +28,7 @@ class MultiLayeredConv1d(torch.nn.Module):
         super(MultiLayeredConv1d, self).__init__()
         self.w_1 = torch.nn.Conv1d(in_chans, hidden_chans, kernel_size,
                                    stride=1, padding=(kernel_size - 1) // 2)
-        self.w_2 = torch.nn.Conv1d(hidden_chans, in_chans, kernel_size,
-                                   stride=1, padding=(kernel_size - 1) // 2)
+        self.w_2 = nn.Linear(hidden_chans, in_chans)
         self.dropout = torch.nn.Dropout(dropout_rate)
 
     def forward(self, x):
@@ -43,5 +42,4 @@ class MultiLayeredConv1d(torch.nn.Module):
 
         """
         x = torch.relu(self.w_1(x.transpose(-1, 1))).transpose(-1, 1)
-        x = torch.relu(self.w_2(x.transpose(-1, 1))).transpose(-1, 1)
-        return self.dropout(x)
+        return self.w_2(self.dropout(x))
