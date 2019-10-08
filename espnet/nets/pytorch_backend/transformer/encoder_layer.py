fix normalization

espnet/nets/pytorch_backend/transformer/encoder_layer.py
@@ -51,6 +51,7 @@ class EncoderLayer(nn.Module):
         :param torch.Tensor cache: cache for x (batch, max_time_in - 1, size)
         :rtype: Tuple[torch.Tensor, torch.Tensor]
         """
+        residual = x
         if self.normalize_before:
             x = self.norm1(x)
 
@@ -59,9 +60,9 @@ class EncoderLayer(nn.Module):
         else:
             assert cache.shape == (x.shape[0], x.shape[1] - 1, self.size)
             x_q = x[:, -1:, :]
+            residual = residual[:, -1:, :]
             mask = None if mask is None else mask[:, -1:, :]
 
-        residual = x_q
         if self.concat_after:
             x_concat = torch.cat((x, self.self_attn(x_q, x, x, mask)), dim=-1)
             x = residual + self.concat_linear(x_concat)