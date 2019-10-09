af7e40370ebc0643ae214af226f2c0a9e3024c96
Fhrozen <nyalta21@gmail.com>
2019-10-09 21:35:17 +0900

    fix wget

--- egs/jesc/mt1/local/download_data.sh
+++ egs/jesc/mt1/local/download_data.sh
@@ -9,7 +9,7 @@ if [ ! -e split ]; then
     echo "$0: downloading JSEC data (it won't re-download if it was already downloaded.)"
     # the following command won't re-get it if it's already there
     # because of the --continue switch.
-    wget --continue https://nlp.stanford.edu/projects/jesc/data/split.tar.gz || exit 1
+    wget --continue --no-check-certificate https://nlp.stanford.edu/projects/jesc/data/split.tar.gz || exit 1
     tar xf "split.tar.gz"
 else
     echo "$0: not downloading or un-tarring split.tar.gz because it already exists."
