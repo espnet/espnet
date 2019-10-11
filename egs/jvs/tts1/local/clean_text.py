86ee4246a278ecab067e72bb9e1cd9ad186c9d2a
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-10-11 12:13:32 +0900

    added clean_text.py

---
+++ egs/jvs/tts1/local/clean_text.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Copyright 2018 Nagoya University (Takenori Yoshimura), Ryuichi Yamamoto
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+import argparse
+import codecs
+
+import pyopenjtalk
+import jaconv
+
+
+def g2p(text, trans_type="char"):
+    text = jaconv.normalize(text)
+    if trans_type == "char":
+        text = pyopenjtalk.g2p(text, kana=True)
+    elif trans_type == "phn":
+        text = pyopenjtalk.g2p(text, kana=False)
+    else:
+        assert False
+    return text
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('in_text', type=str, help='text to be cleaned')
+    parser.add_argument('out_text', type=str, help='text to be cleaned')
+    parser.add_argument("trans_type", type=str, default="kana",
+                        choices=["char", "phn"],
+                        help="Input transcription type")
+    args = parser.parse_args()
+    with codecs.open(args.in_text, 'r', 'utf-8') as f_in, codecs.open(args.out_text, 'w', 'utf-8') as f_out:
+        for line in f_in.readlines():
+            id, content = line.split(":")
+            clean_content = g2p(content, args.trans_type)
+            f_out.write("%s %s\n" % (id, clean_content))
