dd91b77f1d0c3c7d4aa380eeb42aaf859c838406
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-10-11 11:50:09 +0900

    added openjtalk in LD_LIBRARY_PATH

--- egs/jvs/tts1/path.sh
+++ egs/jvs/tts1/path.sh
@@ -12,6 +12,7 @@ else
     source $MAIN_ROOT/tools/venv/bin/activate
 fi
 export PATH=$MAIN_ROOT/utils:$MAIN_ROOT/espnet/bin:$PATH
+export LD_LIBRARY_PATH=$MAIN_ROOT/tools/open_jtalk/src/build:$LD_LIBRARY_PATH
 
 export OMP_NUM_THREADS=1
 
