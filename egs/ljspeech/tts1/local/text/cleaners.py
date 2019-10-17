48690331e82bf9a201f2a1bbefa54cd5ef6db5ad
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-10-17 23:58:14 +0900

    updated

--- egs/ljspeech/tts1/local/text/cleaners.py
+++ egs/ljspeech/tts1/local/text/cleaners.py
@@ -1,21 +1,19 @@
-""" from https://github.com/keithito/tacotron """
+"""This file is derived from https://github.com/keithito/tacotron.
 
-'''
 Cleaners are transformations that run over the input text at both training and eval time.
 
 Cleaners can be selected by passing a comma-delimited list of cleaner names as the "cleaners"
 hyperparameter. Some cleaners are English-specific. You'll typically want to use:
 1. "english_cleaners" for English text
 2. "transliteration_cleaners" for non-English text that can be transliterated to ASCII using
-the Unidecode library (https://pypi.python.org/pypi/Unidecode)
+   the Unidecode library (https://pypi.python.org/pypi/Unidecode)
 3. "basic_cleaners" if you do not want to transliterate (in this case, you should also update
-the symbols in symbols.py to match your data).
-'''
+   the symbols in symbols.py to match your data).
+"""
 
-import sys
 import re
 
-from .numbers import normalize_numbers
+from numbers import normalize_numbers
 from unidecode import unidecode
 
 
