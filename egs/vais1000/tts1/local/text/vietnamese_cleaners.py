27f81e046e0ed39490061c0b3757c1903b84b3ef
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-10-24 14:52:00 +0900

    fixed flake8

--- egs/vais1000/tts1/local/text/vietnamese_cleaners.py
+++ egs/vais1000/tts1/local/text/vietnamese_cleaners.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 
-""" Created on 10:19 AM, 10/17/19
-    @author: ngunhuconchocon
-    @brief: Пролетарии всех стран, соединяйтесь! да здравствует наша советская родина
-            Vietnamese cleaner. This is a naive implementationm which only seperate punctuation and handle some abbreviation.
-            You should see `regex_tokenize.py` for more details. This must be updated later for a "cleaner" cleaner
+"""Vietnamese cleaner.
+
+Created on 10:19 AM, 10/17/19
+@author: ngunhuconchocon
+@brief: Пролетарии всех стран, соединяйтесь! да здравствует наша советская родина
+        Vietnamese cleaner. This is a naive implementationm which only seperate punctuation
+        and handle some abbreviation. You should see `regex_tokenize.py` for more details.
+        This must be updated later for a "cleaner" cleaner
 """
 
-from vietnameseNormUniStd import UniStd
 from regex_tokenize import tokenize
-import re
+from vietnameseNormUniStd import UniStd
+
 
 def vietnamese_cleaner(text):
-    """
-        Handle the Vietnamese oldstyle of putting tones (òa or oà, úy or uý, ...). 
-        This action can directly benefit the result if you train the model with letter. 
-        In case of phoneme training, this cleaner will facilitate the dictionary (syllable->phonemes) preparation process.
+    """Perform Vietnamese cleaning.
 
-        Many thanks to Thang Tat Vu and Thanh-Le Ha
+    Handle the Vietnamese oldstyle of putting tones (òa or oà, úy or uý, ...).
+    This action can directly benefit the result if you train the model with letter.
+    In case of phoneme training, this cleaner will facilitate the dictionary
+    (syllable->phonemes) preparation process.
+
+    Many thanks to Thang Tat Vu and Thanh-Le Ha.
+
     """
     text = UniStd(text)
     return tokenize(text, format='text')
