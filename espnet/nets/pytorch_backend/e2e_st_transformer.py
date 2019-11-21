5a97d8acd4998011a40a87ca527e951008db0c35
hirofumi0810 <hiro.mhbc@gmail.com>
2019-11-21 18:06:46 +0900

    Support MT multitask

--- espnet/nets/pytorch_backend/e2e_st_transformer.py
+++ espnet/nets/pytorch_backend/e2e_st_transformer.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Shigeki Karita
+# Copyright 2019 Hirofumi Inaguma
 #  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
 
 """Transformer speech recognition model (pytorch)."""
@@ -82,7 +82,7 @@ class E2E(STInterface, torch.nn.Module):
         """Return PlotAttentionReport."""
         return PlotAttentionReport
 
-    def __init__(self, idim, odim, args, ignore_id=-1, asr_model=None, mt_model=None):
+    def __init__(self, idim, odim, args, ignore_id=-1):
         """Construct an E2E object.
 
         :param int idim: dimension of inputs
@@ -114,6 +114,7 @@ class E2E(STInterface, torch.nn.Module):
             self_attention_dropout_rate=args.transformer_attn_dropout_rate,
             src_attention_dropout_rate=args.transformer_attn_dropout_rate
         )
+        self.pad = 0
         self.sos = odim - 1
         self.eos = odim - 1
         self.odim = odim
@@ -125,23 +126,9 @@ class E2E(STInterface, torch.nn.Module):
         self.criterion = LabelSmoothingLoss(self.odim, self.ignore_id, args.lsm_weight,
                                             args.transformer_length_normalized_loss)
         # self.verbose = args.verbose
-        self.reset_parameters(args)
         self.adim = args.adim
+        # submodule for ASR task
         self.mtlalpha = args.mtlalpha
-        if args.mtlalpha > 0.0:
-            self.ctc = CTC(odim, args.adim, args.dropout_rate, ctc_type=args.ctc_type, reduce=True)
-        else:
-            self.ctc = None
-
-        if args.report_cer or args.report_wer:
-            from espnet.nets.e2e_asr_common import ErrorCalculator
-            self.error_calculator = ErrorCalculator(args.char_list,
-                                                    args.sym_space, args.sym_blank,
-                                                    args.report_cer, args.report_wer)
-        else:
-            self.error_calculator = None
-        self.rnnlm = None
-
         self.asr_weight = getattr(args, "asr_weight", 0.0)
         if self.asr_weight > 0 and args.mtlalpha < 1:
             self.decoder_asr = Decoder(
@@ -155,6 +142,35 @@ class E2E(STInterface, torch.nn.Module):
                 self_attention_dropout_rate=args.transformer_attn_dropout_rate,
                 src_attention_dropout_rate=args.transformer_attn_dropout_rate,
             )
+        # submodule for MT task
+        self.mt_weight = getattr(args, "mt_weight", 0.0)
+        if self.mt_weight > 0:
+            self.encoder_mt = Encoder(
+                idim=odim,
+                attention_dim=args.adim,
+                attention_heads=args.aheads,
+                linear_units=args.dunits,
+                num_blocks=args.dlayers,
+                input_layer='embed',
+                dropout_rate=args.dropout_rate,
+                positional_dropout_rate=args.dropout_rate,
+                attention_dropout_rate=args.transformer_attn_dropout_rate,
+                padding_idx=0
+            )
+        self.reset_parameters(args)  # place after the submodule initialization
+        if args.mtlalpha > 0.0:
+            self.ctc = CTC(odim, args.adim, args.dropout_rate, ctc_type=args.ctc_type, reduce=True)
+        else:
+            self.ctc = None
+
+        if self.asr_weight > 0 and (args.report_cer or args.report_wer):
+            from espnet.nets.e2e_asr_common import ErrorCalculator
+            self.error_calculator = ErrorCalculator(args.char_list,
+                                                    args.sym_space, args.sym_blank,
+                                                    args.report_cer, args.report_wer)
+        else:
+            self.error_calculator = None
+        self.rnnlm = None
 
         # multilingual E2E-ST related
         self.multilingual = getattr(args, "multilingual", False)
@@ -166,14 +182,17 @@ class E2E(STInterface, torch.nn.Module):
         """Initialize parameters."""
         # initialize parameters
         initialize(self, args.transformer_init)
+        if self.mt_weight > 0:
+            torch.nn.init.normal_(self.encoder_mt.embed[0].weight, mean=0, std=args.adim ** -0.5)
+            torch.nn.init.constant_(self.encoder_mt.embed[0].weight[self.pad], 0)
 
-    def forward(self, xs_pad, ilens, ys_pad, ys_pad_asr):
+    def forward(self, xs_pad, ilens, ys_pad, ys_pad_src):
         """E2E forward.
 
         :param torch.Tensor xs_pad: batch of padded source sequences (B, Tmax, idim)
         :param torch.Tensor ilens: batch of lengths of source sequences (B)
         :param torch.Tensor ys_pad: batch of padded target sequences (B, Lmax)
-        :param torch.Tensor ys_pad_asr: batch of padded target sequences (B, Lmax)
+        :param torch.Tensor ys_pad_src: batch of padded target sequences (B, Lmax)
         :return: ctc loass value
         :rtype: torch.Tensor
         :return: attention loss value
@@ -182,7 +201,7 @@ class E2E(STInterface, torch.nn.Module):
         :rtype: float
         """
         # 0. Extract target language ID
-        src_lang_ids = None
+        # src_lang_ids = None
         tgt_lang_ids = None
         if self.multilingual:
             tgt_lang_ids = ys_pad[:, 0:1]
@@ -199,23 +218,34 @@ class E2E(STInterface, torch.nn.Module):
         # replace <sos> with target language ID
         if self.replace_sos:
             ys_in_pad = torch.cat([tgt_lang_ids, ys_in_pad[:, 1:]], dim=1)
-        if (self.asr_weight > 0 and self.mtlalpha < 1) or src_lang_ids is not None:
-            ys_in_pad_asr, ys_out_pad_asr = add_sos_eos(ys_pad_asr, self.sos, self.eos, self.ignore_id)
         ys_mask = target_mask(ys_in_pad, self.ignore_id)
         pred_pad, pred_mask = self.decoder(ys_in_pad, ys_mask, hs_pad, hs_mask)
         self.pred_pad = pred_pad
-        pred_pad_asr = None
-        if self.asr_weight > 0 and self.mtlalpha < 1.0:
-            # Multi-task w/ ASR
-            ys_mask_asr = target_mask(ys_in_pad_asr, self.ignore_id)
-            pred_pad_asr, _ = self.decoder_asr(ys_in_pad_asr, ys_mask_asr, hs_pad, hs_mask)
+        pred_pad_asr, pred_pad_mt = None, None
 
         # 3. compute attention loss
-        loss_asr = 0.0
+        loss_asr, loss_mt = 0.0, 0.0
         loss_att = self.criterion(pred_pad, ys_out_pad)
         # Multi-task w/ ASR
         if self.asr_weight > 0 and self.mtlalpha < 1.0:
+            # forward ASR decoder
+            ys_in_pad_asr, ys_out_pad_asr = add_sos_eos(ys_pad_src, self.sos, self.eos, self.ignore_id)
+            ys_mask_asr = target_mask(ys_in_pad_asr, self.ignore_id)
+            pred_pad_asr, _ = self.decoder_asr(ys_in_pad_asr, ys_mask_asr, hs_pad, hs_mask)
+            # compute loss
             loss_asr = self.criterion(pred_pad_asr, ys_out_pad_asr)
+        # Multi-task w/ MT
+        if self.mt_weight > 0:
+            # forward MT encoder
+            ilens_mt = torch.sum(ys_pad_src != 0, dim=1).cpu().numpy()
+            ys_pad_src = ys_pad_src[:, :max(ilens_mt)]  # for data parallel
+            src_mask_mt = (~make_pad_mask(ilens_mt.tolist())).to(ys_pad_src.device).unsqueeze(-2)
+            # ys_pad_src, ys_pad = self.target_forcing(ys_pad_src, ys_pad)
+            hs_pad_mt, hs_mask_mt = self.encoder_mt(ys_pad_src, src_mask_mt)
+            # forward MT decoder
+            pred_pad_mt, _ = self.decoder(ys_in_pad, ys_mask, hs_pad_mt, hs_mask_mt)
+            # compute loss
+            loss_mt = self.criterion(pred_pad_mt, ys_out_pad)
 
         self.acc = th_accuracy(pred_pad.view(-1, self.odim), ys_out_pad,
                                ignore_label=self.ignore_id)
@@ -224,6 +254,11 @@ class E2E(STInterface, torch.nn.Module):
                                        ignore_label=self.ignore_id)
         else:
             self.acc_asr = 0.0
+        if pred_pad_mt is not None:
+            self.acc_mt = th_accuracy(pred_pad_mt.view(-1, self.odim), ys_out_pad,
+                                      ignore_label=self.ignore_id)
+        else:
+            self.acc_mt = 0.0
 
         # TODO(karita) show predicted text
         # TODO(karita) calculate these stats
@@ -233,14 +268,14 @@ class E2E(STInterface, torch.nn.Module):
         else:
             batch_size = xs_pad.size(0)
             hs_len = hs_mask.view(batch_size, -1).sum(1)
-            loss_ctc = self.ctc(hs_pad.view(batch_size, -1, self.adim), hs_len, ys_pad_asr)
+            loss_ctc = self.ctc(hs_pad.view(batch_size, -1, self.adim), hs_len, ys_pad_src)
             if self.error_calculator is not None:
                 ys_hat = self.ctc.argmax(hs_pad.view(batch_size, -1, self.adim)).data
-                cer_ctc = self.error_calculator(ys_hat.cpu(), ys_pad_asr.cpu(), is_ctc=True)
+                cer_ctc = self.error_calculator(ys_hat.cpu(), ys_pad_src.cpu(), is_ctc=True)
 
         # 5. compute cer/wer
         cer, wer = None, None  # TODO(hirofumi0810): fix later
-        # if self.training or self.error_calculator is None:
+        # if self.training or (self.asr_weight == 0 or self.mtlalpha == 1 or not (self.report_cer or self.report_wer)):
         #     cer, wer = None, None
         # else:
         #     ys_hat = pred_pad.argmax(dim=-1)
@@ -248,28 +283,42 @@ class E2E(STInterface, torch.nn.Module):
 
         # copyied from e2e_asr
         alpha = self.mtlalpha
-        if self.asr_weight == 0:  # E2E-ST
+        if self.asr_weight == 0 and self.mt_weight == 0:  # E2E-ST
             self.loss = loss_att
-            loss_att_data = float(loss_att)
+            loss_st_data = float(loss_att)
             loss_asr_data = None
-        elif alpha == 0:  # E2E-ST + att-ASR
-            self.loss = (1 - self.asr_weight) * loss_att + self.asr_weight * loss_asr
-            loss_att_data = float(loss_att)
-            loss_asr_data = float(loss_asr)
-        elif alpha == 1:  # E2E-ST + CTC-ASR
-            self.loss = (1 - self.asr_weight) * loss_att + self.asr_weight * loss_ctc
-            loss_att_data = float(loss_att)
-            loss_asr_data = float(loss_ctc)
-        else:  # E2E-ST + att-ASR + CTC-ASR
-            self.loss = (1 - self.asr_weight) * loss_att + self.asr_weight * \
-                (alpha * loss_ctc + (1 - alpha) * loss_asr)
-            loss_att_data = float(loss_att)
-            loss_asr_data = float(alpha * loss_ctc + (1 - alpha) * loss_asr)
+            loss_mt_data = None
+        else:
+            if self.asr_weight == 0:  # E2E-ST + MT
+                self.loss = (1 - self.mt_weight) * loss_att + self.mt_weight * loss_mt
+                loss_st_data = float(loss_att)
+                loss_asr_data = None
+                loss_mt_data = float(loss_mt)
+            else:
+                if alpha == 0:  # E2E-ST + att-ASR
+                    self.loss = (1 - self.asr_weight - self.mt_weight) * loss_att + self.asr_weight * \
+                        loss_asr + self.mt_weight * loss_mt
+                    loss_st_data = float(loss_att)
+                    loss_asr_data = float(loss_asr)
+                    loss_mt_data = None if self.mt_weight == 0 else float(loss_mt)
+                elif alpha == 1:  # E2E-ST + CTC-ASR
+                    self.loss = (1 - self.asr_weight - self.mt_weight) * loss_att + self.asr_weight * \
+                        loss_ctc + self.mt_weight * loss_mt
+                    loss_st_data = float(loss_att)
+                    loss_asr_data = float(loss_ctc)
+                    loss_mt_data = None if self.mt_weight == 0 else float(loss_mt)
+                else:  # E2E-ST + att-ASR + CTC-ASR
+                    self.loss = (1 - self.asr_weight - self.mt_weight) * loss_att + self.asr_weight * \
+                        (alpha * loss_ctc + (1 - alpha) * loss_asr) + self.mt_weight * loss_mt
+                    loss_st_data = float(loss_att)
+                    loss_asr_data = float(alpha * loss_ctc + (1 - alpha) * loss_asr)
+                    loss_mt_data = None if self.mt_weight == 0 else float(loss_mt)
 
         loss_data = float(self.loss)
         if loss_data < CTC_LOSS_THRESHOLD and not math.isnan(loss_data):
-            self.reporter.report(loss_asr_data, loss_att_data, self.acc_asr, self.acc,
-                                 cer_ctc, cer, wer, 0.0,  # bleu
+            self.reporter.report(loss_asr_data, loss_mt_data, loss_st_data,
+                                 self.acc_asr, self.acc_mt, self.acc,
+                                 cer_ctc, cer, wer, 0.0,  # TODO(hirofumi0810): bleu
                                  loss_data)
         else:
             logging.warning('loss (=%f) is not correct', loss_data)
@@ -445,20 +494,20 @@ class E2E(STInterface, torch.nn.Module):
         logging.info('normalized log probability: ' + str(nbest_hyps[0]['score'] / len(nbest_hyps[0]['yseq'])))
         return nbest_hyps
 
-    def calculate_all_attentions(self, xs_pad, ilens, ys_pad, ys_pad_asr):
+    def calculate_all_attentions(self, xs_pad, ilens, ys_pad, ys_pad_src):
         """E2E attention calculation.
 
         :param torch.Tensor xs_pad: batch of padded input sequences (B, Tmax, idim)
         :param torch.Tensor ilens: batch of lengths of input sequences (B)
         :param torch.Tensor ys_pad: batch of padded token id sequence tensor (B, Lmax)
-        :param torch.Tensor ys_pad_asr: batch of padded token id sequence tensor (B, Lmax)
+        :param torch.Tensor ys_pad_src: batch of padded token id sequence tensor (B, Lmax)
         :return: attention weights with the following shape,
             1) multi-head case => attention weights (B, H, Lmax, Tmax),
             2) other case => attention weights (B, Lmax, Tmax).
         :rtype: float ndarray
         """
         with torch.no_grad():
-            self.forward(xs_pad, ilens, ys_pad, ys_pad_asr)
+            self.forward(xs_pad, ilens, ys_pad, ys_pad_src)
         ret = dict()
         for name, m in self.named_modules():
             if isinstance(m, MultiHeadedAttention) and m.attn is not None:  # skip MHA for submodules
