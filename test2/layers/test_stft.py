e1185730cc27141ab8155313b2eefbecee53b0a0
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2019-12-03 20:18:46 +0900

    Add test codes

--- test2/layers/test_stft.py
+++ test2/layers/test_stft.py
@@ -9,31 +9,32 @@ def test_repr():
 
 
 def test_forward():
-    layer = Stft()
-    x = torch.randn(2, 1000)
+    layer = Stft(win_length=4, hop_length=2,
+                 n_fft=4)
+    x = torch.randn(2, 30)
     y, _ = layer(x)
-    assert y.shape == (2, 257, 8, 2)
-    y, ylen = layer(x, torch.tensor([1000, 980], dtype=torch.long))
-    assert (ylen == torch.tensor((8, 8), dtype=torch.long)).all()
+    assert y.shape == (2, 3, 16, 2)
+    y, ylen = layer(x, torch.tensor([30, 15], dtype=torch.long))
+    assert (ylen == torch.tensor((16, 8), dtype=torch.long)).all()
 
 
 def test_backward_leaf_in():
     layer = Stft()
-    x = torch.randn(2, 1000, requires_grad=True)
+    x = torch.randn(2, 400, requires_grad=True)
     y, _ = layer(x)
     y.sum().backward()
 
 
 def test_backward_not_leaf_in():
     layer = Stft()
-    x = torch.randn(2, 1000, requires_grad=True)
+    x = torch.randn(2, 400, requires_grad=True)
     x = x + 2
     y, _ = layer(x)
     y.sum().backward()
 
 
-def test_invsere():
+def test_inverse():
     layer = Stft()
-    x = torch.randn(2, 1000, requires_grad=True)
+    x = torch.randn(2, 400, requires_grad=True)
     with pytest.raises(NotImplementedError):
         y, _ = layer.inverse(x)
--- test2/utils/test_nested_dict_action.py
+++ test2/utils/test_nested_dict_action.py
@@ -9,18 +9,21 @@ from espnet2.utils.nested_dict_action import NestedDictAction
 def test_NestedDictAction():
     parser = argparse.ArgumentParser()
     parser.add_argument('--conf', action=NestedDictAction,
-                        default={'a': 4})
+                        default=3)
 
     assert parser.parse_args(['--conf', 'a=3', '--conf', 'c=4']) == \
         Namespace(conf={'a': 3, 'c': 4})
     assert parser.parse_args(['--conf', 'c.d=4']) == \
-        Namespace(conf={'a': 4, 'c': {'d': 4}})
+        Namespace(conf={'c': {'d': 4}})
     assert parser.parse_args(['--conf', 'c.d=4', '--conf', 'c=2']) == \
-        Namespace(conf={'a': 4, 'c': 2})
+        Namespace(conf={'c': 2})
     assert parser.parse_args(['--conf', '{d: 5, e: 9}']) == \
         Namespace(conf={'d': 5, 'e': 9})
     assert parser.parse_args(['--conf', '{"d": 5, "e": 9}']) == \
         Namespace(conf={'d': 5, 'e': 9})
+    assert parser.parse_args(['--conf', '{"d": 5, "e": 9}',
+                              '--conf', 'd.e=3']) == \
+        Namespace(conf={'d': {'e': 3}, 'e': 9})
 
 
 def test_NestedDictAction_exception():
@@ -39,10 +42,3 @@ def test_NestedDictAction_exception():
     with pytest.raises(SystemExit):
         parser.parse_args(['--conf', '[cd, e, aaa]'])
 
-
-def test_NestedDictAction_not_dict_default():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--conf', action=NestedDictAction)
-    with pytest.raises(TypeError):
-        parser.add_argument('--conf', action=NestedDictAction,
-                            default=3)
