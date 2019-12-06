5372b43063ce9c3e65361e38dd708a7147c4c889
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2019-12-06 11:23:29 +0900

    modified:   ci/test_flake8.sh

--- ci/test_flake8.sh
+++ ci/test_flake8.sh
@@ -75,7 +75,7 @@ cov=$(echo "scale = 4; 100 * ${n_ok} / ${n_all}" | bc)
 echo "flake8-docstrings ready files coverage: ${n_ok} / ${n_all} = ${cov}%"
 
 # --extend-ignore for wip files for flake8-docstrings
-flake8 --extend-ignore=D test utils doc ${flake8_black_list}
+flake8 --extend-ignore=D expnet2 test utils doc ${flake8_black_list}
 
 # white list of files that should support flake8-docstrings
-flake8 espnet espnet2 --exclude=${flake8_black_list//$'\n'/,}
+flake8 espnet --exclude=${flake8_black_list//$'\n'/,}
