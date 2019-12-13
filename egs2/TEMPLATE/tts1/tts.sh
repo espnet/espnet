62c3736bb4697a8af381a3131454c503c34bb39b
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-12-13 18:44:42 +0900

    update

--- egs2/TEMPLATE/tts1/tts.sh
+++ egs2/TEMPLATE/tts1/tts.sh
@@ -5,7 +5,9 @@
 
 # Set bash to 'debug' mode, it will exit on :
 # -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
-set -euo pipefail
+set -e
+set -u
+set -o pipefail
 
 log() {
     local fname=${BASH_SOURCE[1]##*/}
