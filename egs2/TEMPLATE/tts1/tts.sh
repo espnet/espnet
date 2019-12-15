96bd7bf7202003caad0127118a2c67feba20c938
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-12-16 00:36:23 +0900

    added SECONDS

--- egs2/TEMPLATE/tts1/tts.sh
+++ egs2/TEMPLATE/tts1/tts.sh
@@ -13,6 +13,7 @@ log() {
     local fname=${BASH_SOURCE[1]##*/}
     echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
 }
+SECONDS=0
 
 # General configuration
 stage=1          # Processes starts from the specified stage.
