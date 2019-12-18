d48765c6e0e426cd41b6221caf23872d5b0579f2
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2019-12-18 20:32:45 +0900

    fixed SC2103

--- ci/test_integration.sh
+++ ci/test_integration.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # test asr recipe
+cwd=$(pwd)
 cd ./egs/mini_an4/asr1 || exit 1
 . path.sh  # source here to avoid undefined variable errors
 
@@ -14,18 +15,18 @@ echo "==== ASR (backend=chainer) ==="
 ./run.sh --stage 3 --backend chainer
 echo "==== ASR (backend=pytorch num-encs 2) ==="
 ./run.sh --stage 2 --train-config ./conf/train_mulenc2.yaml --decode-config ./conf/decode_mulenc2.yaml --mulenc true
-cd -
+cd ${cwd} || exit 1
 
 # test asr_mix recipe
 cd ./egs/mini_an4/asr_mix1 || exit 1
 echo "==== ASR Mix (backend=pytorch) ==="
 ./run.sh
-cd -
+cd ${cwd} || exit 1
 
 # test tts recipe
 cd ./egs/mini_an4/tts1 || exit 1
 echo "==== TTS (backend=pytorch) ==="
 ./run.sh
-cd -
+cd ${cwd} || exit 1
 
 # TODO(karita): test mt, st?
