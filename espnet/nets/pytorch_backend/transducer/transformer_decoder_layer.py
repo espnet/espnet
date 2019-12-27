a353e6b97cc8733a2e27c74a0487715c698cb724
fboyer <florian.boyer@labri.fr>
2019-12-27 17:51:13 +0100

    fix lm decoding and docstrings

--- espnet/nets/pytorch_backend/transducer/transformer_decoder_layer.py
+++ espnet/nets/pytorch_backend/transducer/transformer_decoder_layer.py
@@ -1,4 +1,4 @@
-"""Decoder self-attention layer definition for transducer."""
+"""Decoder layer definition for transformer-transducer models."""
 
 import torch
 from torch import nn
@@ -7,13 +7,13 @@ from espnet.nets.pytorch_backend.transformer.layer_norm import LayerNorm
 
 
 class DecoderLayer(nn.Module):
-    """Single self-attention decoder layer module.
+    """Single decoder layer module for transformer-transducer models.
 
     Args:
         size (int): input dim
         self_attn (MultiHeadedAttention): self attention module
         feed_forward (PositionwiseFeedForward): feed forward layer module
-        dropout (float): dropout rate
+        dropout_rate (float): dropout rate
         normalize_before (bool): whether to use layer_norm before the first block
         concat_after (bool): whether to concat attention layer's input and output
 
