ede5e4407b821b9fdbccc9a67383dee0002e4ad4
hirofumi0810 <hiro.mhbc@gmail.com>
2019-12-27 10:14:59 +0900

    Fix plot

--- espnet/nets/pytorch_backend/transformer/plot.py
+++ espnet/nets/pytorch_backend/transformer/plot.py
@@ -49,50 +49,52 @@ def savefig(plot, filename):
     plt.clf()
 
 
+def plot_multi_head_attention(data, attn_dict, outdir, suffix="png", savefn=savefig,
+                              ikey="input", iaxis=0, okey="output", oaxis=0):
+    """Plot multi head attentions.
+
+    :param dict data: utts info from json file
+    :param dict[str, torch.Tensor] attn_dict: multi head attention dict.
+        values should be torch.Tensor (head, input_length, output_length)
+    :param str outdir: dir to save fig
+    :param str suffix: filename suffix including image type (e.g., png)
+    :param savefn: function to save
+
+    """
+    for name, att_ws in attn_dict.items():
+        for idx, att_w in enumerate(att_ws):
+            filename = "%s/%s.%s.%s" % (outdir, data[idx][0], name, suffix)
+            dec_len = int(data[idx][1][okey][oaxis]['shape'][0])
+            enc_len = int(data[idx][1][ikey][iaxis]['shape'][0])
+            xtokens, ytokens = None, None
+            if "encoder" in name:
+                att_w = att_w[:, :enc_len, :enc_len]
+                # for MT
+                if 'token' in data[idx][1][ikey][iaxis].keys():
+                    xtokens = data[idx][1][ikey][iaxis]['token'].split()
+                    ytokens = xtokens[:]
+            elif "decoder" in name:
+                if "self" in name:
+                    att_w = att_w[:, :dec_len + 1, :dec_len + 1]  # +1 for <sos>
+                else:
+                    att_w = att_w[:, :dec_len + 1, :enc_len]  # +1 for <sos>
+                    # for MT
+                    if 'token' in data[idx][1][ikey][iaxis].keys():
+                        xtokens = data[idx][1][ikey][iaxis]['token'].split()
+                # for ASR/ST/MT
+                if 'token' in data[idx][1][okey][oaxis].keys():
+                    ytokens = ['<sos>'] + data[idx][1][okey][oaxis]['token'].split()
+                    if "self" in name:
+                        xtokens = ytokens[:]
+            else:
+                logging.warning("unknown name for shaping attention")
+            fig = _plot_and_save_attention(att_w, filename, xtokens, ytokens)
+            savefn(fig, filename)
+
+
 class PlotAttentionReport(asr_utils.PlotAttentionReport):
     def plotfn(self, *args, **kwargs):
-        self.plot_multi_head_attention(*args, **kwargs)
-
-    def plot_multi_head_attention(self, data, attn_dict, outdir, suffix="png", savefn=savefig):
-        """Plot multi head attentions
-
-        :param dict data: utts info from json file
-        :param dict[str, torch.Tensor] attn_dict: multi head attention dict.
-            values should be torch.Tensor (head, input_length, output_length)
-        :param str outdir: dir to save fig
-        :param str suffix: filename suffix including image type (e.g., png)
-        :param savefn: function to save
-
-        """
-        for name, att_ws in attn_dict.items():
-            for idx, att_w in enumerate(att_ws):
-                filename = "%s/%s.%s.%s" % (outdir, data[idx][0], name, suffix)
-                dec_len = int(data[idx][1][self.okey][self.oaxis]['shape'][0])
-                enc_len = int(data[idx][1][self.ikey][self.iaxis]['shape'][0])
-                xtokens, ytokens = None, None
-                if "encoder" in name:
-                    att_w = att_w[:, :enc_len, :enc_len]
-                    # for MT
-                    if 'token' in data[idx][1][self.ikey][self.iaxis].keys():
-                        xtokens = data[idx][1][self.ikey][self.iaxis]['token'].split()
-                        ytokens = xtokens[:]
-                elif "decoder" in name:
-                    if "self" in name:
-                        att_w = att_w[:, :dec_len + 1, :dec_len + 1]  # +1 for <sos>
-                    else:
-                        att_w = att_w[:, :dec_len + 1, :enc_len]  # +1 for <sos>
-                        # for MT
-                        if 'token' in data[idx][1][self.ikey][self.iaxis].keys():
-                            xtokens = data[idx][1][self.ikey][self.iaxis]['token'].split()
-                    # for ASR/ST/MT
-                    if 'token' in data[idx][1][self.okey][self.oaxis].keys():
-                        ytokens = ['<sos>'] + data[idx][1][self.okey][self.oaxis]['token'].split()
-                        if "self" in name:
-                            xtokens = ytokens[:]
-                else:
-                    logging.warning("unknown name for shaping attention")
-                fig = _plot_and_save_attention(att_w, filename, xtokens, ytokens)
-                savefn(fig, filename)
+        plot_multi_head_attention(*args, **kwargs)
 
     def __call__(self, trainer):
         attn_dict = self.get_attention_weights()
