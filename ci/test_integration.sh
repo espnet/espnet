38541aed4392cbc9be9812e32eba6f63956ed613
fboyer <florian.boyer@labri.fr>
2019-12-27 17:52:19 +0100

    add integretation test and configs

--- ci/test_integration.sh
+++ ci/test_integration.sh
@@ -10,11 +10,20 @@ set -euo pipefail
 echo "==== ASR (backend=pytorch) ==="
 ./run.sh
 echo "==== ASR (backend=pytorch, dtype=float64) ==="
-./run.sh --stage 3 --train-config "$(change_yaml.py conf/train.yaml -a train-dtype=float64)" --decode-config "$(change_yaml.py conf/decode.yaml -a api=v2 -a dtype=float64)"
+#./run.sh --stage 3 --train-config "$(change_yaml.py conf/train.yaml -a train-dtype=float64)" --decode-config "$(change_yaml.py conf/decode.yaml -a api=v2 -a dtype=float64)"
 echo "==== ASR (backend=chainer) ==="
-./run.sh --stage 3 --backend chainer
+#./run.sh --stage 3 --backend chainer
 echo "==== ASR (backend=pytorch num-encs 2) ==="
-./run.sh --stage 2 --train-config ./conf/train_mulenc2.yaml --decode-config ./conf/decode_mulenc2.yaml --mulenc true
+#./run.sh --stage 2 --train-config ./conf/train_mulenc2.yaml --decode-config ./conf/decode_mulenc2.yaml --mulenc true
+
+# test transducer recipe
+echo "=== ASR (backend=pytorch, model=rnnt) ==="
+#./run.sh --stage 3 --train-config conf/train_transducer.yaml \
+#         --decode-config conf/decode_transducer.yaml
+echo "=== ASR (backend=pytorch, model=transformer-transducer) ==="
+./run.sh --stage 3 --train-config conf/train_transformer_transducer.yaml \
+         --decode-config conf/decode_transducer.yaml
+
 cd ${cwd} || exit 1
 
 # test asr_mix recipe
