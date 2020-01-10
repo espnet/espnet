daef1f0358551a41cdee9c37983a55af404ad24a
fboyer <florian.boyer@labri.fr>
2020-01-10 14:05:31 +0100

    use log instead of echo for printing logs

--- egs2/yesno/asr1/local/data.sh
+++ egs2/yesno/asr1/local/data.sh
@@ -36,38 +36,38 @@ train_dev="train_dev"
 eval_set="test_yesno"
 
 if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
-    echo "stage -1: Data download"
+    log "stage -1: Data download"
 
     mkdir -p ${datadir}
 
     if [ -d ${datadir}/waves_yesno ] || [ -f ${datadir}/waves_yesno.tar.gz ]; then
-        echo "$0: yesno directory or archive already exists in ${datadir}. Skipping download."
+        log "$0: yesno directory or archive already exists in ${datadir}. Skipping download."
     else
         if ! which wget >/dev/null; then
-            echo "$0: wget is not installed."
+            log "$0: wget is not installed."
             exit 1;
         fi
-        echo "$0: downloading data from ${data_url}"
+        log "$0: downloading data from ${data_url}"
 
         cd ${datadir}
         if ! wget --no-check-certificate ${data_url}; then
-            echo "$0: error executing wget ${data_url}"
+            log "$0: error executing wget ${data_url}"
             exit 1;
         fi
 
         if ! tar -xvzf waves_yesno.tar.gz; then
-            echo "$0: error un-tarring archive ${datadir}/waves_yesno.tar.gz"
+            log "$0: error un-tarring archive ${datadir}/waves_yesno.tar.gz"
             exit 1;
         fi
         cd ..
         rm ${yesno_root}/README*
 
-        echo "$0: Successfully downloaded and un-tarred ${datadir}/waves_yesno.tar.gz"
+        log "$0: Successfully downloaded and un-tarred ${datadir}/waves_yesno.tar.gz"
     fi
 fi
 
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
-    echo "stage 0: Data preparation"
+    log "stage 0: Data preparation"
 
     mkdir -p data/local
     ls -1 ${yesno_root} > data/local/waves_all.list
@@ -100,7 +100,7 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
 fi
 
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
-    echo "stage 1: Splitting train set into train+dev set"
+    log "stage 1: Splitting train set into train+dev set"
 
     utils/subset_data_dir.sh --first            \
                              data/train_yesno   \
