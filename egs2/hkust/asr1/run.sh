cdf0b42df9d27e90b6f61117e69e97a03fb69aae
Wangyou Zhang <C0me_On@163.com>
2020-01-14 23:21:08 +0800

    create ESPnet2 recipes: aishell, hkust, timit

---
+++ egs2/hkust/asr1/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+train_set=train_nodup
+dev_set=train_dev
+eval_sets="test "
+
+# speed perturbation related
+speed_perturb=True  # train_set will be "${train_set}_sp" if speed_perturb = True
+perturb_factors="0.9 1.0 1.1"
+
+./asr.sh \
+    --nbpe 5000 \
+    --token_type char \
+    --train_set "${train_set}" \
+    --dev_set "${dev_set}" \
+    --eval_sets "${eval_sets}" \
+    --srctexts "data/${train_set}/text" "$@" \
+    --speed_perturb "${speed_perturb}" \
+    --perturb_factors "${perturb_factors}"
