5ca01e98d8ad2ecc6c34b5c53351c3def83541ac
Yosuke Higuchi <wasapon.dev@gmail.com>
2020-01-16 02:51:54 +0900

    update asr.sh

--- egs/jsut/asr1/run.sh
+++ egs/jsut/asr1/run.sh
@@ -23,8 +23,10 @@ sampling_rate=48000
 do_delta=false
 
 lm_config=conf/lm.yaml
-train_config=conf/train.yaml
-decode_config=conf/decode.yaml
+# train_config=conf/train.yaml
+train_config=conf/tuning/train_pytorch_transformer.v2.yaml
+# decode_config=conf/decode.yaml
+decode_config=conf/tuning/decode_rnn.yaml
 
 lm_resume=
 lmtag=
