05b1d3f11b76af263b1492b3c6c30d580cb650e2
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2020-01-23 19:11:51 +0900

    add repr:   espnet2/schedulers/noam_lr.py, warmup_lr.py

--- espnet2/schedulers/noam_lr.py
+++ espnet2/schedulers/noam_lr.py
@@ -37,10 +37,6 @@ class NoamLR(_LRScheduler, AbsBatchStepScheduler):
         self.model_size = model_size
         self.warmup_steps = warmup_steps
 
-        # __init__() must be invoked before setting field
-        # because step() is also invoked in __init__()
-        super().__init__(optimizer, last_epoch)
-
         lr = list(optimizer.param_groups)[0]["lr"]
         new_lr = self.lr_for_WarmupLR(lr)
         warnings.warn(
@@ -48,8 +44,18 @@ class NoamLR(_LRScheduler, AbsBatchStepScheduler):
             f"Use WarmupLR(warmup_steps={warmup_steps}) with Optimizer(lr={new_lr})",
         )
 
+        # __init__() must be invoked before setting field
+        # because step() is also invoked in __init__()
+        super().__init__(optimizer, last_epoch)
+
     def lr_for_WarmupLR(self, lr: float) -> float:
         return lr / self.model_size ** 0.5 / self.warmup_steps ** 0.5
+
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__}(model_size={self.model_size}, "
+            f"warmup_steps={self.warmup_steps})"
+        )
 
     def get_lr(self):
         step_num = self.last_epoch + 1
--- espnet2/schedulers/warmup_lr.py
+++ espnet2/schedulers/warmup_lr.py
@@ -40,6 +40,9 @@ class WarmupLR(_LRScheduler, AbsBatchStepScheduler):
         # because step() is also invoked in __init__()
         super().__init__(optimizer, last_epoch)
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}(warmup_steps={self.warmup_steps})"
+
     def get_lr(self):
         step_num = self.last_epoch + 1
         return [
