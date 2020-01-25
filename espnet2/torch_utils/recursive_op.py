e41bbe61a67fd4648da4b826210e03e94985d9c2
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2020-01-26 00:59:46 +0900

    bug fix:   espnet2/torch_utils/recursive_op.py

--- espnet2/torch_utils/recursive_op.py
+++ espnet2/torch_utils/recursive_op.py
@@ -11,9 +11,9 @@ else:
 def recursive_sum(obj, weight: torch.Tensor, distributed: bool = False):
     assert weight.dim() == 1, weight.size()
     if isinstance(obj, (tuple, list)):
-        return type(obj)(recursive_sum(v, weight) for v in obj)
+        return type(obj)(recursive_sum(v, weight, distributed) for v in obj)
     elif isinstance(obj, dict):
-        return {k: recursive_sum(v, weight) for k, v in obj.items()}
+        return {k: recursive_sum(v, weight, distributed) for k, v in obj.items()}
     elif isinstance(obj, torch.Tensor):
         assert obj.size() == weight.size(), (obj.size(), weight.size())
         obj = (obj * weight.type(obj.dtype)).sum()
