17a9c76dd4a872d1abe7772235398285e727ff93
fboyer <florian.boyer@labri.fr>
2020-02-02 18:09:46 +0100

    revisit normalization step

--- egs2/how2/asr1/local/normalize_texts.sh
+++ egs2/how2/asr1/local/normalize_texts.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 normdir=local/data_normalization
-# keep the file order
-normfiles="shortened symbols url n_special dates n_ordinal nlsyms n_cardinal"
-
-_left_s="(^| )"
-_right_s="( |$)"
-_left_m="(^| |[[:punct:]])"
-_right_m="([[:punct:]]| |$)"
+normfiles="shortened symbols url nlsyms"
 
 log() {
     local fname=${BASH_SOURCE[1]##*/}
@@ -27,51 +21,68 @@ outidspk=$4
 
 [ ! -f ${intext} ] && log "${intext} was not found." && exit 2
 
-# ambiguous regex in simple left-right context are treated beforehand.
-# it concerns square brackets, brckets, curly brackets and dollars symbol.
+# some patterns are treated beforehand (due to tokenization or ambiguity).
+# it concerns apostrophe, simple brackets, squared bracked curly brackets,
+# slash in urls, okay and hash terms, and dollars symbol.
 cat ${intext} | \
-    sed "s:([^)]*)::g" | sed -r "s:\[[^]]*\]::g" | \
-    sed -r "s:\{[^]]*\}::g" | sed -r "s|^[A-Z ]+:||g" | \
-    sed -r "s:(o|O)\.(k|K)\.: okay :g" | \
-    sed -r 's:(\$)([0-9]*)([[:punct:]]$|$| ):\2 dollars \3:g' \
+    sed -r -e "s: &apos;:':g" \
+        -e "s:&#91;:\[:g" \
+        -e "s:&#93;:\]:g" \
+        -e "s:\([^)]*\)::g" \
+        -e "s:\[[^]]*\]::g" \
+        -e "s:\{[^]]*\}::g" \
+        -e "s|^[A-Za-z ]+:||g" \
+        -e "s:(\.com|\.org) /:\1 slash:g" \
+        -e "s:(^|[:punct:]| )(o|O)(\.)*(k|K)( |[:punct:]|$): okay :g" \
+        -e "s:\\$ 10-plant:10 dollars plant:g" \
+        -e "s:\\$ 3.50's: 3.50's dollars:g" \
+        -e "s:(C|D|F) #:\1 sharp:g" \
+        -e "s:#:number:g" \
+        -e 's:(\$) ([0-9[:punct:]]*)($| ):\2 dollars \3:g' \
         > ${outtext}.tmp
 
-# text normalization
+_left_s="(^| )"
+_right_s="( |$)"
+_left_m="(^| |[[:punct:]])"
+_right_m="([[:punct:]]| |$)"
+
+# further normalization with simple left-right pattern and common terms
 for file in $normfiles; do
     while read line || [ -n "$line" ]; do
         before=$(echo $line | cut -d ' ' -f1)
         after=$(echo $line | cut -d ' ' -f2-)
 
-        if [ $file == "shortened" ] || [ $file == "n_special" ] || [ $file == "n_ordinal" ]; then
+        if [ $file == "shortened" ]; then
             sed -i -r "s:${_left_m}${before}${_right_m}: ${after} :g" ${outtext}.tmp
         elif [ $file == "url" ] || [ $file == "symbols" ]; then
             sed -i -r "s:${before}: ${after} :g" ${outtext}.tmp
-        elif [ $file == "n_cardinal" ]; then
-            sed -i -r "s:${left_s}${before}${_right_s}: ${after} :g" ${outtext}.tmp
         elif [ $file == "nlsyms" ]; then
             sed -i -r "s:${_left_s}${before}${_right_m}: ${after} :g" ${outtext}.tmp
-        elif [ $file == "dates" ]; then
-            sed -i -r "s: ${before}${_right_m}: ${after} :g" ${outtext}.tmp
         else
-            log "Error: $file is undefined for normalization."
+            log "Error: unknown normalization files"
         fi
     done < "${normdir}/${file}"
 done
 
-# punctuation normalization
+# final punctuation and number normalization
 cat ${outtext}.tmp | \
     sed -r -e "s:': APOSTROPHE :g" \
+        -e "s:([0-9]) *(x|X) *([0-9]):\1 by \3:g" \
+        -e "s:,([0-9]{3}):\1:g" \
+        -e "s:\.(0)+${_right_s}: :g" \
+        -e "s:([0-9])(\.)([0-9]):\1 point \3:g" \
         -e "s:[[:punct:]]: :g" \
         -e "s: APOSTROPHE :':g" \
-        -e "s:hesmark:[hes]:g" \
+        -e "s:hesmark:\[hes\]:g" \
         -e "s:^ +::g" \
         -e "s: +$::g" | \
     tr -s ' ' > ${outtext}.tmp2
 
 basedir=$(dirname $intext)
 
-grep -n -e "^$" -e "ü" -e "è" -e "ñ" -e "é" -e "[0-9]" ${outtext}.tmp2 | \
-    cut -d':' -f1 | sed -r '/^ *$/d' > ${basedir}/utterances.deleted
+# Remove empty lines and save line number to modify other needed files.
+grep -n -e "^$" ${outtext}.tmp2 | cut -d':' -f1 | sed -r '/^ *$/d' \
+                                                      > ${basedir}/utterances.deleted
 
 if [ -s ${basedir}/utterances.deleted ]; then
     awk 'NR==FNR{l[$0];next;} !(FNR in l)' ${basedir}/utterances.deleted ${outtext}.tmp2 \
