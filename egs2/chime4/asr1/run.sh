7b194d6031d61aa7a88566837c782e745ff760a5
yzhan <zhangyuekai@foxmail.com>
2020-02-11 18:13:25 -0500

    initial commit

---
+++ egs2/chime4/asr1/run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+stage=1
+stop_stage=12
+nj=16
+decode_asr_model=valid.loss.best.pth
+
+
+train_set=tr05_multi_noisy_si284 # tr05_multi_noisy (original training data) or tr05_multi_noisy_si284 (add si284 data)
+dev_set=dt05_multi_isolated_1ch_track
+eval_set="\
+dt05_real_isolated_1ch_track dt05_simu_isolated_1ch_track et05_real_isolated_1ch_track et05_simu_isolated_1ch_track \
+dt05_real_beamformit_2mics dt05_simu_beamformit_2mics et05_real_beamformit_2mics et05_simu_beamformit_2mics \
+dt05_real_beamformit_5mics dt05_simu_beamformit_5mics et05_real_beamformit_5mics et05_simu_beamformit_5mics \
+"
+
+asr_config=conf/train_rnn.yaml
+decode_config=conf/decode_rnn.yaml
+lm_config=conf/lm.yaml
+
+
+use_word_lm=false
+word_vocab_size=65000
+
+./asr.sh                                   \
+    --stage ${stage}                       \
+    --stop_stage ${stop_stage}             \
+    --nj ${nj}                             \
+    --decode_asr_model ${decode_asr_model} \
+    --token_type char                      \
+    --feats_type fbank_pitch               \
+    --asr_config "${asr_config}"           \
+    --decode_config "${decode_config}"     \
+    --lm_config "${lm_config}"             \
+    --train_set "${train_set}"             \
+    --dev_set "${dev_set}"                 \
+    --eval_sets "${eval_set}"              \
+    --srctexts "data/${train_set}/text data/local/other_text/text" "$@"
