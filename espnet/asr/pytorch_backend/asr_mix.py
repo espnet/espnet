6425e3f38f85db18422d870d1e3501adc11dcad5
CharlieTang <user.email>
2020-02-27 11:47:20 +0800

    embed sized setting in default lm bug fix

--- espnet/asr/pytorch_backend/asr_mix.py
+++ espnet/asr/pytorch_backend/asr_mix.py
@@ -157,7 +157,7 @@ def train(args):
         rnnlm_args = get_model_conf(args.rnnlm, args.rnnlm_conf)
         rnnlm = lm_pytorch.ClassifierWithState(
             lm_pytorch.RNNLM(
-                len(args.char_list), rnnlm_args.layer, rnnlm_args.unit))
+                len(args.char_list), rnnlm_args.layer, rnnlm_args.unit, rnnlm_args.embed_unit))
         torch.load(args.rnnlm, rnnlm)
         model.rnnlm = rnnlm
 
