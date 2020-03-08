cd6f8d03cb2e12324b6e315c5861636e591ba22f
unilight <vincenthuang316@gmail.com>
2020-03-08 20:09:46 +0900

    integrate with PWG

---
+++ egs/vcc2020/vc1/README.md
@@ -0,0 +1,5 @@
+
+
+```
+./run.sh --stage 12 --trgspk TGM1 --tts_model_dir exp/TGM1_train_no_dev_pytorch_train_pytorch_transformer+spkemb_freeze_emb.mailabs_en_de/ --pretrained_model mailabs_en_de --model snapshot.ep.200 --trans_type char --srcspk SEM2
+```
--- egs/vcc2020/vc1/local/pretrained_model_download.sh
+++ egs/vcc2020/vc1/local/pretrained_model_download.sh
@@ -15,6 +15,8 @@ if [ $# != 2 ]; then
     echo "    - tts1_en_fi"
     echo "    - tts1_en_zh"
     echo "    - tts1 (LibriTTS)"
+    echo "    - pwg_task1"
+    echo "    - pwg_task2"
     exit 1
 fi
 
@@ -23,6 +25,8 @@ case "${pretrained_model}" in
     "tts1_en_fi") share_url="https://drive.google.com/open?id=1XYpBZe9-9AgAxGpKfrgQPDjlW2S6duac" ;;
     "tts1_en_zh") share_url="https://drive.google.com/open?id=1E6vzNaXT6r7Zybefat_p9ncnMOQCXtem" ;;
     "tts1")       share_url="https://drive.google.com/open?id=1Xj73mDPuuPH8GsyNO8GnOC3mn0_OK4g3" ;;
+    "pwg_task1")  share_url="https://drive.google.com/open?id=" ;;
+    "pwg_task2")  share_url="https://drive.google.com/open?id=1li9DLZGnAheWZrB4oXGo0KWq-fHuFH_l" ;;
     *) echo "No such pretrained model: ${pretrained_model}"; exit 1 ;;
 esac
 
