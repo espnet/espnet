72a34aa172c01b113ada59fb771cd819c747d93e
fboyer <florian.boyer@labri.fr>
2020-03-22 17:07:39 +0100

    add missing initializations for RNN + update

--- espnet/nets/pytorch_backend/transducer/initializer.py
+++ espnet/nets/pytorch_backend/transducer/initializer.py
@@ -3,7 +3,10 @@
 
 """Parameter initialization for transducer RNN/Transformer parts."""
 
+import six
+
 from espnet.nets.pytorch_backend.initialization import lecun_normal_init_parameters
+from espnet.nets.pytorch_backend.initialization import set_forget_bias_to_one
 
 from espnet.nets.pytorch_backend.transformer.initializer import initialize
 
@@ -19,11 +22,14 @@ def initializer(model, args):
     if args.dtype != 'transformer':
         if args.etype == 'transformer':
             initialize(model.encoder, args.transformer_init)
-            lecun_normal_init_parameters(model.decoder)
+            lecun_normal_init_parameters(model.dec)
         else:
             lecun_normal_init_parameters(model)
 
-        model.decoder.embed.weight.data.normal_(0, 1)
+        model.dec.embed.weight.data.normal_(0, 1)
+
+        for l in six.moves.range(len(model.dec.decoder)):
+            set_forget_bias_to_one(model.dec.decoder[l].bias_ih)
     else:
         if args.etype == 'transformer':
             initialize(model, args.transformer_init)
