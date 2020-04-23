526e59e559781b299197fb0aeee995f07b9cbc9c
Piotr Żelasko <petezor@gmail.com>
2020-04-23 14:52:47 -0400

    CI fix - Black reformatting changes

--- egs/discophone/asr1/local/prepare_experiment_configs.py
+++ egs/discophone/asr1/local/prepare_experiment_configs.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 from pathlib import Path
 
-CONF_DIR = Path('conf/experiments')
-BABEL_LANGS_OF_INTEREST = frozenset('101 103 107 203 206 307 402 404'.split())
-GLOBALPHONE_LANGS_OF_INTEREST = frozenset("Arabic Czech French Korean Mandarin Spanish Thai".split())
+CONF_DIR = Path("conf/experiments")
+BABEL_LANGS_OF_INTEREST = frozenset("101 103 107 203 206 307 402 404".split())
+GLOBALPHONE_LANGS_OF_INTEREST = frozenset(
+    "Arabic Czech French Korean Mandarin Spanish Thai".split()
+)
 
 CONF_TEMPLATE = """
 # BABEL TRAIN:
@@ -33,79 +35,79 @@ for babel_lang in BABEL_LANGS_OF_INTEREST:
     config = CONF_TEMPLATE.format(
         BABEL_LANGS=babel_lang,
         BABEL_RECOG_LANGS=babel_lang,
-        GLOBALPHONE_LANGS='',
-        GLOBALPHONE_RECOG_LANGS='',
-        MBOSHI_TRAIN='false',
-        MBOSHI_RECOG='false',
-        USE_IPA='true'
+        GLOBALPHONE_LANGS="",
+        GLOBALPHONE_RECOG_LANGS="",
+        MBOSHI_TRAIN="false",
+        MBOSHI_RECOG="false",
+        USE_IPA="true",
     )
-    (CONF_DIR / f'monolingual-{babel_lang}-ipa.conf').write_text(config)
+    (CONF_DIR / f"monolingual-{babel_lang}-ipa.conf").write_text(config)
 for gp_lang in GLOBALPHONE_LANGS_OF_INTEREST:
     config = CONF_TEMPLATE.format(
-        BABEL_LANGS='',
-        BABEL_RECOG_LANGS='',
+        BABEL_LANGS="",
+        BABEL_RECOG_LANGS="",
         GLOBALPHONE_LANGS=gp_lang,
         GLOBALPHONE_RECOG_LANGS=gp_lang,
-        MBOSHI_TRAIN='false',
-        MBOSHI_RECOG='false',
-        USE_IPA='true'
+        MBOSHI_TRAIN="false",
+        MBOSHI_RECOG="false",
+        USE_IPA="true",
     )
-    (CONF_DIR / f'monolingual-{gp_lang}-ipa.conf').write_text(config)
+    (CONF_DIR / f"monolingual-{gp_lang}-ipa.conf").write_text(config)
 # MBOSHI
 config = CONF_TEMPLATE.format(
-    BABEL_LANGS='',
-    BABEL_RECOG_LANGS='',
-    GLOBALPHONE_LANGS='',
-    GLOBALPHONE_RECOG_LANGS='',
-    MBOSHI_TRAIN='true',
-    MBOSHI_RECOG='true',
-    USE_IPA='true'
+    BABEL_LANGS="",
+    BABEL_RECOG_LANGS="",
+    GLOBALPHONE_LANGS="",
+    GLOBALPHONE_RECOG_LANGS="",
+    MBOSHI_TRAIN="true",
+    MBOSHI_RECOG="true",
+    USE_IPA="true",
 )
-(CONF_DIR / f'monolingual-mboshi-ipa.conf').write_text(config)
+(CONF_DIR / f"monolingual-mboshi-ipa.conf").write_text(config)
 
 # Leave-one-out schemes
 for babel_lang in BABEL_LANGS_OF_INTEREST:
     config = CONF_TEMPLATE.format(
-        BABEL_LANGS=' '.join(BABEL_LANGS_OF_INTEREST - {babel_lang}),
+        BABEL_LANGS=" ".join(BABEL_LANGS_OF_INTEREST - {babel_lang}),
         BABEL_RECOG_LANGS=babel_lang,
-        GLOBALPHONE_LANGS=' '.join(GLOBALPHONE_LANGS_OF_INTEREST),
-        GLOBALPHONE_RECOG_LANGS='',
-        MBOSHI_TRAIN='true',
-        MBOSHI_RECOG='false',
-        USE_IPA='true'
+        GLOBALPHONE_LANGS=" ".join(GLOBALPHONE_LANGS_OF_INTEREST),
+        GLOBALPHONE_RECOG_LANGS="",
+        MBOSHI_TRAIN="true",
+        MBOSHI_RECOG="false",
+        USE_IPA="true",
     )
-    (CONF_DIR / f'oneout-{babel_lang}-ipa.conf').write_text(config)
+    (CONF_DIR / f"oneout-{babel_lang}-ipa.conf").write_text(config)
 for gp_lang in GLOBALPHONE_LANGS_OF_INTEREST:
     config = CONF_TEMPLATE.format(
-        BABEL_LANGS=' '.join(BABEL_LANGS_OF_INTEREST),
-        BABEL_RECOG_LANGS='',
-        GLOBALPHONE_LANGS=' '.join(GLOBALPHONE_LANGS_OF_INTEREST - {gp_lang}),
+        BABEL_LANGS=" ".join(BABEL_LANGS_OF_INTEREST),
+        BABEL_RECOG_LANGS="",
+        GLOBALPHONE_LANGS=" ".join(GLOBALPHONE_LANGS_OF_INTEREST - {gp_lang}),
         GLOBALPHONE_RECOG_LANGS=gp_lang,
-        MBOSHI_TRAIN='true',
-        MBOSHI_RECOG='false',
-        USE_IPA='true'
+        MBOSHI_TRAIN="true",
+        MBOSHI_RECOG="false",
+        USE_IPA="true",
     )
-    (CONF_DIR / f'oneout-{gp_lang}-ipa.conf').write_text(config)
+    (CONF_DIR / f"oneout-{gp_lang}-ipa.conf").write_text(config)
 # MBOSHI
 config = CONF_TEMPLATE.format(
-    BABEL_LANGS=' '.join(BABEL_LANGS_OF_INTEREST),
-    BABEL_RECOG_LANGS='',
-    GLOBALPHONE_LANGS=' '.join(GLOBALPHONE_LANGS_OF_INTEREST),
-    GLOBALPHONE_RECOG_LANGS='',
-    MBOSHI_TRAIN='false',
-    MBOSHI_RECOG='true',
-    USE_IPA='true'
+    BABEL_LANGS=" ".join(BABEL_LANGS_OF_INTEREST),
+    BABEL_RECOG_LANGS="",
+    GLOBALPHONE_LANGS=" ".join(GLOBALPHONE_LANGS_OF_INTEREST),
+    GLOBALPHONE_RECOG_LANGS="",
+    MBOSHI_TRAIN="false",
+    MBOSHI_RECOG="true",
+    USE_IPA="true",
 )
-(CONF_DIR / f'oneout-mboshi-ipa.conf').write_text(config)
+(CONF_DIR / f"oneout-mboshi-ipa.conf").write_text(config)
 
 # Train-all-test-all scheme
 config = CONF_TEMPLATE.format(
-    BABEL_LANGS=' '.join(BABEL_LANGS_OF_INTEREST),
-    BABEL_RECOG_LANGS=' '.join(BABEL_LANGS_OF_INTEREST),
-    GLOBALPHONE_LANGS=' '.join(GLOBALPHONE_LANGS_OF_INTEREST),
-    GLOBALPHONE_RECOG_LANGS=' '.join(GLOBALPHONE_LANGS_OF_INTEREST),
-    MBOSHI_TRAIN='true',
-    MBOSHI_RECOG='true',
-    USE_IPA='true'
+    BABEL_LANGS=" ".join(BABEL_LANGS_OF_INTEREST),
+    BABEL_RECOG_LANGS=" ".join(BABEL_LANGS_OF_INTEREST),
+    GLOBALPHONE_LANGS=" ".join(GLOBALPHONE_LANGS_OF_INTEREST),
+    GLOBALPHONE_RECOG_LANGS=" ".join(GLOBALPHONE_LANGS_OF_INTEREST),
+    MBOSHI_TRAIN="true",
+    MBOSHI_RECOG="true",
+    USE_IPA="true",
 )
-(CONF_DIR / f'all-ipa.conf').write_text(config)
+(CONF_DIR / f"all-ipa.conf").write_text(config)
