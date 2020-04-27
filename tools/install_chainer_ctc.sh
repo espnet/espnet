8de3e69c1c91fdcdf846a2a0104c7a94c3b9d8fd
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2020-04-28 06:19:00 +0900

    add pt1.5 to ci

---
+++ tools/install_chainer_ctc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+if [ $# != 0 ]; then
+    echo "Usage: $0"
+    exit 1;
+fi
+
+rm -rf chainer_ctc
+git clone https://github.com/jheymann85/chainer_ctc.git
+pip install cython
+( 
+    set -euo pipefail
+    cd chainer_ctc && chmod +x install_warp-ctc.sh && ./install_warp-ctc.sh && pip install . 
+)
