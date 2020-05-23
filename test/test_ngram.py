e2bd4116f16903f9b87c2855b5a4bf1d84868e30
tanghaoyu <tanghaoyu@bigo.sg>
2020-05-23 17:55:51 +0800

    bug fix

--- test/test_ngram.py
+++ test/test_ngram.py
@@ -7,7 +7,7 @@ kenlm = pytest.importorskip("kenlm")
 test_sens = ["I like apple", "you love coffee"]
 
 
-@pytest.mark.parameterize(test_sens)
+@pytest.mark.parametrize(test_sens)
 def test_ngram_build(test_sens):
     lm = kenlm("test.arpa")
     assert isclose(lm.score(test_sens[0]), -1.04)
