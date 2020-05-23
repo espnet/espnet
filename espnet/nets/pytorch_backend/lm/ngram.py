c4aebd49ee0da09c76a8fc41ce52e61450fec3a3
tanghaoyu <tanghaoyu@bigo.sg>
2020-05-23 11:03:20 +0800

    format

--- espnet/nets/pytorch_backend/lm/ngram.py
+++ espnet/nets/pytorch_backend/lm/ngram.py
@@ -10,31 +10,35 @@ from abc import ABC
 class Ngrambase(ABC):
     def __init__(self, ngram_model, token_list, space=" "):
         self.space = space
-        self.chardict = [x if x!= "<eos>" else "</s>" for x in token_list]
+        self.chardict = [x if x != "<eos>" else "</s>" for x in token_list]
         self.charlen = len(self.chardict)
         self.lm = kenlm.LanguageModel(ngram_model)
         self.tmpkenlmstate = kenlm.State()
 
     def init_state(self, x):
         state = kenlm.State()
-        # since there is no <s> only </s> 
+        # since there is no <s> only </s>
         self.lm.NullContextWrite(state)
         return [0.0, state]
-    
+
     def select_state(self, state, i):
         return state
-    
+
     def score_partial_(self, y, next_token, state, x):
         out_state = kenlm.State()
         state[0] += self.lm.BaseScore(state[1], self.chardict[y[-1]], out_state)
         scores = torch.full(next_token.size(), state[0])
         for i, j in enumerate(next_token):
-            scores[i] += self.lm.BaseScore(out_state, self.chardict[j], self.tmpkenlmstate)
-        return scores, [state[0], out_state] 
+            scores[i] += self.lm.BaseScore(
+                out_state, self.chardict[j], self.tmpkenlmstate
+            )
+        return scores, [state[0], out_state]
+
 
 class NgramFullScorer(Ngrambase, ScorerInterface):
     def score(self, y, state, x):
         return self.score_partial_(y, torch.tensor(range(len(self.chardict))), state, x)
+
 
 class NgramPartScorer(Ngrambase, PartialScorerInterface):
     def score_partial(self, y, next_token, state, x):
