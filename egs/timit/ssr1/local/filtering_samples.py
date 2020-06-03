dfb32e5307176a107d5f50fe0eb028fc79ecda61
Zixiong SU <soshiyuu@gmail.com>
2020-06-03 23:49:35 +0900

    Update filtering_samples.py

--- egs/timit/ssr1/local/filtering_samples.py
+++ egs/timit/ssr1/local/filtering_samples.py
@@ -41,10 +41,10 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--etype",
-        default='vggblstm',
+        default="vggblstm",
         type=str,
         help="Encoder type"
-        #I don't know why there is no such option in WSJ recipe, but it doesn't work without this argument."
+        # I don't know why there is no such option in WSJ recipe, but it doesn't work without this argument."
     )
     args, _ = parser.parse_known_args(cmd_args)
 
