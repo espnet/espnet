6b7ddf9de4c35384395632566a1743673b46ef4b
Zixiong SU <soshiyuu@gmail.com>
2020-06-04 12:23:29 +0900

    Update run.sh

--- egs/timit/ssr1/run.sh
+++ egs/timit/ssr1/run.sh
@@ -65,13 +65,15 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     ### But you can utilize Kaldi recipes in most cases
     echo "stage 1: Feature Generation"
     # Generate the fbank features; by default 80-dimensional fbanks with pitch on each frame
-    mkdir -p data/train
-    mkdir -p data/test
+
     if [ ! -s data/Bruce_list_train.ark ] && [ ! -s data/Bruce_list_test.ark ];then
         wget https://storage.googleapis.com/szxs/dct_features.tar.gz
         tar xzvf dct_features.tar.gz;rm dct_features.tar.gz
   
     fi
+    mkdir -p data/train
+    mkdir -p data/test
+    
     chmod 755 local/featprepare.py
     local/featprepare.py
 
