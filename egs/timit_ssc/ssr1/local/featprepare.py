a4b96c9135d35264aee64e2643f156c33ac6cfc2
Zixiong SU <soshiyuu@gmail.com>
2020-06-11 15:53:29 +0900

    download dataset from the scp server

--- egs/timit_ssc/ssr1/local/featprepare.py
+++ egs/timit_ssc/ssr1/local/featprepare.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-for dataset in ["data/train", "data/test"]:
-    with open(dataset + ".scp", "r") as s:
+timit_dir = "data/timit_data/"
+for dataset in ["train", "test"]:
+    with open(timit_dir + dataset + ".scp", "r") as s:
         utts = s.readlines()
         n_utts = len(utts)
         for i in range(n_utts):
             utts[i] = utts[i].split()[0]
-    with open(dataset + ".txt", "r") as t:
+    with open(timit_dir + dataset + ".txt", "r") as t:
         txts = t.readlines()
     text = []
     spk2utt = "Bruce " + " ".join(utts)
@@ -15,13 +16,14 @@ for dataset in ["data/train", "data/test"]:
         text.append(utts[i] + " " + txts[i])
         utt2spk.append(utts[i] + " Bruce\n")
         wav.append(utts[i] + " " + str(i) + ".wav\n")
-    with open(dataset + "/text", "w") as a:
-        a.writelines(text)
-    with open(dataset + "/spk2utt", "w") as b:
-        b.writelines(spk2utt)
-    with open(dataset + "/utt2spk", "w") as c:
-        c.writelines(utt2spk)
+    dataset = "data/" + dataset
+    with open(dataset + "/text", "w") as t:
+        t.writelines(text)
+    with open(dataset + "/spk2utt", "w") as s:
+        s.writelines(spk2utt)
+    with open(dataset + "/utt2spk", "w") as u:
+        u.writelines(utt2spk)
     # wav.scp is not actually used.
     # It is generated just for fixing data directory.
-    with open(dataset + "/wav.scp", "w") as d:
-        d.writelines(wav)
+    with open(dataset + "/wav.scp", "w") as w:
+        w.writelines(wav)
