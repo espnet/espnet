cec07beb88a576fca8ec1eef9c438cc6ba17c063
Zixiong SU <soshiyuu@gmail.com>
2020-06-12 18:33:09 +0900

    Update ssc_data_prepare.sh

--- egs/timit_ssc/ssr1/local/ssc_data_prepare.sh
+++ egs/timit_ssc/ssr1/local/ssc_data_prepare.sh
@@ -10,14 +10,16 @@
 timit_dir=data/timit_data/
 mkdir -p ${timit_dir} ${timit_data}
 wget -c -N ftp://ftp.espci.fr/pub/sigma/Features/DCT_Features/features/30-dct/* -P ${timit_dir}
-wget https://ftp.espci.fr/pub/sigma/TIMIT_training/TIMIT_Transcripts.txt -P ${timit_dir}
-wget https://ftp.espci.fr/pub/sigma/WSJ05K_Test/WSJ0_5K_Transcripts.txt -P ${timit_dir}
+wget -c -N https://ftp.espci.fr/pub/sigma/TIMIT_training/TIMIT_Transcripts.txt -P ${timit_dir}
+wget -c -N https://ftp.espci.fr/pub/sigma/WSJ05K_Test/WSJ0_5K_Transcripts.txt -P ${timit_dir}
 
 sed "s/^[0-9]*[.].//g" ${timit_dir}/TIMIT_Transcripts.txt  | sed "s/([^)]*)//g" | tr [:lower:] [:upper:] > ${timit_dir}/train.txt
 sed "s/^[0-9]*[.].//g" ${timit_dir}/WSJ0_5K_Transcripts.txt | sed "s/([^)]*)//g" | tr [:lower:] [:upper:] > ${timit_dir}/test.txt
 for x in train test;do
     cat ${timit_dir}/${x}-feats.scp \
-    | sed "s%/home/jiyan/kaldi-update/egs/jiyan/dct30-wsj/voxforge/mfcc/%"${PWD}"/data/timit_data/%g" > data/${x}/feats.scp
+    | sed "s%/home/jiyan/kaldi-update/egs/jiyan/dct30-wsj/voxforge/mfcc/%"${PWD}"/data/timit_data/%g" \
+    | sed "s%/home/jiyan/kaldi-update/egs/jiyan/s5-jiyan1.1/voxforge/mfcc/%"${PWD}"/data/timit_data/%g" \
+    > data/${x}/feats.scp
 done
 
 mkdir -p data/train
