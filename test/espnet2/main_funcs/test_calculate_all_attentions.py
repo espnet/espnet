9688fff0b066c7d7e7d65642fcbc4efb9ab20cfc
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2020-06-12 20:07:31 +0900

    fix

--- test/espnet2/main_funcs/test_calculate_all_attentions.py
+++ test/espnet2/main_funcs/test_calculate_all_attentions.py
@@ -19,7 +19,7 @@ class Dummy(AbsESPnetModel):
         self.desired = defaultdict(list)
 
     def forward(self, x, x_lengths, y, y_lengths):
-        a1 = self.att1(y, x, x, None)
+        self.att1(y, x, x, None)
         _, a2 = self.att2(x, x_lengths, y, None)
         self.desired["att1"].append(self.att1.attn.squeeze(0))
         self.desired["att2"].append(a2)
