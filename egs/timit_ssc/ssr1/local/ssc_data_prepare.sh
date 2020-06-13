e682d54b6d1c5730af4577af99bf64065a60a128
Zixiong SU <soshiyuu@gmail.com>
2020-06-14 03:01:22 +0900

    Add options to use other features.

--- egs/timit_ssc/ssr1/local/ssc_data_prepare.sh
+++ egs/timit_ssc/ssr1/local/ssc_data_prepare.sh
@@ -5,24 +5,30 @@
 # Apache 2.0
 
 # Downloads extracted features and text data from SSC ftp server.
+# You can specify the feature directory on the server to try different features.
+# Available directories:
+# DCT_Features/features/{5,10,20,30}-dct
+# Auto_Features/features/{5,10,20,30}d
+# e.g., ssc_data_prepare.sh --feat_dir Auto_Features/features/30d
+#       ssc_data_prepare.sh --feat_dir DCT_Features/features/20-dct"
+
 . ./path.sh
 
-timit_dir=data/timit_data/
-mkdir -p ${timit_dir} ${timit_data}
-wget -c -N ftp://ftp.espci.fr/pub/sigma/Features/DCT_Features/features/30-dct/* -P ${timit_dir}
-wget -c -N https://ftp.espci.fr/pub/sigma/TIMIT_training/TIMIT_Transcripts.txt -P ${timit_dir}
-wget -c -N https://ftp.espci.fr/pub/sigma/WSJ05K_Test/WSJ0_5K_Transcripts.txt -P ${timit_dir}
+feat_dir=DCT_Features/features/30-dct
 
-sed "s/^[0-9]*[.].//g" ${timit_dir}/TIMIT_Transcripts.txt  | sed "s/([^)]*)//g" | tr [:lower:] [:upper:] > ${timit_dir}/train.txt
-sed "s/^[0-9]*[.].//g" ${timit_dir}/WSJ0_5K_Transcripts.txt | sed "s/([^)]*)//g" | tr [:lower:] [:upper:] > ${timit_dir}/test.txt
+feat_local_dir=data/${feat_dir}
+mkdir -p ${feat_local_dir}
+wget -c -N ftp://ftp.espci.fr/pub/sigma/Features/${feat_dir}/* -P ${feat_local_dir}
+wget -c -N https://ftp.espci.fr/pub/sigma/TIMIT_training/TIMIT_Transcripts.txt -P ${feat_local_dir}
+wget -c -N https://ftp.espci.fr/pub/sigma/WSJ05K_Test/WSJ0_5K_Transcripts.txt -P ${feat_local_dir}
+
+sed "s/^[0-9]*[.].//g" ${feat_local_dir}/TIMIT_Transcripts.txt  | sed "s/([^)]*)//g" | tr [:lower:] [:upper:] > data/train/text
+sed "s/^[0-9]*[.].//g" ${feat_local_dir}/WSJ0_5K_Transcripts.txt | sed "s/([^)]*)//g" | tr [:lower:] [:upper:] > data/test/text
 for x in train test;do
-    cat ${timit_dir}/${x}-feats.scp \
-    | sed "s%/home/jiyan/kaldi-update/egs/jiyan/dct30-wsj/voxforge/mfcc/%"${PWD}"/data/timit_data/%g" \
-    | sed "s%/home/jiyan/kaldi-update/egs/jiyan/s5-jiyan1.1/voxforge/mfcc/%"${PWD}"/data/timit_data/%g" \
-    > data/${x}/feats.scp
+    sed "s% .*mfcc/% "${PWD}"/"${feat_local_dir}"/%g" ${feat_local_dir}/${x}*.scp \
+    | sed "s/t\([0-9]\)_/t0\1_/g" | sort > data/${x}/feats.scp
 done
 
 mkdir -p data/train
 mkdir -p data/test
 local/featprepare.py
-
