985aa56d803bf86b25cbec6ca326343faff83258
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2020-06-18 23:07:40 +0900

    normalize_type -> cleaner_type:   espnet2/text/cleaner.py

--- espnet2/text/cleaner.py
+++ espnet2/text/cleaner.py
@@ -7,7 +7,7 @@ from vietnamese_cleaner import vietnamese_cleaners
 
 
 class TextCleaner:
-    """Text cleaner
+    """Text cleaner.
 
     Examples:
         >>> cleaner = TextCleaner("tacotron")
@@ -16,18 +16,18 @@ class TextCleaner:
 
     """
 
-    def __init__(self, normalize_types: Collection[str] = None):
+    def __init__(self, cleaner_types: Collection[str] = None):
         assert check_argument_types()
 
-        if normalize_types is None:
-            self.normalize_types = []
-        elif isinstance(normalize_types, str):
-            self.normalize_types = [normalize_types]
+        if cleaner_types is None:
+            self.cleaner_types = []
+        elif isinstance(cleaner_types, str):
+            self.cleaner_types = [cleaner_types]
         else:
-            self.normalize_types = list(normalize_types)
+            self.cleaner_types = list(cleaner_types)
 
     def __call__(self, text: str) -> str:
-        for t in self.normalize_types:
+        for t in self.cleaner_types:
             if t == "tacotron":
                 text = tacotron_cleaner.cleaners.custom_english_cleaners(text)
             elif t == "jaconv":
