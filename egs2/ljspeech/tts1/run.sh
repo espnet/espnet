7c5a1a397290812d29994e6eb6ec8136ff62c17c
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2020-06-28 23:31:00 +0900

    added ljspeech recipe

---
+++ egs2/ljspeech/tts1/run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+fs=22050
+n_fft=1024
+n_shift=256
+
+opts=
+if [ "${fs}" -eq 48000 ]; then
+    # To suppress recreation, specify wav format
+    opts="--audio_format wav "
+else
+    opts="--audio_format flac "
+fi
+
+train_set=tr_no_dev
+dev_set=dev
+eval_set=eval1
+
+train_config=conf/train.yaml
+decode_config=conf/decode.yaml
+
+./tts.sh \
+    --feats_type fbank \
+    --fs "${fs}" \
+    --n_fft "${n_fft}" \
+    --n_shift "${n_shift}" \
+    --token_type phn \
+    --cleaner tacotron \
+    --g2p g2p_en \
+    --train_config "${train_config}" \
+    --decode_config "${decode_config}" \
+    --train_set "${train_set}" \
+    --dev_set "${dev_set}" \
+    --eval_sets "${eval_set}" \
+    --srctexts "data/${train_set}/text" \
+    ${opts} "$@"
