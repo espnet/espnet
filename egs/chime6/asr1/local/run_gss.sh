a1bfa8e6901214745744423e47af2e28844adec1
andrusenkoau <andrusenkoau@gmail.com>
2020-07-11 10:57:23 +0300

    default gss settings (16 channels) was be ыÑset

--- egs/chime6/asr1/local/run_gss.sh
+++ egs/chime6/asr1/local/run_gss.sh
@@ -8,8 +8,8 @@ if [ -f ./path.sh ]; then . ./path.sh; fi
 # Config:
 cmd=run.pl
 nj=4
-#multiarray=outer_array_mics  -- 12 channels for gss
-multiarray=True              #-- 24 channels for gss
+multiarray=outer_array_mics   #-- 16 channels for gss; usually better than 24 channels gss
+#multiarray=True              #-- 24 channels for gss
 bss_iterations=5
 context_samples=160000
 . utils/parse_options.sh || exit 1;
