13f2e434da0a22af35c6806230b2db362529e545
Alexander Laptev <laptev@speechpro.com>
2020-07-19 22:20:01 +0300

    apply black

--- egs/chime6/asr1/local/make_noise_list.py
+++ egs/chime6/asr1/local/make_noise_list.py
@@ -6,12 +6,14 @@ import sys
 
 
 if len(sys.argv) != 2:
-    print ("Usage: {} <noises-dir>".format(sys.argv[0]))
+    print("Usage: {} <noises-dir>".format(sys.argv[0]))
     raise SystemExit(1)
 
 
 for line in glob.glob("{}/*.wav".format(sys.argv[1])):
     fname = os.path.basename(line.strip())
 
-    print ("--noise-id {} --noise-type point-source "
-           "--bg-fg-type foreground {}".format(fname, line.strip()))
+    print(
+        "--noise-id {} --noise-type point-source "
+        "--bg-fg-type foreground {}".format(fname, line.strip())
+    )
