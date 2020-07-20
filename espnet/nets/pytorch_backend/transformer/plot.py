90f214840cbea9a1116d9c5e0878a9c77d1949e0
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2020-07-20 09:58:03 +0900

    Fix attention plotting for Transformer

--- espnet/nets/pytorch_backend/transformer/plot.py
+++ espnet/nets/pytorch_backend/transformer/plot.py
@@ -74,28 +74,34 @@ def plot_multi_head_attention(
     for name, att_ws in attn_dict.items():
         for idx, att_w in enumerate(att_ws):
             filename = "%s/%s.%s.%s" % (outdir, data[idx][0], name, suffix)
-            dec_len = int(data[idx][1][okey][oaxis]["shape"][0])
+            dec_len = int(data[idx][1][okey][oaxis]["shape"][0]) + 1  # +1 for <sos>
             enc_len = int(data[idx][1][ikey][iaxis]["shape"][0])
+            is_mt = "token" in data[idx][1][ikey][iaxis].keys()
+            # for ASR/ST
+            if not is_mt:
+                enc_len //= 4  # /4 for subsampling in CNN
             xtokens, ytokens = None, None
             if "encoder" in name:
                 att_w = att_w[:, :enc_len, :enc_len]
                 # for MT
-                if "token" in data[idx][1][ikey][iaxis].keys():
+                if is_mt:
                     xtokens = data[idx][1][ikey][iaxis]["token"].split()
                     ytokens = xtokens[:]
             elif "decoder" in name:
                 if "self" in name:
-                    att_w = att_w[:, : dec_len + 1, : dec_len + 1]  # +1 for <sos>
+                    # self-attention
+                    att_w = att_w[:, :dec_len, :dec_len]
+                    if "token" in data[idx][1][okey][oaxis].keys():
+                        ytokens = data[idx][1][okey][oaxis]["token"].split() + ["<eos>"]
+                        xtokens = ["<sos>"] + data[idx][1][okey][oaxis]["token"].split()
                 else:
-                    att_w = att_w[:, : dec_len + 1, :enc_len]  # +1 for <sos>
+                    # cross-attention
+                    att_w = att_w[:, :dec_len, :enc_len]
+                    if "token" in data[idx][1][okey][oaxis].keys():
+                        ytokens = data[idx][1][okey][oaxis]["token"].split() + ["<eos>"]
                     # for MT
-                    if "token" in data[idx][1][ikey][iaxis].keys():
+                    if is_mt:
                         xtokens = data[idx][1][ikey][iaxis]["token"].split()
-                # for ASR/ST/MT
-                if "token" in data[idx][1][okey][oaxis].keys():
-                    ytokens = ["<sos>"] + data[idx][1][okey][oaxis]["token"].split()
-                    if "self" in name:
-                        xtokens = ytokens[:]
             else:
                 logging.warning("unknown name for shaping attention")
             fig = _plot_and_save_attention(att_w, filename, xtokens, ytokens)
