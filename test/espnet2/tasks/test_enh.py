b18cd4ed60f5cabb08d6b0134722e53429438159
shincling <shijing609@163.com>
2020-07-21 21:55:31 -0400

    Change the unit test for enhancement task.

--- test/espnet2/tasks/test_enh.py
+++ test/espnet2/tasks/test_enh.py
@@ -1,36 +1,36 @@
 import pytest
 
-from espnet2.tasks.frontend import FrontendTask
+from espnet2.tasks.enh import EnhancementTask
 
 
 def test_add_arguments():
-    FrontendTask.get_parser()
+    EnhancementTask.get_parser()
 
 
 def test_add_arguments_help():
-    parser = FrontendTask.get_parser()
+    parser = EnhancementTask.get_parser()
     with pytest.raises(SystemExit):
         parser.parse_args(["--help"])
 
 
 def test_main_help():
     with pytest.raises(SystemExit):
-        FrontendTask.main(cmd=["--help"])
+        EnhancementTask.main(cmd=["--help"])
 
 
 def test_main_print_config():
     with pytest.raises(SystemExit):
-        FrontendTask.main(cmd=["--print_config"])
+        EnhancementTask.main(cmd=["--print_config"])
 
 
 def test_main_with_no_args():
     with pytest.raises(SystemExit):
-        FrontendTask.main(cmd=[])
+        EnhancementTask.main(cmd=[])
 
 
 def test_print_config_and_load_it(tmp_path):
     config_file = tmp_path / "config.yaml"
     with config_file.open("w") as f:
-        FrontendTask.print_config(f)
-    parser = FrontendTask.get_parser()
+        EnhancementTask.print_config(f)
+    parser = EnhancementTask.get_parser()
     parser.parse_args(["--config", str(config_file)])
