93b1a8200a81cfba08f571d5252038dd11cdcdb9
Takaaki Hori <thori@merl.com>
2020-07-21 22:39:44 -0400

    Fix code to pass the test

--- espnet/nets/scorer_interface.py
+++ espnet/nets/scorer_interface.py
@@ -175,7 +175,8 @@ class BatchPartialScorerInterface(BatchScorerInterface, PartialScorerInterface):
 
         Args:
             ys (torch.Tensor): torch.int64 prefix tokens (n_batch, ylen).
-            next_tokens (torch.Tensor): torch.int64 next token to score (n_batch, n_tokens)
+            next_tokens (torch.Tensor):
+                torch.int64 next token to score (n_batch, n_tokens).
             states (List[Any]): Scorer states for prefix tokens.
             xs (torch.Tensor):
                 The encoder feature that generates ys (n_batch, xlen, n_feat).
