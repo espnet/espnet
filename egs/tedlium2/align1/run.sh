7c47a8db6e88d4739df442731da3c660b1316efb
Ludwig Kürzinger <ludwig.kuerzinger@tum.de>
2020-08-14 14:31:50 +0200

    CTC segmentation - fix CI warnings

--- egs/tedlium2/align1/run.sh
+++ egs/tedlium2/align1/run.sh
@@ -70,8 +70,7 @@ if [ -z "${dict}" ]; then
         mkdir ${download_dir}/${models}/data/lang_autochar/ -p
         model_config=$(find -L ${download_dir}/${models}/exp/*/results/model.json | head -n 1)
         dict=${download_dir}/${models}/data/lang_autochar/dict.txt
-        cat ${model_config} |
-            ${python} -c 'import json,sys;obj=json.load(sys.stdin);[print(char + " " + str(i + 1)) for i, char in enumerate(obj[2]["char_list"])]' > ${dict}
+        ${python} -c 'import json,sys;obj=json.load(sys.stdin);[print(char + " " + str(i + 1)) for i, char in enumerate(obj[2]["char_list"])]' > ${dict} < ${model_config}
     fi
 fi
 
