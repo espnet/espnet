0919d6fcf6972c481b67b05f4a3d91f0928d2e0a
Amir Hussein <36240131+AmirHussein96@users.noreply.github.com>
2020-08-17 09:15:55 +0400

    Update text_segmenting.py

--- egs/mgb2/asr/local/text_segmenting.py
+++ egs/mgb2/asr/local/text_segmenting.py
@@ -7,6 +7,19 @@ import numpy as np
 import pandas as pd
 import argparse
 
+
+def read_tsv(data_file):
+    text_data = list()
+    infile = open(data_file, encoding='utf-8')
+    for line in infile:
+        if not line.strip():
+            continue
+        text= line.split('\t')
+        text_data.append(text)
+    
+    return text_data
+
+
 def get_split(text, maxlen=200, overlap=50):
     """Segment text data with overlapped window of len maxlen"""
     l_total=[]
@@ -18,7 +31,7 @@ def get_split(text, maxlen=200, overlap=50):
         n=1
     for w in range(n):
         if w == 0:
-            l_partial =text.split()[:maxlen]
+            l_partial = text.split()[:maxlen]
             l_total.append(" ".join(l_partial))
         else:
             l_partial = text.split()[w*den:w*den + maxlen]
@@ -28,20 +41,28 @@ def get_split(text, maxlen=200, overlap=50):
 def get_args():
     # Get some basic command line arguements
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file_dir', help='Directory of text to be segmented', type=str, default='lm_text_clean_bw')
+    parser.add_argument('-f', '--file_dir', help='Directory of text to be segmented', type=str)
+    parser.add_argument('-o', '--out', help='Output file', type=str)
     return parser.parse_args()
 
 
 def main():
 	args = get_args()
-	myfile = pd.read_csv(args.file_dir, header=None, encoding='utf-8')
+	#myfile = pd.read_csv(args.file_dir, header=None, encoding='utf-8', error_bad_lines=False)
+	data = read_tsv(args.file_dir)
+	myfile = pd.DataFrame(data=data)
+	nan_value = float("NaN")
+	myfile.replace("", nan_value, inplace=True)
+	myfile.dropna(inplace=True)
+	myfile.reset_index(drop=True, inplace=True)
 
 	segmented_text=[]
 	for i in range(len(myfile)):
 		segmented_text.extend(get_split(myfile.iloc[i][0]))
 		
 	df=pd.DataFrame(segmented_text, index=None)
-	df.to_csv("segmented_text", sep = '\n', header=False, index=False)
+	df.to_csv(args.out, sep = '\n', header=False, index=False)
     
 if __name__ == "__main__":
     main()
+
