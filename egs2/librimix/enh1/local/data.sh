c20d96ea9a48158d96238d19eef9095edac5589b
LiChenda <lichenda1996@sjtu.edu.cn>
2020-09-02 16:32:05 +0800

    add librimix

---
+++ egs2/librimix/enh1/local/data.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# Copyright 2020  Shanghai Jiao Tong University (Authors: Chenda Li, Wangyou Zhang)
+# Apache 2.0
+set -e
+set -u
+set -o pipefail
+
+log() {
+  local fname=${BASH_SOURCE[1]##*/}
+  echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
+}
+
+help_message=$(cat << EOF
+Usage: $0 [--min_or_max <min/max>] [--sample_rate <8k/16k>]
+  optional argument:
+    [--min_or_max]: min (Default), max
+    [--sample_rate]: 8k (Default), 16k
+EOF
+)
+
+. ./db.sh
+
+
+min_or_max=max
+sample_rate=16k
+
+. utils/parse_options.sh
+
+train_set="tr_"${min_or_max}_${sample_rate}
+train_dev="cv_"${min_or_max}_${sample_rate}
+recog_set="tt_"${min_or_max}_${sample_rate}
+
+cdir=`pwd`
+
+
+git clone https://github.com/JorisCos/LibriMix ./data/LibriMix
+
+
+# the simulation program will write data to wham_noie, so copy it to user directory in case of permission issues
+rsync -r -P ${WHAM} ${cdir}/data/wham_noise
+
+# false &&
+  {
+cd ./data/LibriMix
+librimix_outdir=./libri_mix_single
+
+
+python scripts/augment_train_noise.py --wham_dir ${cdir}/data/wham_noise
+for n_src in 2; 
+do
+  metadata_dir=metadata/Libri$n_src"Mix"
+  python scripts/create_librimix_from_metadata.py --librispeech_dir $LIBRISPEECH \
+    --wham_dir ${cdir}/data/wham_noise \
+    --metadata_dir $metadata_dir \
+    --librimix_outdir $librimix_outdir \
+    --n_src $n_src \
+    --freqs 8k 16k \
+    --modes min max \
+    --types mix_clean mix_both mix_single
+    # --types mix_single
+  done
+
+  cd ../..
+}
+
+exit
+
+mkdir -p data/dev
+mkdir -p data/test
+mkdir -p data/train
+cat data/LibriMix/libri_mix/Libri2Mix/wav${sample_rate}/${min_or_max}/metadata/mixture_dev_mix_both.csv | grep -v mixture_ID | sort -u | awk -F ',' '{print $1, $2}' > data/dev/wav.scp
+cat data/LibriMix/libri_mix/Libri2Mix/wav${sample_rate}/${min_or_max}/metadata/mixture_dev_mix_both.csv | grep -v mixture_ID | sort -u | awk -F ',' '{print $1, $3}' > data/dev/spk1.scp
+cat data/LibriMix/libri_mix/Libri2Mix/wav${sample_rate}/${min_or_max}/metadata/mixture_dev_mix_both.csv | grep -v mixture_ID | sort -u | awk -F ',' '{print $1, $4}' > data/dev/spk2.scp
+cat data/LibriMix/libri_mix/Libri2Mix/wav${sample_rate}/${min_or_max}/metadata/mixture_dev_mix_both.csv | grep -v mixture_ID | sort -u | awk -F ',' '{print $1, $5}' > data/dev/noise1.scp
+cat data/LibriMix/libri_mix/Libri2Mix/wav${sample_rate}/${min_or_max}/metadata/mixture_dev_mix_both.csv | grep -v mixture_ID | sort -u | awk -F ',' '{print $1, $1}' > data/dev/utt2spk
+cat data/LibriMix/libri_mix/Libri2Mix/wav${sample_rate}/${min_or_max}/metadata/mixture_dev_mix_both.csv | grep -v mixture_ID | sort -u | awk -F ',' '{print $1, $1}' > data/dev/spk2utt
+
+cat data/LibriMix/libri_mix/Libri2Mix/wav${sample_rate}/${min_or_max}/metadata/mixture_test_mix_both.csv | grep -v mixture_ID | sort -u | awk -F ',' '{print $1, $2}' > data/test/wav.scp
+cat data/LibriMix/libri_mix/Libri2Mix/wav${sample_rate}/${min_or_max}/metadata/mixture_test_mix_both.csv | grep -v mixture_ID | sort -u | awk -F ',' '{print $1, $3}' > data/test/spk1.scp
+cat data/LibriMix/libri_mix/Libri2Mix/wav${sample_rate}/${min_or_max}/metadata/mixture_test_mix_both.csv | grep -v mixture_ID | sort -u | awk -F ',' '{print $1, $4}' > data/test/spk2.scp
+cat data/LibriMix/libri_mix/Libri2Mix/wav${sample_rate}/${min_or_max}/metadata/mixture_test_mix_both.csv | grep -v mixture_ID | sort -u | awk -F ',' '{print $1, $5}' > data/test/noise1.scp
+cat data/LibriMix/libri_mix/Libri2Mix/wav${sample_rate}/${min_or_max}/metadata/mixture_test_mix_both.csv | grep -v mixture_ID | sort -u | awk -F ',' '{print $1, $1}' > data/test/utt2spk
+cat data/LibriMix/libri_mix/Libri2Mix/wav${sample_rate}/${min_or_max}/metadata/mixture_test_mix_both.csv | grep -v mixture_ID | sort -u | awk -F ',' '{print $1, $1}' > data/test/spk2utt
+
+cat data/LibriMix/libri_mix/Libri2Mix/wav${sample_rate}/${min_or_max}/metadata/mixture_train-*_mix_both.csv | grep -v mixture_ID | sort -u | awk -F ',' '{print $1, $2}' > data/train/wav.scp
+cat data/LibriMix/libri_mix/Libri2Mix/wav${sample_rate}/${min_or_max}/metadata/mixture_train-*_mix_both.csv | grep -v mixture_ID | sort -u | awk -F ',' '{print $1, $3}' > data/train/spk1.scp
+cat data/LibriMix/libri_mix/Libri2Mix/wav${sample_rate}/${min_or_max}/metadata/mixture_train-*_mix_both.csv | grep -v mixture_ID | sort -u | awk -F ',' '{print $1, $4}' > data/train/spk2.scp
+cat data/LibriMix/libri_mix/Libri2Mix/wav${sample_rate}/${min_or_max}/metadata/mixture_train-*_mix_both.csv | grep -v mixture_ID | sort -u | awk -F ',' '{print $1, $5}' > data/train/noise1.scp
+cat data/LibriMix/libri_mix/Libri2Mix/wav${sample_rate}/${min_or_max}/metadata/mixture_train-*_mix_both.csv | grep -v mixture_ID | sort -u | awk -F ',' '{print $1, $1}' > data/train/utt2spk
+cat data/LibriMix/libri_mix/Libri2Mix/wav${sample_rate}/${min_or_max}/metadata/mixture_train-*_mix_both.csv | grep -v mixture_ID | sort -u | awk -F ',' '{print $1, $1}' > data/train/spk2utt
---
+++ egs2/librimix/enh1/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+min_or_max=max # "min" or "max". This is to determine how the mixtures are generated in local/data.sh.
+sample_rate=16k
+
+
+train_set="train"
+valid_set="dev"
+test_sets="test "
+
+./enh.sh \
+    ----use_noise_ref true \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" \
+    --fs "${sample_rate}" \
+    --lang en \
+    --ngpu 4 \
+    --enh_config ./conf/tuning/train_enh_PSM.yaml \
+    "$@"
