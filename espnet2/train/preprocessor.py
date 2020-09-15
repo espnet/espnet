b061f7c2d36dc678d65baa7b1d8025662ce96321
Tomoki Hayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2020-09-15 10:20:26 +0900

    follow new version black manner

--- espnet2/train/preprocessor.py
+++ espnet2/train/preprocessor.py
@@ -62,7 +62,8 @@ class CommonPreprocessor(AbsPreprocessor):
                 g2p_type=g2p_type,
             )
             self.token_id_converter = TokenIDConverter(
-                token_list=token_list, unk_symbol=unk_symbol,
+                token_list=token_list,
+                unk_symbol=unk_symbol,
             )
         else:
             self.text_cleaner = None
@@ -127,7 +128,8 @@ class CommonPreprocessor_multi(AbsPreprocessor):
                 g2p_type=g2p_type,
             )
             self.token_id_converter = TokenIDConverter(
-                token_list=token_list, unk_symbol=unk_symbol,
+                token_list=token_list,
+                unk_symbol=unk_symbol,
             )
         else:
             self.text_cleaner = None
