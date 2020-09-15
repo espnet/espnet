902a534213cd11958323521f40ad98b144dc8dba
fboyer <florian.boyer@labri.fr>
2020-09-15 12:40:27 +0200

    modify warp-rnnt install and requirements

--- tools/installers/install_warp-rnnt.sh
+++ tools/installers/install_warp-rnnt.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
+
 set -euo pipefail
 
-
-if [ $# != 2 ]; then
-    echo "Usage: $0 <cuda-version> <torch-version>"
-    echo "e.g.: $0 10.0 1.1.0"
+if [ $# != 1 ]; then
+    echo "Usage: $0 <cuda-version>"
+    echo "e.g.: $0 10.0"
     exit 1;
 fi
 
 cuda_ver=$1
-torch_ver=$2
 
-[ $cuda_ver != "10.0" ] && \
-    [ $torch_ver != "1.1.0" ] && \
-    echo "warp-rnnt only tested with CUDA_VERSION=10.0 with TH_VERSION=1.1.0 yet. Skipping install." && \
+if [ $cuda_ver != "10.0" ]; then
+    echo "warp-rnnt was only tested with CUDA_VERSION=10.0. Skipping install."
     exit 0
+fi
 
 # TODO(kamo): Consider clang case
 # Note: Requires gcc>=5.4 to build extensions with pytorch>=1.0
