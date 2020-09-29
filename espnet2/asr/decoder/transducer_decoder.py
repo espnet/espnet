38beda90e54345b1cbbb4b27ee36c89251649cdb
fboyer <florian.boyer@labri.fr>
2020-09-30 00:32:13 +0200

    refactor: use RNNDecoder and JointNetwork + unification

--- espnet2/asr/decoder/transducer_decoder.py
+++ espnet2/asr/decoder/transducer_decoder.py
@@ -1,4 +1,4 @@
-"""RNN-Transducer implementation for training and decoding."""
+"""Transducer decoder implementation."""
 
 import torch
 
@@ -8,15 +8,17 @@ from typing import List
 from typing import Tuple
 from typing import Union
 
-from espnet.nets.beam_search_transducer_espnet2 import Hypothesis
-from espnet.nets.pytorch_backend.nets_utils import get_activation
+from espnet.nets.beam_search_transducer import Hypothesis
 from espnet.nets.pytorch_backend.nets_utils import to_device
+from espnet.nets.pytorch_backend.transducer.joint_network import JointNetwork
 
-from espnet2.asr.decoder.abs_decoder import AbsDecoder
+from espnet2.asr.decoder.rnn_decoder import build_attention_list
+from espnet2.asr.decoder.rnn_decoder import RNNDecoder
+from espnet2.utils.get_default_kwargs import get_default_kwargs
 
 
-class RNNTDecoder(AbsDecoder):
-    """RNN-T Decoder module.
+class TransducerDecoder(RNNDecoder):
+    """Transducer Decoder module.
 
     Args:
         vocab_size: Vocabulary size
@@ -27,9 +29,10 @@ class RNNTDecoder(AbsDecoder):
         hidden_size: Dimension of hidden layers
         embedding_size: Dimension of embedding layer
         dropout: Dropout rate of hidden layers
-        dropout_embedding: Dropout rate of embedding layer
         joint_space_size: Dimension of joint space
         joint_activation_type: Activation type for joint network
+        use_attention: Whether to use attention module
+        att_conf: attention module configuration
 
     """
 
@@ -41,50 +44,39 @@ class RNNTDecoder(AbsDecoder):
         rnn_type: str = "lstm",
         num_layers: int = 1,
         hidden_size: int = 320,
-        embedding_size: int = 320,
         dropout: float = 0.0,
-        dropout_embedding: float = 0.0,
         joint_space_size: int = 320,
         joint_activation_type: str = "tanh",
+        use_attention: bool = False,
+        att_conf: dict = get_default_kwargs(build_attention_list),
     ):
         """Transducer initializer."""
         assert check_argument_types()
 
-        if rnn_type not in {"lstm", "gru"}:
-            raise ValueError(f"Not supported: rnn_type={rnn_type}")
+        super().__init__(
+            vocab_size,
+            encoder_output_size,
+            rnn_type=rnn_type,
+            num_layers=num_layers,
+            hidden_size=hidden_size,
+            embed_pad=blank,
+            dropout=dropout,
+            att_conf=att_conf,
+            is_rnnt=not use_attention,
+        )
 
-        super().__init__()
-
-        self.embed = torch.nn.Embedding(vocab_size, embedding_size, padding_idx=blank)
-        self.dropout_embed = torch.nn.Dropout(p=dropout_embedding)
-
-        if rnn_type == "lstm":
-            dec_net = torch.nn.LSTMCell
-        else:
-            dec_net = torch.nn.GRUCell
-
-        self.decoder = torch.nn.ModuleList([dec_net(embedding_size, hidden_size)])
-        self.dropout_dec = torch.nn.ModuleList([torch.nn.Dropout(p=dropout)])
-
-        for _ in range(1, num_layers):
-            self.decoder += [dec_net(hidden_size, hidden_size)]
-            self.dropout_dec += [torch.nn.Dropout(p=dropout)]
-
-        self.lin_enc = torch.nn.Linear(encoder_output_size, joint_space_size)
-        self.lin_dec = torch.nn.Linear(hidden_size, joint_space_size, bias=False)
-        self.lin_out = torch.nn.Linear(joint_space_size, vocab_size)
-
-        self.joint_activation = get_activation(joint_activation_type)
-
-        self.num_layers = num_layers
-        self.hidden_size = hidden_size
-        self.rnn_type = rnn_type
-        self.embedding_size = embedding_size
-        self.joint_space_size = joint_space_size
-        self.vocab_size = vocab_size
+        self.joint_network = JointNetwork(
+            vocab_size,
+            encoder_output_size,
+            hidden_size,
+            joint_space_size,
+            joint_activation_type,
+        )
 
         self.ignore_id = -1
         self.blank = blank
+
+        self.use_attention = use_attention
 
     def init_state(self, init_tensor: torch.Tensor) -> torch.Tensor:
         """Initialize decoder states.
@@ -97,15 +89,18 @@ class RNNTDecoder(AbsDecoder):
 
         """
         z_list = [
-            to_device(init_tensor, torch.zeros(init_tensor.size(0), self.hidden_size))
-            for _ in range(self.num_layers)
+            to_device(init_tensor, torch.zeros(init_tensor.size(0), self.dunits))
+            for _ in range(self.dlayers)
         ]
         c_list = [
-            to_device(init_tensor, torch.zeros(init_tensor.size(0), self.hidden_size))
-            for _ in range(self.num_layers)
+            to_device(init_tensor, torch.zeros(init_tensor.size(0), self.dunits))
+            for _ in range(self.dlayers)
         ]
 
-        return (z_list, c_list)
+        if self.use_attention:
+            return ((z_list, c_list), None)
+        else:
+            return (z_list, c_list)
 
     def rnn_forward(
         self, ey: torch.Tensor, state: Tuple[List, List]
@@ -122,41 +117,19 @@ class RNNTDecoder(AbsDecoder):
 
         """
         z_prev, c_prev = state
-        z_list, c_list = self.init_state(ey)
 
-        if self.rnn_type == "lstm":
-            z_list[0], c_list[0] = self.decoder[0](ey, (z_prev[0], c_prev[0]))
-
-            for i in range(1, self.num_layers):
-                z_list[i], c_list[i] = self.decoder[i](
-                    self.dropout_dec[i - 1](z_list[i - 1]), (z_prev[i], c_prev[i])
-                )
+        if self.use_attention:
+            (z_list, c_list), _ = self.init_state(ey)
         else:
-            z_list[0] = self.decoder[0](ey, z_prev[0])
+            z_list, c_list = self.init_state(ey)
 
-            for i in range(1, self.num_layers):
-                z_list[i] = self.decoder[i](
-                    self.dropout_dec[i - 1](z_list[i - 1]), z_prev[i]
-                )
+        z_list, c_list = RNNDecoder.rnn_forward(
+            self, ey, z_list, c_list, z_prev, c_prev
+        )
+
         y = self.dropout_dec[-1](z_list[-1])
 
         return y, (z_list, c_list)
-
-    def joint(self, h_enc: torch.Tensor, h_dec: torch.Tensor) -> torch.Tensor:
-        """Joint computation of z.
-
-        Args:
-            h_enc: Batch of expanded hidden state (B, T, 1, D_enc)
-            h_dec: Batch of expanded hidden state (B, 1, U, D_dec)
-
-        Returns:
-            z: Output (B, T, U, vocab_size)
-
-        """
-        z = self.joint_activation(self.lin_enc(h_enc) + self.lin_dec(h_dec))
-        z = self.lin_out(z)
-
-        return z
 
     def forward(self, hs_pad, ys_in_pad, hlens=None):
         """Forward function for transducer.
@@ -171,20 +144,35 @@ class RNNTDecoder(AbsDecoder):
         """
         olength = ys_in_pad.size(1)
 
-        state = self.init_state(hs_pad)
-        eys = self.dropout_embed(self.embed(ys_in_pad))
+        eys = self.dropout_emb(self.embed(ys_in_pad))
 
-        z_all = []
-        for i in range(olength):
-            y, state = self.rnn_forward(eys[:, i, :], state)
-            z_all.append(y)
+        if self.use_attention:
+            self.att_list[0].reset()
 
-        h_enc = hs_pad.unsqueeze(2)
+            hlens = list(map(int, hlens))
+            state, att_w = self.init_state(hs_pad)
+
+            z_all = []
+            for i in range(olength):
+                att_c, att_w = self.att_list[0](
+                    hs_pad, hlens, self.dropout_dec[0](state[0][0]), att_w
+                )
+
+                ey = torch.cat((eys[:, i, :], att_c), dim=1)
+
+                y, state = self.rnn_forward(ey, state)
+                z_all.append(y)
+        else:
+            state = self.init_state(hs_pad)
+
+            z_all = []
+            for i in range(olength):
+                y, state = self.rnn_forward(eys[:, i, :], state)
+                z_all.append(y)
 
         h_dec = torch.stack(z_all, dim=1)
-        h_dec = h_dec.unsqueeze(1)
 
-        z = self.joint(h_enc, h_dec)
+        z = self.joint_network(hs_pad.unsqueeze(2), h_dec.unsqueeze(1))
 
         return z
 
@@ -212,7 +200,21 @@ class RNNTDecoder(AbsDecoder):
         else:
             ey = self.embed(vy)
 
-            y, state = self.rnn_forward(ey[0], hyp.dec_state)
+            if self.use_attention:
+                att_c, att_w = self.att_list[0](
+                    init_tensor,
+                    [init_tensor.size(1)],
+                    hyp.dec_state[0][0][0],
+                    hyp.dec_state[1],
+                )
+
+                ey = torch.cat((ey[0], att_c), dim=1)
+
+                y, dec_state = self.rnn_forward(ey, hyp.dec_state[0])
+                state = (dec_state, att_w)
+            else:
+                y, state = self.rnn_forward(ey[0], hyp.dec_state)
+
             cache[str_yseq] = (y, state)
 
         return y, state, vy[0]
@@ -259,17 +261,37 @@ class RNNTDecoder(AbsDecoder):
 
             tokens = to_device(self, torch.LongTensor(tokens).view(batch))
 
-            dec_state = self.init_state(torch.zeros((batch, self.hidden_size)))
-            dec_state = self.create_batch_states(dec_state, [p[1] for p in process])
+            if self.use_attention:
+                state = self.init_state(init_tensor)
+                dec_state = self.create_batch_states(state, [p[1] for p in process])
 
-            ey = self.embed(tokens)
+                ey = self.embed(tokens)
 
-            y, dec_state = self.rnn_forward(ey, dec_state)
+                enc_hs = init_tensor.expand(batch, -1, -1)
+                enc_len = [init_tensor.squeeze(0).size(0)] * batch
+
+                att_c, att_w = self.att_list[0](
+                    enc_hs, enc_len, state[0][0][0], state[1]
+                )
+
+                ey = torch.cat((ey, att_c), dim=1)
+
+                y, dec_state = self.rnn_forward(ey, state[0])
+            else:
+                dec_state = self.init_state(torch.zeros((batch, self.dunits)))
+                dec_state = self.create_batch_states(dec_state, [p[1] for p in process])
+
+                ey = self.embed(tokens)
+
+                y, dec_state = self.rnn_forward(ey, dec_state)
 
         j = 0
         for i in range(final_batch):
             if done[i] is None:
-                new_state = self.select_state(dec_state, j)
+                if self.use_attention:
+                    new_state = self.select_state((dec_state, att_w), j)
+                else:
+                    new_state = self.select_state(dec_state, j)
 
                 done[i] = (y[j], new_state)
                 cache[process[j][0]] = (y[j], new_state)
@@ -300,10 +322,16 @@ class RNNTDecoder(AbsDecoder):
                 ([L x (1, D_dec)], [L x (1, D_dec)])
 
         """
-        z_list = [batch_states[0][layer][idx] for layer in range(self.num_layers)]
-        c_list = [batch_states[1][layer][idx] for layer in range(self.num_layers)]
+        z_list = [batch_states[0][layer][idx] for layer in range(self.dlayers)]
+        c_list = [batch_states[1][layer][idx] for layer in range(self.dlayers)]
 
-        return (z_list, c_list)
+        if self.use_attention:
+            att_state = (
+                batch_states[1][idx] if batch_states[1] is not None else batch_states[1]
+            )
+            return ((z_list, c_list), att_state)
+        else:
+            return (z_list, c_list)
 
     def create_batch_states(
         self,
@@ -324,8 +352,17 @@ class RNNTDecoder(AbsDecoder):
                 ([L x (B, D_dec)], [L x (B, D_dec)])
 
         """
-        for layer in range(self.num_layers):
+        for layer in range(self.dlayers):
             batch_states[0][layer] = torch.stack([s[0][layer] for s in l_states])
             batch_states[1][layer] = torch.stack([s[1][layer] for s in l_states])
 
-        return batch_states
+        if self.use_attention:
+            att_states = (
+                torch.stack([s[1] for s in l_states])
+                if l_states[0][1] is not None
+                else None
+            )
+
+            return (batch_states[0], att_states)
+        else:
+            return batch_states
