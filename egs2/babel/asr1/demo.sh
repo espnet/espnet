b5f27fb82daf83c799c5205bff418d67fa76e6a4
Brian <brianyan918@gmail.com>
2020-09-29 01:01:19 -0400

    amharic demo files

---
+++ egs2/babel/asr1/demo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+. ./path.sh
+
+python3 -m espnet2.bin.asr_inference \
+	--ngpu "0" \
+	--data_path_and_name_and_type "demo/demo.scp,speech,sound" \
+	--key_file "demo/demo.scp" \
+	--asr_train_config "exp/asr_train_asr_raw_char/config.yaml" \
+	--asr_model_file "exp/asr_train_asr_raw_char/67epoch.pth" \
+	--output_dir "demo/transcribed" \
+	--config "conf/decode_asr.yaml" \
+	--lm_train_config "exp/lm_train_lm_char/config.yaml" \
+	--lm_file "exp/lm_train_lm_char/20epoch.pth"
