d3a4b6279f99856d6102dc11d292664819ff93a5
Brian <brianyan918@gmail.com>
2020-09-29 23:50:59 -0400

    fix model path

--- egs2/babel/asr1/demo.sh
+++ egs2/babel/asr1/demo.sh
@@ -6,7 +6,7 @@ python3 -m espnet2.bin.asr_inference \
 	--data_path_and_name_and_type "demo/demo.scp,speech,sound" \
 	--key_file "demo/demo.scp" \
 	--asr_train_config "exp/asr_train_asr_raw_char/config.yaml" \
-	--asr_model_file "exp/asr_train_asr_raw_char/valid.acc.ave.pth" \
+	--asr_model_file "exp/asr_train_asr_raw_char/43epoch.pth" \
 	--output_dir "demo/transcribed" \
 	--config "conf/decoder_asr.yaml" \
 	--lm_train_config "exp/lm_train_lm_char/config.yaml" \
