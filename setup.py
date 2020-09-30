c1e81983f035923fc0b47a266ec73a292f3e0c43
Tomoki Hayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2020-09-30 19:08:02 +0900

    🎉 v.0.9.4 release

--- setup.py
+++ setup.py
@@ -119,7 +119,7 @@ extras_require = {
 dirname = os.path.dirname(__file__)
 setup(
     name="espnet",
-    version="0.9.3",
+    version="0.9.4",
     url="http://github.com/espnet/espnet",
     author="Shinji Watanabe",
     author_email="shinjiw@ieee.org",
