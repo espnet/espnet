33ca66ad185645451bf5defde92d58b73412b10d
Brian <brianyan918@gmail.com>
2020-09-29 23:30:46 -0400

    change demo model

--- egs2/babel/asr1/demo.sh
+++ egs2/babel/asr1/demo.sh
@@ -6,8 +6,8 @@ python3 -m espnet2.bin.asr_inference \
 	--data_path_and_name_and_type "demo/demo.scp,speech,sound" \
 	--key_file "demo/demo.scp" \
 	--asr_train_config "exp/asr_train_asr_raw_char/config.yaml" \
-	--asr_model_file "exp/asr_train_asr_raw_char/67epoch.pth" \
+	--asr_model_file "exp/asr_train_asr_raw_char/valid.acc.ave.pth" \
 	--output_dir "demo/transcribed" \
-	--config "conf/decode_asr.yaml" \
+	--config "conf/decoder_asr.yaml" \
 	--lm_train_config "exp/lm_train_lm_char/config.yaml" \
 	--lm_file "exp/lm_train_lm_char/20epoch.pth"
