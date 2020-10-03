2d826676e86cc725a22a6f651c6b0f590a391cb9
fboyer <florian.boyer@labri.fr>
2020-10-03 12:10:45 +0200

    fix default run script

--- egs2/vivos/asr1/run.sh
+++ egs2/vivos/asr1/run.sh
@@ -10,24 +10,26 @@ test_sets="train_dev test"
 
 asr_config=conf/train_asr.yaml
 inference_config=conf/decode.yaml
+inference_asr_model=valid.loss.best.pth
 
 lm_config=conf/train_lm_char.yaml
 use_lm=true
 use_wordlm=false
 word_vocab_size=7184
 
-./asr.sh                                        \
-    --lang vi                                   \
-    --audio_format wav                          \
-    --feats_type raw                            \
-    --token_type char                           \
-    --use_lm ${use_lm}                          \
-    --use_word_lm ${use_wordlm}                 \
-    --word_vocab_size ${word_vocab_size}        \
-    --lm_config "${lm_config}"                  \
-    --asr_config "${asr_config}"                \
-    --inference_config "${inference_config}"          \
-    --train_set "${train_set}"                  \
-    --valid_set "${valid_set}"                  \
-    --test_sets "${test_sets}"                  \
+./asr.sh                                                \
+    --lang vi                                           \
+    --audio_format wav                                  \
+    --feats_type raw                                    \
+    --token_type char                                   \
+    --use_lm ${use_lm}                                  \
+    --use_word_lm ${use_wordlm}                         \
+    --word_vocab_size ${word_vocab_size}                \
+    --lm_config "${lm_config}"                          \
+    --asr_config "${asr_config}"                        \
+    --inference_config "${inference_config}"            \
+    --inference_asr_model "${inference_asr_model}"      \
+    --train_set "${train_set}"                          \
+    --valid_set "${valid_set}"                          \
+    --test_sets "${test_sets}"                          \
     --srctexts "data/${train_set}/text" "$@"
