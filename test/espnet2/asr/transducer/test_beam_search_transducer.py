d3b46cbaf48f0350487c6b5328efb17e98e76a9b
fboyer <florian.boyer@labri.fr>
2020-10-04 13:06:09 +0200

    add test for transducer beam search w/ LM

--- test/espnet2/asr/transducer/test_beam_search_transducer.py
+++ test/espnet2/asr/transducer/test_beam_search_transducer.py
@@ -4,6 +4,7 @@ import torch
 from espnet.nets.pytorch_backend.transducer.joint_network import JointNetwork
 from espnet2.asr.decoder.rnn_decoder import RNNDecoder
 from espnet2.asr.transducer.beam_search_transducer import BeamSearchTransducer
+from espnet2.lm.seq_rnn_lm import SequentialRNNLM
 
 
 @pytest.mark.parametrize("rnn_type", ["lstm", "gru"])
@@ -14,11 +15,11 @@ from espnet2.asr.transducer.beam_search_transducer import BeamSearchTransducer
 def test_transducer_decoder_beam_search(
     rnn_type, search_type, use_attention, beam_size, nstep
 ):
-    encoder_output_size = 4
+    encoder = torch.randn(10, 4)
 
     decoder = RNNDecoder(
         10,
-        encoder_output_size,
+        4,
         hidden_size=12,
         embed_pad=0,
         rnn_type=rnn_type,
@@ -26,9 +27,7 @@ def test_transducer_decoder_beam_search(
         use_attention=use_attention,
         use_output=False,
     )
-    joint_net = JointNetwork(
-        10, encoder_output_size, decoder.dunits, joint_space_size=20
-    )
+    joint_net = JointNetwork(10, 4, decoder.dunits, joint_space_size=20)
 
     beam_search = BeamSearchTransducer(
         decoder=decoder,
@@ -44,6 +43,41 @@ def test_transducer_decoder_beam_search(
         score_norm=False,
     )
 
-    enc = torch.randn(10, encoder_output_size)
     with torch.no_grad():
-        beam_search(enc)
+        beam_search(encoder)
+
+
+@pytest.mark.parametrize("rnn_type", ["lstm", "gru"])
+@pytest.mark.parametrize("search_type", ["default", "alsd", "nsc", "tsd"])
+def test_transducer_beam_search_with_lm(rnn_type, search_type):
+    encoder = torch.randn(10, 4)
+
+    decoder = RNNDecoder(
+        10,
+        4,
+        hidden_size=12,
+        embed_pad=0,
+        rnn_type=rnn_type,
+        num_layers=1,
+        use_attention=False,
+        use_output=False,
+    )
+    joint_net = JointNetwork(10, 4, decoder.dunits, joint_space_size=20)
+
+    lm_model = SequentialRNNLM(10, rnn_type=rnn_type)
+
+    beam_search = BeamSearchTransducer(
+        decoder=decoder,
+        joint_network=joint_net,
+        beam_size=2,
+        lm=lm_model,
+        search_type=search_type,
+        max_sym_exp=2,
+        u_max=10,
+        nstep=2,
+        prefix_alpha=2,
+        score_norm=True,
+    )
+
+    with torch.no_grad():
+        beam_search(encoder)
