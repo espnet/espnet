ef41d0475804f74f6b8b0478923b0ec9844bb670
jubang0219 <jubang0219@gmail.com>
2020-10-05 13:33:59 +0900

    Initial commit

--- egs/ksponspeech/asr1/run.sh
+++ egs/ksponspeech/asr1/run.sh
@@ -9,7 +9,7 @@
 
 # general configuration
 backend=pytorch
-stage=0       # start from -1 if you need to start from data download
+stage=0       
 stop_stage=100
 ngpu=4         # number of gpus ("0" uses cpu, otherwise use gpu)
 nj=32
