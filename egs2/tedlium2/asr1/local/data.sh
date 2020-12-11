677adc03590aa356787b216282b07d62bdcd783d
flylili001 <flylili001@163.com>
2020-12-11 16:44:10 +0800

    submit egs2/tedlium2/asr1

---
+++ egs2/tedlium2/asr1/local/data.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copyright 2017 Johns Hopkins University (Shinji Watanabe)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+# Modified by flylili001
+
+. ./path.sh || exit 1;
+. ./cmd.sh || exit 1;
+
+# general configuration
+stage=-1       # start from -1 if you need to start from data download
+stop_stage=100
+
+if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
+    echo "stage -1: Data Download"
+    local/download_data.sh
+fi
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+    ### Task dependent. You have to make data the following preparation part by yourself.
+    ### But you can utilize Kaldi recipes in most cases
+    echo "stage 0: Data preparation"
+    local/prepare_data.sh
+    for dset in dev test train; do
+    utils/data/modify_speaker_info.sh --seconds-per-spk-max 180 data/${dset}.orig data/${dset}
+    done
+fi
+
+echo "Finished"
---
+++ egs2/tedlium2/asr1/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+train_set="train"
+valid_set="dev"
+test_sets="test dev"
+
+asr_config=conf/train_asr_transformer.yaml
+lm_config=conf/train_lm_adam.yaml
+inference_config=conf/decode_asr.yaml
+
+stage=10
+./asr.sh \
+    --stage "${stage}" \
+    --lang en \
+    --ngpu 8 \
+    --nbpe 500 \
+    --max_wav_duration 20 \
+    --feats_type fbank_pitch \
+    --speed_perturb_factors "0.9 1.0 1.1" \
+    --asr_config "${asr_config}" \
+    --lm_config "${lm_config}" \
+    --inference_config "${inference_config}" \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" \
+    --srctexts "data/${train_set}/text" "$@"
