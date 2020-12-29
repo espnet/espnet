7180dc9db4ad7ac9c03706f272f85aa987d4d6f6
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2020-12-29 10:23:00 +0900

    fixed option

--- egs2/libritts/tts1/local/data.sh
+++ egs2/libritts/tts1/local/data.sh
@@ -77,7 +77,7 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
             cwd=$(pwd)
             cd "${db_root}/LibriTTS/${name}"
             find . -follow -name "*.normalized.txt" -print0 \
-                | tar cv -T - --null f - | tar xvf - -C "${cwd}/data/local/${name}"
+                | tar c -T - --null -f - | tar xf - -C "${cwd}/data/local/${name}"
             cd "${cwd}"
 
             # Create kaldi data directory with the trimed audio
