db07aa25d73e28b8757bf0829419d03eca2fc3e9
houwenxin <641686859@qq.com>
2021-01-02 05:19:57 +0000

    batch decoding test

--- test/test_e2e_asr_transformer.py
+++ test/test_e2e_asr_transformer.py
@@ -221,6 +221,10 @@ def test_transformer_trainable_and_decodable(module, model_dict):
             nbest = model.recognize(x[0, : ilens[0]].numpy(), recog_args)
             print(y[0])
             print(nbest[0]["yseq"][1:-1])
+            nbests = model.recognize_batch(x, recog_args)  # batch decodable
+            for idx, nbest in enumerate(nbests):
+                print(y[idx])
+                print(nbest[idx][0]["yseq"][1:-1])
     else:
         # test trainable
         optim = chainer.optimizers.Adam(0.01)
