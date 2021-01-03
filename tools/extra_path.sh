676b993a3d7b490b6425d4af70be4b081206c508
jumon <jumon.nozaki@gmail.com>
2021-01-03 15:06:06 +0900

    fix the wrong path setting

--- tools/extra_path.sh
+++ tools/extra_path.sh
@@ -18,7 +18,7 @@ fi
 export PATH="${TOOL_DIR}"/sph2pipe_v2.5:"${PATH:-}"
 export PATH="${TOOL_DIR}"/sctk-2.4.10/bin:"${PATH:-}"
 export PATH="${TOOL_DIR}"/mwerSegmenter:"${PATH:-}"
-export PATH="${TOOL_DIR}"/moses/scripts/tokenizer:"${TOOL_DIR}"/moses/scripts/generic:"${TOOL_DIR}"/tools/moses/scripts/recaser/:"${TOOL_DIR}"/moses/scripts/training/"${PATH:-}"
+export PATH="${TOOL_DIR}"/moses/scripts/tokenizer:"${TOOL_DIR}"/moses/scripts/generic:"${TOOL_DIR}"/tools/moses/scripts/recaser:"${TOOL_DIR}"/moses/scripts/training:"${PATH:-}"
 export PATH="${TOOL_DIR}"/nkf/nkf-2.1.4:"${PATH:-}"
 export PATH="${TOOL_DIR}"/PESQ/P862_annex_A_2005_CD/source:"${PATH:-}"
 export PATH="${TOOL_DIR}"/kenlm/build/bin:"${PATH:-}"
