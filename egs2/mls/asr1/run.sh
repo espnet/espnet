f81fd12760d1c07e4774648756405d7495dc9288
ftshijt <728307998@qq.com>
2021-01-22 12:14:26 -0500

    update final

--- egs2/mls/asr1/run.sh
+++ egs2/mls/asr1/run.sh
@@ -20,14 +20,15 @@ ngpu=1
 
 ./asr.sh \
     --local_data_opts "--lang ${lang}" \
-    --stage 7 \
-    --stop_stage 12 \
+    --stage 1 \
+    --stop_stage 100 \
     --nj 40 \
     --ngpu ${ngpu} \
     --use_lm true \
     --token_type bpe \
     --nbpe 150 \
     --feats_type raw \
+    --asr_tag transformer \
     --asr_config "${asr_config}" \
     --inference_config "${inference_config}" \
     --train_set "${train_set}" \
