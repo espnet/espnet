2d705dc6060a210b48f0690c8ecb831579151c4e
Shinji Watanabe <sw005320@gmail.com>
2021-01-27 17:47:01 -0500

    fix bash bugs and change some configs

--- egs2/open_li52/asr1/run.sh
+++ egs2/open_li52/asr1/run.sh
@@ -6,7 +6,7 @@ set -u
 set -o pipefail
 
 langs="ar_as_br_ca_cnh_cs_cv_cy_de_dv\
-_el_eo_es_et_eu_fa_fr_fy-NL_ga-IE_hsb\
+_el_en_eo_es_et_eu_fa_fr_fy-NL_ga-IE_hsb\
 _ia_id_it_ja_ka_kab_ky_lv_mn_mt_nl_or\
 _pa-IN_pl_pt_rm-sursilv_rm-vallader_ro\
 _ru_rw_sah_sl_sv-SE_ta_tr_tt_uk_vi_\
@@ -37,25 +37,25 @@ train_dev=dev_li52_lid
 #  test_vi_commonvoice test_zh_CN_commonvoice test_zh_HK_commonvoice\
 #  test_zh_TW_commonvoice"
 
-test_set="test_ar_commonvoice_lid test_as_commonvoice_lid test_br_commonvoice_lid\ 
-test_ca_commonvoice_lid test_cnh_commonvoice_lid test_cs_commonvoice_lid\ 
-test_cv_commonvoice_lid test_cy_commonvoice_lid test_de_commonvoice_lid\ 
-test_de_voxforge_lid test_dv_commonvoice_lid test_el_commonvoice_lid\ 
-test_en_commonvoice_lid test_en_voxforge_lid test_eo_commonvoice_lid\ 
-test_es_commonvoice_lid test_es_voxforge_lid test_et_commonvoice_lid\ 
-test_eu_commonvoice_lid test_fa_commonvoice_lid test_fr_commonvoice_lid\ 
-test_fr_voxforge_lid test_fy_NL_commonvoice_lid test_ga_IE_commonvoice_lid\ 
-test_hsb_commonvoice_lid test_ia_commonvoice_lid test_id_commonvoice_lid\ 
-test_it_commonvoice_lid test_it_voxforge_lid test_ja_commonvoice_lid\ 
-test_ka_commonvoice_lid test_kab_commonvoice_lid test_ky_commonvoice_lid\ 
-test_lv_commonvoice_lid test_mn_commonvoice_lid test_mt_commonvoice_lid\ 
-test_nl_commonvoice_lid test_nl_voxforge_lid test_or_commonvoice_lid\ 
-test_pa_IN_commonvoice_lid test_pl_commonvoice_lid test_pt_commonvoice_lid\ 
-test_pt_voxforge_lid test_rm_sursilv_commonvoice_lid test_rm_vallader_commonvoice_lid\ 
-test_ro_commonvoice_lid test_ru_commonvoice_lid test_ru_voxforge_lid\ 
-test_rw_commonvoice_lid test_sah_commonvoice_lid test_sl_commonvoice_lid\ 
-test_sv_SE_commonvoice_lid test_ta_commonvoice_lid test_tr_commonvoice_lid\ 
-test_tt_commonvoice_lid test_uk_commonvoice_lid test_vi_commonvoice_lid\ 
+test_set="test_ar_commonvoice_lid test_as_commonvoice_lid test_br_commonvoice_lid \
+test_ca_commonvoice_lid test_cnh_commonvoice_lid test_cs_commonvoice_lid \
+test_cv_commonvoice_lid test_cy_commonvoice_lid test_de_commonvoice_lid \
+test_de_voxforge_lid test_dv_commonvoice_lid test_el_commonvoice_lid \
+test_en_commonvoice_lid test_en_voxforge_lid test_eo_commonvoice_lid \
+test_es_commonvoice_lid test_es_voxforge_lid test_et_commonvoice_lid \
+test_eu_commonvoice_lid test_fa_commonvoice_lid test_fr_commonvoice_lid \
+test_fr_voxforge_lid test_fy_NL_commonvoice_lid test_ga_IE_commonvoice_lid \
+test_hsb_commonvoice_lid test_ia_commonvoice_lid test_id_commonvoice_lid \
+test_it_commonvoice_lid test_it_voxforge_lid test_ja_commonvoice_lid \
+test_ka_commonvoice_lid test_kab_commonvoice_lid test_ky_commonvoice_lid \
+test_lv_commonvoice_lid test_mn_commonvoice_lid test_mt_commonvoice_lid \
+test_nl_commonvoice_lid test_nl_voxforge_lid test_or_commonvoice_lid \
+test_pa_IN_commonvoice_lid test_pl_commonvoice_lid test_pt_commonvoice_lid \
+test_pt_voxforge_lid test_rm_sursilv_commonvoice_lid test_rm_vallader_commonvoice_lid \
+test_ro_commonvoice_lid test_ru_commonvoice_lid test_ru_voxforge_lid \
+test_rw_commonvoice_lid test_sah_commonvoice_lid test_sl_commonvoice_lid \
+test_sv_SE_commonvoice_lid test_ta_commonvoice_lid test_tr_commonvoice_lid \
+test_tt_commonvoice_lid test_uk_commonvoice_lid test_vi_commonvoice_lid \
 test_zh_CN_commonvoice_lid test_zh_HK_commonvoice_lid test_zh_TW_commonvoice_lid"
 
 nlsyms_txt=data/local/nlsyms.txt
@@ -69,7 +69,7 @@ ngpu=4
     --local_data_opts "--langs ${langs} --stage 0 --lid ${lid} --nlsyms_txt ${nlsyms_txt}" \
     --stage 1 \
     --stop_stage 100 \
-    --nj 40 \
+    --nj 80 \
     --ngpu ${ngpu} \
     --use_lm false \
     --token_type char \
