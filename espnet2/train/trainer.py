976e6d2714ae51d7e2f8be43444c691463196aed
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2021-02-17 13:10:40 +0000

    Support sharded training of fairscale

--- espnet2/train/trainer.py
+++ espnet2/train/trainer.py
@@ -12,6 +12,7 @@ from typing import List
 from typing import Optional
 from typing import Sequence
 from typing import Tuple
+from typing import Union
 
 import humanfriendly
 import numpy as np
@@ -21,6 +22,7 @@ import torch.optim
 from typeguard import check_argument_types
 
 from espnet2.iterators.abs_iter_factory import AbsIterFactory
+from espnet2.main_funcs.average_nbest_models import average_nbest_models
 from espnet2.main_funcs.calculate_all_attentions import calculate_all_attentions
 from espnet2.schedulers.abs_scheduler import AbsBatchStepScheduler
 from espnet2.schedulers.abs_scheduler import AbsEpochStepScheduler
@@ -59,10 +61,17 @@ else:
 
     GradScaler = None
 
+try:
+    import fairscale
+except ImportError:
+    fairscale = None
+
 
 @dataclasses.dataclass
 class TrainerOptions:
     ngpu: int
+    resume: bool
+    use_amp: bool
     train_dtype: str
     grad_noise: bool
     accum_grad: int
@@ -72,6 +81,16 @@ class TrainerOptions:
     no_forward_run: bool
     use_tensorboard: bool
     use_wandb: bool
+    output_dir: Union[Path, str]
+    max_epoch: int
+    seed: int
+    sharded_ddp: bool
+    patience: Optional[int]
+    keep_nbest_models: Union[int, List[int]]
+    early_stopping_criterion: Sequence[str]
+    best_model_criterion: Sequence[Sequence[str]]
+    val_scheduler_criterion: Sequence[str]
+    unused_parameters: bool
 
 
 class Trainer:
@@ -116,6 +135,35 @@ class Trainer:
         """Reserved for future development of another Trainer"""
         pass
 
+    @staticmethod
+    def resume(
+        checkpoint: Union[str, Path],
+        model: torch.nn.Module,
+        reporter: Reporter,
+        optimizers: Sequence[torch.optim.Optimizer],
+        schedulers: Sequence[Optional[AbsScheduler]],
+        scaler: Optional[GradScaler],
+        ngpu: int = 0,
+    ):
+        states = torch.load(
+            checkpoint,
+            map_location=f"cuda:{torch.cuda.current_device()}" if ngpu > 0 else "cpu",
+        )
+        model.load_state_dict(states["model"])
+        reporter.load_state_dict(states["reporter"])
+        for optimizer, state in zip(optimizers, states["optimizers"]):
+            optimizer.load_state_dict(state)
+        for scheduler, state in zip(schedulers, states["schedulers"]):
+            if scheduler is not None:
+                scheduler.load_state_dict(state)
+        if scaler is not None:
+            if states["scaler"] is None:
+                logging.warning("scaler state is not found")
+            else:
+                scaler.load_state_dict(states["scaler"])
+
+        logging.info(f"The training was resumed using {checkpoint}")
+
     @classmethod
     def run(
         cls,
@@ -125,53 +173,82 @@ class Trainer:
         train_iter_factory: AbsIterFactory,
         valid_iter_factory: AbsIterFactory,
         plot_attention_iter_factory: Optional[AbsIterFactory],
-        reporter: Reporter,
-        scaler: Optional[GradScaler],
-        output_dir: Path,
-        max_epoch: int,
-        seed: int,
-        patience: Optional[int],
-        keep_nbest_models: int,
-        early_stopping_criterion: Sequence[str],
-        best_model_criterion: Sequence[Sequence[str]],
-        val_scheduler_criterion: Sequence[str],
         trainer_options,
         distributed_option: DistributedOption,
-        find_unused_parameters: bool = False,
     ) -> None:
         """Perform training. This method performs the main process of training."""
         assert check_argument_types()
         # NOTE(kamo): Don't check the type more strictly as far trainer_options
         assert is_dataclass(trainer_options), type(trainer_options)
 
+        if isinstance(trainer_options.keep_nbest_models, int):
+            keep_nbest_models = trainer_options.keep_nbest_models
+        else:
+            if len(trainer_options.keep_nbest_models) == 0:
+                logging.warning("No keep_nbest_models is given. Change to [1]")
+                trainer_options.keep_nbest_models = [1]
+            keep_nbest_models = max(trainer_options.keep_nbest_models)
+
+        output_dir = Path(trainer_options.output_dir)
+        reporter = Reporter()
+        if trainer_options.use_amp:
+            if LooseVersion(torch.__version__) < LooseVersion("1.6.0"):
+                raise RuntimeError(
+                    "Require torch>=1.6.0 for  Automatic Mixed Precision"
+                )
+            scaler = GradScaler()
+        else:
+            scaler = None
+
+        if trainer_options.resume and (output_dir / "checkpoint.pth").exists():
+            cls.resume(
+                checkpoint=output_dir / "checkpoint.pth",
+                model=model,
+                optimizers=optimizers,
+                schedulers=schedulers,
+                reporter=reporter,
+                scaler=scaler,
+                ngpu=trainer_options.ngpu,
+            )
+
         start_epoch = reporter.get_epoch() + 1
-        if start_epoch == max_epoch + 1:
+        if start_epoch == trainer_options.max_epoch + 1:
             logging.warning(
                 f"The training has already reached at max_epoch: {start_epoch}"
             )
 
         if distributed_option.distributed:
-            dp_model = torch.nn.parallel.DistributedDataParallel(
-                model,
-                device_ids=(
-                    # Perform multi-Process with multi-GPUs
-                    [torch.cuda.current_device()]
-                    if distributed_option.ngpu == 1
-                    # Perform single-Process with multi-GPUs
-                    else None
-                ),
-                output_device=(
-                    torch.cuda.current_device()
-                    if distributed_option.ngpu == 1
-                    else None
-                ),
-                find_unused_parameters=find_unused_parameters,
-            )
+            if trainer_options.sharded_ddp:
+                if fairscale is None:
+                    raise RuntimeError(
+                        "Requiring fairscale. Do 'pip install fairscale'"
+                    )
+                dp_model = fairscale.nn.data_parallel.ShardedDataParallel(
+                    module=model,
+                    sharded_optimizer=optimizers,
+                )
+            else:
+                dp_model = torch.nn.parallel.DistributedDataParallel(
+                    model,
+                    device_ids=(
+                        # Perform multi-Process with multi-GPUs
+                        [torch.cuda.current_device()]
+                        if distributed_option.ngpu == 1
+                        # Perform single-Process with multi-GPUs
+                        else None
+                    ),
+                    output_device=(
+                        torch.cuda.current_device()
+                        if distributed_option.ngpu == 1
+                        else None
+                    ),
+                    find_unused_parameters=trainer_options.unused_parameters,
+                )
         elif distributed_option.ngpu > 1:
             dp_model = torch.nn.parallel.DataParallel(
                 model,
                 device_ids=list(range(distributed_option.ngpu)),
-                find_unused_parameters=find_unused_parameters,
+                find_unused_parameters=trainer_options.unused_parameters,
             )
         else:
             # NOTE(kamo): DataParallel also should work with ngpu=1,
@@ -186,22 +263,22 @@ class Trainer:
             summary_writer = None
 
         start_time = time.perf_counter()
-        for iepoch in range(start_epoch, max_epoch + 1):
+        for iepoch in range(start_epoch, trainer_options.max_epoch + 1):
             if iepoch != start_epoch:
                 logging.info(
                     "{}/{}epoch started. Estimated time to finish: {}".format(
                         iepoch,
-                        max_epoch,
+                        trainer_options.max_epoch,
                         humanfriendly.format_timespan(
                             (time.perf_counter() - start_time)
                             / (iepoch - start_epoch)
-                            * (max_epoch - iepoch + 1)
+                            * (trainer_options.max_epoch - iepoch + 1)
                         ),
                     )
                 )
             else:
-                logging.info(f"{iepoch}/{max_epoch}epoch started")
-            set_all_random_seed(seed + iepoch)
+                logging.info(f"{iepoch}/{trainer_options.max_epoch}epoch started")
+            set_all_random_seed(trainer_options.seed + iepoch)
 
             reporter.set_epoch(iepoch)
             # 1. Train and validation for one-epoch
@@ -241,7 +318,9 @@ class Trainer:
             # 2. LR Scheduler step
             for scheduler in schedulers:
                 if isinstance(scheduler, AbsValEpochStepScheduler):
-                    scheduler.step(reporter.get_value(*val_scheduler_criterion))
+                    scheduler.step(
+                        reporter.get_value(*trainer_options.val_scheduler_criterion)
+                    )
                 elif isinstance(scheduler, AbsEpochStepScheduler):
                     scheduler.step()
 
@@ -279,7 +358,7 @@ class Trainer:
                 p.symlink_to(f"{iepoch}epoch.pth")
 
                 _improved = []
-                for _phase, k, _mode in best_model_criterion:
+                for _phase, k, _mode in trainer_options.best_model_criterion:
                     # e.g. _phase, k, _mode = "train", "loss", "min"
                     if reporter.has(_phase, k):
                         best_epoch = reporter.get_best_epoch(_phase, k, _mode)
@@ -303,7 +382,7 @@ class Trainer:
                 nbests = set().union(
                     *[
                         set(reporter.sort_epochs(ph, k, m)[:keep_nbest_models])
-                        for ph, k, m in best_model_criterion
+                        for ph, k, m in trainer_options.best_model_criterion
                         if reporter.has(ph, k)
                     ]
                 )
@@ -324,12 +403,25 @@ class Trainer:
                 break
 
             # 8. Check early stopping
-            if patience is not None:
-                if reporter.check_early_stopping(patience, *early_stopping_criterion):
+            if trainer_options.patience is not None:
+                if reporter.check_early_stopping(
+                    trainer_options.patience, *trainer_options.early_stopping_criterion
+                ):
                     break
 
         else:
-            logging.info(f"The training was finished at {max_epoch} epochs ")
+            logging.info(
+                f"The training was finished at {trainer_options.max_epoch} epochs "
+            )
+
+        if not distributed_option.distributed or distributed_option.dist_rank == 0:
+            # Generated n-best averaged model
+            average_nbest_models(
+                reporter=reporter,
+                output_dir=output_dir,
+                best_model_criterion=trainer_options.best_model_criterion,
+                nbest=keep_nbest_models,
+            )
 
     @classmethod
     def train_one_epoch(
