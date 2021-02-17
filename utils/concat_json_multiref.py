e2feb83b0f480675bdbe36004c4ac043ed7043cd
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2021-02-17 19:02:35 +0900

    Add description

--- utils/concat_json_multiref.py
+++ utils/concat_json_multiref.py
@@ -15,6 +15,7 @@ from espnet.utils.cli_utils import get_commandline_args
 
 def get_parser():
     parser = argparse.ArgumentParser(
+        description="concatenate multiple json files for data augmentation",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument("jsons", type=str, nargs="+", help="json files")
