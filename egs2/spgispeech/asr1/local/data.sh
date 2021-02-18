f974f2da3f8d0151622714436d35539430355edc
Shinji Watanabe <sw005320@gmail.com>
2021-02-18 00:23:32 -0500

    first commit for spgispeec

---
+++ egs2/spgispeech/asr1/local/data.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+log() {
+    local fname=${BASH_SOURCE[1]##*/}
+    echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
+}
+SECONDS=0
+
+
+stage=1
+stop_stage=100000
+
+log "$0 $*"
+. utils/parse_options.sh
+
+. ./db.sh
+. ./path.sh
+. ./cmd.sh
+
+
+if [ $# -ne 0 ]; then
+    log "Error: No positional arguments are required."
+    exit 2
+fi
+
+if [ -z "${SPGISPEECH}" ]; then
+    log "Fill the value of 'SPGISPEECH' of db.sh"
+    exit 1
+fi
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    log "stage 2: Data Preparation"
+    # we assume the data structure to be the following
+    # spgispeech
+    # ├── train
+    # ├── train.csv
+    # ├── val
+    # └── val.csv
+    for part in train val; do
+        # use underscore-separated names in data directories.
+        local/data_prep.sh ${part} ${SPGISPEECH} data/${part}
+    done
+fi
+
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+    # make a development set
+    utils/subset_data_dir.sh --first data/train 4000 data/dev_4k
+    n=$(($(wc -l < data/train/text) - 4000))
+    utils/subset_data_dir.sh --last data/train ${n} data/train_nodev
+fi
+
+log "Successfully finished. [elapsed=${SECONDS}s]"
