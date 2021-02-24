cb5159dbb6192f22fb0d63781b814a175d5a2046
Hirofumi Inaguma <hiro.mhbc@gmail.com>
2021-02-24 18:45:39 +0900

    Fix local/remove_punctuation.pl -> remove_punctuation.pl

--- egs/must_c/st1/local/data_prep.sh
+++ egs/must_c/st1/local/data_prep.sh
@@ -101,7 +101,7 @@ for set in train dev tst-COMMON tst-HE; do
         cp ${dst}/${lang}.norm ${dst}/${lang}.norm.tc
 
         # remove punctuation
-        local/remove_punctuation.pl < ${dst}/${lang}.norm.lc > ${dst}/${lang}.norm.lc.rm
+        remove_punctuation.pl < ${dst}/${lang}.norm.lc > ${dst}/${lang}.norm.lc.rm
 
         # tokenization
         tokenizer.perl -l ${lang} -q < ${dst}/${lang}.norm.tc > ${dst}/${lang}.norm.tc.tok
