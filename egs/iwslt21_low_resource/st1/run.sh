8683a01c530e5fc03750a1532356ae37fedfd311
ftshijt <728307998@qq.com>
2021-03-01 14:50:35 -0500

    add data_prep.sh to simplify run.sh

--- egs/iwslt21_low_resource/st1/run.sh
+++ egs/iwslt21_low_resource/st1/run.sh
@@ -90,33 +90,7 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
     python local/data_prep.py ${iwslt_low_resource} ${src_lang} ${tgt_lang}
     for set in train valid; do
         dst=data/${set}.${src_lang}-${tgt_lang}
-        for lang in ${src_lang} ${tgt_lang}; do
-            # normalize punctuation
-            normalize-punctuation.perl -l ${lang} < ${dst}/${lang}.org > ${dst}/${lang}.norm
-
-            # lowercasing
-            lowercase.perl < ${dst}/${lang}.norm > ${dst}/${lang}.norm.lc
-            cp ${dst}/${lang}.norm ${dst}/${lang}.norm.tc
-
-            # remove punctuation
-            remove_punctuation.pl < ${dst}/${lang}.norm.lc > ${dst}/${lang}.norm.lc.rm
-
-            # tokenization
-            # no tokenizer for swa, swc, skip it
-            tokenizer.perl -l ${lang} -q < ${dst}/${lang}.norm.tc > ${dst}/${lang}.norm.tc.tok
-            tokenizer.perl -l ${lang} -q < ${dst}/${lang}.norm.lc > ${dst}/${lang}.norm.lc.tok
-            tokenizer.perl -l ${lang} -q < ${dst}/${lang}.norm.lc.rm > ${dst}/${lang}.norm.lc.rm.tok
-
-            paste -d " " ${dst}/utt_id ${dst}/${lang}.norm.tc.tok | sort > ${dst}/text.tc.${lang}
-            paste -d " " ${dst}/utt_id ${dst}/${lang}.norm.lc.tok | sort > ${dst}/text.lc.${lang}
-            paste -d " " ${dst}/utt_id ${dst}/${lang}.norm.lc.rm.tok | sort > ${dst}/text.lc.rm.${lang}
-
-            # save original and cleaned punctuation
-            lowercase.perl < ${dst}/${lang}.org | text2token.py -s 0 -n 1 | tr " " "\n" \
-                | sort | uniq | grep -v -e '^\s*$' | awk '{print $0 " " NR+1}' > ${dst}/punctuation.${lang}
-            lowercase.perl < ${dst}/${lang}.norm.tc | text2token.py -s 0 -n 1 | tr " " "\n" \
-                | sort | uniq | grep -v -e '^\s*$' | awk '{print $0 " " NR+1}' > ${dst}/punctuation.clean.${lang}
-        done
+        ./local/data_prep.sh ${dst} ${src_lang} ${tgt_lang}
     done
     ./utils/fix_data_dir.sh --utt_extra_files "text.tc.${src_lang} text.lc.${src_lang} text.lc.rm.${src_lang} text.tc.${tgt_lang} text.lc.${tgt_lang} text.lc.rm.${tgt_lang}" data/valid.${src_lang}-${tgt_lang}
     ./utils/fix_data_dir.sh --utt_extra_files "text.tc.${src_lang} text.lc.${src_lang} text.lc.rm.${src_lang} text.tc.${tgt_lang} text.lc.${tgt_lang} text.lc.rm.${tgt_lang}" data/train.${src_lang}-${tgt_lang}
