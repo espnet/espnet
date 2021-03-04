1b48ef3551ab86d3a385f4737bf91ffe5000b1ea
ftshijt <728307998@qq.com>
2021-03-04 11:05:16 -0500

    Update run.sh

--- egs/puebla_nahuatl/asr1/run.sh
+++ egs/puebla_nahuatl/asr1/run.sh
@@ -64,8 +64,8 @@ recog_set="${train_set} ${train_dev} ${test_set}"
 
 if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
     # Download the Data
-   local/download_and_untar.sh local  https://www.openslr.org/resources/92/Puebla-Nahuatl-Manifest.tgz Pueble-Nahuatl-Manifest.tgz
-   local/download_and_untar.sh ${download_dir} https://www.openslr.org/resources/92/Sound-Files-Puebla-Nahuatl.tgz.part0 Sound-Files-Pueble-Nahuatl.tgz.part0 9
+   local/download_and_untar.sh local  https://www.openslr.org/resources/92/Puebla-Nahuatl-Manifest.tgz Puebla-Nahuatl-Manifest.tgz
+   local/download_and_untar.sh ${download_dir} https://www.openslr.org/resources/92/Sound-Files-Puebla-Nahuatl.tgz.part0 Sound-Files-Puebla-Nahuatl.tgz.part0 9
 fi
 
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
