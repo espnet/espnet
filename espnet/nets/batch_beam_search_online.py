0d122d07d75fd692e12897401af53d8b46d47225
Hiromasa Fujihara <fujihara@laboro.ai>
2021-03-17 01:33:21 +0000

    Added real online beam search

--- espnet/nets/batch_beam_search_online.py
+++ espnet/nets/batch_beam_search_online.py
@@ -3,14 +3,19 @@
 import logging
 from pathlib import Path
 from typing import List
+from typing import Tuple
+from typing import Dict
+from typing import Any
 
 import yaml
 
 import torch
-
+import sys
 from espnet.nets.batch_beam_search import BatchBeamSearch
+from espnet.nets.batch_beam_search import BatchHypothesis
 from espnet.nets.beam_search import Hypothesis
 from espnet.nets.e2e_asr_common import end_detect
+
 
 
 class BatchBeamSearchOnline(BatchBeamSearch):
@@ -25,76 +30,49 @@ class BatchBeamSearchOnline(BatchBeamSearch):
     (https://arxiv.org/abs/2006.14941).
     """
 
-    def set_streaming_config(self, asr_config: str):
-        """Set config file for streaming decoding.
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.reset()
+        
+    def reset(self):
+        self.encbuffer = None
+        self.running_hyps = None
+        self.prev_hyps = []
+        self.ended_hyps = []
+        self.process_idx = 0
+        self.prev_nbest_hyps = []
+
+    def score_full(
+        self, hyp: BatchHypothesis, x: torch.Tensor
+    ) -> Tuple[Dict[str, torch.Tensor], Dict[str, Any]]:
+        """Score new hypothesis by `self.full_scorers`.
 
         Args:
-            asr_config (str): The config file for asr training
+            hyp (Hypothesis): Hypothesis with prefix tokens to score
+            x (torch.Tensor): Corresponding input feature
+
+        Returns:
+            Tuple[Dict[str, torch.Tensor], Dict[str, Any]]: Tuple of
+                score dict of `hyp` that has string keys of `self.full_scorers`
+                and tensor score values of shape: `(self.n_vocab,)`,
+                and state dict that has string keys
+                and state values of `self.full_scorers`
 
         """
-        train_config_file = Path(asr_config)
-        self.block_size = None
-        self.hop_size = None
-        self.look_ahead = None
-        config = None
-        with train_config_file.open("r", encoding="utf-8") as f:
-            args = yaml.safe_load(f)
-            if "encoder_conf" in args.keys():
-                if "block_size" in args["encoder_conf"].keys():
-                    self.block_size = args["encoder_conf"]["block_size"]
-                if "hop_size" in args["encoder_conf"].keys():
-                    self.hop_size = args["encoder_conf"]["hop_size"]
-                if "look_ahead" in args["encoder_conf"].keys():
-                    self.look_ahead = args["encoder_conf"]["look_ahead"]
-            elif "config" in args.keys():
-                config = args["config"]
-                if config is None:
-                    logging.info(
-                        "Cannot find config file for streaming decoding: "
-                        + "apply batch beam search instead."
-                    )
-                    return
-        if (
-            self.block_size is None or self.hop_size is None or self.look_ahead is None
-        ) and config is not None:
-            config_file = Path(config)
-            with config_file.open("r", encoding="utf-8") as f:
-                args = yaml.safe_load(f)
-            if "encoder_conf" in args.keys():
-                enc_args = args["encoder_conf"]
-            if enc_args and "block_size" in enc_args:
-                self.block_size = enc_args["block_size"]
-            if enc_args and "hop_size" in enc_args:
-                self.hop_size = enc_args["hop_size"]
-            if enc_args and "look_ahead" in enc_args:
-                self.look_ahead = enc_args["look_ahead"]
-
-    def set_block_size(self, block_size: int):
-        """Set block size for streaming decoding.
-
-        Args:
-            block_size (int): The block size of encoder
-        """
-        self.block_size = block_size
-
-    def set_hop_size(self, hop_size: int):
-        """Set hop size for streaming decoding.
-
-        Args:
-            hop_size (int): The hop size of encoder
-        """
-        self.hop_size = hop_size
-
-    def set_look_ahead(self, look_ahead: int):
-        """Set look ahead size for streaming decoding.
-
-        Args:
-            look_ahead (int): The look ahead size of encoder
-        """
-        self.look_ahead = look_ahead
-
+        scores = dict()
+        states = dict()
+        for k, d in self.full_scorers.items():
+            if len(hyp.yseq) > 0 and len(hyp.yseq[0]) > 20:
+                temp_yseq = hyp.yseq.narrow(1, -10, 10).clone()
+                temp_yseq[:,0] = 3951
+                scores[k], states[k] = d.batch_score(temp_yseq, hyp.states[k], x)
+            else:
+                scores[k], states[k] = d.batch_score(hyp.yseq, hyp.states[k], x)
+        return scores, states
+        
     def forward(
-        self, x: torch.Tensor, maxlenratio: float = 0.0, minlenratio: float = 0.0
+        self, x: torch.Tensor, maxlenratio: float = 0.0, minlenratio: float = 0.0,
+        is_final: bool = True
     ) -> List[Hypothesis]:
         """Perform beam search.
 
@@ -109,131 +87,78 @@ class BatchBeamSearchOnline(BatchBeamSearch):
             list[Hypothesis]: N-best decoding results
 
         """
+        if is_final == False and x.size(0) == 0:
+            return self.prev_nbest_hyps
+        
         self.conservative = True  # always true
-
-        if self.block_size and self.hop_size and self.look_ahead:
-            cur_end_frame = int(self.block_size - self.look_ahead)
+        if not self.running_hyps:
+            self.running_hyps = self.init_hyp(x)
+        if self.encbuffer is None:
+            self.encbuffer = x
         else:
-            cur_end_frame = x.shape[0]
-        process_idx = 0
-        if cur_end_frame < x.shape[0]:
-            h = x.narrow(0, 0, cur_end_frame)
-        else:
-            h = x
-
+            self.encbuffer = torch.cat([self.encbuffer,x], axis=0)
+        
         # set length bounds
         if maxlenratio == 0:
-            maxlen = x.shape[0]
+            maxlen = self.encbuffer.shape[0]
         else:
-            maxlen = max(1, int(maxlenratio * x.size(0)))
-        minlen = int(minlenratio * x.size(0))
+            maxlen = max(1, int(maxlenratio * self.encbuffer.size(0)))
         logging.info("decoder input length: " + str(x.shape[0]))
+        logging.info("accumulated input length: " + str(self.encbuffer.shape[0]))
         logging.info("max output length: " + str(maxlen))
-        logging.info("min output length: " + str(minlen))
 
-        # main loop of prefix search
-        running_hyps = self.init_hyp(h)
-        prev_hyps = []
-        ended_hyps = []
+        # extend states for ctc
+        self.extend(self.encbuffer, self.running_hyps)
+        block_ended_hyps=[]
         prev_repeat = False
+        while self.process_idx < maxlen:
+            logging.debug("position " + str(self.process_idx) + " " + str(maxlen))
+            logging.debug(self.running_hyps.yseq.shape)
 
-        continue_decode = True
-
-        while continue_decode:
-            move_to_next_block = False
-            if cur_end_frame < x.shape[0]:
-                h = x.narrow(0, 0, cur_end_frame)
+            if self.encbuffer.shape[0] > 200:
+                h = self.encbuffer.narrow(0, self.encbuffer.shape[0]-200, 200)
             else:
-                h = x
+                h = self.encbuffer
+            h =  self.encbuffer
+            self.running_hyps.states['decoder'] = [ None for _ in self.running_hyps.states['decoder']]
+            
+            best = self.search(self.running_hyps, h)
+            
+            n_batch = best.yseq.shape[0]
 
-            # extend states for ctc
-            self.extend(h, running_hyps)
-
-            while process_idx < maxlen:
-                logging.debug("position " + str(process_idx))
-                best = self.search(running_hyps, h)
-
-                if process_idx == maxlen - 1:
-                    # end decoding
-                    running_hyps = self.post_process(
-                        process_idx, maxlen, maxlenratio, best, ended_hyps
-                    )
-                n_batch = best.yseq.shape[0]
-                local_ended_hyps = []
-                is_local_eos = (
-                    best.yseq[torch.arange(n_batch), best.length - 1] == self.eos
-                )
-                for i in range(is_local_eos.shape[0]):
-                    if is_local_eos[i]:
-                        hyp = self._select(best, i)
-                        local_ended_hyps.append(hyp)
-                    # NOTE(tsunoo): check repetitions here
-                    # This is a implicit implementation of
-                    # Eq (11) in https://arxiv.org/abs/2006.14941
-                    # A flag prev_repeat is used instead of using set
-                    elif (
-                        not prev_repeat
-                        and best.yseq[i, -1] in best.yseq[i, :-1]
-                        and cur_end_frame < x.shape[0]
-                    ):
-                        move_to_next_block = True
-                        prev_repeat = True
-                if maxlenratio == 0.0 and end_detect(
-                    [lh.asdict() for lh in local_ended_hyps], process_idx
-                ):
-                    logging.info(f"end detected at {process_idx}")
-                    continue_decode = False
-                    break
-                if len(local_ended_hyps) > 0 and cur_end_frame < x.shape[0]:
-                    move_to_next_block = True
-
-                if move_to_next_block:
-                    if (
-                        self.hop_size
-                        and cur_end_frame + int(self.hop_size) + int(self.look_ahead)
-                        < x.shape[0]
-                    ):
-                        cur_end_frame += int(self.hop_size)
-                    else:
-                        cur_end_frame = x.shape[0]
-                    logging.debug("Going to next block: %d", cur_end_frame)
-                    if process_idx > 1 and len(prev_hyps) > 0 and self.conservative:
-                        running_hyps = prev_hyps
-                        process_idx -= 1
-                        prev_hyps = []
-                    break
-
-                prev_repeat = False
-                prev_hyps = running_hyps
-                running_hyps = self.post_process(
-                    process_idx, maxlen, maxlenratio, best, ended_hyps
+            if not is_final:
+                is_eos = (
+                    best.yseq[torch.arange(n_batch), best.length - 1]
+                    == self.eos
                 )
 
-                if cur_end_frame >= x.shape[0]:
-                    for hyp in local_ended_hyps:
-                        ended_hyps.append(hyp)
-
-                if len(running_hyps) == 0:
-                    logging.info("no hypothesis. Finish decoding.")
-                    continue_decode = False
+                if is_eos.any():
                     break
                 else:
-                    logging.debug(f"remained hypotheses: {len(running_hyps)}")
-                # increment number
-                process_idx += 1
+                    self.running_hyps = best
+            if is_final:
+                # post process of one iteration
+                self.running_hyps = self.post_process(self.process_idx,
+                                                      maxlen,
+                                                      maxlenratio,
+                                                      best,
+                                                      self.ended_hyps)
+                if maxlenratio == 0.0 and end_detect([h.asdict() for h in self.ended_hyps], self.process_idx):
+                    logging.info(f"end detected at {self.process_idx}")
+                    break
+                elif len(self.running_hyps) == 0:
+                    logging.info("no hypothesis. Finish decoding.")
+                    break
+            logging.debug(f"remained hypothesis: {len(self.running_hyps)}")
 
+            self.process_idx += 1
+
+        ended_hyps = self.ended_hyps if is_final else self.unbatchfy(self.running_hyps)
         nbest_hyps = sorted(ended_hyps, key=lambda x: x.score, reverse=True)
         # check the number of hypotheses reaching to eos
+        logging.info(str(len(nbest_hyps)))
         if len(nbest_hyps) == 0:
-            logging.warning(
-                "there is no N-best results, perform recognition "
-                "again with smaller minlenratio."
-            )
-            return (
-                []
-                if minlenratio < 0.1
-                else self.forward(x, maxlenratio, max(0.0, minlenratio - 0.1))
-            )
+            return []
 
         # report the best result
         best = nbest_hyps[0]
@@ -250,6 +175,7 @@ class BatchBeamSearchOnline(BatchBeamSearch):
                 + "".join([self.token_list[x] for x in best.yseq[1:-1]])
                 + "\n"
             )
+        self.prev_nbest_hyps = nbest_hyps
         return nbest_hyps
 
     def extend(self, x: torch.Tensor, hyps: Hypothesis) -> List[Hypothesis]:
