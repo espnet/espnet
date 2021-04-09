9e6b4a3b8ee6142d7ccbdadad78f357dae4348de
Siddharth Dalmia <sdalmia@andrew.cmu.edu>
2021-04-09 18:52:38 -0400

    md configs for nahutl

---
+++ egs/puebla_nahuatl/st1/run_md.sh
@@ -0,0 +1,189 @@
+#!/usr/bin/env bash
+
+#  Copyright 2021 Johns Hopkins University (Jiatong Shi)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+. ./path.sh || exit 1;
+. ./cmd.sh || exit 1;
+
+# general configuration
+backend=pytorch
+stage=0        # start from 0 if you need to start from data preparation
+stop_stage=100
+ngpu=1         # number of gpus ("0" uses cpu, otherwise use gpu)
+debugmode=1
+N=0            # number of minibatches to be used (mainly for debugging). "0" uses all minibatches.
+verbose=0      # verbose option
+resume=        # Resume the training from snapshot
+seed=1         # seed to generate random number
+nj=32          # number of jobs for cpu processing
+download_dir=/export/c04/jiatong/data/Puebla-Nahuatl  # download file from openslr
+
+
+# dataset related
+wavdir=${download_dir}/Sound-files-Puebla-Nahuatl
+annotation_dir=${download_dir}/SpeechTranslation210217
+annotation_type=eaf
+annotation_id=st
+src_lang=na
+tgt_lang=es
+
+# preprocessing related
+src_case=tc
+tgt_case=tc
+
+# feature configuration
+do_delta=false
+
+preprocess_config=conf/specaug.yaml
+train_config=conf/tuning/md/mdtrain-speechattn.yaml
+decode_config=conf/tuning/md/mddecode.yaml
+
+# decoding parameter
+n_average=10
+use_valbest_average=false
+
+metric=bleu
+
+asr_model=
+mt_model=
+
+dumpdir=dump   # directory to dump full features
+
+# bpemode (unigram or bpe)
+nbpe=1000
+bpemode=unigram
+
+# exp tag
+tag=${annotation_id} # tag for managing experiments.
+
+. utils/parse_options.sh || exit 1;
+
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+train_set=train_${annotation_id}.${tgt_lang}
+train_dev=dev_${annotation_id}.${tgt_lang}
+test_set=test_${annotation_id}.${tgt_lang}
+recog_set="${train_dev} ${test_set}"
+
+train_set=train_${annotation_id}_sp.${tgt_lang}
+feat_tr_dir=${dumpdir}/${train_set}/delta${do_delta}; mkdir -p ${feat_tr_dir}
+feat_dt_dir=${dumpdir}/${train_dev}/delta${do_delta}; mkdir -p ${feat_dt_dir}
+
+dict=data/lang_1spm/${train_set}_${bpemode}${nbpe}_units_${tgt_case}.txt
+nlsyms=data/lang_1spm/${train_set}_non_lang_syms_${tgt_case}.txt
+bpemodel=data/lang_1spm/${train_set}_${bpemode}${nbpe}_${tgt_case}
+echo "dictionary: ${dict}"
+
+if [[ ${tag} == "" ]]; then
+    expname=${train_set}_${tgt_case}_${backend}_$(basename ${train_config%.*})_${bpemode}${nbpe}_gpu${ngpu}
+    if ${do_delta}; then
+        expname=${expname}_delta
+    fi
+    if [ -n "${preprocess_config}" ]; then
+        expname=${expname}_$(basename ${preprocess_config%.*})
+    fi
+    if [ -n "${asr_model}" ]; then
+        expname=${expname}_asrtrans
+    fi
+    if [ -n "${mt_model}" ]; then
+        expname=${expname}_mttrans
+    fi
+fi
+
+if [[ ${tag} != "" ]]; then
+    expname=${expname}_${tag}
+fi
+
+expdir=exp/${expname}
+mkdir -p ${expdir}
+
+if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
+    echo "stage 4: Network Training"
+
+    ${cuda_cmd} --gpu ${ngpu} ${expdir}/train.log \
+        st_train.py \
+        --config ${train_config} \
+        --n-iter-process 8 \
+        --preprocess-conf ${preprocess_config} \
+        --ngpu ${ngpu} \
+        --backend ${backend} \
+        --outdir ${expdir}/results \
+        --tensorboard-dir tensorboard/${expname} \
+        --debugmode ${debugmode} \
+        --dict ${dict} \
+        --debugdir ${expdir} \
+        --minibatches ${N} \
+        --seed ${seed} \
+        --verbose ${verbose} \
+        --resume ${resume} \
+        --train-json ${feat_tr_dir}/data_${bpemode}${nbpe}.${src_case}_${tgt_case}.json \
+        --valid-json ${feat_dt_dir}/data_${bpemode}${nbpe}.${src_case}_${tgt_case}.json \
+        --enc-init ${asr_model} \
+        --dec-init ${mt_model}
+fi
+
+if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
+    echo "stage 5: Decoding"
+    if [[ $(get_yaml.py ${train_config} model-module) = *transformer* ]] || \
+       [[ $(get_yaml.py ${train_config} model-module) = *conformer* ]]; then
+        # Average ST models
+        if ${use_valbest_average}; then
+            trans_model=model.val${n_average}.avg.best
+            opt="--log ${expdir}/results/log --metric ${metric}"
+        else
+            trans_model=model.last${n_average}.avg.best
+            opt="--log"
+        fi
+        average_checkpoints.py \
+            ${opt} \
+            --backend ${backend} \
+            --snapshots ${expdir}/results/snapshot.ep.* \
+            --out ${expdir}/results/${trans_model} \
+            --num ${n_average}
+    fi
+
+    pids=() # initialize pids
+    for x in ${recog_set}; do
+    (
+        decode_dir=decode_${x}_$(basename ${decode_config%.*})
+        feat_trans_dir=${dumpdir}/${x}/delta${do_delta}
+        mkdir -p ${expdir}/${decode_dir}/asr
+
+        # reset log for RTF calculation
+        if [ -f ${expdir}/${decode_dir}/log/decode.1.log ]; then
+            rm ${expdir}/${decode_dir}/log/decode.*.log
+        fi
+
+        # split data
+        splitjson.py --parts ${nj} ${feat_trans_dir}/data_${bpemode}${nbpe}.${src_case}_${tgt_case}.json
+
+        ${decode_cmd} JOB=1:${nj} ${expdir}/${decode_dir}/log/decode.JOB.log \
+            st_trans.py \
+            --config ${decode_config} \
+            --ngpu 0 \
+            --backend ${backend} \
+            --batchsize 0 \
+            --trans-json ${feat_trans_dir}/split${nj}utt/data_${bpemode}${nbpe}.JOB.json \
+            --result-label ${expdir}/${decode_dir}/data.JOB.json \
+            --asr-si-result-label ${expdir}/${decode_dir}/asr/data.JOB.json \
+            --model ${expdir}/results/${trans_model}
+
+        score_bleu.sh --case ${tgt_case} --bpe ${nbpe} --bpemodel ${bpemodel}.model \
+            ${expdir}/${decode_dir} ${tgt_lang} ${dict}
+
+        score_sclite_case.sh --case ${src_case} --bpe ${nbpe} --bpemodel ${bpemodel}.model --wer true \
+            ${expdir}/${decode_dir}/asr ${dict}
+
+        calculate_rtf.py --log-dir ${expdir}/${decode_dir}/log
+    ) &
+    pids+=($!) # store background pids
+    done
+    i=0; for pid in "${pids[@]}"; do wait ${pid} || ((++i)); done
+    [ ${i} -gt 0 ] && echo "$0: ${i} background jobs are failed." && false
+    echo "Finished"
+fi
