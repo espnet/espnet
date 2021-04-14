96807c7f37bd797cc1d90fb554ac65360136153c
Siddharth Dalmia <sdalmia@andrew.cmu.edu>
2021-04-14 17:15:20 -0400

    run md for iwslt

---
+++ egs/iwslt21/st1/run_md.sh
@@ -0,0 +1,197 @@
+#!/usr/bin/env bash
+
+# Copyright 2021 Kyoto University (Hirofumi Inaguma)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+. ./path.sh || exit 1;
+. ./cmd.sh || exit 1;
+
+# general configuration
+backend=pytorch # chainer or pytorch
+stage=0         # start from -1 if you need to start from data download
+stop_stage=5
+ngpu=1          # number of gpus during training ("0" uses cpu, otherwise use gpu)
+dec_ngpu=0      # number of gpus during decoding ("0" uses cpu, otherwise use gpu)
+nj=8            # number of parallel jobs for decoding
+debugmode=1
+dumpdir=dump    # directory to dump full features
+N=0             # number of minibatches to be used (mainly for debugging). "0" uses all minibatches.
+verbose=0       # verbose option
+resume=         # Resume the training from snapshot
+seed=1          # seed to generate random number
+# feature configuration
+do_delta=false
+
+preprocess_config=conf/specaug.yaml
+train_config=conf/train.yaml
+decode_config=conf/decode.yaml
+
+# decoding parameter
+trans_model=model.acc.best # set a model to be used for decoding: 'model.acc.best' or 'model.loss.best'
+
+# model average realted (only for transformer)
+n_average=10                  # the number of ST models to be averaged
+use_valbest_average=false     # if true, the validation `n_average`-best ST models will be averaged.
+                             # if false, the last `n_average` ST models will be averaged.
+metric=bleu                  # loss/acc/bleu
+
+# pre-training related
+asr_model=
+mt_model=
+
+# preprocessing related
+src_case=lc.rm
+tgt_case=tc
+# tc: truecase
+# lc: lowercase
+# lc.rm: lowercase with punctuation removal
+
+# postprocessing related
+remove_nonverbal=true  # remove non-verbal labels such as "( Applaus )"
+# NOTE: IWSLT community accepts this setting and therefore we use this by default
+
+# bpemode (unigram or bpe)
+nbpe=8000
+bpemode=bpe
+
+# exp tag
+tag="" # tag for managing experiments.
+
+. utils/parse_options.sh || exit 1;
+
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+mkdir -p ${dumpdir}
+rsync -av --progress iwslt_dump/* ${dumpdir}/
+trap 'rm -rf ${dumpdir}' EXIT
+
+# data directories
+mustc_dir=../../must_c
+mustc_v2_dir=../../must_c_v2
+stted_dir=../../iwslt18
+
+train_set=train.de
+train_dev=dev.de
+trans_subset="et_mustc_dev_org.de et_mustc_tst-COMMON.de et_mustc_tst-HE.de"
+trans_set="et_mustc_dev_org.de et_mustc_tst-COMMON.de et_mustc_tst-HE.de \
+           et_mustcv2_dev_org.de et_mustcv2_tst-COMMON.de et_mustcv2_tst-HE.de \
+           et_stted_dev2010.de et_stted_tst2010.de et_stted_tst2013.de et_stted_tst2014.de et_stted_tst2015.de \
+           et_stted_tst2018.de et_stted_tst2019.de"
+feat_tr_dir=${dumpdir}/${train_set}/delta${do_delta}; mkdir -p ${feat_tr_dir}
+feat_dt_dir=${dumpdir}/${train_dev}/delta${do_delta}; mkdir -p ${feat_dt_dir}
+dict=data/lang_1spm/${train_set}_${bpemode}${nbpe}_units_${tgt_case}.txt
+nlsyms=data/lang_1spm/${train_set}_non_lang_syms_${tgt_case}.txt
+bpemodel=data/lang_1spm/${train_set}_${bpemode}${nbpe}_${tgt_case}
+echo "dictionary: ${dict}"
+
+# NOTE: skip stage 3: LM Preparation
+
+if [[ ${tag} == "" ]]; then
+    expname=${train_set}_${src_case}_${tgt_case}_${backend}_$(basename ${train_config%.*})_${bpemode}${nbpe}_gpu${ngpu}
+    if ${do_delta}; then
+        expname=${expname}_delta
+    fi
+    if [ -n "${preprocess_config}" ]; then
+        expname=${expname}_$(basename ${preprocess_config%.*})
+    fi
+    if [ -n "${asr_model}" ]; then
+        expname=${expname}_asrtrans
+    fi
+    if [ -n "${mt_model}" ]; then
+        expname=${expname}_mttrans
+    fi
+fi
+
+if [[ ${tag} != "" ]]; then
+    expname=${expname}_${tag}
+fi
+
+expdir=exp/${expname}
+mkdir -p ${expdir}
+
+if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
+    echo "stage 4: Network Training"
+
+    ${cuda_cmd} --gpu ${ngpu} ${expdir}/train.log \
+        st_train.py \
+        --config ${train_config} \
+        --n-iter-process 8 \
+        --preprocess-conf ${preprocess_config} \
+        --ngpu ${ngpu} \
+        --backend ${backend} \
+        --outdir ${expdir}/results \
+        --tensorboard-dir tensorboard/${expname} \
+        --debugmode ${debugmode} \
+        --dict ${dict} \
+        --debugdir ${expdir} \
+        --minibatches ${N} \
+        --seed ${seed} \
+        --verbose ${verbose} \
+        --resume ${resume} \
+        --train-json ${feat_tr_dir}/data_${bpemode}${nbpe}.${src_case}_${tgt_case}.json \
+        --valid-json ${feat_dt_dir}/data_${bpemode}${nbpe}.${src_case}_${tgt_case}.json \
+        --enc-init ${asr_model} \
+        --dec-init ${mt_model}
+fi
+
+if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
+    echo "stage 5: Decoding"
+    if [[ $(get_yaml.py ${train_config} model-module) = *transformer* ]] || \
+       [[ $(get_yaml.py ${train_config} model-module) = *conformer* ]]; then
+        # Average ST models
+        if ${use_valbest_average}; then
+            trans_model=model.val${n_average}.avg.best
+            opt="--log ${expdir}/results/log --metric ${metric}"
+        else
+            trans_model=model.last${n_average}.avg.best
+            opt="--log"
+        fi
+        average_checkpoints.py \
+            ${opt} \
+            --backend ${backend} \
+            --snapshots ${expdir}/results/snapshot.ep.* \
+            --out ${expdir}/results/${trans_model} \
+            --num ${n_average}
+    fi
+
+    if [ ${dec_ngpu} = 1 ]; then
+        nj=1
+    fi
+
+    pids=() # initialize pids
+    for x in ${trans_subset}; do
+    (
+        decode_dir=decode_${x}_$(basename ${decode_config%.*})
+        feat_trans_dir=${dumpdir}/${x}/delta${do_delta}
+        mkdir -p ${expdir}/${decode_dir}/asr
+
+        # split data
+        splitjson.py --parts ${nj} ${feat_trans_dir}/data_${bpemode}${nbpe}.${src_case}_${tgt_case}.json
+
+        ${decode_cmd} JOB=1:${nj} ${expdir}/${decode_dir}/log/decode.JOB.log \
+            st_trans.py \
+            --config ${decode_config} \
+            --ngpu ${dec_ngpu} \
+            --backend ${backend} \
+            --batchsize 0 \
+            --trans-json ${feat_trans_dir}/split${nj}utt/data_${bpemode}${nbpe}.JOB.json \
+            --result-label ${expdir}/${decode_dir}/data.JOB.json \
+            --asr-si-result-label ${expdir}/${decode_dir}/asr/data.JOB.json \
+            --model ${expdir}/results/${trans_model}
+
+        score_bleu.sh --case ${tgt_case} --bpe ${nbpe} --bpemodel ${bpemodel}.model \
+            --remove_nonverbal ${remove_nonverbal} \
+            ${expdir}/${decode_dir} "de" ${dict}
+
+        score_sclite_case.sh --case ${src_case} --bpe ${nbpe} --bpemodel ${bpemodel}.model --wer true ${expdir}/${decode_dir}/asr ${dict}
+    ) &
+    pids+=($!) # store background pids
+    done
+    i=0; for pid in "${pids[@]}"; do wait ${pid} || ((++i)); done
+    [ ${i} -gt 0 ] && echo "$0: ${i} background jobs are failed." && false
+    echo "Finished"
+fi
