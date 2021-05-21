c8dc76fc997bb39c53b60bbb97af6b9d388c7b96
Ludwig Kürzinger <ludwig.kuerzinger@tum.de>
2021-05-21 20:33:12 +0200

    Fix missing nlsyms.txt for pretrained models

--- espnet2/text/char_tokenizer.py
+++ espnet2/text/char_tokenizer.py
@@ -21,8 +21,11 @@ class CharTokenizer(AbsTokenizer):
             self.non_linguistic_symbols = set()
         elif isinstance(non_linguistic_symbols, (Path, str)):
             non_linguistic_symbols = Path(non_linguistic_symbols)
-            with non_linguistic_symbols.open("r", encoding="utf-8") as f:
-                self.non_linguistic_symbols = set(line.rstrip() for line in f)
+            try:
+                with non_linguistic_symbols.open("r", encoding="utf-8") as f:
+                    self.non_linguistic_symbols = set(line.rstrip() for line in f)
+            except FileNotFoundError:
+                self.non_linguistic_symbols = set()
         else:
             self.non_linguistic_symbols = set(non_linguistic_symbols)
         self.remove_non_linguistic_symbols = remove_non_linguistic_symbols
