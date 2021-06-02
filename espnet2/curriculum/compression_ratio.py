aac6cabf10437132e465c03be2aa17568b8c0607
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-06-02 18:05:03 -0400

    adapt script for mls

--- espnet2/curriculum/compression_ratio.py
+++ espnet2/curriculum/compression_ratio.py
@@ -14,7 +14,6 @@ def calc_CR(wav_scp, data_dir, res_dir):
     for cmd in tqdm(commands):
         fname = cmd.split()[0]
         fpath = os.path.join(data_dir, "/".join(fname.split('_')[:-1]), fname)
-        print(fpath)
         cmd_convert = cmd.split()[1:-2]
         cmd_convert.pop(0)
         cmd_convert.insert(0, 'ffmpeg')
