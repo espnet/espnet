cd5099a6784505d6959bf76e0bc9f2a2158f2d01
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-06-04 11:08:01 -0400

    multi-proc

--- espnet2/curriculum/split_scp.sh
+++ espnet2/curriculum/split_scp.sh
@@ -1,7 +1,7 @@
 
 
 
-key_file="/shared/workspaces/anakuzne/tmp/wav_test.scp"
+key_file="/shared/50k_train/mls_english_opus/data/train/wav.scp"
 _nj=10
 split_scps=""
 _logdir="/shared/workspaces/anakuzne/tmp/log"
@@ -13,8 +13,8 @@ done
                                                                                                                                                                                                               
 /shared/50k_train/mls_english_opus/utils/split_scp.pl "${key_file}" ${split_scps}
 
-#queue.pl JOB=1:"${_nj}" "${_logdir}"/stats.JOB.log \
-#                ${python} -m espnet2.bin.lm_train \
-#                    --train_shape_file "${_logdir}/train.JOB.scp" \
-#                    --valid_shape_file "${_logdir}/dev.JOB.scp" \
-#                    --output_dir "${_logdir}/stats.JOB" 
+echo "Split files..."
+echo "Run jobs..."
+/shared/50k_train/mls_english_opus/utils/queue.pl JOB=1:"${_nj}" "${_logdir}"/train.JOB.scp \
+/shared/workspaces/jp/rev-kaldi/src/featbin/compute-comp-ratio scp,p:"${_logdir}/train.JOB.scp" \
+                                                                    ark,t:"${_logdir}comp_ratio.JOB.txt"
