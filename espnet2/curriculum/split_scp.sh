2b6149a57dab17da795947a15e82a58db3fc20cf
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-06-04 13:32:50 -0400

    debug

--- espnet2/curriculum/split_scp.sh
+++ espnet2/curriculum/split_scp.sh
@@ -5,9 +5,11 @@ key_file="/shared/50k_train/mls_english_opus/data/train/wav.scp"
 _nj=10
 split_scps=""
 _logdir="/shared/workspaces/anakuzne/tmp/log"
+_inp_dir = "/shared/workspaces/anakuzne/tmp/inp_scp"
+_out_dir = "/shared/workspaces/anakuzne/tmp/res"
 
 for n in $(seq ${_nj}); do
-    split_scps+=" ${_logdir}/train.${n}.scp"
+    split_scps+=" ${_inp_dir}/train.${n}.scp"
 done
 
 echo "Split files..."                                                                                                                                                                                                           
@@ -16,5 +18,5 @@ echo "Split files..."
 
 echo "Run jobs..."
 /shared/50k_train/mls_english_opus/utils/queue.pl JOB=1:"${_nj}" "${_logdir}"/train.JOB.scp \
-/shared/workspaces/jp/rev-kaldi/src/featbin/compute-comp-ratio scp,p:${_logdir}/train.JOB.scp \
-                                                                ark,t:${_logdir}/comp_ratio.JOB.txt
+/shared/workspaces/jp/rev-kaldi/src/featbin/compute-comp-ratio scp,p:${_inp_dir}/train.JOB.scp \
+                                                            ark,t:${_out_dir}/comp_ratio.JOB.txt
