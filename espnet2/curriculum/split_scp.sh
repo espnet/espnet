7ca4861125ea69d4fff0878558e4c38ce7e430df
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-06-08 12:20:15 -0400

    CR on segments

--- espnet2/curriculum/split_scp.sh
+++ espnet2/curriculum/split_scp.sh
@@ -7,6 +7,7 @@ split_scps=""
 _logdir="/shared/workspaces/anakuzne/tmp/log"
 _inp_dir="/shared/workspaces/anakuzne/tmp/inp_scp"
 _out_dir="/shared/workspaces/anakuzne/tmp/res"
+format='scp'
 
 for n in $(seq ${_nj}); do
     split_scps+=" ${_inp_dir}/train.${n}.scp"
@@ -16,7 +17,12 @@ echo "Split files..."
 /shared/50k_train/mls_english_opus/utils/split_scp.pl "${key_file}" ${split_scps}
 
 
-echo "Run jobs..."
-/shared/50k_train/mls_english_opus/utils/queue.pl JOB=1:"${_nj}" "${_logdir}"/train.JOB.scp \
-/shared/workspaces/jp/rev-kaldi/src/featbin/compute-comp-ratio scp,p:${_inp_dir}/train.JOB.scp \
-                                                            ark,t:${_out_dir}/comp_ratio.JOB.txt
+echo "Split files..."
+/shared/50k_train/mls_english_opus/utils/queue.pl JOB=1:"${_nj}" "${_logdir}"/train.JOB.scp
+
+echo "Calculate complexity..."
+if [ $format == "scp" ]; then
+    /shared/workspaces/jp/rev-kaldi/src/featbin/compute-comp-ratio scp,p:${_inp_dir}/train.JOB.scp \
+                                                                ark,t:${_out_dir}/comp_ratio.JOB.txt
+else
+    ./extract-segments scp,p:${_inp_dir}/train.JOB.scp test.segments ark:-  |  ./compute-comp-ratio ark:- ark,t:test.output.txt
