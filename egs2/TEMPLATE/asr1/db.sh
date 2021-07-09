fb414005ebf8023f6c338e146429e80929212b02
Peter <peter.yh.wu@gmail.com>
2021-07-08 22:18:58 -0700

    removed sunda from db.sh

--- egs2/TEMPLATE/asr1/db.sh
+++ egs2/TEMPLATE/asr1/db.sh
@@ -78,7 +78,6 @@ HOW2_TEXT=downloads/how2-300h-v1
 HOW2_FEATS=downloads/fbank_pitch_181516
 ZEROTH_KOREAN=downloads
 JAVA=downloads
-SUNDA=downloads
 RU_OPEN_STT=downloads
 GIGASPEECH=
 NOISY_SPEECH=
@@ -160,6 +159,5 @@ if [[ "$(hostname -d)" == clsp.jhu.edu ]]; then
     HOW2_FEATS=
     ZEROTH_KOREAN=downloads
     JAVA=
-    SUNDA=
 
 fi
