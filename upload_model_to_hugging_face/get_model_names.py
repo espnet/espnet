8d22ffba45b3702db6beda761ff55b7eff107951
siddhu001 <siddhantarora1806@gmail.com>
2021-07-11 10:23:03 -0400

    Add template scripts to port to hugging face

--- upload_model_to_hugging_face/get_model_names.py
+++
@@ -1,5 +0,0 @@
-from espnet_model_zoo.downloader import ModelDownloader
-d = ModelDownloader()
-for model_name in d.query("name"):
-	model_path = d.download(model_name)
-	print(model_path+"||"+model_name.replace(" ","_"))
