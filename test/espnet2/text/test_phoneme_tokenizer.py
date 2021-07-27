3d80c9f40575333ee270669b797d27beb9426efd
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2021-07-27 19:52:13 +0900

    added spanish g2p

--- test/espnet2/text/test_phoneme_tokenizer.py
+++ test/espnet2/text/test_phoneme_tokenizer.py
@@ -285,6 +285,9 @@ def test_text2tokens(phoneme_tokenizer: PhonemeTokenizer):
     elif phoneme_tokenizer.g2p_type == "espeak_ng_french":
         input = "Bonjour le monde."
         output = ["b", "ɔ̃", "ʒ", "ˈu", "ʁ", "l", "ə-", "m", "ˈɔ̃", "d", "."]
+    elif phoneme_tokenizer.g2p_type == "espeak_ng_spanish":
+        input = "Hola Mundo."
+        output = ["ˈo", "l", "a", "m", "ˈu", "n", "d", "o", "."]
     else:
         raise NotImplementedError
     assert phoneme_tokenizer.text2tokens(input) == output
