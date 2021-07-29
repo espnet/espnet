576fd7501e4429402e094352756ed98913ba673a
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2021-07-29 10:28:59 +0900

    added silence trimming stage

--- egs2/hui_acg/tts1/local/data.sh
+++ egs2/hui_acg/tts1/local/data.sh
@@ -14,10 +14,11 @@ log() {
 SECONDS=0
 
 stage=-1
-stop_stage=2
+stop_stage=100
 spk=Hokuspokus
 text_format=raw
-nj=8
+threshold=35
+nj=32
 g2p=espeak_ng_german
 
 log "$0 $*"
@@ -57,7 +58,20 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
 fi
 
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
-    log "stage 1: utils/subset_data_dir.sh"
+    log "stage 1: scripts/audio/trim_silence.sh"
+    # shellcheck disable=SC2154
+    scripts/audio/trim_silence.sh \
+        --cmd "${train_cmd}" \
+        --nj "${nj}" \
+        --fs 44100 \
+        --win_length 2048 \
+        --shift_length 512 \
+        --threshold "${threshold}" \
+        "data/${spk,,}" "data/${spk,,}/log"
+fi
+
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+    log "stage 2: utils/subset_data_dir.sh"
     utils/subset_data_dir.sh "data/${spk,,}" 500 "data/${spk,,}_deveval"
     utils/subset_data_dir.sh --first "data/${spk,,}_deveval" 250 "data/${dev_set}"
     utils/subset_data_dir.sh --last "data/${spk,,}_deveval" 250 "data/${eval_set}"
@@ -67,8 +81,8 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     utils/fix_data_dir.sh "data/${train_set}"
 fi
 
-if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ] && [ "${text_format}" = phn ]; then
-    log "stage 2: pyscripts/utils/convert_text_to_phn.py"
+if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ] && [ "${text_format}" = phn ]; then
+    log "stage 3: pyscripts/utils/convert_text_to_phn.py"
     for dset in "${train_set}" "${dev_set}" "${eval_set}"; do
         utils/copy_data_dir.sh "data/${dset}" "data/${dset}_phn"
         pyscripts/utils/convert_text_to_phn.py --g2p "${g2p}" --nj "${nj}" \
