f548570eec7c09e04361584df39f272855e92389
takenori-y <takenori.yoshimura24@gmail.com>
2021-08-01 16:06:02 +0900

    apply black

--- egs2/jmd/tts1/local/clean_text.py
+++ egs2/jmd/tts1/local/clean_text.py
@@ -9,15 +9,16 @@ import re
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--skip-header', action='store_true',
-                        help='if true, skip first line')
-    parser.add_argument('text', type=str, help='text to be cleaned')
+    parser.add_argument(
+        "--skip-header", action="store_true", help="if true, skip first line"
+    )
+    parser.add_argument("text", type=str, help="text to be cleaned")
     args = parser.parse_args()
-    with open(args.text, 'r', encoding='utf-8') as fid:
+    with open(args.text, "r", encoding="utf-8") as fid:
         if args.skip_header:
             fid.readline()
         for line in fid.readlines():
-            id, content = line.split(',')
-            content = re.sub('（.*?）', '', content.rstrip())
-            content = re.sub('「(.*?)」', '\\1', content)
+            id, content = line.split(",")
+            content = re.sub("（.*?）", "", content.rstrip())
+            content = re.sub("「(.*?)」", "\\1", content)
             print("%s %s" % (id, content))
