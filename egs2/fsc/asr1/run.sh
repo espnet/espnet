b3f2fedad0ac503f86acddb0e867a637fc920215
siddhu001 <siddhantarora1806@gmail.com>
2021-08-06 10:12:12 -0400

    Acc spectoral augmentation

--- egs2/fsc/asr1/run.sh
+++ egs2/fsc/asr1/run.sh
@@ -9,17 +9,18 @@ train_set="train"
 valid_set="valid"
 test_sets="test valid"
 
-asr_config=conf/train_asr.yaml
+asr_config=conf/tuning/train_asr_transformer_adam_small_specaug.yaml
 
 ./asr.sh \
     --lang en \
     --ngpu 1 \
     --use_lm false \
     --nbpe 5000 \
-    --token_type char\
+    --token_type word\
     --audio_format wav\
     --feats_type raw\
     --max_wav_duration 30 \
+    --inference_asr_model last10_model.pth\
     --asr_config "${asr_config}" \
     --train_set "${train_set}" \
     --valid_set "${valid_set}" \
