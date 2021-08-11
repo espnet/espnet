ff2955b55fb60640c53d04e496691ca87472c8a6
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2021-08-12 00:14:06 +0900

    updated

--- espnet2/tts/vits/vits.py
+++ espnet2/tts/vits/vits.py
@@ -9,19 +9,34 @@ This code is based on https://github.com/jaywalnut310/vits.
 
 import math
 
+from typing import Any
+from typing import Dict
+from typing import Optional
+
+import numpy as np
 import torch
 import torch.nn.functional as F
 
+from typeguard import check_argument_types
+
 from espnet.nets.pytorch_backend.nets_utils import make_non_pad_mask
+from espnet2.torch_utils.device_funcs import force_gatherable
+from espnet2.tts.abs_gan_tts import AbsGANTTS
 from espnet2.tts.vits.flow import ResidualAffineCouplingBlock
 from espnet2.tts.vits.hifigan import HiFiGANGenerator
+from espnet2.tts.vits.hifigan import HiFiGANMultiScaleMultiPeriodDiscriminator
+from espnet2.tts.vits.loss import DiscriminatorAdversarialLoss
+from espnet2.tts.vits.loss import FeatureMatchLoss
+from espnet2.tts.vits.loss import GeneratorAdversarialLoss
+from espnet2.tts.vits.loss import KLDivergenceLoss
+from espnet2.tts.vits.loss import MelSpectrogramLoss
 from espnet2.tts.vits.posterior_encoder import PosteriorEncoder
 from espnet2.tts.vits.stochastic_duration_predictor import StochasticDurationPredictor
 from espnet2.tts.vits.text_encoder import TextEncoder
 
 
-class VITS(torch.nn.Module):
-    """VITS module.
+class VITSGenerator(torch.nn.Module):
+    """VITS generator module.
 
     This is a module of VITS described in `Conditional Variational Autoencoder
     with Adversarial Learning for End-to-End Text-to-Speech`_.
@@ -184,6 +199,7 @@ class VITS(torch.nn.Module):
             global_channels=global_channels,
         )
 
+        self.upsample_factor = np.prod(decoder_upsample_scales)
         self.spks = spks
         if self.spks > 1:
             assert global_channels > 0
@@ -294,7 +310,7 @@ class VITS(torch.nn.Module):
         logs_p = torch.matmul(attn.squeeze(1), logs_p.transpose(1, 2)).transpose(1, 2)
 
         # forward decoder with segments
-        z_segments, z_start_idxs = self._get_random_segments(
+        z_segments, z_start_idxs = self.get_random_segments(
             z,
             feats_lengths,
             self.segment_size,
@@ -311,7 +327,7 @@ class VITS(torch.nn.Module):
             (z, z_p, m_p, logs_p, m_q, logs_q),
         )
 
-    def _get_random_segments(self, x, x_lengths=None, segment_size=32):
+    def get_random_segments(self, x, x_lengths=None, segment_size=32):
         """Get random segments.
 
         Args:
@@ -331,10 +347,10 @@ class VITS(torch.nn.Module):
         start_idxs = (torch.rand([b]).to(x.device) * max_start_idx).to(
             dtype=torch.long,
         )
-        segments = self._get_segments(x, start_idxs, segment_size)
+        segments = self.get_segments(x, start_idxs, segment_size)
         return segments, start_idxs
 
-    def _get_segments(self, x, start_idxs, segment_size=32):
+    def get_segments(self, x, start_idxs, segment_size=32):
         """Get segments.
 
         Args:
@@ -453,3 +469,306 @@ class VITS(torch.nn.Module):
         #   [1., 1., 1., 1., 1.]]]       [0., 0., 0., 0., 1.]]]
         path = path - F.pad(path, [0, 0, 1, 0, 0, 0])[:, :-1]
         return path.unsqueeze(1).transpose(2, 3) * mask
+
+
+class VITS(AbsGANTTS):
+    """VITS module (generator + discriminator).
+
+    This is a module of VITS described in `Conditional Variational Autoencoder
+    with Adversarial Learning for End-to-End Text-to-Speech`_.
+
+    .. _`Conditional Variational Autoencoder with Adversarial Learning for End-to-End
+        Text-to-Speech`: https://arxiv.org/abs/2006.04558
+
+    """
+
+    def __init__(
+        self,
+        # generator related
+        idim: int,
+        odim: int,
+        generator_params: Dict[str, Any] = {
+            "aux_channels": 80,
+            "hidden_channels": 192,
+            "spks": -1,
+            "global_channels": -1,
+            "segment_size": 32,
+            "text_encoder_attention_heads": 2,
+            "text_encoder_attention_expand": 4,
+            "text_encoder_blocks": 6,
+            "text_encoder_kernel_size": 3,
+            "text_encoder_dropout_rate": 0.1,
+            "text_encoder_positional_dropout_rate": 0.0,
+            "text_encoder_attention_dropout_rate": 0.0,
+            "decoder_kernel_size": 7,
+            "decoder_channels": 512,
+            "decoder_upsample_scales": (8, 8, 2, 2),
+            "decoder_upsample_kernel_sizes": (16, 16, 4, 4),
+            "decoder_resblock_kernel_sizes": (3, 7, 11),
+            "decoder_resblock_dilations": [(1, 3, 5), (1, 3, 5), (1, 3, 5)],
+            "use_weight_norm_in_decoder": True,
+            "posterior_encoder_kernel_size": 5,
+            "posterior_encoder_layers": 16,
+            "posterior_encoder_stacks": 1,
+            "posterior_encoder_base_dilation": 1,
+            "posterior_encoder_dropout_rate": 0.0,
+            "use_weight_norm_in_posterior_encoder": True,
+            "flow_flows": 4,
+            "flow_kernel_size": 5,
+            "flow_base_dilation": 1,
+            "flow_layers": 5,
+            "flow_dropout_rate": 0.0,
+            "use_weight_norm_in_flow": True,
+            "use_only_mean_in_flow": True,
+            "stochastic_duration_predictor_kernel_size": 3,
+            "stochastic_duration_predictor_dropout_rate": 0.5,
+            "stochastic_duration_predictor_flows": 4,
+            "stochastic_duration_predictor_dds_conv_layers": 3,
+        },
+        # discriminator related
+        discriminator_params: Dict[str, Any] = {
+            "scales": 3,
+            "scale_downsample_pooling": "AvgPool1d",
+            "scale_downsample_pooling_params": {
+                "kernel_size": 4,
+                "stride": 2,
+                "padding": 2,
+            },
+            "scale_discriminator_params": {
+                "in_channels": 1,
+                "out_channels": 1,
+                "kernel_sizes": [15, 41, 5, 3],
+                "channels": 128,
+                "max_downsample_channels": 1024,
+                "max_groups": 16,
+                "bias": True,
+                "downsample_scales": [2, 2, 4, 4, 1],
+                "nonlinear_activation": "LeakyReLU",
+                "nonlinear_activation_params": {"negative_slope": 0.1},
+            },
+            "follow_official_norm": True,
+            "periods": [2, 3, 5, 7, 11],
+            "period_discriminator_params": {
+                "in_channels": 1,
+                "out_channels": 1,
+                "kernel_sizes": [5, 3],
+                "channels": 32,
+                "downsample_scales": [3, 3, 3, 3, 1],
+                "max_downsample_channels": 1024,
+                "bias": True,
+                "nonlinear_activation": "LeakyReLU",
+                "nonlinear_activation_params": {"negative_slope": 0.1},
+                "use_weight_norm": True,
+                "use_spectral_norm": False,
+            },
+        },
+        # loss related
+        lambda_adv: float = 1.0,
+        lambda_mel: float = 45.0,
+        lambda_feat_match: float = 1.0,
+        lambda_dur: float = 1.0,
+        lambda_kl: float = 1.0,
+        generator_adv_loss_params: Dict[str, Any] = {
+            "average_by_discriminators": False,
+        },
+        discriminator_adv_loss_params: Dict[str, Any] = {
+            "average_by_discriminators": False,
+        },
+        feat_match_loss_params: Dict[str, Any] = {
+            "average_by_discriminators": False,
+            "average_by_layers": False,
+            "include_final_outputs": True,
+        },
+        mel_loss_params: Dict[str, Any] = {
+            "fs": 22050,
+            "n_fft": 1024,
+            "hop_length": 256,
+            "win_length": None,
+            "window": "hann",
+            "n_mels": 80,
+            "fmin": 0,
+            "fmax": None,
+            "log_base": None,
+        },
+    ):
+        """Initialize VITS module."""
+        assert check_argument_types()
+        super().__init__()
+        generator_params.update(idim=idim, odim=odim)
+        self.generator = VITSGenerator(
+            **generator_params,
+        )
+        self.discriminator = HiFiGANMultiScaleMultiPeriodDiscriminator(
+            **discriminator_params,
+        )
+        self.generator_adv_loss = GeneratorAdversarialLoss(
+            **generator_adv_loss_params,
+        )
+        self.discriminator_adv_loss = DiscriminatorAdversarialLoss(
+            **discriminator_adv_loss_params,
+        )
+        self.feat_match_loss = FeatureMatchLoss(
+            **feat_match_loss_params,
+        )
+        self.mel_loss = MelSpectrogramLoss(
+            **mel_loss_params,
+        )
+        self.kl_loss = KLDivergenceLoss()
+
+        self.lambda_adv = lambda_adv
+        self.lambda_mel = lambda_mel
+        self.lambda_kl = lambda_kl
+        self.lambda_feat_match = lambda_feat_match
+        self.lambda_dur = lambda_dur
+
+    def forward_generator(
+        self,
+        text: torch.Tensor,
+        text_lengths: torch.Tensor,
+        feats: torch.Tensor,
+        feats_lengths: torch.Tensor,
+        speech: torch.Tensor,
+        speech_lengths: torch.Tensor,
+        sids: Optional[torch.Tensor],
+    ):
+        """Perform generator forward.
+
+        Args:
+            text (Tensor): Text index tensor (B, T_text).
+            text_lengths (Tensor): Text length tensor (B,).
+            feats (Tensor): Feature tensor (B, aux_channels, T_feats).
+            feats_lengths (Tensor): Feature length tensor (B,)
+            speech (Tensor): Speech waveform tensor (B, 1, T_wav).
+            speech_lengths (Tensor): Speech length tensor (B,).
+            sids (Optional[Tensor]): Speaker index tensor (B,).
+
+        Returns:
+            Tensor: Loss scalar tensor.
+            Dict[str, float]: Statistics to be monitored.
+            Tensor: Weight value.
+
+        """
+        batch_size = text.size(0)
+        outs = self.generator(text, text_lengths, feats, feats_lengths, sids)
+        speech_hat_, dur, attn, start_idxs, x_mask, z_mask, outs_ = outs
+        z, z_p, m_p, logs_p, m_q, logs_q = outs_
+        start_idxs *= self.generator.upsample_factor
+        segment_size = (self.generator.segment_size * self.generator.upsample_factor,)
+        speech_ = self.generator.get_segments(
+            x=speech,
+            start_idxs=start_idxs,
+            segment_size=segment_size,
+        )
+        p_hat = self.discriminator(speech_hat_)
+        p = self.discriminator(speech_)
+        mel_loss = self.mel_loss(speech_hat_, speech_)
+        kl_loss = self.kl_loss(z_p, logs_q, m_p, logs_p, z_mask)
+        dur_loss = torch.sum(dur.float())
+        adv_loss = self.generator_adv_loss(speech_hat_, speech_)
+        feat_match_loss = self.feat_match_loss(p_hat, p)
+
+        mel_loss *= self.lambda_mel
+        kl_loss *= self.lambda_kl
+        dur_loss *= self.lambda_dur
+        adv_loss *= self.lambda_adv
+        feat_match_loss *= self.lambda_feat_match
+        loss = mel_loss + kl_loss + dur_loss + adv_loss + feat_match_loss
+
+        stats = dict(
+            generator_loss=loss.item(),
+            generator_mel_loss=mel_loss.item(),
+            generator_kl_loss=kl_loss.item(),
+            generator_dur_loss=dur_loss.item(),
+            generator_adv_loss=adv_loss.item(),
+            generator_feat_match_loss=feat_match_loss.item(),
+        )
+
+        loss, stats, weight = force_gatherable((loss, stats, batch_size), loss.device)
+        return loss, stats, weight
+
+    def forward_discrminator(
+        self,
+        text: torch.Tensor,
+        text_lengths: torch.Tensor,
+        feats: torch.Tensor,
+        feats_lengths: torch.Tensor,
+        speech: torch.Tensor,
+        speech_lengths: torch.Tensor,
+        sids: Optional[torch.Tensor],
+    ):
+        """Perform discriminator forward.
+
+        Args:
+            text (Tensor): Text index tensor (B, T_text).
+            text_lengths (Tensor): Text length tensor (B,).
+            feats (Tensor): Feature tensor (B, aux_channels, T_feats).
+            feats_lengths (Tensor): Feature length tensor (B,)
+            speech (Tensor): Speech waveform tensor (B, 1, T_wav).
+            speech_lengths (Tensor): Speech length tensor (B,).
+            sids (Optional[Tensor]): Speaker index tensor (B,).
+
+        Returns:
+            Tensor: Loss scalar tensor.
+            Dict[str, float]: Statistics to be monitored.
+            Tensor: Weight value.
+
+        """
+        batch_size = text.size(0)
+        speech_hat_, _, _, start_idxs, *_ = self.generator(
+            text, text_lengths, feats, feats_lengths, sids
+        )
+        start_idxs *= self.generator.upsample_factor
+        segment_size = (self.generator.segment_size * self.generator.upsample_factor,)
+        speech_ = self.generator.get_segments(
+            x=speech,
+            start_idxs=start_idxs,
+            segment_size=segment_size,
+        )
+        p_hat = self.discriminator(speech_hat_.detach())
+        p = self.discriminator(speech_)
+        real_loss, fake_loss = self.discriminator_adv_loss(p_hat, p)
+        loss = real_loss + fake_loss
+
+        stats = dict(
+            discriminator_loss=loss.item(),
+            discriminator_real_loss=real_loss.item(),
+            discriminator_fake_loss=fake_loss.item(),
+        )
+
+        loss, stats, weight = force_gatherable((loss, stats, batch_size), loss.device)
+        return loss, stats, weight
+
+    def inference(
+        self,
+        text,
+        sids=None,
+        noise_scale=1.0,
+        length_scale=1.0,
+        noise_scale_w=1.0,
+        max_len=None,
+    ):
+        """Run inference.
+
+        Args:
+            text (Tensor): Input text index tensor (T_text,).
+            sids (Tensor): Speaker index tensor (1,)
+            noise_scale (float): Noise scale value for flow.
+            length_scale (float): Length scaling value.
+            noise_scale_w (float): Noise scale value for duration predictor.
+            max_len (Optional[int]): Maximum length.
+
+        Returns:
+            Tensor: Generated waveform tensor (T_wav).
+            Tensor: Attention weight tensor (T_feats, T_text).
+            None: Dummy outputs for compatibility.
+
+        """
+        wav, att_w, _ = self.generator.inference(
+            text=text,
+            sids=sids,
+            noise_scale=noise_scale,
+            length_scale=length_scale,
+            noise_scale_w=noise_scale_w,
+            max_len=max_len,
+        )
+        return wav, att_w, None
