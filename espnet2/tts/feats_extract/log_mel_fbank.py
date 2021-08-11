9ae8bd9be70ddffa9b52dd3910cc94d24e8eb386
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2021-08-12 00:02:24 +0900

    support log_base change

--- espnet2/tts/feats_extract/log_mel_fbank.py
+++ espnet2/tts/feats_extract/log_mel_fbank.py
@@ -33,6 +33,7 @@ class LogMelFbank(AbsFeatsExtract):
         fmin: Optional[int] = 80,
         fmax: Optional[int] = 7600,
         htk: bool = False,
+        log_base: Optional[float] = 10.0,
     ):
         assert check_argument_types()
         super().__init__()
@@ -65,7 +66,7 @@ class LogMelFbank(AbsFeatsExtract):
             fmin=fmin,
             fmax=fmax,
             htk=htk,
-            log_base=10.0,
+            log_base=log_base,
         )
 
     def output_size(self) -> int:
