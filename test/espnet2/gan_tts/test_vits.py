3ba1445aeb5fd2bd3cdf92a1a019018c3b9bedc0
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2021-08-12 17:50:06 +0900

    fixed skip condition

--- test/espnet2/gan_tts/test_vits.py
+++ test/espnet2/gan_tts/test_vits.py
@@ -136,10 +136,11 @@ def make_vits_loss_args(**kwargs):
 
 
 @pytest.mark.skipif(
-    LooseVersion(torch.__version__) < "1.4", reason="Pytorch >= 1.4 is required."
+    LooseVersion(torch.__version__) < LooseVersion("1.4"),
+    reason="Pytorch >= 1.4 is required.",
 )
 @pytest.mark.skipif(
-    LooseVersion(torch.__version__) == "1.6",
+    LooseVersion(torch.__version__) == LooseVersion("1.6"),
     reason="Group conv in pytorch 1.6 has an issue. "
     "See https://github.com/pytorch/pytorch/issues/42446.",
 )
@@ -215,10 +216,11 @@ def test_vits_generator_forward(model_dict):
 
 
 @pytest.mark.skipif(
-    LooseVersion(torch.__version__) < "1.4", reason="Pytorch >= 1.4 is required."
+    LooseVersion(torch.__version__) < LooseVersion("1.4"),
+    reason="Pytorch >= 1.4 is required.",
 )
 @pytest.mark.skipif(
-    LooseVersion(torch.__version__) == "1.6",
+    LooseVersion(torch.__version__) == LooseVersion("1.6"),
     reason="Group conv in pytorch 1.6 has an issue. "
     "See https://github.com/pytorch/pytorch/issues/42446.",
 )
