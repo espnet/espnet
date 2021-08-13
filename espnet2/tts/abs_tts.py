841ef7640fd66f466d06d37fa6d3ba2fb0bfe57d
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2021-08-13 14:27:45 +0900

    updated tts abstract class for new format

--- espnet2/tts/abs_tts.py
+++ espnet2/tts/abs_tts.py
@@ -2,6 +2,7 @@ from abc import ABC
 from abc import abstractmethod
 from typing import Dict
 from typing import Tuple
+from typing import Union
 
 import torch
 
@@ -24,7 +25,11 @@ class AbsTTS(torch.nn.Module, ABC):
     def inference(
         self,
         text: torch.Tensor,
-        spembs: torch.Tensor = None,
         **kwargs,
-    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    ) -> Dict[str, torch.Tensor],
         raise NotImplementedError
+
+    @property
+    def require_vocoder(self):
+        """Return whether or not vocoder is required."""
+        return True
