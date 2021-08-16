0c3cc07039e70aff54fb89bd4e613b12b09c5ce6
wtc6 <sathvikudupa66@gmail.com>
2021-08-16 20:31:34 +0530

    change name in db.sh

--- egs2/mucs21_subtask2/asr1/local/data.sh
+++ egs2/mucs21_subtask2/asr1/local/data.sh
@@ -13,9 +13,9 @@ log() {
     local fname=${BASH_SOURCE[1]##*/}
     echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
 }
-mkdir -p ${MUCS_subtask2}
-if [ -z "${MUCS_subtask2}" ]; then
-    log "Fill the value of 'MUCS_subtask2' of db.sh"
+mkdir -p ${MUCS_SUBTASK2}
+if [ -z "${MUCS_SUBTASK2}" ]; then
+    log "Fill the value of 'MUCS_SUBTASK2' of db.sh"
     exit 1
 fi
 
@@ -26,18 +26,18 @@ set -o pipefail
 log "data preparation started"
 
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
-    log "stage1: Download data to ${MUCS_subtask2}"
-    mkdir -p ${MUCS_subtask2}
-    local/download_data.sh ${MUCS_subtask2} $lang
+    log "stage1: Download data to ${MUCS_SUBTASK2}"
+    mkdir -p ${MUCS_SUBTASK2}
+    local/download_data.sh ${MUCS_SUBTASK2} $lang
 fi
 
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
-  if [ ! -e ${MUCS_subtask2}/${lang}.path_done ]; then
+  if [ ! -e ${MUCS_SUBTASK2}/${lang}.path_done ]; then
     log "stage2: Preparing data for MUCS subtask2"
     for dset in test train; do
-        local/prepare_data.sh ${MUCS_subtask2}/$lang/$dset/transcripts/wav.scp ${MUCS_subtask2}/$lang/$dset/ out.scp
+        local/prepare_data.sh ${MUCS_SUBTASK2}/$lang/$dset/transcripts/wav.scp ${MUCS_SUBTASK2}/$lang/$dset/ out.scp
       done
-    touch ${MUCS_subtask2}/${lang}.path_done
+    touch ${MUCS_SUBTASK2}/${lang}.path_done
     else
         echo "Path written already. Skipping."
     fi
