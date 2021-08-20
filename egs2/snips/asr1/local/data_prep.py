68c3354d22af66173c09a02836a693d190b4cc5c
root <yuekai.zhang@zoom.us>
2021-08-20 05:55:02 +0000

    fix ci error

--- egs2/snips/asr1/local/data_prep.py
+++ egs2/snips/asr1/local/data_prep.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-# Yuekai Zhang,2021
+
+# Copyright 2021  Yuekai Zhang
 
 import json
 import argparse
 
-
 parser = argparse.ArgumentParser(description='Process snips dataset.')
-parser.add_argument('--wav_path', type=str, default=
-"/uekai/data/slu/snips/smart-lights-en-close-field/",
+parser.add_argument('--wav_path', type=str,
                     help='file path for audios')
 parser.add_argument('--text_f', type=str, default = 'data/text.trans',
                     help='file path for text')
@@ -19,18 +18,14 @@ parser.add_argument('--wavscp_f', type=str, default = 'data/wav.scp',
                     help='file path for wav.scp')
 parser.add_argument('--non_linguistic_symbols', type=str, default = 'data/non_linguistic_symbols.txt',
                     help='non_linguistic_symbols')
-
 args = parser.parse_args()
-
 
 meta = args.wav_path + "/speech_corpus/metadata.json"
 dataset = args.wav_path + "/dataset.json"
 
-
-with open(meta,'r') as meta_f, open(args.text_f,
-"w") as text_f,open(dataset,'r') as dataset,open(args.non_linguistic_symbols,
-"w") as non_linguistic_symbols,open(args.wavscp_f,"w") as wavscp_f, open(
-args.utt2spk_f,"w") as utt2spk_f,open(args.semantics,"w") as semantics:
+with open(meta,'r') as meta_f, open(args.text_f,"w") as text_f,open(
+    dataset,'r') as dataset,open(args.non_linguistic_symbols,"w") as non_linguistic_symbols,open(
+        args.wavscp_f,"w") as wavscp_f, open(args.utt2spk_f,"w") as utt2spk_f,open(args.semantics,"w") as semantics:
     meta_info,dataset = json.load(meta_f),json.load(dataset)
     intents = dataset["intents"]
     for intent,utts in intents.items():
@@ -57,8 +52,6 @@ args.utt2spk_f,"w") as utt2spk_f,open(args.semantics,"w") as semantics:
             # 1310.wav missed in the original dataset
             if utt_text != "Turn the lights up":
                 semantics.write(f"{utt_text}|{utt_semantic}\n")
-
-
     for wav in meta_info.values():
         wav,text,spk_id = wav["filename"],wav["text"],wav["worker"]["id"]
         wav_name = spk_id + '-' + wav
@@ -68,4 +61,3 @@ args.utt2spk_f,"w") as utt2spk_f,open(args.semantics,"w") as semantics:
         wav_path = args.wav_path + "/speech_corpus/audio/" + wav
         wavscp_f.write(f"{wav_name} {wav_path}\n")
         utt2spk_f.write(f"{wav_name} {spk_id}\n")
-
