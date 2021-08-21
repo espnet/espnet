97cf4c656c8db5160877ea5eb270cdbb99386088
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2021-08-21 10:50:16 +0900

    support sid and lid inputs in tacotron2

--- espnet2/tts/tacotron2.py
+++ espnet2/tts/tacotron2.py
@@ -4,12 +4,15 @@
 """Tacotron 2 related modules for ESPnet2."""
 
 import logging
+
 from typing import Dict
+from typing import Optional
 from typing import Sequence
 from typing import Tuple
 
 import torch
 import torch.nn.functional as F
+
 from typeguard import check_argument_types
 
 from espnet.nets.pytorch_backend.e2e_tts_tacotron2 import GuidedAttentionLoss
@@ -34,56 +37,6 @@ class Tacotron2(AbsTTS):
 
     .. _`Natural TTS Synthesis by Conditioning WaveNet on Mel Spectrogram Predictions`:
        https://arxiv.org/abs/1712.05884
-
-    Args:
-        idim (int): Dimension of the inputs.
-        odim: (int) Dimension of the outputs.
-        spk_embed_dim (int, optional): Dimension of the speaker embedding.
-        embed_dim (int, optional): Dimension of character embedding.
-        elayers (int, optional): The number of encoder blstm layers.
-        eunits (int, optional): The number of encoder blstm units.
-        econv_layers (int, optional): The number of encoder conv layers.
-        econv_filts (int, optional): The number of encoder conv filter size.
-        econv_chans (int, optional): The number of encoder conv filter channels.
-        dlayers (int, optional): The number of decoder lstm layers.
-        dunits (int, optional): The number of decoder lstm units.
-        prenet_layers (int, optional): The number of prenet layers.
-        prenet_units (int, optional): The number of prenet units.
-        postnet_layers (int, optional): The number of postnet layers.
-        postnet_filts (int, optional): The number of postnet filter size.
-        postnet_chans (int, optional): The number of postnet filter channels.
-        output_activation (str, optional): The name of activation function for outputs.
-        adim (int, optional): The number of dimension of mlp in attention.
-        aconv_chans (int, optional): The number of attention conv filter channels.
-        aconv_filts (int, optional): The number of attention conv filter size.
-        cumulate_att_w (bool, optional): Whether to cumulate previous attention weight.
-        use_batch_norm (bool, optional): Whether to use batch normalization.
-        use_concate (bool, optional): Whether to concatenate encoder embedding with
-            decoder lstm outputs.
-        reduction_factor (int, optional): Reduction factor.
-        spk_embed_dim (int, optional): Number of speaker embedding dimenstions.
-        spk_embed_integration_type (str, optional): How to integrate speaker embedding.
-        use_gst (str, optional): Whether to use global style token.
-        gst_tokens (int, optional): The number of GST embeddings.
-        gst_heads (int, optional): The number of heads in GST multihead attention.
-        gst_conv_layers (int, optional): The number of conv layers in GST.
-        gst_conv_chans_list: (Sequence[int], optional):
-            List of the number of channels of conv layers in GST.
-        gst_conv_kernel_size (int, optional): Kernal size of conv layers in GST.
-        gst_conv_stride (int, optional): Stride size of conv layers in GST.
-        gst_gru_layers (int, optional): The number of GRU layers in GST.
-        gst_gru_units (int, optional): The number of GRU units in GST.
-        dropout_rate (float, optional): Dropout rate.
-        zoneout_rate (float, optional): Zoneout rate.
-        use_masking (bool, optional): Whether to mask padded part in loss calculation.
-        use_weighted_masking (bool, optional): Whether to apply weighted masking in
-            loss calculation.
-        bce_pos_weight (float, optional): Weight of positive sample of stop token
-            (only for use_masking=True).
-        loss_type (str, optional): How to calculate loss.
-        use_guided_attn_loss (bool, optional): Whether to use guided attention loss.
-        guided_attn_loss_sigma (float, optional): Sigma in guided attention loss.
-        guided_attn_loss_lamdba (float, optional): Lambda in guided attention loss.
 
     """
 
@@ -115,6 +68,9 @@ class Tacotron2(AbsTTS):
         use_concate: bool = True,
         use_residual: bool = False,
         reduction_factor: int = 1,
+        # extra embedding related
+        spks: int = -1,
+        langs: int = -1,
         spk_embed_dim: int = None,
         spk_embed_integration_type: str = "concat",
         use_gst: bool = False,
@@ -137,7 +93,61 @@ class Tacotron2(AbsTTS):
         guided_attn_loss_sigma: float = 0.4,
         guided_attn_loss_lambda: float = 1.0,
     ):
-        """Initialize Tacotron2 module."""
+        """Initialize Tacotron2 module.
+
+        Args:
+            idim (int): Dimension of the inputs.
+            odim: (int) Dimension of the outputs.
+            spk_embed_dim (int): Dimension of the speaker embedding.
+            embed_dim (int): Dimension of character embedding.
+            elayers (int): The number of encoder blstm layers.
+            eunits (int): The number of encoder blstm units.
+            econv_layers (int): The number of encoder conv layers.
+            econv_filts (int): The number of encoder conv filter size.
+            econv_chans (int): The number of encoder conv filter channels.
+            dlayers (int): The number of decoder lstm layers.
+            dunits (int): The number of decoder lstm units.
+            prenet_layers (int): The number of prenet layers.
+            prenet_units (int): The number of prenet units.
+            postnet_layers (int): The number of postnet layers.
+            postnet_filts (int): The number of postnet filter size.
+            postnet_chans (int): The number of postnet filter channels.
+            output_activation (str): The name of activation function for outputs.
+            adim (int): The number of dimension of mlp in attention.
+            aconv_chans (int): The number of attention conv filter channels.
+            aconv_filts (int): The number of attention conv filter size.
+            cumulate_att_w (bool): Whether to cumulate previous attention weight.
+            use_batch_norm (bool): Whether to use batch normalization.
+            use_concate (bool): Whether to concatenate encoder embedding with
+                decoder lstm outputs.
+            reduction_factor (int): Reduction factor.
+            spks: Number of speakers. If set to > 0, speaker ID embedding will be used.
+            langs: Number of langs. If set to > 0, lang ID embedding will be used.
+            spk_embed_dim (int): Pretrained speaker embedding dimension.
+            spk_embed_integration_type (str): How to integrate speaker embedding.
+            use_gst (str): Whether to use global style token.
+            gst_tokens (int): The number of GST embeddings.
+            gst_heads (int): The number of heads in GST multihead attention.
+            gst_conv_layers (int): The number of conv layers in GST.
+            gst_conv_chans_list: (Sequence[int]):
+                List of the number of channels of conv layers in GST.
+            gst_conv_kernel_size (int): Kernal size of conv layers in GST.
+            gst_conv_stride (int): Stride size of conv layers in GST.
+            gst_gru_layers (int): The number of GRU layers in GST.
+            gst_gru_units (int): The number of GRU units in GST.
+            dropout_rate (float): Dropout rate.
+            zoneout_rate (float): Zoneout rate.
+            use_masking (bool): Whether to mask padded part in loss calculation.
+            use_weighted_masking (bool): Whether to apply weighted masking in
+                loss calculation.
+            bce_pos_weight (float): Weight of positive sample of stop token
+                (only for use_masking=True).
+            loss_type (str): How to calculate loss ("L1", "L2", or "L1+L2").
+            use_guided_attn_loss (bool): Whether to use guided attention loss.
+            guided_attn_loss_sigma (float): Sigma in guided attention loss.
+            guided_attn_loss_lambda (float): Lambda in guided attention loss.
+
+        """
         assert check_argument_types()
         super().__init__()
 
@@ -148,6 +158,8 @@ class Tacotron2(AbsTTS):
         self.spk_embed_dim = spk_embed_dim
         self.cumulate_att_w = cumulate_att_w
         self.reduction_factor = reduction_factor
+        self.spks = spks
+        self.langs = langs
         self.use_gst = use_gst
         self.use_guided_attn_loss = use_guided_attn_loss
         self.loss_type = loss_type
@@ -196,6 +208,11 @@ class Tacotron2(AbsTTS):
                 gru_layers=gst_gru_layers,
                 gru_units=gst_gru_units,
             )
+
+        if self.spks > 0:
+            self.sid_emb = torch.nn.Embedding(spks, embed_dim)
+        if self.langs > 0:
+            self.lid_emb = torch.nn.Embedding(langs, embed_dim)
 
         if spk_embed_dim is None:
             dec_idim = eunits
@@ -263,16 +280,20 @@ class Tacotron2(AbsTTS):
         text_lengths: torch.Tensor,
         speech: torch.Tensor,
         speech_lengths: torch.Tensor,
-        spembs: torch.Tensor = None,
+        spembs: Optional[torch.Tensor] = None,
+        sids: Optional[torch.Tensor] = None,
+        lids: Optional[torch.Tensor] = None,
     ) -> Tuple[torch.Tensor, Dict[str, torch.Tensor], torch.Tensor]:
         """Calculate forward propagation.
 
         Args:
-            text (LongTensor): Batch of padded character ids (B, Tmax).
+            text (LongTensor): Batch of padded character ids (B, T_text).
             text_lengths (LongTensor): Batch of lengths of each input batch (B,).
-            speech (Tensor): Batch of padded target features (B, Lmax, odim).
+            speech (Tensor): Batch of padded target features (B, T_feats, odim).
             speech_lengths (LongTensor): Batch of the lengths of each target (B,).
-            spembs (Tensor, optional): Batch of speaker embeddings (B, spk_embed_dim).
+            spembs (Optional[Tensor]): Batch of speaker embeddings (B, spk_embed_dim).
+            sids (Optional[Tensor]): Batch of speaker IDs (B, 1).
+            lids (Optional[Tensor]): Batch of language IDs (B, 1).
 
         Returns:
             Tensor: Loss scalar value.
@@ -300,7 +321,13 @@ class Tacotron2(AbsTTS):
 
         # calculate tacotron2 outputs
         after_outs, before_outs, logits, att_ws = self._forward(
-            xs, ilens, ys, olens, spembs
+            xs=xs,
+            ilens=ilens,
+            ys=ys,
+            olens=olens,
+            spembs=spembs,
+            sids=sids,
+            lids=lids,
         )
 
         # modify mod part of groundtruth
@@ -359,11 +386,19 @@ class Tacotron2(AbsTTS):
         ys: torch.Tensor,
         olens: torch.Tensor,
         spembs: torch.Tensor,
+        sids: torch.Tensor,
+        lids: torch.Tensor,
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         hs, hlens = self.enc(xs, ilens)
         if self.use_gst:
             style_embs = self.gst(ys)
             hs = hs + style_embs.unsqueeze(1)
+        if self.spks > 0:
+            sid_embs = self.sid_emb(sids.view(-1))
+            hs = hs + sid_embs.unsqueeze(1)
+        if self.langs > 0:
+            lid_embs = self.lid_emb(lids.view(-1))
+            hs = hs + lid_embs.unsqueeze(1)
         if self.spk_embed_dim is not None:
             hs = self._integrate_with_spk_embed(hs, spembs)
         return self.dec(hs, hlens, ys)
@@ -372,7 +407,9 @@ class Tacotron2(AbsTTS):
         self,
         text: torch.Tensor,
         speech: torch.Tensor = None,
-        spembs: torch.Tensor = None,
+        spembs: Optional[torch.Tensor] = None,
+        sids: Optional[torch.Tensor] = None,
+        lids: Optional[torch.Tensor] = None,
         threshold: float = 0.5,
         minlenratio: float = 0.0,
         maxlenratio: float = 10.0,
@@ -384,22 +421,24 @@ class Tacotron2(AbsTTS):
         """Generate the sequence of features given the sequences of characters.
 
         Args:
-            text (LongTensor): Input sequence of characters (T,).
+            text (LongTensor): Input sequence of characters (T_text,).
             speech (Tensor, optional): Feature sequence to extract style (N, idim).
-            spembs (Tensor, optional): Speaker embedding vector (spk_embed_dim,).
-            threshold (float, optional): Threshold in inference.
-            minlenratio (float, optional): Minimum length ratio in inference.
-            maxlenratio (float, optional): Maximum length ratio in inference.
-            use_att_constraint (bool, optional): Whether to apply attention constraint.
-            backward_window (int, optional): Backward window in attention constraint.
-            forward_window (int, optional): Forward window in attention constraint.
-            use_teacher_forcing (bool, optional): Whether to use teacher forcing.
+            spembs (Optional[Tensor]): Speaker embedding (spk_embed_dim,).
+            sids (Optional[Tensor]): Speaker IDs (1,).
+            lids (Optional[Tensor]): Language IDs (1,).
+            threshold (float): Threshold in inference.
+            minlenratio (float): Minimum length ratio in inference.
+            maxlenratio (float): Maximum length ratio in inference.
+            use_att_constraint (bool): Whether to apply attention constraint.
+            backward_window (int): Backward window in attention constraint.
+            forward_window (int): Forward window in attention constraint.
+            use_teacher_forcing (bool): Whether to use teacher forcing.
 
         Returns:
             Dict[str, Tensor]: Output dict including the following items:
-                * feat_gen (Tensor): Output sequence of features (L, odim).
-                * prob (Tensor): Output sequence of stop probabilities (L,).
-                * att_w (Tensor): Attention weights (L, T).
+                * feat_gen (Tensor): Output sequence of features (T_feats, odim).
+                * prob (Tensor): Output sequence of stop probabilities (T_feats,).
+                * att_w (Tensor): Attention weights (T_feats, T).
 
         """
         x = text
@@ -417,7 +456,15 @@ class Tacotron2(AbsTTS):
             spembs = None if spemb is None else spemb.unsqueeze(0)
             ilens = x.new_tensor([xs.size(1)]).long()
             olens = y.new_tensor([ys.size(1)]).long()
-            outs, _, _, att_ws = self._forward(xs, ilens, ys, olens, spembs)
+            outs, _, _, att_ws = self._forward(
+                xs=xs,
+                ilens=ilens,
+                ys=ys,
+                olens=olens,
+                spembs=spembs,
+                sids=sids,
+                lids=lids,
+            )
 
             return dict(feat_gen=outs[0], att_w=att_ws[0])
 
