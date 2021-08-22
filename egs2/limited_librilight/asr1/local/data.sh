21b51cdc289bcd034ffc9d59474445a9f6d06c38
Jzmo <jz.momo@outlook.com>
2021-08-22 10:29:53 -0400

    stype: fix shellcheck

--- egs2/limited_librilight/asr1/local/data.sh
+++ egs2/limited_librilight/asr1/local/data.sh
@@ -14,7 +14,8 @@ SECONDS=0
 
 stage=1
 stop_stage=100000
-data_url=https://dl.fbaipublicfiles.com/librilight/data/librispeech_finetuning.tgz
+ll_data_url=https://dl.fbaipublicfiles.com/librilight/data/librispeech_finetuning.tgz
+ls_data_url=www.openslr.org/resources/12
 train_set="train_10h"
 
 log "$0 $*"
@@ -44,9 +45,9 @@ src=${LIBRILIGHT}/librispeech_finetuning
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     log "Stage 1: Data Download to ${LIBRILIGHT}"
     if [ ! -d ${src}/1h ] && [ ! -d ${src}/9h ]; then
-	mkdir -p ${LIBRILIGHT}
-	wget ${data_url} -P ${LIBRILIGHT}
-	tar vxfz ${LIBRILIGHT}/librispeech_finetuning.tgz -C ${LIBRILIGHT}
+	mkdir -p "${LIBRILIGHT}"
+	wget "${ll_data_url}" -P "${LIBRILIGHT}"
+	tar vxfz "${LIBRILIGHT}/librispeech_finetuning.tgz" -C "${LIBRILIGHT}"
     else
 	log "${LIBRILIGHT}/librispeech_finetuning is already existing. Skip data downloading"
     fi
@@ -58,21 +59,25 @@ if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
 	dataname=$(echo ${part} | sed 's/\//_/g')
 	
 	data_part=$(./utils/make_absolute.sh ${src}/${part})
-	data_new_path=data/train_${dataname}
-	mkdir -p ${data_new_path}
-	files=( `find -L ${data_part}/ -name "*.flac"` )
-	
-	for f in ${files[@]}; do
-	    filename=`basename $f`
+	data_new_path="data/train_${dataname}"
+	mkdir -p "${data_new_path}"
+	files=( "$(find -L ${data_part}/ -name '*.flac')" )
+
+	for f in "${files[@]}"; do
+	    filename=$(basename "$f")
 	    filename=${filename%%.flac}
 	    echo "${filename} flac -c -d -s ${f} |" 
 	done | sort | uniq > ${data_new_path}/wav.scp
 	
 	paste -d' ' <(awk '{print $1}' ${data_new_path}/wav.scp) \
-              <(awk '{print $1}' ${data_new_path}/wav.scp | cut -d'-' -f1) \
-              > ${data_new_path}/utt2spk
-	./utils/utt2spk_to_spk2utt.pl ${data_new_path}/utt2spk > ${data_new_path}/spk2utt
-	cat `find -L ${data_part} -name "*.trans.txt"` | sort > ${data_new_path}/text
+              <(awk '{print $1}' "${data_new_path}/wav.scp" | cut -d'-' -f1) \
+              > "${data_new_path}/utt2spk"
+	./utils/utt2spk_to_spk2utt.pl "${data_new_path}/utt2spk" > "${data_new_path}/spk2utt"
+	text_files=( "$(find -L ${data_part}/ -name '*.trans.txt')" )
+	echo "${text_files[@]}"
+	for f in "${text_files[@]}"; do
+	    cat ${f}
+	done | sort | uniq > "${data_new_path}/text"
     done
 fi
 
@@ -86,7 +91,7 @@ if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
     if [ ! -e "${LIBRISPEECH}/LibriSpeech/LICENSE.TXT" ]; then
 	echo "stage 4: Evaluation Data Download to ${LIBRISPEECH}"
 	for part in dev-clean test-clean dev-other test-other; do
-            local/download_and_untar_eval.sh ${LIBRISPEECH} ${data_url} ${part}
+            local/download_and_untar_eval.sh "${LIBRISPEECH}" "${ls_data_url}" "${part}"
 	done
     else
         log "stage 4: ${LIBRISPEECH}/LibriSpeech/LICENSE.TXT is already existing. Skip data downloading"
