3668ebe9bf79ec96d1916a01240a7672f7fd519d
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2021-08-24 14:32:49 +0900

    added melgan modules

---
+++ espnet2/gan_tts/melgan/__init__.py
@@ -0,0 +1,3 @@
+from espnet2.gan_tts.melgan.melgan import MelGANDiscriminator  # NOQA
+from espnet2.gan_tts.melgan.melgan import MelGANGenerator  # NOQA
+from espnet2.gan_tts.melgan.melgan import MelGANMultiScaleDiscriminator  # NOQA
