16fd6db965b7f3db4e4d65894bc67e0c836603e8
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2021-09-04 17:51:06 +0900

    updated

--- egs2/jmd/tts1/run.sh
+++ egs2/jmd/tts1/run.sh
@@ -44,6 +44,10 @@ g2p=pyopenjtalk
 # (e.g. k 1 0 o 1 0 pau k 5 -4 o 5 -4 N 5 -3 n 5 -2 i 5 -2 ch 5 -1 i 5 -1 w 5 0 a 5 0)
 # g2p=pyopenjtalk_accent_with_pause
 
+# 5. Phoneme + Prosody symbols
+# (e.g. ^, k, #, o, _, k, o, [, N, n, i, ch, i, w, a, $)
+# g2p=pyopenjtalk_prosody
+
 ./tts.sh \
     --lang jp \
     --feats_type raw \
