787ebccc1a8a78185f6d28a0197f8ac4f075554d
kan-bayashi <kan-bayashi@users.noreply.github.com>
2021-09-06 01:44:13 +0000

    deploy: 31de8f2b394d4b4f9feb6450c3d0eeb2e92e4dce

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.10.3a1',
+    VERSION: '0.10.3a2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
