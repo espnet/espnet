cca26b0754408c67c3f2d69d8d7ce6030b456252
Yifan Peng <pengyf21@gmail.com>
2021-09-18 20:13:17 -0400

    add speech commands to db.sh

--- egs2/TEMPLATE/asr1/db.sh
+++ egs2/TEMPLATE/asr1/db.sh
@@ -108,6 +108,7 @@ MUCS_SUBTASK1=downloads
 MUCS_SUBTASK2=downloads
 GAMAYUN=downloads
 IWSLT21LR=downloads/iwslt21
+SPEECHCOMMANDS=downloads
 
 # For only JHU environment
 if [[ "$(hostname -d)" == clsp.jhu.edu ]]; then
