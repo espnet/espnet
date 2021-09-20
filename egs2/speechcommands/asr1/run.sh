3d7764a2b2c44fe92e1babda4e7f635d9a6be0e9
Yifan Peng <pengyf21@gmail.com>
2021-09-20 11:06:24 -0400

    add run.sh for initial experiment

---
+++ egs2/speechcommands/asr1/run.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+train_set=train
+valid_set=dev
+test_sets="dev test"
+
+asr_config=conf/tuning/train_asr_transformer_adam.yaml
+inference_config=conf/decode_asr.yaml
+
+# speed perturbation related
+# (train_set will be "${train_set}_sp" if speed_perturb_factors is specified)
+speed_perturb_factors="0.9 1.0 1.1"
+
+# lm_train_text is set to avoid errors if speed_perturb is used
+./asr.sh                                                \
+    --skip_data_prep true                              \
+    --skip_train true                                  \
+    --skip_eval false                                   \
+    --ngpu 1                                            \
+    --nj 8                                              \
+    --inference_nj 8                                    \
+    --speed_perturb_factors "${speed_perturb_factors}"  \
+    --feats_type fbank_pitch                            \
+    --audio_format wav                                  \
+    --fs 16000                                          \
+    --token_type word                                   \
+    --use_lm false                                      \
+    --asr_tag balanced                                  \
+    --asr_config "${asr_config}"                        \
+    --inference_tag infer                               \
+    --inference_config "${inference_config}"            \
+    --inference_asr_model valid.loss.ave_2best.pth      \
+    --train_set "${train_set}"                          \
+    --valid_set "${valid_set}"                          \
+    --test_sets "${test_sets}"                          \
+    --lm_train_text "data/${train_set}/text" "$@"           
