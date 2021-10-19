8f4b0212bb63e4e130434fa92fba941adaa40c0f
CTinRay <b03902072@ntu.edu.tw>
2021-10-19 10:51:30 -0400

    Raise exception when librosa.stft is used in the training mode

--- espnet2/layers/stft.py
+++ espnet2/layers/stft.py
@@ -106,6 +106,12 @@ class Stft(torch.nn.Module, InversibleInterface):
                 stft_kwargs["return_complex"] = False
             output = torch.stft(input, **stft_kwargs)
         else:
+            if self.training:
+                raise NotImplementedError(
+                    "stft is implemented with librosa on this device, which does not "
+                    "support the training mode."
+                )
+
             # use stft_kwargs to flexibly control different PyTorch versions' kwargs
             stft_kwargs = dict(
                 n_fft=self.n_fft,
