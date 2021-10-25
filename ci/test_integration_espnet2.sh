97277026dc9b68f16094adf1e4d9f2deb1c34f5c
Siddhant Arora <siddhana@br014.ib.bridges2.psc.edu>
2021-10-25 15:24:14 -0400

    Add s3prl in pythonpath

--- ci/test_integration_espnet2.sh
+++ ci/test_integration_espnet2.sh
@@ -2,8 +2,9 @@
 
 set -euo pipefail
 
-source tools/activate_python.sh
-PYTHONPATH="${PYTHONPATH:-}:$(pwd)/tools/s3prl"
+#source tools/activate_python.sh
+export PYTHONPATH="${PYTHONPATH:-}:$(pwd)/tools/s3prl"
+echo $PYTHONPATH
 python="coverage run --append"
 cwd=$(pwd)
 
