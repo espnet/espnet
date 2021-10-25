45d3580c0c814cddf5479f76d2f47e8f15a81f03
Dee <33377212+200987299@users.noreply.github.com>
2021-10-25 18:00:19 +0800

    Update install_sph2pipe.sh

--- tools/installers/install_sph2pipe.sh
+++ tools/installers/install_sph2pipe.sh
@@ -9,9 +9,9 @@ fi
 
 
 if [ ! -e sph2pipe_v2.5.tar.gz ]; then
-    wget -T 10 https://github.com/espnet/kaldi-bin/releases/download/v0.0.2/sph2pipe_v2.5.tar.gz || \
-	wget -T 10 -t 3 http://www.openslr.org/resources/3/sph2pipe_v2.5.tar.gz || \
-	wget --no-check-certificate -T 10  https://sourceforge.net/projects/kaldi/files/sph2pipe_v2.5.tar.gz
+    wget -T 100 https://github.com/espnet/kaldi-bin/releases/download/v0.0.2/sph2pipe_v2.5.tar.gz || \
+	wget -T 100 -t 13 http://www.openslr.org/resources/3/sph2pipe_v2.5.tar.gz || \
+	wget --no-check-certificate -T 100  https://sourceforge.net/projects/kaldi/files/sph2pipe_v2.5.tar.gz
 fi
 
 if [ ! -e sph2pipe_v2.5 ]; then
