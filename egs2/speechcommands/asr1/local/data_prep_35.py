eb06d3254ff4ad070092da18ea82aff88ea60adf
Yifan Peng <pengyf21@gmail.com>
2021-10-25 06:13:48 +0000

    update local

--- egs2/speechcommands/asr1/local/data_prep_35.py
+++ egs2/speechcommands/asr1/local/data_prep_35.py
@@ -16,28 +16,22 @@ parser = argparse.ArgumentParser(
     description="Process speech commands dataset with 35 commands."
 )
 parser.add_argument(
-    '--data_path', 
-    type=str, 
-    default='downloads/speech_commands_v0.02', 
-    help='folder containing the original data'
+    "--data_path",
+    type=str,
+    default="downloads/speech_commands_v0.02",
+    help="folder containing the original data",
 )
 parser.add_argument(
-    '--train_dir',
+    "--train_dir",
     type=str,
-    default='data/train',
-    help='output folder for training data'
+    default="data/train",
+    help="output folder for training data",
 )
 parser.add_argument(
-    '--dev_dir',
-    type=str,
-    default='data/dev',
-    help='output folder for validation data'
+    "--dev_dir", type=str, default="data/dev", help="output folder for validation data"
 )
 parser.add_argument(
-    '--test_dir',
-    type=str,
-    default='data/test',
-    help='output folder for test data'
+    "--test_dir", type=str, default="data/test", help="output folder for test data"
 )
 args = parser.parse_args()
 
@@ -79,50 +73,51 @@ WORDS = [
     "follow",
     "learn",
     "visual",
-]       # 35 commands
+]  # 35 commands
 
 
 # Generate train and dev data
-with open(os.path.join(args.data_path, 'validation_list.txt'), 'r') as dev_f:
+with open(os.path.join(args.data_path, "validation_list.txt"), "r") as dev_f:
     dev_file_list = [line.rstrip() for line in dev_f.readlines()]
-    dev_file_list = [os.path.abspath(os.path.join(args.data_path, line)) for line in dev_file_list]
-with open(os.path.join(args.data_path, 'testing_list.txt'), 'r') as test_f:
+    dev_file_list = [
+        os.path.abspath(os.path.join(args.data_path, line)) for line in dev_file_list
+    ]
+with open(os.path.join(args.data_path, "testing_list.txt"), "r") as test_f:
     test_file_list = [line.rstrip() for line in test_f.readlines()]
-    test_file_list = [os.path.abspath(os.path.join(args.data_path, line)) for line in test_file_list]
+    test_file_list = [
+        os.path.abspath(os.path.join(args.data_path, line)) for line in test_file_list
+    ]
 
 full_file_list = []
 for word in WORDS:
     for wav_file in os.listdir(os.path.join(args.data_path, word)):
-        if wav_file.endswith('.wav'):
-            full_file_list.append(os.path.abspath(
-                os.path.join(args.data_path, word, wav_file)
-            ))
+        if wav_file.endswith(".wav"):
+            full_file_list.append(
+                os.path.abspath(os.path.join(args.data_path, word, wav_file))
+            )
 
 train_file_list = list(set(full_file_list) - set(dev_file_list) - set(test_file_list))
-assert len(train_file_list) + len(dev_file_list) + len(test_file_list) == \
-        len(full_file_list)
+assert len(train_file_list) + len(dev_file_list) + len(test_file_list) == len(
+    full_file_list
+)
 
-for name in ['train', 'dev', 'test']:
-    if name == 'train':
+for name in ["train", "dev", "test"]:
+    if name == "train":
         file_list = train_file_list
         out_dir = args.train_dir
-    elif name == 'dev':
+    elif name == "dev":
         file_list = dev_file_list
         out_dir = args.dev_dir
     else:
         file_list = test_file_list
         out_dir = args.test_dir
-    
-    with open(
-        os.path.join(out_dir, 'text'), 'w'
-    ) as text_f, open(
-        os.path.join(out_dir, 'wav.scp'), 'w'
-    ) as wav_scp_f, open(
-        os.path.join(out_dir, 'utt2spk'), 'w'
-    ) as utt2spk_f:
-        for wav_abspath in file_list:       # absolute path
-            word, wav = wav_abspath.split('/')[-2:]
-            uttid = f'{word}_{wav[:-4]}'
-            text_f.write(uttid + ' ' + word + '\n')
-            wav_scp_f.write(uttid + ' ' + wav_abspath + '\n')
-            utt2spk_f.write(uttid + ' ' + uttid + '\n')
+
+    with open(os.path.join(out_dir, "text"), "w") as text_f, open(
+        os.path.join(out_dir, "wav.scp"), "w"
+    ) as wav_scp_f, open(os.path.join(out_dir, "utt2spk"), "w") as utt2spk_f:
+        for wav_abspath in file_list:  # absolute path
+            word, wav = wav_abspath.split("/")[-2:]
+            uttid = f"{word}_{wav[:-4]}"
+            text_f.write(uttid + " " + word + "\n")
+            wav_scp_f.write(uttid + " " + wav_abspath + "\n")
+            utt2spk_f.write(uttid + " " + uttid + "\n")
--- egs2/speechcommands/asr1/local/score.py
+++ egs2/speechcommands/asr1/local/score.py
@@ -6,21 +6,15 @@ import os
 import os.path
 import argparse
 
-parser = argparse.ArgumentParser(
-    description="Calculate classification accuracy."
-)
-parser.add_argument(
-    '--wer_dir', 
-    type=str, 
-    help='folder containing hyp.trn and ref.trn'
-)
+parser = argparse.ArgumentParser(description="Calculate classification accuracy.")
+parser.add_argument("--wer_dir", type=str, help="folder containing hyp.trn and ref.trn")
 args = parser.parse_args()
 
 
-with open(os.path.join(args.wer_dir, 'hyp.trn'), 'r') as f:
-    hyp_dict = {ln.split()[1]:ln.split()[0] for ln in f.readlines()}
-with open(os.path.join(args.wer_dir, 'ref.trn'), 'r') as f:
-    ref_dict = {ln.split()[1]:ln.split()[0] for ln in f.readlines()}
+with open(os.path.join(args.wer_dir, "hyp.trn"), "r") as f:
+    hyp_dict = {ln.split()[1]: ln.split()[0] for ln in f.readlines()}
+with open(os.path.join(args.wer_dir, "ref.trn"), "r") as f:
+    ref_dict = {ln.split()[1]: ln.split()[0] for ln in f.readlines()}
 
 n_correct = 0
 n_samples = 0
@@ -29,6 +23,6 @@ for sample_id in ref_dict:
     if ref_dict[sample_id] == hyp_dict[sample_id]:
         n_correct += 1
 
-with open(os.path.join(args.wer_dir, '..', 'accuracy.csv'), 'w') as f:
-    f.write('total,correct,accuracy\n')
-    f.write(f'{n_samples},{n_correct},{n_correct/n_samples}\n')
+with open(os.path.join(args.wer_dir, "..", "accuracy.csv"), "w") as f:
+    f.write("total,correct,accuracy\n")
+    f.write(f"{n_samples},{n_correct},{n_correct/n_samples}\n")
