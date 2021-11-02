b5c1f618a782a31453e03e96df84784b4c65d31c
Siddhant Arora <siddhana@br011.ib.bridges2.psc.edu>
2021-11-02 11:43:39 -0400

    Add slurp entity evaluation

--- egs2/slurp_entity/asr1/local/score.sh
+++ egs2/slurp_entity/asr1/local/score.sh
@@ -12,18 +12,23 @@
 . parse_options.sh || exit 1;
 
 if [ $# -lt 1 ]; then
-  echo "Usage: local/score.sh <asr-exp-dir> <inference_config>"
+  echo "Usage: local/score.sh <asr-exp-dir> <valid_inference_folder> <test_inference_folder>"
   exit 1;
 fi
+. ./db.sh
 
+SLURP="/ocean/projects/cis210027p/siddhana/slurp"
 asr_expdir=$1
 
 if [ $# -gt 1 ]; then
-	inference_config=$2
-	python local/score.py ${asr_expdir} ${inference_config}
+	valid_inference_folder=$2
+	test_inference_folder=$3
+	python local/score.py --exp_root ${asr_expdir} --valid_folder ${valid_inference_folder} --test_folder ${test_inference_folder}
+	python local/convert_to_entity_file.py --exp_root ${asr_expdir} --valid_folder ${valid_inference_folder} --test_folder ${test_inference_folder}
 else
-	python local/score.py ${asr_expdir}
+	python local/score.py --exp_root ${asr_expdir}
+	python local/convert_to_entity_file.py --exp_root ${asr_expdir}
 fi
-
+python local/evaluation/evaluate.py -g ${SLURP}/dataset/slurp/test.jsonl -p result_test.json
 exit 0
 
