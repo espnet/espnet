5183adb17697ae7ed32506de43d922deee9acbeb
Dan Berrebbi <dan.berrebbi@polytechnique.edu>
2021-12-14 15:14:17 -0500

    pass CI tests

--- test/espnet2/asr/frontend/test_fused.py
+++ test/espnet2/asr/frontend/test_fused.py
@@ -12,22 +12,35 @@ list_frontends = [DefaultFrontend(fs="16k"), DefaultFrontend(fs="16k")]
 
 
 def test_frontend_init():
-    frontend = FusedFrontend(fs="16k", align_method="linear_projection", proj_dim=100, frontends=list_frontends)
+    frontend = FusedFrontend(
+        fs="16k",
+        align_method="linear_projection",
+        proj_dim=100,
+        frontends=list_frontends,
+    )
     assert len(frontend.frontends) == 2
     assert len(frontend.factors) == len(frontend.frontends)
     assert frontend[0].frontend_type == "default"
 
 
 def test_frontend_output_size():
-    frontend = FusedFrontend(fs="16k", align_method="linear_projection", proj_dim=100, frontends=list_frontends)
+    frontend = FusedFrontend(
+        fs="16k",
+        align_method="linear_projection",
+        proj_dim=100,
+        frontends=list_frontends,
+    )
     assert frontend.output_size() == 100 * len(list_frontends)
 
 
 def test_frontend_backward():
-    frontend = FusedFrontend(fs="16k", align_method="linear_projection", proj_dim=100, frontends=list_frontends)
+    frontend = FusedFrontend(
+        fs="16k",
+        align_method="linear_projection",
+        proj_dim=100,
+        frontends=list_frontends,
+    )
     x = torch.randn(2, 300, requires_grad=True)
     x_lengths = torch.LongTensor([300, 89])
     y, y_lengths = frontend(x, x_lengths)
     y.sum().backward()
-
-
