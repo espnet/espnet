de2d236a534284f5eebd89b6cba01be5d625d08d
Brian Yan <brianyan918@gmail.com>
2021-12-22 17:46:01 -0500

    black

--- egs/aesrc2020/asr1/local/preprocess.py
+++ egs/aesrc2020/asr1/local/preprocess.py
@@ -3,16 +3,16 @@
 
 import sys
 
-fin=open(sys.argv[1], 'r')
-fout_text = open(sys.argv[2], 'w')
-fout_utt2spk = open(sys.argv[3], 'w')
+fin = open(sys.argv[1], "r")
+fout_text = open(sys.argv[2], "w")
+fout_utt2spk = open(sys.argv[3], "w")
 
 for line in fin.readlines():
-    uttid, path = line.strip('\n').split('\t')
-    text_path = path.replace('.wav', '.txt')
-    text_ori = open(text_path, 'r').readlines()[0].strip('\n')
-    feild = path.split('/')
+    uttid, path = line.strip("\n").split("\t")
+    text_path = path.replace(".wav", ".txt")
+    text_ori = open(text_path, "r").readlines()[0].strip("\n")
+    feild = path.split("/")
     accid = feild[-3]
-    spkid = accid + '-' + feild[-2]
-    fout_utt2spk.write(uttid + '\t' + spkid + '\n')
-    fout_text.write(text_ori + '\n')
+    spkid = accid + "-" + feild[-2]
+    fout_utt2spk.write(uttid + "\t" + spkid + "\n")
+    fout_text.write(text_ori + "\n")
