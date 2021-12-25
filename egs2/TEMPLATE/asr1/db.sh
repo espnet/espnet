cc7bc9959eb8f2c2027a75432c2aa08c5df05283
Yifan Peng <yifanpen@cs.cmu.edu>
2021-12-24 20:29:47 -0500

    update grabo

--- egs2/TEMPLATE/asr1/db.sh
+++ egs2/TEMPLATE/asr1/db.sh
@@ -144,6 +144,7 @@ if [[ "$(hostname)" == tir.lti.cs.cmu.edu ]]; then
     BABEL_402=/projects/tir5/data/speech_corpora/babel/IARPA-babel402b-v1.0b-build/BABEL_OP3_402/
     BABEL_403=/projects/tir5/data/speech_corpora/babel/IARPA-babel403b-v1.0b-build/BABEL_OP3_403/
     BABEL_404=/projects/tir5/data/speech_corpora/babel/IARPA_BABEL_OP3_404/
+    GRABO=/projects/tir5/data/speech_corpora/Grabo
 fi
 
 # For only JHU environment
