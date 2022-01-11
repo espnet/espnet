ce8768c47600c660dc70b0bf5a31fc8a01f1a09f
ftshijt <728307998@qq.com>
2022-01-11 11:42:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ requirements = {
     ],
     # train: The modules invoked when training only.
     "train": [
-        "matplotlib==3.1.0",
+        "matplotlib==3.5.1",
         "pillow>=6.1.0",
         "editdistance==0.5.2",
         "wandb",
