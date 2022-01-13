a8ba47de1afeec428796b04f69c05aa787685193
Yushi Ueda <yueda@br013.ib.bridges2.psc.edu>
2022-01-13 14:42:21 -0500

    modify for CI test

--- test/espnet2/bin/test_diar_inference.py
+++ test/espnet2/bin/test_diar_inference.py
@@ -39,7 +39,7 @@ def config_file(tmp_path: Path):
 @pytest.mark.parametrize("batch_size", [1])
 @pytest.mark.parametrize(
     "input_size, segment_size, normalize_segment_scale, num_spk",
-    [(16000, None, False, 2), (35000, 2.4, False, 2), (35000, 2.4, True, 2)],
+    [(16000, None, False, 2), (35000, 2.4, False, 2), (34000, 2.4, True, 2)],
 )
 def test_DiarizeSpeech(
     config_file, batch_size, input_size, segment_size, normalize_segment_scale, num_spk
