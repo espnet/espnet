0dc9a50512ff31347685aa7a58bcf273b6bbc9ae
Heng-Jui Chang <b06901020@ntu.edu.tw>
2022-01-16 10:44:29 +0800

    make all data preprocessing in asr.sh

--- egs2/seame/asr1/run.sh
+++ egs2/seame/asr1/run.sh
@@ -16,36 +16,36 @@ asr_config=conf/tuning/train_asr_conformer.yaml
 lm_config=conf/tuning/train_lm_transformer.yaml
 inference_config=conf/decode_asr.yaml
 
-if [ ${stage} -eq 1 ] || [ ! -f "data/train/token.man.2" ]; then
+if [ ! -f "data/train/token.man.2" ]; then
     # must preprocess data first to get Mandarin character tokens
-    local/data.sh
+    if [ ${stage} -eq 1 ]; then
+        ./asr.sh --stage 1 --stop_stage 1
+        stage=2
+    else
+        echo "Error: data/train/token.man.2 does not exist! Run from stage=1 again."
+        exit 1
+    fi
 fi
 
-if [ ${stage} -eq 1 ]; then
-    stage=2
-fi
+source data/train/token.man.2  # for bpe_nlsyms & man_chars
+nbpe=$((3000 + man_chars + 4))  # 5626
+# English BPE: 3000 / Mandarin: 2622 / other symbols: 4
 
-if [ ${stop_stage} -ge 2 ]; then
-    source data/train/token.man.2  # for bpe_nlsyms & man_chars
-    nbpe=$((3000 + man_chars + 4))  # 5626
-    # English BPE: 3000 / Mandarin: 2622 / other symbols: 4
-
-    ./asr.sh \
-        --ngpu 2 \
-        --stage ${stage} \
-        --stop_stage ${stop_stage} \
-        --nbpe ${nbpe} \
-        --bpe_nlsyms "${bpe_nlsyms}" \
-        --speed_perturb_factors "0.9 1.0 1.1" \
-        --max_wav_duration 30 \
-        --asr_config "${asr_config}" \
-        --lm_config "${lm_config}" \
-        --inference_config "${inference_config}" \
-        --train_set "${train_set}" \
-        --valid_set "${valid_set}" \
-        --test_sets "${test_sets}" \
-        --lm_train_text "data/${train_set}/text" \
-        --bpe_train_text "data/${train_set}/text.eng.bpe" \
-        --score_opts "-e utf-8 -c NOASCII" \
-        "$@"
-fi
+./asr.sh \
+    --ngpu 2 \
+    --stage ${stage} \
+    --stop_stage ${stop_stage} \
+    --nbpe ${nbpe} \
+    --bpe_nlsyms "${bpe_nlsyms}" \
+    --speed_perturb_factors "0.9 1.0 1.1" \
+    --max_wav_duration 30 \
+    --asr_config "${asr_config}" \
+    --lm_config "${lm_config}" \
+    --inference_config "${inference_config}" \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" \
+    --lm_train_text "data/${train_set}/text" \
+    --bpe_train_text "data/${train_set}/text.eng.bpe" \
+    --score_opts "-e utf-8 -c NOASCII" \
+    "$@"
