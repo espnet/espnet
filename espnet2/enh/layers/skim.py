7d7686fec615bef79724eef1f11fd2c6dce56087
LiChenda <lichenda1996@sjtu.edu.cn>
2022-02-10 19:10:07 +0800

    update skim.py

--- espnet2/enh/layers/skim.py
+++ espnet2/enh/layers/skim.py
@@ -1,10 +1,11 @@
-# An implementation of SkiM model described in 
+# An implementation of SkiM model described in
 # "SkiM: Skipping Memory LSTM for Low-Latency Real-Time Continuous Speech Separation"
 # (https://arxiv.org/abs/2201.10800)
-# 
+#
 
 
 from turtle import forward, shape
+from black import main
 import torch
 import torch.nn as nn
 
@@ -13,34 +14,62 @@ from espnet2.enh.layers.tcn import chose_norm
 
 
 class MemLSTM(nn.Module):
-    """ the Mem-LSTM of SkiM
+    """the Mem-LSTM of SkiM
 
     args:
         hidden_size: int, dimension of the hidden state.
         dropout: float, dropout ratio. Default is 0.
         bidirectional: bool, whether the LSTM layers are bidirectional. Default is False.
-        mem_type: 'hc', 'h', 'c' or 'id'. 
+        mem_type: 'hc', 'h', 'c' or 'id'.
             It controls whether the hidden (or cell) state of SegLSTM will be processed by MemLSTM.
-            In 'id' mode, both the hidden and cell states will be identically returned.  
+            In 'id' mode, both the hidden and cell states will be identically returned.
         norm_type: gLN, cLN. cLN is for causal implementation.
     """
 
-    def __init__(self,hidden_size, dropout=0.0, bidirectional=False, mem_type='hc', norm_type='gLN'):
+    def __init__(
+        self,
+        hidden_size,
+        dropout=0.0,
+        bidirectional=False,
+        mem_type="hc",
+        norm_type="cLN",
+    ):
         super().__init__()
         self.hidden_size = hidden_size
         self.bidirectional = bidirectional
         self.input_size = (int(bidirectional) + 1) * hidden_size
         self.mem_type = mem_type
 
-        assert mem_type in ["hc", "h", 'c', 'id'], f"only support 'hc', 'h', 'c' and 'id', current type: {mem_type}"
+        assert mem_type in [
+            "hc",
+            "h",
+            "c",
+            "id",
+        ], f"only support 'hc', 'h', 'c' and 'id', current type: {mem_type}"
 
-        if mem_type in ["hc", 'h']:
-            self.h_net = SingleRNN('LSTM', input_size=self.input_size, hidden_size=self.hidden_size, dropout=dropout, bidirectional=bidirectional)
-            self.h_norm = chose_norm(norm_type=norm_type, channel_size=self.input_size, shape='BTD')
-        if mem_type in ["hc", 'c']:
-            self.c_net = SingleRNN('LSTM', input_size=self.input_size, hidden_size=self.hidden_size, dropout=dropout, bidirectional=bidirectional)
-            self.c_norm = chose_norm(norm_type=norm_type, channel_size=self.input_size, shape='BTD')
-    
+        if mem_type in ["hc", "h"]:
+            self.h_net = SingleRNN(
+                "LSTM",
+                input_size=self.input_size,
+                hidden_size=self.hidden_size,
+                dropout=dropout,
+                bidirectional=bidirectional,
+            )
+            self.h_norm = chose_norm(
+                norm_type=norm_type, channel_size=self.input_size, shape="BTD"
+            )
+        if mem_type in ["hc", "c"]:
+            self.c_net = SingleRNN(
+                "LSTM",
+                input_size=self.input_size,
+                hidden_size=self.hidden_size,
+                dropout=dropout,
+                bidirectional=bidirectional,
+            )
+            self.c_norm = chose_norm(
+                norm_type=norm_type, channel_size=self.input_size, shape="BTD"
+            )
+
     def extra_repr(self) -> str:
         return f"Mem_type: {self.mem_type}, bidirectional: {self.bidirectional}"
 
@@ -48,25 +77,25 @@ class MemLSTM(nn.Module):
         # hc = (h, c), tuple of hidden and cell states from SegLSTM
         # shape of h and c: (d, B*S, H)
         # S: number of segments in SegLSTM
-        
-        if self.mem_type == 'id':
+
+        if self.mem_type == "id":
             ret_val = hc
         else:
             h, c = hc
             d, BS, H = h.shape
             B = BS // S
-            h = h.transpose(1, 0).contiguous().view(B, S, d * H)  # B, S, dH  
-            c = c.transpose(1, 0).contiguous().view(B, S, d * H)  # B, S, dH  
-            if self.mem_type == 'hc':
+            h = h.transpose(1, 0).contiguous().view(B, S, d * H)  # B, S, dH
+            c = c.transpose(1, 0).contiguous().view(B, S, d * H)  # B, S, dH
+            if self.mem_type == "hc":
                 h = h + self.h_norm(self.h_net(h))
                 c = c + self.c_norm(self.c_net(c))
-            elif self.mem_type == 'h':
+            elif self.mem_type == "h":
                 h = h + self.h_norm(self.h_net(h))
                 c = torch.zeros_like(c)
-            elif self.mem_type == 'c':
+            elif self.mem_type == "c":
                 h = torch.zeros_like(h)
                 c = c + self.c_norm(self.c_net(c))
-            
+
             h = h.view(B * S, d, H).transpose(1, 0).contiguous()
             c = c.view(B * S, d, H).transpose(1, 0).contiguous()
             ret_val = (h, c)
@@ -76,38 +105,48 @@ class MemLSTM(nn.Module):
             causal_ret_val = []
             for x in ret_val:
                 x_ = torch.zeros_like(x)
-                x_[:, 1:, :] = x[:, :-1,:]
+                x_[:, 1:, :] = x[:, :-1, :]
                 causal_ret_val.append(x_)
             ret_val = tuple(causal_ret_val)
-        
+
         return ret_val
-   
-            
 
 
 class SegLSTM(nn.Module):
 
-    """ the Seg-LSTM of SkiM
+    """the Seg-LSTM of SkiM
 
     args:
         input_size: int, dimension of the input feature. The input should have shape
-                    (batch, seq_len, input_size).  
+                    (batch, seq_len, input_size).
         hidden_size: int, dimension of the hidden state.
         dropout: float, dropout ratio. Default is 0.
         bidirectional: bool, whether the LSTM layers are bidirectional. Default is False.
+        norm_type: gLN, cLN. cLN is for causal implementation.
     """
 
-    def __init__(self, input_size, hidden_size, dropout=0.0, bidirectional=False):
+    def __init__(
+        self, input_size, hidden_size, dropout=0.0, bidirectional=False, norm_type="cLN"
+    ):
         super().__init__()
 
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_direction = int(bidirectional) + 1
 
-        self.lstm = nn.LSTM(input_size, hidden_size, 1,batch_first=True,bidirectional=bidirectional,)
+        self.lstm = nn.LSTM(
+            input_size,
+            hidden_size,
+            1,
+            batch_first=True,
+            bidirectional=bidirectional,
+        )
         self.dropout = nn.Dropout(p=dropout)
-        self.proj = nn.Linear(hidden_size * self.num_direction, input_size)  
-    
+        self.proj = nn.Linear(hidden_size * self.num_direction, input_size)
+        self.norm = chose_norm(
+            norm_type=norm_type, channel_size=hidden_size, shape="BTD"
+        )
+
     def forward(self, input, hc):
         # input shape: B, T, H
 
@@ -121,12 +160,111 @@ class SegLSTM(nn.Module):
         else:
             h, c = hc
 
-        output, (h, c) = self.lstm(input, (h, c)) 
+        output, (h, c) = self.lstm(input, (h, c))
         output = self.dropout(output)
-        output = self.proj(
-            output.contiguous().view(-1, output.shape[2])
-        ).view(output.shape)       
+        output = self.proj(output.contiguous().view(-1, output.shape[2])).view(
+            output.shape
+        )
+        output = input + self.norm(output)
 
         return output, (h, c)
 
 
+class SkiM(nn.Module):
+    def __init__(
+        self,
+        input_size,
+        hidden_size,
+        output_size,
+        dropout=0.0,
+        num_blocks=2,
+        segment_size=20,
+        bidirectional=True,
+        mem_type="hc",
+        norm_type="gLN",
+        seg_overlap=False,
+    ):
+        super().__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        self.segment_size = segment_size
+        self.dropout = dropout
+        self.num_blocks = num_blocks
+        self.mem_type = mem_type
+        self.norm_type = norm_type
+        self.seg_overlap = seg_overlap
+
+        self.seg_lstms = nn.ModuleList([])
+        for i in range(num_blocks):
+            self.seg_lstms.append(
+                SegLSTM(
+                    input_size=input_size,
+                    hidden_size=hidden_size,
+                    dropout=dropout,
+                    bidirectional=bidirectional,
+                    norm_type=norm_type,
+                )
+            )
+        if self.mem_type is not None:
+            self.mem_lstms = nn.ModuleList([])
+            for i in range(num_blocks - 1):
+                self.mem_lstms.append(
+                    MemLSTM(
+                        hidden_size,
+                        dropout=dropout,
+                        bidirectional=bidirectional,
+                        mem_type=mem_type,
+                        norm_type=norm_type,
+                    )
+                )
+        self.output_fc = nn.Sequential(nn.PReLU(), nn.Conv1d(input_size, output_size, 1))
+
+    def forward(self, input):
+        # input shape: B, T (S*K), D
+        B, T, D = input.shape
+
+        if self.seg_overlap:
+            input, rest = split_feature(input.transpose(1, 2),
+                                        segment_size=self.segment_size)  # B, D, K, S
+            input = input.permute(0, 3, 2, 1).contiguous() # B, S, K, D
+        else:
+            input, rest = self._padfeature(input=input)
+            input = input.view(B, -1, self.segment_size, D)  # B, S, K, D
+        B, S, K, D = input.shape
+
+        assert K == self.segment_size
+
+        output = input.view(B * S, K, D).contiguous()  # BS, K, D
+        hc = None
+        for i in range(self.num_blocks):
+            output, hc = self.seg_lstms[i](output, hc)  # BS, K, D
+            if self.mem_type and i < self.num_blocks - 1:
+                hc = self.mem_lstms[i](hc, S)
+        
+        if self.seg_overlap:
+            output = output.view(B, S, K, D).permute(0, 3, 2, 1) # B, D, K, S
+            output = merge_feature(output, rest) # B, D, T
+            output = self.output(output).transpose(1, 2)
+
+        else:
+            output = output.view(B, S * K, D)[:, :T, :]  # B, T, D
+            output = self.output(output.transpose(1, 2)).transpose(1, 2)
+        
+        return output
+
+    def _padfeature(self, input):
+        B, T, D = input.shape
+        rest = self.segment_size - T % self.segment_size
+
+        if rest > 0:
+            pad = torch.zeros(B, rest, D, device=input.device)
+            input = torch.cat([input, pad], dim=1)
+        return input, rest
+
+
+if __name__ == "__main__":
+
+    model = SkiM(256, 123, 345, dropout=0.1, num_blocks=3, segment_size=20,bidirectional=True, mem_type='hc', norm_type='gLN', seg_overlap=False)
+    input = torch.randn(2, 1002, 256)
+    print(model(input).shape)
