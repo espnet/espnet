ac9c651f9facacf14923017ce6da4a797011ae27
roshansh-cmu <roshansh@andrew.cmu.edu>
2022-02-15 18:06:27 -0500

    pep fixes

--- espnet/nets/pytorch_backend/transformer/longformer_attention.py
+++ espnet/nets/pytorch_backend/transformer/longformer_attention.py
@@ -1,17 +1,25 @@
-from torch import nn
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-from longformer.longformer import LongformerSelfAttention, LongformerConfig
+# Copyright 2022 Roshan Sharma (Carnegie Mellon University)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+"""Longformer based Local Attention Definition """
+
+import torch
+from torch import nn
+from longformer.longformer import LongformerSelfAttention
+from longformer.longformer import LongformerConfig
 
 
 class LongformerAttention(nn.Module):
     def __init__(self, config: LongformerConfig, layer_id: int):
-        """Longformer Self-Attention Wrapper.
+        """Does Longformer based Self-Attention.
 
         Args:
             config : Longformer attention configuration
             layer_id: Integer representing the layer index
         """
-        assert check_argument_types()
         super().__init__()
         self.attention_window = config.attention_window[layer_id]
         self.attention_layer = LongformerSelfAttention(config, layer_id=layer_id)
@@ -20,13 +28,9 @@ class LongformerAttention(nn.Module):
     def forward(self, query, key, value, mask):
         """Computes Longformer Self-Attention with masking.
 
-        Expects `len(hidden_states)` to be multiple of `attention_window`.Padding to `attention_window` happens in :meth:`encoder.forward` to avoid redoing the padding on each layer.
-        The `attention_mask` is changed in :meth:`LongformerAttention.forward`
-        from 0, 1, 2 to:
-            * -1: no attention
-            * 0: local attention
-            * +1: global attention
-
+        Expects `len(hidden_states)` to be multiple of `attention_window`.
+        Padding to `attention_window` happens in :meth:`encoder.forward`
+        to avoid redoing the padding on each layer.
         Args:
             query (torch.Tensor): Query tensor (#batch, time1, size).
             key (torch.Tensor): Key tensor (#batch, time2, size).
