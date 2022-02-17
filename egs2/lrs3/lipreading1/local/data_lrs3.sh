de43d9d893876af13ef8e6bb2d81bd035a65896b
Debayan Ghosh <debayang@br012.ib.bridges2.psc.edu>
2022-02-17 16:57:33 -0500

    v1 run

--- egs2/lrs3/lipreading1/local/data_lrs3.sh
+++ egs2/lrs3/lipreading1/local/data_lrs3.sh
@@ -15,13 +15,34 @@ log() {
 . ./cmd.sh
 
 cmd=run.pl
+log_dir=./local/python_debug_log
 nj=1
 
 echo ${LRS3}
 
-log_dir=./local/python_debug_log
+# for dataset in trainval test; do
+#     for mp4_path in ${LRS3}/${dataset}/*/*.mp4; do
+#         wav_path=${mp4_path//.mp4/.wav}
+#         ffmpeg -y -i ${mp4_path} -loglevel panic -ar 16000 -ac 1 ${wav_path} 
+#     done
+# done
 
-$cmd JOB=1:$nj ${log_dir}_${nj}.log python ./local/data_prep.py --train_val_path ${LRS3}/trainval --test_path ${LRS3}/test 
+# Make the Folders where ESPNet essential files will be stored
+# for dataset in train dev test; do
+#     mkdir -p ./data/${dataset}
+# done
 
-# output=(python ./local/data_prep.py --train_val_path ${LRS3}/trainval --test_path ${LRS3}/test)
-# echo 4[]
+# $cmd JOB=1:$nj ${log_dir}.JOB.log python ./local/data_prep.py --train_val_path ${LRS3}/trainval --test_path ${LRS3}/test 
+
+utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
+utils/utt2spk_to_spk2utt.pl data/dev/utt2spk > data/dev/spk2utt
+utils/utt2spk_to_spk2utt.pl data/test/utt2spk > data/test/spk2utt
+
+utils/fix_data_dir.sh data/train
+utils/fix_data_dir.sh data/test
+utils/fix_data_dir.sh data/dev
+
+utils/validate_data_dir.sh data/train --no-feats
+utils/validate_data_dir.sh data/test --no-feats
+utils/validate_data_dir.sh data/dev --no-feats
+
--- egs2/lrs3/lipreading1/run.sh
+++ egs2/lrs3/lipreading1/run.sh
@@ -6,18 +6,33 @@ set -u
 set -o pipefail
 
 train_set="train"
-valid_set="val"
+valid_set="dev"
 test_set="test "
 
+test_sets="test dev"
+
+
+asr_tag=conformer_lr2e-3_warmup15k_amp_nondeterministic
+asr_config=conf/train_asr_transformer.yaml
+
 ./asr.sh \
+    --skip_data_prep false \
+    --skip_train false \
+    --skip_eval false \
     --lang en \
-    --train_set ${train_set} \
-    --token_type bpe\
-    --nbpe 200\
-    --lm_config conf/train_lm.yaml \
-    --valid_set ${valid_set} \
-    --test_sets ${test_set} \
-    --feats_type extracted \
-    --asr_config conf/train_asr_transformer.yaml \
+    --ngpu 1 \
+    --nj 32 \
+    --inference_nj 32 \
+    --nbpe 5000 \
+    --max_wav_duration 30 \
+    --speed_perturb_factors "0.9 1.0 1.1" \
+    --audio_format "wav" \
+    --feats_type raw \
+    --use_lm false \
+    --asr_tag "${asr_tag}" \
+    --asr_config "${asr_config}" \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" \
     --lm_train_text "data/${train_set}/text" \
     --bpe_train_text "data/${train_set}/text" "$@"
