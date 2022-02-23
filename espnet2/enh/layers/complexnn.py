4c845283862b33052872ce45e2d7a5f233950f39
LinShaoxiong <Johnson-Lin@sjtu.edu.cn>
2022-02-23 22:48:44 +0800

    Removed some unused code

--- espnet2/enh/layers/complexnn.py
+++ espnet2/enh/layers/complexnn.py
@@ -16,28 +16,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def get_casual_padding1d():
-    pass
-
-
-def get_casual_padding2d():
-    pass
-
-
-class cPReLU(nn.Module):
-    def __init__(self, complex_axis=1):
-        super(cPReLU, self).__init__()
-        self.r_prelu = nn.PReLU()
-        self.i_prelu = nn.PReLU()
-        self.complex_axis = complex_axis
-
-    def forward(self, inputs):
-        real, imag = torch.chunk(inputs, 2, self.complex_axis)
-        real = self.r_prelu(real)
-        imag = self.i_prelu(imag)
-        return torch.cat([real, imag], self.complex_axis)
-
-
 class NavieComplexLSTM(nn.Module):
     def __init__(
         self,
