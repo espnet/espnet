38861d22a38192cd4ef1ac8f506f35ec485dbdb3
wentao <yuwentaoxandry@gmail.com>
2022-03-02 09:55:01 +0100

    remove sudo

--- egs/lrs/avsr1/local/installpackage.sh
+++ egs/lrs/avsr1/local/installpackage.sh
@@ -12,81 +12,73 @@ OPENFACE_DIR=$1			# Path to OpenFace build directory
 VIDAUG_DIR=$2 			# Path to vidaug directory
 DEEPXI_DIR=$3			# DeepXi directory
 
-conda install -n espnet_venv tensorflow tqdm pysoundfile
+conda install -n espnet_venv tensorflow tqdm pysoundfile boost
+conda install -n espnet_venv dlib pythran-openblas==0.3.6 opencv-python
 conda install -c esri tensorflow-addons
-while true
-do
-    read -r -p "For this recipe you will need sudo right to install OpenFace, do you want to continue? [Y/n] " input
-    case $input in 
-	[yY][eE][sS]|[yY])
-	    mkdir -p local/installations
-    	    if [ -d "$OPENFACE_DIR" ] ; then
-		echo "OpenFace already installed."
-    	    else
-    		while true
-    		do
-        	    read -r -p "Have you already installed OpenFace on your computer [Y/n] " input
-        	    case $input in 
-	    		[yY][eE][sS]|[yY])
-			    echo "Please path OpenFace directory"
-			    exit 1;
-	        	    ;;
-            		[nN][oO]|[nN])
-		    	   cd local/installations
-		    	   $MAIN_ROOT/tools/installers/install_openface.sh || exit 1;
-		    	   cd ../..
-			   ;;
-        	    esac      
-    		done
-    	    fi
-    	    if [ -d "$VIDAUG_DIR" ] ; then
-		echo "Vidaug already installed."
-    	    else
-    		while true
-    		do
-        	    read -r -p "Have you already installed Vidaug on your computer [Y/n] " input
-        	    case $input in 
-	    		[yY][eE][sS]|[yY])
-			    echo "Please path Vidaug directory"
-			    exit 1;
-	        	    ;;
-            		[nN][oO]|[nN])
-		    	    cd local/installations
-		            $MAIN_ROOT/tools/installers/install_vidaug.sh || exit 1;
-		    	    cd ../..
-			    ;;
-        	    esac      
-    		done
-    	    fi
-    	    if [ -d "$DEEPXI_DIR" ] ; then
-		echo "DeepXi already installed."
-    	    else
-    		while true
-    		do
-        	    read -r -p "Have you already installed DeepXi on your computer [Y/n] " input
-         	    case $input in 
-	    		[yY][eE][sS]|[yY])
-			    echo "Please path DeepXi directory"
-			    exit 1;
-	        	    ;;
-            		[nN][oO]|[nN])
-		            cd local/installations
-		    	    $MAIN_ROOT/tools/installers/install_deepxi.sh || exit 1;
-		    	    cd ../..
-			    ;;
-        	    esac      
-    		done
-    	    fi
-	    break
-	    ;;
-        [nN][oO]|[nN])
-            exit 1;
-	    ;;
-        *)
-            echo "Invalid choice..."
-            exit 1;
-            ;;
-    esac      
-done
+
+mkdir -p local/installations
+if [ -d "$OPENFACE_DIR" ] ; then
+    echo "OpenFace already installed."
+else
+    while true
+    do
+        read -r -p "Have you already installed OpenFace on your computer [Y/n] " input
+        case $input in 
+	    [yY][eE][sS]|[yY])
+		echo "Please path OpenFace directory"
+		exit 1;
+	        ;;
+            [nN][oO]|[nN])
+		cd local/installations
+		$MAIN_ROOT/tools/installers/install_openface.sh || exit 1;
+		cd ../..
+     		break
+		;;
+        esac      
+    done
+fi
+
+if [ -d "$VIDAUG_DIR" ] ; then
+    echo "Vidaug already installed."
+else
+    while true
+    do
+        read -r -p "Have you already installed Vidaug on your computer [Y/n] " input
+        case $input in 
+	    [yY][eE][sS]|[yY])
+		echo "Please path Vidaug directory"
+		exit 1;
+	        ;;
+            [nN][oO]|[nN])
+		cd local/installations
+		$MAIN_ROOT/tools/installers/install_vidaug.sh || exit 1;
+		cd ../..
+     		break
+		;;
+       	 esac      
+    done
+fi
+
+if [ -d "$DEEPXI_DIR" ] ; then
+    echo "DeepXi already installed."
+else
+    while true
+    do
+        read -r -p "Have you already installed DeepXi on your computer [Y/n] " input
+        case $input in 
+	    [yY][eE][sS]|[yY])
+		echo "Please path DeepXi directory"
+		exit 1;
+	        ;;
+            [nN][oO]|[nN])
+		cd local/installations
+		$MAIN_ROOT/tools/installers/install_deepxi.sh || exit 1;
+		cd ../..
+     		break
+		;;
+        esac      
+    done
+fi
+
 
 exit 0
--- tools/installers/install_openface.sh
+++ tools/installers/install_openface.sh
@@ -27,92 +27,14 @@ if [ `lsb_release -d` != "18.04" ] || [ `lsb_release -d` != "20.04" ]; then
     exit 1
 fi
 
-
-# Essential Dependencies
-echo "Installing Essential dependencies..."
-sudo apt-get update
-sudo apt-get -y install build-essential
-sudo apt-get -y install g++-8
-sudo apt-get -y install zip
-sudo apt-get -y install libopenblas-dev liblapack-dev
-sudo apt-get -y install libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev
-sudo apt-get -y install libtbb2 libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
-echo "Essential dependencies installed."
-
-# Install cmake
-cmake_install=$1
-if [[ "$cmake_install" == INSTALL_CMAKE ]]; then
-    echo "Installing specific cmake version 3.20.0"
-    echo "Downloading cmake 3.20.0"
-    wget https://github.com/Kitware/CMake/releases/download/v3.20.0/cmake-3.20.0.tar.gz
-    tar -zxvf cmake-3.20.0.tar.gz
-    sudo rm -r cmake-3.20.0.tar.gz
-    cd cmake-3.20.0
-    sudo ./bootstrap
-    make
-    sudo make install
-    export PATH="`pwd`/cmake-3.20.0/bin:$PATH"
-    cd ..
-elif [[ "$cmake_install" == INSTALL_CMAKE_PERMANENT ]]; then
-    echo "Installing specific cmake version 3.20.0 and add it to path"
-    echo "Downloading cmake 3.20.0"
-    wget https://github.com/Kitware/CMake/releases/download/v3.20.0/cmake-3.20.0.tar.gz
-    tar -zxvf cmake-3.20.0.tar.gz
-    sudo rm -r cmake-3.20.0.tar.gz
-    cd cmake-3.20.0
-    sudo ./bootstrap
-    make
-    sudo make install
-    echo "Add cmake to path"
-    DIR_CMAKE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-    cd
-    sudo echo \export PATH="$DIR_CMAKE/bin:\$PATH" >> .bashrc
-    cd $DIR_CMAKE
-    cd ..
-else
-    echo "Installing latest cmake version"
-    sudo apt-get -y install cmake
-fi
-
-# Get OpenBLAS
-sudo apt-get install libopenblas-dev
-
-# Download and Compile OpenCV 4.1.0
-echo "Downloading OpenCV..."
-wget https://github.com/opencv/opencv/archive/4.1.0.zip
-unzip 4.1.0.zip
-cd opencv-4.1.0
-mkdir -p build
-cd build
-echo "Installing OpenCV..."
-sudo cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D BUILD_TIFF=ON -D WITH_TBB=ON ..
-sudo make -j$(nproc)
-sudo make install
-cd ../..
-rm 4.1.0.zip
-sudo rm -r opencv-4.1.0
-echo "OpenCV installed."
-
-# Download and Compile dlib
-echo "Downloading dlib"
-wget http://dlib.net/files/dlib-19.13.tar.bz2
-tar xf dlib-19.13.tar.bz2
-cd dlib-19.13
-mkdir -p build
-cd build
-echo "Installing dlib"
-cmake ..
-cmake --build . --config Release
-sudo make install
-sudo ldconfig
-cd ../..   
-rm -r dlib-19.13.tar.bz2
-echo "dlib installed"
-
 # OpenFace installation
 echo "Downloading OpenFace"
 git clone https://github.com/TadasBaltrusaitis/OpenFace.git
 cd OpenFace
+rm -rf CMakeLists.txt
+cd ../..
+cp CMakeLists.txt installations/OpenFace
+cd installations/OpenFace
 echo "Installing OpenFace..."
 mkdir -p build
 cd build
