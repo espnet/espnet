73a2d230ffe3eec98d56fa181cccf19ff041b90c
Jiatong Shi <jiatong@br013.ib.bridges2.psc.edu>
2022-03-10 03:14:00 -0500

    apply black

--- egs2/dsing/asr1/local/data_prep.py
+++ egs2/dsing/asr1/local/data_prep.py
@@ -24,13 +24,17 @@ class DataSet:
         arrangement, performance, country, gender, user = recording[:-4].split("-")
 
         # the following mapping is necessary for errors in gender in country IN
-        insensitive_none = re.compile(re.escape('none'), re.IGNORECASE)
+        insensitive_none = re.compile(re.escape("none"), re.IGNORECASE)
 
-        gender = insensitive_none.sub('', utt["gender"])
-        spk = "{}{}".format(insensitive_none.sub('', gender).upper(), insensitive_none.sub('', user))
+        gender = insensitive_none.sub("", utt["gender"])
+        spk = "{}{}".format(
+            insensitive_none.sub("", gender).upper(), insensitive_none.sub("", user)
+        )
 
         rec_id = recording[:-4]
-        utt_id = "{}-{}-{}-{}-{}-{:03}".format(spk, arrangement, performance, country, gender.upper(), utt["index"])
+        utt_id = "{}-{}-{}-{}-{}-{:03}".format(
+            spk, arrangement, performance, country, gender.upper(), utt["index"]
+        )
 
         start = utt["start"]
         end = utt["end"]
@@ -102,9 +106,19 @@ def map_rec2chec(db_path, countries):
     """
     rec2chec = {}
     for country in countries:
-        recordings = [f for f in listdir(join(db_path, country, country + "Vocals")) if f.endswith(".m4a")]
+        recordings = [
+            f
+            for f in listdir(join(db_path, country, country + "Vocals"))
+            if f.endswith(".m4a")
+        ]
         for record in recordings:
-            rec2chec[hashlib.md5(open(join(db_path, country, country + "Vocals", record), 'rb').read()).hexdigest()] = record
+            rec2chec[
+                hashlib.md5(
+                    open(
+                        join(db_path, country, country + "Vocals", record), "rb"
+                    ).read()
+                ).hexdigest()
+            ] = record
 
     return rec2chec
 
@@ -117,9 +131,39 @@ def main(args):
 
     countries = ["GB"]
     countries += ["US", "AU"] if dset in ["train3", "train30"] else []
-    countries += ['AE', 'AR', 'BR', 'CL', 'CN', 'DE', 'ES', 'FR', 'HU',
-                  'ID', 'IN', 'IQ', 'IR', 'IT', 'JP', 'KR', 'MX', 'MY',
-                  'NO', 'PH', 'PT', 'RU', 'SA', 'SG', 'TH', 'VN', 'ZA'] if dset in ["train30"] else []
+    countries += (
+        [
+            "AE",
+            "AR",
+            "BR",
+            "CL",
+            "CN",
+            "DE",
+            "ES",
+            "FR",
+            "HU",
+            "ID",
+            "IN",
+            "IQ",
+            "IR",
+            "IT",
+            "JP",
+            "KR",
+            "MX",
+            "MY",
+            "NO",
+            "PH",
+            "PT",
+            "RU",
+            "SA",
+            "SG",
+            "TH",
+            "VN",
+            "ZA",
+        ]
+        if dset in ["train30"]
+        else []
+    )
 
     performances = map_rec2chec(db_path, countries)
     utterances = read_json(utts_path)
@@ -131,12 +175,18 @@ def main(args):
     dataset.save()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("workspace", type=str, help="Path where the output files will be saved")
+    parser.add_argument(
+        "workspace", type=str, help="Path where the output files will be saved"
+    )
     parser.add_argument("db_path", type=str, help="Path to DAMP 300x30x2 database")
-    parser.add_argument("utterances", type=str, help="Path to utterance details in json format",
-                        default="metadata.json")
+    parser.add_argument(
+        "utterances",
+        type=str,
+        help="Path to utterance details in json format",
+        default="metadata.json",
+    )
     parser.add_argument("dset", type=str, help="Name of the dataset")
 
     args = parser.parse_args()
