aa5bfa550083033b3eccce319b588bab5b7bb5ce
Yosuke Kashiwagi <yosuke.kashiwagi.0530@gmail.com>
2022-03-10 10:05:14 -0500

    add speaker open setting in egs2/vctk/asr1

--- egs2/vctk/asr1/run.sh
+++ egs2/vctk/asr1/run.sh
@@ -5,6 +5,8 @@ set -e
 set -u
 set -o pipefail
 
+# if ture, speaker open setting will be used.
+use_speakeropen=false
 train_set=tr_no_dev
 valid_set=dev
 test_sets="dev eval1"
@@ -21,7 +23,9 @@ speed_perturb_factors="1.1 0.9 1.0"
 ./asr.sh \
     --ngpu 4 \
     --token_type "${token_type}" \
-    --feats_type fbank_pitch \
+    --feats_type raw \
+    --fs 16k \
+    --local_data_opts "--use_speakeropen ${use_speakeropen}" \
     --train_set "${train_set}" \
     --valid_set "${valid_set}" \
     --test_sets "${test_sets}" \
