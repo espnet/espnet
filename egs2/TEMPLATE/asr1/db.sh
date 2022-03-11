558b5d42d27cce0e038abef066921a50065d7ffe
Karthik Ganesan <Karthikganesan17@gmail.com>
2022-03-11 04:05:28 -0500

    Update db.sh

--- egs2/TEMPLATE/asr1/db.sh
+++ egs2/TEMPLATE/asr1/db.sh
@@ -1,6 +1,7 @@
 # Set the path of your corpus
 # "downloads" means the corpus can be downloaded by the recipe automatically
 
+DSTC2=downloads
 AIDATATANG_200ZH=downloads
 AISHELL=downloads
 AISHELL3=downloads
