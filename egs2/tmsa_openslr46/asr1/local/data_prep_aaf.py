57d75fd29d1069f4e3c38b6f9b3ffbba5a018055
mvijay97 <malaika.vijay@gmail.com>
2022-03-13 16:16:44 -0400

    Add fix to data preparation

--- egs2/tmsa_openslr46/asr1/local/data_prep_aaf.py
+++ egs2/tmsa_openslr46/asr1/local/data_prep_aaf.py
@@ -18,11 +18,14 @@ TRAIN_SPEECH_DIR = os.path.join(DATASET_DIR, "speech/train")
 TRAIN_SPEECH_CA16_DIR = os.path.join(TRAIN_SPEECH_DIR, "ca16")
 TRAIN_SPEECH_YAOUNDE_DIR = os.path.join(TRAIN_SPEECH_DIR, "yaounde")
 
+utterances = set()
+
 for speaker in os.listdir(TRAIN_SPEECH_CA16_DIR):
     for utterance in os.listdir(os.path.join(TRAIN_SPEECH_CA16_DIR, speaker)):
         src = os.path.join(os.path.join(TRAIN_SPEECH_CA16_DIR, speaker, utterance))
         dest = os.path.join(DEST_DIR, 'train', utterance)
         shutil.copy(src, dest)
+        utterances.add(utterance[0:-4])
 
 
 utterance_types = ["read", "answers"]
@@ -32,9 +35,10 @@ for utt_type in utterance_types:
             src = os.path.join(TRAIN_SPEECH_YAOUNDE_DIR, utt_type, speaker, utterance)
             dest = os.path.join(DEST_DIR, 'train', utt_type + '_' + utterance)
             shutil.copy(src, dest)
+            utterances.add(utt_type + '_' + utterance[0:-4])
 
 # write the transcripts for training data
-
+print(utterances)
 TRAIN_TRANSCRIPT_DIR = os.path.join(DATASET_DIR, "transcripts/train")
 
 with open(os.path.join(DATA_DIR, 'train', 'text'), 'w') as f_text:
@@ -45,24 +49,26 @@ with open(os.path.join(DATA_DIR, 'train', 'text'), 'w') as f_text:
                     for line in f_trans:
                         utt_id, text = line.split(' ', 1)
                         utt_id = utt_id[0: -4]
-                        f_text.write(utt_id + " " + text)
+                        if utt_id in utterances:
+                            f_text.write(utt_id + " " + text)
 
-                        wav_path = os.path.join('egs2/tmsa_openslr46/asr1', DEST_DIR, 'train', utt_id)
-                        f_wav.write(utt_id + " " + wav_path + "\n")
+                            wav_path = os.path.join('egs2/tmsa_openslr46/asr1', DEST_DIR, 'train', utt_id)
+                            f_wav.write(utt_id + " " + wav_path + "\n")
 
-                        f_utt2spk.write(utt_id+ " "+ utt_id + "\n")
-                        f_spk2utt.write(utt_id + " " + utt_id + "\n")
+                            f_utt2spk.write(utt_id+ " "+ utt_id + "\n")
+                            f_spk2utt.write(utt_id + " " + utt_id + "\n")
 
                 with open(os.path.join(TRAIN_TRANSCRIPT_DIR, 'ca16_read', 'conditioned.txt')) as f_trans:
                     for line in f_trans:
                         utt_id, text = line.split(' ', 1)
-                        f_text.write(utt_id + " " + text)
+                        if utt_id in utterances:
+                            f_text.write(utt_id + " " + text)
 
-                        wav_path = os.path.join('egs2/tmsa_openslr46/asr1', DEST_DIR, 'train', utt_id)
-                        f_wav.write(utt_id + " " + wav_path + "\n")
+                            wav_path = os.path.join('egs2/tmsa_openslr46/asr1', DEST_DIR, 'train', utt_id)
+                            f_wav.write(utt_id + " " + wav_path + "\n")
 
-                        f_utt2spk.write(utt_id + " " + utt_id + "\n")
-                        f_spk2utt.write(utt_id + " " + utt_id + "\n")
+                            f_utt2spk.write(utt_id + " " + utt_id + "\n")
+                            f_spk2utt.write(utt_id + " " + utt_id + "\n")
 
                 with open(os.path.join(TRAIN_TRANSCRIPT_DIR, 'yaounde', 'fn_text.txt')) as f_trans:
                     for line in f_trans:
@@ -72,24 +78,26 @@ with open(os.path.join(DATA_DIR, 'train', 'text'), 'w') as f_text:
                             utt_id = 'read' + '_' + file[-1][0: -4]
                         else:
                             utt_id = 'answers' + '_' + file[-1][0: -4]
+                        if utt_id in utterances:
+                            f_text.write(utt_id + " " + text)
 
-                        f_text.write(utt_id + " " + text)
+                            wav_path = os.path.join('egs2/tmsa_openslr46/asr1', DEST_DIR, 'train', utt_id)
+                            f_wav.write(utt_id + " " + wav_path + "\n")
 
-                        wav_path = os.path.join('egs2/tmsa_openslr46/asr1', DEST_DIR, 'train', utt_id)
-                        f_wav.write(utt_id + " " + wav_path + "\n")
-
-                        f_utt2spk.write(utt_id + " " + utt_id + "\n")
-                        f_spk2utt.write(utt_id + " " + utt_id + "\n")
+                            f_utt2spk.write(utt_id + " " + utt_id + "\n")
+                            f_spk2utt.write(utt_id + " " + utt_id + "\n")
 
 # prepare the test data
 
 TEST_SPEECH_DIR = os.path.join(DATASET_DIR, "speech/test")
+utterances = set()
 
 for speaker in os.listdir(os.path.join(TEST_SPEECH_DIR, 'ca16')):
     for utterance in os.listdir(os.path.join(TEST_SPEECH_DIR, 'ca16', speaker)):
         src = os.path.join(TEST_SPEECH_DIR, 'ca16', speaker, utterance)
         dest = os.path.join(DEST_DIR, 'test', utterance)
         shutil.copy(src, dest)
+        utterances.add(utterance[0:-4])
 
 # write the transcripts for test data
 
@@ -102,23 +110,25 @@ with open(os.path.join(DATA_DIR, 'test', 'text'), 'w') as f_text:
                 with open(TEST_TRANSCRIPT_PATH, 'r') as f_trans:
                     for line in f_trans:
                         utt_id, text = line.split(' ', 1)
-                        f_text.write(line)
+                        if utt_id in utterances:
+                            f_text.write(line)
 
-                        wav_path = os.path.join('egs2/tmsa_openslr46/asr1', DEST_DIR, 'test', utt_id)
-                        f_wav.write(utt_id + " " +  wav_path + "\n")
+                            wav_path = os.path.join('egs2/tmsa_openslr46/asr1', DEST_DIR, 'test', utt_id)
+                            f_wav.write(utt_id + " " +  wav_path + "\n")
 
-                        f_utt2spk.write(utt_id + " " + utt_id + "\n")
-                        f_spk2utt.write(utt_id + " " + utt_id + "\n")
+                            f_utt2spk.write(utt_id + " " + utt_id + "\n")
+                            f_spk2utt.write(utt_id + " " + utt_id + "\n")
 
 # prepare the validation data
 
 VALID_SPEECH_DIR = os.path.join(DATASET_DIR, "speech/devtest")
-
+utterances = set()
 for speaker in os.listdir(os.path.join(VALID_SPEECH_DIR, 'ca16')):
     for utterance in os.listdir(os.path.join(VALID_SPEECH_DIR, 'ca16', speaker)):
         src = os.path.join(VALID_SPEECH_DIR, 'ca16', speaker, utterance)
         dest = os.path.join(DEST_DIR, 'valid', utterance)
         shutil.copy(src, dest)
+        utterances.add(utterance[0:-4])
 
 
 # write the transcripts for validation data
@@ -132,11 +142,12 @@ with open(os.path.join(DATA_DIR, 'valid', 'text'), 'w') as f_text:
                 with open(VALID_TRANSCRIPT_PATH, 'r') as f_trans:
                     for line in f_trans:
                         utt_id, text = line.split(' ', 1)
-                        f_text.write(line)
+                        if utt_id in utterances:
+                            f_text.write(line)
 
-                        wav_path = os.path.join('egs2/tmsa_openslr46/asr1', DEST_DIR, 'test', utt_id)
-                        f_wav.write(utt_id + " " + wav_path + "\n")
+                            wav_path = os.path.join('egs2/tmsa_openslr46/asr1', DEST_DIR, 'valid', utt_id)
+                            f_wav.write(utt_id + " " + wav_path + "\n")
 
-                        f_utt2spk.write(utt_id + " " + utt_id + "\n")
-                        f_spk2utt.write(utt_id + " " + utt_id + "\n")
+                            f_utt2spk.write(utt_id + " " + utt_id + "\n")
+                            f_spk2utt.write(utt_id + " " + utt_id + "\n")
 
