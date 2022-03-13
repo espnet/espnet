bed40ba89c4c1a56856dba40d509ad594caa4db3
mvijay97 <malaika.vijay@gmail.com>
2022-03-13 15:06:11 -0400

    Add Recipe for African Accented French

---
+++ egs2/tmsa_openslr46/asr1/local/data.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+. ./path.sh || exit 1;
+. ./cmd.sh || exit 1;
+. ./db.sh || exit 1;
+
+# general configuration
+stage=0       # start from 0 if you need to start from data preparation
+stop_stage=1
+# inclusive, was 100
+SECONDS=0
+
+log() {
+    local fname=${BASH_SOURCE[1]##*/}
+    echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
+}
+
+
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+. utils/parse_options.sh
+
+log "data preparation started"
+
+DATA_DEST=data_asr
+mkdir -p ${DATA_DEST}
+mkdir -p ${DATA_DEST}/train
+mkdir -p ${DATA_DEST}/test
+mkdir -p ${DATA_DEST}/valid
+
+DATA_KALDI=data
+mkdir -p ${DATA_KALDI}
+mkdir -p ${DATA_KALDI}/train
+mkdir -p ${DATA_KALDI}/test
+mkdir -p ${DATA_KALDI}/valid
+
+workspace=$PWD
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+    log "sub-stage 0: Download Data to downloads"
+
+    wget https://www.openslr.org/resources/57/African_Accented_French.tar.gz
+    tar -xvf African_Accented_French.tar.gz
+    rm -f African_Accented_French.tar.gz
+
+fi
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    log "sub-stage 1: Preparing Data for openslr"
+
+    python3 local/data_prep_aaf.py --data-src African_Accented_French --data-dest ${DATA_DEST} --data-kaldi ${DATA_KALDI}
+    rm -r African_Accented_French
+
+    utils/fix_data_dir.sh data/train
+    utils/fix_data_dir.sh data/valid
+    utils/fix_data_dir.sh data/test
+
+    utils/validate_data_dir.sh data/train --no-feats
+    utils/validate_data_dir.sh data/valid --no-feats
+    utils/validate_data_dir.sh data/test --no-feats
+fi
+
+log "Successfully finished. [elapsed=${SECONDS}s]"
---
+++ egs2/tmsa_openslr46/asr1/local/data_prep_aaf.py
@@ -0,0 +1,142 @@
+import os
+import shutil
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data-src', type=str)
+parser.add_argument('--data-dest', type=str)
+parser.add_argument('--data-kaldi', type=str)
+
+args = parser.parse_args()
+
+DATASET_DIR = args.data_src
+DATA_DIR = args.data_kaldi
+DEST_DIR = args.data_dest
+# prepare the training data
+
+TRAIN_SPEECH_DIR = os.path.join(DATASET_DIR, "speech/train")
+TRAIN_SPEECH_CA16_DIR = os.path.join(TRAIN_SPEECH_DIR, "ca16")
+TRAIN_SPEECH_YAOUNDE_DIR = os.path.join(TRAIN_SPEECH_DIR, "yaounde")
+
+for speaker in os.listdir(TRAIN_SPEECH_CA16_DIR):
+    for utterance in os.listdir(os.path.join(TRAIN_SPEECH_CA16_DIR, speaker)):
+        src = os.path.join(os.path.join(TRAIN_SPEECH_CA16_DIR, speaker, utterance))
+        dest = os.path.join(DEST_DIR, 'train', utterance)
+        shutil.copy(src, dest)
+
+
+utterance_types = ["read", "answers"]
+for utt_type in utterance_types:
+    for speaker in os.listdir(os.path.join(TRAIN_SPEECH_YAOUNDE_DIR, utt_type)):
+        for utterance in os.listdir(os.path.join(TRAIN_SPEECH_YAOUNDE_DIR, utt_type, speaker)):
+            src = os.path.join(TRAIN_SPEECH_YAOUNDE_DIR, utt_type, speaker, utterance)
+            dest = os.path.join(DEST_DIR, 'train', utt_type + '_' + utterance)
+            shutil.copy(src, dest)
+
+# write the transcripts for training data
+
+TRAIN_TRANSCRIPT_DIR = os.path.join(DATASET_DIR, "transcripts/train")
+
+with open(os.path.join(DATA_DIR, 'train', 'text'), 'w') as f_text:
+    with open(os.path.join(DATA_DIR, "train", "wav.scp"), 'w') as f_wav:
+        with open(os.path.join(DATA_DIR, "train", "utt2spk"), 'w') as f_utt2spk:
+            with open(os.path.join(DATA_DIR, "train", "spk2utt"), 'w') as f_spk2utt:
+                with open(os.path.join(TRAIN_TRANSCRIPT_DIR, 'ca16_conv', 'transcripts.txt')) as f_trans:
+                    for line in f_trans:
+                        utt_id, text = line.split(' ', 1)
+                        utt_id = utt_id[0: -4]
+                        f_text.write(utt_id + " " + text)
+
+                        wav_path = os.path.join('egs2/tmsa_openslr46/asr1', TRAIN_SPEECH_DIR, 'check', utt_id)
+                        f_wav.write(utt_id + " " + wav_path + "\n")
+
+                        f_utt2spk.write(utt_id+ " "+ utt_id + "\n")
+                        f_spk2utt.write(utt_id + " " + utt_id + "\n")
+
+                with open(os.path.join(TRAIN_TRANSCRIPT_DIR, 'ca16_read', 'conditioned.txt')) as f_trans:
+                    for line in f_trans:
+                        utt_id, text = line.split(' ', 1)
+                        f_text.write(utt_id + " " + text)
+
+                        wav_path = os.path.join('egs2/tmsa_openslr46/asr1', TRAIN_SPEECH_DIR, 'check', utt_id)
+                        f_wav.write(utt_id + " " + wav_path + "\n")
+
+                        f_utt2spk.write(utt_id + " " + utt_id + "\n")
+                        f_spk2utt.write(utt_id + " " + utt_id + "\n")
+
+                with open(os.path.join(TRAIN_TRANSCRIPT_DIR, 'yaounde', 'fn_text.txt')) as f_trans:
+                    for line in f_trans:
+                        file, text = line.split(' ', 1)
+                        file = file.split('/')
+                        if 'read' in file:
+                            utt_id = 'read' + '_' + file[-1][0: -4]
+                        else:
+                            utt_id = 'answers' + '_' + file[-1][0: -4]
+
+                        f_text.write(utt_id + " " + text)
+
+                        wav_path = os.path.join('egs2/tmsa_openslr46/asr1', TRAIN_SPEECH_DIR, 'check', utt_id)
+                        f_wav.write(utt_id + " " + wav_path + "\n")
+
+                        f_utt2spk.write(utt_id + " " + utt_id + "\n")
+                        f_spk2utt.write(utt_id + " " + utt_id + "\n")
+
+# prepare the test data
+
+TEST_SPEECH_DIR = os.path.join(DATASET_DIR, "speech/test")
+
+for speaker in os.listdir(os.path.join(TEST_SPEECH_DIR, 'ca16')):
+    for utterance in os.listdir(os.path.join(TEST_SPEECH_DIR, 'ca16', speaker)):
+        src = os.path.join(TEST_SPEECH_DIR, 'ca16', speaker, utterance)
+        dest = os.path.join(DEST_DIR, 'test', utterance)
+        shutil.copy(src, dest)
+
+# write the transcripts for test data
+
+TEST_TRANSCRIPT_PATH = os.path.join(DATASET_DIR, "transcripts", "test", "ca16", "prompts.txt")
+
+with open(os.path.join(DATA_DIR, 'test', 'text'), 'w') as f_text:
+    with open(os.path.join(DATA_DIR, "test", "wav.scp"), 'w') as f_wav:
+        with open(os.path.join(DATA_DIR, "test", "utt2spk"), 'w') as f_utt2spk:
+            with open(os.path.join(DATA_DIR, "test", "spk2utt"), 'w') as f_spk2utt:
+                with open(TEST_TRANSCRIPT_PATH, 'r') as f_trans:
+                    for line in f_trans:
+                        utt_id, text = line.split(' ', 1)
+                        f_text.write(line)
+
+                        wav_path = os.path.join('egs2/tmsa_openslr46/asr1', TEST_SPEECH_DIR, 'check', utt_id)
+                        f_wav.write(utt_id + " " +  wav_path + "\n")
+
+                        f_utt2spk.write(utt_id + " " + utt_id + "\n")
+                        f_spk2utt.write(utt_id + " " + utt_id + "\n")
+
+# prepare the validation data
+
+VALID_SPEECH_DIR = os.path.join(DATASET_DIR, "speech/devtest")
+
+for speaker in os.listdir(os.path.join(VALID_SPEECH_DIR, 'ca16')):
+    for utterance in os.listdir(os.path.join(VALID_SPEECH_DIR, 'ca16', speaker)):
+        src = os.path.join(VALID_SPEECH_DIR, 'ca16', speaker, utterance)
+        dest = os.path.join(DEST_DIR, 'valid', utterance)
+        shutil.copy(src, dest)
+
+
+# write the transcripts for validation data
+
+VALID_TRANSCRIPT_PATH = os.path.join(DATASET_DIR, "transcripts", "devtest", "ca16_read", "conditioned.txt")
+
+with open(os.path.join(DATA_DIR, 'valid', 'text'), 'w') as f_text:
+    with open(os.path.join(DATA_DIR, "valid", "wav.scp"), 'w') as f_wav:
+        with open(os.path.join(DATA_DIR, "valid", "utt2spk"), 'w') as f_utt2spk:
+            with open(os.path.join(DATA_DIR, "valid", "spk2utt"), 'w') as f_spk2utt:
+                with open(VALID_TRANSCRIPT_PATH, 'r') as f_trans:
+                    for line in f_trans:
+                        utt_id, text = line.split(' ', 1)
+                        f_text.write(line)
+
+                        wav_path = os.path.join('egs2/tmsa_openslr46/asr1', VALID_SPEECH_DIR, 'check', utt_id)
+                        f_wav.write(utt_id + " " + wav_path + "\n")
+
+                        f_utt2spk.write(utt_id + " " + utt_id + "\n")
+                        f_spk2utt.write(utt_id + " " + utt_id + "\n")
+
