712bf5cdcbd9c3a94e702021a6b899e3ca649359
Siddhant <siddhana@br013.ib.bridges2.psc.edu>
2022-03-13 20:26:07 -0400

    Fix ASR computation with bpe

--- egs2/slue-voxceleb/asr1/local/score.sh
+++ egs2/slue-voxceleb/asr1/local/score.sh
@@ -8,6 +8,9 @@
 # data=data/eval2000
 # #end configuration section.
 
+# TODO(siddhana): Automatically determine the decoding folder name
+# TODO(siddhana): Show SLU results in RESULTS.md
+
 [ -f ./path.sh ] && . ./path.sh
 . parse_options.sh || exit 1;
 
@@ -22,11 +25,13 @@ if [ $# -gt 1 ]; then
 	valid_inference_folder=$2
 	test_inference_folder=$3
 	python local/score.py --exp_root ${asr_expdir} --valid_folder ${valid_inference_folder} --test_folder ${test_inference_folder}
+	python local/generate_asr_files.py --exp_root ${asr_expdir} --valid_folder ${valid_inference_folder} --test_folder ${test_inference_folder}
 	python local/f1_score.py --exp_root ${asr_expdir} --valid_folder ${valid_inference_folder} --test_folder ${test_inference_folder}
 else
 	valid_inference_folder="inference_asr_model_valid.acc.ave_10best/devel/"
 	test_inference_folder="inference_asr_model_valid.acc.ave_10best/test/"
 	python local/score.py --exp_root ${asr_expdir}
+	python local/generate_asr_files.py --exp_root ${asr_expdir}
 	python local/f1_score.py --exp_root ${asr_expdir}
 fi
 
