df59c8e2f7f4cde576f00d199fbd3c505f74d16c
rubenjohn1999 <rubenjohn1999@gmail.com>
2022-03-14 11:20:29 -0400

    Added LM files

--- egs2/ml_openslr63/asr1/run.sh
+++ egs2/ml_openslr63/asr1/run.sh
@@ -7,31 +7,32 @@ set -o pipefail
 
 lid=false # whether to use language id as additional label
 
-train_set="ml_train"
-train_dev="ml_dev"
-test_set="ml_test"
+train_set="train_ml"
+train_dev="dev_ml"
+test_set="test_ml"
 
-asr_config=conf/train_asr.yaml
+asr_config=conf/tuning/train_asr_conformer5.yaml
 inference_config=conf/decode_asr.yaml
+lm_config=conf/train_lm.yaml
 
-ngpu=1
+ngpu=4
 
 ./asr.sh \
     --stage 1 \
-    --stop_stage 100 \
-    --ngpu ${ngpu} \
-    --nj 80 \
-    --inference_nj 256 \
-    --gpu_inference true \
-    --inference_args "--batch_size 1" \
-    --use_lm false \
+    --stop_stage 13 \
+    --ngpu 4 \
+    --lang "${lang}" \
+    --local_data_opts "--lang ${lang}" \
+    --use_lm true \
+    --lm_config "${lm_config}" \
     --token_type bpe \
     --nbpe 1000 \
     --feats_type raw \
+    --speed_perturb_factors "0.9 1.0 1.1" \
     --asr_config "${asr_config}" \
     --inference_config "${inference_config}" \
     --train_set "${train_set}" \
     --valid_set "${train_dev}" \
     --test_sets "${test_set}" \
-    --lm_train_text "data/${train_set}/text" \
-    --local_score_opts "--score_lang_id ${lid}" "$@"
+    --bpe_train_text "data/${train_set}/text" \
+    --lm_train_text "data/${train_set}/text" "$@"
