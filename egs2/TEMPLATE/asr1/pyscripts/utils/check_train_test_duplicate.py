f9c8fa0607f093c7a735d0b6d19058fbf906890f
Ubuntu <ubuntu@ip-172-31-18-124.us-east-2.compute.internal>
2022-03-16 05:02:57 +0000

    remove user confirmation in check duplicate script

--- egs2/TEMPLATE/asr1/pyscripts/utils/check_train_test_duplicate.py
+++ egs2/TEMPLATE/asr1/pyscripts/utils/check_train_test_duplicate.py
@@ -29,30 +29,30 @@ for test_name in ("test", "dev"):
     duplicate_uttids = set(duplicate_uttids)
     print(count, "duplicates in", test_name)
 
-    if input("continue? [y/n]") == 'y':
-        # remove all instances of duplicate uttids in these files: spk2utt, text, utt2spk, wav.scp
-        with open(f"data/{test_name}/spk2utt", 'r') as f:
-            # replace all uttid with empty string
-            text = f.read()
-            for uttid in duplicate_uttids:
-                text = text.replace(" "+uttid, "")
-            for line in text.split("\n"):
-                if not line: continue
-                if len(line.strip().split(" ")) < 2:
-                    print(f"removing {line} from spk2utt")
-                    text = text.replace(line+'\n', "")
-        with open(f"data/{test_name}/spk2utt", 'w') as f:
-            f.write(text)   
+    # if input("continue? [y/n]") == 'y':
+    # remove all instances of duplicate uttids in these files: spk2utt, text, utt2spk, wav.scp
+    with open(f"data/{test_name}/spk2utt", 'r') as f:
+        # replace all uttid with empty string
+        text = f.read()
+        for uttid in duplicate_uttids:
+            text = text.replace(" "+uttid, "")
+        for line in text.split("\n"):
+            if not line: continue
+            if len(line.strip().split(" ")) < 2:
+                print(f"removing {line} from spk2utt")
+                text = text.replace(line+'\n', "")
+    with open(f"data/{test_name}/spk2utt", 'w') as f:
+        f.write(text)   
 
-        for name in ("text", "utt2spk", "wav.scp"):
-            with open(f"data/{test_name}/{name}", "r") as f:
-                # remove all lines that contain ids that correspond to duplicate sentences
-                out_lines = []
-                for line in f:
-                    if not line.split()[0] in duplicate_uttids:
-                        out_lines.append(line.strip())
-            with open(f"data/{test_name}/{name}", "w") as f:
-                f.write("\n".join(out_lines))
-                f.write("\n")
-    else:
-        print("ok.")
+    for name in ("text", "utt2spk", "wav.scp"):
+        with open(f"data/{test_name}/{name}", "r") as f:
+            # remove all lines that contain ids that correspond to duplicate sentences
+            out_lines = []
+            for line in f:
+                if not line.split()[0] in duplicate_uttids:
+                    out_lines.append(line.strip())
+        with open(f"data/{test_name}/{name}", "w") as f:
+            f.write("\n".join(out_lines))
+            f.write("\n")
+    # else:
+    #     print("ok.")
