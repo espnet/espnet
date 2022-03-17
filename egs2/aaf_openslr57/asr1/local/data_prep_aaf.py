5103d4d6ecd434f9ceae564d5ea94284720aeb85
mvijay97 <malaika.vijay@gmail.com>
2022-03-16 22:31:28 -0400

    Remove additional test set and add preprocesing

--- egs2/aaf_openslr57/asr1/local/data_prep_aaf.py
+++ egs2/aaf_openslr57/asr1/local/data_prep_aaf.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import argparse
+import string
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data-src', type=str)
@@ -17,88 +18,67 @@ DEST_DIR = args.data_dest
 
 TRAIN_SPEECH_DIR = os.path.join(DATASET_DIR, "speech/train")
 TRAIN_SPEECH_CA16_DIR = os.path.join(TRAIN_SPEECH_DIR, "ca16")
-TRAIN_SPEECH_YAOUNDE_DIR = os.path.join(TRAIN_SPEECH_DIR, "yaounde")
 
 utterances = set()
-
-for speaker in os.listdir(TRAIN_SPEECH_CA16_DIR):
-    for utterance in os.listdir(os.path.join(TRAIN_SPEECH_CA16_DIR, speaker)):
-        src = os.path.join(os.path.join(TRAIN_SPEECH_CA16_DIR, speaker, utterance))
-        dest = os.path.join(DEST_DIR, 'train', utterance)
-        shutil.copy(src, dest)
-        utterances.add(utterance[0:-4])
-
-
-utterance_types = ["read", "answers"]
-for utt_type in utterance_types:
-    for speaker in os.listdir(os.path.join(TRAIN_SPEECH_YAOUNDE_DIR, utt_type)):
-        for utterance in os.listdir(os.path.join(TRAIN_SPEECH_YAOUNDE_DIR, utt_type, speaker)):
-            src = os.path.join(TRAIN_SPEECH_YAOUNDE_DIR, utt_type, speaker, utterance)
-            dest = os.path.join(DEST_DIR, 'train', utt_type + '_' + utterance)
+with open(os.path.join(DATA_DIR, "train", "utt2spk"), 'w') as f_utt2spk:
+    for speaker in os.listdir(TRAIN_SPEECH_CA16_DIR):
+        for utterance in os.listdir(os.path.join(TRAIN_SPEECH_CA16_DIR, speaker)):
+            src = os.path.join(os.path.join(TRAIN_SPEECH_CA16_DIR, speaker, utterance))
+            dest = os.path.join(DEST_DIR, 'train', utterance)
             shutil.copy(src, dest)
-            utterances.add(utt_type + '_' + utterance[0:-4])
+            utt_id = utterance[0: -4]
+            if utt_id in utterances:
+                print(utt_id)
+            utterances.add(utt_id)
+            f_utt2spk.write(speaker + '_'+ utt_id + " " + speaker+"\n")
 
 # write the transcripts for training data
-print(utterances)
 TRAIN_TRANSCRIPT_DIR = os.path.join(DATASET_DIR, "transcripts/train")
 
 with open(os.path.join(DATA_DIR, 'train', 'text'), 'w') as f_text:
     with open(os.path.join(DATA_DIR, "train", "wav.scp"), 'w') as f_wav:
-        with open(os.path.join(DATA_DIR, "train", "utt2spk"), 'w') as f_utt2spk:
-            with open(os.path.join(DATA_DIR, "train", "spk2utt"), 'w') as f_spk2utt:
-                with open(os.path.join(TRAIN_TRANSCRIPT_DIR, 'ca16_conv', 'transcripts.txt')) as f_trans:
-                    for line in f_trans:
-                        utt_id, text = line.split(' ', 1)
-                        utt_id = utt_id[0: -4]
-                        if utt_id in utterances:
-                            f_text.write(utt_id + " " + text)
+        with open(os.path.join(TRAIN_TRANSCRIPT_DIR, 'ca16_conv', 'transcripts.txt'), 'r') as f_trans:
+            for line in f_trans:
+                utt_id, text = line.split(' ', 1)
+                text = "".join([i.lower() for i in text if i not in string.punctuation])
 
-                            wav_path = os.path.join(DEST_DIR, 'train', utt_id+'.wav')
-                            f_wav.write(utt_id + " " + wav_path + "\n")
+                utt_id = utt_id[0: -4]
+                speaker = utt_id.split('_')[2]
+                if utt_id in utterances:
+                    f_text.write(speaker + '_' + utt_id + " " + text)
 
-                            f_utt2spk.write(utt_id+ " "+ utt_id + "\n")
-                            f_spk2utt.write(utt_id + " " + utt_id + "\n")
+                    wav_path = os.path.join(DEST_DIR, 'train', utt_id + '.wav')
+                    f_wav.write(speaker + '_' + utt_id + " " + wav_path + "\n")
 
-                with open(os.path.join(TRAIN_TRANSCRIPT_DIR, 'ca16_read', 'conditioned.txt')) as f_trans:
-                    for line in f_trans:
-                        utt_id, text = line.split(' ', 1)
-                        if utt_id in utterances:
-                            f_text.write(utt_id + " " + text)
+        with open(os.path.join(TRAIN_TRANSCRIPT_DIR, 'ca16_read', 'conditioned.txt')) as f_trans:
+            for line in f_trans:
+                utt_id, text = line.split(' ', 1)
+                text = "".join([i.lower() for i in text if i not in string.punctuation])
 
-                            wav_path = os.path.join(DEST_DIR, 'train', utt_id+'.wav')
-                            f_wav.write(utt_id + " " + wav_path + "\n")
+                speaker = utt_id.split('_')[2]
+                if utt_id in utterances:
+                    f_text.write(speaker + '_' + utt_id + " " + text)
 
-                            f_utt2spk.write(utt_id + " " + utt_id + "\n")
-                            f_spk2utt.write(utt_id + " " + utt_id + "\n")
-
-                with open(os.path.join(TRAIN_TRANSCRIPT_DIR, 'yaounde', 'fn_text.txt')) as f_trans:
-                    for line in f_trans:
-                        file, text = line.split(' ', 1)
-                        file = file.split('/')
-                        if 'read' in file:
-                            utt_id = 'read' + '_' + file[-1][0: -4]
-                        else:
-                            utt_id = 'answers' + '_' + file[-1][0: -4]
-                        if utt_id in utterances:
-                            f_text.write(utt_id + " " + text)
-
-                            wav_path = os.path.join(DEST_DIR, 'train', utt_id+'.wav')
-                            f_wav.write(utt_id + " " + wav_path + "\n")
-
-                            f_utt2spk.write(utt_id + " " + utt_id + "\n")
-                            f_spk2utt.write(utt_id + " " + utt_id + "\n")
+                    wav_path = os.path.join(DEST_DIR, 'train', utt_id + '.wav')
+                    f_wav.write(speaker + '_' + utt_id + " " + wav_path + "\n")
 
 # prepare the test data
 
 TEST_SPEECH_DIR = os.path.join(DATASET_DIR, "speech/test")
 utterances = set()
 
-for speaker in os.listdir(os.path.join(TEST_SPEECH_DIR, 'ca16')):
-    for utterance in os.listdir(os.path.join(TEST_SPEECH_DIR, 'ca16', speaker)):
-        src = os.path.join(TEST_SPEECH_DIR, 'ca16', speaker, utterance)
-        dest = os.path.join(DEST_DIR, 'test', utterance)
-        shutil.copy(src, dest)
-        utterances.add(utterance[0:-4])
+
+with open(os.path.join(DATA_DIR, "test", "utt2spk"), 'w') as f_utt2spk:
+    for speaker in os.listdir(os.path.join(TEST_SPEECH_DIR, 'ca16')):
+        for utterance in os.listdir(os.path.join(TEST_SPEECH_DIR, 'ca16', speaker)):
+                src = os.path.join(TEST_SPEECH_DIR, 'ca16', speaker, utterance)
+                dest = os.path.join(DEST_DIR, 'test', utterance)
+                shutil.copy(src, dest)
+                utt_id = utterance[0: -4]
+                if utt_id in utterances:
+                    print(utt_id)
+                utterances.add(utt_id)
+                f_utt2spk.write(speaker + '_'+ utt_id + " " + speaker+"\n")
 
 # write the transcripts for test data
 
@@ -106,31 +86,34 @@ TEST_TRANSCRIPT_PATH = os.path.join(DATASET_DIR, "transcripts", "test", "ca16", 
 
 with open(os.path.join(DATA_DIR, 'test', 'text'), 'w') as f_text:
     with open(os.path.join(DATA_DIR, "test", "wav.scp"), 'w') as f_wav:
-        with open(os.path.join(DATA_DIR, "test", "utt2spk"), 'w') as f_utt2spk:
-            with open(os.path.join(DATA_DIR, "test", "spk2utt"), 'w') as f_spk2utt:
-                with open(TEST_TRANSCRIPT_PATH, 'r') as f_trans:
-                    for line in f_trans:
-                        utt_id, text = line.split(' ', 1)
-                        if utt_id in utterances:
-                            f_text.write(line)
+        with open(TEST_TRANSCRIPT_PATH, 'r') as f_trans:
+            for line in f_trans:
+                utt_id, text = line.split(' ', 1)
+                text = "".join([i.lower() for i in text if i not in string.punctuation])
 
-                            wav_path = os.path.join(DEST_DIR, 'test', utt_id+'.wav')
-                            f_wav.write(utt_id + " " +  wav_path + "\n")
+                speaker = utt_id.split('_')[2]
+                if utt_id in utterances:
+                    f_text.write(speaker + '_' + line)
 
-                            f_utt2spk.write(utt_id + " " + utt_id + "\n")
-                            f_spk2utt.write(utt_id + " " + utt_id + "\n")
+                    wav_path = os.path.join(DEST_DIR, 'test', utt_id + '.wav')
+                    f_wav.write(speaker + '_'+ utt_id + " " + wav_path + "\n")
 
 # prepare the validation data
 
 VALID_SPEECH_DIR = os.path.join(DATASET_DIR, "speech/devtest")
 utterances = set()
-for speaker in os.listdir(os.path.join(VALID_SPEECH_DIR, 'ca16')):
-    for utterance in os.listdir(os.path.join(VALID_SPEECH_DIR, 'ca16', speaker)):
-        src = os.path.join(VALID_SPEECH_DIR, 'ca16', speaker, utterance)
-        dest = os.path.join(DEST_DIR, 'valid', utterance)
-        shutil.copy(src, dest)
-        utterances.add(utterance[0:-4])
 
+with open(os.path.join(DATA_DIR, "test", "utt2spk"), 'w') as f_utt2spk:
+    for speaker in os.listdir(os.path.join(VALID_SPEECH_DIR, 'ca16')):
+        for utterance in os.listdir(os.path.join(VALID_SPEECH_DIR, 'ca16', speaker)):
+            src = os.path.join(VALID_SPEECH_DIR, 'ca16', speaker, utterance)
+            dest = os.path.join(DEST_DIR, 'valid', utterance)
+            shutil.copy(src, dest)
+            utt_id = utterance[0: -4]
+            if utt_id in utterances:
+                print(utt_id)
+            utterances.add(utt_id)
+            f_utt2spk.write(speaker + '_'+ utt_id + " " + speaker+"\n")
 
 # write the transcripts for validation data
 
@@ -138,17 +121,13 @@ VALID_TRANSCRIPT_PATH = os.path.join(DATASET_DIR, "transcripts", "devtest", "ca1
 
 with open(os.path.join(DATA_DIR, 'valid', 'text'), 'w') as f_text:
     with open(os.path.join(DATA_DIR, "valid", "wav.scp"), 'w') as f_wav:
-        with open(os.path.join(DATA_DIR, "valid", "utt2spk"), 'w') as f_utt2spk:
-            with open(os.path.join(DATA_DIR, "valid", "spk2utt"), 'w') as f_spk2utt:
-                with open(VALID_TRANSCRIPT_PATH, 'r') as f_trans:
-                    for line in f_trans:
-                        utt_id, text = line.split(' ', 1)
-                        if utt_id in utterances:
-                            f_text.write(line)
+        with open(VALID_TRANSCRIPT_PATH, 'r') as f_trans:
+            for line in f_trans:
+                utt_id, text = line.split(' ', 1)
+                text = "".join([i.lower() for i in text if i not in string.punctuation])
+                speaker = utt_id.split('_')[2]
+                if utt_id in utterances:
+                    f_text.write(speaker + '_' + line)
 
-                            wav_path = os.path.join(DEST_DIR, 'valid', utt_id+'.wav')
-                            f_wav.write(utt_id + " " + wav_path + "\n")
-
-                            f_utt2spk.write(utt_id + " " + utt_id + "\n")
-                            f_spk2utt.write(utt_id + " " + utt_id + "\n")
-
+                    wav_path = os.path.join(DEST_DIR, 'valid', utt_id + '.wav')
+                    f_wav.write(speaker + '_' + utt_id + " " + wav_path + "\n")
