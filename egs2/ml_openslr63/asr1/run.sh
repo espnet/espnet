e1fd0138c3753d37e9ceb070e37d44a90e92541e
rubenjohn1999 <rubenjohn1999@gmail.com>
2022-03-18 11:13:44 -0400

    Hubert files added

--- egs2/ml_openslr63/asr1/run.sh
+++ egs2/ml_openslr63/asr1/run.sh
@@ -11,19 +11,18 @@ train_set="train_ml"
 train_dev="dev_ml"
 test_set="test_ml"
 
-asr_config=conf/tuning/train_asr_conformer_s3prlfrontend_hubert.yaml
+asr_config=conf/tuning/train_asr_conformer_s3prlfrontend_hubert_fused.yaml
 inference_config=conf/decode_asr.yaml
 lm_config=conf/train_lm.yaml
 
 ngpu=1
 
 ./asr.sh \
-    --stage 1 \
+    --stage 10 \
     --stop_stage 13 \
     --ngpu 1 \
-    --nj 3 \
-    --inference_nj 4 \
-    --gpu_inference true \
+    --nj 1 \
+    --inference_nj 1 \
     --lang "ml" \
     --use_lm true \
     --lm_config "${lm_config}" \
