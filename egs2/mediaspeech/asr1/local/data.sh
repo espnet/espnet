a2749f9ea52c9a21c3e38aa6201cffe0c0a9fcaa
abhesrivas <abhesrivastava@gmail.com>
2022-03-19 02:22:27 +0000

    Update.

---
+++ egs2/mediaspeech/asr1/local/data.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+. ./path.sh || exit 1;
+. ./cmd.sh || exit 1;
+. ./db.sh || exit 1;
+
+# general configuration
+stage=0       # start from 0 if you need to start from data preparation
+stop_stage=1
+# inclusive, was 100
+SECONDS=0
+
+log() {
+    local fname=${BASH_SOURCE[1]##*/}
+    echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
+}
+
+
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+. utils/parse_options.sh
+
+log "data preparation started"
+
+# mkdir -p ${JAVA}
+log "data preparation started"
+mkdir -p ${MEDIASPEECH}
+if [ -z "${MEDIASPEECH}" ]; then
+    log "Fill the value of 'MEDIASPEECH' of db.sh"
+    exit 1
+fi
+
+workspace=$PWD
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+    log "sub-stage 0: Download Data to downloads"
+
+    cd ${MEDIASPEECH}
+    wget https://www.openslr.org/resources/108/TR.tgz
+    tar -xvf TR.tgz
+    rm -f TR.tgz
+    mv TR/* .
+    rm -rf TR
+    cd $workspace
+fi
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    log "sub-stage 1: Preparing Data for openslr"
+
+    python3 local/data_prep.py -d ${MEDIASPEECH}
+    utils/spk2utt_to_utt2spk.pl data/mediaspeech_train/spk2utt > data/mediaspeech_train/utt2spk
+    utils/spk2utt_to_utt2spk.pl data/mediaspeech_dev/spk2utt > data/mediaspeech_dev/utt2spk
+    utils/spk2utt_to_utt2spk.pl data/mediaspeech_test/spk2utt > data/mediaspeech_test/utt2spk
+    utils/fix_data_dir.sh data/mediaspeech_train
+    utils/fix_data_dir.sh data/mediaspeech_dev
+    utils/fix_data_dir.sh data/mediaspeech_test
+fi
+
+log "Successfully finished. [elapsed=${SECONDS}s]"
---
+++ egs2/mediaspeech/asr1/local/data_prep.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 Carnegie Mellon University (Peter Wu)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+
+import argparse
+import os
+import random
+import pdb
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", help="downloads directory", type=str, default="downloads")
+    args = parser.parse_args()
+
+    # Read all file names in directory
+    file_names = os.listdir(args.d)
+    file_names = list(set([f.split(".")[0] for f in file_names]))
+    
+    spk2utt = {}
+    utt2text = {}
+    for fname in file_names:
+        
+        # File ID
+        fid = fname
+        
+        # Speaker ID – Info not provided so treat each as unique
+        spk = fname
+        
+        # Text – Read from file
+        text_path = "%s/%s.txt" % (args.d, fname)
+        with open(text_path, "r", encoding="utf-8") as f:
+          text = f.read()
+        
+        path = "%s/%s.flac" % (args.d, fid)
+        
+        if os.path.exists(path):
+            utt2text[fid] = text
+            if spk in spk2utt:
+                spk2utt[spk].append(fid)
+            else:
+                spk2utt[spk] = [fid]
+
+    spks = sorted(list(spk2utt.keys()))
+    num_fids = 0
+    num_test_spks = 0
+    test_percentage = 0.2
+    for spk in spks:
+        num_test_spks += 1
+        fids = sorted(list(set(spk2utt[spk])))
+        num_fids += len(fids)
+        if num_fids >= len(spks)*test_percentage:
+            break
+    
+    # Train-Test Split
+    test_spks = spks[:num_test_spks]
+    train_dev_spks = spks[num_test_spks:]
+    
+    # Shuffle Train
+    random.Random(0).shuffle(train_dev_spks)
+    num_train = int(len(train_dev_spks) * 0.9)
+    
+    # Train-Dev Split
+    train_spks = train_dev_spks[:num_train]
+    dev_spks = train_dev_spks[num_train:]
+
+    spks_by_phase = {"train": train_spks, "dev": dev_spks, "test": test_spks}
+    flac_dir = "%s/" % args.d
+    
+    sr = 16000
+    for phase in spks_by_phase:
+        spks = spks_by_phase[phase]
+        text_strs = []
+        wav_scp_strs = []
+        spk2utt_strs = []
+        num_fids = 0
+        for spk in spks:
+            fids = sorted(list(set(spk2utt[spk])))
+            num_fids += len(fids)
+            
+            if phase == "test" and num_fids > 2000:
+                curr_num_fids = num_fids - 2000
+                random.Random(1).shuffle(fids)
+                fids = fids[:curr_num_fids]
+            
+            utts = fids
+            utts_str = " ".join(utts)
+            spk2utt_strs.append("%s %s" % (spk, utts_str))
+            
+            for fid, utt in zip(fids, utts):
+                cmd = "ffmpeg -i %s/%s.flac -f wav -ar %d -ab 16 -ac 1 - |" % (
+                    flac_dir,
+                    fid,
+                    sr,
+                )
+
+                text_strs.append("%s %s" % (utt, utt2text[fid]))
+                wav_scp_strs.append("%s %s" % (utt, cmd))
+        
+        phase_dir = "data/mediaspeech_%s" % phase
+    
+        if not os.path.exists(phase_dir):
+            os.makedirs(phase_dir)
+    
+        text_strs = sorted(text_strs)
+        wav_scp_strs = sorted(wav_scp_strs)
+        spk2utt_strs = sorted(spk2utt_strs)
+    
+        with open(os.path.join(phase_dir, "text"), "w+") as ouf:
+            for s in text_strs:
+                ouf.write("%s\n" % s)
+    
+        with open(os.path.join(phase_dir, "wav.scp"), "w+") as ouf:
+            for s in wav_scp_strs:
+                ouf.write("%s\n" % s)
+    
+        with open(os.path.join(phase_dir, "spk2utt"), "w+") as ouf:
+            for s in spk2utt_strs:
+                ouf.write("%s\n" % s)
