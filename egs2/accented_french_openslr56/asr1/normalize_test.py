bc66bd14c77029fcaed44ffd6a2fb5ed5af2d5d6
jessicah25 <8203518+jessicah25@users.noreply.github.com>
2022-03-21 10:36:39 -0700

    prepare test set with normalization

---
+++ egs2/accented_french_openslr56/asr1/normalize_test.py
@@ -0,0 +1,14 @@
+f = open('downloads/African_Accented_French/transcripts/test/ca16/prompts.txt')
+
+new_f = open('downloads/African_Accented_French/transcripts/test/ca16/new_prompts.txt', 'w')
+
+for row in f:
+    uttid = row.split(' ')[0]
+    utt = ' '.join(row.split(' ')[1:-1])
+    
+    utt = utt.lower()
+    utt = utt.strip('.?!')
+    utt = utt.replace(',', '')
+    utt = utt.replace(';', '')
+
+    new_f.write(uttid + ' ' + utt + '\n')
