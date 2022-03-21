12ba9f6fef9c0ff3ecb3b05ff54095dd62e6beb3
Yong Min Kim <bahducoup@gmail.com>
2022-03-21 00:13:11 +0000

    add minds14

---
+++ egs2/minds14/asr1/local/data_prep.py
@@ -0,0 +1,94 @@
+# Copyright 2021  Young Min Kim
+#           2021  Carnegie Mellon University
+# Apache 2.0
+
+import os
+import re
+import sys
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+if len(sys.argv) != 3:
+    print("Usage: python data_prep.py [MINDS14_DIR] [LANG]")
+    sys.exit(1)
+
+
+MINDS14_DIR = sys.argv[1]  # raw dataset zip file contents should be here
+LANG = sys.argv[2] # language
+DATA_DIR = "data"  # processed data should go here
+
+
+def read_minds14_data(dataset_csv, export_csv):
+    dataset_df = pd.read_csv(dataset_csv)
+    output_df = []
+
+    for i in range(len(dataset_df)):
+        wav_name, intent_name, transcript = (
+            dataset_df.iloc[i]["filepath"],
+            dataset_df.iloc[i]["intent"],
+            dataset_df.iloc[i]["text_asr"],
+        )
+
+        # clean transcript
+        # export audio of for the crop with wav_path_start_duration
+        export_path = os.path.join(MINDS14_DIR, "audio", wav_name)
+        # Append to output_df
+        output_df.append(
+            [
+                export_path,
+                "unknown",
+                transcript,
+                intent_name,
+            ]
+        )
+
+    X = pd.DataFrame(
+        output_df, columns=["path", "speakerId", "transcription", "task_type"]
+    )
+    Y = X.pop("task_type").to_frame()
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, Y, stratify=Y, test_size=0.20, random_state=42
+    )
+    X_test, X_val, y_test, y_val = train_test_split(
+        X_test, y_test, stratify=y_test, test_size=0.50, random_state=42
+    )
+
+    pd.concat([X_train, y_train], axis=1).to_csv(os.path.join(DATA_DIR, "train.csv"))
+    pd.concat([X_test, y_test], axis=1).to_csv(os.path.join(DATA_DIR, "test.csv"))
+    pd.concat([X_val, y_val], axis=1).to_csv(os.path.join(DATA_DIR, "validation.csv"))
+
+
+read_minds14_data(
+    os.path.join(MINDS14_DIR, f"text/{LANG}.csv"),  # ~/text/lang-LOCALE.csv
+    os.path.join(MINDS14_DIR, "export.csv")
+)
+
+
+dir_dict = {
+    "train": "train.csv",
+    "valid": "validation.csv",
+    "test": "test.csv",
+}
+
+
+for partition in dir_dict:
+    with open(os.path.join(DATA_DIR, partition, "text"), "w", encoding='utf-8') as text_f, \
+         open(os.path.join(DATA_DIR, partition, "wav.scp"), "w", encoding='utf-8') as wav_scp_f, \
+         open(os.path.join(DATA_DIR, partition, "transcript"), "w", encoding='utf-8') as transcript_f, \
+         open(os.path.join(DATA_DIR, partition, "utt2spk"), "w", encoding='utf-8') as utt2spk_f:
+
+        text_f.truncate()
+        wav_scp_f.truncate()
+        utt2spk_f.truncate()
+
+        transcript_df = pd.read_csv(os.path.join(DATA_DIR, dir_dict[partition]))
+        for row in transcript_df.values:
+            words = row[4] + " " + " ".join([ch for ch in row[3]])
+            path_arr = row[1].split("/")
+            utt_id = path_arr[-2] + "_" + path_arr[-1]
+            text_f.write(f"{utt_id} {words}\n")
+            wav_scp_f.write(utt_id + " " + row[1] + "\n")
+            utt2spk_f.write(utt_id + " " + row[2] + "\n")
---
+++ egs2/minds14/asr1/run.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+train_set="train"
+valid_set="valid"
+test_sets="test valid"
+language="es-ES"
+
+asr_config=conf/train_wav2vec.yaml
+
+./asr.sh \
+    --ngpu 1 \
+    --stage 1 \
+    --use_lm false \
+    --nbpe 500 \
+    --token_type word\
+    --audio_format wav\
+    --feats_type raw\
+    --max_wav_duration 30 \
+    --inference_asr_model valid.acc.ave_5best.pth\
+    --asr_config "${asr_config}" \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" "$@" \
+    --local_data_opts "${language}"
