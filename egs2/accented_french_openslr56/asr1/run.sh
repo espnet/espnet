1af9d2fa777d1232aa0c86ce1c1db07ea0ffa23f
Dan Georges Berrebbi <dberrebb@tir.lti.cs.cmu.edu>
2022-03-21 15:50:48 -0400

    tuning conf

--- egs2/accented_french_openslr56/asr1/run.sh
+++ egs2/accented_french_openslr56/asr1/run.sh
@@ -8,31 +8,30 @@ set -o pipefail
 
 
 asr_config=conf/tuning/train_asr_conformer5.yaml
-inference_config=conf/tuning/decode_rnn.yaml
+inference_config=conf/decode_asr.yaml
 lm_config=conf/train_lm_transformer.yaml
-use_lm=true
+use_lm=false
 use_wordlm=false
 
 
-# token_type are char and not bpe for chineese 
 
-./asr.sh                                               \
-    --lang zh                                          \
-    --audio_format wav                                 \
-    --feats_type raw                                   \
-    --ngpu 1                                           \
-    --token_type char                                  \
-    --use_lm ${use_lm}                                 \
-    --use_word_lm ${use_wordlm}                        \
-    --lm_config "${lm_config}"                         \
-    --asr_config "${asr_config}"                       \
-    --inference_config "${inference_config}"           \
-    --train_set "train"                       \
-    --valid_set "dev"                       \
-    --test_sets "test"                        \
-    --asr_speech_fold_length 512 \
-    --asr_text_fold_length 150 \
-    --lm_fold_length 150 \
-    --max_wav_duration 20. \
-    --lm_train_text "data/train/text" "$@" \
-    --nlsyms_txt data/nlsyms.txt
+./asr.sh \
+    --ngpu 1 \
+    --use_lm false \
+    --lm_config "${lm_config}" \
+    --token_type bpe \
+    --inference_nj 4 \
+    --nbpe 250 \
+    --nj 32 \
+    --feats_type raw \
+    --inference_asr_model "valid.acc.ave_10best.pth" \
+    --feats_normalize "utterance_mvn" \
+    --speed_perturb_factors "0.9 1.0 1.1" \
+    --asr_config "${asr_config}" \
+    --inference_config "${inference_config}" \
+    --train_set "train" \
+    --valid_set "dev" \
+    --test_sets "dev" \
+    --expdir "exp" \
+    --bpe_train_text "data/train/text" \
+    --lm_train_text "data/train/text" "$@"
