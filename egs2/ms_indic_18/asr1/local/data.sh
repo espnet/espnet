a05a622f21266c9d3c55df1a63b24d66d7dc52e5
Chaitanya Narisetty <cnariset@andrew.cmu.edu>
2022-03-23 22:53:53 -0400

    Added recipe for Microsoft Indic Languages (Speech Corpus)

---
+++ egs2/ms_indic_18/asr1/local/data.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Copyright 2020 Johns Hopkins University (Shinji Watanabe)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+. ./path.sh || exit 1;
+. ./cmd.sh || exit 1;
+. ./db.sh || exit 1;
+
+# general configuration
+stage=0       # start from 0 if you need to start from data preparation
+stop_stage=100
+SECONDS=0
+lang=te # te ta gu
+
+ . utils/parse_options.sh || exit 1;
+
+
+log() {
+    local fname=${BASH_SOURCE[1]##*/}
+    echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
+}
+
+mkdir -p ${MS_INDIC_IS18}
+if [ -z "${MS_INDIC_IS18}" ]; then
+    log "Fill the value of 'MS_INDIC_IS18' of db.sh"
+    exit 1
+fi
+
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+train_set=train_"$(echo "${lang}" | tr - _)"
+train_dev=dev_"$(echo "${lang}" | tr - _)"
+test_set=test_"$(echo "${lang}" | tr - _)"
+
+log "data preparation started"
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then 
+    if [[ ! -d "${lang}-in-Train}" ]];; then
+        log "stage0: Download data to ${MS_INDIC_IS18}. ${lang}-in-Train} directory is missing"
+    elif [[ ! -d "${lang}-in-Test}" ]];; then
+        log "stage0: Download data to ${MS_INDIC_IS18}. ${lang}-in-Test} directory is missing"
+    fi
+    exit 1
+fi
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    log "stage1: Preparing data for Microsoft Speech Corpus (Indian languages)"
+    ### Task dependent. You have to make data the following preparation part by yourself.
+    local/prepare_data.py ${MS_INDIC_IS18} ${lang}
+fi
+
+log "Successfully finished. [elapsed=${SECONDS}s]"
