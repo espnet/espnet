51203daea36a815b4a753b850d2ad719de8d1cc9
Karthik Ganesan <Karthikganesan17@gmail.com>
2022-03-24 07:42:48 -0400

    Update data_prep.py

--- egs2/dstc2/asr1/local/data_prep.py
+++ egs2/dstc2/asr1/local/data_prep.py
@@ -10,10 +10,11 @@ import re
 import sys
 import pandas as pd
 
-if len(sys.argv) != 2:
-    print("Usage: python data_prep.py [dstc2_root]")
+if len(sys.argv) != 3:
+    print("Usage: python data_prep.py [dstc2_root] coverage")
     sys.exit(1)
 dstc2_root = sys.argv[1]
+coverage = int(sys.argv[2])
 
 dir_dict = {
     "train": "train.csv",
@@ -22,12 +23,11 @@ dir_dict = {
 }
 
 #specify the coverage 
-coverage = 0.05 
 
 def _get_stratified_sampled_data(data,coverage):
     total_sample_count = data.shape[0]
 
-    data["labels_tuple"] = data.labels.apply(lambda x: tuple(x))   
+    data["labels_tuple"] = data.dialog_acts.apply(lambda x: tuple(x))   
     unique_data = data.drop_duplicates(subset=['labels_tuple'], keep='first')
 
     
@@ -64,15 +64,21 @@ for x in dir_dict:
         wav_scp_f.truncate()
         utt2spk_f.truncate()
         transcript_df = pd.read_csv(os.path.join(dstc2_root, "data", dir_dict[x]))
-        for row in transcript_df.values:
+        
+        if x==train and coverage<1:
+            transcript_df = _get_stratified_sampled_data(transcript_df,coverage)
+            
+        transcript_df  = transcript_df.reset_index()  # make sure indexes pair with number of rows
+        
+        for index, row in transcript_df.iterrows():
             words = (
-                " <sep> ".join(sorted(eval(row[5])))
+                " <sep> ".join(sorted(eval(row["dialog_acts"])))
                 + " <utt> "
-                + row[2].replace("<unk>", "unk").encode("ascii", "ignore").decode()
+                + row["transcript"].replace("<unk>", "unk").encode("ascii", "ignore").decode()
             )
-            path_arr = row[4].split("/")
-            utt_id = path_arr[-2] + "_" + path_arr[-1]
+            path_arr = row["audio_file_path"].split("/")
+            utt_id = path_arr["audio_file_path"] + "_" + path_arr[-1]
             speaker_id = path_arr[-2]
             text_f.write(utt_id + " " + words + "\n")
-            wav_scp_f.write(utt_id + " " + dstc2_root + "/" + row[4] + "\n")
+            wav_scp_f.write(utt_id + " " + dstc2_root + "/" + row["audio_file_path"] + "\n")
             utt2spk_f.write(utt_id + " " + speaker_id + "\n")
