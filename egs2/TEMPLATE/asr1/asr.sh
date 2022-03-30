296733d4124d2959b3d09ad6819881930c3a4bcc
Dan Berrebbi <dan.berrebbi@polytechnique.edu>
2022-03-30 15:22:23 -0400

    added a check for nbpe

--- egs2/TEMPLATE/asr1/asr.sh
+++ egs2/TEMPLATE/asr1/asr.sh
@@ -649,7 +649,7 @@ if ! "${skip_data_prep}"; then
             fi
 
             ${python} -m espnet2.bin.tokenize_text  \
-                --token_type "chars" \
+                --token_type "char" \
                 --input "${data_feats}/lm_train.txt" --output "${token_list}" ${_opts} \
                 --field 2- \
                 --cleaner "${cleaner}" \
