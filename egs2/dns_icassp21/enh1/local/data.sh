aca7d5a49637b2f13e51e759c1a00627558936b6
Yoshiki <mas03151997@gmail.com>
2022-04-11 01:28:27 +0900

    fix usage note

--- egs2/dns_icassp21/enh1/local/data.sh
+++ egs2/dns_icassp21/enh1/local/data.sh
@@ -17,9 +17,9 @@ Usage: $0 [--stage <stage>] [--stop_stage <stop_stage>]
     NOTE:
         stage 1: Create the Data Mixture from the DNS scripts. You can skip this step when you already have the audio mixture for training.
         stage 2: Prepare the data for ESPNet-se
-        You can clone the DNS-icassp2021 by git clone https://github.com/microsoft/DNS-Challenge.git DNS-Challenge
-        You can download the data by using download-dns-challenge-2.sh in the master branch
-        In addition, "datasets/wideband/acoustic_params_wideband" is required, which can be downloded from the interspeech2021/adddata branch
+        You can get scripts by git clone -b icassp2021-final https://github.com/microsoft/DNS-Challenge.git DNS-Challenge
+        You can download the data by using download-dns-challenge-2.sh in the master branch without git lfs
+        In addition, "datasets/wideband/acoustic_params_wideband" and "datasets/wideband/dev_testset_wideband/track1" are required, which are not downloaded by the above script
         For evaluation, synthetic data in the "datasets/wideband/dev_testset_wideband/track1" in the the interspeech2021/adddata branch is used 
         To avoid issues related to hard-coded paths, please change the current directory to DNS-Challenge in noisyspeech_synthesizer_singleprocess.py
         Also, please make sure the destination is under data/dns_wav
