11059c75fbca58c1f6b0f027a842763e5849d300
Dan Georges Berrebbi <dberrebb@tir.lti.cs.cmu.edu>
2022-04-15 18:10:12 -0400

    reformated files

--- egs2/accented_french_openslr56/asr1/local/remove_missing.py
+++ egs2/accented_french_openslr56/asr1/local/remove_missing.py
@@ -1,15 +1,16 @@
 import os
 
+base="downloads/African_Accented_French/"
 existing = []
-for _, _, f in os.walk("downloads/African_Accented_French/speech/train/ca16"):
+for _, _, f in os.walk(base+"speech/train/ca16"):
     for fi in f:
         existing.append(fi[:-4])
 
 old_f = open(
-    "downloads/African_Accented_French/transcripts/train/ca16_conv/transcripts.txt"
+    base+"transcripts/train/ca16_conv/transcripts.txt"
 )
 new_f = open(
-    "downloads/African_Accented_French/transcripts/train/ca16_conv/new_transcripts.txt",
+    base+"transcripts/train/ca16_conv/new_transcripts.txt",
     "w",
 )
 
@@ -18,10 +19,10 @@ for row in old_f:
         new_f.write(row)
 
 old_f = open(
-    "downloads/African_Accented_French/transcripts/train/ca16_read/conditioned.txt"
+    base+"transcripts/train/ca16_read/conditioned.txt"
 )
 new_f = open(
-    "downloads/African_Accented_French/transcripts/train/ca16_read/new_conditioned.txt",
+    base+"transcripts/train/ca16_read/new_conditioned.txt",
     "w",
 )
 
@@ -30,15 +31,15 @@ for row in old_f:
         new_f.write(row)
 
 existing = []
-for _, _, f in os.walk("downloads/African_Accented_French/speech/devtest/ca16"):
+for _, _, f in os.walk(base+"speech/devtest/ca16"):
     for fi in f:
         existing.append(fi[:-4])
 
 old_f = open(
-    "downloads/African_Accented_French/transcripts/devtest/ca16_read/conditioned.txt"
+    base+"transcripts/devtest/ca16_read/conditioned.txt"
 )
 new_f = open(
-    "downloads/African_Accented_French/transcripts/devtest/ca16_read/new_conditioned.txt",
+    base+"transcripts/devtest/ca16_read/new_conditioned.txt",
     "w",
 )
 
