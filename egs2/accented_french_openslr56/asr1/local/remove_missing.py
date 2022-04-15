f6cbc61353e0a1cefe81ae596278f7db1f0b7dd9
Dan Georges Berrebbi <dberrebb@tir-0-17.eth>
2022-04-15 18:31:26 -0400

    reformated

--- egs2/accented_french_openslr56/asr1/local/remove_missing.py
+++ egs2/accented_french_openslr56/asr1/local/remove_missing.py
@@ -1,16 +1,14 @@
 import os
 
-base="downloads/African_Accented_French/"
+base = "downloads/African_Accented_French/"
 existing = []
-for _, _, f in os.walk(base+"speech/train/ca16"):
+for _, _, f in os.walk(base + "speech/train/ca16"):
     for fi in f:
         existing.append(fi[:-4])
 
-old_f = open(
-    base+"transcripts/train/ca16_conv/transcripts.txt"
-)
+old_f = open(base + "transcripts/train/ca16_conv/transcripts.txt")
 new_f = open(
-    base+"transcripts/train/ca16_conv/new_transcripts.txt",
+    base + "transcripts/train/ca16_conv/new_transcripts.txt",
     "w",
 )
 
@@ -18,11 +16,9 @@ for row in old_f:
     if row.split(" ")[0][:-4] in existing:
         new_f.write(row)
 
-old_f = open(
-    base+"transcripts/train/ca16_read/conditioned.txt"
-)
+old_f = open(base + "transcripts/train/ca16_read/conditioned.txt")
 new_f = open(
-    base+"transcripts/train/ca16_read/new_conditioned.txt",
+    base + "transcripts/train/ca16_read/new_conditioned.txt",
     "w",
 )
 
@@ -31,15 +27,13 @@ for row in old_f:
         new_f.write(row)
 
 existing = []
-for _, _, f in os.walk(base+"speech/devtest/ca16"):
+for _, _, f in os.walk(base + "speech/devtest/ca16"):
     for fi in f:
         existing.append(fi[:-4])
 
-old_f = open(
-    base+"transcripts/devtest/ca16_read/conditioned.txt"
-)
+old_f = open(base + "transcripts/devtest/ca16_read/conditioned.txt")
 new_f = open(
-    base+"transcripts/devtest/ca16_read/new_conditioned.txt",
+    base + "transcripts/devtest/ca16_read/new_conditioned.txt",
     "w",
 )
 
