488d106e3c5ffb94cbc280cbbd3f9545e1e033ff
Jiatong <728307998@qq.com>
2022-04-22 11:44:30 -0400

    Update egs2/covost2/st1/run.sh
    Co-authored-by: Chaitanya Narisetty <cnariset@andrew.cmu.edu>

--- egs2/covost2/st1/run.sh
+++ egs2/covost2/st1/run.sh
@@ -91,7 +91,7 @@ fi
 ./st.sh \
     --ngpu 1 \
     --local_data_opts "--stage 0 --src_lang ${src_lang} --tgt_lang ${tgt_lang}" \
-    --speed_perturb_factors ${speed_perturb_factors} \
+    --speed_perturb_factors "${speed_perturb_factors}" \
     --use_lm false \
     --feats_type raw \
     --audio_format "flac.ark" \
