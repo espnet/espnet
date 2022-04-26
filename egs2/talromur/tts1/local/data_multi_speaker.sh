87b7b2592b65536f111b432c93e7cd528426900c
Gunnar Thor <ornolfsson@gmail.com>
2022-04-26 11:44:21 +0000

    Add data_download.sh for talromur recipe. Reduced batch size of fastspeech2 to facilitate 1-gpu training Reduced VITS batch size to prevent OOM failures in 4-GPU setup

--- egs2/talromur/tts1/local/data_multi_speaker.sh
+++ egs2/talromur/tts1/local/data_multi_speaker.sh
@@ -16,17 +16,12 @@ stop_stage=2
 
 . utils/parse_options.sh
 
-if [ $# -ne 1 ]; then
-    log "Error: Only speaker id is required. It should be one of the values [a, b, c, d, e, f, g, h]"
-    exit 2
-fi
+# if [ $# -ne 1 ]; then
+#     log "Error: Only speaker id is required. It should be one of the values [a, b, c, d, e, f, g, h]"
+#     exit 2
+# fi
 
-speaker_id=$1
-
-if [ $speaker_id = "all" ]; then
-    ./local/data_multi_speaker.sh
-    exit 0
-fi
+# speaker_id=$1
 
 . ./path.sh || exit 1
 . ./cmd.sh || exit 1
@@ -38,10 +33,11 @@ if [ -z "${TALROMUR}" ]; then
 fi
 db_root=${TALROMUR}
 
-full_set=full_${speaker_id}
-train_set=train_${speaker_id}
-train_dev=dev_${speaker_id}
-eval_set=eval1_${speaker_id}
+full_set=full_all
+train_set=train_all
+deveval_set=deveval_all
+train_dev=dev_all
+eval_set=eval1_all
 
 if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
     log "stage -1: Data Download"
@@ -64,23 +60,48 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
     [ -e ${text} ] && rm ${text}
 
     # make scp, utt2spk, and spk2utt
-    find ${db_root}/${speaker_id} -follow -name "*.wav" | sort | while read -r filename;do
+    find ${db_root}/{a,b,c,d,e,f,g,h} -follow -name "*.wav" | sort | while read -r filename;do
+        spk_id=$(basename $(dirname $(dirname ${filename})))
         id=$(basename ${filename} | sed -e "s/\.[^\.]*$//g")
-        echo "${id} ${filename}" >> ${scp}
-        echo "${id} ${speaker_id}" >> ${utt2spk}
+        echo "${spk_id}_${id} ${filename}" >> ${scp}
+        echo "${spk_id}_${id} ${spk_id}" >> ${utt2spk}
     done
+    sort ${utt2spk} > ${utt2spk}_sorted
+    mv ${utt2spk}_sorted ${utt2spk}
+    sort ${scp} > ${scp}_sorted
+    mv ${scp}_sorted ${scp}
     utils/utt2spk_to_spk2utt.pl ${utt2spk} > ${spk2utt}
 
+    # Collect all alignment files in one directory
+    echo "Gathering alignment data"
+    mkdir -p data/alignments
+    find ${db_root}/alignments/ -name "*.TextGrid" -type f | while read -r filepath;do
+        #filepath is .../<spk_id>/audio/<filename>
+        spk_id=$(basename $(dirname $(dirname ${filepath})))
+        filename=$(basename $filepath)
+        if [ ! -e data/alignments/${spk_id}_$filename ]; then
+            ln -s $filepath data/alignments/${spk_id}_$filename 
+        fi
+    done
+
     # Trim leading and trailing silences from audio using sox
-    python local/data_utils.py ${db_root}/alignments/${speaker_id}/audio ${scp}
+    echo "Trimming audio"
+    python local/data_utils.py data/alignments ${scp}
 
     # make text usign the original text
     # cleaning and phoneme conversion are performed on-the-fly during the training
-    paste -d " " \
-        <(cut -f 1 < ${db_root}/${speaker_id}/index.tsv) \
-        <(cut -f 4 < ${db_root}/${speaker_id}/index.tsv) \
-        >> ${text}
-
+    echo "Collecting text prompts"
+    for speaker_id in "a" "b" "c" "d" "e" "f" "g" "h"
+    do
+        paste -d " " \
+            <(cut -f 1 < ${db_root}/${speaker_id}/index.tsv | sed "s/^/${speaker_id}_/") \
+            <(cut -f 4 < ${db_root}/${speaker_id}/index.tsv) \
+            >> ${text}
+    done
+    sort ${text} > ${text}_sorted
+    mv ${text}_sorted ${text}
+    echo "Validating data directory"
+    utils/fix_data_dir.sh data/${full_set}
     utils/validate_data_dir.sh --no-feats data/${full_set}
 fi
 
@@ -89,18 +110,19 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     if [ -e val_utts.txt ]; then rm val_utts.txt; fi
     if [ -e test_utts.txt ]; then rm test_utts.txt; fi
     if [ -e train_utts.txt ]; then rm train_utts.txt; fi
-
     # make evaluation and devlopment sets
-    paste -d " " \
-        <(cut -f 1 < ${db_root}/split/${speaker_id}_val.txt) \
-        >> val_utts.txt
-    paste -d " " \
-        <(cut -f 1 < ${db_root}/split/${speaker_id}_test.txt) \
-        >> test_utts.txt
-    paste -d " " \
-        <(cut -f 1 < ${db_root}/split/${speaker_id}_train.txt) \
-        >> train_utts.txt
-
+    for speaker_id in "a" "b" "c" "d" "e" "f" "g" "h"
+    do
+        paste -d " " \
+            <(cut -f 1 < ${db_root}/split/${speaker_id}_val.txt | sed "s/^/${speaker_id}_/") \
+            >> val_utts.txt
+        paste -d " " \
+            <(cut -f 1 < ${db_root}/split/${speaker_id}_test.txt | sed "s/^/${speaker_id}_/") \
+            >> test_utts.txt
+        paste -d " " \
+            <(cut -f 1 < ${db_root}/split/${speaker_id}_train.txt | sed "s/^/${speaker_id}_/") \
+            >> train_utts.txt
+    done
     utils/subset_data_dir.sh --utt-list val_utts.txt data/${full_set} data/${train_dev}
     utils/subset_data_dir.sh --utt-list test_utts.txt data/${full_set} data/${eval_set}
     utils/subset_data_dir.sh --utt-list train_utts.txt data/${full_set} data/${train_set}
--- egs2/talromur/tts1/train_fastspeech2.sh
+++ egs2/talromur/tts1/train_fastspeech2.sh
@@ -19,12 +19,12 @@ expdir="exp/${speaker_id}"
     --cleaner none \
     --g2p none \
     --train_config ./conf/tuning/train_tacotron2.yaml \
-    --tts_exp exp_${speaker_id}/tts_train_tacotron2_raw_phn_none \
+    --tts_exp exp/${speaker_id}/tts_train_tacotron2_raw_phn_none \
     --inference_args "--use_teacher_forcing true"
 
 # # Run fastspeech2 training
 ./run.sh --stage 5 \
-    --ngpu 2 \
+    --ngpu 1 \
     --train_set train_${speaker_id}_phn \
     --valid_set dev_${speaker_id}_phn \
     --test_sets "dev_${speaker_id}_phn eval1_${speaker_id}_phn" \
@@ -33,6 +33,6 @@ expdir="exp/${speaker_id}"
     --g2p none \
     --cleaner none \
     --train_config conf/tuning/train_fastspeech2.yaml \
-    --teacher_dumpdir exp_${speaker_id}/tts_train_tacotron2_raw_phn_none/decode_use_teacher_forcingtrue_train.loss.ave \
-    --tts_stats_dir exp_${speaker_id}/tts_train_tacotron2_raw_phn_none/decode_use_teacher_forcingtrue_train.loss.ave/stats
+    --teacher_dumpdir exp/${speaker_id}/tts_train_tacotron2_raw_phn_none/decode_use_teacher_forcingtrue_train.loss.ave \
+    --tts_stats_dir exp/${speaker_id}/tts_train_tacotron2_raw_phn_none/decode_use_teacher_forcingtrue_train.loss.ave/stats
     --write_collected_feats true
