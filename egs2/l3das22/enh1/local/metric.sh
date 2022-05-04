996717f8bdb6525e2c3d9fe440f05e193972a5ec
neillu23 <neilyenjulu@gmail.com>
2022-05-04 13:05:16 -0400

    Add installation for the metric calculation

--- egs2/l3das22/enh1/local/metric.sh
+++ egs2/l3das22/enh1/local/metric.sh
@@ -3,17 +3,21 @@
 [ -f ./path.sh ] && . ./path.sh
 . parse_options.sh || exit 1;
 
+pip show -f jiwer >/dev/null || pip install jiwer
+pip show -f pystoi >/dev/null || pip install pystoi
+pip show -f transformers >/dev/null || pip install transformers
+
 if [ $# -lt 1 ]; then
-  echo "Usage: local/metric.sh <predicted scp> <target scp>"
-  exit 1;
+    echo "Usage: local/metric.sh <predicted scp> <target scp>"
+    exit 1;
 fi
 
 if [ $# -gt 1 ]; then
-	predicted_path=$1
-	target_path=$2
-  python local/metrics.py --predicted_path ${predicted_path} --target_path ${target_path}
+    predicted_path=$1
+    target_path=$2
+    python local/metric.py --predicted_path ${predicted_path} --target_path ${target_path}
 else
-	python local/metrics.py
+    python local/metric.py
 fi
 
 exit 0
