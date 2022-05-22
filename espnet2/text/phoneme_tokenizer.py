26e6c84bc3fb095a57e4a85d2541347b7485fb4a
Gunnar Thor <ornolfsson@gmail.com>
2022-05-22 11:11:26 +0000

    add icelandic g2p integration into phoneme_tokenizer.py

--- espnet2/text/phoneme_tokenizer.py
+++ espnet2/text/phoneme_tokenizer.py
@@ -36,6 +36,7 @@ g2p_choices = [
     "g2pk_no_space",
     "korean_jaso",
     "korean_jaso_no_space",
+    "g2p_is",
 ]
 
 
@@ -357,6 +358,37 @@ class Phonemizer:
             return [c.replace(" ", "<space>") for c in tokens]
 
 
+class IsG2p:  # pylint: disable=too-few-public-methods  
+    """
+    Minimal wrapper for https://github.com/grammatek/ice-g2p
+    The g2p module uses a Bi-LSTM model along with
+    a pronunciation dictionary to generate phonemization
+    Unfortunately does not support multi-thread phonemization as of yet
+    """
+    def __init__(
+            self,
+            dialect: str = "standard",
+            syllabify: bool = True,
+            word_sep: str = ",",
+            use_dict: bool = True,
+    ):
+        self.dialect = dialect
+        self.syllabify = syllabify
+        self.use_dict = use_dict
+        from ice_g2p.transcriber import Transcriber
+
+        self.transcriber = Transcriber(
+            use_dict=self.use_dict,
+            syllab_symbol='.',
+            stress_label=True,
+            word_sep=word_sep,
+            lang_detect=True
+        )
+
+    def __call__(self, text) -> List[str]:
+        return self.transcriber.transcribe(text).split()
+
+
 class PhonemeTokenizer(AbsTokenizer):
     def __init__(
         self,
@@ -477,6 +509,10 @@ class PhonemeTokenizer(AbsTokenizer):
             self.g2p = Jaso(space_symbol=space_symbol, no_space=False)
         elif g2p_type == "korean_jaso_no_space":
             self.g2p = Jaso(no_space=True)
+        elif g2p_type == "g2p_is":
+            self.g2p = IsG2p()
+        elif g2p_type == "g2p_is_north":
+            self.g2p = IsG2p(dialect="north")
         else:
             raise NotImplementedError(f"Not supported: g2p_type={g2p_type}")
 
