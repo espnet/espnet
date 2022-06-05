415f89195690c87d6711bc2a0864990d48fee459
ftshijt <728307998@qq.com>
2022-06-05 12:48:24 -0400

    clean data.sh

--- egs2/open_li110/asr1/local/data.sh
+++ egs2/open_li110/asr1/local/data.sh
@@ -10,7 +10,7 @@
 shopt -s extglob
 
 # general configuration
-stage=0       # start from 0 if you need to start from data preparation
+stage=2       # start from 0 if you need to start from data preparation
 stop_stage=100
 SECONDS=0
 # missing tig because of only 6 utt in cv
@@ -22,10 +22,7 @@ langs="ab af bg cv eo fy-NL hu ka lv mt pl sah\
  ba ckb dv fa ha ig ky yo ml nn-NO ro sr tr vot bas\
  cnh el fi hi it lg mn or ru sv-SE tt yue be cs en\
  fr hsb ja jv lt mr pa-IN rw sw ug zh-CN si af in hr sv"
-langs="vot bas\
- cnh el fi hi it lg mn or ru sv-SE tt yue be cs en\
- fr hsb ja jv lt mr pa-IN rw sw ug zh-CN si af in hr sv"
-extra_langs="rm-sursilv rm-vallader kmr"
+extra_langs="rm-sursilv rm-vallader kmr mhr sv-SE" # for cv only
 lid=true
 nlsyms_txt=data/local/nlsyms.txt
 
@@ -46,10 +43,10 @@ set -o pipefail
 
 langs=$(echo "${langs}" | tr _ " ")
 voxforge_lang="de en es fr it nl pt ru"
-commonvoice_lang="ab bg cv eo fy-NL hu ka lv mt pl sah ta uk zh-HK ar bn cy es ga-IE hy-AM kab mdf myv pt sat th ur zh-TW as br da et gl ia kk mhr nan-tw rm-sursilv sk uz az ca de eu gn id kmr mk nl rm-vallader sl tok vi ba ckb dv fa ha ig ky ml nn-NO ro sr tr vot bas cnh el fi hi it lg mn or ru sv-SE tt yue be cs en fr hsb ja lt mr pa-IN rw sw ug zh-CN"
+commonvoice_lang="ab bg cv eo fy-NL hu ka lv mt pl sah ta uk zh-HK ar bn cy es ga-IE hy-AM kab mdf myv pt sat th ur zh-TW as br da et gl ia kk nan-tw sk uz az ca de eu gn id mk nl sl tok vi ba ckb dv fa ha ig ky ml nn-NO ro sr tr vot bas cnh el fi hi it lg mn or ru tt yue be cs fr hsb ja lt pa-IN rw sw ug zh-CN"
 googlei18n_lang_30_32_37="af st tn xh bn"
 googlei18n_lang_asr="jv su si bn ne"
-googlei18n_lang_tts="jv km ne su es ml mr ta te ca en es es es es es eu gl gu kn yo"
+googlei18n_lang_tts="jv km ne su es ml mr ta te ca es es es es es eu gl gu kn yo"
 mls_lang="en de fr it es nl pl pt"
 voxpopuli_lang="en de fr es pl it ro hu cs nl fi hr sk sl et lt"
 
@@ -74,7 +71,8 @@ for lang in ${langs}; do
             # base url for downloads.
             data_url=https://mozilla-common-voice-datasets.s3.dualstack.us-west-2.amazonaws.com/cv-corpus-9.0-2022-04-27/cv-corpus-9.0-2022-04-27-${lang}.tar.gz
 
-            # local/voxforge/download_and_untar.sh ${COMMONVOICE} ${data_url} ${lang}.tar.gz
+            local/voxforge/download_and_untar.sh ${COMMONVOICE} ${data_url} ${lang}.tar.gz
+            # (Optional) remove archived file
             # rm -f ${COMMONVOICE}/${lang}.tar.gz
         fi
 
@@ -176,8 +174,8 @@ for lang in ${langs}; do
         if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
             log "sub-stage 0: Download Data to ${MLS}"
         
-            # local/mls/download_and_untar.sh ${MLS} ${data_url} mls_${download_id}.tar.gz
-            # local/mls/download_and_untar.sh ${MLS} ${lm_data_url} mls_lm_${download_id}.tar.gz
+            local/mls/download_and_untar.sh ${MLS} ${data_url} mls_${download_id}.tar.gz
+            local/mls/download_and_untar.sh ${MLS} ${lm_data_url} mls_lm_${download_id}.tar.gz
             # Optional: mls corpus is large. You might want to remove them after processing
             # rm -f ${MLS}/mls_${download_id}.tar.gz
             # rm -f ${MLS}/mls_lm_${download_id}.tar.gz
@@ -291,7 +289,6 @@ for lang in ${langs}; do
                 lang_id=$(echo ${dialect_lang} | cut -f2 -d-)
                 gender=$(echo ${dialect_lang} | cut -f3 -d-)
                 data_folder=${GOOGLEI18N}/openslr${openslr_id}_${lang_id}/${lang_id}_${gender}
-                # cat ${data_folder}/line_index_male.tsv ${data_folder}/line_index_female.tsv > ${data_folder}/line_index.tsv
             done
              
         fi
@@ -312,13 +309,20 @@ for lang in ${langs}; do
                 awk -v lang_id="${lang_id}" -F '[\t]' \
                     '{print $1 "-" lang_id "_openslr " $2}' \
                     ${data_folder}/line_index.tsv  > ${target_dir}/text
-                awk -v lang_id="${lang_id}" -v src_dir="${data_folder}" -F '[_\t]' \
-                    '{print $1 "_" $2 "_" $3 "-" lang_id "_openslr " src_dir "/" $1 "_" $2 "_" $3 ".wav"}' \
-                    ${data_folder}/line_index.tsv  > ${target_dir}/wav.scp
+                if [ ${lang_id} = bn_in ]; then
+                    awk -v lang_id="${lang_id}" -v src_dir="${data_folder}" -F '[_\t]' \
+                        '{print $1 "_" $2 "_" $3 "-" lang_id "_openslr " src_dir "/" $1 "/" $1 "_" $2 "_" $3}' \
+                        ${data_folder}/line_index.tsv  > ${target_dir}/wav.scp
+                else
+                    awk -v lang_id="${lang_id}" -v src_dir="${data_folder}" -F '[_\t]' \
+                        '{print $1 "_" $2 "_" $3 "-" lang_id "_openslr " src_dir "/" $1 "/" $1 "_" $2 "_" $3 ".wav"}' \
+                        ${data_folder}/line_index.tsv  > ${target_dir}/wav.scp
+                fi
                 sort ${target_dir}/utt2spk -o ${target_dir}/utt2spk
                 sort ${target_dir}/text -o ${target_dir}/text
                 sort ${target_dir}/wav.scp -o ${target_dir}/wav.scp
                 utils/utt2spk_to_spk2utt.pl ${target_dir}/utt2spk > ${target_dir}/spk2utt
+                echo "${data_folder}"
                 utils/validate_data_dir.sh --non-print --no-feats ${target_dir}
 
                 utils/subset_data_dir.sh \
@@ -357,7 +361,7 @@ for lang in ${langs}; do
                 openslr_id=$(echo ${dialect_lang} | cut -f1 -d-)
                 lang_id=$(echo ${dialect_lang} | cut -f2 -d-)
                 gender=$(echo ${dialect_lang} | cut -f3 -d-)
-                data_folder=${GOOGLEI18N}/openslr${openslr_id}_${lang_id}/${lang_id}_${gender}
+                data_folder=${GOOGLEI18N}/openslr${openslr_id}_${lang_id}/${lang_id}_${gender}/
                 target_dir=data/openslr${openslr_id}_${lang_id}_${gender}
                 mkdir -p ${target_dir}
 
@@ -368,7 +372,7 @@ for lang in ${langs}; do
                     '{print $1 "-" lang_id "_openslr " $2}' \
                     ${data_folder}/line_index.tsv  > ${target_dir}/text
                 awk -v lang_id="${lang_id}" -v src_dir="${data_folder}" -F '[_\t]' \
-                    '{print $1 "_" $2 "_" $3 "-" lang_id "_openslr " src_dir "/" $1 "_" $2 "_" $3 ".wav"}' \
+                    '{print $1 "_" $2 "_" $3 "-" lang_id "_openslr " src_dir "/wavs/" $1 "_" $2 "_" $3 ".wav"}' \
                     ${data_folder}/line_index.tsv  > ${target_dir}/wav.scp
                 sort ${target_dir}/utt2spk -o ${target_dir}/utt2spk
                 sort ${target_dir}/text -o ${target_dir}/text
@@ -457,7 +461,7 @@ for lang in ${langs}; do
                 ${prefix_info}/utt_spk_text.tsv  > ${target_dir}/text
 
             awk -v lang_id="${lang_id}" -v src_dir="${prefix_info}" -F '[_\t]' \
-                '{print $2 "_" $1 "-" lang_id "_openslr " src_dir "data/" $1 ".wav"}' \
+                '{print $2 "_" $1 "-" lang_id "_openslr " src_dir "data/" substr($1, 1, 2) "/" $1 ".flac"}' \
                 ${prefix_info}/utt_spk_text.tsv  > ${target_dir}/wav.scp
 
             sort ${target_dir}/utt2spk -o ${target_dir}/utt2spk
@@ -520,7 +524,7 @@ for lang in ${langs}; do
         fi
 
         if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
-            log "sub-stage 1: Data Preparation for Openslr ASR corpora"
+            log "sub-stage 1: Data Preparation for Openslr low-resource ASR corpora"
             for direct_lang in ${processing_list}; do
                 openslr_id=$(echo ${direct_lang} | cut -f1 -d-)
                 prefix=$(echo ${direct_lang} | cut -f2 -d-)
@@ -537,7 +541,7 @@ for lang in ${langs}; do
                     ${data_folder}/line_index.tsv  > ${target_dir}/text
 
                 awk -v lang="${lang}" -v src_dir="${data_folder}" -F '[_\t]' \
-                    '{print $1 "_" $2 "_" $3 "-" lang_id "_openslr " src_dir "/" $1 "_" $2 "_" $3 ".wav"}' \
+                    '{print $1 "_" $2 "_" $3 "-" lang_id "_openslr " src_dir "/wavs/" $1 "_" $2 "_" $3 ".wav"}' \
                     ${data_folder}/line_index.tsv  > ${target_dir}/wav.scp
 
                 sort ${target_dir}/utt2spk -o ${target_dir}/utt2spk
@@ -603,7 +607,7 @@ for lang in ${langs}; do
                 awk -v lang="${lang}" -v src_dir="${VOXPOPULI}/transcribed_data/${lang}/" \
                     -F '[\t]' \
                    '{
-                        printf "%010d_%s-%s_voxpopuli sox %s%s/%s.ogg - |\n",
+                        printf "%010d_%s-%s_voxpopuli sox %s%s/%s.ogg -t wav - |\n",
                         $4, $1, lang, src_dir, substr($1, 1, 4), $1
                    }' \
                    ${src_index} > ${target_dir}/wav.scp
@@ -622,14 +626,14 @@ done
 
 # Processing extra langs
 for lang in ${extra_langs}; do
-    if [[ "${commonvoice_lang}" == *" ${lang}"* || "${commonvoice_lang}" == *"${lang} "* ]]; then
         if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
             log "sub-stage 0: Download Data to ${COMMONVOICE}"
 
             # base url for downloads.
             data_url=https://mozilla-common-voice-datasets.s3.dualstack.us-west-2.amazonaws.com/cv-corpus-9.0-2022-04-27/cv-corpus-9.0-2022-04-27-${lang}.tar.gz
 
-            # local/voxforge/download_and_untar.sh ${COMMONVOICE} ${data_url} ${lang}.tar.gz
+            local/voxforge/download_and_untar.sh ${COMMONVOICE} ${data_url} ${lang}.tar.gz
+            # (Optional) remove archieve files
             # rm -f ${COMMONVOICE}/${lang}.tar.gz
         fi
 
@@ -661,8 +665,66 @@ for lang in ${extra_langs}; do
             utils/fix_data_dir.sh data/${train_subset}
         fi
         test_set="${test_set} ${test_subset}"
-    fi
 done
+
+# Full test set for reference
+'''
+test_set="test_ab_commonvoice test_fy_NL_commonvoice test_or_commonvoice \
+test_af_openslr32 test_ga_IE_commonvoice test_pa_IN_commonvoice \
+test_ar_commonvoice test_gl_commonvoice test_pl \
+test_as_commonvoice test_gl_openslr77 test_pl_commonvoice \
+test_az_commonvoice test_gn_commonvoice test_pl_mls \
+test_ba_commonvoice test_gu_openslr78 test_pl_voxpopuli \
+test_bas_commonvoice test_ha_commonvoice test_pt_commonvoice \
+test_be_commonvoice test_hi_commonvoice test_pt_mls \
+test_bg_commonvoice test_hr_voxpopuli test_pt_voxforge \
+test_bn_commonvoice test_hsb_commonvoice test_rm_sursilv_commonvoice \
+test_bn_openslr37 test_hu_commonvoice \
+test_bn_openslr53 test_hu_voxpopuli test_rm_vallader_commonvoice \
+test_br_commonvoice test_hy_AM_commonvoice \
+test_ca_commonvoice test_ia_commonvoice test_ro_commonvoice \
+test_ca_openslr69 test_id_commonvoice test_ro_voxpopuli \
+test_ckb_commonvoice test_ig_commonvoice test_ru_commonvoice \
+test_cnh_commonvoice test_it_commonvoice test_ru_voxforge \
+test_cs_commonvoice test_it_mls test_rw_commonvoice \
+test_cs_voxpopuli test_it_voxforge test_sah_commonvoice \
+test_cv_commonvoice test_it_voxpopuli test_sat_commonvoice \
+test_cy_commonvoice test_ja_commonvoice test_si_openslr52 \
+test_da_commonvoice test_jv_openslr35 test_sk_commonvoice \
+test_de_commonvoice test_jv_openslr41_female test_sk_voxpopuli \
+test_de_mls test_jv_openslr41_male test_sl_commonvoice \
+test_de_voxforge test_kab_commonvoice test_sl_voxpopuli \
+test_de_voxpopuli test_ka_commonvoice test_sr_commonvoice \
+test_dv_commonvoice test_kk_commonvoice test_st_openslr32 \
+test_el_commonvoice test_km_openslr42_male test_su_openslr36 \
+test_en_commonvoice test_kmr_commonvoice test_su_openslr44_female \
+test_en_mls test_su_openslr44_male \
+test_en_openslr70 test_kn_openslr79 test_sv_SE_commonvoice \
+test_en_voxforge test_ky_commonvoice \
+test_en_voxpopuli test_lg_commonvoice test_sw_commonvoice \
+test_eo_commonvoice test_lt_commonvoice test_ta_commonvoice \
+test_es_commonvoice test_lt_voxpopuli test_ta_openslr65 \
+test_es_mls test_lv_commonvoice test_te_openslr66 \
+test_es_openslr61 test_mdf_commonvoice test_th_commonvoice \
+test_es_openslr71 test_mhr_commonvoice test_tig_commonvoice \
+test_es_openslr72 test_tn_openslr32 \
+test_es_openslr73 test_mk_commonvoice test_tok_commonvoice \
+test_es_openslr74 test_ml_commonvoice test_tr_commonvoice \
+test_es_openslr75 test_ml_openslr63 test_tt_commonvoice \
+test_es_voxforge test_mn_commonvoice test_ug_commonvoice \
+test_es_voxpopuli test_mr_commonvoice test_uk_commonvoice \
+test_et_commonvoice test_mr_openslr64 test_ur_commonvoice \
+test_et_voxpopuli test_mt_commonvoice test_uz_commonvoice \
+test_eu_commonvoice test_myv_commonvoice test_vi_commonvoice \
+test_eu_openslr76 test_nan_tw_commonvoice test_vot_commonvoice \
+test_fa_commonvoice test_ne_openslr43_female test_xh_openslr32 \
+test_fi_commonvoice test_ne_openslr54 test_yo_openslr86 \
+test_fi_voxpopuli test_nl_commonvoice test_yue_commonvoice \
+test_fr_commonvoice test_nl_mls test_zh_CN_commonvoice \
+test_fr_mls test_nl_voxforge test_zh_HK_commonvoice \
+test_fr_voxforge test_nl_voxpopuli test_zh_TW_commonvoice \
+test_fr_voxpopuli test_nn_NO_commonvoice"
+'''
 
 log "Using test sets: ${test_set}"
 
@@ -691,9 +753,14 @@ if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
               <(cut -f 1 -d" " ${x}/text.org) \
               <(cut -f 2- -d" " ${x}/text.org \
                 | python3 -c 'import sys; print(sys.stdin.read().upper(), end="")' \
-                | python3 -c 'import string; print(sys.stdin.read().translate(str.maketrans("", "", string.punctuation)), end="")') \
+                | python3 -c 'import string; import sys; print(sys.stdin.read().translate(str.maketrans("", "", string.punctuation)), end="")') \
               > ${x}/text
         rm ${x}/text.org
+        paste -d " " \
+            <(cut -f 1 -d" " ${x}/utt2spk) \
+            <(cut -f 1 -d" " ${x}/utt2spk) \
+            > ${x}/utt2spk.identical
+        mv ${x}/utt2spk.identical ${x}/utt2spk
     done
 
     for x in ${test_set}; do
@@ -702,10 +769,11 @@ if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
               <(cut -f 1 -d" " data/${x}/text.org) \
               <(cut -f 2- -d" " data/${x}/text.org \
                 | python3 -c 'import sys; print(sys.stdin.read().upper(), end="")' \
-                | python3 -c 'import string; print(sys.stdin.read().translate(str.maketrans("", "", string.punctuation)), end="")') \
+                | python3 -c 'import string; import sys; print(sys.stdin.read().translate(str.maketrans("", "", string.punctuation)), end="")') \
               > data/${x}/text
         rm data/${x}/text.org
     done
+
 fi
 
 if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
