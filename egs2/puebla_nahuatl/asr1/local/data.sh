a982a04f0ecc1064d340f4ab4e28c8593e4264dc
Jiatong <728307998@qq.com>
2022-07-20 23:38:19 -0700

    Add extra space for aligning

--- egs2/puebla_nahuatl/asr1/local/data.sh
+++ egs2/puebla_nahuatl/asr1/local/data.sh
@@ -43,9 +43,9 @@ log "data preparation started"
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then 
     log "stage1: Download data to ${PUEBLA_NAHUATL}"
     mkdir -p ${PUEBLA_NAHUATL}
-   local/download_and_untar.sh local  https://www.openslr.org/resources/92/Puebla-Nahuatl-Manifest.tgz Puebla-Nahuatl-Manifest.tgz
-   local/download_and_untar.sh ${PUEBLA_NAHUATL} https://www.openslr.org/resources/92/Sound-Files-Puebla-Nahuatl.tgz.part0 Sound-Files-Puebla-Nahuatl.tgz.part0 9
-   git clone https://github.com/ftshijt/Puebla_Nahuatl_Split.git local/split
+    local/download_and_untar.sh local  https://www.openslr.org/resources/92/Puebla-Nahuatl-Manifest.tgz Puebla-Nahuatl-Manifest.tgz
+    local/download_and_untar.sh ${PUEBLA_NAHUATL} https://www.openslr.org/resources/92/Sound-Files-Puebla-Nahuatl.tgz.part0 Sound-Files-Puebla-Nahuatl.tgz.part0 9
+    git clone https://github.com/ftshijt/Puebla_Nahuatl_Split.git local/split
 fi
 
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
