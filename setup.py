8379cc345154920c8201a8988769af045d894e36
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2022-08-17 22:35:27 +0900

    Fix github-actions's caching problems

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ requirements = {
         "torch>=1.3.0",
         "torch_complex",
         "nltk>=3.4.5",
+        "numpy<=1.21.4",
         # https://github.com/espnet/espnet/runs/6646737793?check_suite_focus=true#step:8:7651
         "protobuf<=3.20.1",
         # ASR
@@ -75,7 +76,6 @@ requirements = {
         "gtn==0.0.0",
     ],
     "setup": [
-        "numpy<=1.21.4",
         "pytest-runner",
     ],
     "test": [
