7b2f2cf6ef3808d009d1c1933b5b05d2ee6e1c9a
Siddhant <siddhana@v034.ib.bridges2.psc.edu>
2022-08-20 15:44:24 -0400

    Fix CI check

--- espnet2/bin/slu_inference.py
+++ espnet2/bin/slu_inference.py
@@ -464,7 +464,9 @@ def inference(
         batch_size=batch_size,
         key_file=key_file,
         num_workers=num_workers,
-        preprocess_fn=SLUTask.build_preprocess_fn(speech2understand.asr_train_args, False),
+        preprocess_fn=SLUTask.build_preprocess_fn(
+            speech2understand.asr_train_args, False
+        ),
         collate_fn=SLUTask.build_collate_fn(speech2understand.asr_train_args, False),
         allow_variable_data_keys=allow_variable_data_keys,
         inference=True,
