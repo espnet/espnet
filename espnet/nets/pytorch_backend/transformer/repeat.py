a294ca6907406d75efcb87fea0bb48ee3bc06138
Kwangyoun Kim <kkim@asapp.com>
2022-12-07 09:09:03 -0800

    Set default value for argument

--- espnet/nets/pytorch_backend/transformer/repeat.py
+++ espnet/nets/pytorch_backend/transformer/repeat.py
@@ -12,7 +12,7 @@ import torch
 class MultiSequential(torch.nn.Sequential):
     """Multi-input multi-output torch.nn.Sequential."""
 
-    def __init__(self, *args, layer_drop_rate):
+    def __init__(self, *args, layer_drop_rate=0.0):
         """Initialize MultiSequential with layer_drop.
 
         Args:
@@ -26,7 +26,7 @@ class MultiSequential(torch.nn.Sequential):
         """Repeat."""
         _probs = torch.empty(len(self)).uniform_()
         for idx, m in enumerate(self):
-            if not self.training or (_probs[idx] > self.layer_drop_rate):
+            if not self.training or (_probs[idx] >= self.layer_drop_rate):
                 args = m(*args)
         return args
 
