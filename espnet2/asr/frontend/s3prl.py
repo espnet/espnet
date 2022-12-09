39ee02645f262df2f05def80aab4db973412561a
karthik19967829 <karthikganesan17@gmail.com>
2022-12-09 01:00:52 -0500

    add isort

--- espnet2/asr/frontend/s3prl.py
+++ espnet2/asr/frontend/s3prl.py
@@ -7,9 +7,9 @@ import torch
 from typeguard import check_argument_types
 
 from espnet2.asr.frontend.abs_frontend import AbsFrontend
+from espnet2.asr.frontend.adapter_utils.add_adapters import add_adapters
 from espnet2.utils.get_default_kwargs import get_default_kwargs
 from espnet.nets.pytorch_backend.frontends.frontend import Frontend
-from espnet2.asr.frontend.adapter_utils.add_adapters import add_adapters
 
 
 class S3prlFrontend(AbsFrontend):
