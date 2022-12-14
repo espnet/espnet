36a5c7445c3c69d36d1b86e007abb24177d0de36
Fhrozen <nyalta21@gmail.com>
2022-12-14 21:56:57 +0900

    format files

--- egs2/TEMPLATE/asr1/scripts/utils/prep_data_mfa_align.sh
+++ egs2/TEMPLATE/asr1/scripts/utils/prep_data_mfa_align.sh
@@ -4,8 +4,8 @@ set -e
 set -u
 set -o pipefail
 
-. ./path.sh || exit 1;
-. ./cmd.sh || exit 1;
+# Generate MFA alignement
+
 
 log() {
     local fname=${BASH_SOURCE[1]##*/}
@@ -58,9 +58,12 @@ EOF
 
 log "$0 $*"
 # Save command line args for logging (they will be lost after utils/parse_options.sh)
-run_args=$(pyscripts/utils/print_args.py $0 "$@")
+# run_args=$(pyscripts/utils/print_args.py $0 "$@")
+. ./path.sh || exit 1;
+. ./cmd.sh || exit 1;
+
 . utils/parse_options.sh
-echo $@
+
 if [ $# -ne 0 ]; then
     log "${help_message}"
     log "Error: No positional arguments are required."
@@ -73,12 +76,7 @@ tempdir=${workdir}/tmp
 
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
     log "stage 0: Prepare Data set for MFA"
-    # TODO(fhrozen): replace this for a general data set, and use utt2spk to generate the folder structure
-    for fn in $(find ${db_root}/LJSpeech-1.1/wavs -name "*.wav");do
-        ln -sf $(readlink -e ${fn}) ${corpus_dir}
-    done
-
-    # Text cleaning and save it in lab files (independly)
+    # Text cleaning and save it in wav/lab files (by file)
     python pyscripts/prepare_labs.py --cleaner tacotron \
         ${corpus_txt} \
         ${corpus_dir}
@@ -159,7 +157,7 @@ EOF
         ${acoustic} \
         ${workdir}/aligments
 
-    if ${clean}; then
+    if ${clean_temp}; then
         # Be careful, this will delete all the files employed for training the mfa's models.
         rm -rf ${tempdir}
     fi
@@ -189,3 +187,76 @@ if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
         utils/filter_scp.pl data/${dset}_phn/utt2spk ${workdir}/word_durations > data/${dset}_phn/word_durations
     done
 fi
+
+
+
+# You need to install the following tools to run this script:
+# $ conda config --append channels conda-forge
+# $ conda install montreal-forced-aligner
+# If you are not using LJSpeech, be sure to define how your
+# dataset is processed in `scripts/utils/mfa_format.py`.
+
+set -e
+
+acoustic_model="english_mfa"
+dictionary="english_us_mfa"
+g2p_model="english_us_mfa"
+dataset="ljspeech"
+wavs_dir="downloads/LJSpeech-1.1/wavs"
+
+. utils/parse_options.sh
+
+if [[ "$(basename "$(pwd)")" != tts* ]]; then
+    echo "You must cd to a tts directory"
+    exit 1
+fi
+
+# download pretrained MFA models
+mfa models download acoustic "${acoustic_model}"
+mfa models download dictionary "${dictionary}"
+mfa models download g2p "${g2p_model}"
+python scripts/utils/mfa_format.py labs --dataset "${dataset}" --wavs_dir "${wavs_dir}"
+
+# create OOV dictionary
+set +e
+mfa validate "${wavs_dir}" "${dictionary}" "${acoustic_model}" --brackets '' | while read -r line; do
+    if [[ $line =~ "jobs" ]]; then
+        echo "OOV file created, stopping MFA."
+        pkill mfa
+    else
+        echo "$line"
+    fi
+done
+set -e
+
+# create new dictionary including OOV
+mfa_dir="$HOME/Documents/MFA"
+dict_dir="${mfa_dir}/pretrained_models/dictionary"
+oov_dict="${dict_dir}/oov_${dataset}.dict"
+mfa g2p "${g2p_model}" "${mfa_dir}/wavs_validate_pretrained/oovs_found_${dictionary}.txt" "${oov_dict}"
+cat "${dict_dir}/${dictionary}.dict" "${oov_dict}" > "${dict_dir}/${dictionary}_${dataset}.dict"
+
+# check again
+mfa validate "${wavs_dir}" "${dictionary}_${dataset}" "${acoustic_model}" --brackets ''
+
+# perform force alignment
+mfa align "${wavs_dir}" "${dictionary}_${dataset}" "${acoustic_model}" ./textgrids
+
+echo "Successfully finished generating MFA alignments."
+
+# NOTE(iamanigeeit): If you want to train FastSpeech2 with the alignments,
+#   please check `egs2/ljspeech/tts1/local/run_mfa.sh`. For example:
+# $ ./local/run_mfa.sh --stage 0 --stop_stage 0
+# $ ./local/run_mfa.sh --stage 1 --stop_stage 1
+# $ ./local/run_mfa.sh --stage 2 --stop_stage 2
+# $ ./local/run_mfa.sh --stage 3 --stop_stage 3
+# $ ./local/run_mfa.sh --stage 4 --stop_stage 4
+# $ ./local/run_mfa.sh --stage 5 --stop_stage 5 \
+#     --train_config conf/tuning/train_fastspeech2.yaml \
+#     --teacher_dumpdir data \
+#     --tts_stats_dir data/stats \
+#     --write_collected_feats true
+# $ ./local/run_mfa.sh --stage 6 --stop_stage 6 \
+#     --train_config conf/tuning/train_fastspeech2.yaml \
+#     --teacher_dumpdir data \
+#     --tts_stats_dir data/stats
