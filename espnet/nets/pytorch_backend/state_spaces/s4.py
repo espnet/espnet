f980b09f79f0294a35a143c608629c62ba0f4c62
Koichi Miyazaki <koichi_miyazaki_xa@cyberagent.co.jp>
2022-12-20 19:42:50 +0900

    fix import error

--- espnet/nets/pytorch_backend/state_spaces/s4.py
+++ espnet/nets/pytorch_backend/state_spaces/s4.py
@@ -16,7 +16,7 @@ from typing import Callable, Any, Optional
 import os
 from einops import rearrange, repeat
 import opt_einsum as oe
-from espnet.nets.pytorch_backend.state_spaces.components import LinearActivation, DropoutNd
+from espnet.nets.pytorch_backend.state_spaces.components import LinearActivation, DropoutNd, Activation
 
 contract = oe.contract
 contract_expression = oe.contract_expression
@@ -238,82 +238,6 @@ if tuple(map(int, torch.__version__.split(".")[:2])) >= (1, 10):
     _resolve_conj = lambda x: x.conj().resolve_conj()
 else:
     _resolve_conj = lambda x: x.conj()
-
-
-""" Simple nn.Module components """
-
-
-# def Activation(activation=None, dim=-1):
-#     if activation in [None, "id", "identity", "linear"]:
-#         return nn.Identity()
-#     elif activation == "tanh":
-#         return nn.Tanh()
-#     elif activation == "relu":
-#         return nn.ReLU()
-#     elif activation == "gelu":
-#         return nn.GELU()
-#     elif activation in ["swish", "silu"]:
-#         return nn.SiLU()
-#     elif activation == "glu":
-#         return nn.GLU(dim=dim)
-#     elif activation == "sigmoid":
-#         return nn.Sigmoid()
-#     else:
-#         raise NotImplementedError(
-#             "hidden activation '{}' is not implemented".format(activation)
-#         )
-
-
-# def LinearActivation(
-#     d_input,
-#     d_output,
-#     bias=True,
-#     transposed=False,
-#     activation=None,
-#     activate=False,  # Apply activation as part of this module
-#     **kwargs,
-# ):
-#     """Returns a linear nn.Module with control over axes order, initialization, and activation"""
-
-#     # Construct core module
-#     linear_cls = partial(nn.Conv1d, kernel_size=1) if transposed else nn.Linear
-#     if activation == "glu":
-#         d_output *= 2
-#     linear = linear_cls(d_input, d_output, bias=bias, **kwargs)
-
-#     if activate and activation is not None:
-#         activation = Activation(activation, dim=-2 if transposed else -1)
-#         linear = nn.Sequential(linear, activation)
-#     return linear
-
-
-# class DropoutNd(nn.Module):
-#     def __init__(self, p: float = 0.5, tie=True, transposed=True):
-#         """
-#         tie: tie dropout mask across sequence lengths (Dropout1d/2d/3d)
-#         """
-#         super().__init__()
-#         if p < 0 or p >= 1:
-#             raise ValueError(
-#                 "dropout probability has to be in [0, 1), " "but got {}".format(p)
-#             )
-#         self.p = p
-#         self.tie = tie
-#         self.transposed = transposed
-#         self.binomial = torch.distributions.binomial.Binomial(probs=1 - self.p)
-
-#     def forward(self, X):
-#         """X: (batch, dim, lengths...)"""
-#         if self.training:
-#             if not self.transposed:
-#                 X = rearrange(X, "b d ... -> b ... d")
-#             mask_shape = X.shape[:2] + (1,) * (X.ndim - 2) if self.tie else X.shape
-#             mask = torch.rand(*mask_shape, device=X.device) < 1.0 - self.p
-#             X = X * mask * (1.0 / (1 - self.p))
-#             if not self.transposed:
-#                 X = rearrange(X, "b ... d -> b d ...")
-#             return X
-#         return X
 
 
 """ Misc functional utilities """
