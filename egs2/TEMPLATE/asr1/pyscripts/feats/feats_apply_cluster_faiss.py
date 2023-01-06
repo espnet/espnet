e4b3a0374b3cf009af17c584906eaa573cd9728e
Dongji Gao <dgao5@jhu.edu>
2023-01-06 13:51:05 -0500

    add copyright

--- egs2/TEMPLATE/asr1/pyscripts/feats/feats_apply_cluster_faiss.py
+++ egs2/TEMPLATE/asr1/pyscripts/feats/feats_apply_cluster_faiss.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3 -u
 # Copyright (c) Facebook, Inc. and its affiliates.
+#               2022 Johns Hopkins University (Author: Dongji Gao)
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
