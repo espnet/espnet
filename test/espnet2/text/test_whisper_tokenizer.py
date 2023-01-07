ead5daf1d5d4e344b26924354d665468577b10d8
Shih-Lun Wu <slseanwu@dt-login03.delta.internal.ncsa.edu>
2023-01-07 15:42:22 -0600

    add tests for tokenizer

---
+++ test/espnet2/text/test_whisper_tokenizer.py
@@ -0,0 +1,32 @@
+import pytest
+
+from espnet2.text.whisper_tokenizer import (
+    OpenAIWhisperTokenizer
+)
+
+@pytest.fixture(params=["whisper_multilingual"])
+def whisper_tokenizer(request):
+    return OpenAIWhisperTokenizer(request.param)
+
+def test_init_en():
+    tokenizer = OpenAIWhisperTokenizer(
+        "whisper_en"
+    )
+    assert tokenizer.tokenizer.tokenizer.vocab_size == 50257
+
+def test_init_invalid():
+    with pytest.raises(ValueError):
+        OpenAIWhisperTokenizer(
+            "whisper_aaa"
+        )
+
+def test_repr(whisper_tokenizer: OpenAIWhisperTokenizer):
+    print(whisper_tokenizer)
+
+def test_tokenization_consistency(whisper_tokenizer: OpenAIWhisperTokenizer):
+    s = "Hi, today's weather is nice. Hmm..."
+
+    assert s == \
+        whisper_tokenizer.tokens2text(
+            whisper_tokenizer.text2tokens(s)
+        )
