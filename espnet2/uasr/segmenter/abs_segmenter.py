46a201fde518ef17780ead945274450006eab4ed
ftshijt <ftshijt@gmail.com>
2023-01-22 00:56:28 -0500

    remove trailing whitespace

--- espnet2/uasr/segmenter/abs_segmenter.py
+++ espnet2/uasr/segmenter/abs_segmenter.py
@@ -1,9 +1,9 @@
 """
 Segmenter definition for UASR task
 
-Practially, the output of the generator (in frame-level) may 
+Practially, the output of the generator (in frame-level) may
 predict the same phoneme for consecutive frames, which makes
-it too easy for the discriminator. So, the segmenter here is 
+it too easy for the discriminator. So, the segmenter here is
 to merge frames with a similar prediction from the generator output.
 """
 
