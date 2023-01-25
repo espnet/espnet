aa72918d4e9c296d4865e42712a32e76dd9a0c70
Shinji Watanabe <sw005320@gmail.com>
2023-01-25 08:51:42 -0500

    Update test_whisper_encoder.py

--- test/espnet2/asr/encoder/test_whisper_encoder.py
+++ test/espnet2/asr/encoder/test_whisper_encoder.py
@@ -9,7 +9,9 @@ pytest.importorskip("whisper")
 # NOTE(Shih-Lun): needed for `return_complex` param in torch.stft()
 is_torch_1_7_plus = V(torch.__version__) >= V("1.7.0")
 
+is_python_3_8_plus = sys.version_info[0] >= (3, 8)
 
+@pytest.mark.skipif(is_python_3_8_plus)
 @pytest.fixture()
 def whisper_encoder(request):
     if not is_torch_1_7_plus:
@@ -28,6 +30,7 @@ def test_encoder_init(whisper_encoder):
     assert whisper_encoder.output_size() == 384
 
 
+@pytest.mark.skipif(is_python_3_8_plus)
 def test_encoder_invalid_init():
     if not is_torch_1_7_plus:
         return
@@ -37,6 +40,7 @@ def test_encoder_invalid_init():
         del encoder
 
 
+@pytest.mark.skipif(is_python_3_8_plus)
 @pytest.mark.timeout(50)
 def test_encoder_forward_no_ilens(whisper_encoder):
     if not is_torch_1_7_plus:
@@ -50,6 +54,7 @@ def test_encoder_forward_no_ilens(whisper_encoder):
     assert xs_pad.size() == torch.Size([4, 10, 384])
 
 
+@pytest.mark.skipif(is_python_3_8_plus)
 @pytest.mark.timeout(50)
 def test_encoder_forward_ilens(whisper_encoder):
     if not is_torch_1_7_plus:
@@ -67,6 +72,7 @@ def test_encoder_forward_ilens(whisper_encoder):
     assert torch.equal(olens.cpu(), torch.tensor([2, 3, 5, 10]))
 
 
+@pytest.mark.skipif(is_python_3_8_plus)
 @pytest.mark.timeout(50)
 def test_encoder_backward(whisper_encoder):
     if not is_torch_1_7_plus:
