8a550f480155efaf9f298c1316371b0134a19ed4
popcornell <cornellsamuele@gmail.com>
2023-01-30 19:32:11 +0100

    installation script for CHiME-7 Task 1

---
+++ tools/installers/install_chime7task1.sh
@@ -0,0 +1,10 @@
+./install_s3prl.sh
+pip install git+https://github.com/popcornell/lhotse@master
+# cupy
+conda install -yc conda-forge cupy=10.2
+# gpu-gss
+pip install git+http://github.com/desh2608/gss
+# jiwer
+pip install jiwer
+# pyannote
+pip install pyannote-metrics
