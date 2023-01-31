c9feabb513638f32d6668a8974d77744c8ea8d6b
popcornell <cornellsamuele@gmail.com>
2023-02-01 00:52:41 +0100

    renamed to run.sh, moved gss and others in local

---
+++ egs2/chime7_task1/discard4now/evaluate.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+
+stage=
+CHIME7Task1_DIR=
+
