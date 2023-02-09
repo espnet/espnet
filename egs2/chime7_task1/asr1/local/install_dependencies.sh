3b2b91087a1892b895d0f2dc9a6cfc16d452177a
popcornell <cornellsamuele@gmail.com>
2023-02-09 20:44:19 +0100

    yoshiki thanks

--- egs2/chime7_task1/asr1/local/install_dependencies.sh
+++ egs2/chime7_task1/asr1/local/install_dependencies.sh
@@ -19,11 +19,11 @@ if ! command -v wav-reverberate &>/dev/null; then
 fi
 
 # install s3prl
-./tools/installers/install_s3prl.sh
+${MAIN_ROOT}/tools/installers/install_s3prl.sh
 
 if ! command -v gss &>/dev/null; then
   conda install -yc conda-forge cupy=10.2
-  ./${MAIN_ROOT}/tools/install_gss.sh
+  ${MAIN_ROOT}/tools/installers/install_gss.sh.
 fi
 
 sox_conda=`command -v ../../../tools/venv/bin/sox 2>/dev/null`
