4f0c34db958f6da312630ad601e9504f5504f61c
ftshijt <ftshijt@gmail.com>
2023-02-25 09:41:41 -0500

    add additional time ”

--- test/test_transform.py
+++ test/test_transform.py
@@ -9,7 +9,7 @@ from espnet.transform.spectrogram import logmelspectrogram
 from espnet.transform.transformation import Transformation
 
 
-@pytest.mark.execution_timeout(5)
+@pytest.mark.execution_timeout(10)
 def test_preprocessing(tmpdir):
     cmvn_ark = str(tmpdir.join("cmvn.ark"))
     kwargs = {
