478e68faa14da64151e6215ec2b5de9662b41c2b
Naoyuki Kamo <naoyuki.kamo829@gmail.com>
2023-03-07 00:52:41 +0900

    Update install.sh

--- ci/install.sh
+++ ci/install.sh
@@ -20,7 +20,8 @@ ${CXX:-g++} -v
     fi
 
     . ./activate_python.sh
-    make TH_VERSION="${TH_VERSION}" WITH_OMP="${WITH_OMP-ON}" all warp-transducer.done chainer_ctc.done nkf.done moses.done mwerSegmenter.done pesq pyopenjtalk.done py3mmseg.done s3prl.done transformers.done phonemizer.done fairseq.done k2.done gtn.done longformer.done whisper.done
+    # FIXME(kamo): Failed to compile pesq
+    make TH_VERSION="${TH_VERSION}" WITH_OMP="${WITH_OMP-ON}" all warp-transducer.done chainer_ctc.done nkf.done moses.done mwerSegmenter.done pyopenjtalk.done py3mmseg.done s3prl.done transformers.done phonemizer.done fairseq.done k2.done gtn.done longformer.done whisper.done
     rm -rf kaldi
 )
 . tools/activate_python.sh
