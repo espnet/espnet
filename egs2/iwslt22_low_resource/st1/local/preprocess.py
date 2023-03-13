3fd6e274baa2a723c73d34f33c583dc7119b0214
jamfly <freddy@iis.sinica.edu.tw>
2023-03-13 22:56:45 +0800

    Fix linter

--- egs2/iwslt22_low_resource/st1/local/preprocess.py
+++ egs2/iwslt22_low_resource/st1/local/preprocess.py
@@ -1,6 +1,7 @@
 import yaml
 import argparse
 
+
 def get_args() -> argparse.Namespace():
     parser = argparse.ArgumentParser()
     parser.add_argument("--data", "-d", type=str, help="Path to original corpus.")
@@ -9,16 +10,22 @@ def get_args() -> argparse.Namespace():
 
     return args
 
+
 def yaml2files(data_path: str, out_path: str):
     split = data_path.split("/")[-2]
     split = "train" if split == "train_full" else split
 
-    data = {}
-    with open(f"{data_path}/txt/{split}.fra", "r", encoding="utf-8") as org_text_file, \
-        open(f"{data_path}/txt/{split}.yaml", "r", encoding="utf-8") as yaml_file, \
-        open(f"{out_path}/text.fr", "w", encoding="utf-8") as processed_text_file, \
-        open(f"{out_path}/wav.scp", "w", encoding="utf-8") as wav_scp_file, \
-        open(f"{out_path}/utt2spk", "w", encoding="utf-8") as utt2spk_file:
+    with open(
+        f"{data_path}/txt/{split}.fra", "r", encoding="utf-8"
+    ) as org_text_file, open(
+        f"{data_path}/txt/{split}.yaml", "r", encoding="utf-8"
+    ) as yaml_file, open(
+        f"{out_path}/text.fr", "w", encoding="utf-8"
+    ) as processed_text_file, open(
+        f"{out_path}/wav.scp", "w", encoding="utf-8"
+    ) as wav_scp_file, open(
+        f"{out_path}/utt2spk", "w", encoding="utf-8"
+    ) as utt2spk_file:
         text_lines = org_text_file.readlines()
         raw_yaml = yaml.safe_load(yaml_file)
 
@@ -26,7 +33,7 @@ def yaml2files(data_path: str, out_path: str):
             text_line = text_line.strip()
             uid = row["wav"].strip()
             speaker_id = row["speaker_id"].strip()
-            
+
             wav_path = f"{data_path}/wav/{uid}.wav"
 
             processed_text_file.write(f"{uid} {text_line}\n")
@@ -36,7 +43,7 @@ def yaml2files(data_path: str, out_path: str):
 
 if __name__ == "__main__":
     args = get_args()
-    
+
     data_path = args.data
     out_path = args.out
 
