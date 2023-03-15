6c150afe38fd197611d252b19f11f98b1b771c6d
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2023-03-16 02:09:09 +0900

    modified:   ci/test_python.sh

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -5,23 +5,23 @@
 
 set -euo pipefail
 
-modules="espnet espnet2 test utils setup.py egs*/*/*/local egs2/TEMPLATE/*/pyscripts tools/*.py ci/*.py"
+exclude="^(egs2/TEMPLATE/asr1/utils|egs2/TEMPLATE/asr1/steps|egs2/TEMPLATE/tts1/sid|doc)"
 
 # black
-if ! black --check ${modules}; then
-    printf '[INFO] Please apply black:\n    $ black %s\n' "${modules}"
+if ! black --exclude "${exclude}" .; then
+    printf '[INFO] Please apply black:\n    $ black --exclude "$s"\n' "${exclude}"
     exit 1
 fi
 # isort
-if ! isort -c -v ${modules}; then
-    printf '[INFO] Please apply isort:\n    $ isort %s\n' "${modules}"
+if ! isort -c -v --exclude "${exclude}" .; then
+    printf '[INFO] Please apply isort:\n    $ black --exclude "$s"\n' "${exclude}"
     exit 1
 fi
 
 # flake8
 "$(dirname $0)"/test_flake8.sh
 # pycodestyle
-pycodestyle -r ${modules} --show-source --show-pep8
+pycodestyle -r --exclude "${exclude}" . --show-source --show-pep8
 
 LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:$(pwd)/tools/chainer_ctc/ext/warp-ctc/build" pytest -q
 
