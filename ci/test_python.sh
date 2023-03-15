ad8443a35688b414a220cf19fb989fb7a3cf9b5e
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2023-03-16 02:23:04 +0900

    modified:   ci/test_python.sh

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -7,17 +7,6 @@ set -euo pipefail
 
 exclude="^(egs2/TEMPLATE/asr1/utils|egs2/TEMPLATE/asr1/steps|egs2/TEMPLATE/tts1/sid|doc)"
 
-# black
-if ! black --exclude "${exclude}" .; then
-    printf '[INFO] Please apply black:\n    $ black --exclude "$s"\n' "${exclude}"
-    exit 1
-fi
-# isort
-if ! isort -c -v --exclude "${exclude}" .; then
-    printf '[INFO] Please apply isort:\n    $ black --exclude "$s"\n' "${exclude}"
-    exit 1
-fi
-
 # flake8
 "$(dirname $0)"/test_flake8.sh
 # pycodestyle
