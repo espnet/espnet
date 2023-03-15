4b9df86a40c098c58980372e9409cafa2f81f196
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2023-03-16 03:13:15 +0900

    modified:   ci/test_python.sh

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -5,7 +5,7 @@
 
 set -euo pipefail
 
-exclude="^(egs2/TEMPLATE/asr1/utils|egs2/TEMPLATE/asr1/steps|egs2/TEMPLATE/tts1/sid|doc)"
+exclude="^(egs2/TEMPLATE/asr1/utils|egs2/TEMPLATE/asr1/steps|egs2/TEMPLATE/tts1/sid|doc|tools/venv)"
 
 # flake8
 "$(dirname $0)"/test_flake8.sh
