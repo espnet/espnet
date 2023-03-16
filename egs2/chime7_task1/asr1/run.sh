5624ddcf42d29c869a3cdc8a6a6e8e0b840062f6
popcornell <cornellsamuele@gmail.com>
2023-03-16 01:06:19 +0100

    renamed to da_wer

--- egs2/chime7_task1/asr1/run.sh
+++ egs2/chime7_task1/asr1/run.sh
@@ -241,7 +241,7 @@ if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
 
   done
   LOG_OUT=${asr_exp}/${inference_tag}/scoring/scoring.log
-  python local/chime7dasr_score.py -s ${asr_exp}/${inference_tag}/chime7dasr_hyp/$split \
+  python local/da_wer_scoring.py -s ${asr_exp}/${inference_tag}/chime7dasr_hyp/$split \
      -r $chime7_root -p $split -o ${asr_exp}/${inference_tag}/scoring -d 0 2>&1 | tee $LOG_OUT
 fi
 
