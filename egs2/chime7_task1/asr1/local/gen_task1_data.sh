064be345d792b3b35a577824e62c21d8bac4167d
popcornell <cornellsamuele@gmail.com>
2023-03-16 20:00:28 +0100

    addressed Shinji comment

--- egs2/chime7_task1/asr1/local/gen_task1_data.sh
+++ egs2/chime7_task1/asr1/local/gen_task1_data.sh
@@ -52,12 +52,20 @@ if [ ${stage} -le 2 ] && ! contains $skip_stages 2; then
   python local/gen_task1_data.py -c $chime6_root \
       -d $dipco_root -m $mixer6_root -o $chime7_root --eval_opt $gen_eval
   if [ $gen_eval -le 0 ]; then
+    # this creates UEM files for training portions of mixer6, where the UEM
+    # file has start and end boundaries for the interview portion or the call
+    # portion.
     mkdir $chime7_root/mixer6/uem/train_intv
     awk -F'[,_]' '(NR>1){print $4"_"$1"_"$2"_"$3"_"$4, 1, $7, $8}' \
     $mixer6_root/metadata/iv_components_final.csv | sort -u | cut -d'_' -f2- > $chime7_root/mixer6/uem/train_intv/all.uem
+    # will produce ./chime7_dasr/mixer6/uem/train_intv/all.uem with contents:
+    # 20090925_114220_HRM_110236 1 33.959 981.802
+    # 20091006_131045_HRM_110236 1 59.169 1060.465
+    # 20091110_121729_HRM_110236 1 27.828 1140.203
 
     mkdir $chime7_root/mixer6/uem/train_call
     awk -F'[,_]' '(NR>1){print $4"_"$1"_"$2"_"$3"_"$4, 1, $11, $12}' \
     $mixer6_root/metadata/iv_components_final.csv | sort -u | cut -d'_' -f2- > $chime7_root/mixer6/uem/train_call/all.uem
+    # as before but for train_call portion.
   fi
 fi
