f6d36aeb49792a52e513ab6d40d0346f66c1dcba
popcornell <cornellsamuele@gmail.com>
2023-03-18 19:08:08 +0100

    adding parent in mkdir

--- egs2/chime7_task1/asr1/local/gen_task1_data.sh
+++ egs2/chime7_task1/asr1/local/gen_task1_data.sh
@@ -55,7 +55,7 @@ if [ ${stage} -le 2 ] && ! contains $skip_stages 2; then
     # this creates UEM files for training portions of mixer6, where the UEM
     # file has start and end boundaries for the interview portion or the call
     # portion.
-    mkdir $chime7_root/mixer6/uem/train_intv
+    mkdir -p $chime7_root/mixer6/uem/train_intv
     awk -F'[,_]' '(NR>1){print $4"_"$1"_"$2"_"$3"_"$4, 1, $7, $8}' \
     $mixer6_root/metadata/iv_components_final.csv | sort -u | cut -d'_' -f2- > $chime7_root/mixer6/uem/train_intv/all.uem
     # will produce ./chime7_dasr/mixer6/uem/train_intv/all.uem with contents:
@@ -63,7 +63,7 @@ if [ ${stage} -le 2 ] && ! contains $skip_stages 2; then
     # 20091006_131045_HRM_110236 1 59.169 1060.465
     # 20091110_121729_HRM_110236 1 27.828 1140.203
 
-    mkdir $chime7_root/mixer6/uem/train_call
+    mkdir -p $chime7_root/mixer6/uem/train_call
     awk -F'[,_]' '(NR>1){print $4"_"$1"_"$2"_"$3"_"$4, 1, $11, $12}' \
     $mixer6_root/metadata/iv_components_final.csv | sort -u | cut -d'_' -f2- > $chime7_root/mixer6/uem/train_call/all.uem
     # as before but for train_call portion.
