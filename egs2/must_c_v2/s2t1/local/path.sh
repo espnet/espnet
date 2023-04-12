e1ace5e4e9b0b269fba2edf9055d8c44f08ee22e
Yifan Peng <pengyf21@gmail.com>
2023-04-11 21:00:25 -0500

    add dependency check in local/path.sh

---
+++ egs2/must_c_v2/s2t1/local/path.sh
@@ -0,0 +1,9 @@
+MAIN_ROOT=$PWD/../../..
+
+# check extra module installation
+if ! command -v tokenizer.perl > /dev/null; then
+    echo "Error: it seems that moses is not installed." >&2
+    echo "Error: please install moses as follows." >&2
+    echo "Error: cd ${MAIN_ROOT}/tools && make moses.done" >&2
+    exit 1
+fi
