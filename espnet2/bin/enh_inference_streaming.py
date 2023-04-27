f92d796b62f3d64368bd205be4dd355ee80b658f
Chenda Li <lichenda1996@gmail.com>
2023-04-27 11:47:35 +0800

    Update enh_inference_streaming.py

--- espnet2/bin/enh_inference_streaming.py
+++ espnet2/bin/enh_inference_streaming.py
@@ -333,7 +333,6 @@ def inference(
             (w / abs(w).max(dim=1, keepdim=True)[0] * 0.9).cpu().numpy() for w in waves
         ]
 
-        # waves = [w.cpu().numpy() for w in waves]
 
         for spk, w in enumerate(waves):
             for b in range(batch_size):
