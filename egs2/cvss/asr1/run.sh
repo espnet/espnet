221a99911fa61a2c416f3d070de6d7cb78c8809b
ftshijt <ftshijt@gmail.com>
2023-05-11 00:38:58 -0500

    fix shell ci

--- egs2/cvss/asr1/run.sh
+++ egs2/cvss/asr1/run.sh
@@ -6,8 +6,7 @@ set -u
 set -o pipefail
 
 # language related
-src_lang=es # ar ca cy de et es fa fr id it ja lv mn nl pt ru sl sv ta tr zh
-version=c # c or t (please refer to cvss paper for details)
+src_lang=all # ar ca cy de et es fa fr id it ja lv mn nl pt ru sl sv ta tr zh
 
 train_set=train_${src_lang}
 train_dev=dev_${src_lang}
@@ -19,12 +18,13 @@ inference_config=conf/decode_asr.yaml
 ./asr.sh \
     --lang en \
     --ngpu 1 \
-    --nbpe 500 \
+    --stage 11 \
+    --nbpe 7000 \
     --max_wav_duration 30 \
     --asr_config "${asr_config}" \
     --use_lm false \
     --inference_config "${inference_config}" \
     --train_set "${train_set}" \
-    --valid_set "${valid_set}" \
+    --valid_set "${train_dev}" \
     --test_sets "${test_sets}" \
     --bpe_train_text "data/${train_set}/text" "$@"
