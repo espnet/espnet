5d7197cbbb8b112a0215e2112fb7f468af58b154
Yerbolat Khassanov <yerb.khass@bytedance.com>
2023-05-11 13:12:03 +0800

    added ksc

---
+++ egs2/ksc/asr1/run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+train_set="train"
+valid_set="dev"
+test_sets="test dev"
+
+asr_config=conf/train_asr_transformer.yaml
+inference_config=conf/decode_asr.yaml
+
+./asr.sh \
+    --stage 1 \
+    --stop_stage 13 \
+    --nj 32 \
+    --ngpu 1 \
+    --gpu_inference true \
+    --feats_type raw \
+    --audio_format "flac" \
+    --token_type bpe \
+    --nbpe 2000
+    --use_lm false \
+    --max_wav_duration 30.0 \
+    --speed_perturb_factors "0.9 1.0 1.1" \
+    --asr_config "${asr_config}" \
+    --inference_config "${inference_config}" \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" \
+    --bpe_train_text "data/local/text"
