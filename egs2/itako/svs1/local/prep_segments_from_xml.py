71b31f5e2f69aa6bbac2fb8808450b7d24e9bc5a
Yuning Wu <1057758290@qq.com>
2023-05-29 13:59:11 +0000

    fix recipe itako

--- egs2/itako/svs1/local/prep_segments_from_xml.py
+++ egs2/itako/svs1/local/prep_segments_from_xml.py
@@ -82,22 +82,37 @@ def make_segment(file_id, tempo, notes, threshold, sil=["P", "B"]):
     for i in range(len(notes)):
         note = notes[i]
         # fix errors in dataset
+        # fix time
+        if "itako48" in file_id:
+            # correct note time
+            if i == 43:
+                continue
+            if i in range(39, 43):
+                note.et = notes[i + 1].et
+            # correct rest to note with lyric
+            if i == 142:
+                segments.extend(segment.split(threshold=threshold))
+                segment = SegInfo()
+                segment.add(45.0, 45.372 , "た", 67)
+                segment.add(45.372, 45.46, "ち", 67)
+                segments.extend(segment.split(threshold=threshold))
+                segment = SegInfo()
         # remove rest note
-        if "itako04" in file_id and i in [
-            191,
-            193,
-            194,
-            195,
-            197,
-            199,
-            200,
-            201,
-            202,
-            203,
-            207,
-        ]:
-            notes[i + 1].st = note.st
-            continue
+        if "itako04" in file_id:
+            if i in [191, 193, 194, 195, 197, 199, 200, 201, 202, 203, 207]:
+                continue
+            # correct timee
+            if i == 198:
+                note.et = 81.572
+            if i == 192:
+                note.et = 80.92
+            # add missing lyric
+            if i == 190:
+                note.lyric = "て"
+            if i == 205:
+                note.lyric = "あ"
+            if i == 208:
+                note.lyric = "ひ"
         # replace wrong note lyric with correct one
         if "itako02" in file_id and note.lyric == "—":
             note.lyric = "P"
