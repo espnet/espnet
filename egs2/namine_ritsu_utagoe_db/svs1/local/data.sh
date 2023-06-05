cf1ab4c8e468455b04d7efca17082a7a2f104aa4
jerryuhoo <jerryuhoo@gmail.com>
2023-06-05 09:09:16 -0400

    remove unused code

--- egs2/namine_ritsu_utagoe_db/svs1/local/data.sh
+++ egs2/namine_ritsu_utagoe_db/svs1/local/data.sh
@@ -49,23 +49,6 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
         data/${train_set} data/${train_dev} data/${recog_set} --fs ${fs}
 fi
 
-# if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
-#     log "stage 2: Prepare segments"
-#     for x in ${train_set} ${train_dev} ${recog_set}; do
-#         src_data=data/${x}
-#         local/prep_segments.py --silence pau --silence sil --silence br ${src_data}
-#         mv ${src_data}/segments.tmp ${src_data}/segments
-#         mv ${src_data}/label.tmp ${src_data}/label
-#         local/prep_segments_from_xml.py --silence P --silence B ${src_data}
-#         mv ${src_data}/text.tmp ${src_data}/text
-#         mv ${src_data}/segments_from_xml.tmp ${src_data}/segments_from_xml
-#         mv ${src_data}/score.scp.tmp ${src_data}/score.scp
-#         awk '{printf("%s namine\n", $1);}' < ${src_data}/segments > ${src_data}/utt2spk
-#         utils/utt2spk_to_spk2utt.pl < ${src_data}/utt2spk > ${src_data}/spk2utt
-#         utils/fix_data_dir.sh --utt_extra_files "label score.scp" ${src_data}
-#     done
-# fi
-
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
     log "stage 2: Prepare segments"
     for x in ${train_set} ${train_dev} ${recog_set}; do
