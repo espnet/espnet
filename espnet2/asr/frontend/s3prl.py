e5e569023fb8399fc38033d891a2cf25451af021
jwrh <jwrh@users.noreply.github.com>
2023-06-11 14:37:47 -0400

    revert to original s3prl.py

--- espnet2/asr/frontend/s3prl.py
+++ espnet2/asr/frontend/s3prl.py
@@ -7,7 +7,6 @@ import torch
 from typeguard import check_argument_types
 
 from espnet2.asr.frontend.abs_frontend import AbsFrontend
-from espnet2.asr.frontend.adapters_utils.add_adapters import *
 from espnet2.utils.get_default_kwargs import get_default_kwargs
 from espnet.nets.pytorch_backend.frontends.frontend import Frontend
 
@@ -55,23 +54,6 @@ class S3prlFrontend(AbsFrontend):
             if getattr(upstream.upstream.model, "feature_grad_mult", None):
                 upstream.upstream.model.feature_grad_mult = 1.0
         upstream.eval()
-
-        # check if adapter is added
-        if (
-            upstream.upstream.model.__class__.__name__ == "Wav2Vec2Model"
-            and frontend_conf.get("add_adapters", None)
-        ):
-            upstream.upstream = add_adapters_wav2vec2(
-                upstream.upstream,
-                adapter_down_dim=frontend_conf.get("adapter_down_dim", 192),
-                adapt_layers=frontend_conf.get("adapter_layers", []),
-            )
-        elif (
-            upstream.upstream.model.__class__.__name__ == "Wav2Vec2Model"
-            and frontend_conf.get("adapter_find", None)
-        ):
-            multilayer_feature = True
-            self.adapter_num = frontend_conf.get("adapter_num", None)
 
         if layer != -1:
             layer_selections = [layer]
