f5ed49c9c80c231e83ea27b3efb053ec71510dd1
Jungjee <jeewon.leo.jung@gmail.com>
2023-06-22 22:17:17 -0400

    add collect_stat process'

--- egs2/TEMPLATE/spk1/spk.sh
+++ egs2/TEMPLATE/spk1/spk.sh
@@ -11,6 +11,17 @@ log() {
         echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
 }
 
+min() {
+    local a b
+    a=$1
+    for b in "$@"; do
+        if [ "${b}" -le "${a}" ]; then
+            a="${b}"
+        fi
+    done
+    echo "${a}"
+}
+
 SECONDS=0
 
 # General configuration
@@ -90,9 +101,16 @@ if [ -z "${spk_tag}" ]; then
 fi
 
 # Set directory used for training commands
+spk_stats_dir="${expdir}/spk_stats_${fs}"
 if [ -z "${spk_exp}"  ]; then
     spk_exp="${expdir}/spk_${spk_tag}"
 fi
+
+# TODO: add speed perturb
+#if [ -n "${speed_perturb_factors}"  ]; then
+#    spk_stats_dir="${spk_stats_dir}_sp"
+#    spk_exp="${spk_exp}_sp"
+#fi
 
 
 if [ ${stage} -le 1  ] && [ ${stop_stage} -ge 1  ] && ! [[ " ${skip_stages} " =~ [[:space:]]1[[:space:]]  ]]; then
@@ -132,7 +150,76 @@ if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
 fi
 
 if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
-    echo "Stage 3: Train."
+    echo "Stage 3: Collect stats"
+    _spk_train_dir="${data_feats}/${train_set}"
+    _spk_valid_dir="${data_feats}/${valid_set}"
+
+    if [ -n "${spk_config}"  ]; then
+        # To generate the config file: e.g.
+        #   % python3 -m espnet2.bin.spk_train --print_config --optim adam
+        _opts+="--config ${spk_config} "
+    fi
+
+    _scp=wav.scp
+    if [[ "${audio_format}" == *ark* ]]; then
+        _type=kaldi_ark
+    else
+        # sound supports "wav", "flac", etc.
+        _type=sound
+    fi
+
+    # 1. Split key file
+    _logdir="${spk_stats_dir}/logdir"
+    mkdir -p "${_logdir}"
+
+    _nj=$(min "${nj}" "$(<${_spk_train_dir}/${_scp} wc -l)" "$(<${_spk_valid_dir}/${_scp} wc -l)")
+
+    key_file="${_spk_train_dir}/${_scp}"
+    split_scps=""
+    for n in $(seq "${_nj}"); do
+        split_scps+=" ${_logdir}/train.${n}.scp"
+    done
+    utils/split_scp.pl "${key_file}" ${split_scps}
+
+    key_file="${_spk_valid_dir}/${_scp}"
+    split_scps=""
+    for n in $(seq "${_nj}"); do
+        split_scps+=" ${_logdir}/valid.${n}.scp"
+    done
+    utils/split_scp.pl "${key_file}" ${split_scps}
+
+    # 2. Generate run.sh
+    log "Generate '${spk_stats_dir}/run.sh'. You can resume the process from stage 3 using this script"
+    mkdir -p "${spk_stats_dir}"; echo "${run_args} -- stage3 \"\$@\"; exit \$?" > "${spk_stats_dir}/run.sh"; chmod +x "${spk_stats_dir}/run.sh"
+
+    # 3. Submit jobs
+    log "Speaker collect-stats started... log: '${_logdir}/stats.*.log'"
+
+    # shellcheck disable=SC2046,SC2086
+    ${train_cmd} JOB=1:"${_nj}" "${_logdir}"/stats.JOB.log \
+        ${python} -m espnet2.bin.spk_train \
+            --collect_stats true \
+            --train_data_path_and_name_and_type ${_spk_train_dir}/wav.scp,speech,sound \
+            --train_data_path_and_name_and_type ${_spk_train_dir}/utt2spk,spk_labels,text \
+            --valid_data_path_and_name_and_type ${_spk_valid_dir}/wav.scp,speech,sound \
+            --valid_data_path_and_name_and_type ${_spk_valid_dir}/utt2spk,spk_labels,text \
+            --train_shape_file "${_logdir}/train.JOB.scp" \
+            --valid_shape_file "${_logdir}/valid.JOB.scp" \
+            --output_dir "${_logdir}/stats.JOB" \
+            ${_opts} ${spk_args} || { cat $(grep -l -i error "${_logdir}"/stats.*.log) ; exit 1;  }
+
+    # 4. Aggregate shape files
+    _opts=
+    for i in $(seq "${nj}"); do
+        _opts+="--input_dir ${_logidr}/stats.${i} "
+    done
+    # shellcheck disable=SC2086
+    ${python} -m espnet2.bin.aggregate_stats_dirs ${_opts} --skip_sum_stats --output_dir "${spk_stats_dir}"
+
+fi
+
+if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
+    echo "Stage 4: Train."
 
     _spk_train_dir="${data_feats}/${train_set}"
     _spk_valid_dir="${data_feats}/${valid_set}"
@@ -141,7 +228,7 @@ if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
         # To generate the config file: e.g.
         #   % python3 -m espnet2.bin.spk_train --print_config --optim adam
         _opts+="--config ${spk_config} "
-fi
+    fi
 
     log "Spk training started... log: '${spk_exp}/train.log'"
     if echo "${cuda_cmd}" | grep -e queue.pl -e queue-freegpu.pl &> /dev/null; then
