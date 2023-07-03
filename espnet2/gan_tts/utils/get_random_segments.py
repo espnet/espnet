cc5e745c1906c4c58a806f5f5dce53a291b33779
perry <iamanigeeit@gmail.com>
2023-07-04 00:34:19 +0800

    Fix get_random_segments.py

--- espnet2/gan_tts/utils/get_random_segments.py
+++ espnet2/gan_tts/utils/get_random_segments.py
@@ -9,7 +9,9 @@ import torch
 
 
 def get_random_segments(
-    x: torch.Tensor, x_lengths: torch.Tensor, segment_size: int,
+    x: torch.Tensor,
+    x_lengths: torch.Tensor,
+    segment_size: int,
 ) -> Tuple[torch.Tensor, torch.Tensor]:
     """Get random segments.
 
@@ -26,14 +28,18 @@ def get_random_segments(
     b, c, t = x.size()
     max_start_idx = x_lengths - segment_size
     max_start_idx[max_start_idx < 0] = 0
-    start_idxs = (torch.rand([b]).to(x.device) * max_start_idx).to(dtype=torch.long,)
+    start_idxs = (torch.rand([b]).to(x.device) * max_start_idx).to(
+        dtype=torch.long,
+    )
     segments = get_segments(x, start_idxs, segment_size)
 
     return segments, start_idxs
 
 
 def get_segments(
-    x: torch.Tensor, start_idxs: torch.Tensor, segment_size: int,
+    x: torch.Tensor,
+    start_idxs: torch.Tensor,
+    segment_size: int,
 ) -> torch.Tensor:
     """Get segments.
 
