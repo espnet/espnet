745db7620e0dd459009931314d514cb4cb31426b
Kwanghee Choi <juice500ml@gmail.com>
2023-07-23 05:28:45 -0400

    Finish implementoing simple_dict.sh

--- utils/simple_dict.sh
+++ utils/simple_dict.sh
@@ -1,47 +1,48 @@
 #!/usr/bin/env bash
 
-dict_init() {
-    # Parameter safety
-    if [ "$#" != 1 ]; then
+_check_parameter_count() {
+    if [ $1 != $2 ]; then
         echo "Bad number of arguments."
         exit 1
     fi
+}
 
-    if [[ "$1" == *\/* ]]; then
-        echo "Bad dictionary name."
-        echo "Name: $1"
+_check_parameter_space_or_slash() {
+    if [[ "$1" == *\/* ]] || [[ "$1" == *\ * ]]; then
+        echo "Bad name: $1"
         exit 1
     fi
+}
+
+dict_init() {
+    _check_parameter_count 1 "$#"
+
+    dict_name=$1
+
+    _check_parameter_space_or_slash "${dict_name}"
 
     # Initialize temporary directory in /tmp
     if [ -z ${simple_storage+x} ]; then
         simple_storage=$(mktemp -d)
     fi
-
-    dict_name=$1
     mkdir "${simple_storage}/${dict_name}"
 }
 
 dict_put() {
-    # Parameter safety
-    if [ "$#" != 3 ]; then
-        echo "Bad number of arguments."
-        exit 1
-    fi
+    _check_parameter_count 3 "$#"
 
     dict_name=$1
     key=$2
     value=$3
 
+    _check_parameter_space_or_slash "${key}"
+    _check_parameter_space_or_slash "${value}"
+
     echo ${value} > "${simple_storage}/${dict_name}/${key}"
 }
 
 dict_get() {
-    # Parameter safety
-    if [ "$#" != 2 ]; then
-        echo "Bad number of arguments."
-        exit 1
-    fi
+    _check_parameter_count 2 "$#"
 
     dict_name=$1
     key=$2
@@ -50,14 +51,32 @@ dict_get() {
 }
 
 dict_remove() {
-    # Parameter safety
-    if [ "$#" != 2 ]; then
-        echo "Bad number of arguments."
-        exit 1
-    fi
+    _check_parameter_count 2 "$#"
 
     dict_name=$1
     key=$2
 
     rm "${simple_storage}/${dict_name}/${key}"
+}
+
+dict_keys() {
+    _check_parameter_count 1 "$#"
+
+    dict_name=$1
+
+    keys=$(ls "${simple_storage}/${dict_name}")
+    echo $keys | sort
+}
+
+dict_values() {
+    _check_parameter_count 1 "$#"
+
+    dict_name=$1
+
+    values=$(
+        for key in $(dict_keys ${dict_name}); do
+            dict_get $dict_name $key
+        done | tr "\n" " "
+    )
+    echo ${values/%?/}
 }
