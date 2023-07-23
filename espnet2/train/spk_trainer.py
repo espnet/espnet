5f0be75ce334f141cee9e00e690d7dc026d04219
Jungjee <jeewon.leo.jung@gmail.com>
2023-07-23 15:56:02 -0400

    fix minor bug

--- espnet2/train/spk_trainer.py
+++ espnet2/train/spk_trainer.py
@@ -189,10 +189,10 @@ class SpkTrainer(Trainer):
                 scores_nontrg.append(_s)
             else:
                 raise ValueError(f"{_l}, {type(_l)}")
-        trg_mean = torch.mean(scores_trg)
-        trg_std = torch.std(scores_trg)
-        nontrg_mean = torch.mean(scores_nontrg)
-        nontrg_std = torch.std(scores_nontrg)
+        trg_mean = np.mean(scores_trg)
+        trg_std = np.std(scores_trg)
+        nontrg_mean = np.std(scores_nontrg)
+        nontrg_std = np.std(scores_nontrg)
 
         # exception for collect_stats.
         if len(scores) == 1:
