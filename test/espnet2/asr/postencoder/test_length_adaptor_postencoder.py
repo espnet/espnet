0ab23a865891ffc2d681b0e09ae0b06c6a39c654
Pavel Denisov <pavel.denisov@ims.uni-stuttgart.de>
2023-07-27 10:39:48 +0200

    Fix Length Adaptor test

--- test/espnet2/asr/postencoder/test_length_adaptor_postencoder.py
+++ test/espnet2/asr/postencoder/test_length_adaptor_postencoder.py
@@ -4,15 +4,16 @@ import torch
 from espnet2.asr.postencoder.length_adaptor_postencoder import LengthAdaptorPostEncoder
 
 
-@pytest.mark.parametrize("input_layer, odim", [[None, None], ["linear", 400]])
+@pytest.mark.parametrize("input_layer, odim", [[None, None], ["linear", 100]])
 def test_length_adaptor_forward(input_layer, odim):
-    idim = 400
+    idim = 200
+    x_max_length = 50
     length_adaptor_n_layers = 1
     postencoder = LengthAdaptorPostEncoder(
         idim, length_adaptor_n_layers, input_layer, odim
     )
-    x = torch.randn([4, 50, idim], requires_grad=True)
-    x_lengths = torch.LongTensor([20, 5, 50, 15])
+    x = torch.randn([4, x_max_length, idim], requires_grad=True)
+    x_lengths = torch.LongTensor([20, 5, x_max_length, 15])
     y, y_lengths = postencoder(x, x_lengths)
     y.sum().backward()
 
@@ -23,7 +24,7 @@ def test_length_adaptor_forward(input_layer, odim):
 
     assert postencoder.output_size() == odim_expected
 
-    y_shape_1_expected = 50 // 2**length_adaptor_n_layers
+    y_shape_1_expected = x_max_length // 2**length_adaptor_n_layers
     y_lengths_expected = (
         x_lengths.float().div(2**length_adaptor_n_layers).floor().long()
     )
@@ -34,7 +35,7 @@ def test_length_adaptor_forward(input_layer, odim):
 
 def test_transformers_too_short_utt():
     idim = 400
-    postencoder = LengthAdaptorPostEncoder(idim, 2, "linear", idim)
+    postencoder = LengthAdaptorPostEncoder(idim, 2)
     x = torch.randn([2, 3, idim], requires_grad=True)
     x_lengths = torch.LongTensor([3, 2])
     with pytest.raises(Exception):
