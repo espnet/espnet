83479b9f050d5fe0ef49bb4260050bf46020e5bc
Kwanghee Choi <juice500ml@gmail.com>
2023-08-16 22:50:28 -0400

    Make download_and_untar.sh more cowardly

--- egs2/mls/asr1/local/download_and_untar.sh
+++ egs2/mls/asr1/local/download_and_untar.sh
@@ -6,18 +6,25 @@
 # Apache 2.0
 
 # Adapted from egs/mini_librispeech/s5/local/download_and_untar.sh (commit 1cd6d2ac3a935009fdc4184cb8a72ddad98fe7d9)
+set -x
 
 remove_archive=false
-
 if [ "$1" == --remove-archive ]; then
-  remove_archive=true
-  shift
+    remove_archive=true
+    shift
 fi
 
-if [ $# -ne 2 ]; then
-  echo "Usage: $0 [--remove-archive] <data-base> <url> <filename>"
-  echo "e.g.: $0 /export/data/ https://common-voice-data-download.s3.amazonaws.com/cv_corpus_v1.tar.gz cv_corpus_v1.tar.gz"
-  echo "With --remove-archive it will remove the archive after successfully un-tarring it."
+md5=""
+if [ "$1" == --md5sum ]; then
+    md5=$2
+    shift; shift
+fi
+
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 [--remove-archive] [--md5sum <md5sum>] <data-base> <url> <filename>"
+    echo "e.g.: $0 /export/data/ https://common-voice-data-download.s3.amazonaws.com/cv_corpus_v1.tar.gz cv_corpus_v1.tar.gz"
+    echo "With --remove-archive it will remove the archive after successfully un-tarring it."
+    echo "With --md5sum, it will check the sanity of the file using md5sum."
 fi
 
 data=$1
@@ -26,64 +33,68 @@ filename=$3
 filepath="$data/$filename"
 workspace=$PWD
 
+# Parameter sanity check.
 if [ ! -d "$data" ]; then
-  echo "$0: no such directory $data"
-  exit 1;
+    echo "$0: no such directory $data"
+    exit 1;
 fi
 
 if [ -z "$url" ]; then
-  echo "$0: empty URL."
-  exit 1;
+    echo "$0: empty URL."
+    exit 1;
 fi
 
+# Cases where it skips downloading
 if [ -f $data/$filename.complete ]; then
-  echo "$0: data was already successfully extracted, nothing to do."
-  exit 0;
+    echo "$0: data was already successfully extracted, nothing to do."
+    exit 0;
 fi
 
-
+need_download=true
 if [ -f $filepath ]; then
-  size=$(/bin/ls -l $filepath | awk '{print $5}')
-  size_ok=false
-  if [ "$filesize" -eq "$size" ]; then size_ok=true; fi;
-  if ! $size_ok; then
-    echo "$0: removing existing file $filepath because its size in bytes ($size)"
-    echo "does not equal the size of the archives ($filesize)."
-    rm $filepath
-  else
-    echo "$filepath exists and appears to be complete."
-  fi
+    if [ -z "${md5}" ]; then
+        echo "You need to supply the md5sum via --md5sum option to verify."
+        echo "Cowardly stopping the script."
+        exit 1
+    fi
+
+    echo "Checking md5sum..."
+    md5_filepath=$(md5sum $filepath | cut -d " " -f 1)
+    if [ $md5 -eq $md5_filepath ]; then
+        echo "$filepath exists and its md5sum checks out. Skipping downloading file $filepath."
+        need_download=false
+    else
+        echo "Supplied md5sum does not match. Removing existing file $filepath."
+        rm $filepath
+    fi
 fi
 
-if [ ! -f $filepath ]; then
-  if ! which wget >/dev/null; then
-    echo "$0: wget is not installed."
-    exit 1;
-  fi
-  echo "$0: downloading data from $url.  This may take some time, please be patient."
+if [ "$need_download" = true ] ; then
+    if ! which wget >/dev/null; then
+        echo "$0: wget is not installed."
+        exit 1;
+    fi
+    echo "$0: downloading data from $url.  This may take some time, please be patient."
 
-  cd $data
-  if ! wget --no-check-certificate $url; then
-    echo "$0: error executing wget $url"
-    exit 1;
-  fi
-  cd $workspace
+    cd $data
+    if ! wget --no-check-certificate $url; then
+        echo "$0: error executing wget $url"
+        exit 1;
+    fi
+    cd $workspace
 fi
 
 cd $data
-
 if ! tar -xzf $filename; then
-  echo "$0: error un-tarring archive $filepath"
-  exit 1;
+    echo "$0: error un-tarring archive $filepath"
+    exit 1;
 fi
 
 cd $workspace
-
 touch $data/$filename.complete
-
 echo "$0: Successfully downloaded and un-tarred $filepath"
 
-if $remove_archive; then
-  echo "$0: removing $filepath file since --remove-archive option was supplied."
-  rm $filepath
+if [ "$remove_archive" = true ] ; then
+    echo "$0: removing $filepath file since --remove-archive option was supplied."
+    rm $filepath
 fi
