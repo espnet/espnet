cf62b326ad79dbddc8fd24f74534f29a49a9a90f
Kwanghee Choi <juice500ml@gmail.com>
2023-08-18 14:25:25 -0400

    Make ci happy

--- ci/test_utils.sh
+++ ci/test_utils.sh
@@ -2,12 +2,12 @@
 
 echo "=== run integration tests at test_utils ==="
 
-PATH=$(pwd)/bats-core/bin:$PATH
+PATH=$(pwd)/test_utils/bats-core/bin:$PATH
 if ! [ -x "$(command -v bats)" ]; then
     echo "=== install bats ==="
-    git clone https://github.com/bats-core/bats-core.git test_utils
-    git clone https://github.com/bats-core/bats-support.git test_utils
-    git clone https://github.com/bats-core/bats-assert.git test_utils
+    git clone https://github.com/bats-core/bats-core.git "$(pwd)"/test_utils/bats-core
+    git clone https://github.com/bats-core/bats-support.git "$(pwd)"/test_utils/bats-support
+    git clone https://github.com/bats-core/bats-assert.git "$(pwd)"/test_utils/bats-assert
 fi
 bats test_utils/integration_test_*.bats
 
