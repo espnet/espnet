7711f0ababa7d8ed1bfe3e4f2ae4ba5ff69e337f
anthony <b09902033@csie.ntu.edu.tw>
2023-09-13 21:21:18 +0800

    update run.sh

--- egs2/wenetspeech/asr2/run.sh
+++ egs2/wenetspeech/asr2/run.sh
@@ -5,22 +5,28 @@ set -e
 set -u
 set -o pipefail
 
-
-kmeans_feature="wavlm_large/21"  # use model_type/layer_index
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+export PATH=$PATH:/tmp2/anthony/ffmpeg-6.0-amd64-static
+export CUDNN_PATH=/tmp2/anthony/anthony_espnet/espnet/tools/miniconda/envs/dgslm/lib/python3.8/site-packages/nvidia/cudnn
+export LD_LIBRARY_PATH=$CUDNN_PATH/lib
+export PYTHONPATH=/tmp2/anthony/anthony_espnet/espnet
+kmeans_feature="hubert_custom/21"  # use model_type/layer_index
 nclusters=2000
 
 src_lang=$(echo "${kmeans_feature}_km${nclusters}" | tr "/" "_")
-tgt_lang=en
+tgt_lang=ch
 
-train_set="train_960"
-train_dev="dev"
-test_sets="test_clean test_other dev_clean dev_other"
+set=L    # S for the small set, M for the mediate set, L for the large set
 
-asr_config=conf/tuning/train_discrete_asr_e_branchformer1.yaml
+train_set=train_"$(echo "${set}" | tr "[:upper:]" "[:lower:]")"
+valid_set=dev
+test_sets="dev test_meeting test_net"
+
+asr_config=conf/train_discrete_asr_e_branchformer1.yaml
 inference_config=conf/decode_ctc0.3.yaml
 
 src_nbpe=6000   # I use src_nbpe=6000 for 2000-cluster kmeans.
-tgt_nbpe=5000   # if token_joint is True, then only tgt_nbpe is used
+tgt_nbpe=6000   # if token_joint is True, then only tgt_nbpe is used
 
 # ts: true sequence
 # rm: deduplicated sequence which removes duplicated tokens
@@ -28,10 +34,13 @@ src_case="rm"
 tgt_case="ts"
 
 ./asr2.sh \
-    --kmeans_opts "--batch_bins 4800000" \
+    --stage 1 \
+    --stop_stage 12 \
+    --skip_stages 8,9,10,11 \
+    --kmeans_opts "--batch_bins 4800000 --portion 0.01 --storage_save_mode true" \
     --kmeans_feature "${kmeans_feature}" \
     --nclusters "${nclusters}" \
-    --ngpu 2 \
+    --ngpu 4 \
     --src_lang ${src_lang} \
     --tgt_lang ${tgt_lang} \
     --src_token_type "bpe" \
@@ -40,12 +49,16 @@ tgt_case="ts"
     --tgt_nbpe $tgt_nbpe \
     --src_case ${src_case} \
     --tgt_case ${tgt_case} \
-    --speed_perturb_factors "0.9 1.0 1.1" \
     --asr_config "${asr_config}" \
     --inference_config "${inference_config}" \
+    --local_data_opts "--set ${set}" \
     --train_set "${train_set}" \
-    --valid_set "${train_dev}" \
+    --valid_set "${valid_set}" \
     --test_sets "${test_sets}" \
     --src_bpe_train_text "data/${train_set}/text.${src_case}.${src_lang}" \
     --tgt_bpe_train_text "data/${train_set}/text.${tgt_case}.${tgt_lang}" \
-    --lm_train_text "data/${train_set}/text.${tgt_case}.${tgt_lang} data/local/other_text/text" "$@"
+    --lm_train_text "data/${train_set}/text.${tgt_case}.${tgt_lang}" \
+    --num_splits_asr 8 \
+    --nj 32
+    # --lm_train_text "data/${train_set}/text.${tgt_case}.${tgt_lang} data/local/other_text/text" "$@"
+    # --speed_perturb_factors "0.9 1.0 1.1" \
