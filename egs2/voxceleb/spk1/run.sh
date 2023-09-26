3863d416d6871589b5853b6f701f101f5e090776
Jungjee <jeewon.leo.jung@gmail.com>
2023-09-26 17:48:57 -0400

    change default train test cfg

--- egs2/voxceleb/spk1/run.sh
+++ egs2/voxceleb/spk1/run.sh
@@ -6,13 +6,15 @@ set -o pipefail
 
 spk_config=conf/train_RawNet3.yaml
 
-train_set="voxceleb12_devs"
+train_set="voxceleb2_dev_sp"
 valid_set="voxceleb1_test"
 test_sets="voxceleb1_test"
+feats_type="raw"
 
 ./spk.sh \
     --spk_config ${spk_config} \
     --train_set ${train_set} \
     --valid_set ${valid_set} \
     --test_sets ${test_sets} \
+    --feats_type ${feats_type} \
     "$@"
