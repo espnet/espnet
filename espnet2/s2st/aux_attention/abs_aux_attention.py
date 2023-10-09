5afb219073ece25066458a8d8ef959f11c134fef
ftshijt <ftshijt@gmail.com>
2023-10-09 09:48:54 -0700

    update aux attention reference

--- espnet2/s2st/aux_attention/abs_aux_attention.py
+++ espnet2/s2st/aux_attention/abs_aux_attention.py
@@ -7,7 +7,7 @@ EPS = torch.finfo(torch.get_default_dtype()).eps
 
 class AbsS2STAuxAttention(torch.nn.Module, ABC):
     """Base class for all S2ST auxiliary attention modules.
-    (aim to provide uniform APIs for s2st attention)"""
+    Refer to https://arxiv.org/abs/2107.08661"""
 
     # the name will be the key that appears in the reporter
     @property
