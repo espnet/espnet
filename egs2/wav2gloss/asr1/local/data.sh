61792ed22018a6155bbc2bf1fc6e68f4857e0d92
Kwanghee Choi <juice500ml@gmail.com>
2023-10-18 10:28:56 -0400

    add fixes

--- egs2/wav2gloss/asr1/local/data.sh
+++ egs2/wav2gloss/asr1/local/data.sh
@@ -8,8 +8,6 @@
 # general configuration
 stage=1       # start from 0 if you need to start from data preparation
 stop_stage=100
-langs=  # comma separated list of languages
-tasks=  # comma separated list of tasks
 min_wav_duration=0.5
 SECONDS=0
 
@@ -54,7 +52,7 @@ fi
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     log "stage 1: Preparing Data for wav2gloss"
 
-    python3 local/data_prep.py --source ${WAV2GLOSS} --langs ${langs} --tasks ${tasks} --min_wav_length ${min_wav_duration}
+    python3 local/data_prep.py --source ${WAV2GLOSS} --min_wav_length ${min_wav_duration}
     find data -type f -empty -delete
     find data -type d -empty -delete
     for f in data/w2g_*; do
--- egs2/wav2gloss/asr1/local/data_prep.py
+++ egs2/wav2gloss/asr1/local/data_prep.py
@@ -69,14 +69,6 @@ def get_parser():
         default=Path("data"),
     )
     parser.add_argument(
-        "--langs",
-        type=str,  # comma separated values
-    )
-    parser.add_argument(
-        "--tasks",
-        type=str,  # comma separated values
-    )
-    parser.add_argument(
         "--min_wav_length",
         type=float,
         default=0.5,
@@ -113,7 +105,7 @@ def write_dir(target_dir, metadata, min_wav_length):
         if meta["length"] < min_wav_length * 1000:
             continue
 
-        header = f"<task|{task}> <lang|{lang}>"
+        header = f"<task|{task}><lang|{lang}>"
         content = _cleaner(meta[task])
 
         _id = f"aaaaa_{lang}_{task}_{meta['id']}"
@@ -122,7 +114,7 @@ def write_dir(target_dir, metadata, min_wav_length):
         wavscp.write(f"{_id} downloads/data/{lang}/audio/{split}/{fname}\n")
         text.write(f"{_id} {content}\n")
         utt2spk.write(f"{_id} aaaaa\n")
-        lm.write(f"{count:010} {header} {content}\n")
+        lm.write(f"{count:010} {header}{content}\n")
         text_prev.write(f"{_id} {header}\n")
 
         count += 1
@@ -157,60 +149,53 @@ def merge_dir(target_dir, source_dirs):
         print(f"{target_dir}: merged {source_dirs}")
 
 
-def sanity_check(args):
-    assert set(LANGUAGES) == {p.name for p in (args.source_dir / "data").glob("*/")}
-    assert len(set(args.langs) - set(LANGUAGES)) == 0
-    assert len(set(args.tasks) - set(TASKS)) == 0
-
-
 def write_symbols(path):
     with open(path, "w", encoding="utf-8") as f:
         for lang in LANGUAGES:
             f.write(f"<lang|{lang}>\n")
         for task in TASKS:
             f.write(f"<task|{task}>\n")
+        f.write("<unk>\n")
 
 
 if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
 
-    args.langs = args.langs.split(",")
-    args.tasks = args.tasks.split(",")
-    sanity_check(args)
+    assert set(LANGUAGES) == {p.name for p in (args.source_dir / "data").glob("*/")}
 
     args.target_dir.mkdir(parents=True, exist_ok=True)
     write_symbols(args.target_dir / "non_linguistic_symbols.txt")
 
-    for lang in args.langs:
+    for lang in LANGUAGES:
         for split in ("train", "dev", "test"):
             metafile = args.source_dir / "data" / lang / f"{split}.json"
             if metafile.exists():
                 metadata = json.load(open(metafile, encoding="utf-8"))
-                for task in args.tasks:
+                for task in TASKS:
                     target_dir = build_dir(task, lang, split)
                     write_dir(target_dir, metadata, args.min_wav_length)
 
     for split in ("train", "dev"):
-        for lang in args.langs:
+        for lang in LANGUAGES:
             target_dir = build_dir("all", lang, split)
             merge_dir(
                 target_dir,
-                [args.target_dir / f"w2g_{task}_{lang}_{split}" for task in args.tasks],
+                [args.target_dir / f"w2g_{task}_{lang}_{split}" for task in TASKS],
             )
-        for task in args.tasks:
+        for task in TASKS:
             target_dir = build_dir(task, "full", split)
             merge_dir(
                 target_dir,
-                [args.target_dir / f"w2g_{task}_{lang}_{split}" for lang in args.langs],
+                [args.target_dir / f"w2g_{task}_{lang}_{split}" for lang in LANGUAGES],
             )
         target_dir = build_dir("all", "full", split)
         merge_dir(
             target_dir,
             [
                 args.target_dir / f"w2g_{task}_{lang}_{split}"
-                for lang in args.langs
-                for task in args.tasks
+                for lang in LANGUAGES
+                for task in TASKS
             ],
         )
 
