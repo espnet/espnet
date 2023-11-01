79936042e94a74b62e72657618207992bd55387a
Yi-Hui Chou <sophia14tw315@gmail.com>
2023-11-01 14:11:21 -0400

    fix eval data dir

--- egs2/speechMatrix/s2st1/local/proc_eval_data.py
+++ egs2/speechMatrix/s2st1/local/proc_eval_data.py
@@ -1,7 +1,7 @@
 import os
 import argparse
 
-def prep(filename, datadir, src_wav_fp, tgt_text_fp=None, vp=False):
+def prep(filename, datadir, src_lang, tgt_lang, utt2spk_fp, src_wav_fp, tgt_text_fp=None, vp=False):
     with open(filename) as f:
         lines = f.readlines()
         for i in range(1, len(lines)):
@@ -13,7 +13,8 @@ def prep(filename, datadir, src_wav_fp, tgt_text_fp=None, vp=False):
                 # en/es/2010/20100705-0900-PLENARY-16-es_20100705-22:08:00_1
                 src = f"{datadir}/voxpopuli_valid_test/{_id}.ogg"
             src_wav_fp.write(f"{_id} {src}\n")
- 
+            utt2spk_fp.write(f"{_id} {src_lang}_{tgt_lang}\n")
+    
             if tgt_text_fp:
                 tgt_text_fp.write(f"{_id} {tgt}\n")
 
@@ -29,28 +30,30 @@ if __name__ == "__main__":
     parser.add_argument("--tgt_lang", type=str, default="en")
 
     args = parser.parse_args()
+    src_lang, tgt_lang = args.src_lang, args.tgt_lang
 
     # UNIT (text) and PATH (wav.scp)
-    wavscp = f"{args.dest}/{args.subset}_{args.src_lang}/wav.scp.{args.src_lang}"
+    wavscp = f"{args.dest}/wav.scp.{args.src_lang}"
     os.makedirs(os.path.dirname(wavscp), exist_ok=True)
     wavscp_fp = open(wavscp, 'w')
+    utt2spk_fp = open(f"{args.dest}/utt2spk", "w")
 
     tgt_text_fp = None
     if args.subset != "test":
-        src_text = f"{args.dest}/{args.subset}_{args.src_lang}/text.{args.src_lang}"
+        src_text = f"{args.dest}/text.{args.src_lang}"
         src_text_fp = open(src_text, "w")
-        tgt_text = f"{args.dest}/{args.subset}_{args.src_lang}/text.{args.tgt_lang}"
+        tgt_text = f"{args.dest}/text.{args.tgt_lang}"
         tgt_text_fp = open(tgt_text, "w")
     
     # Fleurs Valid + Test
     split = "valid" if args.subset == "dev" else args.subset
     fleurs = f"{args.datadir}/flores200_{split}/s2u_manifests/es-en/{split}_fleurs.tsv"
-    prep(fleurs, args.datadir, wavscp_fp, tgt_text_fp)
+    prep(fleurs, args.datadir, src_lang, tgt_lang, utt2spk_fp, wavscp_fp, tgt_text_fp)
 
     # voxpopuli valid
     if args.subset == "dev":
         voxpopuli = f"{args.datadir}/s2u_manifests/es-en/valid_vp.tsv"
-        prep(voxpopuli, args.datadir, wavscp_fp, tgt_text_fp, vp=True)
+        prep(voxpopuli, args.datadir, src_lang, tgt_lang, utt2spk_fp, wavscp_fp, tgt_text_fp, vp=True)
 
         src_unit = f"{args.datadir}/s2u_manifests/en-es/source_unit/valid_vp.tsv"
         with open(src_unit) as f:
@@ -63,9 +66,11 @@ if __name__ == "__main__":
     # EuroParl Test
     if args.subset == "test":
         europarl = f"{args.datadir}/europarl-st-1.1/mined_data/s2u_manifests/es-en/test_epst.tsv"
-        prep(europarl, args.datadir, wavscp_fp)
+        prep(europarl, args.datadir, src_lang, tgt_lang, utt2spk_fp, wavscp_fp)
 
+    # close files
     wavscp_fp.close()
+    utt2spk_fp.close()
     if args.subset != "test":
         src_text_fp.close()
         tgt_text_fp.close()
