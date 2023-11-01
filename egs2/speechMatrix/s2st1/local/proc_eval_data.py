2351c197ca8c7942b10a8d9010d0afd96c1ab983
Ziang Zhou <ziangz@andrew.cmu.edu>
2023-11-01 17:33:06 -0400

    update wav path to be abs path, handle flac/ogg -> wav in wav.scp

--- egs2/speechMatrix/s2st1/local/proc_eval_data.py
+++ egs2/speechMatrix/s2st1/local/proc_eval_data.py
@@ -12,7 +12,8 @@ def prep(filename, datadir, src_lang, tgt_lang, utt2spk_fp, src_wav_fp, tgt_text
             if vp: 
                 # en/es/2010/20100705-0900-PLENARY-16-es_20100705-22:08:00_1
                 src = f"{datadir}/voxpopuli_valid_test/{_id}.ogg"
-            src_wav_fp.write(f"{_id} {src}\n")
+            # Use ffmpeg to convert audio to WAV
+            src_wav_fp.write(f"{_id} ffmpeg -i {src} -acodec pcm_s16le -ac 1 -ar 16000 -f wav - |\n")
             utt2spk_fp.write(f"{_id} {src_lang}_{tgt_lang}\n")
     
             if tgt_text_fp:
@@ -32,6 +33,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
     src_lang, tgt_lang = args.src_lang, args.tgt_lang
 
+    real_dd = os.path.realpath(args.datadir)
+
     # UNIT (text) and PATH (wav.scp)
     wavscp = f"{args.dest}/wav.scp.{args.src_lang}"
     os.makedirs(os.path.dirname(wavscp), exist_ok=True)
@@ -47,15 +50,15 @@ if __name__ == "__main__":
     
     # Fleurs Valid + Test
     split = "valid" if args.subset == "dev" else args.subset
-    fleurs = f"{args.datadir}/flores200_{split}/s2u_manifests/es-en/{split}_fleurs.tsv"
-    prep(fleurs, args.datadir, src_lang, tgt_lang, utt2spk_fp, wavscp_fp, tgt_text_fp)
+    fleurs = f"{real_dd}/flores200_{split}/s2u_manifests/es-en/{split}_fleurs.tsv"
+    prep(fleurs, real_dd, src_lang, tgt_lang, utt2spk_fp, wavscp_fp, tgt_text_fp)
 
     # voxpopuli valid
     if args.subset == "dev":
-        voxpopuli = f"{args.datadir}/s2u_manifests/es-en/valid_vp.tsv"
-        prep(voxpopuli, args.datadir, src_lang, tgt_lang, utt2spk_fp, wavscp_fp, tgt_text_fp, vp=True)
+        voxpopuli = f"{real_dd}/s2u_manifests/es-en/valid_vp.tsv"
+        prep(voxpopuli, real_dd, src_lang, tgt_lang, utt2spk_fp, wavscp_fp, tgt_text_fp, vp=True)
 
-        src_unit = f"{args.datadir}/s2u_manifests/en-es/source_unit/valid_vp.tsv"
+        src_unit = f"{real_dd}/s2u_manifests/en-es/source_unit/valid_vp.tsv"
         with open(src_unit) as f:
             lines = f.readlines()
             for i in range(1, len(lines)):
@@ -65,8 +68,8 @@ if __name__ == "__main__":
 
     # EuroParl Test
     if args.subset == "test":
-        europarl = f"{args.datadir}/europarl-st-1.1/mined_data/s2u_manifests/es-en/test_epst.tsv"
-        prep(europarl, args.datadir, src_lang, tgt_lang, utt2spk_fp, wavscp_fp)
+        europarl = f"{real_dd}/europarl-st-1.1/mined_data/s2u_manifests/es-en/test_epst.tsv"
+        prep(europarl, real_dd, src_lang, tgt_lang, utt2spk_fp, wavscp_fp)
 
     # close files
     wavscp_fp.close()
