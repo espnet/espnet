4990a6ee170dfff7348b7cdad73577231074247f
Yi-Hui Chou <sophia14tw315@gmail.com>
2023-11-02 11:33:02 -0400

    add src_unit

--- egs2/speechMatrix/s2st1/local/proc_eval_data.py
+++ egs2/speechMatrix/s2st1/local/proc_eval_data.py
@@ -1,7 +1,17 @@
 import os
 import argparse
 
-def prep(filename, datadir, src_lang, tgt_lang, utt2spk_fp, src_wav_fp, tgt_text_fp=None, vp=False):
+def prep(
+    filename, 
+    datadir, 
+    src_lang, 
+    tgt_lang, 
+    utt2spk_fp, 
+    src_wav_fp, 
+    src_text_fp=None,
+    tgt_text_fp=None,
+    option=""
+):
     with open(filename) as f:
         lines = f.readlines()
         for i in range(1, len(lines)):
@@ -9,15 +19,30 @@ def prep(filename, datadir, src_lang, tgt_lang, utt2spk_fp, src_wav_fp, tgt_text
             # id  src_audio   src_n_frames    tgt_audio   tgt_n_frames
             _id, src, _, tgt, _ = line.split('\t')
             _id = _id.split('/')[-1]
-            if vp: 
-                # en/es/2010/20100705-0900-PLENARY-16-es_20100705-22:08:00_1
-                src = f"{datadir}/voxpopuli_valid_test/{_id}.ogg"
+
+            
             # Use ffmpeg to convert audio to WAV
             src_wav_fp.write(f"{_id} ffmpeg -i {src} -acodec pcm_s16le -ac 1 -ar 16000 -f wav - |\n")
             utt2spk_fp.write(f"{_id} {src_lang}_{tgt_lang}\n")
     
             if tgt_text_fp:
                 tgt_text_fp.write(f"{_id} {tgt}\n")
+
+    if src_text_fp:
+        #NOTE: test set does not have src_unit, so the path is for valid set
+        if option == "voxpopuli": 
+            # en/es/2010/20100705-0900-PLENARY-16-es_20100705-22:08:00_1
+            src = f"{datadir}/voxpopuli_valid_test/{_id}.ogg"
+            src_unit = f"{datadir}/s2u_manifests/{src_lang}-{tgt_lang}/source_unit/valid_vp.tsv"
+        elif option == "fleurs":
+            src_unit = f"{datadir}/flores200_valid/s2u_manifests/{src_lang}-{tgt_lang}/source_unit/valid_fleurs.tsv"
+
+        with open(src_unit) as f:
+            lines = f.readlines()
+            for i in range(1, len(lines)):
+                _id, src_txt = lines[i].split('\t')
+                _id = _id.split('/')[-1]
+                src_text_fp.write(f"{_id} {src_txt}")
 
     return
 
@@ -26,7 +51,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--datadir", type=str, default="/ocean/projects/cis210027p/shared/corpora/speechmatrix")
     parser.add_argument("--dest", type=str, required=True)
-    parser.add_argument("--subset", type=str, required=True)
+    parser.add_argument("--subset", type=str, required=True, choices=["dev", "test"])
     parser.add_argument("--src_lang", type=str, default="es")
     parser.add_argument("--tgt_lang", type=str, default="en")
 
@@ -41,7 +66,7 @@ if __name__ == "__main__":
     wavscp_fp = open(wavscp, 'w')
     utt2spk_fp = open(f"{args.dest}/utt2spk", "w")
 
-    tgt_text_fp = None
+    src_text_fp, tgt_text_fp = None, None
     if args.subset != "test":
         src_text = f"{args.dest}/text.{args.src_lang}"
         src_text_fp = open(src_text, "w")
@@ -51,20 +76,12 @@ if __name__ == "__main__":
     # Fleurs Valid + Test
     split = "valid" if args.subset == "dev" else args.subset
     fleurs = f"{real_dd}/flores200_{split}/s2u_manifests/es-en/{split}_fleurs.tsv"
-    prep(fleurs, real_dd, src_lang, tgt_lang, utt2spk_fp, wavscp_fp, tgt_text_fp)
+    prep(fleurs, real_dd, src_lang, tgt_lang, utt2spk_fp, wavscp_fp, src_text_fp, tgt_text_fp, "fleurs")
 
     # voxpopuli valid
     if args.subset == "dev":
         voxpopuli = f"{real_dd}/s2u_manifests/es-en/valid_vp.tsv"
-        prep(voxpopuli, real_dd, src_lang, tgt_lang, utt2spk_fp, wavscp_fp, tgt_text_fp, vp=True)
-
-        src_unit = f"{real_dd}/s2u_manifests/en-es/source_unit/valid_vp.tsv"
-        with open(src_unit) as f:
-            lines = f.readlines()
-            for i in range(1, len(lines)):
-                _id, src_txt = lines[i].split('\t')
-                _id = _id.split('/')[-1]
-                src_text_fp.write(f"{_id} {src_txt}")
+        prep(voxpopuli, real_dd, src_lang, tgt_lang, utt2spk_fp, wavscp_fp, src_text_fp, tgt_text_fp, "voxpopuli")
 
     # EuroParl Test
     if args.subset == "test":
