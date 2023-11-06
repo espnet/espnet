20f602bce23da4427e4eb10156ab1a765f29fc18
Fujimoto Seiji <fujimoto@ceptord.net>
2023-11-06 11:46:10 +0900

    Fix sentencepiece version for Python 3.11
    SentencePiece adds support for Python 3.11 in v0.1.98.
    
    > https://github.com/google/sentencepiece/releases/tag/v0.1.98
    
    Since ESPnet pins sentencepiece to v0.1.97, users with newer Python
    version can see failure during installation. Fix it thusly.
    
    Signed-off-by: Fujimoto Seiji <fujimoto@ceptord.net>

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ requirements = {
         "hydra-core",
         "opt-einsum",
         # ASR
-        "sentencepiece==0.1.97",
+        "sentencepiece>=0.1.97",
         "ctc-segmentation>=1.6.6",
         # TTS
         "pyworld>=0.3.4",
