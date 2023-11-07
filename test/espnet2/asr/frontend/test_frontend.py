964357b704a37ca25df23a8158f76108ebd3d7cc
Wangyou Zhang <C0me_On@163.com>
2023-11-06 22:47:00 -0500

    Improve error robustness of unit tests

--- test/espnet2/asr/frontend/test_frontend.py
+++ test/espnet2/asr/frontend/test_frontend.py
@@ -3,44 +3,10 @@ import torch
 
 from espnet2.asr.frontend.default import DefaultFrontend
 
-random_speech = torch.tensor(
+random_rir = torch.tensor(
     [
-        [
-            [0.026, 0.031],
-            [0.027, 0.031],
-            [0.026, 0.030],
-            [0.024, 0.028],
-            [0.025, 0.027],
-            [0.027, 0.026],
-            [0.028, 0.026],
-            [0.029, 0.024],
-            [0.028, 0.024],
-            [0.029, 0.026],
-            [0.029, 0.027],
-            [0.029, 0.031],
-            [0.030, 0.038],
-            [0.029, 0.040],
-            [0.028, 0.040],
-            [0.028, 0.041],
-        ],
-        [
-            [0.015, 0.021],
-            [0.005, 0.034],
-            [0.011, 0.029],
-            [0.031, 0.036],
-            [0.031, 0.031],
-            [0.050, 0.038],
-            [0.025, 0.054],
-            [0.042, 0.056],
-            [0.053, 0.048],
-            [0.054, 0.044],
-            [0.053, 0.036],
-            [0.039, 0.025],
-            [0.053, 0.032],
-            [0.054, 0.034],
-            [0.031, 0.025],
-            [0.008, 0.023],
-        ],
+        [0.0291, -0.0318, 0.0325, -0.0298, 0.0216, -0.0057, -0.0263, 0.0989],
+        [-0.0564, 0.1428, -0.2752, 0.4941, -0.5598, 1.2620, 0.6407, -1.7159],
     ]
 )
 
@@ -79,7 +45,10 @@ def test_frontend_backward_multi_channel(train, use_wpe, use_beamformer):
         frontend.train()
     else:
         frontend.eval()
-    x = random_speech.repeat(1, 1024, 1)
+    pad = random_rir.size(1)
+    x = torch.nn.functional.pad(torch.randn(2, 1, 1024), (pad, pad))
+    x = torch.nn.functional.conv1d(x, random_rir.unsqueeze(1)).transpose(1, 2)
+    x = x[:, (pad - 1) // 2: (pad - 1) // 2 + 1024]
     x.requires_grad = True
     x_lengths = torch.LongTensor([1024, 1000])
     y, y_lengths = frontend(x, x_lengths)
