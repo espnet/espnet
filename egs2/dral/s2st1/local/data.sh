e42b404b19251d6e23ed750843bbd4d2d5d8cb34
Jiajun Bao <jiajunb@umich.edu>
2023-11-08 22:51:41 +0000

    added dataset preparation; added sym link of boilplate

---
+++ egs2/dral/s2st1/local/data.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Copyright 2020 Johns Hopkins University (Shinji Watanabe)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+. ./path.sh || exit 1;
+. ./cmd.sh || exit 1;
+. ./db.sh || exit 1;
+
+# general configuration
+stage=0       # start from 0 if you need to start from data preparation
+stop_stage=100
+
+src_lang=EN # ar ca cy de et es fa fr id it ja lv mn nl pt ru sl sv ta tr zh
+tgt_lang=ES
+RAW_DIR=raw
+time_type=long
+output_dir=data
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+log "data preparation started"
+
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    log "stage1: Download data to ${RAW_DIR}"
+    log "Prepare source data from commonvoice 4.0"
+    python3 local/prepare_datasets.py download conf/dataset_urls.yaml $RAW_DIR
+fi
+
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+    log "stage2: Preparing data for commonvoice and cvss"
+    python3 local/prepare_datasets.py  extract_recordings $RAW_DIR $src_lang $tgt_lang $time_type $output_dir
+fi
+
+log "Successfully finished. [elapsed=${SECONDS}s]"
---
+++ egs2/dral/s2st1/local/prepare_datasets.py
@@ -0,0 +1,340 @@
+"""download datasets and convert them into kaldi format"""
+import logging
+import multiprocessing
+import re
+import shutil
+import tarfile
+import tempfile
+from concurrent.futures import Future, ThreadPoolExecutor, wait
+from pathlib import Path
+from typing import Dict, List, Tuple
+
+import backoff
+import pandas as pd
+import requests
+import typer
+import yaml
+from pydantic import BaseModel
+
+_BACKOFF_NUM_RETRIES: int = 3
+app = typer.Typer()
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+_RECORDINGS: str = "recordings"
+_FRAGMENTS_LONG: str = "fragments-long"
+_FRAGMENTS_SHORT: str = "fragments-short"
+_AUDIO_FOLDERS: List[str] = [_RECORDINGS, _FRAGMENTS_LONG, _FRAGMENTS_SHORT]
+LOGGER = logging.getLogger(__name__)
+_WAV_NAME_PATTERN_LONG: str = r"^[A-Z]{2}_\d{3}_#\d+\.wav$"
+_WAV_NAME_PATTERN_SHORT: str = r"^[A-Z]{2}_\d{3}_\d+\.wav$"
+_WAV_NAME_PATTERN_RECORDING: str = r"^[A-Z]{2}_\d{3}.wav$"
+
+
+def remove_suffix(s: str, suffix: str):
+    if s.endswith(suffix):
+        return s[:len(s) - len(suffix)]
+    return s
+
+
+class DatasetHelper:
+    def __init__(self, disable_ssl_verification: bool = True):
+        self._verify: bool = not disable_ssl_verification
+
+    @backoff.on_exception(
+        backoff.expo,
+        Exception,
+        max_tries=_BACKOFF_NUM_RETRIES,
+    )
+    def download(self, url: str, destination: Path) -> Path:
+        response = requests.get(url, stream=True, verify=self._verify)
+        if response.status_code == 200:
+            with open(destination, "wb") as f:
+                for chunk in response.iter_content(chunk_size=1024):
+                    if chunk:
+                        f.write(chunk)
+            return destination
+        else:
+            raise requests.exceptions.RequestException(
+                f"response.json() = {response.json()}; "
+                f"response.status_code = {response.status_code}"
+            )
+
+    @staticmethod
+    def uncompress(archive_path: Path, destination_path: Path) -> Path:
+        destination_path.mkdir(exist_ok=True, parents=True)
+        with tarfile.open(archive_path, "r") as tar_ref:
+            tar_ref.extractall(destination_path)
+        # the suffix name of release is inconsistent (.tgz or .tar.gz)
+        if archive_path.suffix == ".tgz":
+            subfolder = remove_suffix(archive_path.name, ".tgz")
+        elif archive_path.suffix == ".tar":
+            subfolder = remove_suffix(archive_path.name, ".tar")
+        elif archive_path.suffix == ".tar.gz":
+            subfolder = remove_suffix(archive_path.name, ".tar.gz")
+        else:
+            subfolder = archive_path.name
+        return destination_path / subfolder
+
+
+def download_thread_helper(url: str, dest_dir: Path) -> Tuple[str, Path]:
+    dataset_helper = DatasetHelper()
+    with tempfile.TemporaryDirectory() as tmp_dir_str:
+        tmp_dir: Path = Path(tmp_dir_str)
+        tar_gz_path: Path = dataset_helper.download(url, tmp_dir / Path(url).name)
+        return url, dataset_helper.uncompress(tar_gz_path, dest_dir)
+
+
+class DatasetConfig(BaseModel):
+    dataset_urls: List[str]
+
+
+def _write_kaldi_files(
+    raw_data_dir: Path, data_file_stem: str, df: pd.DataFrame, output_dir: Path
+):
+    LOGGER.info(f"writing kaldi-style files: {data_file_stem} -> {output_dir}")
+    assert data_file_stem in _AUDIO_FOLDERS, data_file_stem
+    output_dir.mkdir(exist_ok=True, parents=True)
+    # wav.scp
+    df["audio_path"] = df["id"].apply(
+        lambda x: str(raw_data_dir / data_file_stem / f"{x}.wav")
+    )
+    df[["id", "audio_path"]].to_csv(
+        output_dir / "wav.scp", sep=" ", header=False, index=False
+    )
+    # utt2spk
+    if data_file_stem in {_RECORDINGS, _FRAGMENTS_LONG}:
+        df["spk"] = df.apply(
+            lambda x: "-".join(
+                sorted(
+                    [x["participant_id_left_unique"], x["participant_id_right_unique"]]
+                )
+            ),
+            axis=1,
+        )
+    else:
+        df["spk"] = df["participant_id_unique"]
+    df[["id", "spk"]].to_csv(output_dir / "utt2spk", sep=" ", header=False, index=False)
+    # spk2utt
+    # Group by 'spk' and collect 'id' values into sets
+    grouped_data = df.groupby("spk")["id"].apply(set).reset_index()
+    with (output_dir / "spk2utt").open("w") as spk2utt_out:
+        for _, row in grouped_data.iterrows():
+            spk = row["spk"]
+            utt_str = " ".join(sorted(list(row["id"])))
+            spk2utt_out.write(f"{spk} {utt_str}\n")
+    # segments
+    if data_file_stem == _RECORDINGS:
+        df[["id_fragments", "id_recordings", "time_start", "time_end"]].to_csv(
+            output_dir / "segments", sep=" ", header=False, index=False
+        )
+    LOGGER.info("completed!")
+
+
+def _train_dev_split(df: pd.DataFrame):
+    # split
+    conv_ids = sorted(df["conv_id"].unique())
+    devtest_ratio = 0.1
+    devtest_size = int(len(conv_ids) * devtest_ratio)
+    devtest_ids = set(conv_ids[:devtest_size])
+    dev_ids = set(conv_ids[devtest_size : devtest_size * 2])
+    train_ids = set(conv_ids[devtest_size * 2 :])
+    train_df = df[df["conv_id"].isin(train_ids)]
+    dev_df = df[df["conv_id"].isin(dev_ids)]
+    devtest_df = df[df["conv_id"].isin(devtest_ids)]
+    return train_df, dev_df, devtest_df
+
+
+@app.command(name="extract_fragments")
+def extract_fragments(
+    input_dir: Path, src_lang: str, tgt_lang: str, audio_type: str, output_dir: Path
+) -> Path:
+    assert audio_type in {"fragments-long", "fragments-short"}
+    file_stem: str
+    if audio_type == "fragments-long":
+        file_stem = _FRAGMENTS_LONG
+    else:
+        file_stem = _RECORDINGS
+    conversation_df = pd.read_csv(
+        input_dir / "conversation.csv",
+        usecols=["id", "participant_id_left", "participant_id_right"],
+    )
+    audio_df = pd.read_csv(
+        input_dir / f"{file_stem}.csv",
+        sep=",",
+        header=0,
+        usecols=[
+            "id",
+            "lang_code",
+            "conv_id",
+            "original_or_reenacted",
+            "trans_id",
+        ],
+    )
+    audio_df["trans_lang_code"] = audio_df["trans_id"].apply(lambda x: x.split("_", 1)[0])
+    df = pd.merge(
+        audio_df,
+        conversation_df,
+        left_on="id",
+        right_on="conv_id",
+        how="inner",
+        suffixes=("recordings", "fragments"),
+    )
+    assert len(df) == len(
+        audio_df
+    ), f"some audio rows do not exist in conversation.csv: {len(df)} vs {len(audio_df)}"
+    src_lang = src_lang.upper()
+    tgt_lang = tgt_lang.upper()
+    df = df[(df["lang_code"] == src_lang) & (df["trans_lang_code"] == tgt_lang)]
+    LOGGER.info(f"{len(df)} segments for src_lang = {src_lang} & tgt_lang = {tgt_lang}")
+    train_df, dev_df, devtest_df = _train_dev_split(df)
+    _write_kaldi_files(input_dir, _RECORDINGS, train_df, output_dir / "train")
+    _write_kaldi_files(input_dir, _RECORDINGS, dev_df, output_dir / "dev")
+    _write_kaldi_files(input_dir, _RECORDINGS, devtest_df, output_dir / "test")
+    return output_dir
+
+
+@app.command(name="extract_recordings")
+def extract_recordings(
+    input_dir: Path, src_lang: str, tgt_lang: str, time_type: str, output_dir: Path
+) -> Path:
+    assert time_type in {"long", "short"}
+    fragment_file_stem: str
+    if time_type == "long":
+        fragment_file_stem = _FRAGMENTS_LONG
+    else:
+        fragment_file_stem = _FRAGMENTS_SHORT
+    # we join the conversation table with fragment table to get segment start and end time
+    conversation_df = pd.read_csv(
+        input_dir / f"{fragment_file_stem}.csv",
+        usecols=["id", "participant_id_left", "participant_id_right"],
+    )
+    audio_df = pd.read_csv(
+        input_dir / "fragments-long.csv",
+        sep=",",
+        header=0,
+        usecols=[
+            "id",
+            "lang_code",
+            "conv_id",
+            "original_or_reenacted",
+            "time_start",
+            "time_end",
+            "trans_id",
+        ],
+    )
+    audio_df["trans_lang_code"] = audio_df["trans_id"].apply(lambda x: x.split("_", 1)[0])
+    df = pd.merge(
+        audio_df,
+        conversation_df,
+        left_on="id",
+        right_on="conv_id",
+        how="inner",
+        suffixes=("recordings", "fragments"),
+    )
+    assert len(df) == len(
+        audio_df
+    ), f"some audio rows do not exist in conversation.csv: {len(df)} vs {len(audio_df)}"
+    src_lang = src_lang.upper()
+    tgt_lang = tgt_lang.upper()
+    df = df[(df["lang_code"] == src_lang) & (df["trans_lang_code"] == tgt_lang)]
+    LOGGER.info(f"{len(df)} segments for src_lang = {src_lang} & tgt_lang = {tgt_lang}")
+    train_df, dev_df, devtest_df = _train_dev_split(df)
+    _write_kaldi_files(input_dir, _RECORDINGS, train_df, output_dir / "train")
+    _write_kaldi_files(input_dir, _RECORDINGS, dev_df, output_dir / "dev")
+    _write_kaldi_files(input_dir, _RECORDINGS, devtest_df, output_dir / "test")
+    return output_dir
+
+@app.command(name="download")
+def download(dataset_config: Path, output_dir: Path) -> Path:
+    """
+    download datasets and their fixes according to section 5 of
+    https://www.cs.utep.edu/nigel/papers/dral-techreport2.pdf
+
+    Args:
+        dataset_config (Path): The path to the dataset configuration file.
+        output_dir (Path): The path to store the merged dataset.
+
+    Returns:
+        Path: The path where the merged dataset is stored.
+    """
+    # read dataset config
+    with dataset_config.open("r") as f_in:
+        config: DatasetConfig = DatasetConfig.parse_obj(yaml.safe_load(f_in))
+    with tempfile.TemporaryDirectory() as cache_dir_str:
+        cache_dir: Path = Path(cache_dir_str)
+        # "download each installment, starting with DRAL-2.0, and unpack each into its own folder"
+        num_cores = multiprocessing.cpu_count()
+        LOGGER.info(f"downloading raw datasets: max_wokers = {num_cores}")
+        ordered_dests: List[Tuple[str, Path]] = []
+        with ThreadPoolExecutor(max_workers=num_cores) as executor:
+            futures: List[Future[Path]] = []
+            for url in config.dataset_urls:
+                futures.append(
+                    executor.submit(
+                        download_thread_helper, url, cache_dir
+                    )
+                )
+            results = wait(futures)
+            # Wait for all threads to finish
+            url2dest: Dict[str, Path] = {}
+            for result in results.done:
+                if result.exception() is not None:
+                    raise result.exception()
+                else:
+                    url, destination_path = result.result()
+                    url2dest[url] = destination_path
+        for url in config.dataset_urls:
+            ordered_dests.append(
+                (
+                    url,
+                    destination_path,
+                )
+            )
+        LOGGER.info("completed!")
+        output_dir.mkdir(exist_ok=True, parents=True)
+        LOGGER.info(f"populating metadatafiles from the last relese")
+        # Discard all metadata (csv files) except those from the last release.
+        for path in ordered_dests[-1][1].iterdir():
+            if path.suffix == ".csv":
+                shutil.move(str(path), str(output_dir / path.name))
+        LOGGER.info("completed!")
+        # Create directories for recordings/, fragments-long/, and fragments-short/
+        recordings_dir = output_dir / _RECORDINGS
+        fragments_long_dir = output_dir / _FRAGMENTS_LONG
+        fragments_short_dir = output_dir / _FRAGMENTS_SHORT
+        recordings_dir.mkdir(exist_ok=True, parents=True)
+        fragments_long_dir.mkdir(exist_ok=True, parents=True)
+        fragments_short_dir.mkdir(exist_ok=True, parents=True)
+        # then populate each by copying over all files from the corresponding directories in the releases.
+        # Do this copying starting with 2.0 and working forward, to ensure that any revised audio files overwrite
+        # the old ones.
+        for url, path in ordered_dests:
+            LOGGER.info(f"populating {url} from {path} to {output_dir}")
+            for subfolder in _AUDIO_FOLDERS:
+                src_dir = path / subfolder
+                if src_dir.is_dir():
+                    shutil.copytree(
+                        str(src_dir),
+                        str(output_dir / subfolder),
+                        dirs_exist_ok=True,
+                        copy_function=shutil.copy2,
+                    )
+            # release 6.1 does not follow the structure mentioned in their report
+            # it scatters audios in the root directory
+            for file in path.iterdir():
+                if re.match(file.name, _WAV_NAME_PATTERN_LONG):
+                    shutil.copy(str(file), fragments_long_dir / file.name)
+                if re.match(file.name, _WAV_NAME_PATTERN_SHORT):
+                    shutil.copy(str(file), fragments_short_dir / file.name)
+                if re.match(file.name, _WAV_NAME_PATTERN_RECORDING):
+                    shutil.copy(str(file), recordings_dir / file.name)
+            LOGGER.info("completed!")
+        LOGGER.info(f"audio data stored at {output_dir}")
+    return output_dir
+
+
+if __name__ == "__main__":
+    app()
