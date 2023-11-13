d2a8fd137863c835f2241341ca45d5b5c9eb472a
Jiajun Bao <jiajunb@umich.edu>
2023-11-13 12:14:41 -0500

    simplify data.sh

--- egs2/TEMPLATE/s2st1/s2st.sh
+++ egs2/TEMPLATE/s2st1/s2st.sh
@@ -458,7 +458,7 @@ if ! "${skip_data_prep}"; then
     if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
         log "Stage 1: Data preparation for data/${train_set}, data/${valid_set}, etc."
         # [Task dependent] Need to create data.sh for new corpus
-        local/data.sh ${local_data_opts}
+        local/data.sh $src_lang $tgt_lang data
     fi
 
     # NOTE(jiatong): we may add speed perturbation?
--- egs2/dral/s2st1/local/data.sh
+++ egs2/dral/s2st1/local/data.sh
@@ -7,33 +7,20 @@
 . ./cmd.sh || exit 1;
 . ./db.sh || exit 1;
 
-# general configuration
-stage=0       # start from 0 if you need to start from data preparation
-stop_stage=100
-
-src_lang=es # ar ca cy de et es fa fr id it ja lv mn nl pt ru sl sv ta tr zh
-tgt_lang=en
+src_lang=$1 # ar ca cy de et es fa fr id it ja lv mn nl pt ru sl sv ta tr zh
+tgt_lang=$2
 RAW_DIR=raw
 time_type=long
-output_dir=data
+output_dir=$3
 # Set bash to 'debug' mode, it will exit on :
 # -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
 set -e
 set -u
 set -o pipefail
 
-log "data preparation started"
-
-
-if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
-    log "stage1: Download data to ${RAW_DIR}"
-    log "Prepare source data from commonvoice 4.0"
-    python3 local/prepare_datasets.py download conf/dataset_urls.yaml $RAW_DIR
-fi
-
-if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
-    log "stage2: Preparing data for commonvoice and cvss"
-    python3 local/prepare_datasets.py  extract_recordings $RAW_DIR $src_lang $tgt_lang $time_type $output_dir
-fi
+log "Download data from DRAL into $RAW_DIR"
+python3 local/prepare_datasets.py download conf/dataset_urls.yaml $RAW_DIR
+log "Extract $src_lang-$tgt_lang into $output_dir"
+python3 local/prepare_datasets.py  extract_recordings $RAW_DIR $src_lang $tgt_lang $time_type $output_dir
 
 log "Successfully finished. [elapsed=${SECONDS}s]"
