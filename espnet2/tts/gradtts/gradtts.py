945bbfe0caa674beb8227b27ae1081e14f46b8e9
Arya-Li <921877965@qq.com>
2023-11-15 10:39:05 +0800

    modify forward()

--- espnet2/tts/gradtts/gradtts.py
+++ espnet2/tts/gradtts/gradtts.py
@@ -9,7 +9,17 @@ from typing import Dict, Optional
 import torch.nn.functional as F
 import argparse
 from espnet2.tts.gradtts.text_encoder import TextEncoder
-
+from espnet.nets.pytorch_backend.fastspeech.duration_predictor import DurationPredictor
+from espnet.nets.pytorch_backend.transformer.encoder import (
+    Encoder as TransformerEncoder,
+)
+from espnet.nets.pytorch_backend.transformer.embedding import (
+    PositionalEncoding,
+    ScaledPositionalEncoding,
+)
+from espnet.nets.pytorch_backend.fastspeech.length_regulator import LengthRegulator
+from espnet2.tts.gradtts.denoiser import Diffusion
+from espnet2.torch_utils.device_funcs import force_gatherable
 
 class GradTTS(AbsTTS):
     def __init__(
@@ -32,7 +42,22 @@ class GradTTS(AbsTTS):
             beta_min=0.05,
             beta_max=20.0,
             pe_scale=1000,
-
+            #encoder parameters
+            # encoder_type: str = "transformer",
+            # decoder_type: str = "diffusion",
+            # transformer_enc_dropout_rate: float = 0.1,
+            # transformer_enc_positional_dropout_rate: float = 0.1,
+            # transformer_enc_attn_dropout_rate: float = 0.1,
+            # adim: int = 384,
+            # aheads: int = 2,
+            # elayers: int = 4,
+            # eunits: int = 1024,
+            # encoder_normalize_before: bool = True,
+            # encoder_concat_after: bool = False,
+            # positionwise_layer_type: str = "conv1d-linear",
+            # positionwise_conv_kernel_size: int = 1,
+            # use_scaled_pos_enc: bool = True,
+            #reduction_factor: int = 1,
     ):
         super(GradTTS, self).__init__()
         self.n_vocab = idim
@@ -51,16 +76,173 @@ class GradTTS(AbsTTS):
         self.beta_min = beta_min
         self.beta_max = beta_max
         self.pe_scale = pe_scale
+        # self.encoder_type = encoder_type
+        # self.decoder_type = decoder_type
+        # use idx 0 as padding idx
+        self.padding_idx = 0
+        #self.use_scaled_pos_enc = use_scaled_pos_enc
+        #self.reduction_factor = reduction_factor
+
+        # get positional encoding class
+        # pos_enc_class = (
+        #     ScaledPositionalEncoding if self.use_scaled_pos_enc else PositionalEncoding
+        # )
 
         if n_spks > 1:
             self.spk_emb = torch.nn.Embedding(n_spks, spk_emb_dim)
         self.encoder = TextEncoder(idim, odim, n_enc_channels,
-                                   filter_channels, filter_channels_dp, n_heads,
-                                   n_enc_layers, enc_kernel, enc_dropout, window_size)
+                                    filter_channels, filter_channels_dp, n_heads,
+                                    n_enc_layers, enc_kernel, enc_dropout, window_size)
         self.decoder = Diffusion(odim, dec_dim, n_spks, spk_emb_dim, beta_min, beta_max, pe_scale)
 
-    @torch.no_grad()
+        # # define duration predictor
+        # self.duration_predictor = DurationPredictor(
+        #     idim=idim,
+        #     n_layers=n_enc_layers,
+        #     n_chans=filter_channels,
+        #     kernel_size=enc_kernel,
+        #     dropout_rate=enc_dropout,
+        # )
+        #
+        # # define encoder
+        # encoder_input_layer = torch.nn.Embedding(
+        #     num_embeddings=idim, embedding_dim=adim, padding_idx=self.padding_idx
+        # )
+        # if encoder_type == "transformer":
+        #     self.encoder = TransformerEncoder(
+        #         idim=idim,
+        #         attention_dim=adim,
+        #         attention_heads=aheads,
+        #         linear_units=eunits,
+        #         num_blocks=elayers,
+        #         input_layer=encoder_input_layer,
+        #         dropout_rate=transformer_enc_dropout_rate,
+        #         positional_dropout_rate=transformer_enc_positional_dropout_rate,
+        #         attention_dropout_rate=transformer_enc_attn_dropout_rate,
+        #         pos_enc_class=pos_enc_class,
+        #         normalize_before=encoder_normalize_before,
+        #         concat_after=encoder_concat_after,
+        #         positionwise_layer_type=positionwise_layer_type,
+        #         positionwise_conv_kernel_size=positionwise_conv_kernel_size,
+        #     )
+
+        # # define length regulator
+        # self.length_regulator = LengthRegulator()
+        #
+        # # define decoder
+        # if decoder_type == "diffusion":
+        #     self.decoder = Diffusion(
+        #         odim, dec_dim, n_spks, spk_emb_dim, beta_min, beta_max, pe_scale
+        #     )
+        # else:
+        #     raise NotImplementedError(decoder_type)
+        #
+        # # define final projection
+        # if decoder_type != "diffusion":
+        #     self.feat_out = torch.nn.Linear(adim, odim * reduction_factor)
+        # if reduction_factor > 1:
+        #     raise NotImplementedError()
+
+        # define postnet
+        # self.postnet = (
+        #     None
+        #     if postnet_layers == 0
+        #     else Postnet(
+        #         idim=idim,
+        #         odim=odim,
+        #         n_layers=postnet_layers,
+        #         n_chans=postnet_chans,
+        #         n_filts=postnet_filts,
+        #         use_batch_norm=use_batch_norm,
+        #         dropout_rate=postnet_dropout_rate,
+        #     )
+        # )
+        #
+        # # initialize parameters
+        # self._reset_parameters(
+        #     init_type=init_type,
+        #     init_enc_alpha=init_enc_alpha,
+        #     init_dec_alpha=init_dec_alpha,
+        # )
+
+        # define criterions
+        # self.criterion = ProDiffLoss(
+        #     use_masking=use_masking, use_weighted_masking=use_weighted_masking
+        # )
+
+    def relocate_input(self, x: list):
+        """
+        Relocates provided tensors to the same device set for the module.
+        """
+        device = next(self.parameters()).device
+        for i in range(len(x)):
+            if isinstance(x[i], torch.Tensor) and x[i].device != device:
+                x[i] = x[i].to(device)
+        return x
+
     def forward(
+        self,
+        text: torch.Tensor,
+        text_lengths: torch.Tensor,
+        feats: torch.Tensor,
+        feats_lengths: torch.Tensor,
+        joint_training: bool = False,
+        **kwargs,
+    ):
+        batch_size = text.size(0)
+        # text = text[:, : text_lengths.max()]  # for data-parallel
+        # feats = feats[:, : feats_lengths.max()]  # for data-parallel
+        #x = self.emb(text) * math.sqrt(self.n_channels)
+        #x = torch.transpose(x, 1, -1)
+
+
+        encoder_outputs, decoder_outputs, attn = self._forward(
+            text,
+            text_lengths,
+            feats,
+            feats_lengths,
+            **kwargs,
+        )
+
+        # calculate loss
+        dur_loss, prior_loss, diff_loss = self.compute_loss(
+            x = text,
+            x_lengths = text_lengths,
+            y = feats,
+            y_lengths = feats_lengths,
+            out_size = fix_len_compatibility(2*22050//256),
+            #x, x_lengths, y, y_lengths=self.relocate_input([x, x_lengths, y, y_lengths])
+        )
+        loss = dur_loss + prior_loss + diff_loss
+
+        stats = dict(
+            dur_loss=dur_loss.item(),
+            prior_loss=prior_loss.item(),
+            diff_loss=diff_loss.item(),
+        )
+
+        # report extra information
+        # if self.encoder_type == "transformer" and self.use_scaled_pos_enc:
+        #     stats.update(
+        #         encoder_alpha=self.encoder.embed[-1].alpha.data.item(),
+        #     )
+        # if self.decoder_type == "transformer" and self.use_scaled_pos_enc:
+        #     stats.update(
+        #         decoder_alpha=self.decoder.embed[-1].alpha.data.item(),
+        #     )
+
+        if not joint_training:
+            stats.update(loss=loss.item())
+            loss, stats, weight = force_gatherable(
+                (loss, stats, batch_size), loss.device
+            )
+            return loss, stats, weight
+        # else:
+        #     return loss, stats, after_outs if after_outs is not None else before_outs
+
+
+    @torch.no_grad()
+    def _forward(
         self,
         text: torch.Tensor,
         text_lengths: torch.Tensor,
@@ -88,7 +270,9 @@ class GradTTS(AbsTTS):
             length_scale (float, optional): controls speech pace.
                 Increase value to slow down generated speech and vice versa.
         """
-        x, x_lengths = self.relocate_input([text, text_lengths])
+        x = self.emb(text) * math.sqrt(self.n_channels)
+        x = torch.transpose(x, 1, -1)
+        x, x_lengths = self.relocate_input([x, text_lengths])
 
         if self.n_spks > 1:
             # Get speaker embedding
