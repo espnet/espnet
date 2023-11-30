0437614e355ee2c74e728b550bbee76eae409b12
Ziang Zhou <ziangz@andrew.cmu.edu>
2023-11-29 22:00:46 -0500

    preserve importing inf from torch, update torch to 1.13

--- espnet2/schedulers/warmup_reducelronplateau.py
+++ espnet2/schedulers/warmup_reducelronplateau.py
@@ -2,8 +2,7 @@
 from typing import Union
 
 import torch
-# from torch import inf
-from math import inf
+from torch import inf
 from typeguard import check_argument_types
 
 from espnet2.schedulers.abs_scheduler import (
