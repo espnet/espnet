12292c8f5cc82b76f53fb06b93dd59bb0bb74e65
ftshijt <ftshijt@gmail.com>
2023-12-06 18:21:30 -0500

    add ci test for spk inference

--- espnet2/bin/spk_inference.py
+++ espnet2/bin/spk_inference.py
@@ -15,6 +15,8 @@ from espnet2.fileio.npy_scp import NpyScpWriter
 from espnet2.tasks.spk import SpeakerTask
 from espnet2.torch_utils.device_funcs import to_device
 from espnet2.torch_utils.set_all_random_seed import set_all_random_seed
+from espnet2.utils import config_argparse
+from espnet2.utils.types import str2bool, str2triple_str, str_or_none
 from espnet.utils.cli_utils import get_commandline_args
 
 
