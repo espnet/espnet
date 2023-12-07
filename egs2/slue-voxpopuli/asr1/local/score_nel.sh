91818ae50025218c5642de56e3f22c1ab17c9358
Ankita Pasad <ankitap@ttic.edu>
2023-12-06 21:09:40 -0600

    nel eval

---
+++ egs2/slue-voxpopuli/asr1/local/score_nel.sh
@@ -0,0 +1,15 @@
+dir_name=$1
+split="dev"
+
+python local/reformat_ctc_outputs.py --dir_name $dir_name --split $split
+
+# offset=0.0
+python local/score_nel.py evaluate_submission --dir_name $dir_name --split $split --ms False --offset 0.0
+
+# uncomment the lines below to tune offset parameter
+
+# for offset in $(seq -0.3 0.02 0.3); do
+#     python codes/eval.py evaluate_submission --dir_name $dir_name --split $split --ms False --offset $offset
+# done
+
+# python codes/eval.py choose_best --dir_name $dir_name
