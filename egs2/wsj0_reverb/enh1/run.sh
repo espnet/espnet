6daab39c572899dbb59a4326b284bf730679390d
LiChenda <lichenda1996@sjtu.edu.cn>
2024-01-03 05:04:48 +0800

    update for review

--- egs2/wsj0_reverb/enh1/run.sh
+++ egs2/wsj0_reverb/enh1/run.sh
@@ -14,10 +14,6 @@ test_sets="test "
     --valid_set "${valid_set}" \
     --test_sets "${test_sets}" \
     --lang en \
-    --ngpu 1 \
     --ref_num 1 \
-    --nj 8 \
-    --enh_config conf/tuning/train_enh_blstm_tf.yaml \
+    --enh_config conf/tuning/train_enh_sgmse_ncsnpp.yaml \
     "$@"
-
-    # --local_data_opts "--sample_rate ${sample_rate} --min_or_max ${min_or_max}" \
