cb427ccdcbc636064c401ba06b36507da2a9a40c
ThishRaj <thishraj.96@gmail.com>
2024-01-10 16:15:19 +0530

    pulling to local

--- egs2/dogri/asr1/stage11.sh
+++ egs2/dogri/asr1/stage11.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Command to run
-command="./asr.sh --stage 11 --stop_stage 11 --train_set train --valid_set dev --test_sets test --use_ngram true --asr_config \"conf/train_asr_e_branchformer.yaml\" --use_lm true --ngpu 4 --nbpe 256"
+command="./asr.sh --stage 11 --stop_stage 11 --train_set train --valid_set dev --test_sets test --pretrained_model "/home/rathna/espnet/egs2/e_branchformer_librispeech/exp/asr_train_asr_e_branchformer_raw_en_bpe5000_sp/valid.acc.ave_10best.pth" --use_ngram true --asr_config \"conf/train_asr_e_branchformer.yaml\" --ignore_init_mismatch "true" --use_lm true --ngpu 4 --nbpe 256"
 
 # Output file
 output_file="output.log"
