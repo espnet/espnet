30b214e00a0a704271f1f36929811fff8e9a3c36
Stanwang <ch995308@gmail.com>
2024-02-16 20:12:23 +0800

    restructure code

--- espnet2/layers/create_adapter_fn.py
+++ espnet2/layers/create_adapter_fn.py
@@ -11,6 +11,7 @@ from espnet2.layers.create_adapter_utils import (
 )
 from espnet2.layers.houlsby_adapter_layer import (
     Houlsby_Adapter,
+    HoulsbyTransformerSentenceEncoderLayer,
 )
 
 try:
@@ -27,14 +28,14 @@ try:
     from s3prl.upstream.wav2vec2.wav2vec2_model import TransformerSentenceEncoderLayer
 
     is_s3prl_available = True
-except Exception:
+except ImportError:
     is_s3prl_available = False
 
 try:
     import loralib as lora
 
     is_lora_available = True
-except Exception:
+except ImportError:
     is_lora_available = False
 
 
@@ -165,95 +166,11 @@ def create_new_houlsby_module(target_module: torch.nn.Module, bottleneck: int):
 
     elif isinstance(target_module, TransformerSentenceEncoderLayer):
 
-        class HoulsbyTransformerSentenceEncoderLayer(TransformerSentenceEncoderLayer):
-            """
-            Implements a Transformer Encoder Layer used in BERT/XLM style pre-trained
-            models.
-            """
-
-            def __init__(
-                self,
-                bottleneck: int = 32,
-                **kwargs,
-            ) -> None:
-
-                super(HoulsbyTransformerSentenceEncoderLayer, self).__init__(**kwargs)
-
-                self.adapter = Houlsby_Adapter(
-                    input_size=self.fc2.out_features,
-                    bottleneck=bottleneck,
-                )
-
-            def forward(
-                self,
-                x: torch.Tensor,
-                self_attn_mask: torch.Tensor = None,
-                self_attn_padding_mask: torch.Tensor = None,
-                need_weights: bool = False,
-                att_args=None,
-            ):
-                """
-                LayerNorm is applied either before or after the self-attention/ffn
-                modules similar to the original Transformer imlementation.
-                """
-                residual = x
-
-                if self.layer_norm_first:
-                    x = self.self_attn_layer_norm(x)
-                    x, attn = self.self_attn(
-                        query=x,
-                        key=x,
-                        value=x,
-                        key_padding_mask=self_attn_padding_mask,
-                        attn_mask=self_attn_mask,
-                        need_weights=False,
-                    )
-                    x = self.dropout1(x)
-                    x = residual + x
-
-                    residual = x
-                    x = self.final_layer_norm(x)
-                    x = self.activation_fn(self.fc1(x))
-
-                    x = self.dropout2(x)
-                    x = self.fc2(x)
-
-                    # add adapter input
-                    houlsby_input = x
-
-                    layer_result = x
-
-                    x = self.dropout3(x)
-
-                    x = x + residual + self.adapter(houlsby_input)
-                else:
-                    x, attn = self.self_attn(
-                        query=x,
-                        key=x,
-                        value=x,
-                        key_padding_mask=self_attn_padding_mask,
-                        need_weights=False,
-                    )
-
-                    x = self.dropout1(x)
-                    x = residual + x
-
-                    x = self.self_attn_layer_norm(x)
-
-                    residual = x
-                    x = self.activation_fn(self.fc1(x))
-                    x = self.dropout2(x)
-                    x = self.fc2(x)
-
-                    houlsby_input = x
-
-                    layer_result = x
-
-                    x = self.dropout3(x)
-                    x = x + residual + self.adapter(houlsby_input)
-                    x = self.final_layer_norm(x)
-
-                return x, (attn, layer_result)
+        if HoulsbyTransformerSentenceEncoderLayer is None:
+            raise ImportError(
+                "Error: S3PRL is not properly installed."
+                "Please install S3PRL: cd ${MAIN_ROOT}/tools && make s3prl.done"
+            )
 
         embedding_dim = target_module.embedding_dim
         ffn_embedding_dim = target_module.fc1.out_features
