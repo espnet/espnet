5cbadb589af52a5a90da73d8c219fb0918e7cbab
“siddhu001” <siddhantarora1806@gmail.com>
2024-02-20 18:20:31 -0600

    Fix loading pre-trained model from transformers

--- espnet2/tasks/abs_task.py
+++ espnet2/tasks/abs_task.py
@@ -2089,8 +2089,18 @@ class AbsTask(ABC):
                         }
                         model.load_state_dict(state_dict, strict=not use_lora)
                     else:
-                        raise
+                        if any(["postdecoder" in k for k in state_dict.keys()]):
+                            model.load_state_dict(
+                                state_dict, strict=False,
+                            )
+                        else:
+                            raise
                 else:
-                    raise
+                    if any(["postdecoder" in k for k in state_dict.keys()]):
+                        model.load_state_dict(
+                            state_dict, strict=False,
+                        )
+                    else:
+                        raise
 
         return model, args
