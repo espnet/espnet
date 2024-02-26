aa7ebd46ca98b2685762fa89fa57cb5b531c770a
Jiatong <728307998@qq.com>
2024-02-26 02:37:25 -0500

    reduce length

--- egs2/TEMPLATE/asr1/pyscripts/utils/calculate_bitrate.py
+++ egs2/TEMPLATE/asr1/pyscripts/utils/calculate_bitrate.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
         for key in tokens.keys():
             assert (
                 key in reference_len_dict.keys()
-            ), "a mismatched key ({}) between reference and provided tokens, please check the two input".format(
+            ), "mismatched key ({}) between reference and provided tokens".format(
                 key
             )
             ref_len = reference_len_dict[key]  # in seconds
@@ -53,8 +53,8 @@ if __name__ == "__main__":
             for stream in range(len(token)):
                 assert (
                     str(stream) in vocab.keys()
-                ), "stream {} does not have corresponding vocab, check vocab file".format(
-                    stream
+                ), "stream {} does not have vocab, check vocab file {}".format(
+                    stream, args.vocab,
                 )
                 info = math.log2(len(token[stream]) / ref_len) + vocab[str(stream)]
                 cum_info += info
