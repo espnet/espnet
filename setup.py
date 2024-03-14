c10c3d1cff6a186ce343f0edbe44461b54337e87
Shinji Watanabe <sw005320@gmail.com>
2024-03-14 14:32:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ requirements = {
         "setuptools>=38.5.1",
         "packaging",
         "configargparse>=1.2.1",
+        "typeguard",
         "humanfriendly",
         "scipy>=1.4.1",
         "filelock",
