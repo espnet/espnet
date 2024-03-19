9569584484dcc840c76a6963211f961c181af282
tony-sensei <tonylaijiang@gmail.com>
2024-03-19 17:14:15 -0400

    initial setup for recipe

---
+++ egs2/speechmatrix/s2st1/local/data.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Copyright 2020 Johns Hopkins University (Shinji Watanabe)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+. ./path.sh || exit 1;
+. ./cmd.sh || exit 1;
+. ./db.sh || exit 1;
+
+# general configuration
+stage=0       # start from 0 if you need to start from data preparation
+stop_stage=100
+SECONDS=0
+src_lang=es # 
