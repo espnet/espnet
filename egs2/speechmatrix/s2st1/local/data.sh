5f26ce965a128b8dfec50ba5c8ff52b4cd876b1d
tony-sensei <tonylaijiang@gmail.com>
2024-03-23 13:14:06 -0400

    multilingual data download

--- egs2/speechmatrix/s2st1/local/data.sh
+++ egs2/speechmatrix/s2st1/local/data.sh
@@ -11,8 +11,10 @@
 stage=0       # start from 0 if you need to start from data preparation
 stop_stage=100
 SECONDS=0
-src_lang=lt # cs de en es et fi fr hr hu it lt nl pl pt ro sk sl
-dst_lang=sl # cs de en es et fi fr hr hu it lt nl pl pt ro sk sl
+
+# todo: some alignments tsv are forbidden to download
+langs=(cs de en es et fi fr hr hu it lt nl pl pt ro sk sl)
+# langs=(sl lt)
 
 . utils/parse_options.sh || exit 1;
 
@@ -41,12 +43,6 @@ set -o pipefail
 
 log "data preparation started"
 
-# base url for download speech_matrix data
-# url 1 for source language and url 2 for target language
-# aligned data tsv indicates the aligned utts
-speech_matrix_raw_data_url_1=https://dl.fbaipublicfiles.com/speech_matrix/audios/${src_lang}_aud.zip
-speech_matrix_raw_data_url_2=https://dl.fbaipublicfiles.com/speech_matrix/audios/${dst_lang}_aud.zip
-speech_matrix_aligned_data_tsv=https://dl.fbaipublicfiles.com/speech_matrix/aligned_speech/${src_lang}-${dst_lang}.tsv.gz
 
 # url for download FLORES data (for aligning speech in FLEURS with texts in FLORES)
 flores_raw_data_url=https://dl.fbaipublicfiles.com/flores101/dataset/flores101_dataset.tar.gz
@@ -55,12 +51,32 @@ europarl_raw_data_url=https://www.mllp.upv.es/europarl-st/v1.1.tar.gz
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     log "stage1: Download data to ${SPEECH_MATRIX}"
     log "Prepare source aligned speech data from speech matrix for training"
-    mkdir -p ${SPEECH_MATRIX}/audios/${src_lang}
-    local/download_and_unzip.sh ${SPEECH_MATRIX}/audios/${src_lang} ${speech_matrix_raw_data_url_1} ${src_lang}_aud.zip
-    mkdir -p ${SPEECH_MATRIX}/audios/${dst_lang}
-    local/download_and_unzip.sh ${SPEECH_MATRIX}/audios/${dst_lang} ${speech_matrix_raw_data_url_2} ${dst_lang}_aud.zip
-    mkdir -p ${SPEECH_MATRIX}/aligned_speech/${src_lang}-${dst_lang}
-    local/download_and_unzip.sh ${SPEECH_MATRIX}/aligned_speech/${src_lang}-${dst_lang} ${speech_matrix_aligned_data_tsv} ${src_lang}-${dst_lang}.tsv.gz
+
+    # audio files for each languages
+    for lang in "${langs[@]}"; do
+
+        mkdir -p ${SPEECH_MATRIX}/audios/${lang}
+
+        local/download_and_unzip.sh \
+            ${SPEECH_MATRIX}/audios/${lang} \
+            https://dl.fbaipublicfiles.com/speech_matrix/audios/${lang}_aud.zip \
+            ${lang}_aud.zip
+    done
+
+    # audio alignments for each language pairs
+    for i in {0..17}; do
+        for j in {0..17}; do
+            if [[ $i != $j ]]; then
+
+                mkdir -p ${SPEECH_MATRIX}/aligned_speech/${langs[i]}-${langs[j]}
+
+                local/download_and_unzip.sh \
+                    ${SPEECH_MATRIX}/aligned_speech/${langs[i]}-${langs[j]} \
+                    https://dl.fbaipublicfiles.com/speech_matrix/aligned_speech/${langs[i]}-${langs[j]}.tsv.gz \
+                    ${langs[i]}-${langs[j]}.tsv.gz
+            fi
+        done
+    done
 
     log "Download FLORES data to ${SPEECH_MATRIX}"
     local/download_and_unzip.sh ${FLORES_ROOT} ${flores_raw_data_url} flores101_dataset.tar.gz
@@ -102,5 +118,8 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     log "Fleurs data paraparation done."
     )
 fi
+
+
+
 
 log "Successfully finished. [elapsed=${SECONDS}s]"
