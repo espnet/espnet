2aa6a49caaca0c5a9309588765279ad70ef589ae
William Chen <chen26@dt-login03.delta.ncsa.illinois.edu>
2024-03-25 15:18:11 -0500

    tts mha error

--- espnet2/tts/gst/style_encoder.py
+++ espnet2/tts/gst/style_encoder.py
@@ -270,3 +270,4 @@ class MultiHeadedAttention(BaseMultiHeadedAttention):
         self.linear_out = torch.nn.Linear(n_feat, n_feat)
         self.attn = None
         self.dropout = torch.nn.Dropout(p=dropout_rate)
+        self.use_flash_attn = False
