4a04a265712dd1ad449fd489da809eb8cf89076b
Fhrozen <nyalta21@gmail.com>
2024-03-29 23:12:19 +0900

    ci integration minor udpates

--- ci/test_integration_espnetez.sh
+++ ci/test_integration_espnetez.sh
@@ -51,27 +51,29 @@ python -m coverage run --append ../../../test/espnetez/test_integration_espnetez
 # Remove generated files in order to reduce the disk usage
 rm -rf exp data/spm
 
-# [ESPnet Easy] test asr transducer recipe with coverage
-python -m coverage run --append ../../../test/espnetez/test_integration_espnetez.py \
-    --task asr \
-    --data_path data \
-    --train_dump_path dump/raw/train_nodev \
-    --valid_dump_path dump/raw/train_dev \
-    --exp_path ./exp \
-    --config_path conf/train_asr_transducer_debug.yaml \
-    --train_sentencepiece_model \
-    --run_collect_stats \
-    --run_train
+if python3 -c "from warprnnt_pytorch import RNNTLoss" &> /dev/null; then
+    # [ESPnet Easy] test asr transducer recipe with coverage
+    python -m coverage run --append ../../../test/espnetez/test_integration_espnetez.py \
+        --task asr \
+        --data_path data \
+        --train_dump_path dump/raw/train_nodev \
+        --valid_dump_path dump/raw/train_dev \
+        --exp_path ./exp \
+        --config_path conf/train_asr_transducer_debug.yaml \
+        --train_sentencepiece_model \
+        --run_collect_stats \
+        --run_train
 
-# finetuning
-python -m coverage run --append ../../../test/espnetez/test_integration_espnetez_ft.py \
-    --task asr \
-    --data_path data \
-    --train_dump_path dump/raw/train_nodev \
-    --valid_dump_path dump/raw/train_dev \
-    --exp_path ./exp \
-    --config_path conf/train_asr_transducer_debug.yaml \
-    --run_finetune
+    # finetuning
+    python -m coverage run --append ../../../test/espnetez/test_integration_espnetez_ft.py \
+        --task asr \
+        --data_path data \
+        --train_dump_path dump/raw/train_nodev \
+        --valid_dump_path dump/raw/train_dev \
+        --exp_path ./exp \
+        --config_path conf/train_asr_transducer_debug.yaml \
+        --run_finetune
+fi
 
 # Remove generated files in order to reduce the disk usage
 rm -rf exp data/spm
