3c628f56ff7337e921e8b356ce51053708f4e343
Masao-Someki <masao.someki@gmail.com>
2024-03-30 15:06:02 +0900

    Modified test script

--- ci/test_integration_espnetez.sh
+++ ci/test_integration_espnetez.sh
@@ -28,107 +28,116 @@ cd ./egs2/mini_an4/asr1 || exit
 rm -rf exp data/spm
 # [ESPnet Easy] test asr recipe with coverage
 python -m coverage run --append ../../../test/espnetez/test_integration_espnetez.py \
---task asr \
---data_path data \
---train_dump_path dump/raw/train_nodev \
---valid_dump_path dump/raw/train_dev \
---exp_path ./exp \
---config_path conf/train_asr_transformer_debug.yaml \
---train_sentencepiece_model \
---run_collect_stats \
---run_train
+    --task asr \
+    --data_path data \
+    --train_dump_path dump/raw/train_nodev \
+    --valid_dump_path dump/raw/train_dev \
+    --exp_path ./exp \
+    --config_path conf/train_asr_transformer_debug.yaml \
+    --train_sentencepiece_model \
+    --run_collect_stats \
+    --run_train
 
 # finetuning
 python -m coverage run --append ../../../test/espnetez/test_integration_espnetez_ft.py \
---task asr \
---data_path data \
---train_dump_path dump/raw/train_nodev \
---valid_dump_path dump/raw/train_dev \
---exp_path ./exp \
---config_path conf/train_asr_transformer_debug.yaml \
---run_finetune
+    --task asr \
+    --data_path data \
+    --train_dump_path dump/raw/train_nodev \
+    --valid_dump_path dump/raw/train_dev \
+    --exp_path ./exp \
+    --config_path conf/train_asr_transformer_debug.yaml \
+    --run_finetune
 
 # Remove generated files in order to reduce the disk usage
 rm -rf exp data/spm
 
 # [ESPnet Easy] test asr transducer recipe with coverage
 python -m coverage run --append ../../../test/espnetez/test_integration_espnetez.py \
---task asr \
---data_path data \
---train_dump_path dump/raw/train_nodev \
---valid_dump_path dump/raw/train_dev \
---exp_path ./exp \
---config_path conf/train_asr_transducer_debug.yaml \
---train_sentencepiece_model \
---run_collect_stats \
---run_train
+    --task asr \
+    --data_path data \
+    --train_dump_path dump/raw/train_nodev \
+    --valid_dump_path dump/raw/train_dev \
+    --exp_path ./exp \
+    --config_path conf/train_asr_transducer_debug.yaml \
+    --train_sentencepiece_model \
+    --run_collect_stats \
+    --run_train
 
 # finetuning
 python -m coverage run --append ../../../test/espnetez/test_integration_espnetez_ft.py \
---task asr \
---data_path data \
---train_dump_path dump/raw/train_nodev \
---valid_dump_path dump/raw/train_dev \
---exp_path ./exp \
---config_path conf/train_asr_transducer_debug.yaml \
---run_finetune
+    --task asr \
+    --data_path data \
+    --train_dump_path dump/raw/train_nodev \
+    --valid_dump_path dump/raw/train_dev \
+    --exp_path ./exp \
+    --config_path conf/train_asr_transducer_debug.yaml \
+    --run_finetune
 
 # Remove generated files in order to reduce the disk usage
 rm -rf exp data/spm
 
 # [ESPnet Easy] test lm recipe with coverage
+cd ${cwd}/egs2/mini_an4/lm1
+ln -sf ../asr1/data data
+ln -sf ../asr1/dump dump
 python -m coverage run --append ../../../test/espnetez/test_integration_espnetez.py \
---task lm \
---data_path data \
---train_dump_path dump/raw/train_nodev \
---valid_dump_path dump/raw/train_dev \
---exp_path ./exp \
---config_path ../lm1/conf/train_transformer.yaml \
---train_sentencepiece_model \
---run_collect_stats \
---run_train
+    --task lm \
+    --data_path data \
+    --train_dump_path dump/raw/train_nodev \
+    --valid_dump_path dump/raw/train_dev \
+    --exp_path ./exp \
+    --config_path conf/train_transformer.yaml \
+    --train_sentencepiece_model \
+    --run_collect_stats \
+    --run_train
 
 # finetune
 python -m coverage run --append ../../../test/espnetez/test_integration_espnetez_ft.py \
---task lm \
---data_path data \
---train_dump_path dump/raw/train_nodev \
---valid_dump_path dump/raw/train_dev \
---exp_path ./exp \
---config_path ../lm1/conf/train_transformer.yaml \
---run_finetune
+    --task lm \
+    --data_path data \
+    --train_dump_path dump/raw/train_nodev \
+    --valid_dump_path dump/raw/train_dev \
+    --exp_path ./exp \
+    --config_path conf/train_transformer.yaml \
+    --run_finetune
 
 # Remove generated files in order to reduce the disk usage
 rm -rf exp data/spm
 
 
 # [ESPnet Easy] test slu recipe with coverage
+cd ${cwd}/egs2/mini_an4/s2t1
+ln -sf ../asr1/data data
+ln -sf ../asr1/dump dump
 python -m coverage run --append ../../../test/espnetez/test_integration_espnetez.py \
---task slu \
---data_path data \
---train_dump_path dump/raw/train_nodev \
---valid_dump_path dump/raw/train_dev \
---exp_path ./exp \
---config_path ../s2t1/conf/train_slu_transformer.yaml \
---train_sentencepiece_model \
---run_collect_stats \
---run_train
+    --task slu \
+    --data_path data \
+    --train_dump_path dump/raw/train_nodev \
+    --valid_dump_path dump/raw/train_dev \
+    --exp_path ./exp \
+    --config_path conf/train_slu_transformer.yaml \
+    --train_sentencepiece_model \
+    --run_collect_stats \
+    --run_train
 
 # finetune
 python -m coverage run --append ../../../test/espnetez/test_integration_espnetez_ft.py \
---task slu \
---data_path data \
---train_dump_path dump/raw/train_nodev \
---valid_dump_path dump/raw/train_dev \
---exp_path ./exp \
---config_path ../s2t1/conf/train_slu_transformer.yaml \
---run_finetune
+    --task slu \
+    --data_path data \
+    --train_dump_path dump/raw/train_nodev \
+    --valid_dump_path dump/raw/train_dev \
+    --exp_path ./exp \
+    --config_path conf/train_slu_transformer.yaml \
+    --run_finetune
 
 # Remove generated files in order to reduce the disk usage
 rm -rf exp data/spm
 
 
 # [ESPnet Easy] test tts recipe with coverage
+cd ${cwd}/egs2/mini_an4/tts1
+ln -sf ../asr1/data data
+ln -sf ../asr1/dump dump
 python -m coverage run --append ../../../test/espnetez/test_integration_espnetez.py \
     --task tts \
     --data_path data \
@@ -161,7 +170,7 @@ python -m coverage run --append ../../../test/espnetez/test_integration_espnetez
     --train_dump_path dump/raw/train_nodev \
     --valid_dump_path dump/raw/train_dev \
     --exp_path ./exp \
-    --config_path ../tts1/conf/train_vits_debug.yaml \
+    --config_path conf/train_vits_debug.yaml \
     --train_sentencepiece_model \
     --run_collect_stats \
     --run_train
@@ -173,7 +182,7 @@ python -m coverage run --append ../../../test/espnetez/test_integration_espnetez
     --train_dump_path dump/raw/train_nodev \
     --valid_dump_path dump/raw/train_dev \
     --exp_path ./exp \
-    --config_path ../tts1/conf/train_vits_debug.yaml \
+    --config_path conf/train_vits_debug.yaml \
     --run_finetune
 
 # Remove generated files in order to reduce the disk usage
