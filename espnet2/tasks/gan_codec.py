81ea77a6ea94082aa9a50fe476c16e712ddcd007
ftshijt <ftshijt@gmail.com>
2024-03-31 01:45:59 -0400

    setup codec task

---
+++ espnet2/tasks/gan_codec.py
@@ -0,0 +1,184 @@
+# Copyright 2024 Jiatong Shi
+# Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+"""GAN-based neural codec task."""
+
+import argparse
+import logging
+from typing import Callable, Collection, Dict, List, Optional, Tuple
+
+import numpy as np
+import torch
+from typeguard import check_argument_types, check_return_type
+
+from espnet2.gan_codec.abs_gan_codec import AbsGANCodec
+from espnet2.gan_codec.espnet_model import ESPnetGANCodecModel
+from espnet2.gan_codec.soundstream import SoundStream
+
+
+codec_choices = ClassChoices(
+    "codec",
+    classes=dict(
+        soundstream=SoundStream,
+    ),
+    default="soundstream",
+)
+
+
+class GANCodecTask(AbsTask):
+    """GAN-based neural codec task."""
+
+    # GAN requires two optimizers
+    num_optimizers: int = 2
+
+    # Add variable objects configurations
+    class_choices_list = [
+        # --codec and --codec_conf
+        codec_choices,
+    ]
+
+    # Use GANTrainer instead of Trainer
+    trainer = GANTrainer
+
+    @classmethod
+    def add_task_arguments(cls, parser: argparse.ArgumentParser):
+        # NOTE(kamo): Use '_' instead of '-' to avoid confusion
+        assert check_argument_types()
+        group = parser.add_argument_group(description="Task related")
+
+        # NOTE(kamo): add_arguments(..., required=True) can't be used
+        # to provide --print_config mode. Instead of it, do as
+        required = parser.get_default("required")
+
+        group.add_argument(
+            "--model_conf",
+            action=NestedDictAction,
+            default=get_default_kwargs(ESPnetGANCodecModel),
+            help="The keyword arguments for model class.",
+        )
+
+        group = parser.add_argument_group(description="Preprocess related")
+        group.add_argument(
+            "--use_preprocessor",
+            type=str2bool,
+            default=True,
+            help="Apply preprocessing to data or not",
+        )
+
+        for class_choices in cls.class_choices_list:
+            # Append --<name> and --<name>_conf.
+            # e.g. --encoder and --encoder_conf
+            class_choices.add_arguments(group)
+
+    @classmethod
+    def build_collate_fn(cls, args: argparse.Namespace, train: bool) -> Callable[
+        [Collection[Tuple[str, Dict[str, np.ndarray]]]],
+        Tuple[List[str], Dict[str, torch.Tensor]],
+    ]:
+        assert check_argument_types()
+        return CommonCollateFn(
+            float_pad_value=0.0,
+            int_pad_value=0,
+        )
+
+    @classmethod
+    def build_preprocess_fn(
+        cls, args: argparse.Namespace, train: bool
+    ) -> Optional[Callable[[str, Dict[str, np.array]], Dict[str, np.ndarray]]]:
+        assert check_argument_types()
+        if args.use_preprocessor:
+            retval = CommonPreprocessor(
+                train=train,
+                token_type=None, # disable the text process
+            )
+        else:
+            retval = None
+        assert check_return_type(retval)
+        return retval
+
+    @classmethod
+    def required_data_names(
+        cls, train: bool = True, inference: bool = False
+    ) -> Tuple[str, ...]:
+        if not inference:
+            retval = ("audio",)
+        else:
+            # Inference mode
+            retval = ("audio",)
+        return retval
+
+    @classmethod
+    def optional_data_names(
+        cls, train: bool = True, inference: bool = False
+    ) -> Tuple[str, ...]:
+        return ()
+
+    @classmethod
+    def build_model(cls, args: argparse.Namespace) -> ESPnetGANCodecModel:
+        assert check_argument_types()
+
+        # 1. Codec
+        codec_class = codec_choices.get_class(args.codec)
+        codec = codec_class(**args.codec_conf)
+
+        # 2. Build model
+        model = ESPnetGANCodecModel(
+            codec=codec,
+            **args.model_conf,
+        )
+        assert check_return_type(model)
+        return model
+
+    @classmethod
+    def build_optimizers(
+        cls,
+        args: argparse.Namespace,
+        model: ESPnetGANCodecModel,
+    ) -> List[torch.optim.Optimizer]:
+        # check
+        assert hasattr(model.codec, "generator")
+        assert hasattr(model.codec, "discriminator")
+
+        # define generator optimizer
+        optim_g_class = optim_classes.get(args.optim)
+        if optim_g_class is None:
+            raise ValueError(f"must be one of {list(optim_classes)}: {args.optim}")
+        if args.sharded_ddp:
+            try:
+                import fairscale
+            except ImportError:
+                raise RuntimeError("Requiring fairscale. Do 'pip install fairscale'")
+            optim_g = fairscale.optim.oss.OSS(
+                params=model.codec.generator.parameters(),
+                optim=optim_g_class,
+                **args.optim_conf,
+            )
+        else:
+            optim_g = optim_g_class(
+                model.codec.generator.parameters(),
+                **args.optim_conf,
+            )
+        optimizers = [optim_g]
+
+        # define discriminator optimizer
+        optim_d_class = optim_classes.get(args.optim2)
+        if optim_d_class is None:
+            raise ValueError(f"must be one of {list(optim_classes)}: {args.optim2}")
+        if args.sharded_ddp:
+            try:
+                import fairscale
+            except ImportError:
+                raise RuntimeError("Requiring fairscale. Do 'pip install fairscale'")
+            optim_d = fairscale.optim.oss.OSS(
+                params=model.codec.discriminator.parameters(),
+                optim=optim_d_class,
+                **args.optim2_conf,
+            )
+        else:
+            optim_d = optim_d_class(
+                model.codec.discriminator.parameters(),
+                **args.optim2_conf,
+            )
+        optimizers += [optim_d]
+
+        return optimizers
