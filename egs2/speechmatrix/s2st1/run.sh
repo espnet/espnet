92d7e188cb5b36b4b5873ba67c1021224933b5eb
Kwanghee Choi <juice500ml@gmail.com>
2024-04-03 12:20:28 -0400

    fix run.sh bugs

--- egs2/speechmatrix/s2st1/run.sh
+++ egs2/speechmatrix/s2st1/run.sh
@@ -13,25 +13,26 @@ stage=1
 stop_stage=5
 
 # kmeans related
+ssl_model=mhubert_base_vp_en_es_fr_it3
 clustering_portion=1
-clustering_num_clusters=500
-feature_layer=6
+clustering_num_clusters=1000
+feature_layer=11
 
 train_set=train_${src_lang}_${tgt_lang}
-train_dev=dev_${src_lang}_${tgt_lang}
-test_sets="test_${src_lang}_${tgt_lang} dev_${src_lang}_${tgt_lang}"
+valid_set=dev_${src_lang}_${tgt_lang}
+test_sets="test_fleurs_${src_lang}_${tgt_lang}"
 
-st_config=
-use_src_lang=true
-use_tgt_lang=true
-inference_config=
-vocoder_file=
-score_asr_model_tag=
+st_config=conf/train_s2st_discrete_unit.yaml
+use_src_lang=false  # TODO: 
+use_tgt_lang=false  # TODO:
+inference_config=conf/decode_s2st.yaml
+vocoder_file=none  # TODO: Have to retrain the vocoder with discrete units
+score_asr_model_tag="Shinji Watanabe/librispeech_asr_train_asr_transformer_e18_raw_bpe_sp_valid.acc.best"
 
 ./s2st.sh \
     --stage ${stage} \
     --stop_stage ${stop_stage} \
-    --ngpu 2 \
+    --ngpu 1 \
     --nj 64 \
     --inference_nj 64 \
     --use_discrete_unit true \
@@ -43,7 +44,7 @@ score_asr_model_tag=
     --src_lang ${src_lang} \
     --tgt_lang ${tgt_lang} \
     --feature_layer ${feature_layer} \
-    --s3prl_upstream_name mhubert_base_vp_en_es_fr_it3 \
+    --s3prl_upstream_name ${ssl_model} \
     --storage_save_mode false \
     --clustering_num_threads 60 \
     --clustering_portion ${clustering_portion} \
@@ -54,5 +55,5 @@ score_asr_model_tag=
     --vocoder_file "${vocoder_file}" \
     --score_asr_model_tag "${score_asr_model_tag}" \
     --train_set "${train_set}" \
-    --valid_set "${train_dev}" \
+    --valid_set "${valid_set}" \
     --test_sets "${test_sets}" "$@"
