f2bdb6b0ca25ad7b1569654b6c0eba6c584cb2ab
Satvik Dixit <sdixit1@br011.ib.bridges2.psc.edu>
2024-04-03 12:20:37 -0400

    makerere asr

---
+++ egs2/makerere/asr1/local/data_prep.py
@@ -0,0 +1,51 @@
+import os
+import glob
+import sys
+
+import pandas as pd
+import random
+
+df = pd.read_csv('downloads/makerere_radio_dataset/transcribed/cleaned.csv')
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python data_prep.py [root] [sph2pipe]")
+        sys.exit(1)
+    root = sys.argv[1]
+    # print('root', root)
+    sph2pipe = sys.argv[2]
+
+    all_audio_list = glob.glob(
+        os.path.join(root, 'makerere_radio_dataset/transcribed/dataset', "*.wav")
+    )
+    random.shuffle(all_audio_list)
+
+    for x in ["train", "test"]:
+        if x=='train':
+            audio_list = all_audio_list[:int(len(all_audio_list)*0.8)]
+        else:
+            audio_list = all_audio_list[int(len(all_audio_list)*0.8):]
+        # print('audio_list', len(audio_list))
+        with open(os.path.join("data", x, "text"), "w") as text_f, open(
+            os.path.join("data", x, "wav.scp"), "w"
+        ) as wav_scp_f, open(
+            os.path.join("data", x, "utt2spk"), "w"
+        ) as utt2spk_f:
+            i=0
+            for audio_path in audio_list:
+                # print(i)
+                # print()
+                # print('audio_path', audio_path)
+                filename = os.path.basename(audio_path)
+                # print('filename', filename)    # "o73a.wav" etc
+                speaker = filename.split('.')[0][8]     # "lc", "sk", etc
+                # print('speaker', speaker) 
+                if len(list(df.loc[df['wav_filename'] == filename]['transcript'])) == 0:
+                    continue
+                transcript = list(df.loc[df['wav_filename'] == filename]['transcript'])[0] #" ".join(list(filename[:-5]))  # "o73" -> "o 7 3"
+                # print('transcript', transcript)
+                uttid = filename[:-4]    # "sk-o73a"
+                # print('uttid', uttid)
+                wav_scp_f.write(f"{uttid} {audio_path}\n") 
+                text_f.write(f"{uttid} {transcript}\n")
+                utt2spk_f.write(f"{uttid} {speaker}\n")
+                i=i+1
