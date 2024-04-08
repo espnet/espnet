1531fad67016b89b8f594a760b8bafa92b279df4
ftshijt <728307998@qq.com>
2024-04-08 01:57:30 -0400

    update libritts config

---
+++ egs2/libritts/codec1/run.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+fs=24000
+n_fft=2048
+n_shift=300
+win_length=1200
+
+opts=
+if [ "${fs}" -eq 24000 ]; then
+    # To suppress recreation, specify wav format
+    opts="--audio_format wav "
+else
+    opts="--audio_format flac "
+fi
+
+
+train_set=train-clean-460
+valid_set=dev-clean
+test_sets="dev-clean test-clean"
+
+train_config=conf/train_soundstream.yaml
+inference_config=conf/decode.yaml
+
+./codec.sh \
+    --train_config "${train_config}" \
+    --inference_config "${inference_config}" \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" ${opts} "$@"
