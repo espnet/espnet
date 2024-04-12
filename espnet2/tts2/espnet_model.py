26d9e05761127c5ccbd237e39dc53589a587c215
Jiatong <728307998@qq.com>
2024-04-12 03:07:45 -0400

    Update espnet2/tts2/espnet_model.py
    Co-authored-by: Tomoki Hayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>

--- espnet2/tts2/espnet_model.py
+++ espnet2/tts2/espnet_model.py
@@ -190,6 +190,8 @@ class ESPnetTTS2Model(AbsESPnetModel):
             text_lengths (Tensor): Text length tensor (B,).
             speech (Tensor): Speech waveform tensor (B, T_wav).
             speech_lengths (Tensor): Speech length tensor (B,).
+            discrete_speech (Tensor): Discrete speech tensor (B, T_token).
+            discrete_speech_lengths (Tensor): Discrete speech length tensor (B,).
             durations (Optional[Tensor): Duration tensor.
             durations_lengths (Optional[Tensor): Duration length tensor (B,).
             pitch (Optional[Tensor): Pitch tensor.
