1fe62d765bdf4ce69bd57fd1e7cb77342a0b4966
cromz22 <sshimizu@nlp.ist.i.kyoto-u.ac.jp>
2024-04-18 02:00:33 +0900

    training and inference

--- egs2/iwslt24_indic/st1/run.sh
+++ egs2/iwslt24_indic/st1/run.sh
@@ -12,6 +12,9 @@ train_set=train.en-${tgt_lang}
 train_dev=dev.en-${tgt_lang}
 test_set=tst-COMMON.en-${tgt_lang}
 
+st_config=conf/tuning/train_st_conformer.yaml
+inference_config=conf/tuning/decode_st_conformer.yaml
+
 ./st.sh \
     --src_lang ${src_lang} \
     --tgt_lang ${tgt_lang} \
@@ -19,7 +22,14 @@ test_set=tst-COMMON.en-${tgt_lang}
     --train_set "${train_set}" \
     --valid_set "${train_dev}" \
     --test_sets "${test_set}" \
-    --speed_perturb_factors "0.9 1.0 1.1" \
-    --stage 1 \
-    --stop_stage 3
-
+    --feats_type raw \
+    --audio_format "flac.ark" \
+    --src_token_type "bpe" \
+    --src_nbpe 4000 \
+    --tgt_token_type "bpe" \
+    --tgt_nbpe 4000 \
+    --feats_normalize "utterance_mvn" \
+    --st_config "${st_config}" \
+    --inference_config "${inference_config}" \
+    --gpu_inference true \
+    "$@"
