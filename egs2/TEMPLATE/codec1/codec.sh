f294155c2dc14e589f6eef864e610c23ea1460bd
ftshijt <728307998@qq.com>
2024-04-23 03:46:27 -0400

    fix codec default model

--- egs2/TEMPLATE/codec1/codec.sh
+++ egs2/TEMPLATE/codec1/codec.sh
@@ -66,7 +66,7 @@ inference_config="" # Config for decoding.
 inference_args=""   # Arguments for decoding (e.g., "--threshold 0.75").
                     # Note that it will overwrite args in inference config.
 inference_tag=""    # Suffix for decoding directory.
-inference_model=train.loss.ave.pth # Model path for decoding.
+inference_model=train.total_count.best.pth # Model path for decoding.
                                    # e.g.
                                    # inference_model=train.loss.best.pth
                                    # inference_model=3epoch.pth
