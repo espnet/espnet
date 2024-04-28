3a46eddd716ef07a3b9e7efb8e9630abe86f389a
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2024-04-28 23:08:21 +0900

    Update CI

--- tools/installers/install_torch.sh
+++ tools/installers/install_torch.sh
@@ -151,9 +151,63 @@ if ! python -c "import packaging.version" &> /dev/null; then
     python3 -m pip install packaging
 fi
 
-if $(pytorch_plus 2.1.1); then
+if $(pytorch_plus 2.4.0); then
     log "[ERROR] This script doesn't support pytorch=${torch_version}"
     exit 1
+
+elif $(pytorch_plus 2.3.0); then
+    check_python_version 3.13  # Error if python>=<number>
+    if ! check_cuda_version 12.1 11.8; then
+        log "[INFO] Fallback: cuda_version=${cuda_version} -> cuda_version=12.1"
+        cuda_version=12.1
+        cuda_version_without_dot="${cuda_version/\./}"
+    fi
+    install_torch 2.3.0  # install_torch <torch-audio-ver>
+
+elif $(pytorch_plus 2.2.2); then
+    check_python_version 3.13  # Error if python>=<number>
+    if ! check_cuda_version 12.1 11.8; then
+        log "[INFO] Fallback: cuda_version=${cuda_version} -> cuda_version=12.1"
+        cuda_version=12.1
+        cuda_version_without_dot="${cuda_version/\./}"
+    fi
+    install_torch 2.2.2  # install_torch <torch-audio-ver>
+
+elif $(pytorch_plus 2.2.1); then
+    check_python_version 3.13  # Error if python>=<number>
+    if ! check_cuda_version 12.1 11.8; then
+        log "[INFO] Fallback: cuda_version=${cuda_version} -> cuda_version=12.1"
+        cuda_version=12.1
+        cuda_version_without_dot="${cuda_version/\./}"
+    fi
+    install_torch 2.2.1  # install_torch <torch-audio-ver>
+
+elif $(pytorch_plus 2.2.0); then
+    check_python_version 3.13  # Error if python>=<number>
+    if ! check_cuda_version 12.1 11.8; then
+        log "[INFO] Fallback: cuda_version=${cuda_version} -> cuda_version=12.1"
+        cuda_version=12.1
+        cuda_version_without_dot="${cuda_version/\./}"
+    fi
+    install_torch 2.2.0  # install_torch <torch-audio-ver>
+
+elif $(pytorch_plus 2.1.2); then
+    check_python_version 3.12  # Error if python>=<number>
+    if ! check_cuda_version 12.1 11.8; then
+        log "[INFO] Fallback: cuda_version=${cuda_version} -> cuda_version=12.1"
+        cuda_version=12.1
+        cuda_version_without_dot="${cuda_version/\./}"
+    fi
+    install_torch 2.1.2  # install_torch <torch-audio-ver>
+
+elif $(pytorch_plus 2.1.1); then
+    check_python_version 3.12  # Error if python>=<number>
+    if ! check_cuda_version 12.1 11.8; then
+        log "[INFO] Fallback: cuda_version=${cuda_version} -> cuda_version=12.1"
+        cuda_version=12.1
+        cuda_version_without_dot="${cuda_version/\./}"
+    fi
+    install_torch 2.1.1  # install_torch <torch-audio-ver>
 
 elif $(pytorch_plus 2.1.0); then
     check_python_version 3.12  # Error if python>=<number>
