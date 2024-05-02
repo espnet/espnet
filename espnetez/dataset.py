6c4b52a8a45cff37456b73bdfee631beafb0b687
popcornell <cornellsamuele@gmail.com>
2024-05-02 17:13:20 -0400

    lhotse support for espnet-ez by class inheritance

--- espnetez/dataset.py
+++ espnetez/dataset.py
@@ -23,3 +23,36 @@ class ESPnetEZDataset(AbsDataset):
 
     def __len__(self) -> int:
         return len(self.dataset)
+
+
+class ESPnetEZLhotseDataset(ESPnetEZDataset):
+    """
+    A simple wrapper class for supporting lhotse manifests in ESPNet-EZ.
+
+    Args:
+        supervisions: lhotse.SupervisionSet, path to the lhotse supervision manifest.
+        recordings: lhotse.RecordingSet, path to the corresponding lhotse recordings manifest.
+    """
+    def __init__(self, supervisions, recordings):
+
+        # lazy importing for lhotse
+        try:
+           import lhotse
+        except ImportError:
+            raise ImportError("Cannot import Lhotse. Have you installed it correctly ? "
+                              "See https://github.com/lhotse-speech/lhotse.")
+
+        supervisions = lhotse.load_manifest(supervisions)
+        assert isinstance(supervisions, lhotse.SupervisionSet)
+        recordings = lhotse.load_manifest(recordings)
+        assert isinstance(recordings, lhotse.RecordingSet)
+
+        # these have to be mapped to dataset and data_info now.
+        #TODO
+
+        # call the parent init
+        ESPnetEZDataset.__init__(dataset, data_info)
+
+
+
+
