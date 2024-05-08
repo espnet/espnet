9069635a17494672d0e0ef85d68141d932120297
“siddhu001” <siddhantarora1806@gmail.com>
2024-05-08 08:00:23 -0500

    Fix featurizer name

--- espnet2/asr/espnet_model.py
+++ espnet2/asr/espnet_model.py
@@ -16,7 +16,7 @@ from espnet2.asr.specaug.abs_specaug import AbsSpecAug
 from espnet2.asr.transducer.error_calculator import ErrorCalculatorTransducer
 from espnet2.asr_transducer.utils import get_transducer_task_io
 from espnet2.layers.abs_normalize import AbsNormalize
-from espnet2.slu.postencoder.Featurizer import Featurizer
+from espnet2.slu.postencoder.featurizer import Featurizer
 from espnet2.torch_utils.device_funcs import force_gatherable
 from espnet2.train.abs_espnet_model import AbsESPnetModel
 from espnet.nets.e2e_asr_common import ErrorCalculator
