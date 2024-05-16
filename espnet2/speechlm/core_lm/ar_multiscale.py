5d2f00a980076b9733576b07b4c729bf9be5264b
Jiatong <728307998@qq.com>
2024-05-16 10:24:29 -0700

    Update espnet2/speechlm/core_lm/ar_multiscale.py

--- espnet2/speechlm/core_lm/ar_multiscale.py
+++ espnet2/speechlm/core_lm/ar_multiscale.py
@@ -138,7 +138,7 @@ class MultiScaleLM(AbsCoreLM):
     def inference(
         self,
         prefix: torch.Tensor,
-        opts: dict = SpeechLMInferenceOptions,
+        opts: Dict = SpeechLMInferenceOptions,
         enc_seq: torch.Tensor = None,
         suffix: torch.Tensor = None,
     ):
