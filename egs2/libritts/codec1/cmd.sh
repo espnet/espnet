5fcc22ccde7d56c7a6be65ce6d8faaf1fab577f4
ftshijt <ftshijt@gmail.com>
2024-06-06 17:57:26 -0400

    update cmd

--- egs2/libritts/codec1/cmd.sh
+++ egs2/libritts/codec1/cmd.sh
@@ -80,8 +80,8 @@ elif [ "${cmd_backend}" = slurm ]; then
     # You can use "--gpu * " by default for slurm and it is interpreted as "--gres gpu:*"
     # The devices are allocated exclusively using "${CUDA_VISIBLE_DEVICES}".
 
-    export train_cmd="slurm.pl --mem 2000M --num_threads 4"
-    export cuda_cmd="slurm.pl --mem 4000M --num_threads 8"
+    export train_cmd="slurm.pl"
+    export cuda_cmd="slurm.pl"
     export decode_cmd="slurm.pl"
 
 elif [ "${cmd_backend}" = ssh ]; then
