82f09757a37a2b712c20c13bb870e724c7a2b100
Yip Jia Qi <yipjiaqi@hotmail.com>
2024-06-10 15:38:14 +0800

    written and tested codec_separator and clean up code

---
+++ espnet2/enh/separator/codec_separator.py
@@ -0,0 +1,141 @@
+from collections import OrderedDict
+from typing import Dict, List, Optional, Tuple, Union
+
+import torch
+import torch.nn as nn
+from torch.nn.utils import weight_norm
+
+from packaging.version import parse as V
+
+from espnet2.enh.separator.abs_separator import AbsSeparator
+from espnet2.enh.layers.sb_transformer_block import SBTransformerBlock
+from espnet2.enh.layers.snake1d import Snake1d
+
+is_torch_1_9_plus = V(torch.__version__) >= V("1.9.0")
+
+class CodecSeparator(AbsSeparator):
+    def __init__(
+        self,
+        input_dim: int,
+        internal_dim: int,
+        num_spk: int,
+        predict_noise: bool,
+        trf_num_layers: int = 16,
+        trf_num_heads: int = 8,
+        trf_feedforward: int = 2048,
+        trf_dropout: float = 0.1,
+        trf_use_positional_encoding: bool = True,
+        trf_norm_before: bool = True,
+        
+    ):
+        """Dual-Path RNN (DPRNN) Separator
+
+        Args:
+            input_dim: input feature dimension
+            num_spk: number of speakers
+            
+        """
+        super().__init__()
+
+        self._num_spk = num_spk
+        self.predict_noise = predict_noise
+        self.num_outputs = self.num_spk + 1 if self.predict_noise else self.num_spk
+
+        self.block = SBTransformerBlock(
+            num_layers = 16,
+            d_model = input_dim,
+            nhead = 8,
+            d_ffn = 1024,
+            dropout = 0.1,  
+            use_positional_encoding = True,
+            norm_before = True,
+        )
+
+        self.input_dim = input_dim #this is dependent on the dac model
+        self.internal_dim = internal_dim #this is up to us
+        
+        self.ch_down = nn.Conv1d(input_dim, internal_dim,1,bias=False) #change the dimensions for separator
+        self.ch_up = nn.Conv1d(internal_dim, input_dim,1,bias=False) #return to the original dimension
+        
+        self.masker = weight_norm(nn.Conv1d(input_dim, input_dim*self.num_outputs, 1, bias=False)) #computes masks of the original input
+
+        self.activation = Snake1d(input_dim) #nn.Tanh() #nn.ReLU() #Snake1d(channels)
+        
+        # gated output layer
+        self.output = nn.Sequential(
+            nn.Conv1d(input_dim, input_dim, 1), Snake1d(input_dim) #nn.Tanh() #, Snake1d(channels)#
+        )
+        self.output_gate = nn.Sequential(
+            nn.Conv1d(input_dim, input_dim, 1), nn.Sigmoid()
+        )
+    def forward(
+        self,
+        input: torch.Tensor,
+        ilens: torch.Tensor,
+        additional: Optional[Dict] = None,
+    ) -> Tuple[Tuple[torch.Tensor], torch.Tensor, OrderedDict]:
+        """Forward.
+
+        Args:
+            input (torch.Tensor): Encoded feature [B, T, N]
+            ilens (torch.Tensor): input lengths [Batch]
+            additional (Dict or None): other data included in model
+                NOTE: not used in this model
+
+        Returns:
+            masked (List[Union(torch.Tensor)]): [(B, T, N), ...]
+            ilens (torch.Tensor): (B,)
+            others predicted data, e.g. masks: OrderedDict[
+                'mask_spk1': torch.Tensor(Batch, Frames, Freq),
+                'mask_spk2': torch.Tensor(Batch, Frames, Freq),
+                ...
+                'mask_spkn': torch.Tensor(Batch, Frames, Freq),
+            ]
+        """
+        B, T, N = input.shape
+        
+        x = input
+        #[B,T,N]
+        x = x.permute(0,2,1)
+        #[B,N,T]
+        x = self.ch_down(x)
+        #[B,N,T]
+        x = x.permute(0,2,1)
+        #[B,T,N]
+        x_b = self.block(x)
+        #[B,T,N]
+        x_b = x_b.permute(0,2,1)
+        #[B,N,T]        
+        x = self.ch_up(x_b)
+        #[B,N,T]
+
+        assert x.permute(0,2,1).shape == input.shape
+        #[B, N, T]
+        
+        masks = self.masker(x) 
+        #[B,N*num_spk,T]
+
+        masks = masks.view(B, N, self.num_outputs,T).permute(0,1,3,2)
+
+        masks = [
+            self.activation(self.output(masks[...,i]) * self.output_gate(masks[...,i]))
+            for i in range(self.num_outputs)
+        ]
+        
+        if self.predict_noise:
+            *masks, mask_noise = masks
+
+        masked = [input * m.permute(0,2,1) for m in masks]
+
+        others = OrderedDict(
+            zip(["mask_spk{}".format(i + 1) for i in range(len(masks))], masks)
+        )
+
+        if self.predict_noise:
+            others["noise1"] = input * mask_noise.permute(0,2,1)
+
+        return masked, ilens, others
+
+    @property
+    def num_spk(self):
+        return self._num_spk
