599948a180e2a14b6940eb73a535f7f4c32a89f9
William Chen <chen26@gpuc03.delta.ncsa.illinois.edu>
2024-08-09 20:59:01 -0500

    catch flash attention install fail

--- tools/installers/install_flash_attn.sh
+++ tools/installers/install_flash_attn.sh
@@ -6,4 +6,12 @@ if [ $# != 0 ]; then
     exit 1;
 fi
 
-python3 -m pip install flash-attn --no-build-isolation
+{ 
+    python3 -m pip install flash-attn --no-build-isolation
+} || {
+    echo "Flash Attention failed to install, trying without building CUDA"
+    FLASH_ATTENTION_SKIP_CUDA_BUILD=TRUE python3 -m pip install flash-attn --no-build-isolation
+} || {
+    echo "Failed to install flash attention. Skipping..."
+    echo "Manual install may be required: https://github.com/Dao-AILab/flash-attention"
+}
