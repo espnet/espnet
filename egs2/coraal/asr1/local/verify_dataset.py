a528afa45c78d59be5c34c33a3befc95e2c41606
Kalvin Chang <kalvin1204@gmail.com>
2024-08-16 18:42:50 -0400

    feat: add script to verify all wav files present from the metadata

---
+++ egs2/coraal/asr1/local/verify_dataset.py
@@ -0,0 +1,23 @@
+import glob
+import os.path
+import sys
+import pandas as pd
+
+
+# go through the metadata and check that all the files are there
+if len(sys.argv) != 2:
+    print("Usage: python verify_dataset.py <data_path>")
+    sys.exit(1)
+data_path = sys.argv[1]
+
+expected_count, missing_count = 0, 0
+for metadata_file in glob.glob(f"{data_path}/*_metadata*.txt"):
+    df = pd.read_csv(metadata_file, sep='\t')
+    for file in df["CORAAL.File"]:
+        expected_count += 1
+        if not os.path.isfile(f"{data_path}/{file}.wav"):
+            print(file + ".wav is missing")
+            missing_count += 1
+
+print("Expected:", expected_count)
+print("Missing:", missing_count)
