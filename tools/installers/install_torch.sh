26f1731c272b50d5263bb5d9c181a5c185022b7e
Naoyuki Kamo <naoyuki.kamo829@gmail.com>
2024-08-22 18:37:10 +0900

    Update install_torch.sh

--- tools/installers/install_torch.sh
+++ tools/installers/install_torch.sh
@@ -151,9 +151,31 @@ if ! python -c "import packaging.version" &> /dev/null; then
     python3 -m pip install packaging
 fi
 
-if $(pytorch_plus 2.4.0); then
+if $(pytorch_plus 2.4.1); then
     log "[ERROR] This script doesn't support pytorch=${torch_version}"
     exit 1
+    
+elif $(pytorch_plus 2.4.0); then
+    check_python_version 3.13  # Error if python>=<number>
+    if ! check_cuda_version 12.4 12.1 11.8; then
+        log "[INFO] Fallback: cuda_version=${cuda_version} -> cuda_version=12.4"
+        cuda_version=12.4
+        cuda_version_without_dot="${cuda_version/\./}"
+    fi
+    install_torch 2.4.0  # install_torch <torch-audio-ver>
+
+elif $(pytorch_plus 2.3.2); then
+    log "[ERROR] pytorch=${torch_version} doesn't exist"
+    exit 1
+
+elif $(pytorch_plus 2.3.1); then
+    check_python_version 3.13  # Error if python>=<number>
+    if ! check_cuda_version 12.1 11.8; then
+        log "[INFO] Fallback: cuda_version=${cuda_version} -> cuda_version=12.1"
+        cuda_version=12.1
+        cuda_version_without_dot="${cuda_version/\./}"
+    fi
+    install_torch 2.3.1  # install_torch <torch-audio-ver>
 
 elif $(pytorch_plus 2.3.0); then
     check_python_version 3.13  # Error if python>=<number>
