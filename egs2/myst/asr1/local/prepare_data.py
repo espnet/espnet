61387fa8c8b37b60e7d21d74af69ec843a9170a9
eric102004 <h511163@nehs.hc.edu.tw>
2024-10-02 16:33:36 -0400

    fix: add lines in local/prepare_data.py to convert text file from utf8 to ascii

--- egs2/myst/asr1/local/prepare_data.py
+++ egs2/myst/asr1/local/prepare_data.py
@@ -83,6 +83,10 @@ for original_partition_name, data_partition_name in zip(original_partition_list,
     cmd = f"utils/utt2spk_to_spk2utt.pl {utt2spk_file} > {spk2utt_file}"
     _ = os.system(cmd)
 
+    # remove utf-8 whitespaces
+    cmd = f"iconv -f utf-8 -t ascii//TRANSLIT {text_file} > {text_file}.ascii; mv {text_file}.ascii {text_file}"
+    _ = os.system(cmd)
+
     # validata data
     cmd = f"utils/validate_data_dir.sh --no-feats {data_partition}"
     _ = os.system(cmd)
