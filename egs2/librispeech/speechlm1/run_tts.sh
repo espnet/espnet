02a82791ced25db045df76f67d88480832d42078
jctian98 <jctian98@126.com>
2024-11-06 13:39:31 -0600

    a tts baseline

---
+++ egs2/librispeech/speechlm1/run_tts.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+train_set="train_960"
+valid_set="dev"
+test_sets="test_clean"
+
+train_config=conf/train_delay_tts.yaml
+inference_config=conf/decode_tts_tencent.yaml
+
+codec_opts="--codec_choice inhouse"
+
+./speechlm.sh \
+    --task "tts" \
+    --fs 16000 \
+    --ngpu 4 \
+    --nj 16 \
+    --inference_nj 16 \
+    --nbest 10 \
+    --gpu_inference true \
+    --train_config ${train_config} \
+    --inference_config ${inference_config} \
+    --audio_format "flac.ark" \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" \
+    --min_wav_duration 0.1 \
+    --max_wav_duration 60.0 \
+    ${codec_opts} \
+    "$@"
