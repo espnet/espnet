e4af8fc2944b23949d56ea3b43815529f0f56cc9
Shikhar Bharadwaj <sbharad2@andrew.cmu.edu>
2024-11-21 15:34:54 -0500

    handle comment: https://github.com/espnet/espnet/pull/5915\#issuecomment-2492133351

--- egs2/clotho_v2/asr1/run.sh
+++ egs2/clotho_v2/asr1/run.sh
@@ -1,11 +1,39 @@
 #!/usr/bin/env bash
+# Runs pre-training
 set -euo pipefail
-# Runs fine-tuning
 
-wandb_init_args=""
-other_args="$@"
+timestamp=$(date "+%Y%m%d.%H%M%S")
 
-pt_tag=pt.initfix.bigbatch512.lr2e-4.weighted12layers.20241103.145125
+# Run pre-training
+
+./asr.sh \
+    --asr_tag pt.${timestamp} \
+    --asr_speech_fold_length 1600 \
+    --feats_normalize uttmvn \
+    --stage 1 \
+    --stop_stage 15 \
+    --ngpu 2 \
+    --gpu_inference true \
+    --nj 10 \
+    --inference_nj 1 \
+    --max_wav_duration 30 \
+    --token_type hugging_face \
+    --use_lm false \
+    --hugging_face_model_name_or_path "facebook/bart-base" \
+    --inference_args "--beam_size 10 --ctc_weight 0.0 --hugging_face_decoder True" \
+    --train_set pretrain \
+    --valid_set validation \
+    --test_sets "validation evaluation" \
+    --asr_config conf/beats_bart_pt.yaml \
+    --inference_asr_model valid.acc.best.pth \
+    --asr_args "${@}" \
+    --local_score_opts "exp/asr_pt.${timestamp}/inference_beam_size10_ctc_weight0.0_hugging_face_decoderTrue_asr_model_valid.acc.best"
+
+
+
+# Run fine-tuning
+
+pt_tag=${timestamp}
 
 ckpt_name=valid.acc.ave
 
@@ -17,7 +45,7 @@ ft_tag=${pt_tag}.${ckpt_name}
     --asr_tag ft.${ft_tag} \
     --feats_normalize uttmvn \
     --stage 1 \
-    --stop_stage 13 \
+    --stop_stage 15 \
     --asr_stats_dir ${expdir}/asr_stats_finetune \
     --ngpu 2 \
     --gpu_inference true \
