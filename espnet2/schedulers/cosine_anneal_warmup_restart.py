55bb448cb454796b696c80b33847cab721d65bf3
Masao-Someki <masao.someki@gmail.com>
2024-12-10 13:48:59 -0500

    Update docstrings in espnet2/schedulers

--- espnet2/schedulers/cosine_anneal_warmup_restart.py
+++ espnet2/schedulers/cosine_anneal_warmup_restart.py
@@ -13,16 +13,53 @@ from espnet2.schedulers.abs_scheduler import AbsBatchStepScheduler
 
 
 class CosineAnnealingWarmupRestarts(_LRScheduler, AbsBatchStepScheduler):
-    """Cosine Annealing Warmup Restart.
+    """
+        Cosine Annealing Warmup Restart.
 
-    optimizer (Optimizer): Wrapped optimizer.
-    first_cycle_steps (int): First cycle step size.
-    cycle_mult(float): Cycle steps magnification. Default: -1.
-    max_lr(float): First cycle's max learning rate. Default: 0.1.
-    min_lr(float): Min learning rate. Default: 0.001.
-    warmup_steps(int): Linear warmup step size. Default: 0.
-    gamma(float): Decrease rate of max learning rate by cycle. Default: 1.
-    last_epoch (int): The index of last epoch. Default: -1.
+    This scheduler implements the cosine annealing learning rate schedule with
+    warmup and restarts, allowing for dynamic adjustment of the learning rate
+    during training. It is similar to the official PyTorch `CosineAnnealWarmRestarts`,
+    but includes a linear warmup phase and the ability to scale the maximum learning
+    rate for each restart.
+
+    Attributes:
+        optimizer (Optimizer): Wrapped optimizer.
+        first_cycle_steps (int): First cycle step size.
+        cycle_mult (float): Cycle steps magnification. Default: 1.0.
+        max_lr (float): First cycle's maximum learning rate. Default: 0.1.
+        min_lr (float): Minimum learning rate. Default: 0.001.
+        warmup_steps (int): Linear warmup step size. Default: 0.
+        gamma (float): Decrease rate of maximum learning rate by cycle. Default: 1.0.
+        last_epoch (int): The index of last epoch. Default: -1.
+
+    Args:
+        optimizer (torch.optim.Optimizer): The optimizer for which to adjust the learning rate.
+        first_cycle_steps (int): Number of steps in the first cycle.
+        cycle_mult (float, optional): Factor to increase the cycle length. Default: 1.0.
+        max_lr (float, optional): Maximum learning rate for the first cycle. Default: 0.1.
+        min_lr (float, optional): Minimum learning rate. Default: 0.001.
+        warmup_steps (int, optional): Number of steps for the warmup phase. Default: 0.
+        gamma (float, optional): Factor to decrease the max learning rate each cycle.
+            Default: 1.0.
+        last_epoch (int, optional): The index of the last epoch. Default: -1.
+
+    Examples:
+        >>> from torch.optim import Adam
+        >>> optimizer = Adam(model.parameters(), lr=0.1)
+        >>> scheduler = CosineAnnealingWarmupRestarts(optimizer,
+        ...     first_cycle_steps=2000, warmup_steps=500, cycle_mult=1.0,
+        ...     max_lr=0.1, min_lr=0.001)
+        >>> for epoch in range(10000):
+        ...     train(...)
+        ...     scheduler.step(epoch)
+
+    Note:
+        The learning rate will linearly increase from `min_lr` to `max_lr` during
+        the warmup phase, then follow a cosine decay pattern until the end of the
+        cycle.
+
+    Todo:
+        - Implement more advanced logging features for monitoring learning rate changes.
     """
 
     def __init__(
@@ -56,12 +93,58 @@ class CosineAnnealingWarmupRestarts(_LRScheduler, AbsBatchStepScheduler):
         self.init_lr()
 
     def init_lr(self):
+        """
+            Initializes the learning rates for all parameter groups in the optimizer
+        to the minimum learning rate. This method is called during the initialization
+        of the CosineAnnealingWarmupRestarts class to set the base learning rates
+        before any training steps are performed.
+
+        This function modifies the learning rate of each parameter group in the
+        optimizer to ensure that they all start at the specified minimum learning
+        rate.
+
+        Attributes:
+            base_lrs (list): A list to store the base learning rates for each
+                              parameter group.
+
+        Examples:
+            >>> optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+            >>> scheduler = CosineAnnealingWarmupRestarts(optimizer,
+            ...                                             first_cycle_steps=10,
+            ...                                             warmup_steps=5)
+            >>> scheduler.init_lr()
+            >>> for param_group in optimizer.param_groups:
+            ...     print(param_group['lr'])  # Outputs: 0.001
+        """
         self.base_lrs = []
         for param_group in self.optimizer.param_groups:
             param_group["lr"] = self.min_lr
             self.base_lrs.append(self.min_lr)
 
     def get_lr(self):
+        """
+            Get the learning rate for the current step in the cycle.
+
+        This method calculates the learning rate based on the current step in the
+        cycle, applying a linear warmup for the initial steps and a cosine decay
+        for the remaining steps of the cycle.
+
+        Attributes:
+            base_lrs (list): The base learning rates for each parameter group.
+
+        Returns:
+            list: A list containing the learning rates for each parameter group.
+
+        Examples:
+            >>> scheduler = CosineAnnealingWarmupRestarts(optimizer, 10, warmup_steps=5)
+            >>> scheduler.get_lr()
+            [0.001, 0.001]  # Example output with min_lr = 0.001
+
+            >>> for epoch in range(15):
+            ...     scheduler.step(epoch)
+            ...     print(scheduler.get_lr())
+            [0.1, 0.1]  # Learning rate after warmup and during decay
+        """
         if self.step_in_cycle == -1:
             return self.base_lrs
         elif self.step_in_cycle < self.warmup_steps:
@@ -87,6 +170,27 @@ class CosineAnnealingWarmupRestarts(_LRScheduler, AbsBatchStepScheduler):
             ]
 
     def step(self, epoch=None):
+        """
+            Steps the learning rate according to the cosine annealing schedule with
+        warmup restarts. This method updates the learning rate of the optimizer
+        based on the current epoch or step within the cycle. It supports both
+        warmup steps and the cosine decay after the warmup period.
+
+        Args:
+            epoch (int, optional): The current epoch. If None, it will use the
+                last epoch plus one.
+
+        Examples:
+            >>> scheduler = CosineAnnealingWarmupRestarts(optimizer,
+            ...     first_cycle_steps=10, warmup_steps=5)
+            >>> for epoch in range(30):
+            ...     scheduler.step(epoch)
+            ...     print(f"Epoch {epoch}: Learning rate: {scheduler.get_lr()}")
+
+        Note:
+            This method must be called at the beginning of each epoch to update
+            the learning rate based on the current cycle and step.
+        """
         if epoch is None:
             epoch = self.last_epoch + 1
             self.step_in_cycle = self.step_in_cycle + 1
--- espnet2/schedulers/warmup_step_lr.py
+++ espnet2/schedulers/warmup_step_lr.py
@@ -10,22 +10,58 @@ from espnet2.schedulers.abs_scheduler import AbsBatchStepScheduler
 
 
 class WarmupStepLR(_LRScheduler, AbsBatchStepScheduler):
-    """The WarmupStepLR scheduler.
+    """
+        Step (with Warm up) learning rate scheduler module.
 
-    This scheduler is the combination of WarmupLR and StepLR:
+    The WarmupStepLR scheduler combines the functionalities of WarmupLR and StepLR:
 
     WarmupLR:
-        lr = optimizer.lr * warmup_step ** 0.5
-             * min(step ** -0.5, step * warmup_step ** -1.5)
+        lr = optimizer.lr * warmup_step ** 0.5 * min(step ** -0.5,
+        step * warmup_step ** -1.5)
+
     WarmupStepLR:
         if step <= warmup_step:
-            lr = optimizer.lr * warmup_step ** 0.5
-                 * min(step ** -0.5, step * warmup_step ** -1.5)
+            lr = optimizer.lr * warmup_step ** 0.5 * min(step ** -0.5,
+            step * warmup_step ** -1.5)
         else:
-            lr = optimizer.lr * (gamma ** (epoch//step_size))
+            lr = optimizer.lr * (gamma ** (epoch // step_size))
 
-    Note that the maximum lr equals to optimizer.lr in this scheduler.
+    Note:
+        The maximum learning rate equals optimizer.lr in this scheduler.
 
+    Attributes:
+        warmup_steps (Union[int, float]): The number of steps for the warmup phase.
+        steps_per_epoch (int): The number of steps per epoch.
+        step_size (int): The number of epochs between each learning rate decay.
+        gamma (float): The factor by which the learning rate is multiplied after
+            the warmup phase.
+        last_epoch (int): The index of the last epoch. Default is -1.
+
+    Args:
+        optimizer (torch.optim.Optimizer): The optimizer for which to schedule
+            the learning rate.
+        warmup_steps (Union[int, float]): The number of steps for the warmup
+            phase (default is 25000).
+        steps_per_epoch (int): The number of steps per epoch (default is 10000).
+        step_size (int): The number of epochs between each learning rate decay
+            (default is 1).
+        gamma (float): The factor by which the learning rate is multiplied after
+            the warmup phase (default is 0.1).
+        last_epoch (int): The index of the last epoch (default is -1).
+
+    Returns:
+        List[float]: A list of updated learning rates for each parameter group.
+
+    Examples:
+        >>> optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+        >>> scheduler = WarmupStepLR(optimizer, warmup_steps=1000,
+        ...                          steps_per_epoch=100, step_size=10, gamma=0.1)
+        >>> for epoch in range(50):
+        ...     for batch in data_loader:
+        ...         optimizer.zero_grad()
+        ...         loss.backward()
+        ...         optimizer.step()
+        ...         scheduler.step()
     """
 
     @typechecked
@@ -69,6 +105,45 @@ class WarmupStepLR(_LRScheduler, AbsBatchStepScheduler):
         )
 
     def get_lr(self):
+        """
+            Retrieves the learning rate for the current training step.
+
+        The learning rate is adjusted based on the warmup period and the step
+        schedule. During the warmup phase, the learning rate increases based on
+        the formula:
+
+            lr = optimizer.lr * warmup_step ** 0.5 * min(step ** -0.5, step *
+            warmup_step ** -1.5)
+
+        After the warmup period, the learning rate is decreased by a factor of
+        `gamma` every `step_size` epochs:
+
+            lr = optimizer.lr * (gamma ** (epoch // step_size))
+
+        Attributes:
+            step_num (int): The current step number.
+            epoch_num (int): The current epoch number.
+            warmup_steps (Union[int, float]): Number of warmup steps.
+            steps_per_epoch (int): Number of steps per epoch.
+            step_size (int): Number of epochs to wait before decreasing the
+                learning rate.
+            gamma (float): Factor by which the learning rate is multiplied.
+
+        Returns:
+            List[float]: The updated learning rates for the optimizer.
+
+        Examples:
+            >>> optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+            >>> scheduler = WarmupStepLR(optimizer, warmup_steps=1000,
+            ...                           steps_per_epoch=100, step_size=10,
+            ...                           gamma=0.1)
+            >>> for step in range(2000):
+            ...     scheduler.step()
+            ...     print(scheduler.get_lr())
+
+        Note:
+            The maximum learning rate equals to `optimizer.lr` in this scheduler.
+        """
         self.step_num += 1
         if self.step_num % self.steps_per_epoch == 0:
             self.epoch_num += 1
