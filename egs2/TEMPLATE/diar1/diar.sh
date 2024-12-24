49fece9612848f7ac616b6f0b272ab70c4d09c3b
qingzheng-wang <qingzhew@andrew.cmu.edu>
2024-12-24 15:37:28 -0500

    Resolve conflicts in diar.sh

--- egs2/TEMPLATE/diar1/diar.sh
+++ egs2/TEMPLATE/diar1/diar.sh
@@ -545,7 +545,9 @@ if ! "${skip_eval}"; then
             _inf_dir="${diar_exp}/diarized_${dset}"
             _dir="${diar_exp}/diarized_${dset}/scoring"
             mkdir -p "${_dir}"
-            scripts/utils/score_der.sh ${_dir} ${_inf_dir}/diarize.scp ${_data}/rttm ${collar} ${fs} ${frame_shift} ${subsampling}
+
+            scripts/utils/score_der.sh ${_dir} ${_inf_dir}/diarize.scp ${_data}/rttm \
+                --collar ${collar} --fs ${fs} --frame_shift ${frame_shift} --subsampling ${subsampling}
         done
 
         # Show results in Markdown syntax
