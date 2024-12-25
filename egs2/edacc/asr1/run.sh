fb887af1efffc7b37d4e2c6be438c1acdfb72e6c
uwanny <jianchengsunny@gmail.com>
2024-12-25 14:18:59 -0500

    update the training and decode config for wavLM, update run.sh

--- egs2/edacc/asr1/run.sh
+++ egs2/edacc/asr1/run.sh
@@ -7,25 +7,25 @@ set -o pipefail
 
 
 
-test_set="test"
+test_set="test test_sub"
 train_set="dev_train"
 valid_set="dev_non_train"
-nbpe=3436 # 3436 vocabulary size of bpe could cover all the sentence in the edacc dataset
+nbpe=3884 # 3884 vocabulary size of bpe could cover all the sentence in the edacc dataset
 asr_config=conf/train_asr_wavlm_transformer.yaml
 # asr_config=conf/train_asr_transformer_finetune.yaml
 inference_config=conf/decode_asr.yaml
 
 ./asr.sh \
-    --nj 16 \
+    --nj 32 \
     --inference_nj 2 \
     --gpu_inference true \
     --use_lm false \
     --lang en \
     --ngpu 1 \
     --nbpe "${nbpe}" \
-    --max_wav_duration 30 \
+    --max_wav_duration 20 \
     --speed_perturb_factors "0.9 1.0 1.1" \
-    --audio_format "flac.ark" \
+    --audio_format "wav" \
     --asr_config "${asr_config}" \
     --inference_config "${inference_config}" \
     --train_set "${train_set}" \
