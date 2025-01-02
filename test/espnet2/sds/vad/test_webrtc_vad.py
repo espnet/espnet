c82d7438f1934beee27c64492b6d415f6323c552
Siddhant <siddhana@v001.ib.bridges2.psc.edu>
2025-01-01 20:38:08 -0500

    Add VAD Unit test

--- test/espnet2/sds/vad/test_webrtc_vad.py
+++ test/espnet2/sds/vad/test_webrtc_vad.py
@@ -1,6 +1,9 @@
+import pytest
 import soundfile
 
 from espnet2.sds.vad.webrtc_vad import WebrtcVADModel
+
+pytest.importorskip("webrtcvad")
 
 
 def test_forward():
@@ -8,3 +11,4 @@ def test_forward():
     vad_model.warmup()
     x, rate = soundfile.read("test_utils/ctc_align_test.wav", dtype="int16")
     vad_model.forward(x, rate)
+    vad_model.forward(x, rate, binary=True)
