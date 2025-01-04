526b488178690c8fdfbc24809cad5df6c7116fdd
taiqihe <htq00007@gmail.com>
2025-01-04 15:46:18 -0500

    increase timeout, shorten test audio length

--- test/espnet2/asr/frontend/test_transformers.py
+++ test/espnet2/asr/frontend/test_transformers.py
@@ -6,14 +6,16 @@ from espnet2.asr.frontend.huggingface import HuggingFaceFrontend
 pytest.importorskip("transformers", minversion="4.43.0")
 
 
-@pytest.mark.timeout(4)
 @pytest.mark.parametrize(
     "model, fs",
     [
+        ("facebook/wav2vec2-xls-r-300m", 16000),
+        ("facebook/w2v-bert-2.0", 16000),
         ("facebook/hubert-base-ls960", 16000),
         ("microsoft/wavlm-base-plus", 16000),
     ],
 )
+@pytest.mark.timeout(10)
 def test_frontend_backward(model, fs):
     frontend = HuggingFaceFrontend(
         model,
@@ -21,7 +23,8 @@ def test_frontend_backward(model, fs):
         download_dir="./hf_cache",
         load_pretrained=False,
     )
-    wavs = torch.randn(2, fs, requires_grad=True)
-    lengths = torch.LongTensor([fs, fs])
+    test_length = int(0.3 * fs)
+    wavs = torch.randn(2, test_length, requires_grad=True)
+    lengths = torch.LongTensor([test_length, test_length])
     feats, f_lengths = frontend(wavs, lengths)
     feats.sum().backward()
