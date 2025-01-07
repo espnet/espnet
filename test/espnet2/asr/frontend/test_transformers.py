60253d0cc1ae8f9eb4c02d543c06a1e472b2c308
taiqihe <htq00007@gmail.com>
2025-01-07 12:13:41 -0500

    use custom single layer model for testing; reduce test input length

--- test/espnet2/asr/frontend/test_transformers.py
+++ test/espnet2/asr/frontend/test_transformers.py
@@ -9,13 +9,9 @@ pytest.importorskip("transformers", minversion="4.43.0")
 @pytest.mark.parametrize(
     "model, fs",
     [
-        ("facebook/wav2vec2-xls-r-300m", 16000),
-        ("facebook/w2v-bert-2.0", 16000),
-        ("facebook/hubert-base-ls960", 16000),
-        ("microsoft/wavlm-base-plus", 16000),
+        ("taiqihe/test-w2v-bert-dummy", 16000),
     ],
 )
-@pytest.mark.timeout(10)
 def test_frontend_backward(model, fs):
     frontend = HuggingFaceFrontend(
         model,
@@ -23,7 +19,7 @@ def test_frontend_backward(model, fs):
         download_dir="./hf_cache",
         load_pretrained=False,
     )
-    test_length = int(0.3 * fs)
+    test_length = 640
     wavs = torch.randn(2, test_length, requires_grad=True)
     lengths = torch.LongTensor([test_length, test_length])
     feats, f_lengths = frontend(wavs, lengths)
