063109c0e7f6a0a2f862d7574f749d3a7157a612
Yifan Peng <pengyf21@gmail.com>
2025-01-12 23:53:13 -0600

    refactor

--- tools/installers/install_lightning.sh
+++ tools/installers/install_lightning.sh
@@ -26,25 +26,31 @@ EOF
 
 echo "[INFO] torch_version=${torch_version}"
 
+# Determine the version of lightning
+# See: https://lightning.ai/docs/pytorch/stable/versioning.html#compatibility-matrix
+if $(pt_plus 2.2.0); then
+    lightning_version=2.5.0
+elif $(pt_plus 2.1.0); then
+    lightning_version=2.4.0
+elif $(pt_plus 2.0.0); then
+    lightning_version=2.3.0
+elif $(pt_plus 1.13.0); then
+    lightning_version=2.2.0
+elif $(pt_plus 1.12.0); then
+    lightning_version=2.1.0
+elif $(pt_plus 1.11.0); then
+    lightning_version=2.0.0
+else
+    echo "[ERROR] Our supported lightning requires pytorch>=1.11.0"
+    exit 1;
+fi
+
 cat >> lightning_constraints.txt << EOF
 torch==${torch_version}
+lightning==${lightning_version}
 EOF
 
-if $(pt_plus 2.2.0); then
-    pip install -c lightning_constraints.txt lightning==2.5.0
-elif $(pt_plus 2.1.0); then
-    pip install -c lightning_constraints.txt lightning==2.4.0
-elif $(pt_plus 2.0.0); then
-    pip install -c lightning_constraints.txt lightning==2.3.0
-elif $(pt_plus 1.13.0); then
-    pip install -c lightning_constraints.txt lightning==2.2.0
-elif $(pt_plus 1.12.0); then
-    pip install -c lightning_constraints.txt lightning==2.1.0
-elif $(pt_plus 1.11.0); then
-    pip install -c lightning_constraints.txt lightning==2.0.0
-else
-    echo "[WARNING] Our supported lightning requires pytorch>=1.11.0"
-fi
+python3 -m pip install -c lightning_constraints.txt lightning
 
 
 # Check the pytorch version is not changed from the original version
