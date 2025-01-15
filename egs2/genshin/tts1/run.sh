8e9c9575ccd8c31c26bc3574888138842ce3d666
whaledolphin6om <whaledolphin666@gmail.com>
2025-01-15 05:05:33 +0000

    [fix]fix conf problem

--- egs2/genshin/tts1/run.sh
+++ egs2/genshin/tts1/run.sh
@@ -21,7 +21,7 @@ train_set=tr_no_dev
 valid_set=dev
 test_sets="dev eval1"
 
-train_config=conf/train_vits.yaml
+train_config=conf/tuning/train_full_band_multi_spk_vits.yaml
 inference_config=conf/decode.yaml
 
 # g2p=g2p_en # Include word separator
