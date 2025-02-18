841e246955ae97084c8afbffd84509db2e8485f2
Shikhar Bharadwaj <sbharad2@andrew.cmu.edu>
2025-02-18 15:37:05 -0500

    add audio entailment recipe

---
+++ egs2/clotho_v2/cls1/run.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+train_set="development"
+valid_set="validation"
+test_sets="evaluation"
+cls_config=conf/beats_entailment.yaml
+
+timestamp=$(date "+%Y%m%d.%H%M%S")
+mynametag=${timestamp}
+
+storage_dir=. # change this to where you have space, if needed
+mkdir -p "${storage_dir}"
+
+./cls.sh \
+    --cls_tag "${mynametag}" \
+    --datadir "${storage_dir}/data" \
+    --dumpdir "${storage_dir}/dump" \
+    --expdir "${storage_dir}/exp" \
+    --text_input_filename hypothesis.txt \
+    --hugging_face_model_name_or_path bert-base-uncased \
+    --gpu_inference false \
+    --feats_normalize uttmvn \
+    --stage 5 \
+    --stop_stage 5 \
+    --nj 10 \
+    --label_fold_length 600 \
+    --inference_nj 1 \
+    --inference_model valid.acc.best.pth \
+    --cls_config "${cls_config}" \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" "$@"
