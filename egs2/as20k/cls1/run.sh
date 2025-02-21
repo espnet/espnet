801b14ca84a94a3139871211a2c8118c9292dea8
Shikhar Bharadwaj <sbharad2@andrew.cmu.edu>
2025-02-20 20:44:57 -0500

    temp commit: share configs that run with lightning trainer

--- egs2/as20k/cls1/run.sh
+++ egs2/as20k/cls1/run.sh
@@ -8,23 +8,35 @@ set -o pipefail
 train_set="train"
 valid_set="val"
 test_sets="eval"
-cls_config=conf/beats_cls.yaml
+# cls_config=conf/beats_cls.yaml
+cls_config=conf/beats_cls_lightning.yaml
 
 timestamp=$(date "+%Y%m%d.%H%M%S")
-mynametag=${timestamp}
+# mynametag=finl.EarLarge
+mynametag=finl.beats
 
 storage_dir=. # change this to where you have space, if needed
 mkdir -p "${storage_dir}"
+
+
+## plz change the the wandb_entity
+use_wandb=false
+wandb_project=BEATsAS20K
+wandb_args="--use_wandb ${use_wandb} --wandb_project ${wandb_project} --wandb_name ${mynametag} --wandb_entity shikhar"
+
 
 ./cls.sh \
     --cls_tag "${mynametag}" \
     --datadir "${storage_dir}/data" \
     --dumpdir "${storage_dir}/dump" \
     --expdir "${storage_dir}/exp" \
+    --classification_type multi-label \
     --gpu_inference false \
+    --use_lightning true \
     --feats_normalize uttmvn \
-    --stage 1 \
-    --stop_stage 10 \
+    --ngpu 2 \
+    --stage 6 \
+    --stop_stage 6 \
     --nj 10 \
     --label_fold_length 600 \
     --inference_nj 1 \
@@ -32,4 +44,5 @@ mkdir -p "${storage_dir}"
     --cls_config "${cls_config}" \
     --train_set "${train_set}" \
     --valid_set "${valid_set}" \
-    --test_sets "${test_sets}" "$@"
+    --test_sets "${test_sets}" \
+    --cls_args "${wandb_args}"
