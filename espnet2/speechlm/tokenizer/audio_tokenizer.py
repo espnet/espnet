fix normalization with fbank input

espnet2/speechlm/tokenizer/audio_tokenizer.py
@@ -115,7 +115,7 @@ class AudioTokenizer(AbsTokenizer):
         assert wavs.dim() == 3, "Input wavs should be in shape [B, n_sample, D]"
         if self.codec_choice == "beats" or self.codec_choice == "beats_random":
             wav_in = wavs.squeeze(2)  # [B, n_sample]
-            if wav_in.dim() == 2 and (wav_in.max() > 1.0 or wav_in.min() < -1.0):
+            if self.waveform_input and (wav_in.max() > 1.0 or wav_in.min() < -1.0):
                 # Beats expects input in range [-1, 1]
                 wav_in = wav_in.to(torch.float32)
                 wav_in = wav_in / 2**15

espnet2/speechlm/tokenizer/beats_tokenizer.py
@@ -502,7 +502,7 @@ class BeatsRandomTokenizer(nn.Module):
                     xs_pad, fbank_mean=self.fbank_mean, fbank_std=self.fbank_std
                 )
             else:
-                fbank = xs_pad
+                fbank = (xs_pad - self.fbank_mean) / (2 * self.fbank_std)
         n_mels = fbank.size(2)
         fbank = fbank.unsqueeze(1).float()
         features = self.patch_embedding(fbank)  # B, C, t, d=8