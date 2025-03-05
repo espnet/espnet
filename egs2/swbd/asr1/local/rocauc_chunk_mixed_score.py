cea64abdeea5fa4f3da1a898be396e8c95c6e3ae
“siddhu001” <siddhantarora1806@gmail.com>
2025-03-05 12:18:11 -0600

    Fix CI

--- egs2/swbd/asr1/local/rocauc_chunk_mixed_score.py
+++ egs2/swbd/asr1/local/rocauc_chunk_mixed_score.py
@@ -1,6 +1,10 @@
-# pred_file=
+import sys
+
+import numpy as np
+from sklearn.metrics import roc_auc_score
+
 word_dict = {}
-for line in open("data/Val_Two_Channel_Label_Mono_update.csv"):
+for line in open("data/Test_Two_Channel_Label_Mono.csv"):
     line1 = line.strip().split(",")
     if ("sw0" + line1[0]) not in word_dict:
         word_dict["sw0" + line1[0]] = {}
@@ -10,9 +14,6 @@ for line in open("data/Val_Two_Channel_Label_Mono_update.csv"):
         word_dict["sw0" + line1[0]][float(line1[1])][float(line1[2])] = line1
     else:
         print("error")
-        import pdb
-
-        pdb.set_trace()
 
 spk_dict = {}
 label_dict = {}
@@ -28,36 +29,29 @@ for id1 in word_dict:
     label_dict[id1] = label_text_str
 
 pred_file_arr = []
-str1 = "exp/asr_train_asr_whisper_3_uselast_raw_en_word/decode_asr_chunk_asr_model_valid.loss.ave/valid_full/logdir/output."
+exp_path_str1 = sys.argv[1]
 for k in range(1, 5):
-    pred_file_arr.append(open(str1 + str(k) + "/1best_recog/text"))
+    pred_file_arr.append(open(exp_path_str1 + str(k) + "/1best_recog/text"))
 
 # gt_file=
-gt_arr = [k.strip().split()[1:] for k in open("data/valid_full/text")]
+gt_arr = [k.strip().split()[1:] for k in open("data/test/text")]
 
-gt_id_arr = [k.split()[0] for k in open("data/valid_full/text")]
+gt_id_arr = [k.split()[0] for k in open("data/test/text")]
 pred_arr = []
 spk_arr = []
 count = 0
 for pred_file in pred_file_arr:
     for line in pred_file:
         assert line.split()[0] == gt_id_arr[count]
-        # if gt_id_arr[count]=="sw04153":
-        #     gt_arr[count]=["NA"]+gt_arr[count]
-        # import pdb;pdb.set_trace()
         assert len(spk_dict[gt_id_arr[count]]) == len(gt_arr[count])
-        # if gt_arr[count]!=label_dict[gt_id_arr[count]]:
-        #     import pdb;pdb.set_trace()
         assert gt_arr[count] == label_dict[gt_id_arr[count]]
         spk_arr.append(spk_dict[gt_id_arr[count]])
         pred_index_arr = line.strip().split()[1:]
         if len(pred_index_arr) < len(gt_arr[count]):
             assert len(gt_arr[count]) - len(pred_index_arr) == 1
-            # import pdb;pdb.set_trace()
             gt_arr[count] = gt_arr[count][: len(pred_index_arr)]
         elif len(pred_index_arr) > len(gt_arr[count]):
             assert len(pred_index_arr) - len(gt_arr[count]) == 1
-            # import pdb;pdb.set_trace()
             pred_index_arr = pred_index_arr[: len(gt_arr[count])]
         count += 1
         for subline in pred_index_arr:
@@ -83,13 +77,9 @@ for k in range(len(gt_arr)):
         pred_arr_update.append(pred_arr[count])
 print(len(gt_arr_total))
 print(len(pred_arr_update))
-import pdb
 
-pdb.set_trace()
 labels = ["C", "NA", "I", "BC", "T"]
 score = 0
-import numpy as np
-from sklearn.metrics import roc_auc_score
 
 gt_arr = np.array(gt_arr_total)
 pred_arr = np.array(pred_arr_update)
