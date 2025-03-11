caf5ff9530d606105ee1234522e3f35d8b3af69c
Jiatong <728307998@qq.com>
2025-03-11 15:03:07 -0400

    Update st_inference.py

--- espnet2/bin/st_inference.py
+++ espnet2/bin/st_inference.py
@@ -469,9 +469,7 @@ class Speech2Text:
         Tuple[Optional[str], List[str], List[int], Union[Hypothesis, TransHypothesis]]
     ], Tuple[List[
         Tuple[Optional[str], List[str], List[int], Union[Hypothesis, TransHypothesis]]
-    ], List[
-        Tuple[Optional[str], List[str], List[int], Union[Hypothesis, TransHypothesis]]
-    ]]]:
+    ], Any]:
         """Inference
 
         Args:
