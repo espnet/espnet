2d139f7e596b6b2b47ef694e148567ecd954ae77
Shikhar Bharadwaj <shikhar.ssu@gmail.com>
2025-03-19 16:41:35 -0500

    fsd: dataprep fix

--- egs2/fsd50k/cls1/local/data.sh
+++ egs2/fsd50k/cls1/local/data.sh
@@ -6,15 +6,11 @@
 
 
 # general configuration
-stage=0       # start from 0 if you need to start from data preparation
-stop_stage=100
 SECONDS=0
-
 log() {
     local fname=${BASH_SOURCE[1]##*/}
     echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
 }
-
 
 # Set bash to 'debug' mode, it will exit on :
 # -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
@@ -22,7 +18,8 @@ set -e
 set -u
 set -o pipefail
 
-. ./utils/parse_options.sh
+
+DATADIR=$1
 
 log "data preparation started"
 
@@ -32,9 +29,8 @@ if [ -z "${FSD50K}" ]; then
 fi
 mkdir -p ${FSD50K}
 
-if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+if ! [ -f "${FSD50K}/download.done" ]; then
     log "stage 0: Download Data to ${FSD50K}"
-
     # Downloading from zenodo records
     # https://zenodo.org/records/4060432
     fnames=(
@@ -48,17 +44,15 @@ if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
         "FSD50K.eval_audio.z01"
         "FSD50K.eval_audio.zip"
         "FSD50K.metadata.zip"
+        "FSD50K.ground_truth.zip"
     )
     for fname in "${fnames[@]}"; do
         echo "Downloading ${fname} now"
         wget -O ${FSD50K}/${fname} \
             https://zenodo.org/records/4060432/files/${fname}?download=1       
     done
-fi
 
-if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     log "stage 1: Unzip data in ${FSD50K}"
-
     zip -s 0 ${FSD50K}/FSD50K.dev_audio.zip --out ${FSD50K}/dev.zip
     zip -s 0 ${FSD50K}/FSD50K.eval_audio.zip --out ${FSD50K}/eval.zip
 
@@ -67,32 +61,31 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
         "eval.zip"
         "FSD50K.metadata.zip"
         "FSD50K.doc.zip"
+        "FSD50K.ground_truth.zip"
     )
     for fname in "${fnames[@]}"; do
         unzip ${FSD50K}/${fname} -d ${FSD50K}
     done
+    touch ${FSD50K}/download.done
+else
+    log "Data is already downloaded at ${FSD50K}"
 fi
 
-if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
-    log "stage 2: Prepare data in ${FSD50K}"
+log "Prepare data from ${FSD50K}"
+python3 local/prep_fsd50k.py \
+    --train ${FSD50K}/FSD50K.dev_audio \
+    --test ${FSD50K}/FSD50K.eval_audio \
+    --metadata ${FSD50K}/FSD50K.ground_truth \
+    --output ${DATADIR}
 
-    python3 local/prep_fsd50k.py \
-        --train ${FSD50K}/FSD50K.dev_audio \
-        --test ${FSD50K}/FSD50K.eval_audio \
-        --metadata ${FSD50K}/FSD50K.ground_truth \
-        --output data
-fi
 
-if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
-    log "stage 3: Validate data"
-
-    for x in train val test; do
-        for f in text wav.scp utt2spk; do
-            sort data/${x}/${f} -o data/${x}/${f}
-        done
-        utils/utt2spk_to_spk2utt.pl data/${x}/utt2spk > "data/${x}/spk2utt"
-        utils/validate_data_dir.sh --no-feats data/${x} || exit 1
+log "stage 3: Validate data"
+for x in train val test; do
+    for f in text wav.scp utt2spk; do
+        sort ${DATADIR}/${x}/${f} -o ${DATADIR}/${x}/${f}
     done
-fi
+    utils/utt2spk_to_spk2utt.pl ${DATADIR}/${x}/utt2spk > "${DATADIR}/${x}/spk2utt"
+    utils/validate_data_dir.sh --no-feats ${DATADIR}/${x} || exit 1
+done
 
 log "Successfully finished. [elapsed=${SECONDS}s]"
