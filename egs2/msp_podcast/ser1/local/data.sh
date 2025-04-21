093b1c2a9b410be2239fe317af6a49095bc4e6af
Bo-Hao Su <bsu5@br014.ib.bridges2.psc.edu>
2025-04-21 17:41:48 -0400

    adding examples for running msp_podcast on the primary speech emotion recongition task excluding O and X emotions

---
+++ egs2/msp_podcast/ser1/local/data.sh
@@ -0,0 +1,184 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+log() {
+    local fname=${BASH_SOURCE[1]##*/}
+    echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
+}
+SECONDS=0
+stage=1
+stop_stage=100
+
+lowercase=false
+# Convert into lowercase if "true".
+remove_punctuation=false
+# Remove punctuation (except apostrophes) if "true".
+# Note that punctuation normalization will be performed in the "false" case.
+remove_tag=false
+# Remove [TAGS] (e.g.[LAUGHTER]) if "true".
+remove_emo=
+# Remove the utterances with the specified emotional labels
+# emotional labels: ang (anger), hap (happiness), exc (excitement), sad (sadness),
+# fru (frustration), fea (fear), sur (surprise), neu (neutral), and xxx (other)
+convert_to_sentiment=false
+# for sentiment (positive, negative and neutral) analysis
+# mapping from emotion to sentiment is as follows:
+# Positive: hap, exc, sur
+# Negative: ang, sad, fru, fea
+# Neutral: neu
+
+#data
+datadir=/ocean/projects/cis210027p/shared/corpora/msp_podcast_v1.12
+# msp_podcast
+#  |_ readme.txt
+#  |_ Partitions.txt
+#  |_ Speaker_ids.txt
+#  |_ ForceAligned/
+#  |_ Labels/
+#  |_ Transcripts/
+#  |_ Audios/
+#      |_ ...<wav files for each utterance>
+# In this recipe
+# Training/Development/Testing sets followed the setting in Partitions.txt
+# Training set includes 112712 utterances
+# Development set includes 31961 utterances
+# Test1 set includes 44395 utterances
+# Test2 set includes 14868 utterances
+# Test3 set includes 3200 utterances
+# Download data from here:
+# https://ecs.utdallas.edu/research/researchlabs/msp-lab/MSP-Podcast.html
+
+log "$0 $*"
+. utils/parse_options.sh
+
+if [ $# -ne 0 ]; then
+    log "Error: No positional arguments are required."
+    exit 2
+fi
+
+. ./path.sh
+. ./cmd.sh
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    log "stage 1: MSP_Podcast Data Preparation"
+    # This process may take a few minutes for the first run
+    # Remove "data/${tmp}/tmp.done" if you want to start all over again
+    if [ -n "${remove_emo}" ]; then
+        log "Remove ${remove_emo} from emotional labels"
+        tmp="tmp/remove_emo"
+    else
+        log "Use all 10 emotional labels"
+        tmp=tmp
+    fi
+    if [ ! -e data/${tmp}/tmp.done ];then
+        mkdir -p data/{train,valid,test1,test2}
+        mkdir -p data/${tmp}
+        echo -n "" > data/${tmp}/wav.scp; echo -n "" > data/${tmp}/utt2spk; echo -n "" > data/${tmp}/text
+        tail -n +2 "${datadir}/Labels/labels_consensus.csv" | while IFS=',' read -r FileName EmoClass EmoAct EmoVal EmoDom SpkrID Gender Split_Set
+        do
+            utt_id=$(basename ${FileName} .wav)
+            spk_id=${SpkrID}
+            gender=${Gender}
+            emo_cate=${EmoClass}
+            emo_act=${EmoAct}
+            emo_val=${EmoVal}
+            emo_dom=${EmoDom}
+            split_set=${Split_Set}
+            file="${datadir}/Audios/${FileName}"
+            words=$(cat "${datadir}"/Transcripts/${utt_id}.txt | sed "s/^.*\]:\s\(.*\)$/\1/g")
+            if ! eval "echo ${remove_emo} | grep -q ${emo_cate}" ; then
+                # for sentiment analysis
+                if [ ${convert_to_sentiment} = "true" ]; then
+                    words2=$(echo "$words" | perl local/prepare_sentiment.pl)
+                    if [ ${emo} = "hap" ] || [ ${emo} = "exc" ] || [ ${emo} = "sur" ]; then
+                        echo "${utt_id} Positive ${words2}" >> data/${tmp}/text
+                        echo "${utt_id} ${file}" >> data/${tmp}/wav.scp
+                        echo "${utt_id} ${utt_id}" >> data/${tmp}/utt2spk
+                    elif [ ${emo} = "ang" ] || [ ${emo} = "sad" ] || [ ${emo} = "fru" ] || [ ${emo} = "fea" ]; then
+                        echo "${utt_id} Negative ${words2}" >> data/${tmp}/text
+                        echo "${utt_id} ${file}" >> data/${tmp}/wav.scp
+                        echo "${utt_id} ${utt_id}" >> data/${tmp}/utt2spk
+                    elif [ ${emo} = "neu" ];then
+                        echo "${utt_id} Neutral ${words2}" >> data/${tmp}/text
+                        echo "${utt_id} ${file}" >> data/${tmp}/wav.scp
+                        echo "${utt_id} ${utt_id}" >> data/${tmp}/utt2spk
+                    fi
+                else
+                    echo "${utt_id} ${words}" >> data/${tmp}/text
+                    echo "${utt_id} ${file}" >> data/${tmp}/wav.scp
+                    echo "${utt_id} ${spk_id}" >> data/${tmp}/utt2spk
+                    echo "${utt_id} ${gender}" >> data/${tmp}/utt2gen
+                    echo "${utt_id} ${split_set}" >> data/${tmp}/split_set
+                    echo "${utt_id} ${emo_cate}" >> data/${tmp}/utt2emo
+                fi
+            fi
+        done
+        dos2unix data/${tmp}/wav.scp; dos2unix data/${tmp}/text; dos2unix data/${tmp}/split_set; dos2unix data/${tmp}/utt2emo; dos2unix data/${tmp}/utt2spk; dos2unix data/${tmp}/utt2gen
+        # utils/utt2spk_to_spk2utt.pl data/${tmp}/utt2spk > "data/${tmp}/spk2utt"
+        touch data/${tmp}/tmp.done
+    fi
+
+    for file in wav.scp text utt2spk utt2emo; do
+        while read -r line; do
+            utt_id=$(echo "$line" | awk '{print $1}')
+            split_set=$(grep "^${utt_id} " "data/${tmp}/split_set" | awk '{print $2}')
+
+            # If utt_id is found in utt2split and split_set is known
+            if [ "$split_set" = "Train" ]; then
+                echo "$line" >> data/train/${file}
+            elif [ "$split_set" = "Development" ]; then
+                echo "$line" >> data/valid/${file}
+            elif [ "$split_set" = "Test1" ]; then
+                echo "$line" >> data/test1/${file}
+            elif [ "$split_set" = "Test2" ]; then
+                echo "$line" >> data/test2/${file}
+            else
+                log "Unknown split set: $split_set for utt_id: $utt_id"
+            fi
+        done < "data/${tmp}/${file}"
+    done
+fi
+
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+    if [ ${convert_to_sentiment} != "true" ]; then
+        log "stage 2: MSP-Pocast Transcript Conversion"
+        utils/combine_data.sh --extra-files utt2emo --skip_fix true data/test data/test1 data/test2
+        mkdir -p data/{train,valid,test1,test2,test}/{original,tmp}/
+        for dset in train valid test1 test2 test; do
+            cp data/${dset}/text -t data/${dset}/original/
+            if ${lowercase}; then
+                log "lowercase ${dset}"
+                perl local/lowercase.pl < data/${dset}/text > data/${dset}/tmp/text
+                cp data/${dset}/tmp/text data/${dset}/text
+            fi
+            if ${remove_punctuation}; then
+                log "remove_punctuation ${dset}"
+                perl local/remove_punctuation.pl < data/${dset}/text > data/${dset}/tmp/text
+                cp data/${dset}/tmp/text data/${dset}/text
+            fi
+            if ${remove_tag}; then
+                log "remove_tag ${dset}"
+                perl local/remove_tag.pl < data/${dset}/text > data/${dset}/tmp/text
+                cp data/${dset}/tmp/text data/${dset}/text
+            fi
+            #Remove extra space and normalize punctuation
+            perl local/normalize_punctuation.pl < data/${dset}/text > data/${dset}/tmp/text
+            cp data/${dset}/tmp/text data/${dset}/text
+        done
+    fi
+
+    utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
+    utils/utt2spk_to_spk2utt.pl data/valid/utt2spk > data/valid/spk2utt
+    utils/utt2spk_to_spk2utt.pl data/test1/utt2spk > data/test1/spk2utt
+    utils/utt2spk_to_spk2utt.pl data/test2/utt2spk > data/test2/spk2utt
+    utils/utt2spk_to_spk2utt.pl data/test/utt2spk > data/test/spk2utt
+
+    # for dset in train valid test1 test2 test; do
+    #     utils/validate_data_dir.sh --no-feats --no-spk-sort data/${dset} || exit 1
+    # done
+fi
+log "Successfully finished. [elapsed=${SECONDS}s]"
