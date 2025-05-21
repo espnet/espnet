eb57f6b28e62df24e30f703421218eab8aa0b6db
Masao-Someki <masao.someki@gmail.com>
2025-05-20 23:29:32 -0500

    Config directory fixed.

---
+++ egs3/librispeech_100/asr1_new_conf/path.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export HF_HOME=~/workspace/espnet3/hub
+export PYTHONPATH=${PYTHONPATH}:../../../
+source ../../../tools/activate_python.sh
+
+export LIBRISPEECH=data/librispeech
+
+# export WANDB_API_KEY=0cdbe23a746157c701dfa8ea691597ad65e2d400
+# wandb login
