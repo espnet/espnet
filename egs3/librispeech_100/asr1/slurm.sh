af7931a0eba205e648c19e8e0a34d00b1742e31e
Masao-Someki <masao.someki@gmail.com>
2025-05-23 13:41:24 -0500

    Format recipe

--- egs3/librispeech_100/asr1/slurm.sh
+++ egs3/librispeech_100/asr1/slurm.sh
@@ -5,21 +5,16 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=32G
 #SBATCH --time=48:00:00
-#SBATCH -p gpuA40x4,gpuA100x4
-#SBATCH --account bbjs-delta-gpu
+#SBATCH -p <>
+#SBATCH --account <>
 #SBATCH --gres=gpu:2
 
 
 source path.sh
 
 # We can also create submit.sh for the actual command.
-# python run.py \
-#     --train_config conf/train_ctc_gpu2.yaml \
-#     --eval_config inference.yaml \
-#     # --collect_stats
-#     # --train_tokenizer \
-srun python train.py \
-    --config train_gpu2.yaml
-    # --eval_config inference.yaml \
-    # --collect_stats
-    # --train_tokenizer \
+python run.py \
+    --train_config conf/train_ctc_gpu2.yaml \
+    --eval_config inference.yaml \
+    --collect_stats \
+    --train_tokenizer
