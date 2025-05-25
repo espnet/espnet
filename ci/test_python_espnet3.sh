936a849751ca4ae3f3e5c39f62610cef73b35171
Fhrozen <nyalta21@gmail.com>
2025-05-25 15:57:46 +0000

    fixes pycode espnet3

--- ci/test_python_espnet3.sh
+++ ci/test_python_espnet3.sh
@@ -2,12 +2,20 @@
 
 set -euo pipefail
 
-# exclude="egs2/TEMPLATE/asr1/utils,egs2/TEMPLATE/asr1/steps,egs2/TEMPLATE/tts1/sid,doc,tools,test_utils/bats-core,test_utils/bats-support,test_utils/bats-assert"
+if [ ! -f ./tools/activate_python.sh ]; then
+    touch ./tools/activate_python.sh
+fi
+
+exclude="egs2/TEMPLATE/asr1/utils,"
+exclude+="egs2/TEMPLATE/asr1/steps,"
+exclude+="egs2/TEMPLATE/tts1/sid,doc,tools,"
+exclude+="test_utils/bats-core,test_utils/bats-support,"
+exclude+="test_utils/bats-assert"
 
 # flake8
-# "$(dirname $0)"/test_flake8.sh
+"$(dirname $0)"/test_flake8.sh
 # pycodestyle
-# pycodestyle --exclude "${exclude}" --show-source --show-pep8
+pycodestyle --exclude "${exclude}" --show-source --show-pep8
 
 pytest -q test/espnet3/test_espnet3.py
 
