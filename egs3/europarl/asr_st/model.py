d2fe05119af7346db2f1366300fd97a24c21d44b
Fhrozen <nyalta21@gmail.com>
2025-05-26 11:45:39 +0000

    pycodestyle fixes

---
+++ egs3/europarl/asr_st/model.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+
+from espnet2.bin.s2t_inference import Speech2Text
+
+
+class OWSMFinetune(nn.Module):
+    def __init__(self, model_tag):
+        super().__init__()
+        owsm_model = Speech2Text.from_pretrained(model_tag)
+        self.model = owsm_model.s2t_model
+
+    def forward(
+        self,
+        speech,
+        speech_lengths,
+        text,
+        text_lengths,
+        text_ctc,
+        text_ctc_lengths,
+        text_prev,
+        text_prev_lengths,
+    ):
+        return self.model(
+            speech,
+            speech_lengths,
+            text,
+            text_lengths,
+            text_ctc,
+            text_ctc_lengths,
+            text_prev,
+            text_prev_lengths,
+        )
+
+    def collect_feats(
+        self,
+        speech: torch.Tensor,
+        speech_lengths: torch.Tensor,
+        **kwargs,
+    ):
+        return {"feats": speech, "feats_lengths": speech_lengths}
