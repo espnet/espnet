6934da03a561ef45488ea7213c89afabe3d1f3b2
Masao-Someki <masao.someki@gmail.com>
2025-06-09 13:57:30 -0500

    Exclude espnet-3 CI for now
    - I wanted this PR to be focusing on installation part.
    - I will push CI for espnet-3 on other PR.

--- ci/test_python_espnet3.sh
+++ ci/test_python_espnet3.sh
@@ -13,9 +13,9 @@ exclude+="test_utils/bats-assert,espnet2,espnet,test_utils/utils3"
 # flake8
 "$(dirname $0)"/test_flake8.sh
 # pycodestyle
-pycodestyle --exclude "${exclude}" --show-source --show-pep8
+# pycodestyle --exclude "${exclude}" --show-source --show-pep8
 
-pytest -q test/espnet3/test_espnet3.py
+#pytest -q test/espnet3/test_espnet3.py
 
 echo "=== report ==="
 coverage report
