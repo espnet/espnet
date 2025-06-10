479f8846fb58a9732d922704b715dfefabb53bb6
ftshijt <ftshijt@gmail.com>
2025-06-10 21:26:48 +0000

    clean run.sh setup

--- egs2/urgent24/uni_versa1/run.sh
+++ egs2/urgent24/uni_versa1/run.sh
@@ -6,13 +6,8 @@ set -u
 set -o pipefail
 
 ./uni_versa.sh \
-    --nj 8 \
-    --inference_nj 2 \
-    --dumpdir dump_ark \
-    --audio_format flac.ark \
     --train_config conf/train_universa.yaml \
     --inference_config conf/decode_universa.yaml \
-    --dumpdir dump_ark \
     --train_set train \
     --valid_set dev \
     --test_sets "dev test" \
