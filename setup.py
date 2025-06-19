c2120697897d3a025d0fc25230e892318f0b3330
Fhrozen <nyalta21@gmail.com>
2025-06-19 21:12:46 +0900

    Refactor setup.py and add pyproject.toml for improved package management

--- setup.py
+++ setup.py
@@ -2,9 +2,7 @@
 
 """ESPnet setup script."""
 
-import os
-
-from setuptools import find_packages, setup
+from setuptools import setup
 
 requirements = {
     "install": [
@@ -127,22 +125,7 @@ extras_require = {
     k: v for k, v in requirements.items() if k not in ["install", "setup"]
 }
 
-dirname = os.path.dirname(__file__)
-version_file = os.path.join(dirname, "espnet", "version.txt")
-with open(version_file, "r") as f:
-    version = f.read().strip()
 setup(
-    name="espnet",
-    version=version,
-    url="http://github.com/espnet/espnet",
-    author="Shinji Watanabe",
-    author_email="shinjiw@ieee.org",
-    description="ESPnet: end-to-end speech processing toolkit",
-    long_description=open(os.path.join(dirname, "README.md"), encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    license="Apache Software License",
-    packages=find_packages(include=["espnet*"]),
-    package_data={"espnet": ["version.txt"]},
     # #448: "scripts" is inconvenient for developping because they are copied
     # scripts=get_all_scripts('espnet/bin'),
     install_requires=install_requires,
@@ -150,17 +133,4 @@ setup(
     tests_require=tests_require,
     extras_require=extras_require,
     python_requires=">=3.7.0",
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Science/Research",
-        "Operating System :: POSIX :: Linux",
-        "License :: OSI Approved :: Apache Software License",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
 )
