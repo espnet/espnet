fix average_checkpoints for load weight_only=False

utils/average_checkpoints.py
@@ -70,7 +70,11 @@ def main():
 
         # sum
         for path in last:
-            states = torch.load(path, map_location=torch.device("cpu"))["model"]
+            states = torch.load(
+                path,
+                map_location=torch.device("cpu"),
+                weights_only=False
+            )["model"]
             if avg is None:
                 avg = states
             else: