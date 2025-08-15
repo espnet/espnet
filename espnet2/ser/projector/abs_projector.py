98149152d0ff4f313a4699a802ad098ca5020128
BohaoSu <s0983813130@gmail.com>
2025-08-15 16:24:50 -0400

    resuing abs_projector from SID by creating symbolic link

--- espnet2/ser/projector/abs_projector.py
+++ espnet2/ser/projector/abs_projector.py
@@ -1,13 +1,1 @@
-from abc import ABC, abstractmethod
-
-import torch
-
-
-class AbsProjector(torch.nn.Module, ABC):
-    @abstractmethod
-    def output_size(self) -> int:
-        raise NotImplementedError
-
-    @abstractmethod
-    def forward(self, utt_embd: torch.Tensor) -> torch.Tensor:
-        raise NotImplementedError
+../../spk/projector/abs_projector.py
