f1618e644df3f5e4e1ccbad541326156e4dcc1cd
BohaoSu <s0983813130@gmail.com>
2025-08-15 16:23:03 -0400

    taking care of padded region, which is different from SID in the dimension of mean axis

--- espnet2/ser/pooling/mean_pooling.py
+++ espnet2/ser/pooling/mean_pooling.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import torch
 
 from espnet2.ser.pooling.abs_pooling import AbsPooling
@@ -18,7 +20,31 @@ class MeanPooling(AbsPooling):
     def output_size(self):
         return self._output_size
 
-    def forward(self, x):
-        x = torch.mean(x, dim=1)
+    def forward(
+        self,
+        x: torch.Tensor,
+        feat_lengths: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        """Forward pass of mean pooling.
+
+        Args:
+            x: Input feature tensor of shape (batch_size, feature_dim, seq_len)
+            feat_lengths: Optional tensor of shape (batch_size,) containing
+                          the valid length of each sequence before padding
+
+        Returns:
+            x: Utterance-level embeddings of shape (batch_size, feature_dim)
+        """
+        if feat_lengths is not None:
+            # Pooling over unpadded frames
+            x = torch.stack(
+                [
+                    torch.mean(x[i, :, : int(l.item())], dim=1)
+                    for i, l in enumerate(feat_lengths)
+                ],
+                dim=0,
+            )
+        else:
+            x = torch.mean(x, dim=1)
 
         return x
