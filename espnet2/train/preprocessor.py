93ae3ce87871b83257be41cad9340cd5209f717f
BohaoSu <s0983813130@gmail.com>
2025-08-15 19:25:27 -0400

    to do ci again

--- espnet2/train/preprocessor.py
+++ espnet2/train/preprocessor.py
@@ -688,8 +688,13 @@ class SERPreprocessor(CommonPreprocessor):
             tokens = self.tokenizer.text2tokens(text)
             text_ints = self.token_id_converter.tokens2ids(tokens)
             data[self.text_name] = np.array(text_ints, dtype=np.int64)
+<<<<<<< HEAD
         # Taking care of the emotion labels in SER task,
         # and create the mapping for the emotions as well
+=======
+        #Take care of the emotion labels in SER task
+        #Create the mapping for the emotions as well
+>>>>>>> 03c102c31b (to do ci again)
         if "emo" in data:
             emo = data["emo"]
             emo_ints = self.emo2label[emo]
