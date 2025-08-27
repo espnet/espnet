66abc875c6a64fb75df02196ad0796154f15eac4
Chien-yu Huang <cyhuang1997@gmail.com>
2025-08-27 23:36:29 +0800

    Add test

---
+++ test/espnet2/ps2st/test_espnet_model.py
@@ -0,0 +1,30 @@
+import pytest
+
+import numpy as np
+import torch
+
+from espnet2.ps2st.espnet_model import ESPnetQwen2AudioModel
+from espnet2.text.qwen2audio_tokenizer import Qwen2AudioTokenizer
+
+
+@pytest.mark.parametrize("model_name", ["Qwen/Qwen2-Audio-7B-Instruct"])
+def test_espnet_model_inference(model_name):
+    model = ESPnetQwen2AudioModel(model_name)
+    tokenizer = Qwen2AudioTokenizer(model_name)
+    assert model is not None
+    assert tokenizer is not None
+
+    # We don't test forward function because it's a dummy function here.
+    # Instead, we test the inference function.
+    # Qwen2 scorer is also tested within inference function.
+    text = "welcome to japari park."
+    speech = np.zeros((16000))
+    inputs = tokenizer.create_multimodal_query(
+        text_input=text, audio_input=([speech], 16000)
+    )
+
+    for key in inputs.keys():
+        inputs[key] = torch.from_numpy(inputs[key])
+
+    output = model.inference(**inputs)
+    assert output is not None
---
+++ test/espnet2/text/test_qwen2_tokenizer.py
@@ -0,0 +1,30 @@
+import pytest
+import numpy as np
+import torch
+
+from espnet2.text.qwen2audio_tokenizer import Qwen2AudioTokenizer
+
+
+@pytest.mark.parametrize("model_name", ["Qwen/Qwen2-Audio-7B-Instruct"])
+def test_qwen2audio_tokenizer(model_name):
+    tokenizer = Qwen2AudioTokenizer(model_name)
+    assert tokenizer is not None
+
+    text = "welcome to japari park."
+
+    tokens = tokenizer.text2tokens(text)
+
+    rec_text = tokenizer.tokens2text(tokens)
+
+    assert text == rec_text
+
+    # text-only
+    output = tokenizer.create_multimodal_query(text)
+    assert output is not None
+
+    # text + speech
+    speech = np.zeros((16000))
+    output = tokenizer.create_multimodal_query(
+        text_input=text, audio_input=([speech], 16000)
+    )
+    assert output is not None
