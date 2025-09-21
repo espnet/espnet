9c9874e26379825d23304ce63369aadfd9a4baa9
Alexander Metzger <alexander.le@outlook.dk>
2025-09-21 00:27:59 -0700

    support loading float64 mps models as float32 + test case

---
+++ test/espnet2/bin/test_macos_mps_model.py
@@ -0,0 +1,31 @@
+import torch
+import pytest
+
+from pathlib import Path
+
+from espnet2.bin.s2t_inference import Speech2Text
+
+# NOTE: these fixture imports are necessary to have a config file in place
+from test.espnet2.bin.test_s2t_inference import token_list, s2t_config_file
+
+
+@pytest.mark.skipif(
+    not torch.backends.mps.is_available(),
+    reason="This test is specific to macOS with Metal Performance Shaders enabled",
+)
+def test_macos_mps_model(s2t_config_file, tmp_path: Path):
+    # create a float64 model using the CPU since it supports it
+    dummy_model = torch.nn.Sequential(
+        torch.nn.Linear(80, 10),
+        torch.nn.ReLU(),
+        torch.nn.Linear(10, 5),
+    ).to(dtype=torch.float64)
+    torch.save(dummy_model.state_dict(), tmp_path / "s2t" / "model.pth")
+
+    # load it with mps and make sure it gets loaded as float32 since mps doesn't support float64
+    s2t = Speech2Text.from_pretrained(
+        s2t_model_file=str(tmp_path / "s2t" / "model.pth"),
+        device="mps",
+    )
+    assert s2t.device == "mps"
+    assert s2t.dtype == "float32"
