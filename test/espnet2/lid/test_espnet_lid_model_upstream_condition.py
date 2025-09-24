2548fa06340ad32973c7047d3c8e922f0b0a1ac3
Qingzheng-Wang <qingzhew@andrew.cmu.edu>
2025-09-24 02:11:36 -0700

    Fix test_aamsoftmax_sc_topk_lang2vec_loss.

--- test/espnet2/lid/test_espnet_lid_model_upstream_condition.py
+++ test/espnet2/lid/test_espnet_lid_model_upstream_condition.py
@@ -1,62 +1,9 @@
 import pytest
 import torch
 
-from espnet2.asr.frontend.default import DefaultFrontend
-from espnet2.asr.specaug.specaug import SpecAug
-from espnet2.layers.utterance_mvn import UtteranceMVN
-from espnet2.lid.espnet_model_upstream_condition import (
-    ESPnetLIDModel,
-    ESPnetLIDUpstreamConditionModel,
-)
+from espnet2.lid.espnet_model_upstream_condition import ESPnetLIDUpstreamConditionModel
 from espnet2.lid.frontend.s3prl_condition import S3prlFrontendCondition
 from espnet2.lid.loss.aamsoftmax_sc_topk_lang2vec import AAMSoftmaxSCTopKLang2Vec
-from espnet2.spk.encoder.ecapa_tdnn_encoder import EcapaTdnnEncoder
-from espnet2.spk.pooling.chn_attn_stat_pooling import ChnAttnStatPooling
-from espnet2.spk.projector.rawnet3_projector import RawNet3Projector
-
-default_frontend = DefaultFrontend(
-    fs=16000,
-    n_mels=80,
-)
-
-specaug = SpecAug(
-    apply_time_warp=True,
-    time_warp_window=5,
-    time_warp_mode="bicubic",
-    apply_freq_mask=True,
-    freq_mask_width_range=[0, 30],
-    num_freq_mask=2,
-    apply_time_mask=True,
-    time_mask_width_range=[0, 40],
-    num_time_mask=2,
-)
-
-normalize = UtteranceMVN()
-
-ecapa_tdnn_encoder = EcapaTdnnEncoder(
-    default_frontend.output_size(), model_scale=2, ndim=16, output_size=24
-)
-
-chan_attn_stat_pooling = ChnAttnStatPooling(input_size=ecapa_tdnn_encoder.output_size())
-
-rawnet3_projector = RawNet3Projector(
-    input_size=chan_attn_stat_pooling.output_size(), output_size=8
-)
-
-aamsoftmax_sc_topk_lang2vec_loss = AAMSoftmaxSCTopKLang2Vec(
-    nout=rawnet3_projector.output_size(),
-    nclasses=10,
-    scale=15,
-    margin=0.3,
-    easy_margin=False,
-    K=3,
-    mp=0.06,
-    k_top=5,
-    do_lm=False,
-    lang2vec_dim=299,
-    lang2vec_type="geo",
-    lang2vec_weight=0.2,
-)
 
 
 def test_import_espnet_lid_upstream_condition_model():
@@ -98,35 +45,41 @@ def test_lid_model_upstream_condition_skip():
 
 
 @pytest.mark.parametrize("training", [True, False])
-def test_aamsoftmax_sc_topk_lang2vec_loss(training):
-    inputs = torch.randn(2, 8000)
-    ilens = torch.LongTensor([8000, 7800])
-    lid_labels = torch.randint(0, 10, (2,))
-    lid_model = ESPnetLIDModel(
-        frontend=default_frontend,
-        specaug=specaug,
-        normalize=normalize,
-        encoder=ecapa_tdnn_encoder,
-        pooling=chan_attn_stat_pooling,
-        projector=rawnet3_projector,
-        loss=aamsoftmax_sc_topk_lang2vec_loss,
-    )
-    lang2vecs = torch.randn(2, 299)
+@pytest.mark.parametrize("lang2vec_type", ["geo", "phonology_knn", "syntax_knn", "inventory_knn"])
+def test_aamsoftmax_sc_topk_lang2vec_loss(training, lang2vec_type):
 
-    if training:
-        lid_model.train()
+    inputs = torch.randn(2, 198, requires_grad=True)
+    lid_labels = torch.randint(0, 10, (2,)).long()
+    if lang2vec_type == "geo":
+        lang2vecs = torch.randn(2, 299)
+        lang2vec_dim = 299 # mock data
     else:
-        lid_model.eval()
+        lang2vecs = torch.randn(2, 28)
+        lang2vec_dim = 28 # mock data
+
+    aamsoftmax_sc_topk_lang2vec_loss = AAMSoftmaxSCTopKLang2Vec(
+        nout=198,  # Use fixed embedding dimension
+        nclasses=10,
+        scale=15,
+        margin=0.3,
+        easy_margin=False,
+        K=3,
+        mp=0.06,
+        k_top=5,
+        do_lm=False,
+        lang2vec_dim=lang2vec_dim,
+        lang2vec_type=lang2vec_type,
+        lang2vec_weight=0.2,
+    )
 
     kwargs = {
-        "speech": inputs,
-        "speech_lengths": ilens,
-        "lid_labels": lid_labels,
-        "lang2vecs": lang2vecs,
+        "input": inputs,
+        "label": lid_labels,
+        "lang2vec": lang2vecs,
     }
 
     if training:
-        loss, *_ = lid_model(**kwargs)
+        loss, *_ = aamsoftmax_sc_topk_lang2vec_loss(**kwargs)
         loss.backward()
     else:
-        loss, *_ = lid_model(**kwargs)
+        loss, *_ = aamsoftmax_sc_topk_lang2vec_loss(**kwargs)
