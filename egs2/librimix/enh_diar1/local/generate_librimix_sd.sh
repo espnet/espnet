8c3c6e74e248b1fdba6d69a66f2a978f320e8173
Fhrozen <nyalta21@gmail.com>
2025-09-28 20:22:34 +0900

    update link

--- egs2/librimix/enh_diar1/local/generate_librimix_sd.sh
+++ egs2/librimix/enh_diar1/local/generate_librimix_sd.sh
@@ -53,7 +53,7 @@ function wham() {
 	if ! test -e $wham_dir; then
 		echo "Download wham_noise into $storage_dir"
 		# If downloading stalls for more than 20s, relaunch from previous state.
-		wget -c --tries=0 --read-timeout=20 https://storage.googleapis.com/whisper-public/wham_noise.zip -P $storage_dir
+		wget -c --tries=0 --read-timeout=20 https://my-bucket-a8b4b49c25c811ee9a7e8bba05fa24c7.s3.amazonaws.com/wham_noise.zip -P $storage_dir
 		unzip -qn $storage_dir/wham_noise.zip -d $storage_dir
 		rm -rf $storage_dir/wham_noise.zip
 	fi
