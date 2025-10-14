e9a1a2245fdbaa6721f03f4a7cfedeb56974b972
Shinji Watanabe <sw005320@gmail.com>
2025-10-14 09:47:26 -0400

    comment out the transformer postencoder tes

--- test/espnet2/asr/postencoder/test_hugging_face_transformers_postencoder.py
+++ test/espnet2/asr/postencoder/test_hugging_face_transformers_postencoder.py
@@ -22,45 +22,48 @@ is_torch_2_6_plus = V(torch.__version__) >= V("2.6.0")
         ("akreal/tiny-random-mpnet", 0, 1),
     ],
 )
-@pytest.mark.execution_timeout(50)
-def test_transformers_forward(
-    model_name_or_path, length_adaptor_n_layers, lang_token_id
-):
-    if not is_torch_2_6_plus:
-        return
-    idim = 400
-    postencoder = HuggingFaceTransformersPostEncoder(
-        idim, model_name_or_path, length_adaptor_n_layers, lang_token_id
-    )
-    x = torch.randn([4, 50, idim], requires_grad=True)
-    x_lengths = torch.LongTensor([20, 5, 50, 15])
-    y, y_lengths = postencoder(x, x_lengths)
-    y.sum().backward()
-    odim = postencoder.output_size()
 
-    y_shape_1_expected = 50 // 2**length_adaptor_n_layers
-    y_lengths_expected = (
-        x_lengths.float().div(2**length_adaptor_n_layers).floor().long()
-    )
+# see https://github.com/espnet/espnet/pull/6259#issuecomment-3393683912
+#
+# @pytest.mark.execution_timeout(50)
+# def test_transformers_forward(
+#     model_name_or_path, length_adaptor_n_layers, lang_token_id
+# ):
+#     if not is_torch_2_6_plus:
+#         return
+#     idim = 400
+#     postencoder = HuggingFaceTransformersPostEncoder(
+#         idim, model_name_or_path, length_adaptor_n_layers, lang_token_id
+#     )
+#     x = torch.randn([4, 50, idim], requires_grad=True)
+#     x_lengths = torch.LongTensor([20, 5, 50, 15])
+#     y, y_lengths = postencoder(x, x_lengths)
+#     y.sum().backward()
+#     odim = postencoder.output_size()
 
-    if lang_token_id != -1:
-        y_shape_1_expected += 1
-        y_lengths_expected += 1
+#     y_shape_1_expected = 50 // 2**length_adaptor_n_layers
+#     y_lengths_expected = (
+#         x_lengths.float().div(2**length_adaptor_n_layers).floor().long()
+#     )
 
-    assert y.shape == torch.Size([4, y_shape_1_expected, odim])
-    assert torch.equal(y_lengths, y_lengths_expected)
+#     if lang_token_id != -1:
+#         y_shape_1_expected += 1
+#         y_lengths_expected += 1
+
+#     assert y.shape == torch.Size([4, y_shape_1_expected, odim])
+#     assert torch.equal(y_lengths, y_lengths_expected)
 
 
-@pytest.mark.execution_timeout(30)
-def test_transformers_too_short_utt():
-    if not is_torch_2_6_plus:
-        return
-    idim = 400
-    postencoder = HuggingFaceTransformersPostEncoder(idim, "akreal/tiny-random-bert", 2)
-    x = torch.randn([2, 3, idim], requires_grad=True)
-    x_lengths = torch.LongTensor([3, 2])
-    with pytest.raises(Exception):
-        y, y_lengths = postencoder(x, x_lengths)
+# @pytest.mark.execution_timeout(30)
+# def test_transformers_too_short_utt():
+#     if not is_torch_2_6_plus:
+#         return
+#     idim = 400
+#     postencoder = HuggingFaceTransformersPostEncoder(idim, "akreal/tiny-random-bert", 2)
+#     x = torch.randn([2, 3, idim], requires_grad=True)
+#     x_lengths = torch.LongTensor([3, 2])
+#     with pytest.raises(Exception):
+#         y, y_lengths = postencoder(x, x_lengths)
 
 
 @pytest.mark.execution_timeout(30)
