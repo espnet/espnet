7b0ccce5eaa6c0e0df4646e6df94c3c986a600d9
Masao-Someki <masao.someki@gmail.com>
2025-11-17 07:54:21 -0600

    Rename directory name from asr1 to asr

---
+++ egs3/librispeech_100/asr/path.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+export PYTHONPATH=../../../:../../TEMPLATE/asr1:$(pwd):${PYTHONPATH}
+
+source ../../../tools/activate_python.sh
--- egs3/librispeech_100/asr1/path.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-export PYTHONPATH=../../TEMPLATE/asr1:$(pwd):${PYTHONPATH}
-
-source ../../../tools/activate_python.sh
