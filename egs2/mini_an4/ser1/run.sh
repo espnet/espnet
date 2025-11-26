a58c752de671dc96ac12b176b3b346110848bece
BohaoSu <s0983813130@gmail.com>
2025-11-25 22:30:24 -0500

    re-run again, since local is ok

--- egs2/mini_an4/ser1/run.sh
+++ egs2/mini_an4/ser1/run.sh
@@ -11,7 +11,7 @@ test_sets="test test1 test2 valid"
 
 ser_config=conf/train_ser.yaml
 inference_config=conf/decode_asr.yaml
-local_data_opts="--dummy_data true"
+local_data_opts="--dummy_data true --skip_eval true"
 # local_data_opts: 5 following options can be set (default=false)
 #--lowercase
 #   Convert transcripts into lowercase if "true".
