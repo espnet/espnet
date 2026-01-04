5a853aa6a99ded72a1510cccb7da86b401c3b48c
Fhrozen <nyalta21@gmail.com>
2026-01-04 22:10:37 +0900

    replace symbolics with original files

--- egs2/yesno/asr1/local/create_yesno_txt.pl
+++ egs2/yesno/asr1/local/create_yesno_txt.pl
@@ -1,1 +1,16 @@
-../../../../egs/yesno/asr1/local/create_yesno_txt.pl
+#!/usr/bin/env perl
+
+$in_list = $ARGV[0];
+
+open IL, $in_list;
+
+while ($l = <IL>)
+{
+	chomp($l);
+	$l =~ s/\.wav//;
+	$trans = $l;
+	$trans =~ s/0/NO/g;
+	$trans =~ s/1/YES/g;
+	$trans =~ s/\_/ /g;
+	print "$l $trans\n";
+}
