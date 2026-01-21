75cd8ab9e4f5f990f0e5718b7a05cc73496032d6
Masao-Someki <masao.someki@gmail.com>
2026-01-21 01:09:31 -0500

    Bug fixed

--- egs3/mini_an4/asr/run.py
+++ egs3/mini_an4/asr/run.py
@@ -1,5 +1,5 @@
 from egs3.TEMPLATE.asr.run import (
-    DEFAULT_STAGES,
+    ALL_STAGES,
     build_parser,
     main,
     parse_cli_and_stage_args,
@@ -8,10 +8,10 @@ from espnet3.systems.asr.system import ASRSystem
 
 if __name__ == "__main__":
     parser = build_parser(
-        stages=DEFAULT_STAGES,
+        stages=ALL_STAGES,
         add_arguments=None,
     )
-    args, stages_to_run = parse_cli_and_stage_args(parser, stages=DEFAULT_STAGES)
+    args, stages_to_run = parse_cli_and_stage_args(parser, stages=ALL_STAGES)
 
     main(
         args=args,
