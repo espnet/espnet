f1b273d5d19701932e61fb961b78f9c049383b93
Masao-Someki <masao.someki@gmail.com>
2026-01-27 15:11:02 -0500

    Fixed configs

--- egs3/mini_an4/asr/run.py
+++ egs3/mini_an4/asr/run.py
@@ -1,6 +1,5 @@
 from egs3.TEMPLATE.asr.run import (
     DEFAULT_STAGES,
-    ALL_STAGES,
     build_parser,
     main,
     parse_cli_and_stage_args,
@@ -9,14 +8,13 @@ from espnet3.systems.asr.system import ASRSystem
 
 if __name__ == "__main__":
     parser = build_parser(
-        stages=ALL_STAGES,
-        default_stages=DEFAULT_STAGES,
+        stages=DEFAULT_STAGES,
         add_arguments=None,
     )
-    args, _ = parse_cli_and_stage_args(parser, stages=ALL_STAGES)
+    args, _ = parse_cli_and_stage_args(parser, stages=DEFAULT_STAGES)
 
     main(
         args=args,
         system_cls=ASRSystem,
-        stages=ALL_STAGES,
+        stages=DEFAULT_STAGES,
     )
