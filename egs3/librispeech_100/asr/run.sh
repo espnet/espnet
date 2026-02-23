69c5a6c2c5485113212148b07bf52634c8a292bb
Masao-Someki <masao.someki@gmail.com>
2026-02-22 19:33:20 -0500

    Fixed naming conventions in recipe, and temporal bug fix

--- egs3/librispeech_100/asr/run.sh
+++ egs3/librispeech_100/asr/run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+#SBATCH --partition=general
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32G
+#SBATCH--time=24:00:00 
 
 source path.sh
 
 python run.py \
-    --stages all \
+    --stages infer measure \
     --train_config conf/tuning/train_e_branchformer.yaml \
     --infer_config conf/inference.yaml \
-    --metric_config conf/metric.yaml \
-    # --dry_run
+    --measure_config conf/metric.yaml 
