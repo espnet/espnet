532e842c886453d12a2f88f52e38e36443ba9d0b
Fhrozen <nyalta21@gmail.com>
2026-02-23 10:26:46 +0900

    Enhance random choice compatibility for chunk lengths in iterators

--- espnet2/iterators/chunk_iter_factory.py
+++ espnet2/iterators/chunk_iter_factory.py
@@ -205,7 +205,10 @@ class ChunkIterFactory(AbsIterFactory):
                 cache_chunks = cache_chunks_dict[category].setdefault(0, {})
                 Z, N, W, S = 0, 1, L, 0
             else:
-                W = int(state.choice(chunk_lengths, 1))
+                # Numpy compatible random choice for chunk length (with replacement)
+                _choice = state.choice(chunk_lengths, 1)
+                _choice = _choice if isinstance(_choice, int) else _choice.item()
+                W = int(_choice)
                 cache_id_list = cache_id_list_dict[category].setdefault(W, [])
                 cache_chunks = cache_chunks_dict[category].setdefault(W, {})
 
