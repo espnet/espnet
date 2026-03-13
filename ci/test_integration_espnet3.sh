9bd60782afb9c3f321317254cdb8a12bac0ad0a5
Masao-Someki <masao.someki@gmail.com>
2026-03-13 05:37:09 -0400

    Fixed CI bug in integration test

--- ci/test_integration_espnet3.sh
+++ ci/test_integration_espnet3.sh
@@ -19,17 +19,17 @@ cd ./egs3/mini_an4/asr
 gen_dummy_coverage
 echo "==== [ESPnet3] ASR ===="
 source ./path.sh
-run_with_train_config() {
-    local train_config=$1
+run_with_training_config() {
+    local training_config=$1
     local runner=$2
-    local infer_config=$3
+    local inference_config=$3
 
-    ln -sfn "${train_config}" conf/training.yaml
+    ln -sfn "${training_config}" conf/training.yaml
     ${python} "${runner}" \
         --stages create_dataset train_tokenizer collect_stats train infer measure \
-        --train_config conf/training.yaml \
-        --infer_config "${infer_config}" \
-        --measure_config conf/measure.yaml
+        --training_config conf/training.yaml \
+        --inference_config "${inference_config}" \
+        --metrics_config conf/measure.yaml
     rm -rf exp data
 }
 
@@ -41,12 +41,12 @@ debug_configs=(
     training_asr_transducer.yaml
 )
 
-for train_config in "${debug_configs[@]}"; do
-    run_with_train_config "${train_config}" run.py conf/inference.yaml
+for training_config in "${debug_configs[@]}"; do
+    run_with_training_config "${training_config}" run.py conf/inference.yaml
 done
 
 # We need seprate inference config for transducer task
-run_with_train_config \
+run_with_training_config \
     training_transducer_asr_conformer_rnnt.yaml \
     run.py \
     conf/inference_transducer.yaml
