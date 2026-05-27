cd14cda7f83241e612cb160e5545ba54793dbb7a
Elias Naske <naske.elias@outlook.com>
2026-05-27 12:00:23 +0000

    Add collect_feats() to GraniteSpeechModel, minor fixes

--- egs3/librispeech_100/asr/src/granite_speech.py
+++ egs3/librispeech_100/asr/src/granite_speech.py
@@ -1,19 +1,20 @@
 from espnet2.torch_utils.device_funcs import force_gatherable
-from espnet2.asr.espnet_model import ESPnetASRModel
+from espnet2.train.abs_espnet_model import AbsESPnetModel
 
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor
-from transformers.feature_extraction_utils import BatchFeature
 import torch
 from typing import Tuple, Dict, List, Any
 
 
-class GraniteSpeechModel(ESPnetASRModel):
+class GraniteSpeechModel(AbsESPnetModel):
     def __init__(self, model_tag: str, user_prompt: str):
+        super().__init__()
         self.model = AutoModelForSpeechSeq2Seq.from_pretrained(model_tag)
+        self.model.train()
 
     def forward(
         self,
-        batch: BatchFeature,
+        **batch
     ) -> Tuple[torch.Tensor, Dict[str, torch.Tensor], torch.Tensor]:
         outputs = self.model(**batch)
         loss = outputs.loss
@@ -23,9 +24,14 @@ class GraniteSpeechModel(ESPnetASRModel):
         loss, stats, weight = force_gatherable((loss, stats, batch_size), loss.device)
         return loss, stats, weight
 
+    def collect_feats(self, **batch):
+        feats = batch["input_features"]
+        feats_lengths = batch["input_features_mask"].sum(dim=-1)
+        return {"feats": feats, "feats_lengths": feats_lengths}
+
 
 class GraniteSpeechCollateFn:
-    def __init__(self, model_tag: str, user_prompt: str):
+    def __init__(self, model_tag: str, user_prompt: str, **kwargs):
         self.processor = AutoProcessor.from_pretrained(model_tag)
         self.tokenizer = self.processor.tokenizer
 
@@ -35,14 +41,14 @@ class GraniteSpeechCollateFn:
         self.prompt = self.tokenizer.apply_chat_template(
             chat, tokenize=False, add_generation_prompt=True)
 
-    def __call__(self, data: List[Tuple[str, Dict[str, Any]]]) -> BatchFeature:
-        n = len(data)
-        uids = [data[i][0] for i in range(n)]
-        speech = [data[i][1]["speech"] for i in range(n)]
-        texts = [data[i][1]["text"] for i in range(n)]
+    def __call__(self, data: List[Dict[str, Any]]) -> Tuple[List[str], Dict[str, torch.Tensor]]:
+        batch_size = len(data)
+        uids = [data[i]["utt_id"] for i in range(batch_size)]
+        speech = [data[i]["speech"] for i in range(batch_size)]
+        texts = [data[i]["text"] for i in range(batch_size)]
 
         inputs = self.processor(
-            [self.prompt] * n,
+            [self.prompt] * batch_size,
             audio=speech,
             return_tensors="pt",
             padding=True,
