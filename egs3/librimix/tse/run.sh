79b7072ff82621fc60f07bfe7515c2a694c6d032
Wangyou Zhang <C0me_On@163.com>
2026-05-29 20:06:31 +0800

    Refactor TSE recipe to adapt to the latest design

--- egs3/librimix/tse/run.sh
+++ egs3/librimix/tse/run.sh
@@ -4,8 +4,9 @@ source path.sh
 
 python run.py \
     --stages all \
-    --train_config conf/tuning/train_td_speakerbeam_16k.yaml \
-    --infer_config conf/infer.yaml \
-    --measure_config conf/measure.yaml \
-    --publish_config conf/publish.yaml \
-    --demo_config conf/demo.yaml
+    --training_config conf/tuning/training_td_speakerbeam_16k.yaml \
+    --inference_config conf/infererence.yaml \
+    --metrics_config conf/metrics.yaml \
+    --publication_config conf/publication.yaml \
+    --demo_config conf/demo.yaml \
+    "$@"
