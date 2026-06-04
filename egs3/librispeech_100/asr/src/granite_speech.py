fd42423b3134ac1956017094a8b324dfa251bf98
Elias Naske <naske.elias@outlook.com>
2026-06-04 17:55:42 +0000

    Add model wrapper for inference stage

--- egs3/librispeech_100/asr/src/granite_speech.py
+++ egs3/librispeech_100/asr/src/granite_speech.py
@@ -1,16 +1,15 @@
 from espnet2.torch_utils.device_funcs import force_gatherable
-from espnet2.train.abs_espnet_model import AbsESPnetModel
 
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor
 import torch
+import lightning
 from typing import Tuple, Dict, List, Any
 
 
-class GraniteSpeechModel(AbsESPnetModel):
-    def __init__(self, model_tag: str, user_prompt: str):
+class GraniteSpeechModel(lightning.LightningModule):
+    def __init__(self, model_tag: str, **kwargs):
         super().__init__()
         self.model = AutoModelForSpeechSeq2Seq.from_pretrained(model_tag)
-        self.model.train()
 
     def forward(
         self,
@@ -28,6 +27,39 @@ class GraniteSpeechModel(AbsESPnetModel):
         feats = batch["input_features"]
         feats_lengths = batch["input_features_mask"].sum(dim=-1)
         return {"feats": feats, "feats_lengths": feats_lengths}
+
+
+class GraniteSpeechInferenceSession(lightning.LightningModule):
+    def __init__(self, model_tag: str, user_prompt: str, checkpoint_path: str | None = None, **kwargs):
+        super().__init__()
+        if checkpoint_path:
+            self.model = GraniteSpeechModel.load_from_checkpoint(
+                checkpoint_path, model_tag=model_tag, user_prompt=user_prompt)
+        else:
+            self.model = GraniteSpeechModel(model_tag, user_prompt)
+
+        self.processor = AutoProcessor.from_pretrained(model_tag)
+        self.tokenizer = self.processor.tokenizer
+
+        chat = [
+            {"role": "user", "content": user_prompt},
+        ]
+        self.prompt = self.tokenizer.apply_chat_template(
+            chat, tokenize=False, add_generation_prompt=True)
+
+    def forward(self, speech):
+        inputs = self.processor(self.prompt, speech,
+                                return_tensors="pt").to(self.model.device)
+        outputs = self.model.model.generate(
+            **inputs, max_new_tokens=200, do_sample=False, num_beams=1)
+
+        num_input_tokens = inputs["input_ids"].shape[-1]
+        new_tokens = outputs[0, num_input_tokens:].unsqueeze(0)
+        output_text = self.tokenizer.batch_decode(
+            new_tokens, add_special_tokens=False, skip_special_tokens=True
+        )
+
+        return [output_text]
 
 
 class GraniteSpeechCollateFn:
