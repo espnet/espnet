e02e6f79766aa12a13327af8b537543966613500
Rishab Alagharu <rishabsaia@gmail.com>
2026-06-07 11:03:05 -0400

    ta_openslr127: add LM + speed perturbation

--- egs2/ta_openslr127/asr1/run.sh
+++ egs2/ta_openslr127/asr1/run.sh
@@ -11,6 +11,7 @@ test_sets="dev_ta test_ta"
 
 asr_config=conf/train_asr.yaml
 inference_config=conf/decode_asr.yaml
+lm_config=conf/train_lm.yaml
 
 ./asr.sh \
     --lang ta \
@@ -22,11 +23,14 @@ inference_config=conf/decode_asr.yaml
     --nbpe 1000 \
     --bpemode unigram \
     --max_wav_duration 30 \
+    --speed_perturb_factors "0.9 1.0 1.1" \
     --feats_type raw \
-    --use_lm false \
+    --use_lm true \
+    --lm_config "${lm_config}" \
     --asr_config "${asr_config}" \
     --inference_config "${inference_config}" \
     --train_set "${train_set}" \
     --valid_set "${valid_set}" \
     --test_sets "${test_sets}" \
-    --bpe_train_text "data/${train_set}/text" "$@"
+    --bpe_train_text "data/${train_set}/text" \
+    --lm_train_text "data/${train_set}/text" "$@"
