9d675c78d9897c2ec349db54e2f27a66aee69bc0
Elias Naske <naske.elias@outlook.com>
2026-06-19 13:43:15 +0000

    Add global rank check in HFCheckpointSaveCallback

--- espnet3/components/callbacks/hf_callbacks.py
+++ espnet3/components/callbacks/hf_callbacks.py
@@ -8,10 +8,10 @@ class HFCheckpointSaveCallback(Callback):
         super().__init__()
         self.dirpath = dirpath
 
-    def on_validation_end(self, trainer, pl_module):
-        if not isinstance(pl_module.model, AbsHFTrainingWrapper):
-            raise AttributeError(
-                f"Failed to save Hugging Face model. {pl_module}.model must be an instance of AbsHFTrainingWrapper."
-            )
-
-        pl_module.model.save_pretrained(self.dirpath)
+    def on_train_end(self, trainer, pl_module):
+        if trainer.global_rank == 0:
+            if not isinstance(pl_module.model, AbsHFTrainingWrapper):
+                raise AttributeError(
+                    f"Failed to save Hugging Face model. {pl_module}.model must be an instance of AbsHFTrainingWrapper."
+                )
+            pl_module.model.save_pretrained(self.dirpath)
